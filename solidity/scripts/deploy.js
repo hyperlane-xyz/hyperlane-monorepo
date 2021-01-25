b22a7a86aec2c85761f44de9789619fbb06ecf06
ltchang <luke@clabs.co>
2021-01-25 13:24:21 -0800

    fixed camel casing in deploy.js for hardhat compile

--- solidity/scripts/deploy.js
+++ solidity/scripts/deploy.js
@@ -3,5 +3,5 @@ const { types } = require("hardhat/config");
 task("deploy-home")
   .addParam("slip44", "The origin chain SLIP44 ID", undefined, types.int)
   .addParam("updater", "The origin chain updater", undefined, types.string)
-  .addParam("current root", "The current root")
+  .addParam("currentRoot", "The current root")
   .setAction(async (args) => {});
