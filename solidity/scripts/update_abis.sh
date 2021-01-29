6db1f76325608cda1b0eccb499455d5981bdcbf0
Luke Tchang <ltchang@stanford.edu>
2021-01-29 13:06:59 -0800

    feature:  `npm run compile` updates abis in optics-base (#27)
    * feature:  updates abis through new update_abis.sh script in solidity directory
    
    * refactor: deleted old update_abis script in rust/optics-base and moved update script to solidity/scripts

--- solidity/scripts/update_abis.sh
+++ solidity/scripts/update_abis.sh
@@ -1,6 +1,3 @@
 #!/bin/zsh
-
-cd ../../solidity && \
-npm run compile && \
 cat artifacts/contracts/Replica.sol/ProcessingReplica.json| jq .abi > ../rust/optics-base/src/abis/ProcessingReplica.abi.json && \
 cat artifacts/contracts/Home.sol/Home.json| jq .abi > ../rust/optics-base/src/abis/Home.abi.json
