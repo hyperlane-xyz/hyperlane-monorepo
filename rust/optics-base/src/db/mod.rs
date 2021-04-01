2f943b5c20724be2c6c87cad0e9d1edcb3691fdd
James Prestwich <prestwich@clabs.co>
2021-04-01 13:55:00 -0700

    fix: respect the clippy

--- rust/optics-base/src/db/mod.rs
+++ rust/optics-base/src/db/mod.rs
@@ -8,14 +8,15 @@ pub mod persistence;
 pub use persistence::UsingPersistence;
 
 /// Opens db at `db_path` and creates if missing
+#[tracing::instrument(err)]
 pub fn from_path(db_path: &str) -> Result<DB> {
     let path = Path::new(db_path).canonicalize()?;
 
     let mut opts = Options::default();
     opts.create_if_missing(true);
 
-    Ok(DB::open(&opts, &path).wrap_err(format!(
+    DB::open(&opts, &path).wrap_err(format!(
         "Failed to open db path {}, canonicalized as {:?}",
         db_path, path
-    ))?)
+    ))
 }
