1490207c7d0d63ab99d8cf81532b3cc8ac2432d3
Luke Tchang <ltchang@stanford.edu>
2021-04-01 14:24:55 -0700

    feature: adds unrenroll permissioning to UsingOptics (#219)
    * feature: adds permissioned unenrollReplica to UsingOptics
    
    * feature: adds setWatcherPermission for owner
    
    * fix: add check that replica updater matches signed
    
    * fix: rename watcherPermissions
    
    * fix: only takes domain and updater
    
    * fix: adds domainHash to watcher signed data
    
    * refactor: recovers watcher from sig and checks permissions after
    
    * fix: unenrollReplica reverts instead of silent failure and events emitted

--- solidity/optics-core/contracts/UsingOptics.sol
+++ solidity/optics-core/contracts/UsingOptics.sol
@@ -2,13 +2,29 @@
 pragma solidity >=0.6.11;
 
 import "./Home.sol";
+import "./Replica.sol";
 
+import "@openzeppelin/contracts/cryptography/ECDSA.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@summa-tx/memview-sol/contracts/TypedMemView.sol";
 
 abstract contract UsingOptics is Ownable {
-    mapping(address => uint32) public replicas;
+    mapping(address => uint32) public replicaToDomain;
+    mapping(uint32 => address) public domainToReplica;
     Home public home;
+
+    // watcher address => replica domain => has/doesn't have permission
+    mapping(address => mapping(uint32 => bool)) watcherPermissions;
+
+    event ReplicaEnrolled(uint32 indexed domain, address replica);
+
+    event ReplicaUnenrolled(uint32 indexed domain, address replica);
+
+    event WatcherPermissionSet(
+        uint32 indexed domain,
+        address watcher,
+        bool access
+    );
 
     // solhint-disable-next-line no-empty-blocks
     constructor() Ownable() {}
@@ -22,20 +38,52 @@ abstract contract UsingOptics is Ownable {
         _;
     }
 
+    function unenrollReplica(
+        uint32 _domain,
+        address _updater,
+        bytes memory _signature
+    ) external {
+        address _replica = domainToReplica[_domain];
+        require(_replica != address(0), "!replica exists");
+        require(Replica(_replica).updater() == _updater, "!current updater");
+
+        address _watcher =
+            recoverWatcherFromSig(_domain, _replica, _updater, _signature);
+        require(watcherPermissions[_watcher][_domain], "!valid watcher");
+
+        unenrollReplica(_replica);
+    }
+
     function isReplica(address _replica) public view returns (bool) {
-        return replicas[_replica] != 0;
+        return replicaToDomain[_replica] != 0;
     }
 
-    function enrollReplica(uint32 _domain, address _replica) public onlyOwner {
-        replicas[_replica] = _domain;
+    function ownerEnrollReplica(address _replica, uint32 _domain)
+        public
+        onlyOwner
+    {
+        unenrollReplica(_replica);
+        replicaToDomain[_replica] = _domain;
+        domainToReplica[_domain] = _replica;
+
+        emit ReplicaEnrolled(_domain, _replica);
     }
 
-    function unenrollReplica(address _replica) public onlyOwner {
-        replicas[_replica] = 0;
+    function ownerUnenrollReplica(address _replica) public onlyOwner {
+        unenrollReplica(_replica);
     }
 
     function setHome(address _home) public onlyOwner {
         home = Home(_home);
+    }
+
+    function setWatcherPermission(
+        address _watcher,
+        uint32 _domain,
+        bool _access
+    ) public onlyOwner {
+        watcherPermissions[_watcher][_domain] = _access;
+        emit WatcherPermissionSet(_domain, _watcher, _access);
     }
 
     function originDomain() public view returns (uint32) {
@@ -48,6 +96,28 @@ abstract contract UsingOptics is Ownable {
         bytes memory _body
     ) public {
         home.enqueue(_destination, _recipient, _body);
+    }
+
+    function unenrollReplica(address _replica) internal {
+        uint32 _currentDomain = replicaToDomain[_replica];
+        domainToReplica[_currentDomain] = address(0);
+        replicaToDomain[_replica] = 0;
+
+        emit ReplicaUnenrolled(_currentDomain, _replica);
+    }
+
+    function recoverWatcherFromSig(
+        uint32 _domain,
+        address _replica,
+        address _updater,
+        bytes memory _signature
+    ) internal view returns (address) {
+        bytes32 _replicaDomainHash = Replica(_replica).domainHash();
+
+        bytes32 _digest =
+            keccak256(abi.encodePacked(_replicaDomainHash, _domain, _updater));
+        _digest = ECDSA.toEthSignedMessageHash(_digest);
+        return ECDSA.recover(_digest, _signature);
     }
 }
 
