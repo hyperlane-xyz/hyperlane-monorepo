1136608c8bc95b2e469b156d899ec3600a5ee75d
Luke Tchang <ltchang@stanford.edu>
2021-05-20 09:49:05 -0700

    fix: deletes madison env references (#354)

--- solidity/optics-core/hardhat.config.js
+++ solidity/optics-core/hardhat.config.js
@@ -5,16 +5,6 @@ require('./lib');
 require('./scripts');
 
 const path = require('path');
-require('dotenv').config({
-  path: path.resolve(process.cwd(), '../../.madison.env'),
-});
-
-const {
-  MADISON_RPC_USER,
-  MADISON_RPC_PASS,
-  MADISON_RPC_URL,
-  MADISON_PRIVKEY,
-} = process.env;
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
@@ -37,11 +27,6 @@ module.exports = {
   networks: {
     localhost: {
       url: 'http://localhost:8545',
-    },
-    madison: {
-      chainId: 29739,
-      url: `http://${MADISON_RPC_USER}:${MADISON_RPC_PASS}@${MADISON_RPC_URL}`,
-      accounts: [MADISON_PRIVKEY],
     },
   },
 };
