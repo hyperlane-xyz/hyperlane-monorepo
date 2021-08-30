7de6914863d54bca6aba75514591f1a88bbf92ce
James Prestwich <james@prestwi.ch>
2021-08-30 09:55:37 -0700

    feature: log db status at agent bootup

--- rust/optics-base/src/db/mod.rs
+++ rust/optics-base/src/db/mod.rs
@@ -1,6 +1,7 @@
 use color_eyre::eyre::{Result, WrapErr};
 use rocksdb::{Options, DB};
 use std::path::Path;
+use tracing::info;
 
 /// Shared functionality surrounding use of rocksdb
 pub mod persistence;
@@ -13,6 +14,14 @@ pub fn from_path(db_path: &str) -> Result<DB> {
     // Canonicalize ensures existence, so we have to do that, then extend
     let mut path = Path::new(".").canonicalize()?;
     path.extend(&[db_path]);
+
+    match path.is_dir() {
+        true => info!(
+            "Opening existing db at {path}",
+            path = path.to_str().unwrap()
+        ),
+        false => info!("Creating db a {path}", path = path.to_str().unwrap()),
+    }
 
     let mut opts = Options::default();
     opts.create_if_missing(true);
