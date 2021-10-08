3828a61846bed106746535e0ba1c6f855b3d9fd1
Erin Hales <36751902+ErinHales@users.noreply.github.com>
2021-10-08 13:12:38 -0600

    update: dev contracts for multi-provider (#886)

--- typescript/optics-provider/src/optics/domains/dev.ts
+++ typescript/optics-provider/src/optics/domains/dev.ts
@@ -3,13 +3,13 @@ import { OpticsDomain } from './domain';
 export const alfajores: OpticsDomain = {
   name: 'alfajores',
   id: 1000,
-  bridgeRouter: '0xdaa6e362f9BE0CDaCe107b298639034b8dEC617a',
-  home: '0x47AaF05B1C36015eC186892C43ba4BaF91246aaA',
+  bridgeRouter: '0xDBbe03850fDF986Df1C8f1D856012f80A1f98eCc',
+  home: '0x01652e694BbD82C0900776c0406C3DFaa00e1e91',
   replicas: [
-    { domain: 2000, address: '0x7804079cF55110dE7Db5aA67eB1Be00cBE9CA526' },
+    { domain: 2000, address: '0xB4ceF5bd5328c42F3f09b9A43F6010ea93f145f1' },
     {
       domain: 3000,
-      address: '0x6B8D6947B9b70f3ff1b547a15B969F625d28104a',
+      address: '0xB5EB71E40bcAEAD5DDdc7687724f9F155Fd1a7a8',
     },
   ],
 };
@@ -17,14 +17,14 @@ export const alfajores: OpticsDomain = {
 export const kovan: OpticsDomain = {
   name: 'kovan',
   id: 3000,
-  bridgeRouter: '0x383Eb849c707fE38f3DfBF45679C0c6f21Ba82fF',
-  ethHelper: '0x6D84B823D7FB68E4d6f7Cc334fDd393f6C3a6980',
-  home: '0x5B55C29A10aEe6D5750F128C6a8f490de763ccc7',
+  bridgeRouter: '0xA763Db23711537D7074392b16E84eb04993C9f5d',
+  ethHelper: '0x90b6A931Cf35a1ba72E8959948d0E041320Fa704',
+  home: '0x0ED518F19fEbbd3737e39a55a8a708AFe8a9BE59',
   replicas: [
-    { domain: 2000, address: '0xC1AB4d72548Cc1C248EAdcD340035C3b213a47C3' },
+    { domain: 2000, address: '0x6776ef96A04a40a4dCB835D42bF59649aA9daFeA' },
     {
       domain: 1000,
-      address: '0xE63E73339501EE3A8d2928d6C88cf30aC8556Ee0',
+      address: '0xC6b39Ac67FBE3e029708390ffea130c8C0E7D30b',
     },
   ],
 };
@@ -32,14 +32,14 @@ export const kovan: OpticsDomain = {
 export const rinkeby: OpticsDomain = {
   name: 'rinkeby',
   id: 2000,
-  bridgeRouter: '0xE9fB0b6351Dec7d346282b8274653D36b8199AAF',
-  ethHelper: '0x7a539d7B7f4Acab1d7ce8b3681c3e286511ee444',
-  home: '0x6E6010E6bd43a9d2F7AE3b7eA9f61760e58758f3',
+  bridgeRouter: '0x926Df2b652bC8273BB2F06E8de135875bbE1D271',
+  ethHelper: '0x4e52f7e2F9f3B592dcfBD8957d36973a0308d1eF',
+  home: '0xDf4c0d67489F945C1e52440Ef8F203F4CE6e4176',
   replicas: [
-    { domain: 1000, address: '0x6A5F9531D1877ebE96Bc0631DbF64BBCf1f7421c' },
+    { domain: 1000, address: '0x7f41a2A1D97DD5F75B6FF6E1b012f686fE8121E4' },
     {
       domain: 3000,
-      address: '0x6554bc7a5C35bA64Bf48FA8a9e662d8808aaa890',
+      address: '0x72877419567cd7f697A6a5F2f7dF3e07327Ea7B2',
     },
   ],
 };
