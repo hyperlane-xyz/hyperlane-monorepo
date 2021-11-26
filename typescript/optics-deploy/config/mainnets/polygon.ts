38b5c6d07c9a25786abc9b66562225b233a29b28
nambrot <nambrot@googlemail.com>
2021-11-26 15:53:27 -0500

    Increase polygon price

--- typescript/optics-deploy/config/mainnets/polygon.ts
+++ typescript/optics-deploy/config/mainnets/polygon.ts
@@ -15,7 +15,7 @@ export const chainJson: ChainJson = {
   rpc,
   deployerKey: process.env.POLYGON_DEPLOYER_KEY,
   domain: 0x706f6c79, // b'poly' interpreted as an int
-  gasPrice: '60000000000' // 60 gwei
+  gasPrice: '120000000000' // 120 gwei
 };
 
 export const chain = toChain(chainJson);
