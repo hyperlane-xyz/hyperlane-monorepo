45608cbf65dcef1b863cda2c288841843cc2940f
nambrot <nambrot@googlemail.com>
2022-01-16 18:09:10 -0800

    Add devCommunity to index

--- typescript/optics-provider/src/optics/domains/index.ts
+++ typescript/optics-provider/src/optics/domains/index.ts
@@ -2,5 +2,6 @@ export type { OpticsDomain, ReplicaInfo } from './domain';
 export { mainnetDomains } from './mainnet';
 export { mainnetCommunityDomains } from './mainnetCommunity';
 export { devDomains } from './dev';
+export { devCommunityDomains } from './devCommunity';
 export { stagingDomains } from './staging';
 export { stagingCommunityDomains } from './stagingCommunity';
