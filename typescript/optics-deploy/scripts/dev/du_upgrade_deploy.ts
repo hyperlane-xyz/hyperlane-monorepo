66071efab438d98e12a8203d34074cf3c69cfe4f
nambrot <nambrot@googlemail.com>
2022-01-23 16:20:52 -0500

    Fix order

--- typescript/optics-deploy/scripts/dev/du_upgrade_deploy.ts
+++ typescript/optics-deploy/scripts/dev/du_upgrade_deploy.ts
@@ -12,15 +12,41 @@ let kovanConfig = kovan.devConfig;
 let mumbaiConfig = mumbai.devConfig;
 let fujiConfig = fuji.devConfig;
 
-const alfajoresDeploy = CoreDeploy.fromDirectory('../../rust/config/dev-community/', alfajores.chain, alfajoresConfig)
-const gorliDeploy = CoreDeploy.fromDirectory('../../rust/config/dev-community/', gorli.chain, gorliConfig)
-const kovanDeploy = CoreDeploy.fromDirectory('../../rust/config/dev-community/', kovan.chain, kovanConfig)
-const mumbaiDeploy = CoreDeploy.fromDirectory('../../rust/config/dev-community/', mumbai.chain, mumbaiConfig)
-const fujiDeploy = CoreDeploy.fromDirectory('../../rust/config/dev-community/', fuji.chain, fujiConfig)
+const alfajoresDeploy = CoreDeploy.fromDirectory(
+  '../../rust/config/dev-community/',
+  alfajores.chain,
+  alfajoresConfig,
+);
+const gorliDeploy = CoreDeploy.fromDirectory(
+  '../../rust/config/dev-community/',
+  gorli.chain,
+  gorliConfig,
+);
+const kovanDeploy = CoreDeploy.fromDirectory(
+  '../../rust/config/dev-community/',
+  kovan.chain,
+  kovanConfig,
+);
+const mumbaiDeploy = CoreDeploy.fromDirectory(
+  '../../rust/config/dev-community/',
+  mumbai.chain,
+  mumbaiConfig,
+);
+const fujiDeploy = CoreDeploy.fromDirectory(
+  '../../rust/config/dev-community/',
+  fuji.chain,
+  fujiConfig,
+);
 
-const deploys = [alfajoresDeploy, gorliDeploy, kovanDeploy, mumbaiDeploy, fujiDeploy]
+const deploys = [
+  alfajoresDeploy,
+  mumbaiDeploy,
+  fujiDeploy,
+  gorliDeploy,
+  kovanDeploy,
+];
 async function main() {
-  await deployImplementations(deploys, deployHomeImplementation)
-  await deployImplementations(deploys, deployReplicaImplementation)
+  await deployImplementations(deploys, deployHomeImplementation);
+  await deployImplementations(deploys, deployReplicaImplementation);
 }
-main().then(console.log).catch(console.error)
+main().then(console.log).catch(console.error);
