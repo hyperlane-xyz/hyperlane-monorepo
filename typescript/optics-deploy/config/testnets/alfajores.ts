246c61ff8c5c857c83ec0890a19f2f7a2a4ca79a
nambrot <nambrot@googlemail.com>
2022-01-23 16:30:13 -0500

    Update dev domains

--- typescript/optics-deploy/config/testnets/alfajores.ts
+++ typescript/optics-deploy/config/testnets/alfajores.ts
@@ -22,9 +22,9 @@ export const chain = toChain(chainJson);
 
 export const devConfig: CoreConfig = {
   environment: 'dev',
-  updater: '0x4177372FD9581ceb2367e0Ce84adC5DAD9DF8D55',
+  updater: '0x91631845fab02614e53e5F5A68dFBB0E2f1a9B6d',
   watchers: ['0x20aC2FD664bA5406A7262967C34107e708dCb18E'],
-  recoveryManager: '0x24F6c874F56533d9a1422e85e5C7A806ED11c036',
+  recoveryManager: '0x4FbBB2b0820CF0cF027BbB58DC7F7f760BC0c57e',
   optimisticSeconds: 10,
   recoveryTimelock: 180,
   processGas: 850_000,
