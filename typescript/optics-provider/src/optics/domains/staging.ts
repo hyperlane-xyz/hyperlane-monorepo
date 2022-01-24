d7482eaf992c7bb13ae2577f8696db75c1f0eae3
Asa Oines <a@celo.org>
2022-01-24 18:49:16 -0500

    Update staging domain

--- typescript/optics-provider/src/optics/domains/staging.ts
+++ typescript/optics-provider/src/optics/domains/staging.ts
@@ -4,16 +4,13 @@ export const alfajores: OpticsDomain = {
   name: 'alfajores',
   id: 1000,
   bridgeRouter: '0xd6930Ee55C141E5Bb4079d5963cF64320956bb3E',
-  home: '0x47AaF05B1C36015eC186892C43ba4BaF91246aaA',
+  home: '0xc8abA9c65A292C84EA00441B81124d9507fB22A8',
+  governanceRouter: '0x760AbbE9496BD9cEe159402E2B4d96E3d76dbE6a',
+  xAppConnectionManager: '0x02c144AeBA550634c8EE185F78657fd3C4a3F9B5',
   replicas: [
-    { domain: 2000, address: '0x7804079cF55110dE7Db5aA67eB1Be00cBE9CA526' },
-    {
-      domain: 3000,
-      address: '0x6B8D6947B9b70f3ff1b547a15B969F625d28104a',
-    },
+    { domain: 2000, address: '0x7149bF9f804F27e7259d0Ce328Dd5f6D5639ef19' },
+    { domain: 3000, address: '0xE469D8587D45BF85297BD924b159E726E7CA5408' },
   ],
-  governanceRouter: '',
-  xAppConnectionManager: '',
 };
 
 export const kovan: OpticsDomain = {
@@ -21,16 +18,13 @@ export const kovan: OpticsDomain = {
   id: 3000,
   bridgeRouter: '0x359089D34687bDbFD019fCC5093fFC21bE9905f5',
   ethHelper: '0x411ABcFD947212a0D64b97C9882556367b61704a',
-  home: '0x5B55C29A10aEe6D5750F128C6a8f490de763ccc7',
+  home: '0xB6Ee3e8fE5b577Bd6aB9a06FA169F97303586E7C',
+  governanceRouter: '0xa95868Ffaed7489e9059d4a08A0C1B0F78041b33',
+  xAppConnectionManager: '0x1d9Af80594930574201d919Af0fBfe6bb89800E2',
   replicas: [
-    { domain: 2000, address: '0xC1AB4d72548Cc1C248EAdcD340035C3b213a47C3' },
-    {
-      domain: 1000,
-      address: '0xE63E73339501EE3A8d2928d6C88cf30aC8556Ee0',
-    },
+    { domain: 1000, address: '0xF76995174f3C02e2900d0F6261e8cbeC04078E1f' },
+    { domain: 2000, address: '0xFF47138c42119Fe0B1f267e2fa254321DE287Fc6' },
   ],
-  governanceRouter: '',
-  xAppConnectionManager: '',
 };
 
 export const rinkeby: OpticsDomain = {
@@ -38,16 +32,13 @@ export const rinkeby: OpticsDomain = {
   id: 2000,
   bridgeRouter: '0x8FbEA25D0bFDbff68F2B920df180e9498E9c856A',
   ethHelper: '0x1BEBC8F1260d16EE5d1CFEE9366bB474bD13DC5f',
-  home: '0x6E6010E6bd43a9d2F7AE3b7eA9f61760e58758f3',
+  home: '0x8459EDe1ed4dADD6D5B142d845240088A6530Cf8',
+  governanceRouter: '0x8f8424DC94b4c302984Ab5a03fc4c2d1Ec95DC92',
+  xAppConnectionManager: '0x53B94f2D4a3159b66fcCC4f406Ea388426A3f3cB',
   replicas: [
-    { domain: 1000, address: '0x6A5F9531D1877ebE96Bc0631DbF64BBCf1f7421c' },
-    {
-      domain: 3000,
-      address: '0x6554bc7a5C35bA64Bf48FA8a9e662d8808aaa890',
-    },
+    { domain: 1000, address: '0xb473F5e0AAf47Ba54dac048633e7b578c1eBde01' },
+    { domain: 3000, address: '0x7EB8450a5397b795F2d89BC48EA20c24fa147F11' },
   ],
-  governanceRouter: '',
-  xAppConnectionManager: '',
 };
 
 export const stagingDomains = [alfajores, kovan, rinkeby];
