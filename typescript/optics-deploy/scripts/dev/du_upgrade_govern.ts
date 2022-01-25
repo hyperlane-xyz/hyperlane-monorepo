acc3dfddb80bfe05e5dc5d9cf68c19b664bfd829
Asa Oines <a@celo.org>
2022-01-24 19:02:14 -0500

    Add scripts and config for dev upgrade

---
+++ typescript/optics-deploy/scripts/dev/du_upgrade_govern.ts
@@ -0,0 +1,63 @@
+import { devCommunity } from 'optics-multi-provider-community';
+import * as alfajores from '../../config/testnets/alfajores';
+import * as gorli from '../../config/testnets/gorli';
+import * as kovan from '../../config/testnets/kovan';
+import * as mumbai from '../../config/testnets/mumbai';
+import * as fuji from '../../config/testnets/fuji';
+import { checkCoreDeploys, InvariantViolationCollector } from '../../src/checks';
+import { configPath } from './agentConfig';
+import { makeAllConfigs } from '../../src/config';
+import { ethers } from 'ethers';
+
+async function main() {
+  devCommunity.registerRpcProvider('alfajores', process.env.ALFAJORES_RPC!)
+  devCommunity.registerRpcProvider('gorli', process.env.GORLI_RPC!)
+  devCommunity.registerRpcProvider('kovan', process.env.KOVAN_RPC!)
+  devCommunity.registerRpcProvider('mumbai', process.env.MUMBAI_RPC!)
+  devCommunity.registerRpcProvider('fuji', process.env.FUJI_RPC!)
+  devCommunity.registerSigner('alfajores', new ethers.Wallet(process.env.ALFAJORES_DEPLOYER_KEY!))
+  const governorDomain = await devCommunity.governorDomain()
+  const governorCore = await devCommunity.governorCore()
+  const governanceMessages = await governorCore.newGovernanceBatch()
+
+  const invariantViolationCollector = new InvariantViolationCollector()
+  await checkCoreDeploys(
+    configPath,
+    await Promise.all([
+      makeAllConfigs(alfajores, (_) => _.devConfig),
+      makeAllConfigs(kovan, (_) => _.devConfig),
+      makeAllConfigs(gorli, (_) => _.devConfig),
+      makeAllConfigs(fuji, (_) => _.devConfig),
+      makeAllConfigs(mumbai, (_) => _.devConfig),
+    ]),
+    governorDomain,
+    invariantViolationCollector.handleViolation,
+    // We skip checking the verification input as it is expected to fail
+    // as the not-yet-upgraded-to implementations have already been written
+    // to it.
+    false
+  );
+
+  if (invariantViolationCollector.violations.length === 0) {
+    console.info("No violations, exit")
+    return
+  }
+
+  const violations = invariantViolationCollector.uniqueViolations()
+  console.log('checked core deploys, found', violations.length, 'violations')
+  for (const violation of violations) {
+    console.log(violation.domain, violation.actualImplementationAddress, violation.expectedImplementationAddress)
+    const call = await violation.upgradeBeaconController.populateTransaction.upgrade(violation.beacon.address, violation.expectedImplementationAddress)
+    if (violation.domain === governorDomain) {
+      governanceMessages.pushLocal(call)
+    } else {
+      governanceMessages.pushRemote(violation.domain, call)
+    }
+  }
+  const txs = await governanceMessages.build()
+  // const receipts = await governanceMessages.execute()
+  const receipts = await governanceMessages.estimateGas()
+  console.log(receipts)
+  governanceMessages.write('../../rust/config/dev-community/', `governance_${Date.now()}.json`, txs)
+}
+main().then(console.log).catch(console.error)
