3e93c9f1e1ac6ff4d3c41d0dc291a53134a6e322
Asa Oines <a@celo.org>
2022-01-29 12:17:10 -0500

    Gas reporting

--- scripts/test-solidity.sh
+++ scripts/test-solidity.sh
@@ -8,8 +8,18 @@ cd ./solidity/optics-core
 npm run compile
 cd ../optics-xapps
 npm run compile
+
+cd ../../
+# copy artifacts
+cp -R ./solidity/optics-core/artifacts ./typescript/optics-tests
+cp -R ./solidity/optics-xapps/artifacts ./typescript/optics-tests
+
+# copy cache
+cp -R ./solidity/optics-core/cache ./typescript/optics-tests
+cp -R ./solidity/optics-xapps/cache ./typescript/optics-tests
+
 # run tests
-cd ../../typescript/optics-tests
+cd ./typescript/optics-tests
 npm i
 npm run testNoCompile
 cd ../..
