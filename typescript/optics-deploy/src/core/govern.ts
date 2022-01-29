8c646b829ecd3d4fe35be4a1823ef48e8f52eac8
Asa Oines <a@celo.org>
2022-01-29 16:04:45 -0500

    Cast in handlers

--- typescript/optics-deploy/src/core/govern.ts
+++ typescript/optics-deploy/src/core/govern.ts
@@ -3,11 +3,10 @@ import { OpticsContext } from 'optics-multi-provider-community';
 import { CoreDeploy } from './CoreDeploy';
 import { HomeUpdaterViolation, ReplicaUpdaterViolation, UpgradeBeaconViolation, Violation, ViolationType } from '../checks';
 import { Call, CallBatch } from 'optics-multi-provider-community/dist/optics/govern';
-import { PopulatedTransaction} from 'ethers';
 
-interface DomainedTransaction {
+interface DomainedCall {
   domain: number
-  call: PopulatedTransaction
+  call: Call
 }
 
 export class GovernanceCallBatchBuilder {
@@ -25,12 +24,11 @@ export class GovernanceCallBatchBuilder {
     const governorCore = await this._context.governorCore()
     const batch = await governorCore.newGovernanceBatch()
     const txs = await Promise.all(this._violations.map(this.handleViolation))
-    txs.map((call) => expect(call.call.to).to.not.be.undefined)
-    txs.map((call) => batch.push(call.domain, call.call as Call))
+    txs.map((call) => batch.push(call.domain, call.call))
     return batch
   }
 
-  handleViolation(v: Violation): Promise<DomainedTransaction> {
+  handleViolation(v: Violation): Promise<DomainedCall> {
     switch (v.type) {
       case ViolationType.UpgradeBeacon:
         return this.handleUpgradeBeaconViolation(v)
@@ -44,38 +42,41 @@ export class GovernanceCallBatchBuilder {
     }
   }
 
-  async handleUpgradeBeaconViolation(violation: UpgradeBeaconViolation): Promise<DomainedTransaction> {
+  async handleUpgradeBeaconViolation(violation: UpgradeBeaconViolation): Promise<DomainedCall> {
     const domain = violation.domain
     const deploy = this.getDeploy(domain)
     const ubc = deploy.contracts.upgradeBeaconController;
     if (ubc === undefined) throw new Error('Undefined ubc')
-    const call = await ubc.populateTransaction.upgrade(
+    const tx = await ubc.populateTransaction.upgrade(
       violation.beaconProxy.beacon.address,
       violation.expected
     );
-    return { domain, call }
+    if (tx.to === undefined) throw new Error('undefined tx.to');
+    return { domain, call: tx as Call }
   }
 
-  async handleHomeUpdaterViolation(violation: HomeUpdaterViolation): Promise<DomainedTransaction> {
+  async handleHomeUpdaterViolation(violation: HomeUpdaterViolation): Promise<DomainedCall> {
     const domain = violation.domain
     const deploy = this.getDeploy(domain)
     const manager = deploy.contracts.updaterManager;
     expect(manager).to.not.be.undefined;
-    const call = await manager!.populateTransaction.setUpdater(
+    const tx = await manager!.populateTransaction.setUpdater(
       violation.expected
     );
-    return { domain, call }
+    if (tx.to === undefined) throw new Error('undefined tx.to');
+    return { domain, call: tx as Call }
   }
 
-  async handleReplicaUpdaterViolation(violation: ReplicaUpdaterViolation): Promise<DomainedTransaction> {
+  async handleReplicaUpdaterViolation(violation: ReplicaUpdaterViolation): Promise<DomainedCall> {
     const domain = violation.domain
     const deploy = this.getDeploy(domain)
     const replica = deploy.contracts.replicas[violation.remoteDomain];
     expect(replica).to.not.be.undefined;
-    const call = await replica!.proxy.populateTransaction.setUpdater(
+    const tx = await replica!.proxy.populateTransaction.setUpdater(
       violation.expected
     );
-    return { domain, call }
+    if (tx.to === undefined) throw new Error('undefined tx.to');
+    return { domain, call: tx as Call }
   }
 
   getDeploy(domain: number): CoreDeploy {
