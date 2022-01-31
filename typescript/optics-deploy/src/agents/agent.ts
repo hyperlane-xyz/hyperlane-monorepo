a19b1f544609d8d812bf86ec20d6bb91185751dc
Nam Chu Hoai <nambrot@googlemail.com>
2022-01-31 14:07:21 -0500

    Add AgentAwsKey (#49)
    * Add AWS Key rotation
    
    * Use property accessors and simplify agents code
    
    * PR comments
    
    * Add agent.ts
    
    Co-authored-by: Asa Oines <asaoines@gmail.com>

---
+++ typescript/optics-deploy/src/agents/agent.ts
@@ -0,0 +1,7 @@
+export abstract class AgentKey {
+  abstract get identifier(): string
+  abstract get address(): string
+  abstract get credentialsAsHelmValue(): any
+
+  abstract fetch(): Promise<void>
+}
