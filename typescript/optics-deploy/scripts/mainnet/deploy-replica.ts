b50b9fc325427bed6bdfa2fda3aaa85194d20622
Asa Oines <a@celo.org>
2022-02-01 12:14:29 -0500

    Deploy new replicas and update configs

---
+++ typescript/optics-deploy/scripts/mainnet/deploy-replica.ts
@@ -0,0 +1,18 @@
+import { ImplementationDeployer } from '../../src/core/implementation';
+import { configPath, networks } from './agentConfig';
+import { makeCoreDeploys } from '../../src/core/CoreDeploy';
+
+const coreDeploys = makeCoreDeploys(
+  configPath,
+  networks,
+  (_) => _.chain,
+  (_) => _.config,
+);
+
+async function main() {
+  const deployer = new ImplementationDeployer(coreDeploys);
+  await deployer.deployReplicaImplementations();
+  deployer.writeDeploys(configPath)
+}
+main().then(console.log).catch(console.error);
+
