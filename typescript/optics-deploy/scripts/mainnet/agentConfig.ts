76b25f948b112ecce6d7f75370cf2920b7e07d85
nambrot <nambrot@googlemail.com>
2022-02-01 14:43:38 -0500

    Add production update-key

--- typescript/optics-deploy/scripts/mainnet/agentConfig.ts
+++ typescript/optics-deploy/scripts/mainnet/agentConfig.ts
@@ -2,7 +2,28 @@ import * as celo from '../../config/mainnets/celo';
 import * as ethereum from '../../config/mainnets/ethereum';
 import * as polygon from '../../config/mainnets/polygon';
 import * as avalanche from '../../config/mainnets/avalanche';
+import { AgentChainConfigs, AgentConfig } from '../../src/agents';
 
 const configDirectory = 'production-community';
+
+export const configs: AgentChainConfigs = {
+  celo: celo.chainJson,
+  ethereum: ethereum.chainJson,
+  polygon: polygon.chainJson,
+  avalanche: avalanche.chainJson,
+};
+
+export const agentConfig: AgentConfig = {
+  environment: 'production',
+  namespace: 'optics-production-community',
+  runEnv: 'production-community',
+  awsRegion: process.env.AWS_REGION!,
+  awsKeyId: process.env.AWS_KEY_ID!,
+  awsSecretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+  dockerImageRepo: 'gcr.io/clabs-optics/optics-agent',
+  dockerImageTag: '3594c7d715f0ad1def2b36cb0e29649e1f6712e6',
+};
+
+
 export const configPath = `../../rust/config/${configDirectory}`;
 export const networks = [celo, polygon, avalanche, ethereum];
