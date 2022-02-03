08412e9b38ba6ff1b75f3227c6ee944ae11742e9
Asa Oines <a@celo.org>
2022-02-03 16:10:21 -0700

    Try to improve git diff

--- typescript/optics-provider/src/optics/domains/index.ts
+++ typescript/optics-provider/src/optics/domains/index.ts
@@ -1,6 +1,6 @@
 export type { OpticsDomain, ReplicaInfo } from './domain';
-export { testnetLegacyDomains } from './stagingLegacy';
-export { mainnetLegacyDomains } from './mainnetLegacy';
-export { devDomains } from './dev';
-export { testnetDomains } from './staging';
 export { mainnetDomains } from './mainnet';
+export { mainnetCommunityDomains } from './mainnetCommunity';
+export { devCommunityDomains } from './devCommunity';
+export { stagingDomains } from './staging';
+export { stagingCommunityDomains } from './stagingCommunity';
