ffd40b49995043ad8216e3304007877abb6f0171
Trevor Porter <trkporter@ucdavis.edu>
2022-02-17 23:14:45 +0000

    Update build.sh to use git commit

--- typescript/contract-metrics/build.sh
+++ typescript/contract-metrics/build.sh
@@ -1,1 +1,18 @@
-docker build --platform amd64 -t gcr.io/clabs-optics/optics-monitor:$1 . 
+#!/bin/sh
+
+REPOSITORY=gcr.io/clabs-optics/optics-monitor
+
+# Uses the first argument as the tag if it's present, otherwise uses
+# the latest commit hash.
+TAG=$1
+if [ -z $TAG ]; then
+    # This grabs the most recent commit hash - be sure to commit changes
+    # before running this.
+    TAG=$(git rev-parse HEAD)
+fi
+
+FULL_IMAGE=$REPOSITORY:$TAG
+
+echo "Building $FULL_IMAGE"
+
+docker build --platform amd64 -t $FULL_IMAGE . 
