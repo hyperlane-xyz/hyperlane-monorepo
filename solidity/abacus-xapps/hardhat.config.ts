961befb7c8bc74b2106b77852607a7bfd8b40eba
Asa Oines <a@celo.org>
2022-02-27 11:04:41 -0800

    bridgeMessage tests passing

--- solidity/abacus-xapps/hardhat.config.ts
+++ solidity/abacus-xapps/hardhat.config.ts
@@ -3,6 +3,7 @@ import "@typechain/hardhat";
 import "@nomiclabs/hardhat-etherscan";
 import "@nomiclabs/hardhat-waffle";
 import "hardhat-gas-reporter";
+import './test/lib/index';
 
 import { task } from "hardhat/config";
 import { verifyLatestBridgeDeploy } from "../../typescript/abacus-deploy/src/verification/verifyLatestDeploy";
