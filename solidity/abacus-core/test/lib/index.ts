25e2a71e87a5a08492b57d9c41a241518068d2fd
Asa Oines <a@celo.org>
2022-02-27 11:14:42 -0800

    Remove unused code

--- solidity/abacus-core/test/lib/index.ts
+++ solidity/abacus-core/test/lib/index.ts
@@ -3,10 +3,7 @@ import { extendEnvironment } from 'hardhat/config';
 
 import { abacus } from './core';
 
-import { bridge } from './bridge';
-
 // HardhatRuntimeEnvironment
 extendEnvironment((hre) => {
   hre.abacus = abacus;
-  hre.bridge = bridge;
 });
--- solidity/abacus-core/test/lib/types.ts
+++ solidity/abacus-core/test/lib/types.ts
@@ -1,7 +1,5 @@
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
-import { TokenIdentifier } from '@abacus-network/sdk/dist/abacus';
 import { BytesLike, ethers } from 'ethers';
-import { BridgeMessageTypes } from './bridge';
 
 /********* HRE *********/
 
@@ -19,22 +17,9 @@ export interface HardhatAbacusHelpers {
   signedFailureNotification: Function;
 }
 
-export interface HardhatBridgeHelpers {
-  BridgeMessageTypes: typeof BridgeMessageTypes;
-  typeToByte: Function;
-  MESSAGE_LEN: MessageLen;
-  serializeTransferAction: Function;
-  serializeDetailsAction: Function;
-  serializeRequestDetailsAction: Function;
-  serializeAction: Function;
-  serializeTokenId: Function;
-  serializeMessage: Function;
-}
-
 declare module 'hardhat/types/runtime' {
   interface HardhatRuntimeEnvironment {
     abacus: HardhatAbacusHelpers;
-    bridge: HardhatBridgeHelpers;
   }
 }
 
@@ -100,38 +85,4 @@ export type FailureNotification = {
 export type SignedFailureNotification = {
   failureNotification: FailureNotification;
   signature: string;
-};
-
-/********* TOKEN BRIDGE *********/
-
-export type MessageLen = {
-  identifier: number;
-  tokenId: number;
-  transfer: number;
-  details: number;
-  requestDetails: number;
-};
-
-export type Action = DetailsAction | TransferAction | RequestDetailsAction;
-
-export type Message = {
-  tokenId: TokenIdentifier;
-  action: Action;
-};
-
-export type TransferAction = {
-  type: BridgeMessageTypes.TRANSFER;
-  recipient: ethers.BytesLike;
-  amount: number | ethers.BytesLike;
-};
-
-export type DetailsAction = {
-  type: BridgeMessageTypes.DETAILS;
-  name: string;
-  symbol: string;
-  decimals: number;
-};
-
-export type RequestDetailsAction = {
-  type: BridgeMessageTypes.REQUEST_DETAILS;
 };
