c0887a6d7246c04ea76f5e4cd4203774edb77d19
Asa Oines <a@celo.org>
2022-02-28 17:05:36 -0800

    UpdaterManager tests compile

--- solidity/abacus-core/test/updaterManager.test.ts
+++ solidity/abacus-core/test/updaterManager.test.ts
@@ -1,19 +1,19 @@
-import { ethers } from 'hardhat';
+import { ethers, abacus } from 'hardhat';
 import { expect } from 'chai';
 
 import { AbacusState, Updater } from './lib/core';
 import { Signer } from './lib/types';
 
-import { TestCommon__factory, TestCommon } from '../typechain';
+import { Home__factory, Home, UpdaterManager__factory, UpdaterManager } from '../typechain';
 
-const homeDomainHashTestCases = require('../../../vectors/homeDomainHash.json');
-const signedUpdateTestCases = require('../../../vectors/signedUpdate.json');
+const homeDomainHashCases = require('../../../vectors/homeDomainHash.json');
+const signedUpdateCases = require('../../../vectors/signedUpdate.json');
 const localDomain = 1000;
 
-describe('Common', async () => {
+describe('UpdaterManager', async () => {
   let signer: Signer,
     fakeSigner: Signer,
-    common: TestCommon,
+    updaterManager: UpdaterManager,
     updater: Updater,
     fakeUpdater: Updater;
 
@@ -24,150 +24,99 @@ describe('Common', async () => {
   });
 
   beforeEach(async () => {
-    const commonFactory = new TestCommon__factory(signer);
-    common = await commonFactory.deploy(localDomain, updater.address);
+    const updaterManagerFactory = new UpdaterManager__factory(signer);
+    updaterManager = await updaterManagerFactory.deploy();
   });
 
   it('Accepts updater signature', async () => {
-    const oldRoot = ethers.utils.formatBytes32String('old root');
-    const newRoot = ethers.utils.formatBytes32String('new root');
+    const root = ethers.utils.formatBytes32String('root');
+    const index = 1;
 
-    const { signature } = await updater.signUpdate(oldRoot, newRoot);
-    const isValid = await common.testIsUpdaterSignature(
-      oldRoot,
-      newRoot,
+    const { signature } = await updater.signCheckpoint(root, index);
+    const isValid = await updaterManager.isUpdaterSignature(
+      localDomain,
+      root,
+      index,
       signature,
     );
     expect(isValid).to.be.true;
   });
 
   it('Rejects non-updater signature', async () => {
-    const oldRoot = ethers.utils.formatBytes32String('old root');
-    const newRoot = ethers.utils.formatBytes32String('new root');
+    const root = ethers.utils.formatBytes32String('root');
+    const index = 1;
 
-    const { signature: fakeSignature } = await fakeUpdater.signUpdate(
-      oldRoot,
-      newRoot,
+    const { signature } = await fakeUpdater.signCheckpoint(root, index);
+    const isValid = await updaterManager.isUpdaterSignature(
+      localDomain,
+      root,
+      index,
+      signature,
     );
-    expect(await common.testIsUpdaterSignature(oldRoot, newRoot, fakeSignature))
+    expect(isValid)
       .to.be.false;
   });
 
-  it('Fails on valid double update proof', async () => {
-    const oldRoot = ethers.utils.formatBytes32String('old root');
-    const newRoot = ethers.utils.formatBytes32String('new root 1');
-    const newRoot2 = ethers.utils.formatBytes32String('new root 2');
-
-    const { signature } = await updater.signUpdate(oldRoot, newRoot);
-    const { signature: signature2 } = await updater.signUpdate(
-      oldRoot,
-      newRoot2,
-    );
-
-    await expect(
-      common.doubleUpdate(oldRoot, [newRoot, newRoot2], signature, signature2),
-    ).to.emit(common, 'DoubleUpdate');
-
-    expect(await common.state()).to.equal(AbacusState.FAILED);
-  });
-
-  it('Does not fail contract on invalid double update proof', async () => {
-    const oldRoot = ethers.utils.formatBytes32String('old root');
-    const newRoot = ethers.utils.formatBytes32String('new root');
-
-    const { signature } = await updater.signUpdate(oldRoot, newRoot);
-
-    // Double update proof uses same roots and signatures
-    await common.doubleUpdate(
-      oldRoot,
-      [newRoot, newRoot],
-      signature,
-      signature,
-    );
-
-    // State should not be failed because double update proof does not
-    // demonstrate fraud
-    const state = await common.state();
-    expect(state).not.to.equal(AbacusState.FAILED);
-    expect(state).to.equal(AbacusState.ACTIVE);
-  });
-
-  it('Does not fail on double update after updater rotation', async () => {
-    const oldRoot = ethers.utils.formatBytes32String('old root');
-    const newRoot = ethers.utils.formatBytes32String('new root 1');
-    const newRoot2 = ethers.utils.formatBytes32String('new root 2');
-
-    const { signature } = await updater.signUpdate(oldRoot, newRoot);
-    const { signature: signature2 } = await updater.signUpdate(
-      oldRoot,
-      newRoot2,
-    );
-    await common.setUpdater(fakeUpdater.address);
-    await common.doubleUpdate(
-      oldRoot,
-      [newRoot, newRoot2],
-      signature,
-      signature2,
-    );
-
-    // State should not be failed because double update proof does not
-    // use current updater.
-    const state = await common.state();
-    expect(state).not.to.equal(AbacusState.FAILED);
-    expect(state).to.equal(AbacusState.ACTIVE);
-  });
-
-  it('Checks Rust-produced SignedUpdate', async () => {
-    // Compare Rust output in json file to solidity output
-    for (let testCase of signedUpdateTestCases) {
-      const { oldRoot, newRoot, signature, signer } = testCase;
-
-      const signerAddress = ethers.utils.getAddress(signer);
-      await common.setUpdater(signerAddress);
-
-      expect(
-        await common.testIsUpdaterSignature(
-          oldRoot,
-          newRoot,
-          ethers.utils.joinSignature(signature),
-        ),
-      ).to.be.true;
-    }
-  });
   it('Calculated domain hash matches Rust-produced domain hash', async () => {
     // Compare Rust output in json file to solidity output (json file matches
     // hash for local domain of 1000)
-    for (let testCase of homeDomainHashTestCases) {
-      const homeFactory = new TestHome__factory(signer);
-      const tempHome = await homeFactory.deploy(testCase.homeDomain);
+    for (let testCase of homeDomainHashCases) {
       const { expectedDomainHash } = testCase;
-      const homeDomainHash = await tempHome.homeDomainHash();
-      expect(homeDomainHash).to.equal(expectedDomainHash);
+      const domainHash = await updaterManager.domainHash(testCase.homeDomain);
+      expect(domainHash).to.equal(expectedDomainHash);
     }
   });
 
-  it('Calculated domain hash matches Rust-produced domain hash', async () => {
-    // Compare Rust output in json file to solidity output (json file matches
-    // hash for remote domain of 1000)
-    for (let testCase of homeDomainHashTestCases) {
-      // deploy replica
-      const replicaFactory = new TestReplica__factory(signer);
-      const tempReplica = await replicaFactory.deploy(
-        testCase.homeDomain,
-        processGas,
-        reserveGas,
-      );
-      await tempReplica.initialize(
-        testCase.homeDomain,
-        updater.address,
-        ethers.constants.HashZero,
-        optimisticSeconds,
-      );
+  describe('improper updates', async () => {
+    let home: Home;
+    beforeEach(async () => {
+      const homeFactory = new Home__factory(signer);
+      home = await homeFactory.deploy(localDomain);
+      await home.initialize(updaterManager.address);
+    });
 
-      const { expectedDomainHash } = testCase;
-      const homeDomainHash = await tempReplica.homeDomainHash();
-      expect(homeDomainHash).to.equal(expectedDomainHash);
-    }
+    it('Accepts improper update from updater', async () => {
+      const root = ethers.utils.formatBytes32String('root');
+      const index = 1;
+
+      const { signature } = await updater.signCheckpoint(root, index);
+      // Send message with signer address as msg.sender
+      await expect(
+        updaterManager.improperUpdate(home.address, root, index, signature)
+      )
+        .to.emit(updaterManager, 'ImproperUpdate')
+        .withArgs(
+          home.address, localDomain, updater.address, root, index, signature);
+      expect(await home.state()).to.equal(AbacusState.FAILED);
+    });
+
+    it('Rejects improper update from non-updater', async () => {
+      const root = ethers.utils.formatBytes32String('root');
+      const index = 1;
+
+      const { signature } = await fakeUpdater.signCheckpoint(root, index);
+      // Send message with signer address as msg.sender
+      await expect(
+        updaterManager.improperUpdate(home.address, root, index, signature)
+    ).to.be.revertedWith('!updater sig');
+    });
+
+    it('Rejects proper update from updater', async () => {
+      const message = `0x${Buffer.alloc(10).toString('hex')}`;
+      await home
+          .dispatch(
+            localDomain,
+            abacus.ethersAddressToBytes32(signer.address),
+            message,
+          );
+        await  home.checkpoint()
+      const [root, index] = await home.latestCheckpoint();
+
+      const { signature } = await updater.signCheckpoint(root, index.toNumber());
+      // Send message with signer address as msg.sender
+      await expect(
+        updaterManager.improperUpdate(home.address, root, index, signature)
+    ).to.be.revertedWith('!improper');
+    });
   });
-
 });
