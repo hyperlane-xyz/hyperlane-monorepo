a6ed293ac94da05f7f89c5dc5376f3efeeebc906
Asa Oines <a@celo.org>
2022-03-05 10:50:47 -0800

    no unlimited size

--- typescript/abacus-infrastructure/hardhat.config.ts
+++ typescript/abacus-infrastructure/hardhat.config.ts
@@ -7,12 +7,5 @@ module.exports = {
   solidity: {
     version: "0.7.6",
   },
-  // For some reason the BridgeRouter is oversized here but not in
-  // in abacus-xapps.
-  networks: {
-    hardhat: {
-      allowUnlimitedContractSize: true
-    }
-  },
 };
 
