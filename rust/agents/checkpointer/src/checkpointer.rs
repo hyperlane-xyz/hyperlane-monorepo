72ae94e655895daa51ff36a5f21d8adf1e40726f
Nam Chu Hoai <nambrot@googlemail.com>
2022-03-07 13:17:04 -0500

    Make checkpointer the truly first Abacus agent (#197)
    * Move Checkpointer to (Abacus)Agent
    
    * Lint
    
    * Fix

--- rust/agents/checkpointer/src/checkpointer.rs
+++ rust/agents/checkpointer/src/checkpointer.rs
@@ -1,29 +1,28 @@
 use async_trait::async_trait;
 use color_eyre::Result;
-use futures_util::future::select_all;
 use tokio::task::JoinHandle;
-use tracing::{instrument::Instrumented, Instrument};
+use tracing::instrument::Instrumented;
 
 use crate::{settings::CheckpointerSettings as Settings, submit::CheckpointSubmitter};
-use abacus_base::{AbacusAgent, AgentCore};
-use abacus_core::{db::AbacusDB, Common};
+use abacus_base::{AbacusAgentCore, Agent};
+use abacus_core::{db::AbacusDB, AbacusCommon};
 
 /// An checkpointer agent
 #[derive(Debug)]
 pub struct Checkpointer {
     interval_seconds: u64,
-    pub(crate) core: AgentCore,
+    pub(crate) core: AbacusAgentCore,
 }
 
-impl AsRef<AgentCore> for Checkpointer {
-    fn as_ref(&self) -> &AgentCore {
+impl AsRef<AbacusAgentCore> for Checkpointer {
+    fn as_ref(&self) -> &AbacusAgentCore {
         &self.core
     }
 }
 
 impl Checkpointer {
     /// Instantiate a new checkpointer
-    pub fn new(interval_seconds: u64, core: AgentCore) -> Self {
+    pub fn new(interval_seconds: u64, core: AbacusAgentCore) -> Self {
         Self {
             interval_seconds,
             core,
@@ -32,7 +31,7 @@ impl Checkpointer {
 }
 
 #[async_trait]
-impl AbacusAgent for Checkpointer {
+impl Agent for Checkpointer {
     const AGENT_NAME: &'static str = "checkpointer";
 
     type Settings = Settings;
@@ -42,27 +41,22 @@ impl AbacusAgent for Checkpointer {
         Self: Sized,
     {
         let interval_seconds = settings.interval.parse().expect("invalid uint");
-        let core = settings.as_ref().try_into_core(Self::AGENT_NAME).await?;
+        let core = settings
+            .as_ref()
+            .try_into_abacus_core(Self::AGENT_NAME)
+            .await?;
         Ok(Self::new(interval_seconds, core))
     }
+}
 
-    fn run(&self, _replica: &str) -> Instrumented<JoinHandle<Result<()>>> {
-        let home = self.home();
-        let db = AbacusDB::new(self.home().name(), self.db());
+impl Checkpointer {
+    pub fn run(&self) -> Instrumented<JoinHandle<Result<()>>> {
+        let outbox = self.outbox();
+        let db = AbacusDB::new(self.outbox().name(), self.db());
 
-        let submit = CheckpointSubmitter::new(home, db, self.interval_seconds);
+        let submit = CheckpointSubmitter::new(outbox, db, self.interval_seconds);
 
-        tokio::spawn(async move {
-            let submit_task = submit.spawn();
-
-            let (res, _, rem) = select_all(vec![submit_task]).await;
-
-            for task in rem.into_iter() {
-                task.into_inner().abort();
-            }
-            res?
-        })
-        .in_current_span()
+        self.run_all(vec![submit.spawn()])
     }
 }
 
