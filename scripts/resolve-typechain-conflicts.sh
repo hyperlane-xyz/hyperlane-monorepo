198e6c2074ce7670661ddb597e7c933942e82fa9
Asa Oines <a@celo.org>
2022-03-07 13:33:56 -0800

    remote unused code

--- scripts/resolve-typechain-conflicts.sh
+++
@@ -1,8 +0,0 @@
-# Script for resolving typechain conflicts
-# Run from root (./scripts/resolve-typechain-conflicts.sh)
-cd ./solidity/optics-core
-npm run compile
-cd ../optics-xapps
-npm run compile
-cd ../..
-git add typescript/typechain
