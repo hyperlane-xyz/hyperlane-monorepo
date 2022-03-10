3c5430d0707a3854d8e69d0114b01df0c16fae81
nambrot <nambrot@googlemail.com>
2022-03-10 15:31:45 -0500

    Fix checkpoint hash calc

--- rust/abacus-core/src/types/checkpoint.rs
+++ rust/abacus-core/src/types/checkpoint.rs
@@ -66,12 +66,14 @@ impl Decode for Checkpoint {
 
 impl Checkpoint {
     fn signing_hash(&self) -> H256 {
+        let buffer = [0u8; 28];
         // sign:
-        // domain(home_domain) || previous_root || new_root
+        // domain(home_domain) || root || index (as u256)
         H256::from_slice(
             Keccak256::new()
                 .chain(home_domain_hash(self.outbox_domain))
                 .chain(self.root)
+                .chain(buffer)
                 .chain(self.index.to_be_bytes())
                 .finalize()
                 .as_slice(),
