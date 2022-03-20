9229f9545a10fcd8377436a6f918406f0b1ff56e
nambrot <nambrot@googlemail.com>
2022-03-20 18:43:26 -0400

    PR review

--- rust/agents/relayer/src/checkpoint_relayer.rs
+++ rust/agents/relayer/src/checkpoint_relayer.rs
@@ -56,22 +56,23 @@ impl CheckpointRelayer {
         Ok(Some(messages))
     }
 
+    // Returns the newest "current" checkpoint index
     async fn submit_checkpoint_and_messages(
         &mut self,
         local_storage: &LocalStorage,
-        latest_checkpointed_index: u32,
-        latest_signed_checkpoint_index: u32,
+        onchain_checkpoint_index: u32,
+        signed_checkpoint_index: u32,
         messages: Vec<CommittedMessage>,
     ) -> Result<u32> {
         // If the checkpoint storage is inconsistent, then this arm won't match
         // and it will cause us to have skipped this message batch
         if let Some(latest_signed_checkpoint) = local_storage
-            .fetch_checkpoint(latest_signed_checkpoint_index)
+            .fetch_checkpoint(signed_checkpoint_index)
             .await?
         {
             let batch = MessageBatch::new(
                 messages,
-                latest_checkpointed_index,
+                onchain_checkpoint_index,
                 latest_signed_checkpoint.clone(),
             );
             self.prover_sync.update_from_batch(&batch)?;
@@ -92,7 +93,7 @@ impl CheckpointRelayer {
             sleep(Duration::from_secs(self.interval)).await;
             Ok(latest_signed_checkpoint.checkpoint.index)
         } else {
-            Ok(latest_checkpointed_index)
+            Ok(onchain_checkpoint_index)
         }
     }
 
@@ -103,46 +104,43 @@ impl CheckpointRelayer {
         };
         tokio::spawn(async move {
             let latest_inbox_checkpoint = self.inbox.latest_checkpoint(None).await?;
-            let mut latest_checkpointed_index = latest_inbox_checkpoint.index;
+            let mut onchain_checkpoint_index = latest_inbox_checkpoint.index;
             // Checkpoints are 1-indexed, while leaves are 0-indexed
-            let mut unprocessed_leaf_index = latest_checkpointed_index;
+            let mut unprocessed_leaf_index = onchain_checkpoint_index;
             loop {
                 sleep(Duration::from_secs(5)).await;
 
-                if let Some(latest_signed_checkpoint_index) = local_storage.latest_index().await? {
-                    if latest_signed_checkpoint_index <= latest_checkpointed_index {
+                if let Some(signed_checkpoint_index) = local_storage.latest_index().await? {
+                    if signed_checkpoint_index <= onchain_checkpoint_index {
                         debug!(
-                            onchain = latest_checkpointed_index,
-                            signed = latest_signed_checkpoint_index,
+                            onchain = onchain_checkpoint_index,
+                            signed = signed_checkpoint_index,
                             "Signed checkpoint matches known checkpoint on-chain, continue"
                         );
                         continue;
                     }
 
                     match self
-                        .get_messages_between(
-                            unprocessed_leaf_index,
-                            latest_signed_checkpoint_index,
-                        )
+                        .get_messages_between(unprocessed_leaf_index, signed_checkpoint_index)
                         .await?
                     {
                         None => debug!("Couldn't fetch the relevant messages, retry this range"),
                         Some(messages) if messages.is_empty() => {
-                            unprocessed_leaf_index = latest_signed_checkpoint_index;
+                            unprocessed_leaf_index = signed_checkpoint_index;
                             debug!("New checkpoint does not include messages for inbox")
                         }
                         Some(messages) => {
-                            unprocessed_leaf_index = latest_signed_checkpoint_index;
+                            unprocessed_leaf_index = signed_checkpoint_index;
                             debug!(
                                 len = messages.len(),
                                 "Signed checkpoint allows for processing of new messages"
                             );
 
-                            latest_checkpointed_index = self
+                            onchain_checkpoint_index = self
                                 .submit_checkpoint_and_messages(
                                     &local_storage,
-                                    latest_checkpointed_index,
-                                    latest_signed_checkpoint_index,
+                                    onchain_checkpoint_index,
+                                    signed_checkpoint_index,
                                     messages,
                                 )
                                 .await?;
