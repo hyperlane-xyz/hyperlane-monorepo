f028ceee4f5859bb0313117624863fecff5dce10
Asa Oines <a@celo.org>
2022-03-21 11:03:56 -0700

    comments

--- typescript/deploy/scripts/utils.ts
+++ typescript/deploy/scripts/utils.ts
@@ -271,11 +271,11 @@ export function getGovernance(
 }
 
 export function registerRpcProviders(
-  multiprovider: MultiProvider,
+  multiProvider: MultiProvider,
   chains: ChainConfig[],
 ): void {
   chains.map((c) =>
-    multiprovider.registerRpcProvider(
+    multiProvider.registerRpcProvider(
       c.name,
       process.env[`${c.name.toUpperCase()}_RPC`]!,
     ),
