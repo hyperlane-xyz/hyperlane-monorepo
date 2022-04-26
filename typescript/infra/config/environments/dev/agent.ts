c1b1d5cb3c39832bded3ecf741840443bc76e09b
Trevor Porter <trkporter@ucdavis.edu>
2022-04-26 11:02:03 +0100

    upgrade agents

--- typescript/infra/config/environments/dev/agent.ts
+++ typescript/infra/config/environments/dev/agent.ts
@@ -9,7 +9,7 @@ export const agent: AgentConfig<DevNetworks> = {
   runEnv: ENVIRONMENTS_ENUM.Dev,
   docker: {
     repo: 'gcr.io/abacus-labs-dev/abacus-agent',
-    tag: 'sha-c7fa127',
+    tag: 'f30aa0a68a645bf966638e145ba8a4e15f80280e',
   },
   domainNames,
   validatorSets: validators,
