69ef0c95a62b15fbd29b40a22fd8476d014a9c76
Trevor Porter <trkporter@ucdavis.edu>
2022-04-27 15:29:55 +0100

    prettier

--- typescript/infra/scripts/deploy-agents.ts
+++ typescript/infra/scripts/deploy-agents.ts
@@ -10,7 +10,7 @@ async function deploy() {
   // We intentionally do not Promise.all here to avoid race conditions that could result
   // in attempting to create a user or key multiple times. This was found to happen in
   // situations where agents for different domains would share the same AWS user or
-  // AWS KMS key. 
+  // AWS KMS key.
   for (const name of domainNames) {
     await runAgentHelmCommand(
       HelmCommand.InstallOrUpgrade,
