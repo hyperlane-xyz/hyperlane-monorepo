a0db21d087743bea7d1a2ad3116d8c244210f4f5
Trevor Porter <trkporter@ucdavis.edu>
2022-04-27 15:14:18 +0100

    Better fix for annoying race cond

--- typescript/infra/scripts/deploy-agents.ts
+++ typescript/infra/scripts/deploy-agents.ts
@@ -6,16 +6,19 @@ async function deploy() {
   const environment = await getEnvironment();
   const agentConfig = await getAgentConfig(environment);
   const domainNames = await getDomainNames(environment);
-  await Promise.all(
-    domainNames.map((name) => {
-      return runAgentHelmCommand(
-        HelmCommand.InstallOrUpgrade,
-        agentConfig,
-        name,
-        domainNames,
-      );
-    }),
-  );
+
+  // We intentionally do not Promise.all here to avoid race conditions that could result
+  // in attempting to create a user or key multiple times. This was found to happen in
+  // situations where agents for different domains would share the same AWS user or
+  // AWS KMS key. 
+  for (const name of domainNames) {
+    await runAgentHelmCommand(
+      HelmCommand.InstallOrUpgrade,
+      agentConfig,
+      name,
+      domainNames,
+    );
+  }
 }
 
 deploy().then(console.log).catch(console.error);
