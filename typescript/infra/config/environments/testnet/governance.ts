b7ea3edee7d8d6557ae2ba830c2972586d4687fa
Trevor Porter <trkporter@ucdavis.edu>
2022-04-28 17:29:27 +0100

    Nits, update governance config

--- typescript/infra/config/environments/testnet/governance.ts
+++ typescript/infra/config/environments/testnet/governance.ts
@@ -4,11 +4,29 @@ export const governance: GovernanceConfig = {
   recoveryTimelock: 180,
   addresses: {
     alfajores: {
-      recoveryManager: '0x3909CFACD7a568634716CbCE635F76b9Cf37364B',
-      governor: '0x3909CFACD7a568634716CbCE635F76b9Cf37364B',
+      recoveryManager: '0xfaD1C94469700833717Fa8a3017278BC1cA8031C',
+      governor: '0xfaD1C94469700833717Fa8a3017278BC1cA8031C',
     },
     kovan: {
-      recoveryManager: '0x3909CFACD7a568634716CbCE635F76b9Cf37364B',
+      recoveryManager: '0xfaD1C94469700833717Fa8a3017278BC1cA8031C',
+    },
+    fuji: {
+      recoveryManager: '0xfaD1C94469700833717Fa8a3017278BC1cA8031C',
+    },
+    mumbai: {
+      recoveryManager: '0xfaD1C94469700833717Fa8a3017278BC1cA8031C',
+    },
+    bsctestnet: {
+      recoveryManager: '0xfaD1C94469700833717Fa8a3017278BC1cA8031C',
+    },
+    arbitrumrinkeby: {
+      recoveryManager: '0xfaD1C94469700833717Fa8a3017278BC1cA8031C',
+    },
+    optimismkovan: {
+      recoveryManager: '0xfaD1C94469700833717Fa8a3017278BC1cA8031C',
+    },
+    auroratestnet: {
+      recoveryManager: '0xfaD1C94469700833717Fa8a3017278BC1cA8031C',
     },
   },
 };
