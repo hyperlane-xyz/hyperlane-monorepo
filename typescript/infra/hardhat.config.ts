77130f206295b1fab50e05b359edce0b1acf03ca
yorhodes <yorke@useabacus.network>
2022-05-03 15:11:41 -0400

    Revert hre.ethers.getSigners changes

--- typescript/infra/hardhat.config.ts
+++ typescript/infra/hardhat.config.ts
@@ -1,7 +1,7 @@
 import '@nomiclabs/hardhat-etherscan';
 import '@nomiclabs/hardhat-waffle';
-import { ethers } from 'hardhat';
 import { task } from 'hardhat/config';
+import { HardhatRuntimeEnvironment } from 'hardhat/types';
 
 import { BadRandomRecipient__factory } from '@abacus-network/core';
 import { utils as deployUtils } from '@abacus-network/deploy';
@@ -64,10 +64,13 @@ task('abacus', 'Deploys abacus on top of an already running Hardhat Network')
     'environment',
     'The name of the environment from which to read configs',
   )
-  .setAction(async (args: any) => {
+  // If we import ethers from hardhat, we get error HH9 with included note.
+  // You probably tried to import the "hardhat" module from your config or a file imported from it.
+  // This is not possible, as Hardhat can't be initialized while its config is being defined.
+  .setAction(async (args: any, hre: HardhatRuntimeEnvironment) => {
     const environment = args.environment;
     const environmentConfig = await getCoreEnvironmentConfig(environment);
-    const [signer] = await ethers.getSigners();
+    const [signer] = await hre.ethers.getSigners();
     const multiProvider = deployUtils.initHardhatMultiProvider(
       environmentConfig,
       signer,
@@ -97,9 +100,9 @@ task('kathy', 'Dispatches random abacus messages')
     'environment',
     'The name of the environment from which to read configs',
   )
-  .setAction(async (args) => {
+  .setAction(async (args, hre: HardhatRuntimeEnvironment) => {
     const environmentConfig = await getCoreEnvironmentConfig(args.environment);
-    const [signer] = await ethers.getSigners();
+    const [signer] = await hre.ethers.getSigners();
     const multiProvider = deployUtils.initHardhatMultiProvider(
       environmentConfig,
       signer,
