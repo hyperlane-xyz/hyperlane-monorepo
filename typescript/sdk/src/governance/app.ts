4526fdfc247758d7c1ca1aaa2e8e528606dfa8b5
yorhodes <yorke@useabacus.network>
2022-05-04 10:44:29 -0400

    Fix lint

--- typescript/sdk/src/governance/app.ts
+++ typescript/sdk/src/governance/app.ts
@@ -1,12 +1,15 @@
-import { GovernanceRouter } from '@abacus-network/apps';
-import { types } from '@abacus-network/utils';
-import { ethers } from 'ethers';
 import { GovernanceContracts } from '.';
 import { Call } from '..';
+import { ethers } from 'ethers';
+
+import { GovernanceRouter } from '@abacus-network/apps';
+import { types } from '@abacus-network/utils';
+
 import { AbacusApp } from '../app';
 import { MultiProvider } from '../provider';
 import { ChainMap, ChainName, ChainNameToDomainId } from '../types';
 import { objMap, promiseObjAll } from '../utils';
+
 import { GovernanceAddresses } from './contracts';
 import { environments } from './environments';
 
