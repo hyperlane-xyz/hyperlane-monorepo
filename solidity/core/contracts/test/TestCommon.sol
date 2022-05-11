e21cb9bb7eb0544fced72f6269b30ccb2d30d8ce
yorhodes <yorke@useabacus.network>
2022-05-10 22:22:49 -0400

    Fix initializable bugs

--- solidity/core/contracts/test/TestCommon.sol
+++ solidity/core/contracts/test/TestCommon.sol
@@ -6,7 +6,7 @@ import "../Common.sol";
 contract TestCommon is Common {
     constructor(uint32 _localDomain) Common(_localDomain) {}
 
-    function initialize(address _validatorManager) external {
+    function initialize(address _validatorManager) external initializer {
         __Common_initialize(_validatorManager);
     }
 }
