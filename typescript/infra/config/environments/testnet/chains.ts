6d8d1deec7456269e534238cae7c630f91a9608d
nambrot <nambrot@googlemail.com>
2022-05-19 16:36:15 -0400

    Readd chains

--- typescript/infra/config/environments/testnet/chains.ts
+++ typescript/infra/config/environments/testnet/chains.ts
@@ -3,6 +3,11 @@ import { chainConnectionConfigs } from '@abacus-network/sdk';
 export const testnetConfigs = {
   alfajores: chainConnectionConfigs.alfajores,
   kovan: chainConnectionConfigs.kovan,
+  fuji: chainConnectionConfigs.fuji,
+  mumbai: chainConnectionConfigs.mumbai,
+  bsctestnet: chainConnectionConfigs.bsctestnet,
+  arbitrumrinkeby: chainConnectionConfigs.arbitrumrinkeby,
+  optimismkovan: chainConnectionConfigs.optimismkovan,
 };
 
 export type TestnetChains = keyof typeof testnetConfigs;
