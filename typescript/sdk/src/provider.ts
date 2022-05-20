b8388f859d92d6de630025cc44e561601a1e3c79
Nam Chu Hoai <nambrot@googlemail.com>
2022-05-20 11:43:20 -0400

    Remove TransactionConfig (#465)

--- typescript/sdk/src/provider.ts
+++ typescript/sdk/src/provider.ts
@@ -1,22 +1,22 @@
 import { ethers } from 'ethers';
 
 import { ChainMap, ChainName } from './types';
-import { MultiGeneric } from './utils';
+import { MultiGeneric, objMap } from './utils';
 
 export interface IChainConnection {
-  provider?: ethers.providers.Provider;
+  provider: ethers.providers.Provider;
   signer?: ethers.Signer;
   overrides?: ethers.Overrides;
   confirmations?: number;
 }
 
 export class ChainConnection {
-  provider?: ethers.providers.Provider;
+  provider: ethers.providers.Provider;
   signer?: ethers.Signer;
   overrides: ethers.Overrides;
   confirmations: number;
 
-  constructor(dc: IChainConnection = {}) {
+  constructor(dc: IChainConnection) {
     this.provider = dc.provider;
     this.signer = dc.signer;
     this.overrides = dc.overrides ?? {};
@@ -31,17 +31,13 @@ export class ChainConnection {
 export class MultiProvider<
   Chain extends ChainName = ChainName,
 > extends MultiGeneric<Chain, ChainConnection> {
-  constructor(
-    chainConnectionConfigs: ChainMap<Chain, IChainConnection> | Chain[],
-  ) {
-    const params = Array.isArray(chainConnectionConfigs)
-      ? chainConnectionConfigs.map((v) => [v, {}])
-      : (Object.entries(chainConnectionConfigs) as [Chain, IChainConnection][]);
-    const providerEntries = params.map(([chain, v]) => [
-      chain,
-      new ChainConnection(v),
-    ]);
-    super(Object.fromEntries(providerEntries));
+  constructor(chainConnectionConfigs: ChainMap<Chain, IChainConnection>) {
+    super(
+      objMap(
+        chainConnectionConfigs,
+        (_, connection) => new ChainConnection(connection),
+      ),
+    );
   }
   getChainConnection(chain: Chain) {
     return this.get(chain);
