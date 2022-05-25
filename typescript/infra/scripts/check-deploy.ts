2ce890275aa1970d96e691729fbdfb343d2875b3
yorhodes <yorke@useabacus.network>
2022-05-25 11:53:19 -0400

    Remove controller from SDK

--- typescript/infra/scripts/check-deploy.ts
+++ typescript/infra/scripts/check-deploy.ts
@@ -1,6 +1,5 @@
-import { AbacusCore, ControllerApp } from '@abacus-network/sdk';
+import { AbacusCore } from '@abacus-network/sdk';
 
-import { ControllerChecker } from '../src/controller';
 import { AbacusCoreChecker } from '../src/core';
 
 import { getCoreEnvironmentConfig, getEnvironment } from './utils';
@@ -10,28 +9,8 @@ async function check() {
   const config = getCoreEnvironmentConfig(environment);
   const multiProvider = await config.getMultiProvider();
 
-  if (environment !== 'test') {
-    throw new Error(
-      `Do not have controller addresses for ${environment} in SDK`,
-    );
-  }
-
   const core = AbacusCore.fromEnvironment(environment, multiProvider);
-  const controller = ControllerApp.fromEnvironment(environment, multiProvider);
-
-  const controllerChecker = new ControllerChecker(
-    multiProvider,
-    controller,
-    config.controller,
-  );
-  await controllerChecker.check();
-  controllerChecker.expectEmpty();
-
-  const owners = controller.routerAddresses();
-  const coreChecker = new AbacusCoreChecker(multiProvider, core, {
-    ...config.core,
-    owners,
-  } as any);
+  const coreChecker = new AbacusCoreChecker(multiProvider, core, config.core);
   await coreChecker.check();
   coreChecker.expectEmpty();
 }
