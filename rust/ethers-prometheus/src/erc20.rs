87f90351187491c5e1d6019bc9a31fbfde75572e
Mattie Conover <git@mconover.dev>
2022-05-27 11:47:32 -0700

    remove unused code

--- rust/ethers-prometheus/src/erc20.rs
+++ rust/ethers-prometheus/src/erc20.rs
@@ -1,22 +1,3 @@
 use ethers::prelude::*;
 
 abigen!(Erc20, "$CARGO_MANIFEST_DIR/abis/erc20.abi.json");
-
-// /// Allows us to look up new information on the fly using the ERC20 specification.
-// pub(crate) async fn get_token_info<M: Middleware>(
-//     addr: Address,
-//     client: Arc<M>,
-// ) -> Result<TokenInfo, ContractError<M>> {
-//     let token = Erc20::new(addr, client);
-//
-//     let name = token.name();
-//     let symbol = token.symbol();
-//     let decimals = token.decimals();
-//     let (name, symbol, decimals) = try_join!(name.call(), symbol.call(), decimals.call())?;
-//
-//     Ok(TokenInfo {
-//         name,
-//         symbol,
-//         decimals,
-//     })
-// }
