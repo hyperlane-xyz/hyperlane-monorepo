19c9e7e7259637b8e9b3737b0d96ef4075141c6e
Trevor Porter <trkporter@ucdavis.edu>
2022-06-01 12:19:06 +0100

    mv types/interchain_gas into types/mod

--- rust/abacus-core/src/types/interchain_gas.rs
+++
@@ -1,9 +0,0 @@
-use ethers::types::U256;
-
-/// A payment of Outbox native tokens for a message
-pub struct InterchainGasPayment {
-    /// The index of the message's leaf in the merkle tree
-    pub leaf_index: u32,
-    /// The payment amount, in Outbox native token wei
-    pub amount: U256,
-}
--- rust/abacus-core/src/types/mod.rs
+++ rust/abacus-core/src/types/mod.rs
@@ -1,5 +1,6 @@
+use ethers::types::U256;
+
 mod checkpoint;
-mod interchain_gas;
 mod messages;
 
 /// Unified 32-byte identifier with convenience tooling for handling
@@ -7,5 +8,12 @@ mod messages;
 pub mod identifiers;
 
 pub use checkpoint::*;
-pub use interchain_gas::*;
 pub use messages::*;
+
+/// A payment of Outbox native tokens for a message
+pub struct InterchainGasPayment {
+    /// The index of the message's leaf in the merkle tree
+    pub leaf_index: u32,
+    /// The payment amount, in Outbox native token wei
+    pub amount: U256,
+}
