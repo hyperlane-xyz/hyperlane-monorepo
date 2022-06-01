6c2c4ca5b807213980ca4efc836e19aacd6daefc
yorhodes <yorke@useabacus.network>
2022-06-01 11:39:56 -0400

    Rename Common to Mailbox in solidity

--- solidity/core/interfaces/IMailbox.sol
+++ solidity/core/interfaces/IMailbox.sol
@@ -1,9 +1,11 @@
 // SPDX-License-Identifier: MIT OR Apache-2.0
 pragma solidity >=0.6.11;
 
-interface ICommon {
+interface IMailbox {
     function localDomain() external view returns (uint32);
+
     function cachedCheckpoints(bytes32) external view returns (uint256);
+
     function latestCachedCheckpoint()
         external
         view
