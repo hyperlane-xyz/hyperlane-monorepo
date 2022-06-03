8d207f478e05ad49d7d1f6f5198e56fa8cc4be13
nambrot <nambrot@googlemail.com>
2022-06-03 20:35:49 +0200

    Alpha7

--- typescript/sdk/src/router.ts
+++ typescript/sdk/src/router.ts
@@ -1,20 +1,17 @@
 import { Router, Router__factory } from '@abacus-network/app';
-import {
-  AbacusConnectionManager,
-  AbacusConnectionManager__factory,
-} from '@abacus-network/core';
 
 import { AbacusContracts, AbacusFactories } from './contracts';
+import { ProxiedContract } from './proxy';
 
 export type RouterContracts<RouterContract extends Router = Router> =
   AbacusContracts & {
-    router: RouterContract;
-    abacusConnectionManager: AbacusConnectionManager;
+    router: RouterContract | ProxiedContract<Router, any>;
   };
 
 export type RouterFactories<
   RouterFactory extends Router__factory = Router__factory,
 > = AbacusFactories & {
   router: RouterFactory;
-  abacusConnectionManager: AbacusConnectionManager__factory;
 };
+
+export { Router } from '@abacus-network/app';
