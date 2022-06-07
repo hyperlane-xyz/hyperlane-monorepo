26415a275a30c53221080e52830b87efa948b75a
Mattie Conover <git@mconover.dev>
2022-06-07 11:27:29 -0400

    cleanup

--- rust/utils/loop-control/tests/integration_tests.rs
+++ rust/utils/loop-control/tests/integration_tests.rs
@@ -1,12 +1,12 @@
-use loop_control::{LoopControl::*, loop_ctrl};
+use loop_control::{loop_ctrl, LoopControl::*};
 
 #[test]
 fn flows_loop() {
     let mut i = 0;
     for _ in 0..5 {
-        i+=1;
+        i += 1;
         loop_ctrl!(Flow);
-        i+=1;
+        i += 1;
     }
     assert_eq!(i, 10);
 }
@@ -15,9 +15,9 @@ fn flows_loop() {
 fn continues_loop() {
     let mut i = 0;
     for _ in 0..5 {
-        i+=1;
+        i += 1;
         loop_ctrl!(Continue);
-        i+=1;
+        i += 1;
     }
     assert_eq!(i, 5);
 }
@@ -26,9 +26,9 @@ fn continues_loop() {
 fn breaks_loop() {
     let mut i = 0;
     for _ in 0..5 {
-        i+=1;
+        i += 1;
         loop_ctrl!(Break);
-        i+=1;
+        i += 1;
     }
     assert_eq!(i, 1);
 }
