eda42c0df338a30a71ff96e8606dec16f196a73e
Trevor Porter <trkporter@ucdavis.edu>
2022-06-08 14:17:26 +0100

    Add testnet2 env

--- typescript/infra/config/environments/index.ts
+++ typescript/infra/config/environments/index.ts
@@ -1,9 +1,11 @@
 import { environment as dev } from './dev';
 import { environment as test } from './test';
 import { environment as testnet } from './testnet';
+import { environment as testnet2 } from './testnet2';
 
 export const environments = {
-  testnet,
   dev,
   test,
+  testnet,
+  testnet2,
 };
