14838ce4c6b4893145d6eb686c9146c539f8a52b
Mattie Conover <git@mconover.dev>
2022-06-09 17:13:39 -0400

    naming change

--- rust/agents/relayer/src/settings/mod.rs
+++ rust/agents/relayer/src/settings/mod.rs
@@ -8,7 +8,7 @@ pub mod whitelist;
 decl_settings!(Relayer {
     /// The polling interval to check for new signed checkpoints in seconds
     signedcheckpointpollinginterval: String,
-    /// The maximum number of times a processor will try to process a message
+    /// The maximum number of times a relayer will try to process a message
     maxprocessingretries: String,
     /// The multisig checkpoint syncer configuration
     multisigcheckpointsyncer: abacus_base::MultisigCheckpointSyncerConf,
