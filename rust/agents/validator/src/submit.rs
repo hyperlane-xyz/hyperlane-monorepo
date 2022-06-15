f00624d4b7d15f241101b9157ff95f1bd49f109e
Mattie Conover <git@mconover.dev>
2022-06-15 17:44:33 -0400

    minor refactoring

--- rust/agents/validator/src/submit.rs
+++ rust/agents/validator/src/submit.rs
@@ -35,40 +35,49 @@ impl ValidatorSubmitter {
 
     pub(crate) fn spawn(self) -> Instrumented<JoinHandle<Result<()>>> {
         let span = info_span!("ValidatorSubmitter");
+        tokio::spawn(self.main_task()).instrument(span)
+    }
+
+    async fn main_task(self) -> Result<()> {
         let reorg_period = Some(self.reorg_period);
-        tokio::spawn(async move {
-            // Ensure that the outbox has > 0 messages before we enter the main
-            // validator submit loop. This is to avoid an underflow / reverted
-            // call when we invoke the `outbox.latest_checkpoint()` method,
-            // which returns the **index** of the last element in the tree
-            // rather than just the size.  See
-            // https://github.com/abacus-network/abacus-monorepo/issues/575 for
-            // more details.
-            while self.outbox.count().await? == 0 {
-                info!("waiting for non-zero outbox size");
-                sleep(Duration::from_secs(self.interval)).await;
+        // Ensure that the outbox has > 0 messages before we enter the main
+        // validator submit loop. This is to avoid an underflow / reverted
+        // call when we invoke the `outbox.latest_checkpoint()` method,
+        // which returns the **index** of the last element in the tree
+        // rather than just the size.  See
+        // https://github.com/abacus-network/abacus-monorepo/issues/575 for
+        // more details.
+        while self.outbox.count().await? == 0 {
+            info!("waiting for non-zero outbox size");
+            sleep(Duration::from_secs(self.interval)).await;
+        }
+
+        let mut current_index = self
+            .checkpoint_syncer
+            .latest_index()
+            .await?
+            .unwrap_or_default();
+
+        info!(current_index = current_index, "Starting Validator");
+        loop {
+            sleep(Duration::from_secs(self.interval)).await;
+
+            // Check the latest checkpoint
+            let latest_checkpoint = self.outbox.latest_checkpoint(reorg_period).await?;
+
+            // TODO: add metric here for checkpoint
+
+            if current_index < latest_checkpoint.index {
+                let signed_checkpoint = latest_checkpoint.sign_with(self.signer.as_ref()).await?;
+
+                info!(signature = ?signed_checkpoint, signer=?self.signer, "Sign latest checkpoint");
+                current_index = latest_checkpoint.index;
+
+                self.checkpoint_syncer
+                    .write_checkpoint(signed_checkpoint.clone())
+                    .await?;
+                // TODO: add metric for last written checkpoint
             }
-
-            let mut current_index =
-                self.checkpoint_syncer.latest_index().await?.unwrap_or_default();
-
-            info!(current_index=current_index, "Starting Validator");
-            loop {
-                sleep(Duration::from_secs(self.interval)).await;
-
-                // Check the latest checkpoint
-                let latest_checkpoint = self.outbox.latest_checkpoint(reorg_period).await?;
-
-                if current_index < latest_checkpoint.index {
-                    let signed_checkpoint = latest_checkpoint.sign_with(self.signer.as_ref()).await?;
-
-                    info!(signature = ?signed_checkpoint, signer=?self.signer, "Sign latest checkpoint");
-                    current_index = latest_checkpoint.index;
-
-                    self.checkpoint_syncer.write_checkpoint(signed_checkpoint.clone()).await?;
-                }
-            }
-        })
-            .instrument(span)
+        }
     }
 }
