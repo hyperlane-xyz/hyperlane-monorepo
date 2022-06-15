218a798a79ab59904fdabb818d0355e286d19cc4
J M Rossy <jm.rossy@gmail.com>
2022-06-15 16:23:39 -0400

    Fix kathy scripts for 0.2.2 Hoist resolveJsonModule ts config to root

--- typescript/infra/scripts/deploy-helloworld.ts
+++ typescript/infra/scripts/deploy-helloworld.ts
@@ -1,9 +1,6 @@
 import path from 'path';
 
-import {
-  HelloWorldConfig,
-  HelloWorldDeployer,
-} from '@abacus-network/helloworld';
+import { HelloWorldDeployer } from '@abacus-network/helloworld';
 import {
   AbacusCore,
   objMap,
@@ -11,27 +8,34 @@ import {
   serializeContracts,
 } from '@abacus-network/sdk';
 
+import addresses from '../config/environments/testnet2/helloworld/addresses.json';
+import { CoreEnvironmentConfig } from '../src/config';
 import { writeJSON } from '../src/utils/utils';
 
 import { getCoreEnvironmentConfig, getEnvironmentDirectory } from './utils';
 
+type Chains = keyof typeof addresses;
+
 async function main() {
   const environment = 'testnet2';
-  const coreConfig = getCoreEnvironmentConfig(environment);
+  // TODO Fix need for cast here due to https://github.com/abacus-network/abacus-monorepo/pull/594/files#diff-40a12589668de942078f498e0ab0fda512e1eb7397189d6d286b590ae87c45d1R31
+  const coreConfig = getCoreEnvironmentConfig(
+    environment,
+  ) as CoreEnvironmentConfig<Chains>;
   const multiProvider = await coreConfig.getMultiProvider();
   const core = AbacusCore.fromEnvironment(environment, multiProvider);
 
   const signerMap = await promiseObjAll(
     multiProvider.map(async (_, dc) => dc.signer!),
   );
-  const configMap = await promiseObjAll(
+  const ownerMap = await promiseObjAll(
     objMap(signerMap, async (_, signer) => {
-      const config: HelloWorldConfig = {
+      return {
         owner: await signer.getAddress(),
       };
-      return config;
     }),
   );
+  const configMap = core.extendWithConnectionManagers(ownerMap);
 
   const deployer = new HelloWorldDeployer(multiProvider, configMap, core);
   const contracts = await deployer.deploy();
@@ -39,4 +43,6 @@ async function main() {
   writeJSON(dir, 'addresses.json', serializeContracts(contracts));
 }
 
-main().then(console.log).catch(console.error);
+main()
+  .then(() => console.info('Deployment complete'))
+  .catch(console.error);
--- typescript/infra/scripts/kathy-helloworld.ts
+++ typescript/infra/scripts/kathy-helloworld.ts
@@ -3,7 +3,7 @@ import {
   HelloWorldContracts,
   helloWorldFactories,
 } from '@abacus-network/helloworld/dist/sdk/contracts';
-import { ChainMap, buildContracts } from '@abacus-network/sdk';
+import { ChainMap, ChainName, buildContracts } from '@abacus-network/sdk';
 
 import addresses from '../config/environments/testnet2/helloworld/addresses.json';
 
@@ -23,17 +23,27 @@ async function main() {
     sources.map((source) => {
       const destinations = sources.slice().filter((d) => d !== source);
       return Promise.all(
-        destinations.map(async (destination) => {
-          const receipt = await app.sendHelloWorld(
-            source,
-            destination,
-            `Hello from ${source} to ${destination}!`,
-          );
-          console.log(JSON.stringify(receipt.events || receipt.logs));
-        }),
+        destinations.map((destination) =>
+          sendMessage(app, source, destination),
+        ),
       );
     }),
   );
 }
 
-main().then().catch(console.error);
+async function sendMessage(
+  app: HelloWorldApp<any>,
+  source: ChainName,
+  destination: ChainName,
+) {
+  const receipt = await app.sendHelloWorld(
+    source,
+    destination,
+    `Hello from ${source} to ${destination}!`,
+  );
+  console.log(JSON.stringify(receipt.events || receipt.logs));
+}
+
+main()
+  .then(() => console.info('HelloWorld sent'))
+  .catch(console.error);
