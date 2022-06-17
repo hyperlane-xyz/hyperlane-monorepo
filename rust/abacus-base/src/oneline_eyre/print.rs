80f07f4b4b04b8b9e15fd1b03ec6e483f5888ba3
Mattie Conover <git@mconover.dev>
2022-06-17 16:58:00 -0400

    Oneline backtraces

--- rust/abacus-base/src/oneline_eyre/print.rs
+++ rust/abacus-base/src/oneline_eyre/print.rs
@@ -3,9 +3,8 @@
 
 use std::ffi::c_void;
 use std::fmt;
-use backtrace::{BacktraceFrame, BacktraceSymbol, BytesOrWideString, Frame, Symbol, SymbolName};
 
-const HEX_WIDTH: usize = 2 + 2 * core::mem::size_of::<usize>();
+use backtrace::{BacktraceFrame, BacktraceSymbol, BytesOrWideString, SymbolName};
 
 /// A formatter for backtraces.
 ///
@@ -16,7 +15,8 @@ pub struct BacktraceFmt<'a, 'b> {
     fmt: &'a mut fmt::Formatter<'b>,
     frame_index: usize,
     print_path:
-    &'a mut (dyn FnMut(&mut fmt::Formatter<'_>, BytesOrWideString<'_>) -> fmt::Result + 'b),
+        &'a mut (dyn FnMut(&mut fmt::Formatter<'_>, BytesOrWideString<'_>) -> fmt::Result + 'b),
+    line_separator: &'static str,
 }
 
 impl<'a, 'b> BacktraceFmt<'a, 'b> {
@@ -29,12 +29,14 @@ impl<'a, 'b> BacktraceFmt<'a, 'b> {
     /// any printing of filenames, but this callback is required to do so.
     pub fn new(
         fmt: &'a mut fmt::Formatter<'b>,
+        line_separator: &'static str,
         print_path: &'a mut (dyn FnMut(&mut fmt::Formatter<'_>, BytesOrWideString<'_>) -> fmt::Result
-        + 'b),
+                     + 'b),
     ) -> Self {
         BacktraceFmt {
             fmt,
             frame_index: 0,
+            line_separator,
             print_path,
         }
     }
@@ -51,7 +53,6 @@ impl<'a, 'b> BacktraceFmt<'a, 'b> {
         }
     }
 }
-
 
 /// A formatter for just one frame of a backtrace.
 pub struct BacktraceFrameFmt<'fmt, 'a, 'b> {
@@ -106,19 +107,6 @@ impl BacktraceFrameFmt<'_, '_, '_> {
         Ok(())
     }
 
-    /// Prints a raw traced `Frame` and `Symbol`, typically from within the raw
-    /// callbacks of this crate.
-    pub fn symbol(&mut self, frame: &Frame, symbol: &Symbol) -> fmt::Result {
-        self.print_raw_with_column(
-            frame.ip(),
-            symbol.name(),
-            symbol.filename_raw(),
-            symbol.lineno(),
-            symbol.colno(),
-        )?;
-        Ok(())
-    }
-
     /// Adds a raw frame to the backtrace output.
     ///
     /// This method, unlike the previous, takes the raw arguments in case
@@ -147,22 +135,14 @@ impl BacktraceFrameFmt<'_, '_, '_> {
         lineno: Option<u32>,
         colno: Option<u32>,
     ) -> fmt::Result {
-        // Fuchsia is unable to symbolize within a process so it has a special
-        // format which can be used to symbolize later. Print that instead of
-        // printing addresses in our own format here.
-        if cfg!(target_os = "fuchsia") {
-            self.print_raw_fuchsia(frame_ip)?;
-        } else {
-            self.print_raw_generic(frame_ip, symbol_name, filename, lineno, colno)?;
-        }
+        self.print_raw_generic(frame_ip, symbol_name, filename, lineno, colno)?;
         self.symbol_index += 1;
         Ok(())
     }
 
-    #[allow(unused_mut)]
     fn print_raw_generic(
         &mut self,
-        mut frame_ip: *mut c_void,
+        frame_ip: *mut c_void,
         symbol_name: Option<SymbolName<'_>>,
         filename: Option<BytesOrWideString<'_>>,
         lineno: Option<u32>,
@@ -175,26 +155,19 @@ impl BacktraceFrameFmt<'_, '_, '_> {
         }
 
         // Print the index of the frame as well as the optional instruction
-        // pointer of the frame. If we're beyond the first symbol of this frame
-        // though we just print appropriate whitespace.
-        if self.symbol_index == 0 {
-            write!(self.fmt.fmt, "{:4}: ", self.fmt.frame_index)?;
-            write!(self.fmt.fmt, "{:1$?} - ", frame_ip, HEX_WIDTH)?;
-        } else {
-            write!(self.fmt.fmt, "      ")?;
-            write!(self.fmt.fmt, "{:1$}", "", HEX_WIDTH + 3)?;
-        }
+        // pointer of the frame.
+        write!(self.fmt.fmt, "({}) ", self.fmt.frame_index)?;
+        write!(self.fmt.fmt, "{frame_ip:?} - ")?;
 
         // Next up write out the symbol name, using the alternate formatting for
         // more information if we're a full backtrace. Here we also handle
         // symbols which don't have a name,
         if let Some(name) = symbol_name {
-            // write!(self.fmt.fmt, "{:#}", name)?,
-            write!(self.fmt.fmt, "{}", name)?;
+            write!(self.fmt.fmt, "{name:#}")?;
+            // write!(self.fmt.fmt, "{name}")?;
         } else {
             write!(self.fmt.fmt, "<unknown>")?;
         }
-        self.fmt.fmt.write_str("\n")?;
 
         // And last up, print out the filename/line number if they're available.
         if let (Some(file), Some(line)) = (filename, lineno) {
@@ -212,30 +185,19 @@ impl BacktraceFrameFmt<'_, '_, '_> {
     ) -> fmt::Result {
         // Filename/line are printed on lines under the symbol name, so print
         // some appropriate whitespace to sort of right-align ourselves.
-        write!(self.fmt.fmt, "{:1$}", "", HEX_WIDTH)?;
-        write!(self.fmt.fmt, "             at ")?;
+        write!(self.fmt.fmt, " @ ")?;
 
         // Delegate to our internal callback to print the filename and then
         // print out the line number.
         (self.fmt.print_path)(self.fmt.fmt, file)?;
-        write!(self.fmt.fmt, ":{}", line)?;
+        write!(self.fmt.fmt, ":{line}")?;
 
         // Add column number, if available.
         if let Some(colno) = colno {
-            write!(self.fmt.fmt, ":{}", colno)?;
+            write!(self.fmt.fmt, ":{colno}")?;
         }
 
-        write!(self.fmt.fmt, "\n")?;
-        Ok(())
-    }
-
-    fn print_raw_fuchsia(&mut self, frame_ip: *mut c_void) -> fmt::Result {
-        // We only care about the first symbol of a frame
-        if self.symbol_index == 0 {
-            self.fmt.fmt.write_str("{{{bt:")?;
-            write!(self.fmt.fmt, "{}:{:?}", self.fmt.frame_index, frame_ip)?;
-            self.fmt.fmt.write_str("}}}\n")?;
-        }
+        write!(self.fmt.fmt, "{}", self.fmt.line_separator)?;
         Ok(())
     }
 }
