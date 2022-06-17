536e724480d7cff7980d409c7c4a1a81a4db18a9
Mattie Conover <git@mconover.dev>
2022-06-17 16:29:56 -0400

    Pull over code for printing backtraces

--- rust/abacus-base/src/oneline_eyre/mod.rs
+++ rust/abacus-base/src/oneline_eyre/mod.rs
@@ -28,12 +28,15 @@
 //! [`eyre::EyreHandler`]: https://docs.rs/eyre/*/eyre/trait.EyreHandler.html
 //! [`eyre`]: https://docs.rs/eyre
 
+mod print;
+use print::BacktraceFmt;
+
 use core::fmt::{self, Debug, Formatter};
 use std::error::Error;
-use std::iter;
+use std::fmt::Display;
+use std::{env, iter};
 
-// use ::backtrace::Backtrace;
-// use backtrace::{BacktraceFmt, BytesOrWideString, PrintFmt};
+use backtrace::{Backtrace, BytesOrWideString, PrintFmt};
 use eyre::{EyreHandler, Result};
 
 /// The default separator used to delimitate lines in error messages.
@@ -41,37 +44,12 @@ const DEFAULT_LINE_SEPARATOR: &str = " ## ";
 /// The default separator used to delimitate error sections.
 const DEFAULT_SECTION_SEPARATOR: &str = " ##$$## ";
 
-// pub trait BacktraceExt {
-//     /// Returns a reference to the captured backtrace if one exists
-//     ///
-//     /// # Example
-//     ///
-//     /// ```rust
-//     /// use eyre::eyre;
-//     /// use abacus_base::oneline_eyre::{self, BacktraceExt};
-//     /// oneline_eyre::install();
-//     /// std::env::set_var("RUST_BACKTRACE", "1");
-//     ///
-//     /// let report = eyre!("capture a report");
-//     /// assert!(report.backtrace().is_some());
-//     /// ```
-//     fn backtrace(&self) -> Option<&Backtrace>;
-// }
-//
-// impl BacktraceExt for eyre::Report {
-//     fn backtrace(&self) -> Option<&Backtrace> {
-//         self.handler()
-//             .downcast_ref::<Handler>()
-//             .and_then(|handler| handler.backtrace.as_ref())
-//     }
-// }
-
 /// A custom context type for minimal error reporting via `eyre`
 pub struct Handler {
     line_separator: &'static str,
     section_separator: &'static str,
-    // backtrace: Option<Backtrace>,
-    // show_full_paths: bool,
+    backtrace: Option<Backtrace>,
+    show_full_paths: bool,
 }
 
 impl Handler {
@@ -93,37 +71,36 @@ impl Handler {
         Ok(())
     }
 
-    // /// Format a backtrace onto a single line.
-    // /// Largely stolen from backtrace's Debug implementation.
-    // fn fmt_backtrace(&self, backtrace: &Backtrace, f: &mut Formatter<'_>) -> fmt::Result {
-    //     write!(f, "{}Stack backtrace:", self.section_separator)?;
-    //
-    //     // When printing paths we try to strip the cwd if it exists, otherwise
-    //     // we just print the path as-is. Note that we also only do this for the
-    //     // short format, because if it's full we presumably want to print
-    //     // everything.
-    //     let cwd = if !self.show_full_paths {
-    //         env::current_dir().ok()
-    //     } else { None };
-    //     let mut print_path =
-    //         move |fmt: &mut Formatter<'_>, path: BytesOrWideString<'_>| {
-    //             let path = path.into_path_buf();
-    //             if let Some(cwd) = &cwd {
-    //                 if let Ok(suffix) = path.strip_prefix(cwd) {
-    //                     return Display::fmt(&suffix.display(), fmt);
-    //                 }
-    //             }
-    //             Display::fmt(&path.display(), fmt)
-    //         };
-    //
-    //     let mut backtrace_fmt = BacktraceFmt::new(f, PrintFmt::Full, &mut print_path);
-    //     backtrace_fmt.add_context()?;
-    //     for frame in backtrace.frames() {
-    //         backtrace_fmt.frame().backtrace_frame(frame)?;
-    //         // write!(f, "{}({n}) {}", self.line_separator)
-    //     }
-    //     backtrace_fmt.finish()
-    // }
+    /// Format a backtrace onto a single line.
+    /// Largely stolen from backtrace's Debug implementation.
+    fn fmt_backtrace(&self, backtrace: &Backtrace, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "{}Stack backtrace:", self.section_separator)?;
+
+        // When printing paths we try to strip the cwd if it exists, otherwise
+        // we just print the path as-is. Note that we also only do this for the
+        // short format, because if it's full we presumably want to print
+        // everything.
+        let cwd = if !self.show_full_paths {
+            env::current_dir().ok()
+        } else {
+            None
+        };
+        let mut print_path = move |fmt: &mut Formatter<'_>, path: BytesOrWideString<'_>| {
+            let path = path.into_path_buf();
+            if let Some(cwd) = &cwd {
+                if let Ok(suffix) = path.strip_prefix(cwd) {
+                    return Display::fmt(&suffix.display(), fmt);
+                }
+            }
+            Display::fmt(&path.display(), fmt)
+        };
+
+        let mut formatter = BacktraceFmt::new(f, &mut print_path);
+        for frame in backtrace.frames() {
+            formatter.frame().backtrace_frame(frame)?;
+        }
+        Ok(())
+    }
 }
 
 impl EyreHandler for Handler {
@@ -137,26 +114,24 @@ impl EyreHandler for Handler {
         if let Some(cause) = error.source() {
             self.fmt_cause(cause, f)?;
         }
-        // if let Some(backtrace) = &self.backtrace {
-        //     self.fmt_backtrace(backtrace, f)?;
-        // }
+        if let Some(backtrace) = &self.backtrace {
+            self.fmt_backtrace(backtrace, f)?;
+        }
+
+        if f.alternate() {
+            return core::fmt::Debug::fmt(error, f);
+        }
+
+        write!(f, "{}", error)?;
+
+        if let Some(cause) = error.source() {
+            let errors = std::iter::successors(Some(cause), |e| (*e).source());
+            for error in errors {
+                write!(f, "{}{}", self.line_separator, error)?;
+            }
+        }
 
         Ok(())
-
-        // if f.alternate() {
-        //     return core::fmt::Debug::fmt(error, f);
-        // }
-        //
-        // write!(f, "{}", error)?;
-        //
-        // if let Some(cause) = error.source() {
-        //     let errors = std::iter::successors(Some(cause), |e| (*e).source());
-        //     for error in errors {
-        //         write!(f, "{}{}", self.separator, error)?;
-        //     }
-        // }
-        //
-        // Ok(())
     }
 }
 
@@ -166,31 +141,31 @@ pub struct HookBuilder {
     capture_backtrace_by_default: bool,
     line_separator: Option<&'static str>,
     section_separator: Option<&'static str>,
-    // show_full_paths: bool,
+    show_full_paths: bool,
 }
 
 impl HookBuilder {
     fn make_handler(&self, _error: &(dyn Error + 'static)) -> Handler {
-        // let backtrace = if self.capture_enabled() {
-        //     Some(Backtrace::new())
-        // } else {
-        //     None
-        // };
+        let backtrace = if self.capture_enabled() {
+            Some(Backtrace::new())
+        } else {
+            None
+        };
 
         Handler {
-            // backtrace,
+            backtrace,
             line_separator: self.line_separator.unwrap_or(DEFAULT_LINE_SEPARATOR),
             section_separator: self.section_separator.unwrap_or(DEFAULT_SECTION_SEPARATOR),
-            // show_full_paths: self.show_full_paths,
+            show_full_paths: self.show_full_paths,
         }
     }
 
-    // fn capture_enabled(&self) -> bool {
-    //     env::var("RUST_LIB_BACKTRACE")
-    //         .or_else(|_| env::var("RUST_BACKTRACE"))
-    //         .map(|val| val != "0")
-    //         .unwrap_or(self.capture_backtrace_by_default)
-    // }
+    fn capture_enabled(&self) -> bool {
+        env::var("RUST_LIB_BACKTRACE")
+            .or_else(|_| env::var("RUST_BACKTRACE"))
+            .map(|val| val != "0")
+            .unwrap_or(self.capture_backtrace_by_default)
+    }
 
     /// Configures the default capture mode for `Backtraces` in error reports
     pub fn capture_backtrace_by_default(mut self, cond: bool) -> Self {
@@ -198,12 +173,12 @@ impl HookBuilder {
         self
     }
 
-    // /// Configures whether full paths should be shown in backtraces or relative paths from the
-    // /// current working directory.
-    // pub fn show_full_paths(mut self, cond: bool) -> Self {
-    //     self.show_full_paths = cond;
-    //     self
-    // }
+    /// Configures whether full paths should be shown in backtraces or relative paths from the
+    /// current working directory.
+    pub fn show_full_paths(mut self, cond: bool) -> Self {
+        self.show_full_paths = cond;
+        self
+    }
 
     /// Install the given hook as the global error report hook
     pub fn install(self) -> Result<()> {
