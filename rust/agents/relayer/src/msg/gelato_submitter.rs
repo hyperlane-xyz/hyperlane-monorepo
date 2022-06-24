9cdc78efe05ccb263dc818b360f0a4504014bd28
webbhorn <webb.horn@gmail.com>
2022-06-24 14:34:37 -0400

    drop prover_sync from gelato submitter

--- rust/agents/relayer/src/msg/gelato_submitter.rs
+++ rust/agents/relayer/src/msg/gelato_submitter.rs
@@ -14,10 +14,12 @@ use tracing::{info_span, instrument::Instrumented, Instrument};
 use super::SubmitMessageArgs;
 
 // TODO(webbhorn): Metrics data.
+// TODO(webbhorn): Pull in ForwardRequestOp logic from prior branch.
 
 #[allow(dead_code)]
 #[derive(Debug)]
 pub(crate) struct GelatoSubmitter {
+    // Source of messages to submit.
     rx: mpsc::Receiver<SubmitMessageArgs>,
 
     // Interface to Inbox / InboxValidatorManager on the destination chain.
@@ -32,9 +34,6 @@ pub(crate) struct GelatoSubmitter {
 
     // Interface to agent rocks DB for e.g. writing delivery status upon completion.
     db: AbacusDB,
-
-    // Interface to generating merkle proofs for messages against a checkpoint.
-    prover_sync: MerkleTreeBuilder,
 }
 
 impl GelatoSubmitter {
@@ -51,7 +50,6 @@ impl GelatoSubmitter {
             inbox_contracts,
             interchain_gas_paymaster,
             db: db.clone(),
-            prover_sync: MerkleTreeBuilder::new(db),
         }
     }
 
@@ -60,35 +58,52 @@ impl GelatoSubmitter {
             .instrument(info_span!("submitter work loop"))
     }
 
-    // The relay SDK framework allows us to submit ops in parallel,
-    // subject to certain retry rules. Therefore all we do here is
-    // spin forever asking for work from the rx channel, then spawn
-    // the work to submit to gelato in a root tokio task.
+    // The Gelato relay framework allows us to submit ops in
+    // parallel, subject to certain retry rules. Therefore all we do
+    // here is spin forever asking for work from the rx channel, then
+    // spawn the work to submit to gelato in a root tokio task.
     //
     // It is possible that there has not been sufficient interchain
     // gas deposited in the interchaingaspaymaster account on the source
-    // chain, so we also keep a pending_gas queue of ops that we
+    // chain, so we also keep a wait queue of ops that we
     // periodically scan for any gas updates.
     //
-    // In the future one could maybe imagine also applying a rate-limiter
-    // or something, or a max-inflight-cap on Gelato messages from
-    // relayres, enforced here.
+    // In the future one could maybe imagine also applying a global
+    // rate-limiter against the relevant Gelato HTTP endpoint or
+    // something, or a max-inflight-cap on Gelato messages from
+    // relayers, enforced here. But probably not until that proves to
+    // be necessary.
     async fn work_loop(&mut self) -> Result<()> {
         loop {
-            if self.rx.recv().await.is_none() {
-                break;
-            }
-
-            // TODO(webbhorn): Check if enough gas. If not, put on
-            // pending_gas queue. If there is, spawn it and run the op
-            // in its own task.
-
-            // TODO(webbhorn): Scan pending queue for any newly-eligible
-            // ops and if encountered, spawn them in root task.
-            // Remove them from pending queue.
-            //
-            // Also look for 'expired' ops, i.e. those created >= time ago.
+            self.tick().await?;
+            tokio::task::yield_now().await;
         }
-        Ok(())
+    }
+
+    // Extracted from main loop to enable testing submitter state
+    // after each tick, e.g. in response to a change in environment
+    // conditions like values in InterchainGasPaymaster.
+    async fn tick(&self) -> Result<()> {
+        // TODO(webbhorn): Pull all available messages out of self.rx
+        // and check if enough gas to process them. If not, put on
+        // wait queue. If there is enough, spawn root task and run
+        // the fwd req op.
+
+        // TODO(webbhorn): Scan pending queue for any newly-eligible
+        // ops and if encountered, spawn them in a root task.
+        // Remove them from pending queue if so.
+
+        // TODO(webbhorn): Either wait for finality in the ForwardRequestOp
+        // logic, or follow the pattern from serial_submitter.rs
+        // of implementing a verification queue, where we will stash
+        // successfully submitted ops that have not yet reached finality.
+        // Only after reaching finality will we commit the new status to
+        // AbacusDB and drop those messages from the verificaiton queue.
+        // In case of a destination chain re-org, they would need
+        // to go back to the wait queue.
+
+        // TODO(webbhorn): monitoring / metrics.
+
+        unimplemented!()
     }
 }
