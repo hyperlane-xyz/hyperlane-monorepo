cd498db9bd2d6b384b43ac54e1b9b8ca0aa0b4db
yorhodes <yorke@useabacus.network>
2022-06-27 11:59:01 -0400

    Cleanup script

--- typescript/infra/scripts/fund-relayers-from-deployer.ts
+++ typescript/infra/scripts/fund-relayers-from-deployer.ts
@@ -1,25 +1,57 @@
-// import { ChainMap } from "@abacus-network/sdk";
-// import { BigNumber } from "ethers";
-import { BigNumber, ethers } from 'ethers';
+import { ethers } from 'hardhat';
 
-import { ChainMap, MultiProvider } from '@abacus-network/sdk';
+import {
+  ChainConnection,
+  ChainName,
+  CompleteChainMap,
+} from '@abacus-network/sdk';
 
-import { MainnetChains } from '../config/environments/mainnet/chains';
-import { AgentKey } from '../src/agents/agent';
-import { getAllKeys } from '../src/agents/key-utils';
-import { KEY_ROLE_ENUM } from '../src/agents/roles';
+import { getRelayerKeys } from '../src/agents/key-utils';
 
 import { getCoreEnvironmentConfig, getEnvironment } from './utils';
 
-const desiredBalancePerChain: ChainMap<MainnetChains, BigNumber> = {
-  celo: ethers.utils.parseUnits('0.05', 'ether'),
-  avalanche: ethers.utils.parseUnits('0.1', 'ether'),
-  ethereum: ethers.utils.parseUnits('0.1', 'ether'),
-  polygon: ethers.utils.parseUnits('1', 'ether'),
-  optimism: ethers.utils.parseUnits('0.05', 'ether'),
-  arbitrum: ethers.utils.parseUnits('0.01', 'ether'),
-  bsc: ethers.utils.parseUnits('0.01', 'ether'),
+const desiredBalancePerChain: CompleteChainMap<string> = {
+  celo: '0.05',
+  alfajores: '0.05',
+  avalanche: '0.1',
+  fuji: '0.1',
+  ethereum: '0.1',
+  kovan: '0.1',
+  polygon: '1',
+  mumbai: '1',
+  optimism: '0.05',
+  optimismkovan: '0.05',
+  arbitrum: '0.01',
+  arbitrumrinkeby: '0.01',
+  bsc: '0.01',
+  bsctestnet: '0.01',
+  // unused
+  goerli: '0',
+  auroratestnet: '0',
+  test1: '0',
+  test2: '0',
+  test3: '0',
 };
+
+async function fundAddress(
+  chainConnection: ChainConnection,
+  address: string,
+  desiredBalance: string,
+) {
+  const currentBalance = await chainConnection.provider.getBalance(address);
+
+  const desiredBalanceEther = ethers.utils.parseUnits(desiredBalance, 'ether');
+  const delta = desiredBalanceEther.sub(currentBalance);
+  if (delta.gt(0)) {
+    const tx = await chainConnection.signer!.sendTransaction({
+      to: address,
+      value: delta,
+      ...chainConnection.overrides,
+    });
+    console.log(chainConnection.getTxUrl(tx));
+    await tx.wait(chainConnection.confirmations);
+  }
+}
 
 async function main() {
   const environment = await getEnvironment();
@@ -27,147 +59,20 @@ async function main() {
 
   const multiProvider = await config.getMultiProvider();
 
-  const relayerKeys = getAllKeys(config.agent).filter(
-    (key) => key.role === KEY_ROLE_ENUM.Relayer,
-  );
+  const relayerKeys = getRelayerKeys(config.agent);
 
-  console.log('Deployer key balances:');
-  await printDeployerBalances(multiProvider);
-
-  let addressesToFundPerChain: any = {};
-  const insertAddressToFund = async (
-    _: MainnetChains,
-    remote: MainnetChains,
-    address: string,
-  ) => {
-    if (addressesToFundPerChain[remote]) {
-      addressesToFundPerChain[remote].push(address);
-    } else {
-      addressesToFundPerChain[remote] = [address];
-    }
-  };
-  await forEachRelayerKey(multiProvider, relayerKeys, insertAddressToFund);
-  console.log('addressesToFundPerChain', addressesToFundPerChain);
-
-  const printBalance = async (
-    local: MainnetChains,
-    remote: MainnetChains,
-    address: string,
-  ) => {
-    const provider = multiProvider.getChainConnection(remote).provider;
-
-    const balance = await provider.getBalance(address);
-
-    console.log(
-      `Local ${local} remote ${remote} address ${address} balance ${balance} (${ethers.utils.formatEther(
-        balance,
-      )})`,
-    );
-  };
-  console.log(
-    'relayer key balances before',
-    await forEachRelayerKey(multiProvider, relayerKeys, printBalance),
-  );
-
-  const fundAddress = async (
-    local: MainnetChains,
-    remote: MainnetChains,
-    address: string,
-  ) => {
-    const deployer = multiProvider.getChainConnection(remote).signer;
-    if (!deployer) {
-      throw Error(`No deployer signer for ${remote}`);
-    }
-    const desiredBalance = desiredBalancePerChain[remote];
-    const currentBalance = await deployer.provider!.getBalance(address);
-
-    if (desiredBalance > currentBalance) {
-      const sendAmount = desiredBalance.sub(currentBalance);
-
-      console.log(
-        `Local ${local} remote ${remote} address ${address}, sending ${sendAmount} (${ethers.utils.formatEther(
-          sendAmount,
-        )}) native tokens to ${address}`,
-      );
-
-      const gasPrice = await deployer.getGasPrice();
-
-      console.log('gasPrice', gasPrice.toString());
-
-      try {
-        const tx = await deployer.sendTransaction({
-          to: address,
-          value: sendAmount,
-          gasPrice,
-        });
-        console.log('tx', tx);
-        console.log('receipt', await tx.wait());
-      } catch (err) {
-        // This try/catch was added because of issues with celo-ethers-wrapper.
-        // The only way I could get things to run was by changing parseCeloTransaction
-        // in node_modules and instead using this `tx` object, which would still
-        // result in ethers throwing but at least caused the signed tx to be sent to the network
-        //
-        //   const tx = {
-        //     nonce: handleNumber(transaction[0]).toNumber(),
-        //     gasPrice: handleNumber(transaction[1]),
-        //     gasLimit: handleNumber(transaction[2]),
-        //     feeCurrency: handleAddress('0x'), //transaction[3]),
-        //     gatewayFeeRecipient: handleAddress('0x'), // transaction[4]),
-        //     gatewayFee: handleNumber('0x'), //transaction[5]),
-        //     to: handleAddress(transaction[3]),
-        //     value: handleNumber(transaction[4]),
-        //     data: transaction[5],
-        //     chainId: 0,
-        // };
-        //
-        // Just ignore the error
-        console.log('some err', err);
-      }
-    }
-  };
-
-  await forEachRelayerKey(multiProvider, relayerKeys, fundAddress);
-
-  console.log(
-    'relayer key balances after',
-    await forEachRelayerKey(multiProvider, relayerKeys, printBalance),
-  );
-}
-
-async function forEachRelayerKey(
-  multiProvider: MultiProvider<MainnetChains>,
-  relayerKeys: AgentKey[],
-  cb: (
-    local: MainnetChains,
-    remote: MainnetChains,
-    address: string,
-  ) => Promise<void>,
-) {
   for (const relayerKey of relayerKeys) {
     await relayerKey.fetch();
-    const relayerAddress = relayerKey.address;
 
-    for (const remote of multiProvider.remoteChains(relayerKey.chainName)) {
-      await cb(relayerKey.chainName!, remote, relayerAddress);
+    for (const remote of multiProvider.remoteChains(
+      relayerKey.chainName,
+    ) as ChainName[]) {
+      await fundAddress(
+        multiProvider.getChainConnection(remote),
+        relayerKey.address,
+        desiredBalancePerChain[remote],
+      );
     }
-  }
-}
-
-async function printDeployerBalances(
-  multiProvider: MultiProvider<MainnetChains>,
-) {
-  const deployerAddress = await multiProvider
-    .getChainConnection('celo')
-    .getAddress()!;
-
-  console.log('deployerAddress', deployerAddress);
-
-  for (const chain of multiProvider.chains()) {
-    const balance = await multiProvider
-      .getChainConnection(chain)
-      .provider.getBalance(deployerAddress);
-    console.log(`Chain ${chain} balance: ${ethers.utils.formatEther(balance)}`);
   }
 }
 
