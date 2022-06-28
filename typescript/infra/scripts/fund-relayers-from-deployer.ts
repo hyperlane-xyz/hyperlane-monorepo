941bd41476d43ecc986849e3a77723bdedbd2375
yorhodes <yorke@useabacus.network>
2022-06-28 17:29:43 -0400

    Address pr comments

--- typescript/infra/scripts/fund-relayers-from-deployer.ts
+++ typescript/infra/scripts/fund-relayers-from-deployer.ts
@@ -1,30 +1,29 @@
 import { ethers } from 'ethers';
 
-import {
-  ChainConnection,
-  ChainName,
-  CompleteChainMap,
-} from '@abacus-network/sdk';
+import { ChainConnection, CompleteChainMap } from '@abacus-network/sdk';
 
+import { AgentKey } from '../src/agents/agent';
 import { getRelayerKeys } from '../src/agents/key-utils';
 
 import { getCoreEnvironmentConfig, getEnvironment } from './utils';
 
+const MIN_DELTA = ethers.utils.parseUnits('0.001', 'ether');
+
 const desiredBalancePerChain: CompleteChainMap<string> = {
   celo: '0.05',
-  alfajores: '0.05',
+  alfajores: '0.1',
   avalanche: '0.1',
   fuji: '0.1',
   ethereum: '0.1',
   kovan: '0.1',
   polygon: '1',
-  mumbai: '1',
+  mumbai: '0.1',
   optimism: '0.05',
-  optimismkovan: '0.05',
+  optimismkovan: '0.1',
   arbitrum: '0.01',
-  arbitrumrinkeby: '0.01',
+  arbitrumrinkeby: '0.1',
   bsc: '0.01',
-  bsctestnet: '0.01',
+  bsctestnet: '0.1',
   // unused
   goerli: '0',
   auroratestnet: '0',
@@ -33,24 +32,37 @@ const desiredBalancePerChain: CompleteChainMap<string> = {
   test3: '0',
 };
 
-async function fundAddress(
+async function fundRelayer(
   chainConnection: ChainConnection,
-  address: string,
+  relayer: AgentKey,
   desiredBalance: string,
 ) {
-  const currentBalance = await chainConnection.provider.getBalance(address);
+  const currentBalance = await chainConnection.provider.getBalance(
+    relayer.address,
+  );
   const desiredBalanceEther = ethers.utils.parseUnits(desiredBalance, 'ether');
   const delta = desiredBalanceEther.sub(currentBalance);
 
-  if (delta.gt(0)) {
+  if (delta.gt(MIN_DELTA)) {
+    console.log(
+      `sending ${relayer.chainName} relayer ${ethers.utils.formatEther(
+        delta,
+      )}...`,
+    );
     const tx = await chainConnection.signer!.sendTransaction({
-      to: address,
+      to: relayer.address,
       value: delta,
       ...chainConnection.overrides,
     });
     console.log(chainConnection.getTxUrl(tx));
     await tx.wait(chainConnection.confirmations);
   }
+
+  console.log(
+    `${relayer.chainName} relayer : ${ethers.utils.formatEther(
+      await chainConnection.provider.getBalance(relayer.address),
+    )}`,
+  );
 }
 
 async function main() {
@@ -60,19 +72,28 @@ async function main() {
 
   const relayerKeys = getRelayerKeys(config.agent);
 
-  for (const relayerKey of relayerKeys) {
-    await relayerKey.fetch();
+  const chains = relayerKeys.map((key) => key.chainName!);
 
-    const remotes = multiProvider.remoteChains(
-      relayerKey.chainName,
-    ) as ChainName[];
-    for (const remote of remotes) {
-      await fundAddress(
-        multiProvider.getChainConnection(remote),
-        relayerKey.address,
-        desiredBalancePerChain[remote],
-      );
+  for (const chain of chains) {
+    const chainConnection = multiProvider.getChainConnection(chain);
+
+    const desiredBalance = desiredBalancePerChain[chain];
+
+    console.group(
+      chain,
+      `funder : ${ethers.utils.formatEther(
+        await chainConnection.signer!.getBalance(),
+      )} relayer desired : ${desiredBalance}`,
+    );
+
+    for (const relayerKey of relayerKeys.filter(
+      (key) => key.chainName !== chain,
+    )) {
+      await relayerKey.fetch();
+      await fundRelayer(chainConnection, relayerKey, desiredBalance);
     }
+
+    console.groupEnd();
   }
 }
 
