30649916395fbf86b00a3bb88b4d8648f166b7d0
Mattie Conover <git@mconover.dev>
2022-06-29 11:41:17 -0400

    Better Retrying provider (#654)
    * WIP
    
    * Update retrying provider to only retry on connection issues
    
    * use better base retry time logic
    
    * cleanup
    
    * don't retry serialization errors
    
    * simplify logs
    
    * better error messages
    
    * Update loop logic
    
    * fix lint

--- rust/chains/abacus-ethereum/src/retrying.rs
+++ rust/chains/abacus-ethereum/src/retrying.rs
@@ -1,37 +1,56 @@
 use std::{fmt::Debug, str::FromStr, time::Duration};
 
 use async_trait::async_trait;
-use ethers::providers::{JsonRpcClient, ProviderError};
+use ethers::providers::{Http, JsonRpcClient, ProviderError};
 use serde::{de::DeserializeOwned, Serialize};
 use serde_json::Value;
 use thiserror::Error;
 use tokio::time::sleep;
-use tracing::{debug, instrument, warn};
+use tracing::{debug, instrument, trace, warn};
+
+use crate::HttpClientError;
 
 /// An HTTP Provider with a simple naive exponential backoff built-in
 #[derive(Debug, Clone)]
 pub struct RetryingProvider<P> {
     inner: P,
-    max_requests: usize,
+    max_requests: u32,
+    base_retry_ms: u64,
 }
 
 impl<P> RetryingProvider<P> {
     /// Instantiate a RetryingProvider
-    pub fn new(inner: P, max_requests: usize) -> Self {
-        Self {
+    pub fn new(inner: P, max_requests: u32, base_retry_ms: u64) -> Self {
+        let mut zelf = Self {
             inner,
-            max_requests,
-        }
+            max_requests: 0,
+            base_retry_ms: 0,
+        };
+        zelf.set_max_requests(max_requests);
+        zelf.set_base_retry_ms(base_retry_ms);
+        zelf
     }
 
-    /// Set the max_requests (and by extension the total time a request can take)
-    pub fn set_max_requests(&mut self, max_requests: usize) {
+    /// Set the max_requests (and by extension the total time a request can take).
+    pub fn set_max_requests(&mut self, max_requests: u32) {
+        assert!(max_requests >= 1);
         self.max_requests = max_requests;
     }
 
+    /// Set what the base amount of backoff time there should be.
+    pub fn set_base_retry_ms(&mut self, base_retry_ms: u64) {
+        assert!(base_retry_ms >= 1);
+        self.base_retry_ms = base_retry_ms;
+    }
+
     /// Get the max_requests
-    pub fn get_max_requests(&self) -> usize {
+    pub fn max_requests(&self) -> u32 {
         self.max_requests
+    }
+
+    /// Get the base retry duration in ms.
+    pub fn base_retry_ms(&self) -> u64 {
+        self.base_retry_ms
     }
 }
 
@@ -41,12 +60,12 @@ pub enum RetryingProviderError<P>
 where
     P: JsonRpcClient,
 {
-    // /// An internal error in the JSON RPC Client
-    // #[error(transparent)]
-    // JsonRpcClientError(#[from] <P as JsonRpcClient>::Error),
+    /// An internal error in the JSON RPC Client which we did not want to retry on.
+    #[error(transparent)]
+    JsonRpcClientError(P::Error),
     /// Hit max requests
     #[error("Hit max requests")]
-    MaxRequests(Vec<P::Error>),
+    MaxRequests(P::Error),
 }
 
 impl<P> From<RetryingProviderError<P>> for ProviderError
@@ -60,56 +79,71 @@ where
 }
 
 #[async_trait]
-impl<P> JsonRpcClient for RetryingProvider<P>
-where
-    P: JsonRpcClient + 'static,
-    <P as JsonRpcClient>::Error: Send + Sync,
-{
-    type Error = RetryingProviderError<P>;
+impl JsonRpcClient for RetryingProvider<Http> {
+    type Error = RetryingProviderError<Http>;
 
     #[instrument(
     level = "debug",
     err,
     skip(params),
-    fields(params = % serde_json::to_string(& params).unwrap()))
+    fields(method = %method, params = %serde_json::to_string(&params).unwrap()))
     ]
     async fn request<T, R>(&self, method: &str, params: T) -> Result<R, Self::Error>
     where
         T: Debug + Serialize + Send + Sync,
         R: DeserializeOwned,
     {
-        let mut errors = vec![];
-
         let params = serde_json::to_value(params).expect("valid");
 
-        for i in 0..self.max_requests {
-            let backoff_seconds = 2u64.pow(i as u32);
-            {
-                debug!(attempt = i, "Dispatching request");
+        let mut last_err;
+        let mut i = 1;
+        loop {
+            let backoff_ms = self.base_retry_ms * 2u64.pow(i - 1);
+            debug!(attempt = i, "Dispatching request");
 
-                let fut = match params {
-                    Value::Null => self.inner.request(method, ()),
-                    _ => self.inner.request(method, &params),
-                };
+            let fut = match params {
+                Value::Null => self.inner.request(method, ()),
+                _ => self.inner.request(method, &params),
+            };
 
-                match fut.await {
-                    Ok(res) => return Ok(res),
-                    Err(e) => {
-                        warn!(
-                            backoff_seconds,
-                            retries_remaining = self.max_requests - i - 1,
-                            error = %e,
-                            method = %method,
-                            "Error in retrying provider",
-                        );
-                        errors.push(e);
-                    }
+            match fut.await {
+                Ok(res) => return Ok(res),
+                Err(HttpClientError::ReqwestError(e)) => {
+                    warn!(
+                        backoff_ms,
+                        retries_remaining = self.max_requests - i,
+                        error = %e,
+                        "ReqwestError in retrying provider.",
+                    );
+                    last_err = HttpClientError::ReqwestError(e);
+                }
+                Err(HttpClientError::JsonRpcError(e)) => {
+                    // This is a client error so we do not want to retry on it.
+                    warn!(error = %e, "JsonRpcError in retrying provider; not retrying.");
+                    return Err(RetryingProviderError::JsonRpcClientError(
+                        HttpClientError::JsonRpcError(e),
+                    ));
+                }
+                Err(HttpClientError::SerdeJson { err, text }) => {
+                    warn!(error = %err, "SerdeJson error in retrying provider; not retrying.");
+                    return Err(RetryingProviderError::JsonRpcClientError(
+                        HttpClientError::SerdeJson { err, text },
+                    ));
                 }
             }
-            sleep(Duration::from_secs(backoff_seconds)).await;
-        }
 
-        return Err(RetryingProviderError::MaxRequests(errors));
+            i += 1;
+            if i <= self.max_requests {
+                trace!(backoff_ms, "Retrying provider going to sleep.");
+                sleep(Duration::from_millis(backoff_ms)).await;
+            } else {
+                trace!(
+                    requests_made = self.max_requests,
+                    "Retrying provider reached max requests."
+                );
+                return Err(RetryingProviderError::MaxRequests(last_err));
+            }
+        }
     }
 }
 
@@ -120,6 +154,6 @@ where
     type Err = <P as FromStr>::Err;
 
     fn from_str(src: &str) -> Result<Self, Self::Err> {
-        Ok(Self::new(src.parse()?, 6))
+        Ok(Self::new(src.parse()?, 6, 50))
     }
 }
