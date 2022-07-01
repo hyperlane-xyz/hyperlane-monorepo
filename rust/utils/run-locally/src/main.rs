11ed251795d40ba976f4fa827ad11baf462f3333
Mattie Conover <git@mconover.dev>
2022-07-01 15:25:56 -0400

    Support CI mode

--- rust/utils/run-locally/src/main.rs
+++ rust/utils/run-locally/src/main.rs
@@ -1,12 +1,22 @@
 //! Run this from the abacus-monorepo/rust directory using `cargo run -r -p run-locally`.
+//!
+//! Enviornment arguments:
+//! - `E2E_CI_MODE`: true/false, enables CI mode which will automatically wait for kathy to finish
+//! running and for the queues to empty.
+//! - `E2E_CI_TIMEOUT_SEC`: How long (in seconds) to allow the main loop to run the test for. This
+//! does not include the initial setup time. If this timeout is reached before the end conditions
+//! are met, the test is a failure. Defaults to 5 min.
+//! - `E2E_KATHY_ROUNDS`: Number of rounds to run kathy for. Defaults to 4 if CI mode is enabled.
+//! - `E2E_LOG_ALL`: Log all output instead of writing to log files. Defaults to true if CI mode,
+//! else false.
 
 use std::fs::File;
 use std::io::{BufRead, BufReader, BufWriter, Read, Write};
 use std::path::{Path, PathBuf};
-use std::process::{Child, Command, Stdio};
+use std::process::{Child, Command, ExitCode, Stdio};
 use std::sync::atomic::{AtomicBool, Ordering};
-use std::thread::{sleep, spawn};
-use std::time::{Duration, SystemTime, UNIX_EPOCH};
+use std::thread::{sleep, spawn, JoinHandle};
+use std::time::{Duration, Instant, SystemTime, UNIX_EPOCH};
 use std::{env, fs};
 
 use maplit::hashmap;
@@ -18,12 +28,80 @@ use tempfile::tempdir;
 
 static RUNNING: AtomicBool = AtomicBool::new(true);
 
-fn main() {
+#[derive(Default)]
+struct State {
+    kathy: Option<Child>,
+    node: Option<Child>,
+    relayer: Option<Child>,
+    validator: Option<Child>,
+
+    watchers: Vec<JoinHandle<()>>,
+}
+
+impl Drop for State {
+    fn drop(&mut self) {
+        println!("Signaling children to stop...");
+        if let Some(mut c) = self.kathy.take() {
+            send_sigterm(&mut c);
+            if !c.wait().unwrap().success() {
+                eprintln!("Kathy exited with error code")
+            };
+        }
+        if let Some(mut c) = self.relayer.take() {
+            send_sigterm(&mut c);
+            if !c.wait().unwrap().success() {
+                eprintln!("Relayer exited with error code")
+            };
+        }
+        if let Some(mut c) = self.validator.take() {
+            send_sigterm(&mut c);
+            if !c.wait().unwrap().success() {
+                eprintln!("Validator exited with error code")
+            };
+        }
+        if let Some(mut c) = self.node.take() {
+            send_sigterm(&mut c);
+            if !c.wait().unwrap().success() {
+                eprintln!("Node exited with error code")
+            };
+        }
+        println!("Joining watchers...");
+        RUNNING.store(false, Ordering::Relaxed);
+        for w in self.watchers.drain(..) {
+            w.join().unwrap();
+        }
+    }
+}
+
+fn main() -> ExitCode {
     ctrlc::set_handler(|| {
         println!("Terminating...");
         RUNNING.store(false, Ordering::Relaxed);
     })
     .unwrap();
+
+    let ci_mode = env::var("E2E_CI_MODE")
+        .map(|k| k.parse::<bool>().unwrap())
+        .unwrap_or_default();
+
+    let ci_mode_timeout = env::var("E2E_CI_TIMEOUT_SEC")
+        .map(|k| k.parse::<u64>().unwrap())
+        .unwrap_or(60 * 5);
+
+    let kathy_rounds = {
+        let r = env::var("E2E_KATHY_ROUNDS")
+            .ok()
+            .map(|r| r.parse::<u64>().unwrap());
+        if ci_mode && r.is_none() {
+            Some(4)
+        } else {
+            r
+        }
+    };
+
+    let log_all = env::var("E2E_LOG_ALL")
+        .map(|k| k.parse::<bool>().unwrap())
+        .unwrap_or(ci_mode);
 
     let date_str = SystemTime::now()
         .duration_since(UNIX_EPOCH)
@@ -31,7 +109,9 @@ fn main() {
         .as_secs()
         .to_string();
     let log_dir = concat_path(env::temp_dir(), format!("logs/abacus-agents/{date_str}"));
-    fs::create_dir_all(&log_dir).expect("Failed to make log dir");
+    if !log_all {
+        fs::create_dir_all(&log_dir).expect("Failed to make log dir");
+    }
     let build_log = concat_path(&log_dir, "build.log");
     let hardhat_log = concat_path(&log_dir, "hardhat.stdout.log");
     let relayer_stdout_log = concat_path(&log_dir, "relayer.stdout.log");
@@ -92,7 +172,9 @@ fn main() {
         "ABC_VALIDATOR_CHECKPOINTSYNCER_PATH" => checkpoints_dir.path().to_str().unwrap(),
     };
 
-    println!("Logs in {}", log_dir.display());
+    if !log_all {
+        println!("Logs in {}", log_dir.display());
+    }
     println!("Signed checkpoints in {}", checkpoints_dir.path().display());
     println!("Relayer DB in {}", relayer_db.display());
     println!("Validator DB in {}", validator_db.display());
@@ -101,27 +183,53 @@ fn main() {
     build_cmd(
         &["yarn", "install"],
         &build_log,
+        log_all,
         Some("../typescript/infra"),
     );
-    build_cmd(&["yarn", "build"], &build_log, Some("../typescript"));
+    build_cmd(
+        &["yarn", "build"],
+        &build_log,
+        log_all,
+        Some("../typescript"),
+    );
 
     println!("Building relayer...");
-    build_cmd(&["cargo", "build", "--bin", "relayer"], &build_log, None);
+    build_cmd(
+        &["cargo", "build", "--bin", "relayer"],
+        &build_log,
+        log_all,
+        None,
+    );
 
     println!("Building validator...");
-    build_cmd(&["cargo", "build", "--bin", "validator"], &build_log, None);
+    build_cmd(
+        &["cargo", "build", "--bin", "validator"],
+        &build_log,
+        log_all,
+        None,
+    );
 
-    let mut watchers = Vec::new();
-    let mut children = Vec::new();
-
+    let mut state = State::default();
     println!("Launching hardhat...");
-    let node = Command::new("yarn")
-        .args(&["hardhat", "node"])
-        .current_dir("../typescript/infra")
-        .stdout(append_to(&hardhat_log))
-        .spawn()
-        .expect("Failed to start node");
-    children.push(node);
+    let mut node = Command::new("yarn");
+    node.args(&["hardhat", "node"])
+        .current_dir("../typescript/infra");
+    if log_all {
+        // TODO: should we log this? It seems way too verbose to be useful
+        // node.stdout(Stdio::piped());
+        node.stdout(Stdio::null());
+    } else {
+        node.stdout(append_to(&hardhat_log));
+    }
+    let node = node.spawn().expect("Failed to start node");
+    // if log_all {
+    //     let output = node.stdout.take().unwrap();
+    //     state
+    //         .watchers
+    //         .push(spawn(move || prefix_log(output, "ETH")))
+    // }
+    state.node = Some(node);
+
     sleep(Duration::from_secs(5));
 
     println!("Deploying abacus contracts...");
@@ -143,14 +251,22 @@ fn main() {
         .spawn()
         .expect("Failed to start relayer");
     let relayer_stdout = relayer.stdout.take().unwrap();
-    watchers.push(spawn(move || {
-        inspect_and_write_to_file(relayer_stdout, relayer_stdout_log, Some("ERROR"))
+    state.watchers.push(spawn(move || {
+        if log_all {
+            prefix_log(relayer_stdout, "RLY")
+        } else {
+            inspect_and_write_to_file(relayer_stdout, relayer_stdout_log, Some("ERROR"))
+        }
     }));
     let relayer_stderr = relayer.stderr.take().unwrap();
-    watchers.push(spawn(move || {
-        inspect_and_write_to_file(relayer_stderr, relayer_stderr_log, None)
+    state.watchers.push(spawn(move || {
+        if log_all {
+            prefix_log(relayer_stderr, "RLY")
+        } else {
+            inspect_and_write_to_file(relayer_stderr, relayer_stderr_log, None)
+        }
     }));
-    children.push(relayer);
+    state.relayer = Some(relayer);
 
     println!("Spawning validator...");
     let mut validator = Command::new("target/debug/validator")
@@ -161,50 +277,115 @@ fn main() {
         .spawn()
         .expect("Failed to start validator");
     let validator_stdout = validator.stdout.take().unwrap();
-    watchers.push(spawn(move || {
-        inspect_and_write_to_file(validator_stdout, validator_stdout_log, Some("ERROR"))
+    state.watchers.push(spawn(move || {
+        if log_all {
+            prefix_log(validator_stdout, "VAL")
+        } else {
+            inspect_and_write_to_file(validator_stdout, validator_stdout_log, Some("ERROR"))
+        }
     }));
     let validator_stderr = validator.stderr.take().unwrap();
-    watchers.push(spawn(move || {
-        inspect_and_write_to_file(validator_stderr, validator_stderr_log, None)
+    state.watchers.push(spawn(move || {
+        if log_all {
+            prefix_log(validator_stderr, "VAL")
+        } else {
+            inspect_and_write_to_file(validator_stderr, validator_stderr_log, None)
+        }
     }));
-    children.push(validator);
+    state.validator = Some(validator);
 
     println!("Setup complete! Agents running in background...");
     println!("Ctrl+C to end execution...");
 
     println!("Spawning Kathy to send Abacus message traffic...");
-    let mut kathy = Command::new("yarn")
-        .args(&["kathy"])
+    let mut kathy = Command::new("yarn");
+    kathy.arg("kathy");
+    if let Some(r) = kathy_rounds {
+        kathy.args(&["--rounds", &r.to_string()]);
+    }
+    let mut kathy = kathy
         .current_dir("../typescript/infra")
         .stdout(Stdio::piped())
         .spawn()
         .expect("Failed tp start kathy");
     let kathy_stdout = kathy.stdout.take().unwrap();
-    watchers.push(spawn(move || {
-        inspect_and_write_to_file(kathy_stdout, kathy_log, Some("send"))
+    state.watchers.push(spawn(move || {
+        if log_all {
+            prefix_log(kathy_stdout, "KTY")
+        } else {
+            inspect_and_write_to_file(kathy_stdout, kathy_log, Some("send"))
+        }
     }));
-    children.push(kathy);
+    state.kathy = Some(kathy);
 
-    // curl http://127.0.0.1:9092/metrics 2>/dev/null | egrep -o "^abacus_processor_retry_queue{.+} [0-9]+$" | egrep -o "[0-9]+$"
-
-    // Emit any ERROR logs found in an agent's stdout or the presence of anything at all in stderr.
-
-    // (tail -f "${RELAYER_STDOUT_LOG?}" | grep ERROR) &
-    // (tail -f "${VALIDATOR_STDOUT_LOG?}" | grep ERROR) &
-    // (tail -f "${RELAYER_STDERR_LOG?}") &
-    // (tail -f "${VALIDATOR_STDERR_LOG?}") &
-
+    let loop_start = Instant::now();
+    // give things a chance to fully start.
+    sleep(Duration::from_secs(5));
+    let mut kathy_done = false;
     while RUNNING.fetch_and(true, Ordering::Relaxed) {
-        sleep(Duration::from_millis(100));
+        if !kathy_done {
+            // check if kathy has finished
+            match state.kathy.as_mut().unwrap().try_wait().unwrap() {
+                Some(s) if s.success() => {
+                    kathy_done = true;
+                }
+                Some(_) => {
+                    return ExitCode::from(1);
+                }
+                None => {}
+            }
+        }
+        if ci_mode {
+            // for CI we have to look for the end condition.
+            if kathy_done && retry_queus_empty() {
+                // end condition reached successfully
+                println!("Kathy completed successfully and the retry queues are empty");
+                break;
+            } else if (Instant::now() - loop_start).as_secs() > ci_mode_timeout {
+                // we ran out of time
+                eprintln!("CI timeout reached before queues emptied and or kathy finished.");
+                return ExitCode::from(1);
+            }
+        } else if kathy_done {
+            // when not in CI mode, run until kathy finishes, which should only happen if a number
+            // of rounds is specified.
+            break;
+        }
+        sleep(Duration::from_secs(1));
     }
-    println!("End of sleep");
 
-    for mut c in children {
-        send_sigterm(&mut c);
-    }
-    for w in watchers {
-        w.join().unwrap();
+    ExitCode::from(0)
+}
+
+fn retry_queus_empty() -> bool {
+    reqwest::blocking::get("http://127.0.0.1:9092")
+        .unwrap()
+        .text()
+        .unwrap()
+        .lines()
+        .filter(|l| l.starts_with("abacus_processor_retry_queue"))
+        .map(|l| l.rsplit_once(' ').unwrap().1.parse::<u32>().unwrap())
+        .all(|n| n == 0)
+}
+
+fn prefix_log(output: impl Read, name: &'static str) {
+    let mut reader = BufReader::new(output).lines();
+    loop {
+        if let Some(line) = reader.next() {
+            let line = match line {
+                Ok(l) => l,
+                Err(e) => {
+                    // end of stream, probably
+                    eprintln!("Error reading from output for {name}: {e}");
+                    break;
+                }
+            };
+            println!("<{name}> {line}");
+        } else if RUNNING.fetch_and(true, Ordering::Relaxed) {
+            sleep(Duration::from_millis(10));
+        } else {
+            break;
+        }
     }
 }
 
@@ -237,7 +418,7 @@ fn inspect_and_write_to_file(
             }
             writeln!(writer, "{line}").unwrap();
         } else if RUNNING.fetch_and(true, Ordering::Relaxed) {
-            sleep(Duration::from_millis(10));
+            sleep(Duration::from_millis(10))
         } else {
             break;
         }
@@ -268,10 +449,15 @@ fn append_to(p: impl AsRef<Path>) -> File {
         .expect("Failed to open file")
 }
 
-fn build_cmd(cmd: &[&str], log: impl AsRef<Path>, wd: Option<&str>) {
+fn build_cmd(cmd: &[&str], log: impl AsRef<Path>, log_all: bool, wd: Option<&str>) {
     assert!(!cmd.is_empty(), "Must specify a command!");
     let mut c = Command::new(cmd[0]);
-    c.args(&cmd[1..]).stdout(append_to(log));
+    c.args(&cmd[1..]);
+    if log_all {
+        c.stdout(Stdio::inherit());
+    } else {
+        c.stdout(append_to(log));
+    }
     if let Some(wd) = wd {
         c.current_dir(wd);
     }
