84e60a24ca25a5e66130cd13914da77946694a5c
nambrot <nambrot@googlemail.com>
2022-07-01 14:03:49 -0400

    Remove reference to dev environment

--- typescript/infra/config/environments/index.ts
+++ typescript/infra/config/environments/index.ts
@@ -1,10 +1,8 @@
-import { environment as dev } from './dev';
 import { environment as mainnet } from './mainnet';
 import { environment as test } from './test';
 import { environment as testnet2 } from './testnet2';
 
 export const environments = {
-  dev,
   test,
   testnet2,
   mainnet,
