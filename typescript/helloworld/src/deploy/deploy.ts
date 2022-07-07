7241f36d42c0a07461c877ad96057f337080c7db
J M Rossy <jm.rossy@gmail.com>
2022-07-07 18:30:44 -0400

    Add @abacus-network/helloworld back to monorepo

---
+++ typescript/helloworld/src/deploy/deploy.ts
@@ -0,0 +1,42 @@
+import {
+  AbacusCore,
+  AbacusRouterDeployer,
+  ChainMap,
+  ChainName,
+  MultiProvider,
+} from '@abacus-network/sdk';
+
+import {
+  HelloWorldContracts,
+  HelloWorldFactories,
+  helloWorldFactories,
+} from '../app/contracts';
+
+import { HelloWorldConfig } from './config';
+
+export class HelloWorldDeployer<
+  Chain extends ChainName,
+> extends AbacusRouterDeployer<
+  Chain,
+  HelloWorldContracts,
+  HelloWorldFactories,
+  HelloWorldConfig
+> {
+  constructor(
+    multiProvider: MultiProvider<Chain>,
+    configMap: ChainMap<Chain, HelloWorldConfig>,
+    protected core: AbacusCore<Chain>,
+  ) {
+    super(multiProvider, configMap, helloWorldFactories, {});
+  }
+
+  // Custom contract deployment logic can go here
+  // If no custom logic is needed, call deployContract for the router
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  async deployContracts(chain: Chain, config: HelloWorldConfig) {
+    const router = await this.deployContract(chain, 'router', []);
+    return {
+      router,
+    };
+  }
+}
