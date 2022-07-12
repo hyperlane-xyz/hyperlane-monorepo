f2d606083f22ce112cc221535e0e370aed19c020
Mattie Conover <git@mconover.dev>
2022-07-12 16:00:52 -0400

    add prometheus metrics to kathy

--- typescript/infra/scripts/helloworld/kathy.ts
+++ typescript/infra/scripts/helloworld/kathy.ts
@@ -1,3 +1,5 @@
+import { Counter, Pushgateway, Registry } from 'prom-client';
+
 import { HelloWorldApp } from '@abacus-network/helloworld';
 import { ChainName, Chains } from '@abacus-network/sdk';
 
@@ -6,8 +8,29 @@ import { getCoreEnvironmentConfig, getEnvironment } from '../utils';
 
 import { getApp } from './utils';
 
+const constMetricLabels = {
+  // this needs to get set in main because of async reasons
+  abacus_deployment: '',
+  abacus_context: 'abacus',
+};
+
+const metricsRegister = new Registry();
+const messagesCount = new Counter({
+  name: 'abacus_kathy_messages',
+  help: 'Test messages which have been sent with',
+  registers: [metricsRegister],
+  labelNames: [
+    'origin',
+    'remote',
+    'status',
+    ...(Object.keys(constMetricLabels) as (keyof typeof constMetricLabels)[]),
+  ],
+});
+metricsRegister.registerMetric(messagesCount);
+
 async function main() {
   const environment = await getEnvironment();
+  constMetricLabels.abacus_deployment = environment;
   const coreConfig = getCoreEnvironmentConfig(environment);
   const app = await getApp(coreConfig);
   const chains = app.chains() as Chains[];
@@ -26,20 +49,29 @@ async function main() {
 
   const sources = chains.filter((chain) => !skip || !skip.includes(chain));
   for (const source of sources) {
-    for (const destination of sources.slice().filter((d) => d !== source)) {
+    for (const destination of sources.filter((d) => d !== source)) {
+      const labels = {
+        origin: source,
+        remote: destination,
+        ...constMetricLabels,
+      };
       try {
         await sendMessage(app, source, destination);
+        messagesCount.labels({ ...labels, status: 'success' }).inc();
       } catch (err) {
         console.error(
           `Error sending message from ${source} to ${destination}, continuing...`,
           `${err}`.replaceAll('\n', ' ## '),
         );
         failureOccurred = true;
+        messagesCount.labels({ ...labels, status: 'failure' }).inc();
       }
       // Sleep 500ms to avoid race conditions where nonces are reused
       await sleep(500);
     }
   }
+
+  await submitMetrics();
 
   if (failureOccurred) {
     console.error('Failure occurred at least once');
@@ -55,6 +87,32 @@ async function sendMessage(
   console.log(`Sending message from ${source} to ${destination}`);
   const receipt = await app.sendHelloWorld(source, destination, `Hello!`);
   console.log(JSON.stringify(receipt.events || receipt.logs));
+}
+
+function getPushGateway(): Pushgateway | null {
+  const gatewayAddr = process.env['PROMETHEUS_PUSH_GATEWAY'];
+  if (gatewayAddr) {
+    return new Pushgateway(gatewayAddr, [], metricsRegister);
+  } else {
+    console.warn(
+      'Prometheus push gateway address was not defined; not publishing metrics.',
+    );
+    return null;
+  }
+}
+
+async function submitMetrics() {
+  const gateway = getPushGateway();
+  if (!gateway) return;
+
+  const { resp, body } = await gateway.push({ jobName: 'relayer_funder' });
+  const statusCode =
+    typeof resp == 'object' && resp != null && 'statusCode' in resp
+      ? (resp as any).statusCode
+      : 'unknown';
+  console.log(
+    `Prometheus metrics pushed to PushGateway with status ${statusCode} and body ${body}`,
+  );
 }
 
 main()
