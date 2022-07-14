c7e49c83fab9261772d23c639d51072d8c7fd07c
J M Rossy <jm.rossy@gmail.com>
2022-07-14 13:14:09 -0400

    Remove TODO comment

--- typescript/infra/scripts/helloworld/utils.ts
+++ typescript/infra/scripts/helloworld/utils.ts
@@ -21,7 +21,6 @@ export async function getConfiguration<Chain extends ChainName>(
   environment: DeployEnvironment,
   multiProvider: MultiProvider<Chain>,
 ): Promise<ChainMap<Chain, RouterConfig>> {
-  // TODO rewrite without !
   const signerMap = await promiseObjAll(
     multiProvider.map(async (_, dc) => dc.signer!),
   );
