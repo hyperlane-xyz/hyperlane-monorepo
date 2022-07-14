73574afdaede7951f0aa48aebd038029e2648009
webbhorn <webb.horn@gmail.com>
2022-07-14 10:01:12 -0400

    Just use zero bytes instead of todo! to satisfy clippy etc.

--- rust/agents/relayer/src/msg/gelato_submitter.rs
+++ rust/agents/relayer/src/msg/gelato_submitter.rs
@@ -46,6 +46,7 @@ pub(crate) struct GelatoSubmitter {
     pub(crate) _metrics: GelatoSubmitterMetrics,
 }
 
+#[allow(clippy::too_many_arguments)]
 impl GelatoSubmitter {
     pub fn spawn(mut self) -> Instrumented<JoinHandle<Result<()>>> {
         tokio::spawn(async move { self.work_loop().await })
