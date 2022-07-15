7b89398ec409f5a9ec6c98bf13f8e9cac914e6ea
webbhorn <webb.horn@gmail.com>
2022-07-15 14:04:10 -0400

    Handful of pre-review cleanups:
    -  Only give GelatoSubmitter the inbox domain instead of the whole
       InboxContracts
    -  Don't wrap http: reqwest::Client in an Arc. It's unnecessary and
       handled internally for you, and better to clone() so that cxn pool
       reused.
    -  Shorter names from imports where noticed
    -  stub op.run() impl
    -  shorter submitter field names where appropriate
    -  dont pass GelatoConf to GelatoSubmitter
    -  get correct visibility in abigen'd build code (and allow no doc comments to satisfy clippy)

--- rust/agents/relayer/src/relayer.rs
+++ rust/agents/relayer/src/relayer.rs
@@ -13,7 +13,7 @@ use abacus_core::{
     AbacusCommon, AbacusContract, Inbox, InboxValidatorManager, MultisigSignedCheckpoint, Signers,
 };
 
-use abacus_ethereum::contracts::inbox_validator_manager::INBOXVALIDATORMANAGER_ABI;
+use abacus_ethereum::validator_manager::INBOXVALIDATORMANAGER_ABI;
 use ethers_contract::BaseContract;
 
 use crate::msg::gelato_submitter::{GelatoSubmitter, GelatoSubmitterMetrics};
--- rust/gelato/src/lib.rs
+++ rust/gelato/src/lib.rs
@@ -1,7 +1,6 @@
 pub mod chains;
 pub mod err;
 pub mod fwd_req_call;
-pub mod fwd_req_op;
 pub mod fwd_req_sig;
 pub mod task_status_call;
 
