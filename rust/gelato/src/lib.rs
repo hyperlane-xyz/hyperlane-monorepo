89495d8d198ad24ac7f0fa2a1e9e378417a18c5e
webbhorn <webb.horn@gmail.com>
2022-07-15 14:04:10 -0400

    Handful of pre-review cleanups:
    -  Only give GelatoSubmitter the inbox domain instead of the whole
       InboxContracts
    -  Don't wrap http: reqwest::Client in an Arc. It's unnecessary and
       handled internally for you, and better to clone() so that cxn pool
       reused.
    -  Shorter names from imports where noticed
    -  stub op.run() impl
    -  shorter submitter field names where appropriate
    -  dont pass GelatoConf to GelatoSubmitter
    -  get correct visibility in abigen'd build code (and allow no doc comments to satisfy clippy)

--- rust/gelato/src/lib.rs
+++ rust/gelato/src/lib.rs
@@ -1,8 +1,6 @@
-// TODO(webbhorn): pub mod probably wrong as blanket policy...
 pub mod chains;
 pub mod err;
 pub mod fwd_req_call;
-pub mod fwd_req_op;
 pub mod fwd_req_sig;
 pub mod task_status_call;
 
