e5e4dddbacfe1aea9bd808caa38aa952c3572046
webbhorn <webb.horn@gmail.com>
2022-07-19 16:10:50 -0400

    More natural iter map construction in Token byte packing for call_data on process.

--- rust/agents/relayer/src/msg/gelato_submitter.rs
+++ rust/agents/relayer/src/msg/gelato_submitter.rs
@@ -103,9 +103,7 @@ impl GelatoSubmitter {
                 ),
                 Token::Bytes(msg.committed_message.message.to_vec()),
                 Token::FixedArray(
-                    (0..32)
-                        .map(|i| Token::FixedBytes(msg.proof.path[i].to_vec()))
-                        .collect(),
+                    msg.proof.path[0..32].iter().map(|e| Token::FixedBytes(e.to_vec())).collect()
                 ),
                 Token::Uint(msg.leaf_index.into()),
             ],
@@ -155,15 +153,6 @@ fn abacus_domain_to_gelato_chain(domain: u32) -> Result<Chain> {
         _ => bail!("Unknown domain {}", domain),
     })
 }
-
-// TODO(webbhorn): Remove 'allow unused' once we impl run() and ref internal fields.
-#[allow(unused)]
-#[derive(Debug, Clone)]
-pub struct ForwardRequestOp<S> {
-    args: ForwardRequestArgs,
-    opts: ForwardRequestOptions,
-    signer: S,
-    http: reqwest::Client,
 }
 
 impl<S> ForwardRequestOp<S> {
