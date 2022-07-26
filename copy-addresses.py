ba0747942e2bd5ccaaaf7656655ec1b23c3c6e28
Yorke Rhodes <yorke@useabacus.network>
2022-07-26 18:38:22 -0400

    Use oneline code blocks

--- copy-addresses.py
+++ copy-addresses.py
@@ -8,15 +8,18 @@ data = json.load(f)
 def h(n, s):
     return '#' * n + ' ' + s[0].upper() + s[1:]
 
+def codeline(s):
+    return '`' + s + '`'
+
 def codeblock(s):
     return '```\n' + s + '\n```'
 
 for [network, struct] in data.items():
     print(h(1, network))
     print(h(2, 'Outbox'))
-    print(codeblock(struct['outbox']['proxy']))
+    print(codeline(struct['outbox']['proxy']))
     print(h(2, 'Inboxes'))
     for [inboxNetwork, inboxStruct] in struct['inboxes'].items():
         print(h(3, inboxNetwork))
-        print(codeblock(inboxStruct['inbox']['proxy']))
+        print(codeline(inboxStruct['inbox']['proxy']))
     # outbox = addresses.outbox
