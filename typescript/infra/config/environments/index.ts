22254610ffd95dbf3b8b910edc16f41a63ca77e1
Nam Chu Hoai <nambrot@googlemail.com>
2022-07-26 12:33:14 +0200

    Removes deprecated environments  (#677)
    * Remove dev and testnet environments from rust and sdk config
    
    * Remove testnet configration from infra
    
    * Remove reference to dev environment
    
    * Comment out for now

--- typescript/infra/config/environments/index.ts
+++ typescript/infra/config/environments/index.ts
@@ -1,13 +1,9 @@
-import { environment as dev } from './dev';
 import { environment as mainnet } from './mainnet';
 import { environment as test } from './test';
-import { environment as testnet } from './testnet';
 import { environment as testnet2 } from './testnet2';
 
 export const environments = {
-  dev,
   test,
-  testnet,
   testnet2,
   mainnet,
 };
