44df310300159257907630cf1ce03de9711d128f
J M Rossy <jm.rossy@gmail.com>
2022-07-28 10:59:27 -0400

    Support env subset deployments (#767)
    - Support env subset deployements in AbacusCore and TestCore
    - Harden MultiProvider against missing chain values
    - Add `tryGet*` methods to MultiProvider and other convenience methods
    - Add `getMultiProviderFromConfigAndProvider` and `getChainToOwnerMap` utils to SDK
    - Make `enrollRemoteRouters` static in `AbacusRouterDeployer` 
    - Create minimal envSubsetDeployer app in SDK to test subset chain deployments
    - Reduce boilerplate needed for HelloWorld app
    - Restructure HelloWorld check and deploy scripts to be more realistic
    - Add `pick` util function for objects
    - BREAKING CHANGE: Make order of generic types consistent across core classes
    - BREAKING CHANGE: Replace `getMultiProviderFromSignerAndConfig` with `getTestMultiProvider`

---
+++ typescript/sdk/src/test/envSubsetDeployer/check-single-chain.ts
@@ -0,0 +1,55 @@
+import { buildContracts } from '../../contracts';
+import { AbacusCore } from '../../core/AbacusCore';
+import { getChainToOwnerMap } from '../../deploy/utils';
+import { MultiProvider } from '../../providers/MultiProvider';
+import { RouterContracts } from '../../router';
+import { ChainMap, ChainName } from '../../types';
+
+import {
+  EnvSubsetApp,
+  EnvSubsetChecker,
+  alfajoresChainConfig,
+  envSubsetFactories,
+} from './app';
+import { getAlfajoresProvider } from './utils';
+
+// Copied from output of deploy-single-chain.ts script
+const deploymentAddresses = {
+  alfajores: {
+    router: '0x0666AD4F636210B6a418f97790b7BAABAC54b9A4',
+  },
+};
+
+const ownerAddress = '0x35b74Ed5038bf0488Ff33bD9819b9D12D10A7560';
+
+async function check() {
+  const provider = getAlfajoresProvider();
+
+  console.info('Preparing utilities');
+  const multiProvider = new MultiProvider({
+    alfajores: {
+      provider,
+      confirmations: alfajoresChainConfig.alfajores.confirmations,
+      overrides: alfajoresChainConfig.alfajores.overrides,
+    },
+  });
+
+  const contractsMap = buildContracts(
+    deploymentAddresses,
+    envSubsetFactories,
+  ) as ChainMap<ChainName, RouterContracts>;
+  const app = new EnvSubsetApp(contractsMap, multiProvider);
+  const core = AbacusCore.fromEnvironment('testnet2', multiProvider);
+  const config = core.extendWithConnectionClientConfig(
+    getChainToOwnerMap(alfajoresChainConfig, ownerAddress),
+  );
+  const envSubsetChecker = new EnvSubsetChecker(multiProvider, app, config);
+
+  console.info('Starting check');
+  await envSubsetChecker.check();
+  envSubsetChecker.expectEmpty();
+}
+
+check()
+  .then(() => console.info('Check complete'))
+  .catch(console.error);
