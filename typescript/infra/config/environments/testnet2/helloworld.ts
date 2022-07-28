afe04296bc4f52c568a70cb4e916c5cca9d96fa2
Mattie Conover <git@mconover.dev>
2022-07-28 13:56:27 -0400

    Kathy message retrying (#851)
    * kathy retry logic
    
    * retry config
    
    * fix typo

--- typescript/infra/config/environments/testnet2/helloworld.ts
+++ typescript/infra/config/environments/testnet2/helloworld.ts
@@ -15,5 +15,6 @@ export const helloWorld: HelloWorldConfig<TestnetChains> = {
     namespace: environment,
     fullCycleTime: 1000 * 60 * 60 * 2, // every 2 hours
     messageReceiptTimeout: 1000 * 60 * 15, // 15 min
+    maxSendRetries: 2,
   },
 };
--- typescript/infra/src/config/helloworld.ts
+++ typescript/infra/src/config/helloworld.ts
@@ -11,6 +11,8 @@ export interface HelloWorldKathyConfig<Chain extends ChainName> {
   fullCycleTime: number;
   /** How long kathy should wait before declaring an attempted to send a failure (milliseconds). */
   messageReceiptTimeout: number;
+  /** How many times to attempt retrying sending a message. 0 means only attempt 1 time and do not retry on failure. */
+  maxSendRetries: number;
 }
 
 export interface HelloWorldConfig<Chain extends ChainName> {
