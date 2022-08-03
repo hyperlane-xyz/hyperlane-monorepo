0860c4fe53569f32ab4114007a56dab8e00672c7
Trevor Porter <trkporter@ucdavis.edu>
2022-08-03 13:07:28 +0100

    Deploy testnet2

--- typescript/infra/config/environments/testnet2/funding.ts
+++ typescript/infra/config/environments/testnet2/funding.ts
@@ -6,7 +6,7 @@ import { environment } from './chains';
 export const relayerFunderConfig: RelayerFunderConfig = {
   docker: {
     repo: 'gcr.io/abacus-labs-dev/abacus-monorepo',
-    tag: 'sha-d24eaa4',
+    tag: 'sha-9328005',
   },
   cronSchedule: '45 * * * *', // Every hour at the 45 minute mark
   namespace: environment,
