b91134ccb68d5441a4e95242cd4ae3995adce7a2
Trevor Porter <trkporter@ucdavis.edu>
2022-08-05 18:25:23 +0100

    Deploy kathy / key funder without nonce manager (#890)
    * Deploy testnet2 kathy without nonce manager
    
    * Deploy testnet2 key funder
    
    * deploy on mainnet

--- typescript/infra/config/environments/testnet2/funding.ts
+++ typescript/infra/config/environments/testnet2/funding.ts
@@ -7,7 +7,7 @@ import { environment } from './chains';
 export const keyFunderConfig: KeyFunderConfig = {
   docker: {
     repo: 'gcr.io/abacus-labs-dev/abacus-monorepo',
-    tag: 'sha-51b594b',
+    tag: 'sha-936afa7',
   },
   cronSchedule: '45 * * * *', // Every hour at the 45 minute mark
   namespace: environment,
--- typescript/infra/config/environments/testnet2/helloworld.ts
+++ typescript/infra/config/environments/testnet2/helloworld.ts
@@ -8,7 +8,7 @@ export const helloWorld: HelloWorldConfig<TestnetChains> = {
   kathy: {
     docker: {
       repo: 'gcr.io/abacus-labs-dev/abacus-monorepo',
-      tag: 'sha-1f62484',
+      tag: 'sha-936afa7',
     },
     chainsToSkip: [],
     runEnv: environment,
