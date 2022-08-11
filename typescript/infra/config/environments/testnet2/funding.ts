0dc022b0fdb08a76f712cd2f0f23770435cd2939
Trevor Porter <trkporter@ucdavis.edu>
2022-08-11 11:30:06 +0100

    deploy testnet2 key funder

--- typescript/infra/config/environments/testnet2/funding.ts
+++ typescript/infra/config/environments/testnet2/funding.ts
@@ -7,7 +7,7 @@ import { environment } from './chains';
 export const keyFunderConfig: KeyFunderConfig = {
   docker: {
     repo: 'gcr.io/abacus-labs-dev/abacus-monorepo',
-    tag: 'sha-202125f',
+    tag: 'sha-b5a808c',
   },
   cronSchedule: '45 * * * *', // Every hour at the 45 minute mark
   namespace: environment,
