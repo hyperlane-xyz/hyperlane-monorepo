2f1f4476ec495ec5578acda04a033d4282cd6bb0
Trevor Porter <trkporter@ucdavis.edu>
2022-08-12 10:46:30 +0100

    Add a todo

--- rust/agents/relayer/src/msg/serial_submitter.rs
+++ rust/agents/relayer/src/msg/serial_submitter.rs
@@ -225,6 +225,9 @@ impl SerialSubmitter {
 
         // Go ahead and attempt processing of message to destination chain.
         debug!(msg=?msg, "Ready to process message");
+        // TODO: consider differentiating types of processing errors, and pushing to the front of the
+        // run queue for intermittent types of errors that can occur even if a message's processing isn't
+        // reverting, e.g. timeouts or txs being dropped from the mempool.
         match self.process_message(&msg).await {
             Ok(()) => {
                 info!(msg=?msg, "Message processed");
