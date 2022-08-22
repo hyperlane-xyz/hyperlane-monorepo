e203e963f7b6548197425c62b7bfa0e62a1033db
Trevor Porter <trkporter@ucdavis.edu>
2022-08-22 15:07:43 +0100

    use different public kovan rpc url (#965)

--- typescript/sdk/src/consts/chainConnectionConfigs.ts
+++ typescript/sdk/src/consts/chainConnectionConfigs.ts
@@ -101,7 +101,7 @@ export const goerli: IChainConnection = {
 
 export const kovan: IChainConnection = {
   provider: new ethers.providers.JsonRpcProvider(
-    'https://kovan.poa.network',
+    'https://kovan.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161',
     42,
   ),
   confirmations: 1,
