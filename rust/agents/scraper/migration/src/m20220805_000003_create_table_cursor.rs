25567cc5dc09cd584370cb6347477585b08d5f72
Mattie Conover <git@mconover.dev>
2022-08-23 16:21:01 -0400

    fix table cursor

--- rust/agents/scraper/migration/src/m20220805_000003_create_table_cursor.rs
+++ rust/agents/scraper/migration/src/m20220805_000003_create_table_cursor.rs
@@ -1,4 +1,5 @@
 use sea_orm_migration::prelude::*;
+use crate::m20220805_000001_create_table_domain::Domain;
 
 #[derive(DeriveMigrationName)]
 pub struct Migration;
@@ -12,13 +13,19 @@ impl MigrationTrait for Migration {
                     .table(Cursor::Table)
                     .if_not_exists()
                     .col(
-                        ColumnDef::new(Cursor::DomainId)
+                        ColumnDef::new(Cursor::Domain)
                             .unsigned()
                             .not_null()
                             .primary_key(),
                     )
                     .col(ColumnDef::new(Cursor::TimeUpdated).timestamp().not_null())
                     .col(ColumnDef::new(Cursor::Height).big_unsigned().not_null())
+                    .foreign_key(
+                        ForeignKey::create()
+                            .name("fk-domain")
+                            .from_col(Cursor::Domain)
+                            .to(Domain::Table, Domain::DomainId),
+                    )
                     .to_owned(),
             )
             .await
@@ -33,10 +40,10 @@ impl MigrationTrait for Migration {
 
 /// Learn more at https://docs.rs/sea-query#iden
 #[derive(Iden)]
-enum Cursor {
+pub enum Cursor {
     Table,
     /// Abacus domain ID the cursor is for
-    DomainId,
+    Domain,
     /// Time of the last record update
     TimeUpdated,
     /// Height of the last block read for finality
