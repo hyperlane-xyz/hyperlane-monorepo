e02472b1585c4c60095d14d9b5d6efae5e939d6e
Yorke Rhodes <yorke@useabacus.network>
2022-08-23 17:40:37 -0400

    Remove abicoder v2 annotation

--- solidity/core/contracts/validator-manager/MultisigValidatorManager.sol
+++ solidity/core/contracts/validator-manager/MultisigValidatorManager.sol
@@ -1,6 +1,5 @@
 // SPDX-License-Identifier: MIT OR Apache-2.0
 pragma solidity >=0.8.0;
-pragma abicoder v2;
 
 // ============ External Imports ============
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
