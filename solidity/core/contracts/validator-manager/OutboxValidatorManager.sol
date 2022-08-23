193b7052f8caf10a3df5dc39d66e55a0c6903cc8
Yorke Rhodes <yorke@useabacus.network>
2022-08-23 17:48:04 -0400

    Remove abicoder v2 annotation (#986)

--- solidity/core/contracts/validator-manager/OutboxValidatorManager.sol
+++ solidity/core/contracts/validator-manager/OutboxValidatorManager.sol
@@ -1,6 +1,5 @@
 // SPDX-License-Identifier: MIT OR Apache-2.0
 pragma solidity >=0.8.0;
-pragma abicoder v2;
 
 // ============ Internal Imports ============
 import {IOutbox} from "../../interfaces/IOutbox.sol";
