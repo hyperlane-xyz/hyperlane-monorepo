b2b28494e1805fc4ddce144418f5ab0a364f0925
Mattie Conover <git@mconover.dev>
2022-08-24 14:29:32 -0400

    cleanup

--- rust/agents/scraper/migration/bin/init_db.rs
+++ rust/agents/scraper/migration/bin/init_db.rs
@@ -7,23 +7,17 @@ const LOCAL_DATABASE_URL: &str = "postgresql://postgres:47221c18c610@localhost:5
 
 #[tokio::main]
 async fn main() -> Result<(), DbErr> {
+    #[cfg(all(feature = "tracing", feature = "tracing-subscriber"))]
     tracing_subscriber::fmt()
         .with_max_level(tracing::Level::DEBUG)
         .with_test_writer()
         .init();
 
-    let db =
-        Database::connect(env::var("DATABASE_URL").unwrap_or_else(|_| LOCAL_DATABASE_URL.into()))
-            .await?;
+    let url = env::var("DATABASE_URL").unwrap_or_else(|_| LOCAL_DATABASE_URL.into());
+    println!("Connecting to {url}");
+    let db = Database::connect(url).await?;
 
-    // Apply all pending migrations
-    // let sm = SchemaManager::new(&db);
-    // for m in Migrator::migrations().iter().rev() {
-    //     m.down(&sm).await?;
-    // }
-    Migrator::reset(&db).await?;
     Migrator::up(&db, None).await?;
-    // Migrator::up(&db, None).await?;
 
     Ok(())
 }
