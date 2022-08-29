5b06d0280cc584bfcb21f0d9d0abb79912fc85c1
Mattie Conover <git@mconover.dev>
2022-08-29 13:24:59 -0400

    Scraper DB generation and schema (#991)
    * Cleanup cargo config
    
    * BaseAgent trait
    
    * Pull `run_all` fn out of BaseAgent into static fn
    
    * Add `run` to `BaseAgent`
    
    * Scraper crate init
    
    * Create tables
    
    * WIP
    
    * data init
    
    * Init script
    
    * Fix
    
    * Attempting to run migration
    
    * fix ordering
    
    * fix domain creation
    
    * fix block table creation
    
    * fix checkpoint table
    
    * fix table cursor
    
    * fix table transaction + cleanup
    
    * enum type
    
    * progress
    
    * gas payments
    
    * message
    
    * delivered message
    
    * message state
    
    * cleanup
    
    * cleanup warnings
    
    * undo
    
    * Unique by domain and height
    
    * update index names
    
    * Change to OriginTxId
    
    * Add timestamp to messages
    
    * comment

---
+++ rust/agents/scraper/migration/bin/init_db.rs
@@ -0,0 +1,24 @@
+use std::env;
+
+use migration::sea_orm::Database;
+use migration::{DbErr, Migrator, MigratorTrait as _};
+
+const LOCAL_DATABASE_URL: &str = "postgresql://postgres:47221c18c610@localhost:5432";
+
+#[tokio::main]
+async fn main() -> Result<(), DbErr> {
+    #[cfg(all(feature = "tracing", feature = "tracing-subscriber"))]
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::DEBUG)
+        .with_test_writer()
+        .init();
+
+    let url = env::var("DATABASE_URL").unwrap_or_else(|_| LOCAL_DATABASE_URL.into());
+    println!("Connecting to {url}");
+    let db = Database::connect(url).await?;
+
+    // Migrator::down(&db, None).await?;
+    Migrator::up(&db, None).await?;
+
+    Ok(())
+}
