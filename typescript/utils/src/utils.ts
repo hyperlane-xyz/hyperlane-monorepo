bbc65ff7a483b08be28429bc89145e96f9f199da
Yorke Rhodes <yorke@useabacus.network>
2022-09-03 16:50:36 -0400

    Updates for enrolling new inboxes and unenrolling old inboxes (#1022)
    * General validator/inbox checker/govern un/enroll
    
    * Migrate infra govern to sync encode call
    
    * Skip core deploy when chain has remove config
    
    * Remove IGP deployment script

--- typescript/utils/src/utils.ts
+++ typescript/utils/src/utils.ts
@@ -196,3 +196,28 @@ export function safelyAccessEnvVar(name: string) {
     return undefined;
   }
 }
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set#implementing_basic_set_operations
+export function difference<T>(a: Set<T>, b: Set<T>) {
+  const _difference = new Set(a);
+  for (const elem of b) {
+    _difference.delete(elem);
+  }
+  return _difference;
+}
+
+export function symmetricDifference<T>(a: Set<T>, b: Set<T>) {
+  const _difference = new Set(a);
+  for (const elem of b) {
+    if (_difference.has(elem)) {
+      _difference.delete(elem);
+    } else {
+      _difference.add(elem);
+    }
+  }
+  return _difference;
+}
+
+export function setEquality<T>(a: Set<T>, b: Set<T>) {
+  return symmetricDifference(a, b).size === 0;
+}
