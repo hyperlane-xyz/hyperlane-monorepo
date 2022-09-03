1312672c47d29e2e9940e0fa527aeecba8d4b118
Yorke Rhodes <yorke@useabacus.network>
2022-09-03 17:06:52 -0400

    Fix inbox explorer links

--- copy-addresses.py
+++ copy-addresses.py
@@ -25,30 +25,33 @@ def table(headers, rows):
     return '| ' + ' | '.join(headers) + ' |\n' + '| ' + ' | '.join(['---'] * len(headers)) + ' |\n' + '\n'.join(['| ' + ' | '.join(row) + ' |' for row in rows])
 
 def address_url(network, address):
-    return url(codeline(address), explorers[network] + '/address/' + address)
+    return url(network + ':' + address[:6] + '...', explorers[network] + '/address/' + address)
 
 def any_table(addresses, key):
     header_label = h(2, key)
-    headers = ['Network', 'Address']
+    headers = ['Network', 'Address', 'Explorer']
     rows = []
     for [network, struct] in addresses.items():
         addressOrStruct = struct[key]
         address = addressOrStruct if isinstance(addressOrStruct, str) else addressOrStruct['proxy']
         rows.append([
             capitalize(network),
+            codeline(address),
             address_url(network, address)
         ])
     return header_label + '\n' + table(headers, rows)
 
 def inboxes_table(addresses):
-    headers = ['Network', 'Origin', 'Address']
+    headers = ['Network', 'Origin', 'Address', 'Explorer']
     rows = []
     for [network, struct] in addresses.items():
         for [inboxNetwork, inboxStruct] in struct['inboxes'].items():
+            address = inboxStruct['inbox']['proxy']
             rows.append([
                 capitalize(network),
                 capitalize(inboxNetwork),
-                address_url(inboxNetwork, inboxStruct['inbox']['proxy'])
+                codeline(address),
+                address_url(network, address)
             ])
     return h(2, 'Inboxes') + '\n' + table(headers, rows)
 
