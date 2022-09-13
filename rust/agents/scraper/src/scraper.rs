e3dc740ab7156cd5ce7cc4b5c0fc4bfb57f24645
Mattie Conover <git@mconover.dev>
2022-09-13 13:49:12 -0700

    WIP

--- rust/agents/scraper/src/scraper.rs
+++ rust/agents/scraper/src/scraper.rs
@@ -1,14 +1,23 @@
+use std::collections::HashMap;
+use std::ops::Deref;
+use std::sync::Arc;
+
 use async_trait::async_trait;
+use ethers::prelude::H256;
+use sea_orm::DbConn;
 use tokio::task::JoinHandle;
 use tracing::instrument::Instrumented;
 
 use abacus_base::BaseAgent;
+use abacus_core::{AbacusCommon, AbacusContract, ChainCommunicationError, Checkpoint, Message, Outbox, OutboxIndexer, OutboxState, TxOutcome};
 
 use crate::settings::ScraperSettings;
 
 /// A message explorer scraper agent
 #[derive(Debug)]
-pub struct Scraper {}
+pub struct Scraper {
+    outboxes: HashMap<String, SqlCachingOutbox>
+}
 
 #[async_trait]
 impl BaseAgent for Scraper {
@@ -19,11 +28,38 @@ impl BaseAgent for Scraper {
     where
         Self: Sized,
     {
-        todo!()
+        Ok(Self {
+            outboxes: todo!()
+        })
     }
 
     #[allow(clippy::async_yields_async)]
     async fn run(&self) -> Instrumented<JoinHandle<eyre::Result<()>>> {
-        todo!()
+        let tasks = self.outboxes();
+
+        run_all(tasks)
     }
+}
+
+impl Scraper {
+
+}
+
+#[derive(Debug, Clone)]
+struct SqlCachingOutbox {
+    db: DbConn,
+    outbox: Arc<dyn Outbox>,
+    indexer: Arc<dyn OutboxIndexer>,
+}
+
+impl Deref for SqlCachingOutbox {
+    type Target = dyn Outbox;
+
+    fn deref(&self) -> &Self::Target {
+        &self.outbox
+    }
+}
+
+impl SqlCachingOutbox {
+
 }
