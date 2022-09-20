c61d83ff1d365a6e1e23c5470a58de41a3b2f5ea
Trevor Porter <trkporter@ucdavis.edu>
2022-09-20 16:22:34 +0100

    Deploy testnet2 and mainnet agents, use Gelato where we can for all testnet2 contexts (#1094)
    * Deploy testnet2 and mainnet agents
    
    * Use Gelato on testnet2

--- typescript/infra/config/environments/mainnet/agent.ts
+++ typescript/infra/config/environments/mainnet/agent.ts
@@ -23,7 +23,7 @@ export const abacus: AgentConfig<MainnetChains> = {
   context: Contexts.Abacus,
   docker: {
     repo: 'gcr.io/abacus-labs-dev/abacus-agent',
-    tag: 'sha-58984b5',
+    tag: 'sha-7040e5c',
   },
   aws: {
     region: 'us-east-1',
@@ -80,7 +80,7 @@ export const releaseCandidate: AgentConfig<MainnetChains> = {
   context: Contexts.ReleaseCandidate,
   docker: {
     repo: 'gcr.io/abacus-labs-dev/abacus-agent',
-    tag: 'sha-58984b5',
+    tag: 'sha-7040e5c',
   },
   aws: {
     region: 'us-east-1',
