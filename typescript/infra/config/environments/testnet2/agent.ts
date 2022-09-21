7cda83c30843af70e41df18308be78f28931f4dd
Trevor Porter <trkporter@ucdavis.edu>
2022-09-21 11:34:39 +0100

    Deploy, use Gelato for mainnet rc

--- typescript/infra/config/environments/testnet2/agent.ts
+++ typescript/infra/config/environments/testnet2/agent.ts
@@ -26,7 +26,7 @@ export const abacus: AgentConfig<TestnetChains> = {
   context: Contexts.Abacus,
   docker: {
     repo: 'gcr.io/abacus-labs-dev/abacus-agent',
-    tag: 'sha-7040e5c',
+    tag: 'sha-0d76398',
   },
   aws: {
     region: 'us-east-1',
@@ -92,7 +92,7 @@ export const flowcarbon: AgentConfig<TestnetChains> = {
   context: Contexts.Flowcarbon,
   docker: {
     repo: 'gcr.io/abacus-labs-dev/abacus-agent',
-    tag: 'sha-7040e5c',
+    tag: 'sha-0d76398',
   },
   aws: {
     region: 'us-east-1',
@@ -124,7 +124,7 @@ export const releaseCandidate: AgentConfig<TestnetChains> = {
   context: Contexts.ReleaseCandidate,
   docker: {
     repo: 'gcr.io/abacus-labs-dev/abacus-agent',
-    tag: 'sha-7040e5c',
+    tag: 'sha-0d76398',
   },
   aws: {
     region: 'us-east-1',
