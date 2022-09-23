dcc84eac97832006de3a5c60d314809ee90d103c
Trevor Porter <trkporter@ucdavis.edu>
2022-09-23 15:39:14 +0100

    Support QuorumProvider (i.e. FallbackProvider) in kathy and key funder (#1111)
    * Support QuorumProvider (i.e. FallbackProvider) in kathy and key funder
    
    * Testing deploying

--- typescript/infra/config/environments/testnet2/funding.ts
+++ typescript/infra/config/environments/testnet2/funding.ts
@@ -1,4 +1,5 @@
 import { KEY_ROLE_ENUM } from '../../../src/agents/roles';
+import { ConnectionType } from '../../../src/config/agent';
 import { KeyFunderConfig } from '../../../src/config/funding';
 import { Contexts } from '../../contexts';
 
@@ -7,7 +8,7 @@ import { environment } from './chains';
 export const keyFunderConfig: KeyFunderConfig = {
   docker: {
     repo: 'gcr.io/abacus-labs-dev/hyperlane-monorepo',
-    tag: 'sha-0d76398',
+    tag: 'sha-da9bc61',
   },
   cronSchedule: '45 * * * *', // Every hour at the 45 minute mark
   namespace: environment,
@@ -18,4 +19,5 @@ export const keyFunderConfig: KeyFunderConfig = {
     [Contexts.Abacus]: [KEY_ROLE_ENUM.Relayer, KEY_ROLE_ENUM.Kathy],
     [Contexts.ReleaseCandidate]: [KEY_ROLE_ENUM.Relayer, KEY_ROLE_ENUM.Kathy],
   },
+  connectionType: ConnectionType.HttpQuorum,
 };
