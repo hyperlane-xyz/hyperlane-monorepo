a2909d67015a4e33f2ad477e1f77a490513ebdb4
Yorke Rhodes <yorke@useabacus.network>
2022-09-26 16:10:20 -0400

    Deploy interchain accounts middleware (#1059)
    * Push initial interchain accounts package
    
    * Deploy ICAs to testnet2
    
    * Deploy ICAs to mainnet
    
    * Simplify deploy script
    
    Co-authored-by: Nam Chu Hoai <nambrot@googlemail.com>

---
+++ typescript/ica/index.ts
@@ -0,0 +1,10 @@
+export {
+  InterchainAccountConfig,
+  InterchainAccountDeployer,
+} from './src/deploy';
+export {
+  InterchainAccountContracts,
+  InterchainAccountFactories,
+  interchainAccountFactories,
+} from './src/contracts';
+export * as types from './types';
