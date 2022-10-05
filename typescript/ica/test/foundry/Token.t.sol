1ba24aaa9587b62da01b005be63108d7d663e9d3
nambrot <nambrot@googlemail.com>
2022-10-05 19:26:47 -0400

    Add foundry to ica package

---
+++ typescript/ica/test/foundry/Token.t.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+
+contract TokenTest is Test {
+    function setUp() public {}
+
+    function testName() public {
+        assertEq(true, true);
+    }
+}
