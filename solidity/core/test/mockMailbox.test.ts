9a193e503d17da8fa4d9806a615412e3c944877e
nambrot <nambrot@googlemail.com>
2022-10-06 15:38:57 -0400

    Domain support in Mailboxes

--- solidity/core/test/mockMailbox.test.ts
+++ solidity/core/test/mockMailbox.test.ts
@@ -19,7 +19,6 @@ describe('Mock mailbox contracts', function () {
 
     const data = ethers.utils.toUtf8Bytes('This is a test message');
 
-    // Mailbox mocks do not handle domains currently, so this is arbitrary
     await outbox.dispatch(0, utils.addressToBytes32(recipient.address), data);
     await inbox.processNextPendingMessage();
 
