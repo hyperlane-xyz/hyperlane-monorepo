de3aadc6c85973c7f8bc2f279e24d33d6899582a
Yorke Rhodes <email@yorke.dev>
2022-10-07 12:42:51 -0500

    Add deploy script to infra

--- typescript/ica/index.ts
+++ typescript/ica/index.ts
@@ -7,4 +7,10 @@ export {
   InterchainAccountFactories,
   interchainAccountFactories,
 } from './src/contracts';
+export { InterchainQueryConfig, InterchainQueryDeployer } from './src/deploy';
+export {
+  InterchainQueryContracts,
+  InterchainQueryFactories,
+  interchainQueryFactories,
+} from './src/contracts';
 export * as types from './types';
---
+++ typescript/infra/scripts/interchain/deploy-queries.ts
@@ -0,0 +1,40 @@
+import path from 'path';
+
+import {
+  InterchainQueryDeployer,
+  interchainQueryFactories,
+} from '@hyperlane-xyz/interchain-accounts';
+import { HyperlaneCore } from '@hyperlane-xyz/sdk';
+
+import { deployWithArtifacts } from '../../src/deploy';
+import { getConfiguration } from '../helloworld/utils';
+import {
+  getCoreEnvironmentConfig,
+  getEnvironment,
+  getEnvironmentDirectory,
+} from '../utils';
+
+// similar to hello world deploy script but uses freshly funded account for consistent addresses across chains
+// should eventually be deduped
+async function main() {
+  const environment = await getEnvironment();
+  const coreConfig = getCoreEnvironmentConfig(environment);
+  const multiProvider = await coreConfig.getMultiProvider();
+  const core = HyperlaneCore.fromEnvironment(environment, multiProvider as any);
+
+  const dir = path.join(
+    getEnvironmentDirectory(environment),
+    'interchain/queries',
+  );
+
+  // config gcp deployer key as owner
+  const configMap = await getConfiguration(environment, multiProvider);
+
+  const deployer = new InterchainQueryDeployer(multiProvider, configMap, core);
+
+  await deployWithArtifacts(dir, interchainQueryFactories, deployer);
+}
+
+main()
+  .then(() => console.info('Deployment complete'))
+  .catch(console.error);
