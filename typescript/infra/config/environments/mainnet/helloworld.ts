77f4f3c3ff6e9e3016e9015621560d8702dd1756
nambrot <nambrot@googlemail.com>
2022-10-15 22:38:26 -0400

    Deploy workloads to moonbeam

--- typescript/infra/config/environments/mainnet/helloworld.ts
+++ typescript/infra/config/environments/mainnet/helloworld.ts
@@ -12,7 +12,7 @@ export const abacus: HelloWorldConfig<MainnetChains> = {
   kathy: {
     docker: {
       repo: 'gcr.io/abacus-labs-dev/hyperlane-monorepo',
-      tag: 'sha-dcc84ea',
+      tag: 'sha-a290b07',
     },
     chainsToSkip: [],
     runEnv: environment,
@@ -32,7 +32,7 @@ export const releaseCandidate: HelloWorldConfig<MainnetChains> = {
   kathy: {
     docker: {
       repo: 'gcr.io/abacus-labs-dev/hyperlane-monorepo',
-      tag: 'sha-dcc84ea',
+      tag: 'sha-a290b07',
     },
     chainsToSkip: [],
     runEnv: environment,
