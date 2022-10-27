a08eb2068e116b2fc0521af559cfade16cfdd0c1
Mattie Conover <git@mconover.dev>
2022-10-27 16:16:33 -0700

    WIP attempt to create different make provider with connection

--- rust/chains/abacus-ethereum/src/trait_builder.rs
+++ rust/chains/abacus-ethereum/src/trait_builder.rs
@@ -9,7 +9,7 @@ use ethers::prelude::{
 };
 use reqwest::{Client, Url};
 
-use abacus_core::{ContractLocator, Signers};
+use abacus_core::{AbacusProvider, ContractLocator, Signers};
 use ethers_prometheus::json_rpc_client::{
     JsonRpcClientMetrics, JsonRpcClientMetricsBuilder, NodeInfo, PrometheusJsonRpcClient,
     PrometheusJsonRpcClientConfig,
@@ -18,11 +18,119 @@ use ethers_prometheus::middleware::{
     MiddlewareMetrics, PrometheusMiddleware, PrometheusMiddlewareConf,
 };
 
-use crate::{Connection, RetryingProvider};
+use crate::{Connection, EthereumProvider, RetryingProvider};
 
 // This should be whatever the prometheus scrape interval is
 const METRICS_SCRAPE_INTERVAL: Duration = Duration::from_secs(60);
 const HTTP_CLIENT_TIMEOUT: Duration = Duration::from_secs(60);
+
+pub async fn make_provider_with_connection(
+    conn: Connection,
+    rpc_metrics: Option<impl FnOnce() -> JsonRpcClientMetrics + Send>,
+    middleware_metrics: Option<(MiddlewareMetrics, PrometheusMiddlewareConf)>,
+) -> eyre::Result<Box<dyn AbacusProvider>> {
+    Ok(match conn {
+        Connection::HttpQuorum { urls } => {
+            let rpc_metrics = rpc_metrics.map(|m| m());
+            let quorum_provider = build_quorum_provider(&urls, &rpc_metrics, &middleware_metrics)?;
+            wrap_provider_with_metrics(quorum_provider, middleware_metrics).await?
+        }
+        Connection::Http { url } => {
+            let http_client = Client::builder().timeout(HTTP_CLIENT_TIMEOUT).build()?;
+            let http_provider = Http::new_with_client(url.parse::<Url>()?, http_client);
+            let retrying_http_provider: RetryingProvider<Http> =
+                RetryingProvider::new(http_provider, None, None);
+            wrap_provider_with_metrics(retrying_http_provider, middleware_metrics).await?
+        }
+        Connection::Ws { url } => {
+            let ws = Ws::connect(url).await?;
+            wrap_provider_with_metrics(ws, middleware_metrics).await?
+        }
+    })
+}
+
+fn build_quorum_provider(
+    urls: &str,
+    rpc_metrics: &Option<JsonRpcClientMetrics>,
+    middleware_metrics: &Option<(MiddlewareMetrics, PrometheusMiddlewareConf)>,
+) -> eyre::Result<QuorumProvider<PrometheusJsonRpcClient<RetryingProvider<Http>>>> {
+    let mut builder = QuorumProvider::builder().quorum(Quorum::Majority);
+    let http_client = Client::builder().timeout(HTTP_CLIENT_TIMEOUT).build()?;
+    for url in urls.split(',') {
+        let http_provider = Http::new_with_client(url.parse::<Url>()?, http_client.clone());
+        // Wrap the inner providers as RetryingProviders rather than the QuorumProvider.
+        // We've observed issues where the QuorumProvider will first get the latest
+        // block number and then submit an RPC at that block height,
+        // sometimes resulting in the second RPC getting serviced by a
+        // node that isn't aware of the requested block height yet.
+        // Retrying at the QuorumProvider level will result in both those RPCs
+        // being retried, while retrying at the inner provider level will result in only
+        // the second RPC being retried (the one with the error), which
+        // is the desired behavior.
+        let retrying_provider = RetryingProvider::new(http_provider, Some(5), Some(1000));
+        let metrics_provider = wrap_rpc_provider_with_metrics(
+            retrying_provider,
+            Url::parse(url)?,
+            rpc_metrics,
+            middleware_metrics,
+        );
+        let weighted_provider = WeightedProvider::new(metrics_provider);
+        builder = builder.add_provider(weighted_provider);
+    }
+    Ok(builder.build())
+}
+
+/// Wrap a JsonRpcClient with metrics for use with a quorum provider.
+fn wrap_rpc_provider_with_metrics<C>(
+    client: C,
+    url: Url,
+    rpc_metrics: &Option<JsonRpcClientMetrics>,
+    middleware_metrics: &Option<(MiddlewareMetrics, PrometheusMiddlewareConf)>,
+) -> PrometheusJsonRpcClient<RetryingProvider<Http>> {
+    PrometheusJsonRpcClient::new(
+        client,
+        rpc_metrics
+            .clone()
+            .unwrap_or_else(|| JsonRpcClientMetricsBuilder::default().build().unwrap()),
+        PrometheusJsonRpcClientConfig {
+            node: Some(NodeInfo {
+                host: {
+                    let mut s = String::new();
+                    if let Some(host) = url.host_str() {
+                        s.push_str(host);
+                        if let Some(port) = url.port() {
+                            write!(&mut s, ":{port}").unwrap();
+                        }
+                        Some(s)
+                    } else {
+                        None
+                    }
+                },
+            }),
+            // steal the chain info from the middleware conf
+            chain: middleware_metrics
+                .as_ref()
+                .and_then(|(_, v)| v.chain.clone()),
+        },
+    )
+}
+
+async fn wrap_provider_with_metrics<P>(
+    client: P,
+    metrics: Option<(MiddlewareMetrics, PrometheusMiddlewareConf)>,
+) -> eyre::Result<Box<dyn AbacusProvider>>
+where
+    P: JsonRpcClient + 'static,
+{
+    let provider = Provider::new(client);
+    Ok(if let Some(metrics) = metrics {
+        let provider = Arc::new(PrometheusMiddleware::new(provider, metrics.0, metrics.1));
+        tokio::spawn(provider.start_updating_on_interval(METRICS_SCRAPE_INTERVAL));
+        Box::new(EthereumProvider::from(provider))
+    } else {
+        Box::new(EthereumProvider::from(provider))
+    })
+}
 
 /// A trait for dynamic trait creation with provider initialization.
 #[async_trait]
@@ -44,31 +152,10 @@ pub trait MakeableWithProvider {
         Ok(match conn {
             Connection::HttpQuorum { urls } => {
                 let rpc_metrics = rpc_metrics.map(|f| f());
-                let mut builder = QuorumProvider::builder().quorum(Quorum::Majority);
-                let http_client = Client::builder().timeout(HTTP_CLIENT_TIMEOUT).build()?;
-                for url in urls.split(',') {
-                    let http_provider =
-                        Http::new_with_client(url.parse::<Url>()?, http_client.clone());
-                    // Wrap the inner providers as RetryingProviders rather than the QuorumProvider.
-                    // We've observed issues where the QuorumProvider will first get the latest block
-                    // number and then submit an RPC at that block height, sometimes resulting in the
-                    // second RPC getting serviced by a node that isn't aware of the requested block
-                    // height yet. Retrying at the QuorumProvider level will result in both those RPCs
-                    // being retried, while retrying at the inner provider level will result in only the
-                    // second RPC being retried (the one with the error), which is the desired behavior.
-                    let retrying_provider =
-                        RetryingProvider::new(http_provider, Some(5), Some(1000));
-                    let metrics_provider = self.wrap_rpc_with_metrics(
-                        retrying_provider,
-                        Url::parse(url)?,
-                        &rpc_metrics,
-                        &middleware_metrics,
-                    );
-                    let weighted_provider = WeightedProvider::new(metrics_provider);
-                    builder = builder.add_provider(weighted_provider);
-                }
-                let quorum_provider = builder.build();
-                self.wrap_with_metrics(quorum_provider, locator, signer, middleware_metrics)
+                let quorum_provider =
+                    build_quorum_provider(&urls, &rpc_metrics, &middleware_metrics)?;
+                // TODO: should we still wrap this with metrics as well as the inner providers?
+                self.wrap_with_signer(quorum_provider, locator, signer)
                     .await?
             }
             Connection::Http { url } => {
@@ -85,42 +172,6 @@ pub trait MakeableWithProvider {
                     .await?
             }
         })
-    }
-
-    /// Wrap a JsonRpcClient with metrics for use with a quorum provider.
-    fn wrap_rpc_with_metrics<C>(
-        &self,
-        client: C,
-        url: Url,
-        rpc_metrics: &Option<JsonRpcClientMetrics>,
-        middleware_metrics: &Option<(MiddlewareMetrics, PrometheusMiddlewareConf)>,
-    ) -> PrometheusJsonRpcClient<C> {
-        PrometheusJsonRpcClient::new(
-            client,
-            rpc_metrics
-                .clone()
-                .unwrap_or_else(|| JsonRpcClientMetricsBuilder::default().build().unwrap()),
-            PrometheusJsonRpcClientConfig {
-                node: Some(NodeInfo {
-                    host: {
-                        let mut s = String::new();
-                        if let Some(host) = url.host_str() {
-                            s.push_str(host);
-                            if let Some(port) = url.port() {
-                                write!(&mut s, ":{port}").unwrap();
-                            }
-                            Some(s)
-                        } else {
-                            None
-                        }
-                    },
-                }),
-                // steal the chain info from the middleware conf
-                chain: middleware_metrics
-                    .as_ref()
-                    .and_then(|(_, v)| v.chain.clone()),
-            },
-        )
     }
 
     /// Wrap the provider creation with metrics if provided; this is the second
