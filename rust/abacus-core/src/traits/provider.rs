223f3121ae479a944c418b29083ee6718f403389
Mattie Conover <git@mconover.dev>
2022-10-27 13:17:49 -0700

    WIP

--- rust/abacus-core/src/traits/provider.rs
+++ rust/abacus-core/src/traits/provider.rs
@@ -1,6 +1,22 @@
 use std::fmt::Debug;
+use async_trait::async_trait;
+use ethers::prelude::{H256, U256};
+use eyre::Result;
 
 use crate::AbacusChain;
+
+struct BlockInfo {
+    timestamp: u64,
+
+    gas_price: U256,
+}
+
+pub struct TxnInfo {
+    /// Amount of gas which was used by this transaction
+    gas_used: U256,
+    /// Address of the person who sent this transaction
+    sender: H256,
+}
 
 /// Interface for a provider. Allows abstraction over different provider types
 /// for different chains.
@@ -9,4 +25,9 @@ use crate::AbacusChain;
 /// as the wrappers provided by ethers for given contracts are quite nice,
 /// however, there are some generic calls that we should be able to make outside
 /// the context of a contract.
-pub trait Provider: AbacusChain + Send + Sync + Debug {}
+#[async_trait]
+pub trait Provider: AbacusChain + Send + Sync + Debug {
+    async fn get_transaction_by_hash(hash: &H256) -> Result<TxnInfo>;
+
+    async fn get_block_by_hash(block: &H256) -> Result<BlockInfo>;
+}
