509aa6ebc5a59eff4a0d4c8ef980b0834b607383
Trevor Porter <trkporter@ucdavis.edu>
2022-11-11 15:59:54 +0000

    Deploy kathy and key funder

--- typescript/infra/config/environments/mainnet/helloworld.ts
+++ typescript/infra/config/environments/mainnet/helloworld.ts
@@ -12,7 +12,7 @@ export const abacus: HelloWorldConfig<MainnetChains> = {
   kathy: {
     docker: {
       repo: 'gcr.io/abacus-labs-dev/hyperlane-monorepo',
-      tag: 'sha-7956ff0',
+      tag: 'sha-937fd14',
     },
     chainsToSkip: [],
     runEnv: environment,
@@ -33,7 +33,7 @@ export const releaseCandidate: HelloWorldConfig<MainnetChains> = {
   kathy: {
     docker: {
       repo: 'gcr.io/abacus-labs-dev/hyperlane-monorepo',
-      tag: 'sha-7956ff0',
+      tag: 'sha-937fd14',
     },
     chainsToSkip: [],
     runEnv: environment,
