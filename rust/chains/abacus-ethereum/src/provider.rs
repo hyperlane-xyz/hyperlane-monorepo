09ae6d2601542d3417aa647c49ab01d19fe93df3
Mattie Conover <git@mconover.dev>
2022-11-17 16:05:53 -0800

    Make provider queries more resilient

--- rust/chains/abacus-ethereum/src/provider.rs
+++ rust/chains/abacus-ethereum/src/provider.rs
@@ -1,14 +1,18 @@
-use async_trait::async_trait;
 use std::fmt::Debug;
+use std::future::Future;
 use std::sync::Arc;
+use std::time::Duration;
 
+use async_trait::async_trait;
 use ethers::prelude::{Middleware, H256};
 use eyre::eyre;
+use tokio::time::sleep;
 
-use crate::MakeableWithProvider;
 use abacus_core::{
     AbacusChain, AbacusProvider, BlockInfo, ContractLocator, TxnInfo, TxnReceiptInfo,
 };
+
+use crate::MakeableWithProvider;
 
 /// Connection to an ethereum provider. Useful for querying information about
 /// the blockchain.
@@ -41,27 +45,19 @@ where
     M: Middleware + 'static,
 {
     async fn get_block_by_hash(&self, hash: &H256) -> eyre::Result<BlockInfo> {
-        let block = self
-            .provider
-            .get_block(*hash)
-            .await?
-            .ok_or_else(|| eyre!("Could not find block with hash {}", hash))?;
-        Ok(BlockInfo {
+        let block = get_with_retry_on_none(|| self.provider.get_block(*hash)).await?;
+        return Ok(BlockInfo {
             hash: *hash,
             timestamp: block.timestamp.as_u64(),
             number: block
                 .number
                 .ok_or_else(|| eyre!("Block is not part of the chain yet {}", hash))?
                 .as_u64(),
-        })
+        });
     }
 
     async fn get_txn_by_hash(&self, hash: &H256) -> eyre::Result<TxnInfo> {
-        let txn = self
-            .provider
-            .get_transaction(*hash)
-            .await?
-            .ok_or_else(|| eyre!("Could not find txn with hash {}", hash))?;
+        let txn = get_with_retry_on_none(|| self.provider.get_transaction(*hash)).await?;
         let receipt = self
             .provider
             .get_transaction_receipt(*hash)
@@ -109,4 +105,26 @@ impl MakeableWithProvider for AbacusProviderBuilder {
             domain: locator.domain,
         })
     }
+}
+
+/// Call a get function that returns a Result<Option<T>> and retry if the inner
+/// option is None. This can happen because the provider has not discovered the
+/// object we are looking for yet.
+async fn get_with_retry_on_none<T, F, O, E>(get: F) -> eyre::Result<T>
+where
+    F: Fn() -> O,
+    O: Future<Output = Result<Option<T>, E>>,
+    E: std::error::Error + Send + Sync + 'static,
+{
+    for i in 0..3 {
+        if i > 0 {
+            sleep(Duration::from_secs(5)).await;
+        }
+        if let Some(t) = get().await? {
+            return Ok(t);
+        } else {
+            continue;
+        };
+    }
+    Err(eyre!("Could not find object from provider"))
 }
