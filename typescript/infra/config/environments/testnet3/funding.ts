bd575dee597d0390e91e22b2dcd145f5c520e84f
Asa Oines <asaoines@gmail.com>
2022-11-28 15:42:32 -0500

    Merge testnet3 config

---
+++ typescript/infra/config/environments/testnet3/funding.ts
@@ -0,0 +1,23 @@
+import { KEY_ROLE_ENUM } from '../../../src/agents/roles';
+import { ConnectionType } from '../../../src/config/agent';
+import { KeyFunderConfig } from '../../../src/config/funding';
+import { Contexts } from '../../contexts';
+
+import { environment } from './chains';
+
+export const keyFunderConfig: KeyFunderConfig = {
+  docker: {
+    repo: 'gcr.io/hyperlane-labs-dev/hyperlane-monorepo',
+    tag: 'sha-7956ff0',
+  },
+  cronSchedule: '45 * * * *', // Every hour at the 45 minute mark
+  namespace: environment,
+  prometheusPushGateway:
+    'http://prometheus-pushgateway.monitoring.svc.cluster.local:9091',
+  contextFundingFrom: Contexts.Hyperlane,
+  contextsAndRolesToFund: {
+    [Contexts.Hyperlane]: [KEY_ROLE_ENUM.Relayer, KEY_ROLE_ENUM.Kathy],
+    [Contexts.ReleaseCandidate]: [KEY_ROLE_ENUM.Relayer, KEY_ROLE_ENUM.Kathy],
+  },
+  connectionType: ConnectionType.Http,
+};
---
+++ typescript/infra/config/environments/testnet3/index.ts
@@ -0,0 +1,39 @@
+import { getMultiProviderForRole } from '../../../scripts/utils';
+import { KEY_ROLE_ENUM } from '../../../src/agents/roles';
+import { CoreEnvironmentConfig } from '../../../src/config';
+import { ConnectionType } from '../../../src/config/agent';
+import { Contexts } from '../../contexts';
+
+import { agents } from './agent';
+import {
+  TestnetChains,
+  environment as environmentName,
+  testnetConfigs,
+} from './chains';
+import { core } from './core';
+import { keyFunderConfig } from './funding';
+// import { helloWorld } from './helloworld';
+import { infrastructure } from './infrastructure';
+
+export const environment: CoreEnvironmentConfig<TestnetChains> = {
+  environment: environmentName,
+  transactionConfigs: testnetConfigs,
+  getMultiProvider: (
+    context: Contexts = Contexts.Hyperlane,
+    role: KEY_ROLE_ENUM = KEY_ROLE_ENUM.Deployer,
+    connectionType?: ConnectionType,
+  ) =>
+    getMultiProviderForRole(
+      testnetConfigs,
+      environmentName,
+      context,
+      role,
+      undefined,
+      connectionType,
+    ),
+  agents,
+  core,
+  infra: infrastructure,
+  // helloWorld,
+  keyFunderConfig,
+};
