49b8c61a4162aff19aa5b0f8e7d8c5050cf0213e
Nam Chu Hoai <nambrot@googlemail.com>
2022-12-01 20:20:54 +0530

    Reduce Kathy Polling in V2 (#1357)

--- typescript/sdk/src/core/HyperlaneCore.ts
+++ typescript/sdk/src/core/HyperlaneCore.ts
@@ -133,6 +133,20 @@ export class HyperlaneCore<
     });
   }
 
+  protected async waitForMessageWasProcessed(
+    message: DispatchedMessage,
+  ): Promise<void> {
+    const id = utils.messageId(message.message);
+    const { mailbox } = this.getDestination(message);
+    await utils.pollAsync(async () => {
+      const delivered = await mailbox.delivered(id);
+      if (!delivered) {
+        throw new Error(`Message ${id} not yet processed`);
+      }
+    });
+    return;
+  }
+
   getDispatchedMessages(sourceTx: ethers.ContractReceipt): DispatchedMessage[] {
     const mailbox = Mailbox__factory.createInterface();
     const dispatchLogs = sourceTx.logs
@@ -160,5 +174,14 @@ export class HyperlaneCore<
   ): Promise<ethers.ContractReceipt[]> {
     const messages = this.getDispatchedMessages(sourceTx);
     return Promise.all(messages.map((msg) => this.waitForProcessReceipt(msg)));
+  }
+
+  async waitForMessageProcessed(
+    sourceTx: ethers.ContractReceipt,
+  ): Promise<void> {
+    const messages = this.getDispatchedMessages(sourceTx);
+    await Promise.all(
+      messages.map((msg) => this.waitForMessageWasProcessed(msg)),
+    );
   }
 }
