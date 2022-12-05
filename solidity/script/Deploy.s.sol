5fc6819bdcdccc38a7145e53d0d82a255d7d0994
Yorke Rhodes <email@yorke.dev>
2022-12-05 14:43:18 -0500

    Get mailbox deployment working

--- solidity/script/Deploy.s.sol
+++ solidity/script/Deploy.s.sol
@@ -5,10 +5,12 @@ import "forge-std/Script.sol";
 import {ICREATE3Factory} from "../lib/create3-factory/src/ICREATE3Factory.sol";
 
 import {Mailbox} from "../contracts/Mailbox.sol";
+import {TypeCasts} from "../contracts/libs/TypeCasts.sol";
 
 contract Deploy is Script {
     function getCreate3Factory(string memory network)
         public
+        view
         returns (ICREATE3Factory)
     {
         string memory file = string.concat(
@@ -19,31 +21,32 @@ contract Deploy is Script {
         string memory json = vm.readFile(file);
         return
             ICREATE3Factory(
-                address(bytes20(vm.parseJson(json, "CREATE3Factory")))
+                abi.decode(vm.parseJson(json, "CREATE3Factory"), (address))
             );
     }
 
-    function getDomainId(string memory network) public returns (uint32) {
-        string memory json = vm.readFile("../chains.json");
+    function getDomainId(string memory network) public view returns (uint32) {
+        string memory json = vm.readFile("chains.json");
         return
-            uint32(
-                bytes4(vm.parseJson(string(vm.parseJson(json, network)), "id"))
+            abi.decode(
+                vm.parseJson(json, string.concat(network, ".id")),
+                (uint32)
             );
     }
 
     function run() public {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+
         string memory network = vm.envString("NETWORK");
         // string memory salt = vm.envString("SALT");
         bytes32 salt = keccak256("SALT");
 
         uint32 domain = getDomainId(network);
         ICREATE3Factory factory = getCreate3Factory(network);
-        Mailbox mailbox = Mailbox(
-            factory.deploy(
-                salt,
-                abi.encodePacked(type(Mailbox).creationCode, domain)
-            )
+        factory.deploy(
+            salt,
+            abi.encodePacked(type(Mailbox).creationCode, abi.encode(domain))
         );
     }
 }
