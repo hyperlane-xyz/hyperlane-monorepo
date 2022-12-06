84996241522f7efb5b86e3fee43e4dbee10bc14e
Asa Oines <asaoines@gmail.com>
2022-12-06 10:48:46 -0500

    Attempt to fix e2e

--- typescript/sdk/src/core/TestCoreDeployer.ts
+++ typescript/sdk/src/core/TestCoreDeployer.ts
@@ -3,20 +3,12 @@ import { ethers } from 'ethers';
 import {
   MultisigIsm,
   TestIsm__factory,
-  TestMailbox,
   TestMailbox__factory,
 } from '@hyperlane-xyz/core';
-import { types } from '@hyperlane-xyz/utils';
 
-import { chainMetadata } from '../consts/chainMetadata';
 import { HyperlaneCoreDeployer } from '../deploy/core/HyperlaneCoreDeployer';
 import { CoreConfig } from '../deploy/core/types';
 import { MultiProvider } from '../providers/MultiProvider';
-import {
-  ProxiedContract,
-  ProxyKind,
-  TransparentProxyAddresses,
-} from '../proxy';
 import { ChainMap, TestChainNames } from '../types';
 
 import { TestCoreApp } from './TestCoreApp';
@@ -69,22 +61,6 @@ export class TestCoreDeployer<
     );
     await testIsm.setAccept(true);
     return testIsm as unknown as MultisigIsm;
-  }
-
-  // skip proxying
-  async deployMailbox<LocalChain extends TestChain>(
-    chain: LocalChain,
-    defaultIsmAddress: types.Address,
-  ): Promise<ProxiedContract<TestMailbox, TransparentProxyAddresses>> {
-    const localDomain = chainMetadata[chain].id;
-
-    const mailbox = await this.deployContract(chain, 'mailbox', [localDomain]);
-    await mailbox.initialize(defaultIsmAddress);
-    return new ProxiedContract(mailbox, {
-      kind: ProxyKind.Transparent,
-      proxy: mailbox.address,
-      implementation: mailbox.address,
-    }) as ProxiedContract<TestMailbox, TransparentProxyAddresses>;
   }
 
   async deployApp(): Promise<TestCoreApp<TestChain>> {
