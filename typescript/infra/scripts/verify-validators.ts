5f74aeef46828ba9f1e7349e23aaf82369e7eb3f
Asa Oines <asaoines@gmail.com>
2022-12-07 15:03:31 -0500

    Update verify validators script

---
+++ typescript/infra/scripts/verify-validators.ts
@@ -0,0 +1,47 @@
+import { ethers } from 'ethers';
+
+import {
+  ChainNameToDomainId,
+  hyperlaneCoreAddresses,
+  objMap,
+} from '@hyperlane-xyz/sdk';
+
+import { S3Validator } from '../src/agents/aws/validator';
+import { CheckpointSyncerType } from '../src/config/agent';
+
+import { getContext, getCoreEnvironmentConfig, getEnvironment } from './utils';
+
+async function main() {
+  const environment = await getEnvironment();
+  const coreConfig = getCoreEnvironmentConfig(environment);
+  const context = await getContext();
+  const validatorSets = coreConfig.agents[context]?.validatorSets!;
+  objMap(validatorSets, async (chain, validatorSet) => {
+    const domainId = ChainNameToDomainId[chain];
+    const controlCheckpointSyncer = validatorSet.validators[0].checkpointSyncer;
+    if (controlCheckpointSyncer.type == CheckpointSyncerType.S3) {
+    }
+    const mailbox = hyperlaneCoreAddresses[chain].mailbox;
+    const validators = validatorSet.validators.map((validator) => {
+      const checkpointSyncer = validator.checkpointSyncer;
+      if (checkpointSyncer.type == CheckpointSyncerType.S3) {
+        return new S3Validator(
+          ethers.constants.AddressZero,
+          domainId,
+          mailbox,
+          checkpointSyncer.bucket,
+          checkpointSyncer.region,
+        );
+      }
+      throw new Error('Cannot check non-s3 validator type');
+    });
+    const controlValidator = validators[0];
+    for (let i = 1; i < validators.length; i++) {
+      const prospectiveValidator = validators[i];
+      const metrics = await prospectiveValidator.compare(controlValidator);
+      console.log(JSON.stringify(metrics, null, 2));
+    }
+  });
+}
+
+main().catch(console.error);
