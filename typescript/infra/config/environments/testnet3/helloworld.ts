a8afcb9a956c40d6a0d7050817ab78c21c166cb4
Asa Oines <asaoines@gmail.com>
2022-12-06 21:35:49 -0500

    Deploy helloworld to moonbasealpha

--- typescript/infra/config/environments/testnet3/helloworld.ts
+++ typescript/infra/config/environments/testnet3/helloworld.ts
@@ -1,4 +1,3 @@
-/*
 import { HelloWorldConfig } from '../../../src/config';
 import { ConnectionType } from '../../../src/config/agent';
 import { HelloWorldKathyRunMode } from '../../../src/config/helloworld';
@@ -32,4 +31,3 @@ export const helloWorld: Partial<
 > = {
   [Contexts.Hyperlane]: hyperlane,
 };
-*/
