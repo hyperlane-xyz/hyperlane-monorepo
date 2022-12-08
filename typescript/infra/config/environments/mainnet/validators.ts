ab3d73826ed73124fe8c00e05097b09303805d12
Asa Oines <asaoines@gmail.com>
2022-12-08 12:46:28 -0500

    Add dsrv

--- typescript/infra/config/environments/mainnet/validators.ts
+++ typescript/infra/config/environments/mainnet/validators.ts
@@ -16,7 +16,7 @@ const s3BucketName = <Chain extends ChainName>(
 
 export const validators: ChainValidatorSets<MainnetChains> = {
   celo: {
-    threshold: 3,
+    threshold: 4,
     validators: [
       {
         address: '0x8784f09c2cfb70329b936a3b75eb61c41b467b65',
@@ -47,7 +47,6 @@ export const validators: ChainValidatorSets<MainnetChains> = {
           region: 'eu-central-1',
         },
       },
-      /*
       {
         address: '0x54e2f79e458fa17e2a5b90cf74ace79fe98cc8d0',
         readonly: true,
@@ -58,6 +57,7 @@ export const validators: ChainValidatorSets<MainnetChains> = {
           region: 'eu-central-1',
         },
       },
+      /*
       {
         address: '0x97d91c1d5157338d3d53d452da5e94621a028873',
         readonly: true,
@@ -68,11 +68,21 @@ export const validators: ChainValidatorSets<MainnetChains> = {
           region: 'eu-west-1',
         },
       },
+      {
+        address: '0x82e6b73ee3c67032ead7f7022ebc81fb7902199b',
+        readonly: true,
+        name: 'dsrv-celo',
+        checkpointSyncer: {
+          type: CheckpointSyncerType.S3,
+          bucket: 'dsrv-hyperlane-validator-signatures-validator4-celo',
+          region: 'eu-central-1',
+        },
+      },
       */
     ],
   },
   ethereum: {
-    threshold: 3,
+    threshold: 4,
     validators: [
       {
         address: '0x7c1d96c9e72c41b84d74095dc2e72b979e933904',
@@ -103,7 +113,6 @@ export const validators: ChainValidatorSets<MainnetChains> = {
           region: 'eu-central-1',
         },
       },
-      /*
       {
         address: '0x7a8974723a559d71d16af3f13663cf5bffea0b5e',
         readonly: true,
@@ -114,6 +123,7 @@ export const validators: ChainValidatorSets<MainnetChains> = {
           region: 'eu-central-1',
         },
       },
+      /*
       {
         address: '0x4ed6a1249f4ade53a831def7dd5a44a74e488939',
         readonly: true,
@@ -125,11 +135,21 @@ export const validators: ChainValidatorSets<MainnetChains> = {
           region: 'eu-west-1',
         },
       },
+      {
+        address: '0xff88fad084041bc46664d81955327621878b4ed4',
+        readonly: true,
+        name: 'dsrv-ethereum',
+        checkpointSyncer: {
+          type: CheckpointSyncerType.S3,
+          bucket: 'dsrv-hyperlane-validator-signatures-validator1-ethereum',
+          region: 'eu-central-1',
+        },
+      },
       */
     ],
   },
   avalanche: {
-    threshold: 3,
+    threshold: 4,
     validators: [
       {
         address: '0x1353b91e2b256ca0a283504b02ae3c69374fb105',
@@ -160,7 +180,6 @@ export const validators: ChainValidatorSets<MainnetChains> = {
           region: 'eu-central-1',
         },
       },
-      /*
       {
         address: '0x39b505266aff2d52602f05ceaa7d4261e9494a50',
         readonly: true,
@@ -171,6 +190,7 @@ export const validators: ChainValidatorSets<MainnetChains> = {
           region: 'eu-central-1',
         },
       },
+      /*
       {
         address: '0x46b1023a1d936a4ce1908810f5a3cfabf0e36c15',
         readonly: true,
@@ -182,11 +202,21 @@ export const validators: ChainValidatorSets<MainnetChains> = {
           region: 'eu-west-1',
         },
       },
+      {
+        address: '0x47528d65d30299f0edfc2331b13bf41ee3a7d378',
+        readonly: true,
+        name: 'dsrv-avalanche',
+        checkpointSyncer: {
+          type: CheckpointSyncerType.S3,
+          bucket: 'dsrv-hyperlane-validator-signatures-validator2-avalanche',
+          region: 'eu-central-1',
+        },
+      },
       */
     ],
   },
   polygon: {
-    threshold: 3,
+    threshold: 4,
     validators: [
       {
         address: '0xff198cae21069d865b142f2b9e798d3b299b8df6',
@@ -217,7 +247,6 @@ export const validators: ChainValidatorSets<MainnetChains> = {
           region: 'eu-central-1',
         },
       },
-      /*
       {
         address: '0x860979ccaee9e432d3684e580987bed3e8f04846',
         readonly: true,
@@ -228,6 +257,7 @@ export const validators: ChainValidatorSets<MainnetChains> = {
           region: 'eu-central-1',
         },
       },
+      /*
       {
         address: '0x55b74e07f240d28dd3a72d52a01f3b703322b185',
         readonly: true,
@@ -238,11 +268,21 @@ export const validators: ChainValidatorSets<MainnetChains> = {
           region: 'eu-west-1',
         },
       },
+      {
+        address: '0xf5e112c875c6822930464b997ef5b284bfa9a8d1',
+        readonly: true,
+        name: 'dsrv-polygon',
+        checkpointSyncer: {
+          type: CheckpointSyncerType.S3,
+          bucket: 'dsrv-hyperlane-validator-signatures-validator6-polygon',
+          region: 'eu-central-1',
+        },
+      },
       */
     ],
   },
   bsc: {
-    threshold: 3,
+    threshold: 4,
     validators: [
       {
         address: '0x013d01a570b5cfa19032c5176488b5824e225a6b',
@@ -273,7 +313,6 @@ export const validators: ChainValidatorSets<MainnetChains> = {
           region: 'eu-central-1',
         },
       },
-      /*
       {
         address: '0x73c7d898546a6c595ca7c32d52aa45d75f428a7d',
         readonly: true,
@@ -284,6 +323,7 @@ export const validators: ChainValidatorSets<MainnetChains> = {
           region: 'eu-central-1',
         },
       },
+      /*
       {
         address: '0x417fe403198c7d817a4fcd8cd59d8d7421626296',
         readonly: true,
@@ -294,11 +334,21 @@ export const validators: ChainValidatorSets<MainnetChains> = {
           region: 'eu-west-1',
         },
       },
+      {
+        address: '0x15328b90afd81f27799808fe33c6c7245163e74b',
+        readonly: true,
+        name: 'dsrv-bsc',
+        checkpointSyncer: {
+          type: CheckpointSyncerType.S3,
+          bucket: 'dsrv-hyperlane-validator-signatures-validator3-bsc',
+          region: 'eu-central-1',
+        },
+      },
       */
     ],
   },
   arbitrum: {
-    threshold: 3,
+    threshold: 4,
     validators: [
       {
         address: '0x6289f913acc64c2e6baaa2b5175c7db35f98f62d',
@@ -329,7 +379,6 @@ export const validators: ChainValidatorSets<MainnetChains> = {
           region: 'eu-central-1',
         },
       },
-      /*
       {
         address: '0x5acbfc1493252110b698534bca12ec65bf3b5f1f',
         readonly: true,
@@ -340,6 +389,7 @@ export const validators: ChainValidatorSets<MainnetChains> = {
           region: 'eu-central-1',
         },
       },
+      /*
       {
         address: '0xee699d14bb61e3931bad94e09668be5524bae350',
         readonly: true,
@@ -351,11 +401,21 @@ export const validators: ChainValidatorSets<MainnetChains> = {
           region: 'eu-west-1',
         },
       },
+      {
+        address: '0xdebd62c57346c38a9cf2990210de021caaa20470',
+        readonly: true,
+        name: 'dsrv-arbitrum',
+        checkpointSyncer: {
+          type: CheckpointSyncerType.S3,
+          bucket: 'dsrv-hyperlane-validator-signatures-validator7-arbitrum',
+          region: 'eu-central-1',
+        },
+      },
       */
     ],
   },
   optimism: {
-    threshold: 3,
+    threshold: 4,
     validators: [
       {
         address: '0xcf2dcc3462c84420965501c1dda3d62dde08941d',
@@ -386,7 +446,6 @@ export const validators: ChainValidatorSets<MainnetChains> = {
           region: 'eu-central-1',
         },
       },
-      /*
       {
         address: '0xbae7166acd15d8f0ab1d432c5d874bb24718edb8',
         readonly: true,
@@ -397,6 +456,7 @@ export const validators: ChainValidatorSets<MainnetChains> = {
           region: 'eu-central-1',
         },
       },
+      /*
       {
         address: '0x8d96ffabd6b2283092ec08ae5cf65e06fd4dbaa1',
         readonly: true,
@@ -408,11 +468,21 @@ export const validators: ChainValidatorSets<MainnetChains> = {
           region: 'eu-west-1',
         },
       },
+      {
+        address: '0x7f6a8e8d15c6582ea2267e5acdde068f4b26fd91',
+        readonly: true,
+        name: 'dsrv-optimism',
+        checkpointSyncer: {
+          type: CheckpointSyncerType.S3,
+          bucket: 'dsrv-hyperlane-validator-signatures-validator8-optimism',
+          region: 'eu-central-1',
+        },
+      },
       */
     ],
   },
   moonbeam: {
-    threshold: 2,
+    threshold: 3,
     validators: [
       {
         address: '0x3da42c84aaf9d548feb219bfdf5fcb2217fbfb48',
@@ -432,7 +502,6 @@ export const validators: ChainValidatorSets<MainnetChains> = {
           region: s3BucketRegion,
         },
       },
-      /*
       {
         address: '0x2b3c6844a3968bb613c46bda43223f0700f91917',
         readonly: true,
@@ -443,6 +512,7 @@ export const validators: ChainValidatorSets<MainnetChains> = {
           region: 'eu-central-1',
         },
       },
+      /*
       {
         address: '0x12c31e401ef4a5c494f76ae4c8ed065e3fa0a898',
         readonly: true,
@@ -452,6 +522,16 @@ export const validators: ChainValidatorSets<MainnetChains> = {
           bucket:
             'hyperlane-validator-signatures-production-eu-west-1-moonbeam',
           region: 'eu-west-1',
+        },
+      },
+      {
+        address: '0xba101c924f76eb48915aaab1a2a28ace24f19b36',
+        readonly: true,
+        name: 'dsrv-moonbeam',
+        checkpointSyncer: {
+          type: CheckpointSyncerType.S3,
+          bucket: 'dsrv-hyperlane-validator-signatures-validator5-moonbeam',
+          region: 'eu-central-1',
         },
       },
       */
