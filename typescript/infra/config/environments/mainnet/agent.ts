611a9e9321c9ddd06422ec38bf129fa91bb83f93
Mattie Conover <git@mconover.dev>
2022-12-12 14:15:27 -0800

    deploy

--- typescript/infra/config/environments/mainnet/agent.ts
+++ typescript/infra/config/environments/mainnet/agent.ts
@@ -23,7 +23,7 @@ export const abacus: AgentConfig<MainnetChains> = {
   context: Contexts.Abacus,
   docker: {
     repo: 'gcr.io/abacus-labs-dev/abacus-agent',
-    tag: 'sha-6bdb257',
+    tag: 'sha-01fc263',
   },
   aws: {
     region: 'us-east-1',
