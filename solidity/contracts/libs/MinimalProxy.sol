067d972a1841b582d2e7f9f1962be7e4a2a72df8
Yorke Rhodes <email@yorke.dev>
2022-12-14 13:25:41 -0500

    Pull out constants and add useful comments

--- solidity/contracts/libs/MinimalProxy.sol
+++ solidity/contracts/libs/MinimalProxy.sol
@@ -1,24 +1,16 @@
 // SPDX-License-Identifier: MIT OR Apache-2.0
 pragma solidity >=0.6.11;
 
-// https://eips.ethereum.org/EIPS/eip-1167
-
+// Library for building bytecode of minimal proxies (see https://eips.ethereum.org/EIPS/eip-1167)
 library MinimalProxy {
+    bytes20 constant prefix = hex"3d602d80600a3d3981f3363d3d373d3d3d363d73";
+    bytes15 constant suffix = hex"5af43d82803e903d91602b57fd5bf3";
+
     function bytecode(address implementation)
         internal
         pure
         returns (bytes memory)
     {
-        bytes10 creation = 0x3d602d80600a3d3981f3;
-        bytes10 runtimePrefix = 0x363d3d373d3d3d363d73;
-        bytes20 targetBytes = bytes20(implementation);
-        bytes15 runtimeSuffix = 0x5af43d82803e903d91602b57fd5bf3;
-        return
-            abi.encodePacked(
-                creation,
-                runtimePrefix,
-                targetBytes,
-                runtimeSuffix
-            );
+        return abi.encodePacked(prefix, bytes20(implementation), suffix);
     }
 }
