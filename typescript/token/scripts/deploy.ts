f3e68fb575960bce9edd2f84bfc144abc22a8ead
Nam Chu Hoai <nambrot@googlemail.com>
2022-12-20 16:50:41 +0700

    Add token package back into the monorepo for now (#1451)
    * Add token package back into the monorepo for now
    
    * CI

---
+++ typescript/token/scripts/deploy.ts
@@ -0,0 +1,65 @@
+import { Wallet, ethers } from 'ethers';
+
+import {
+  Chains,
+  HyperlaneCore,
+  MultiProvider,
+  objMap,
+} from '@hyperlane-xyz/sdk';
+import { RouterConfig, chainConnectionConfigs } from '@hyperlane-xyz/sdk';
+
+import { TokenConfig, TokenType } from '../src/config';
+import { HypERC20Deployer } from '../src/deploy';
+
+const connectionConfigs = {
+  goerli: {
+    ...chainConnectionConfigs.goerli,
+    provider: new ethers.providers.JsonRpcProvider(
+      'https://eth-goerli.public.blastapi.io',
+      5,
+    ),
+  },
+  fuji: chainConnectionConfigs.fuji,
+  alfajores: chainConnectionConfigs.alfajores,
+  moonbasealpha: chainConnectionConfigs.moonbasealpha,
+};
+
+async function deployNFTWrapper() {
+  console.info('Getting signer');
+  const signer = new Wallet(
+    '3ed2c141ec02887887e94c1fbe5647fe5741c038deca248afbaaccf2c27d9258',
+  );
+
+  const multiProvider = new MultiProvider(connectionConfigs);
+  multiProvider.rotateSigner(signer);
+  const core = HyperlaneCore.fromEnvironment('testnet2', multiProvider);
+
+  const config = objMap(
+    connectionConfigs,
+    (chain, c) =>
+      ({
+        type: TokenType.synthetic,
+        name: 'Dai',
+        symbol: 'DAI',
+        totalSupply: 0,
+        owner: signer.address,
+        mailbox: '0x1d3aAC239538e6F1831C8708803e61A9EA299Eec',
+        interchainGasPaymaster:
+          core.getContracts(chain).interchainGasPaymaster.address,
+      } as TokenConfig & RouterConfig),
+  );
+  config.goerli = {
+    type: TokenType.collateral,
+    token: '0xb4fbf271143f4fbf7b91a5ded31805e42b2208d6',
+    owner: signer.address,
+    mailbox: '0x1d3aAC239538e6F1831C8708803e61A9EA299Eec',
+    interchainGasPaymaster: core.getContracts(Chains.goerli)
+      .interchainGasPaymaster.address,
+  } as TokenConfig & RouterConfig;
+
+  const deployer = new HypERC20Deployer(multiProvider, config, core);
+
+  await deployer.deploy();
+}
+
+deployNFTWrapper().then(console.log).catch(console.error);
---
+++ typescript/token/src/deploy.ts
@@ -0,0 +1,114 @@
+import { ChainName, HyperlaneRouterDeployer } from '@hyperlane-xyz/sdk';
+
+import {
+  HypERC20CollateralConfig,
+  HypERC20Config,
+  HypERC721CollateralConfig,
+  HypERC721Config,
+  isCollateralConfig,
+  isUriConfig,
+} from './config';
+import { HypERC20Contracts, HypERC721Contracts } from './contracts';
+import {
+  HypERC20Collateral__factory,
+  HypERC20__factory,
+  HypERC721Collateral__factory,
+  HypERC721URICollateral__factory,
+  HypERC721URIStorage__factory,
+  HypERC721__factory,
+} from './types';
+
+export class HypERC20Deployer<
+  Chain extends ChainName, // inferred from configured chains passed to constructor
+> extends HyperlaneRouterDeployer<
+  Chain,
+  HypERC20Config | HypERC20CollateralConfig,
+  HypERC20Contracts,
+  any // RouterFactories doesn't work well when router has multiple types
+> {
+  async deployContracts(
+    chain: Chain,
+    config: HypERC20Config | HypERC20CollateralConfig,
+  ) {
+    const connection = this.multiProvider.getChainConnection(chain);
+    if (isCollateralConfig(config)) {
+      const router = await this.deployContractFromFactory(
+        chain,
+        new HypERC20Collateral__factory(),
+        'HypERC20Collateral',
+        [config.token],
+      );
+      await connection.handleTx(
+        router.initialize(config.mailbox, config.interchainGasPaymaster),
+      );
+      return { router };
+    } else {
+      const router = await this.deployContractFromFactory(
+        chain,
+        new HypERC20__factory(),
+        'HypERC20',
+        [],
+      );
+      await connection.handleTx(
+        router.initialize(
+          config.mailbox,
+          config.interchainGasPaymaster,
+          config.totalSupply,
+          config.name,
+          config.symbol,
+        ),
+      );
+      return { router };
+    }
+  }
+}
+
+// TODO: dedupe?
+export class HypERC721Deployer<
+  Chain extends ChainName,
+> extends HyperlaneRouterDeployer<
+  Chain,
+  HypERC721Config | HypERC721CollateralConfig,
+  HypERC721Contracts,
+  any
+> {
+  async deployContracts(
+    chain: Chain,
+    config: HypERC721Config | HypERC721CollateralConfig,
+  ) {
+    const connection = this.multiProvider.getChainConnection(chain);
+    if (isCollateralConfig(config)) {
+      const router = await this.deployContractFromFactory(
+        chain,
+        isUriConfig(config)
+          ? new HypERC721URICollateral__factory()
+          : new HypERC721Collateral__factory(),
+        `HypERC721${isUriConfig(config) ? 'URI' : ''}Collateral`,
+        [config.token],
+      );
+      await connection.handleTx(
+        router.initialize(config.mailbox, config.interchainGasPaymaster),
+      );
+      return { router };
+    } else {
+      const router = await this.deployContractFromFactory(
+        chain,
+        isUriConfig(config)
+          ? new HypERC721URIStorage__factory()
+          : new HypERC721__factory(),
+        `HypERC721${isUriConfig(config) ? 'URIStorage' : ''}`,
+        [],
+      );
+      await connection.handleTx(
+        router.initialize(
+          config.mailbox,
+          config.interchainGasPaymaster,
+          config.totalSupply,
+          config.name,
+          config.symbol,
+        ),
+      );
+      return { router };
+    }
+  }
+}
