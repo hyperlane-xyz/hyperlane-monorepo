dc9ea0c96c926018ece72b7057fd3ef95d59077b
Mattie Conover <git@mconover.dev>
2023-01-11 10:04:45 -0800

    WIP

--- rust/hyperlane-core/src/traits/signer.rs
+++ rust/hyperlane-core/src/traits/signer.rs
@@ -3,6 +3,7 @@ use auto_impl::auto_impl;
 use ethers::prelude::Signature;
 use primitive_types::H256;
 use std::fmt::Debug;
+use crate::{Signable, SignedType};
 
 /// An error incurred by a signer
 #[derive(thiserror::Error, Debug)]
@@ -18,4 +19,13 @@ pub trait HyperlaneSigner: Send + Sync + Debug {
 
     /// Sign a hyperlane checkpoint hash. This must be a signature without eip 155.
     async fn sign_hash(&self, hash: &H256) -> Result<Signature, HyperlaneSignerError>;
+
+    async fn sign<S: Signable>(&self, value: S) -> Result<SignedType<S>, HyperlaneSignerError> {
+        let signing_hash = value.signing_hash();
+        let signature = self.sign_hash(&signing_hash).await?;
+        Ok(SignedType {
+            value,
+            signature,
+        })
+    }
 }
