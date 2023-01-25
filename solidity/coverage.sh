f9878a1fb743fed97f02d1f151ed742932c5a6ef
Asa Oines <asaoines@gmail.com>
2023-01-25 15:37:25 -0500

    Coverage script

---
+++ solidity/coverage.sh
@@ -0,0 +1,12 @@
+yarn hardhat coverage && forge coverage --report lcov
+# Hardhat uses absolute paths, whereas foundry uses relative paths
+# We convert hardhat's output to relative paths so they can be merged
+sed -i '' 's/\/.*hyperlane-monorepo.*solidity.//g' coverage/lcov.info 
+# Merge the two 
+find . -name lcov.info -exec echo -a {} \; | xargs lcov -o lcov.info
+# Clean up output
+rm -rf coverage
+# Remove files we don't want to test
+lcov --remove coverage/lcov.info -o lcov.info 'contracts/test/**' 'contracts/mock/**' '**/node_modules/**' 'test/*'
+# Print output
+lcov --list lcov.info
