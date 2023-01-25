61c9e8f14c45e2b80244d1b2b63754b36e1a70da
Asa Oines <asaoines@gmail.com>
2023-01-25 17:03:21 -0500

    Fix

--- solidity/coverage.sh
+++ solidity/coverage.sh
@@ -1,15 +1,15 @@
-yarn hardhat coverage && forge coverage --report lcov
+yarn hardhat coverage && forge coverage --report lcov && \
 # Foundry uses paths relative to hyperlane-monorepo/solidity
 # whereas Hardhat uses absolute paths (locally) or paths
 # relative to hyperlane-monorepo (ci).
 # We convert Hardhat paths to Foundry paths so coverage artifacts
 # can be merged.
-sed -i '' 's/\/.*solidity.//g' coverage/lcov.info
+sed -i -e 's/\/.*solidity.//g' coverage/lcov.info && \
 # Merge the two 
-find . -name lcov.info -exec echo -a {} \; | xargs lcov -o lcov.info
+find . -name lcov.info -exec echo -a {} \; | xargs lcov -o lcov.info && \
 # Clean up output
-rm -rf coverage
+rm -rf coverage && \
 # Remove files we don't care about covering
-lcov --remove lcov.info -o lcov.info 'contracts/test/**' 'contracts/mock/**' '**/node_modules/**' 'test/*'
+lcov --remove lcov.info -o lcov.info 'contracts/test/**' 'contracts/mock/**' '**/node_modules/**' 'test/*' && \
 # Print output
 lcov --list lcov.info
