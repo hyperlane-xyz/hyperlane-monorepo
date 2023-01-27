04bfd7986fda8bf5f909cb1ca4ff25ee4edcfcf2
Mattie Conover <git@mconover.dev>
2023-01-27 14:30:28 -0800

    Config for testnet3

--- typescript/infra/config/environments/testnet3/agent.ts
+++ typescript/infra/config/environments/testnet3/agent.ts
@@ -1,23 +1,22 @@
 import { chainMetadata } from '@hyperlane-xyz/sdk';
 
-import { ALL_KEY_ROLES } from '../../../src/agents/roles';
+import { ALL_KEY_ROLES, KEY_ROLE_ENUM } from '../../../src/agents/roles';
 import { AgentConfig } from '../../../src/config';
 import {
   ConnectionType,
   GasPaymentEnforcementPolicyType,
 } from '../../../src/config/agent';
 import { Contexts } from '../../contexts';
+import { helloworldMatchingList } from '../../utils';
 
 import { TestnetChains, chainNames, environment } from './chains';
-// import { helloWorld } from './helloworld';
+import { helloWorld } from './helloworld';
 import { validators } from './validators';
 
-/*
 const releaseCandidateHelloworldMatchingList = helloworldMatchingList(
   helloWorld,
   Contexts.ReleaseCandidate,
 );
-*/
 
 export const hyperlane: AgentConfig<TestnetChains> = {
   environment,
@@ -67,6 +66,7 @@ export const hyperlane: AgentConfig<TestnetChains> = {
   relayer: {
     default: {
       blacklist: [
+        ...releaseCandidateHelloworldMatchingList,
         { recipientAddress: '0xBC3cFeca7Df5A45d61BC60E7898E63670e1654aE' },
       ],
       gasPaymentEnforcementPolicy: {
@@ -77,6 +77,37 @@ export const hyperlane: AgentConfig<TestnetChains> = {
   rolesWithKeys: ALL_KEY_ROLES,
 };
 
+export const releaseCandidate: AgentConfig<TestnetChains> = {
+  environment,
+  namespace: environment,
+  runEnv: environment,
+  context: Contexts.Hyperlane,
+  docker: {
+    repo: 'gcr.io/abacus-labs-dev/hyperlane-agent',
+    tag: 'sha-c163fce',
+  },
+  aws: {
+    region: 'us-east-1',
+  },
+  environmentChainNames: chainNames,
+  contextChainNames: chainNames,
+  validatorSets: validators,
+  gelato: {
+    enabledChains: [],
+  },
+  connectionType: ConnectionType.HttpQuorum,
+  relayer: {
+    default: {
+      whitelist: releaseCandidateHelloworldMatchingList,
+      gasPaymentEnforcementPolicy: {
+        type: GasPaymentEnforcementPolicyType.None,
+      },
+    },
+  },
+  rolesWithKeys: [KEY_ROLE_ENUM.Relayer, KEY_ROLE_ENUM.Kathy],
+};
+
 export const agents = {
   [Contexts.Hyperlane]: hyperlane,
+  [Contexts.ReleaseCandidate]: releaseCandidate,
 };
