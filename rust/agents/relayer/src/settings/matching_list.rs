c19f63d86b3e7af361ea287d49c537f4a804fe08
Mattie Conover <git@mconover.dev>
2023-01-31 12:52:42 -0800

    Fix compiler error

--- rust/agents/relayer/src/settings/matching_list.rs
+++ rust/agents/relayer/src/settings/matching_list.rs
@@ -1,5 +1,5 @@
 use std::fmt;
-use std::fmt::{Display, Formatter};
+use std::fmt::{Debug, Display, Formatter};
 use std::marker::PhantomData;
 use std::num::ParseIntError;
 
@@ -41,7 +41,7 @@ impl<T: PartialEq> Filter<T> {
     }
 }
 
-impl<T: Display> Display for Filter<T> {
+impl<T: Debug> Display for Filter<T> {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
         match self {
             Self::Wildcard => write!(f, "*"),
@@ -276,8 +276,9 @@ fn parse_addr<E: Error>(addr_str: &str) -> Result<H256, E> {
 
 #[cfg(test)]
 mod test {
-    use crate::settings::matching_list::MatchInfo;
     use hyperlane_core::{H160, H256};
+
+    use crate::settings::matching_list::MatchInfo;
 
     use super::{Filter::*, MatchingList};
 
