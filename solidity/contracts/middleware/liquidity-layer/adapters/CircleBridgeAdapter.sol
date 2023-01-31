8831251896b5de7ebcd7de9f425c5aed27dd5bc1
Yorke Rhodes <yorke@hyperlane.xyz>
2023-01-31 15:56:26 -0500

    Use safeTransfer in liquidity layer

--- solidity/contracts/middleware/liquidity-layer/adapters/CircleBridgeAdapter.sol
+++ solidity/contracts/middleware/liquidity-layer/adapters/CircleBridgeAdapter.sol
@@ -8,8 +8,11 @@ import {ICircleMessageTransmitter} from "../interfaces/circle/ICircleMessageTran
 import {ILiquidityLayerAdapter} from "../interfaces/ILiquidityLayerAdapter.sol";
 
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 contract CircleBridgeAdapter is ILiquidityLayerAdapter, Router {
+    using SafeERC20 for IERC20;
+
     /// @notice The CircleBridge contract.
     ICircleBridge public circleBridge;
 
@@ -22,7 +25,7 @@ contract CircleBridgeAdapter is ILiquidityLayerAdapter, Router {
     /// @notice Hyperlane domain => Circle domain.
     /// ATM, known Circle domains are Ethereum = 0 and Avalanche = 1.
     /// Note this could result in ambiguity between the Circle domain being
-    /// Ethereum or unknown. TODO fix?
+    /// Ethereum or unknown.
     mapping(uint32 => uint32) public hyperlaneDomainToCircleDomain;
 
     /// @notice Token symbol => address of token on local chain.
@@ -151,10 +154,9 @@ contract CircleBridgeAdapter is ILiquidityLayerAdapter, Router {
         );
 
         // Transfer the token out to the recipient
-        // TODO: use safeTransfer
         // Circle doesn't charge any fee, so we can safely transfer out the
         // exact amount that was bridged over.
-        require(_token.transfer(_recipient, _amount), "!transfer out");
+        _token.safeTransfer(_recipient, _amount);
 
         return (address(_token), _amount);
     }
