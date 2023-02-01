676e7d29f136b0797edfef7812e7af923baafdbd
Mattie Conover <git@mconover.dev>
2023-02-01 11:01:38 -0800

    Deploy

--- typescript/infra/config/environments/mainnet2/funding.ts
+++ typescript/infra/config/environments/mainnet2/funding.ts
@@ -8,7 +8,7 @@ import { environment } from './chains';
 export const keyFunderConfig: KeyFunderConfig = {
   docker: {
     repo: 'gcr.io/abacus-labs-dev/hyperlane-monorepo',
-    tag: 'sha-59e9fda',
+    tag: 'sha-e80c87d',
   },
   // We're currently using the same deployer key as mainnet.
   // To minimize nonce clobbering we offset the key funder cron
