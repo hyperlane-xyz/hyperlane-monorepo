7f9fa299df67c5f486a0f5f70c64d048e782c55a
Trevor Porter <trkporter@ucdavis.edu>
2023-02-06 18:45:32 +0000

    yarn gas, better comments

--- solidity/contracts/mock/MockLZRelayerV2.sol
+++ solidity/contracts/mock/MockLZRelayerV2.sol
@@ -3,8 +3,9 @@ pragma solidity >=0.7.6;
 
 import {ILZRelayerV2} from "../../interfaces/ILZRelayerV2.sol";
 
+// Mocked version of https://github.com/LayerZero-Labs/LayerZero/blob/main/contracts/RelayerV2.sol
 contract MockLZRelayerV2 is ILZRelayerV2 {
-    // [_chainId] => DstPriceData. change often
+    // LZ domain => DstPrice
     mapping(uint16 => ILZRelayerV2.DstPrice) public override dstPriceLookup;
 
     function setDstPriceLookup(
