0141454e7f71a1996484b7dcb4212bb0c95c309f
Steven Sloboda <steven@eclipse.builders>
2023-02-22 15:26:40 -0600

    Add sealevel test config and script.

---
+++ rust/utils/sealevel-test.bash
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+set -ex
+
+KEYPAIR="${HOME}/.config/eclipse/id.json"
+TARGET_DIR="../sealevel/target"
+DEPLOY_DIR="${TARGET_DIR}/deploy"
+BIN_DIR="${TARGET_DIR}/debug"
+CHAIN_ID="13375"
+
+solana -k "${KEYPAIR}" program deploy "${DEPLOY_DIR}/hyperlane_sealevel_mailbox.so"
+solana -k "${KEYPAIR}" program deploy "${DEPLOY_DIR}/hyperlane_sealevel_recipient_echo.so"
+solana -k "${KEYPAIR}" program deploy "${DEPLOY_DIR}/hyperlane_sealevel_ism_rubber_stamp.so"
+
+set +e
+while ! "${BIN_DIR}/hyperlane-sealevel-client" -k "${KEYPAIR}" init; do
+    sleep 3
+done
+
+while "${BIN_DIR}/hyperlane-sealevel-client" -k "${KEYPAIR}" query | grep -q 'Not yet created'; do
+    sleep 3
+done
+set -e
+"${BIN_DIR}/hyperlane-sealevel-client" -k "${KEYPAIR}" query
+
+# Send N test messages to the outbox to be validated and relayed to the inbox.
+"${BIN_DIR}/hyperlane-sealevel-client" -k "${KEYPAIR}" send -l "${CHAIN_ID}" -d "${CHAIN_ID}"
+"${BIN_DIR}/hyperlane-sealevel-client" -k "${KEYPAIR}" send -l "${CHAIN_ID}" -d "${CHAIN_ID}"
+"${BIN_DIR}/hyperlane-sealevel-client" -k "${KEYPAIR}" send -l "${CHAIN_ID}" -d "${CHAIN_ID}"
