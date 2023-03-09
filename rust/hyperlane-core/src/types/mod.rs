7fb935e8015eff3341ce940858437ee0d4706508
Trevor Porter <trkporter@ucdavis.edu>
2023-03-09 20:02:52 +0000

    Arbitrum gas amount handling in gas enforcement policies (#1912)
    ### Description
    
    See #1728 for detailed explanation
    
    Uses Arbitrum's NodeInterface to get the L1 cost of a tx so we can
    isolate the L2 gas and use that for gas enforcement policies. Very open
    to alternative ideas in the way I wrote it as well
    
    ### Drive-by changes
    
    No
    
    ### Related issues
    
    - Fixes #1728 
    
    ### Backward compatibility
    
    _Are these changes backward compatible?_
    
    Yes
    
    _Are there any infrastructure implications, e.g. changes that would
    prohibit deploying older commits using this infra tooling?_
    
    None
    
    
    ### Testing
    
    _What kind of testing have these changes undergone?_
    
    Unit Tests - haven't deployed yet, but plan to deploy to rc testnet soon

--- rust/hyperlane-core/src/types/mod.rs
+++ rust/hyperlane-core/src/types/mod.rs
@@ -118,10 +118,24 @@ pub struct InterchainGasPaymentWithMeta {
 }
 
 /// A cost estimate for a transaction.
-#[derive(Clone, Debug, Default)]
+#[derive(Clone, Debug, Default, PartialEq, Eq)]
 pub struct TxCostEstimate {
     /// The gas limit for the transaction.
     pub gas_limit: U256,
     /// The gas price for the transaction.
     pub gas_price: U256,
+    /// The amount of L2 gas for the transaction.
+    /// If Some, `gas_limit` is the sum of the gas limit
+    /// covering L1 costs and the L2 gas limit.
+    /// Only present for Arbitrum Nitro chains, where the gas amount
+    /// is used to cover L1 and L2 costs. For details:
+    /// https://medium.com/offchainlabs/understanding-arbitrum-2-dimensional-fees-fd1d582596c9
+    pub l2_gas_limit: Option<U256>,
+}
+
+impl TxCostEstimate {
+    /// The gas limit to be used by gas enforcement policies.
+    pub fn enforceable_gas_limit(&self) -> U256 {
+        self.l2_gas_limit.unwrap_or(self.gas_limit)
+    }
 }
