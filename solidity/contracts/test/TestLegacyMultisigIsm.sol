c80f3fd76e144722662c5f7df15831b96c811134
Asa Oines <asaoines@gmail.com>
2023-03-13 17:37:59 -0400

    missing files

---
+++ solidity/contracts/test/TestLegacyMultisigIsm.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT OR Apache-2.0
+pragma solidity >=0.8.0;
+
+// ============ Internal Imports ============
+import {LegacyMultisigIsm} from "../isms/LegacyMultisigIsm.sol";
+
+contract TestLegacyMultisigIsm is LegacyMultisigIsm {
+    function getDomainHash(uint32 _origin, bytes32 _originMailbox)
+        external
+        pure
+        returns (bytes32)
+    {
+        return _getDomainHash(_origin, _originMailbox);
+    }
+
+    function getCheckpointDigest(bytes calldata _metadata, uint32 _origin)
+        external
+        pure
+        returns (bytes32)
+    {
+        return _getCheckpointDigest(_metadata, _origin);
+    }
+}
