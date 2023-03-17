48c4a98d9728d0221d6650b6b76fb1643250e5cf
Trevor Porter <trkporter@ucdavis.edu>
2023-03-17 12:55:08 +0000

    Make setInterchainSecurityModule onlyOwner

--- typescript/infra/src/testcontracts/testrecipient.ts
+++ typescript/infra/src/testcontracts/testrecipient.ts
@@ -33,11 +33,18 @@ export class TestRecipientDeployer extends HyperlaneDeployer<
     );
   }
   async deployContracts(chain: ChainName) {
+    const deployer = this.multiProvider.getSignerAddress(chain);
     const TestRecipient = await this.deployContract(
       chain,
       'TestRecipient',
       [],
-      { create2Salt: 'testtest32' },
+      {
+        create2Salt: 'TestRecipient-March-17-2023',
+        initCalldata: new TestRecipient__factory().interface.encodeFunctionData(
+          'transferOwnership',
+          [deployer],
+        ),
+      },
     );
     const TestTokenRecipient = await this.deployContract(
       chain,
