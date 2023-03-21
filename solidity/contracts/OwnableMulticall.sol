933d47210aed6cbfee4ffda7a409c04a9bd9bcac
Yorke Rhodes <yorke@hyperlane.xyz>
2023-03-21 11:18:48 -0400

    Inline immutable owner logic

--- solidity/contracts/OwnableMulticall.sol
+++ solidity/contracts/OwnableMulticall.sol
@@ -1,18 +1,24 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.8.13;
 
-// ============ External Imports ============
-
-import {Address} from "@openzeppelin/contracts/utils/Address.sol";
+// ============ Internal Imports ============
 import {CallLib} from "./libs/Call.sol";
-import {ImmutableOwnable} from "./libs/ImmutableOwnable.sol";
 
 /*
  * @title OwnableMulticall
  * @dev Permits immutable owner address to execute calls with value to other contracts.
  */
-contract OwnableMulticall is ImmutableOwnable {
-    constructor(address _owner) ImmutableOwnable(_owner) {}
+contract OwnableMulticall {
+    address public immutable owner;
+
+    constructor(address _owner) {
+        owner = _owner;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "!owner");
+        _;
+    }
 
     function multicall(CallLib.Call[] calldata calls) external onlyOwner {
         return CallLib.multicall(calls);
