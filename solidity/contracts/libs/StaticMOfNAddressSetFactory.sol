bc98771cb60c0767320b1a7b77dd4df4438fdc03
Asa Oines <asaoines@gmail.com>
2023-03-22 10:59:02 -0400

    getter and natspecs

--- solidity/contracts/libs/StaticMOfNAddressSetFactory.sol
+++ solidity/contracts/libs/StaticMOfNAddressSetFactory.sol
@@ -19,9 +19,76 @@ abstract contract StaticMOfNAddressSetFactory {
 
     function _deployImplementation() internal virtual returns (address);
 
-    function deploy(address[] memory _values, uint8 _threshold)
+    /**
+     * @notice Deploys a StaticMOfNAddressSet contract address for the given
+     * values
+     * @dev Consider sorting addresses to ensure contract reuse
+     * @param _values An array of addresses
+     * @param _threshold The threshold value to use
+     * @return set The contract address representing this StaticMOfNAddressSet
+     */
+    function deploy(address[] calldata _values, uint8 _threshold)
         external
         returns (address)
+    {
+        (bytes32 _salt, bytes memory _bytecode) = _saltAndBytecode(
+            _values,
+            _threshold
+        );
+        address _set = _getAddress(_salt, _bytecode);
+        if (!Address.isContract(_set)) {
+            _set = Create2.deploy(0, _salt, _bytecode);
+        }
+        return _set;
+    }
+
+    /**
+     * @notice Returns the StaticMOfNAddressSet contract address for the given
+     * values
+     * @dev Consider sorting addresses to ensure contract reuse
+     * @param _values An array of addresses
+     * @param _threshold The threshold value to use
+     * @return set The contract address representing this StaticMOfNAddressSet
+     */
+    function getAddress(address[] calldata _values, uint8 _threshold)
+        external
+        view
+        returns (address)
+    {
+        (bytes32 _salt, bytes memory _bytecode) = _saltAndBytecode(
+            _values,
+            _threshold
+        );
+        return _getAddress(_salt, _bytecode);
+    }
+
+    /**
+     * @notice Returns the StaticMOfNAddressSet contract address for the given
+     * values
+     * @param _salt The salt used in Create2
+     * @param _bytecode The metaproxy bytecode used in Create2
+     * @return set The contract address representing this StaticMOfNAddressSet
+     */
+    function _getAddress(bytes32 _salt, bytes memory _bytecode)
+        private
+        view
+        returns (address)
+    {
+        bytes32 _bytecodeHash = keccak256(_bytecode);
+        return Create2.computeAddress(_salt, _bytecodeHash);
+    }
+
+    /**
+     * @notice Returns the create2 salt and bytecode for the given values
+     * @param _values An array of addresses
+     * @param _threshold The threshold value to use
+     * @return _salt The salt used in Create2
+     * @return _bytecode The metaproxy bytecode used in Create2
+     */
+    function _saltAndBytecode(address[] calldata _values, uint8 _threshold)
+        private
+        view
+        returns (bytes32, bytes memory)
     {
         bytes memory _metadata = abi.encode(_values, _threshold);
         bytes memory _bytecode = MetaProxyFactory.bytecode(
@@ -29,11 +96,6 @@ abstract contract StaticMOfNAddressSetFactory {
             _metadata
         );
         bytes32 _salt = keccak256(_metadata);
-        bytes32 _bytecodeHash = keccak256(_bytecode);
-        address _set = Create2.computeAddress(_salt, _bytecodeHash);
-        if (!Address.isContract(_set)) {
-            _set = Create2.deploy(0, _salt, _bytecode);
-        }
-        return _set;
+        return (_salt, _bytecode);
     }
 }
