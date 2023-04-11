dfa43acb9a74f4a90a4ff13358639a5fb73f3797
Yorke Rhodes <yorke@hyperlane.xyz>
2023-04-11 14:34:15 -0400

    Add branch coverage flags

--- solidity/coverage.sh
+++ solidity/coverage.sh
@@ -6,24 +6,35 @@ forge coverage --report lcov
 # generates coverage/lcov.info
 yarn hardhat coverage
 
-# Foundry uses paths relative to hyperlane-monorepo/solidity
-# whereas Hardhat uses absolute paths (locally) or paths
-# relative to hyperlane-monorepo (ci).
-# We convert Hardhat paths to Foundry paths so coverage artifacts
-# can be merged.
+# Foundry uses relative paths but Hardhat uses absolute paths.
+# Convert absolute paths to relative paths for consistency.
 sed -i -e 's/\/.*solidity.//g' coverage/lcov.info
 
 # Merge lcov files
-lcov --add-tracefile coverage/lcov.info --add-tracefile lcov.info --output-file merged-lcov.info
+lcov \
+    --rc lcov_branch_coverage=1 \
+    --add-tracefile coverage/lcov.info \
+    --add-tracefile lcov.info \
+    --output-file merged-lcov.info
 
 # Filter out node_modules, test, and mock files
-lcov --remove merged-lcov.info --output-file filtered-lcov.info "*node_modules*" "*test*" "*mock*"
+lcov \
+    --rc lcov_branch_coverage=1 \
+    --remove merged-lcov.info \
+    --output-file filtered-lcov.info \
+    "*node_modules*" "*test*" "*mock*"
 
 # Generate summary
-lcov --summary filtered-lcov.info
+lcov \
+    --rc lcov_branch_coverage=1 \
+    --list filtered-lcov.info
 
 # Open more granular breakdown in browser
 if [ "$CI" != "true" ]
 then
-    genhtml -o coverage filtered-lcov.info && open coverage/index.html
+    genhtml \
+        --rc genhtml_branch_coverage=1 \
+        --output-directory coverage \
+        filtered-lcov.info
+    open coverage/index.html
 fi
