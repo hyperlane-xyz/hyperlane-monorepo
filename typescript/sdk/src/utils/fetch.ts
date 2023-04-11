edc1d3ea4bbca342d5a6bbb7704377c13cec88f4
Trevor Porter <trkporter@ucdavis.edu>
2023-04-11 17:19:03 +0100

    Use Rossy's fetchWithTimeout

--- typescript/sdk/src/utils/fetch.ts
+++ typescript/sdk/src/utils/fetch.ts
@@ -1,14 +1,16 @@
 import fetch from 'cross-fetch';
 
-// Adapted from https://github.com/node-fetch/node-fetch#request-cancellation-with-abortsignal
 export async function fetchWithTimeout(
-  url: RequestInfo | URL,
+  resource: RequestInfo | URL,
   options?: RequestInit,
   timeout = 10000,
 ) {
   const controller = new AbortController();
-  const timeoutId = setTimeout(() => controller.abort(), timeout);
-  return fetch(url, { ...options, signal: controller.signal }).finally(() =>
-    clearTimeout(timeoutId),
-  );
+  const id = setTimeout(() => controller.abort(), timeout);
+  const response = await fetch(resource, {
+    ...options,
+    signal: controller.signal,
+  });
+  clearTimeout(id);
+  return response;
 }
