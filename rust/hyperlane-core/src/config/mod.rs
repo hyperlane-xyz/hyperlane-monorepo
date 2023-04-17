dfcbe368d2fce2d7c6ed5ee064c411514bb2ffd7
Mattie Conover <git@mconover.dev>
2023-04-17 13:58:38 -0700

    Split up config module

---
+++ rust/hyperlane-core/src/config/mod.rs
@@ -0,0 +1,118 @@
+//! A set of traits and types to make configuration parsing easier. The main
+//! flow is to define a config struct and then a raw config struct which mirrors
+//! it but is more forgiving for the deserialization, and then to implement
+//! `FromRawConf` which will allow for better error messages.
+
+use std::fmt::{Debug, Display, Formatter};
+
+use eyre::Report;
+use serde::Deserialize;
+
+pub use config_path::ConfigPath;
+pub use str_or_int::{StrOrInt, StrOrIntParseError};
+pub use trait_ext::*;
+
+mod config_path;
+mod str_or_int;
+mod trait_ext;
+
+/// A result type that is used for config parsing and may contain multiple
+/// errors.
+pub type ConfigResult<T> = Result<T, ConfigParsingError>;
+
+/// A trait that allows for constructing `Self` from a raw config type.
+pub trait FromRawConf<'de, T, F = ()>: Sized
+where
+    // technically we don't need this bound but it enforces
+    // the correct usage.
+    T: Debug + Deserialize<'de>,
+    F: Default,
+{
+    /// Construct `Self` from a raw config type.
+    fn from_config(raw: T, cwp: &ConfigPath) -> ConfigResult<Self> {
+        Self::from_config_filtered(raw, cwp, F::default())
+    }
+
+    /// Construct `Self` from a raw config type with a filter to limit what
+    /// config paths are used.
+    fn from_config_filtered(raw: T, cwp: &ConfigPath, filter: F) -> ConfigResult<Self>;
+}
+
+/// A trait that allows for converting a raw config type into a "parsed" type.
+pub trait IntoParsedConf<'de, F: Default>: Debug + Deserialize<'de> {
+    /// Parse the config with a filter to limit what config paths are used.
+    fn parse_config_with_filter<O: FromRawConf<'de, Self, F>>(
+        self,
+        cwp: &ConfigPath,
+        filter: F,
+    ) -> ConfigResult<O>;
+
+    /// Parse the config.
+    fn parse_config<O: FromRawConf<'de, Self, F>>(self, cwp: &ConfigPath) -> ConfigResult<O> {
+        self.parse_config_with_filter(cwp, F::default())
+    }
+}
+
+impl<'de, S, F> IntoParsedConf<'de, F> for S
+where
+    S: Deserialize<'de> + Debug,
+    F: Default,
+{
+    fn parse_config_with_filter<O: FromRawConf<'de, S, F>>(
+        self,
+        cwp: &ConfigPath,
+        filter: F,
+    ) -> ConfigResult<O> {
+        O::from_config_filtered(self, cwp, filter)
+    }
+}
+
+/// A composite error type that allows for compiling multiple errors into a
+/// single result. Use `default()` to create an empty error and then take other
+/// errors using the extension traits or directly push them.
+#[must_use]
+#[derive(Debug, Default)]
+pub struct ConfigParsingError(Vec<(ConfigPath, Report)>);
+
+impl ConfigParsingError {
+    /// Add a new error to the list.
+    pub fn push(&mut self, conf_path: ConfigPath, report: Report) {
+        self.0.push((conf_path, report));
+    }
+
+    /// Merge all the individual errors from two `ConfigParsingErrors`.
+    pub fn merge(&mut self, other: Self) {
+        self.0.extend(other.0);
+    }
+
+    /// Convert this error into a result, returning `Ok(())` if there are no
+    /// errors.
+    pub fn into_result(self) -> ConfigResult<()> {
+        if self.0.is_empty() {
+            Ok(())
+        } else {
+            Err(self)
+        }
+    }
+}
+
+impl FromIterator<ConfigParsingError> for ConfigParsingError {
+    fn from_iter<T: IntoIterator<Item = ConfigParsingError>>(iter: T) -> Self {
+        Self(iter.into_iter().flat_map(|e| e.0).collect())
+    }
+}
+
+impl Display for ConfigParsingError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        writeln!(f, "ParsingError")?;
+        for (path, report) in &self.0 {
+            writeln!(f, "\n#####\n")?;
+            writeln!(f, "config_path: `{path}`")?;
+            writeln!(f, "env_path: `{}`", path.env_name())?;
+            writeln!(f, "error: {report:?}")?;
+        }
+        Ok(())
+    }
+}
+
+impl std::error::Error for ConfigParsingError {}
---
+++ rust/hyperlane-core/src/config/str_or_int.rs
@@ -0,0 +1,109 @@
+use serde::Deserialize;
+use primitive_types::U256;
+use std::fmt::{Debug, Formatter};
+use thiserror::Error;
+use std::num::{ParseIntError, TryFromIntError};
+
+/// A type which can be used for parsing configs that may be provided as a
+/// string or an integer but will ultimately be read as an integer. E.g. where
+/// `"domain": "42"` and `"domain": 42` should both be considered valid.
+#[derive(Clone, Deserialize)]
+#[serde(untagged)]
+pub enum StrOrInt {
+    /// The parsed type is a string
+    Str(String),
+    /// The parsed type is an integer
+    Int(i64),
+}
+
+impl Debug for StrOrInt {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            StrOrInt::Str(v) => write!(f, "\"{v}\""),
+            StrOrInt::Int(v) => write!(f, "{}", *v),
+        }
+    }
+}
+
+impl From<i64> for StrOrInt {
+    fn from(value: i64) -> Self {
+        StrOrInt::Int(value)
+    }
+}
+
+impl From<String> for StrOrInt {
+    fn from(value: String) -> Self {
+        StrOrInt::Str(value)
+    }
+}
+
+impl From<&str> for StrOrInt {
+    fn from(value: &str) -> Self {
+        StrOrInt::Str(value.to_owned())
+    }
+}
+
+macro_rules! convert_to {
+    ($t:ty) => {
+        impl TryFrom<StrOrInt> for $t {
+            type Error = StrOrIntParseError;
+
+            fn try_from(v: StrOrInt) -> Result<Self, Self::Error> {
+                (&v).try_into()
+            }
+        }
+
+        impl TryFrom<&StrOrInt> for $t {
+            type Error = StrOrIntParseError;
+
+            fn try_from(v: &StrOrInt) -> Result<Self, Self::Error> {
+                Ok(match v {
+                    StrOrInt::Str(s) => s.parse()?,
+                    StrOrInt::Int(i) => (*i).try_into()?,
+                })
+            }
+        }
+    };
+}
+
+convert_to!(u16);
+convert_to!(u32);
+convert_to!(u64);
+
+
+impl TryFrom<StrOrInt> for U256 {
+    type Error = StrOrIntParseError;
+
+    fn try_from(v: StrOrInt) -> Result<Self, Self::Error> {
+        (&v).try_into()
+    }
+}
+
+impl TryFrom<&StrOrInt> for U256 {
+    type Error = StrOrIntParseError;
+
+    fn try_from(v: &StrOrInt) -> Result<Self, Self::Error> {
+        Ok(match v {
+            StrOrInt::Str(s) => s.parse().map_err(|_| {
+                StrOrIntParseError::Other(format!("Unable to parse U256 string ({s})"))
+            })?,
+            StrOrInt::Int(i) => (*i).try_into().map_err(|_| {
+                StrOrIntParseError::Other(format!("Unable to parse integer as U256 ({i})"))
+            })?,
+        })
+    }
+}
+
+/// An error when parsing a StrOrInt type as an integer value.
+#[derive(Error, Debug)]
+pub enum StrOrIntParseError {
+    /// The string is not a valid integer
+    #[error("Invalid integer provided as a string: {0}")]
+    StrParse(#[from] ParseIntError),
+    /// The provided integer does not match the type requirements.
+    #[error("Provided number is an invalid integer: {0}")]
+    InvalidInt(#[from] TryFromIntError),
+    /// Some other error occured.
+    #[error("Could not parse integer: {0}")]
+    Other(String),
+}
