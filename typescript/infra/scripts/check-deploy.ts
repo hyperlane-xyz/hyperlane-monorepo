087984fd18bfcbd873c4049814da399fd2a5aabd
Asa Oines <asaoines@gmail.com>
2023-04-20 12:28:49 -0400

    Set testnet3 ICAs to use proper ISMs

--- typescript/infra/scripts/check-deploy.ts
+++ typescript/infra/scripts/check-deploy.ts
@@ -59,14 +59,22 @@ async function check() {
     const checker = new HyperlaneIgpChecker(multiProvider, igp, config.igp);
     governor = new HyperlaneIgpGovernor(checker, config.owners);
   } else if (module === Modules.INTERCHAIN_ACCOUNTS) {
-    const config = await getRouterConfig(environment, multiProvider);
+    const routerConfig = await getRouterConfig(environment, multiProvider);
     const ica = InterchainAccount.fromEnvironment(env, multiProvider);
-    const checker = new InterchainAccountChecker(multiProvider, ica, config);
+    const checker = new InterchainAccountChecker(
+      multiProvider,
+      ica,
+      routerConfig,
+    );
     governor = new ProxiedRouterGovernor(checker, config.owners);
   } else if (module === Modules.INTERCHAIN_QUERY_SYSTEM) {
-    const config = await getRouterConfig(environment, multiProvider);
+    const routerConfig = await getRouterConfig(environment, multiProvider);
     const iqs = InterchainQuery.fromEnvironment(env, multiProvider);
-    const checker = new InterchainQueryChecker(multiProvider, iqs, config);
+    const checker = new InterchainQueryChecker(
+      multiProvider,
+      iqs,
+      routerConfig,
+    );
     governor = new ProxiedRouterGovernor(checker, config.owners);
   } else {
     console.log(`Skipping ${module}, checker or governor unimplemented`);
