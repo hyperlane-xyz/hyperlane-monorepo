49b345854b65d29134171836ea1e974c0e707b97
Asa Oines <asaoines@gmail.com>
2023-04-24 13:46:53 -0400

    Add static fromAddressesMap constructor to apps (#2136)
    ### Description
    
    Adds a third constructor. Allows apps to be constructed from:
    - Environment name
    - ContractsMap
    - AddressesMap
     
    ### Drive-by changes
    
    None

--- typescript/sdk/src/gas/HyperlaneIgp.ts
+++ typescript/sdk/src/gas/HyperlaneIgp.ts
@@ -8,6 +8,7 @@ import {
   HyperlaneEnvironment,
   hyperlaneEnvironments,
 } from '../consts/environments';
+import { HyperlaneAddressesMap, appFromAddressesMapHelper } from '../contracts';
 import { MultiProvider } from '../providers/MultiProvider';
 import { ChainName } from '../types';
 
@@ -22,15 +23,19 @@ export class HyperlaneIgp extends HyperlaneApp<IgpFactories> {
     if (!envAddresses) {
       throw new Error(`No addresses found for ${env}`);
     }
-    const fromAddressesMap = this.fromAddressesMap(
-      envAddresses,
+    return HyperlaneIgp.fromAddressesMap(envAddresses, multiProvider);
+  }
+
+  static fromAddressesMap(
+    addressesMap: HyperlaneAddressesMap<any>,
+    multiProvider: MultiProvider,
+  ): HyperlaneIgp {
+    const helper = appFromAddressesMapHelper(
+      addressesMap,
       igpFactories,
       multiProvider,
     );
-    return new HyperlaneIgp(
-      fromAddressesMap.contractsMap,
-      fromAddressesMap.multiProvider,
-    );
+    return new HyperlaneIgp(helper.contractsMap, helper.multiProvider);
   }
 
   /**
