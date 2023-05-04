03903f2bdda37f76c9e2c2659093c8ba70a251db
Yorke Rhodes <yorke@hyperlane.xyz>
2023-05-04 16:52:29 -0400

    Optimize calldata usage and memory expansion

--- solidity/contracts/libs/isms/MultisigIsmMetadata.sol
+++ solidity/contracts/libs/isms/MultisigIsmMetadata.sol
@@ -3,29 +3,26 @@ pragma solidity >=0.8.0;
 
 /**
  * Format of metadata:
- * [   0:  32] Merkle root
- * [  32:  36] Root index
- * [  36:  68] Origin mailbox address
- * [  68:  100] Message ID (leaf at root index in merkle tree)
- * [  100:????] Validator signatures, 65 bytes each, length == Threshold
- * [????:????] (Optional) Merkle proof
+ * [   0:   4] Root index
+ * [   4:  36] Origin mailbox address
+ * [  36:  37] Merkle type
+ * [  36:  VS] Validator signatures (where VS = 36 + Threshold * 65 bytes)
+ *
+ * [  VS:????] Merkle root
+ *                OR
+ * [  VS:????] (Message ID, Merkle proof)
  */
 library MultisigIsmMetadata {
-    uint256 private constant MERKLE_ROOT_OFFSET = 0;
-    uint256 private constant MERKLE_INDEX_OFFSET = 32;
-    uint256 private constant ORIGIN_MAILBOX_OFFSET = 36;
-    uint256 private constant MESSAGE_ID_OFFSET = 68;
-    uint256 private constant SIGNATURES_OFFSET = 100;
-    uint256 private constant SIGNATURE_LENGTH = 65;
-    uint256 private constant PROOF_LENGTH = 32 * 32;
+    uint8 private constant MERKLE_INDEX_OFFSET = 0;
+    uint8 private constant ORIGIN_MAILBOX_OFFSET = 4;
+    uint8 private constant MERKLE_TYPE_OFFSET = 36;
+    uint8 private constant SIGNATURES_OFFSET = 37;
+    uint8 private constant SIGNATURE_LENGTH = 65;
+    uint16 private constant PROOF_LENGTH = 32 * 32;
 
-    /**
-     * @notice Returns the merkle root of the signed checkpoint.
-     * @param _metadata ABI encoded Multisig ISM metadata.
-     * @return Merkle root of the signed checkpoint
-     */
-    function root(bytes calldata _metadata) internal pure returns (bytes32) {
-        return bytes32(_metadata[MERKLE_ROOT_OFFSET:MERKLE_INDEX_OFFSET]);
+    enum MerkleType {
+        ROOT,
+        ID_AND_PROOF
     }
 
     /**
@@ -36,7 +33,7 @@ library MultisigIsmMetadata {
     function index(bytes calldata _metadata) internal pure returns (uint32) {
         return
             uint32(
-                bytes4(_metadata[MERKLE_INDEX_OFFSET:ORIGIN_MAILBOX_OFFSET])
+                bytes4(_metadata[MERKLE_INDEX_OFFSET:MERKLE_INDEX_OFFSET + 4])
             );
     }
 
@@ -50,43 +47,18 @@ library MultisigIsmMetadata {
         pure
         returns (bytes32)
     {
-        return bytes32(_metadata[ORIGIN_MAILBOX_OFFSET:MESSAGE_ID_OFFSET]);
+        return
+            bytes32(
+                _metadata[ORIGIN_MAILBOX_OFFSET:ORIGIN_MAILBOX_OFFSET + 32]
+            );
     }
 
-    function messageId(bytes calldata _metadata)
+    function merkleType(bytes calldata _metadata)
         internal
         pure
-        returns (bytes32)
+        returns (MerkleType)
     {
-        return bytes32(_metadata[MESSAGE_ID_OFFSET:SIGNATURES_OFFSET]);
-    }
-
-    function proofOffset(uint256 threshold) private pure returns (uint256) {
-        return SIGNATURES_OFFSET + (threshold * SIGNATURE_LENGTH);
-    }
-
-    function hasProof(bytes calldata _metadata, uint256 threshold)
-        internal
-        pure
-        returns (bool)
-    {
-        return _metadata.length > proofOffset(threshold);
-    }
-
-    /**
-     * @notice Returns the merkle proof branch of the message.
-     * @dev This appears to be more gas efficient than returning a calldata
-     * slice and using that.
-     * @param _metadata ABI encoded Multisig ISM metadata.
-     * @param _threshold Used to determine offset from signatures.
-     * @return Merkle proof branch of the message.
-     */
-    function proof(bytes calldata _metadata, uint256 _threshold)
-        internal
-        pure
-        returns (bytes32[32] memory)
-    {
-        return abi.decode(_metadata[proofOffset(_threshold):], (bytes32[32]));
+        return MerkleType(uint8(_metadata[MERKLE_TYPE_OFFSET]));
     }
 
     /**
@@ -106,5 +78,28 @@ library MultisigIsmMetadata {
         uint256 _start = SIGNATURES_OFFSET + (_index * SIGNATURE_LENGTH);
         uint256 _end = _start + SIGNATURE_LENGTH;
         return _metadata[_start:_end];
+    }
+
+    /**
+     * @notice Returns the merkle root of the signed checkpoint.
+     * @param _metadata ABI encoded Multisig ISM metadata.
+     * @return Merkle root of the signed checkpoint
+     */
+    function root(bytes calldata _metadata) internal pure returns (bytes32) {
+        assert(merkleType(_metadata) == MerkleType.ROOT);
+        return bytes32(_metadata[_metadata.length - 32:]);
+    }
+
+    function idAndProof(bytes calldata _metadata)
+        internal
+        pure
+        returns (bytes32, bytes32[32] memory)
+    {
+        assert(merkleType(_metadata) == MerkleType.ID_AND_PROOF);
+        return
+            abi.decode(
+                _metadata[_metadata.length - (PROOF_LENGTH + 32):],
+                (bytes32, bytes32[32])
+            );
     }
 }
