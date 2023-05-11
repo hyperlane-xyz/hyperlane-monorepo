b72ade21f02a7bbfd8379d0ced353e4e563cda67
Mattie Conover <git@mconover.dev>
2023-05-11 13:30:39 -0700

    Update keys

--- rust/utils/run-locally/src/main.rs
+++ rust/utils/run-locally/src/main.rs
@@ -32,7 +32,18 @@ use nix::{
     sys::signal::{self, Signal},
     unistd::Pid,
 };
-use tempfile::{tempdir};
+use tempfile::tempdir;
+
+const RELAYER_KEYS: &[&str] = &[
+    "8166f546bab6da521a8369cab06c5d2b9e46670292d85c875ee9ec20e84ffb61",
+    "f214f2b2cd398c806f84e317254e0f0b801d0643303237d97a22a48e01628897",
+    "701b615bbdfb9de65240bc28bd21bbc0d996645a3dd57e7b12bc2bdf6f192c82",
+];
+const VALIDATOR_KEYS: &[&str] = &[
+    "59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d",
+    "5de4111afa1a4b94908f83103eb1f1706367c2e68ca870fc3fb9a804cdab365a",
+    "7c852118294e51e653712a81e05800f419141751be58f605c371e15141b007a6",
+];
 
 static RUNNING: AtomicBool = AtomicBool::new(true);
 
@@ -156,10 +167,9 @@ fn main() -> ExitCode {
         "HYP_BASE_CHAINS_TEST3_CONNECTION_URL" => "http://127.0.0.1:8545",
         "HYP_BASE_METRICS" => "9092",
         "HYP_BASE_DB" => relayer_db.to_str().unwrap(),
-        "HYP_BASE_CHAINS_TEST1_SIGNER_KEY" => "8166f546bab6da521a8369cab06c5d2b9e46670292d85c875ee9ec20e84ffb61",
-        "HYP_BASE_CHAINS_TEST2_SIGNER_KEY" => "f214f2b2cd398c806f84e317254e0f0b801d0643303237d97a22a48e01628897",
+        "HYP_BASE_CHAINS_TEST1_SIGNER_KEY" => RELAYER_KEYS[0],
+        "HYP_BASE_CHAINS_TEST2_SIGNER_KEY" => RELAYER_KEYS[1],
         "HYP_BASE_RELAYCHAINS" => "invalidchain,otherinvalid",
-        "HYP_RELAYER_WHITELIST" => r#"[{"senderAddress": "*", "destinationDomain": [13372, 13373], "recipientAddress": "*"}]"#,
         "HYP_RELAYER_ALLOWLOCALCHECKPOINTSYNCERS" => "true",
     };
 
@@ -167,7 +177,7 @@ fn main() -> ExitCode {
     let relayer_args = [
         "--chains.test1.connection.urls=\"http://127.0.0.1:8545,http://127.0.0.1:8545,http://127.0.0.1:8545\"",
         // default is used for TEST3
-        "--defaultSigner.key", "701b615bbdfb9de65240bc28bd21bbc0d996645a3dd57e7b12bc2bdf6f192c82",
+        "--defaultSigner.key", RELAYER_KEYS[2],
         "--relayChains=test1,test2,test3",
     ];
 
@@ -182,7 +192,7 @@ fn main() -> ExitCode {
             "HYP_BASE_CHAINS_TEST3_CONNECTION_URL" => "http://127.0.0.1:8545",
             "HYP_BASE_METRICS" => metrics_port,
             "HYP_VALIDATOR_ORIGINCHAINNAME" => originchainname,
-            "HYP_VALIDATOR_VALIDATOR_KEY" => "59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d",
+            "HYP_VALIDATOR_VALIDATOR_KEY" => VALIDATOR_KEYS[i],
             "HYP_VALIDATOR_REORGPERIOD" => "0",
             "HYP_VALIDATOR_INTERVAL" => "5",
             "HYP_VALIDATOR_CHECKPOINTSYNCER_TYPE" => "localStorage",
