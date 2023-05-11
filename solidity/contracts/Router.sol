318fd11f211dc90d32fa4cebcb1a7bd7a56c048d
vinhyenvodoi98 <vinhyenvodoi98@gmail.com>
2023-05-11 23:15:45 +0900

    test show diff

--- solidity/contracts/Router.sol
+++ solidity/contracts/Router.sol
@@ -17,6 +17,7 @@ abstract contract Router is HyperlaneConnectionClient, IMessageRecipient {
     // ============ Mutable Storage ============
     EnumerableMapExtended.UintToBytes32Map internal _routers;
     uint256[49] private __GAP; // gap for upgrade safety
+    uint256 public Testing = 1;
 
     // ============ Events ============
 
