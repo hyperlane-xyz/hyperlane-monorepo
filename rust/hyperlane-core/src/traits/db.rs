a55229cbe3968ea731bfa6201a7c2e14a12e1eea
Asa Oines <asaoines@gmail.com>
2023-05-14 08:19:23 -0400

    Add HyperlaneDb trait

---
+++ rust/hyperlane-core/src/traits/db.rs
@@ -0,0 +1,29 @@
+use std::fmt::Debug;
+
+use async_trait::async_trait;
+use auto_impl::auto_impl;
+
+use crate::{HyperlaneMessage, InterchainGasPayment, LogMeta, H256};
+
+/// Interface for a HyperlaneDb. 
+#[async_trait]
+#[auto_impl(&, Box, Arc)]
+pub trait HyperlaneDb<E>: Send + Sync + Debug {
+    /// Store a list of dispatched messages and their associated metadata.
+    fn store_dispatched_messages(
+        &self,
+        messages: &[(HyperlaneMessage, LogMeta)],
+    ) -> Result<u32, E>;
+
+    /// Store a list of delivered messages and their associated metadata.
+    fn store_delivered_messages(
+        &self,
+        deliveries: &[(H256, LogMeta)],
+    ) -> Result<u32, E>;
+
+    /// Store a list of interchain gas payments and their associated metadata.
+    fn store_gas_payment(
+        &self,
+        payments: &[(InterchainGasPayment, LogMeta)],
+    ) -> Result<bool, E>;
+}
