a98dde07d7258713ecd09ef283b3f2589a41bea3
Trevor Porter <trkporter@ucdavis.edu>
2023-05-15 12:42:47 +0100

    First test set_validators_and_threshold_already_initialized works

--- rust/sealevel/programs/ism/multisig-ism-message-id/src/processor.rs
+++ rust/sealevel/programs/ism/multisig-ism-message-id/src/processor.rs
@@ -211,8 +211,6 @@ fn set_validators_and_threshold(
     let domain_data =
         DomainDataAccount::fetch_data(&mut &domain_pda_account.data.borrow_mut()[..])?;
 
-    println!("domain_data {:?} domain_pda_account {:?}", domain_data, domain_pda_account);
-
     let bump_seed = match domain_data {
         Some(domain_data) => {
             // The PDA account exists already, we need to confirm the key of the domain_pda_account
@@ -304,83 +302,53 @@ mod test {
         let mut domain_account_lamports = 0;
         let mut domain_account_data = vec![0_u8; 2048];
         let domain_pda_account = AccountInfo::new(
-            // Public key of the account
             &domain_pda_key,
-            // Was the transaction signed by this account's public key?
             false,
-            // Is the account writable?
             true,
-            // The lamports in the account. Modifiable by programs.
             &mut domain_account_lamports,
-            // The data held in this account. Modifiable by programs.
             &mut domain_account_data,
-            // Program that owns this account.
             &program_id,
-            // This account's data contains a loaded program (and is now read-only).
             false,
-            // The epoch at which this account will next owe rent.
             Epoch::default(),
         );
-        {
-            let init_data = DomainData {
-                bump_seed: domain_pda_bump_seed,
-                validators_and_threshold: ValidatorsAndThreshold {
-                    validators: vec![H160::random()],
-                    threshold: 1,
-                },
-            };
-            DomainDataAccount::from(init_data)
-                .store(&domain_pda_account, false)
-                .unwrap();
-        }
-
-        println!("yooo {:?} {:?}", domain_pda_account, domain_pda_account.data);
+        let init_data = DomainData {
+            bump_seed: domain_pda_bump_seed,
+            validators_and_threshold: ValidatorsAndThreshold {
+                validators: vec![H160::random()],
+                threshold: 1,
+            },
+        };
+        DomainDataAccount::from(init_data)
+            .store(&domain_pda_account, false)
+            .unwrap();
 
         let owner_key = Pubkey::new_unique();
         let mut owner_account_lamports = 0;
         let mut owner_account_data = vec![];
         let system_program_id = solana_program::system_program::id();
         let owner_account = AccountInfo::new(
-            // Public key of the account
             &owner_key,
-            // Was the transaction signed by this account's public key?
             true,
-            // Is the account writable?
             false,
-            // The lamports in the account. Modifiable by programs.
             &mut owner_account_lamports,
-            // The data held in this account. Modifiable by programs.
             &mut owner_account_data,
-            // Program that owns this account.
             &system_program_id,
-            // This account's data contains a loaded program (and is now read-only).
             false,
-            // The epoch at which this account will next owe rent.
             Epoch::default(),
         );
 
         let mut program_lamports = 0;
         let mut program_data = vec![];
         let program_account = AccountInfo::new(
-            // Public key of the account
             &program_id,
-            // Was the transaction signed by this account's public key?
             false,
-            // Is the account writable?
             false,
-            // The lamports in the account. Modifiable by programs.
             &mut program_lamports,
-            // The data held in this account. Modifiable by programs.
             &mut program_data,
-            // Owner of this account.
             &owner_key,
-            // This account's data contains a loaded program (and is now read-only).
             true,
-            // The epoch at which this account will next owe rent.
             Epoch::default(),
         );
-
-        println!("domain_account_data before {:?}", domain_pda_account.data.borrow());
 
         let config = Domained {
             domain,
@@ -392,15 +360,10 @@ mod test {
 
         let accounts = vec![owner_account, program_account, domain_pda_account];
 
-        // set_validators_and_threshold(&program_id, &[owner_account, program_account, domain_pda_account.clone()], config.clone())
-        //     .unwrap();
         set_validators_and_threshold(&program_id, &accounts, config.clone())
             .unwrap();
 
-        // println!("domain_account_data after {:?}", domain_account_data);
-        println!("domain_pda_account.data after {:?}", accounts[2].data.borrow());
-// &mut &accounts[0].data.borrow_mut()[..]
-        let domain_data = DomainDataAccount::fetch_data(&mut &accounts[2].data.borrow_mut()[..]).unwrap().unwrap();
+        let domain_data = DomainDataAccount::fetch_data(&mut &accounts[2].try_borrow_data().unwrap()[..]).unwrap().unwrap();
         assert_eq!(domain_data, Box::new(DomainData {
             bump_seed: domain_pda_bump_seed,
             validators_and_threshold: config.data,
