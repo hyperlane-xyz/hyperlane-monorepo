4e4e1f1de28e9a1640e7c8918d4c1bb2fbcd2b35
Trevor Porter <trkporter@ucdavis.edu>
2023-05-19 17:44:11 +0100

    Move over rubber stamp ISM too

--- rust/sealevel/programs/ism/rubber-stamp/src/lib.rs
+++ rust/sealevel/programs/ism/rubber-stamp/src/lib.rs
@@ -5,19 +5,12 @@
 // #![deny(missing_docs)] // FIXME
 #![deny(unsafe_code)]
 
-use std::str::FromStr as _;
-
 use borsh::BorshSerialize;
 use hyperlane_core::IsmType;
 use hyperlane_sealevel_interchain_security_module_interface::InterchainSecurityModuleInstruction;
 use solana_program::{
-    account_info::{next_account_info, AccountInfo},
-    entrypoint,
-    entrypoint::ProgramResult,
-    msg,
-    program::set_return_data,
-    program_error::ProgramError,
-    pubkey::Pubkey,
+    account_info::AccountInfo, entrypoint, entrypoint::ProgramResult, msg,
+    program::set_return_data, program_error::ProgramError, pubkey::Pubkey,
 };
 
 // FIXME Read these in at compile time? And don't use harcoded test keys.
@@ -26,32 +19,20 @@ solana_program::declare_id!("F6dVnLFioQ8hKszqPsmjWPwHn2dJfebgMfztWrzL548V");
 #[cfg(not(feature = "no-entrypoint"))]
 entrypoint!(process_instruction);
 
-const AUTHORITY: &str = "G9CdDjMs6dEd3Tv5eG2ZXo8iKksLRcbHTxpS41sEmX1g";
-
 const ISM_TYPE: IsmType = IsmType::None;
 
 pub fn process_instruction(
     _program_id: &Pubkey,
-    accounts: &[AccountInfo],
+    _accounts: &[AccountInfo],
     instruction_data: &[u8],
 ) -> ProgramResult {
     match InterchainSecurityModuleInstruction::decode(instruction_data)? {
         InterchainSecurityModuleInstruction::Verify(_) => {
-            // TODO: this actually doesn't need to have *any* verification,
-            // but whatever
-
-            let accounts_iter = &mut accounts.iter();
-            let hyperlane_mailbox_auth = next_account_info(accounts_iter)?;
-            if hyperlane_mailbox_auth.key != &Pubkey::from_str(AUTHORITY).unwrap() {
-                return Err(ProgramError::InvalidArgument);
-            }
-            if !hyperlane_mailbox_auth.is_signer {
-                return Err(ProgramError::MissingRequiredSignature);
-            }
-            if accounts_iter.next().is_some() {
-                return Err(ProgramError::InvalidArgument);
-            }
             msg!("hyperlane-sealevel-ism-rubber-stamp: LGTM!");
+            Ok(())
+        }
+        InterchainSecurityModuleInstruction::VerifyAccountMetas(_) => {
+            // No accounts needed!
             Ok(())
         }
         InterchainSecurityModuleInstruction::Type => {
