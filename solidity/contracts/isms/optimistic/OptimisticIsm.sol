3887d599ffb0cab7dbba4599428fb2297048c1bd
joe dakwa <joedakwa@hotmail.co.uk>
2023-05-26 10:44:05 +0100

    added modifiers

--- solidity/contracts/isms/optimistic/OptimisticIsm.sol
+++ solidity/contracts/isms/optimistic/OptimisticIsm.sol
@@ -11,31 +11,82 @@ import {Message} from "../../libs/Message.sol";
 // ============ External Imports ============
 import {Address} from "@openzeppelin/contracts/utils/Address.sol";
 import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 
 /**
  * @title OptimisticIsm
  */
 
- contract OptimisticIsm is AbstractOptimisticIsm, OwnableUpgradeable {
+ contract OptimisticIsm is AbstractOptimisticIsm, Initializable, OwnableUpgradeable {
 
 
     address public submodule;
     uint256 public fraudWindowDuration;
+    bool public isConfigured;
+    
 
         // ============ Public Storage ============
 
     mapping(address => bool) public compromisedSubmodules;
     mapping(uint32 => IInterchainSecurityModule) public modules;
     mapping(bytes32 => bool) public messageVerification;
+    mapping(address => bool) public authorizedWatchers;
 
-function configureSubmodule(address _submodule, uint256 _fraudWindowDuration) public onlyOwner {
+    modifier onlyAuthorizedWatcher() {
+        require(authorizedWatchers[msg.sender], "OptimisticISM: Not authorized watcher");
+        _;
+    }
+
+    modifier isConfigured() {
+        require(submodule != address(0), "Submodule not configured");
+        _;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner(), "OptimisticISM: Not owner");
+        _;
+    }
+
+    // Initialization function to add initial authorized watchers
+    function initialize(address[] memory initialWatchers) public initializer {
+        __Ownable_init();
+
+        // Add the contract owner as an authorized watcher
+        authorizedWatchers[msg.sender] = true;
+
+        // Add initial watchers provided in the function argument
+        for (uint256 i = 0; i < initialWatchers.length; i++) {
+            authorizedWatchers[initialWatchers[i]] = true;
+        }
+    }
+
+    // Function to add an address to the list of authorized watchers
+    function addWatcher(address watcher) external onlyOwner {
+        authorizedWatchers[watcher] = true;
+    }
+
+    // Function to remove an address from the list of authorized watchers
+    function removeWatcher(address watcher) external onlyOwner {
+        authorizedWatchers[watcher] = false;
+    }
+
+function setFraudWindowDuration(uint256 _duration) external onlyOwner {
+    fraudWindowDuration = _duration;
+}    
+
+function configureSubmodule(address _submodule, uint256 _fraudWindowDuration) external returns onlyOwner (bool) {
     submodule = _submodule;
     fraudWindowDuration = _fraudWindowDuration;
+    isConfigured = true;
+    return true;
+
 }
 
 // watchers can call this function to mark a message as fraudulent
-function markFraudulent(address ism) external {
+function markFraudulent(address ism) external returns isConfigured (bool) {
+    require(ism != address(0), "OptimisticISM: Invalid ISM address");
     compromisedSubmodules[ism] = true;
+    return true;
 }
 
 function preVerify(bytes calldata message, bytes calldata metadata) external returns (bool) {
@@ -65,21 +116,8 @@ function verify(bytes memory message, bytes memory metadata) external returns (b
     require(!compromisedSubmodules[submodule], "Submodule is compromised");
 
     // Ensure that the fraud window has elapsed
-    require(block.timestamp >= fraudWindowStart(message, metadata) + fraudWindowDuration, "Fraud window not elapsed");
+    require(block.timestamp >= block.timestamp + fraudWindowDuration, "Fraud window not elapsed");
 
     return true;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
  }
