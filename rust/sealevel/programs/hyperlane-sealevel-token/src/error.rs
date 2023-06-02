bc18662d6d426ebe8f2dfe8b7c1fd5bb5dae6474
Trevor Porter <trkporter@ucdavis.edu>
2023-06-02 15:02:41 +0100

    Clean up hyperlane-sealevel-token a bit

--- rust/sealevel/programs/hyperlane-sealevel-token/src/error.rs
+++ rust/sealevel/programs/hyperlane-sealevel-token/src/error.rs
@@ -10,27 +10,6 @@ pub enum Error {
 
     #[error("Unused account(s) provided")]
     ExtraneousAccount = 2,
-
-    #[error("Associated token account balance is too low")]
-    AtaBalanceTooLow = 3,
-    // #[error("Hyperlane message is malformatted")]
-    // MalformattedHyperlaneMessage = 2,
-    // #[error("Unsupported message version")]
-    // UnsupportedMessageVersion = 3,
-    // #[error("Incorrect destination domain")]
-    // IncorrectDestinationDomain = 4,
-    // #[error("Message has already been processed")]
-    // DuplicateMessage = 5,
-    // #[error("Transaction log budget exceeded so cannot emit event")]
-    // LogBudgetExceeded = 7,
-    // #[error("Message is larger than the maximum allowed")]
-    // MaxMessageSizeExceeded = 8,
-    // #[error("Invalid account public key")]
-    // InvalidPubkey = 9,
-    // #[error("Account not found in the correct order")]
-    // AccountOutOfOrder = 10,
-    // #[error("Account is read only")]
-    // AccountReadOnly = 11,
 }
 
 impl From<Error> for ProgramError {
