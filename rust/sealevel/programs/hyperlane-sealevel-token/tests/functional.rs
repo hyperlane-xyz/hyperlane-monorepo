51dd216c1b6c72eb709aa2ef84eda409f343374c
Trevor Porter <trkporter@ucdavis.edu>
2023-06-02 15:05:33 +0100

    Starting the endeavor to clean/beef up hyperlane-sealevel-token tests

--- rust/sealevel/programs/hyperlane-sealevel-token/tests/functional.rs
+++ rust/sealevel/programs/hyperlane-sealevel-token/tests/functional.rs
@@ -3,17 +3,10 @@
 //! new PDA accounts.
 
 use hyperlane_core::{Encode, HyperlaneMessage, H256};
-use solana_program::{
-    instruction::{AccountMeta, Instruction},
-    pubkey::Pubkey,
-    system_program,
-};
-
 use hyperlane_sealevel_connection_client::router::RemoteRouterConfig;
 use hyperlane_sealevel_mailbox::{
-    instruction::{InboxProcess, Init as InitMailbox, Instruction as MailboxInstruction},
-    mailbox_dispatched_message_pda_seeds, mailbox_inbox_pda_seeds,
-    mailbox_message_dispatch_authority_pda_seeds, mailbox_outbox_pda_seeds,
+    instruction::{InboxProcess, Instruction as MailboxInstruction},
+    mailbox_dispatched_message_pda_seeds, mailbox_message_dispatch_authority_pda_seeds,
     mailbox_process_authority_pda_seeds, mailbox_processed_message_pda_seeds,
 };
 use hyperlane_sealevel_token::{
@@ -25,91 +18,16 @@ use hyperlane_sealevel_token_lib::{
     instruction::{Init, Instruction as HyperlaneTokenInstruction, TransferRemote},
     message::TokenMessage,
 };
+use hyperlane_test_utils::{initialize_mailbox, new_funded_keypair, transfer_lamports};
+use solana_program::{
+    instruction::{AccountMeta, Instruction},
+    pubkey::Pubkey,
+};
 use solana_program_test::*;
 use solana_sdk::{signature::Signer, signer::keypair::Keypair, transaction::Transaction};
 use spl_token_2022::{
     extension::StateWithExtensions, instruction::initialize_mint2, state::Account,
 };
-
-async fn new_funded_keypair(
-    banks_client: &mut BanksClient,
-    payer: &Keypair,
-    lamports: u64,
-) -> Keypair {
-    let keypair = Keypair::new();
-    transfer_lamports(banks_client, payer, &keypair.pubkey(), lamports).await;
-    keypair
-}
-
-async fn transfer_lamports(
-    banks_client: &mut BanksClient,
-    payer: &Keypair,
-    to: &Pubkey,
-    lamports: u64,
-) {
-    let recent_blockhash = banks_client.get_latest_blockhash().await.unwrap();
-    let mut transaction = Transaction::new_with_payer(
-        &[solana_sdk::system_instruction::transfer(
-            &payer.pubkey(),
-            to,
-            lamports,
-        )],
-        Some(&payer.pubkey()),
-    );
-    transaction.sign(&[payer], recent_blockhash);
-    banks_client.process_transaction(transaction).await.unwrap();
-}
-
-struct MailboxAccounts {
-    program: Pubkey,
-    #[allow(dead_code)]
-    inbox: Pubkey,
-    outbox: Pubkey,
-}
-
-async fn initialize_mailbox(
-    banks_client: &mut BanksClient,
-    mailbox_program_id: &Pubkey,
-    payer: &Keypair,
-    local_domain: u32,
-) -> MailboxAccounts {
-    let (inbox_account, inbox_bump) =
-        Pubkey::find_program_address(mailbox_inbox_pda_seeds!(), mailbox_program_id);
-    let (outbox_account, outbox_bump) =
-        Pubkey::find_program_address(mailbox_outbox_pda_seeds!(), mailbox_program_id);
-
-    let ixn = MailboxInstruction::Init(InitMailbox {
-        local_domain,
-        inbox_bump_seed: inbox_bump,
-        outbox_bump_seed: outbox_bump,
-    });
-    let init_instruction = Instruction {
-        program_id: *mailbox_program_id,
-        data: ixn.into_instruction_data().unwrap(),
-        accounts: vec![
-            AccountMeta::new(system_program::id(), false),
-            AccountMeta::new(payer.pubkey(), true),
-            AccountMeta::new(inbox_account, false),
-            AccountMeta::new(outbox_account, false),
-        ],
-    };
-
-    let recent_blockhash = banks_client.get_latest_blockhash().await.unwrap();
-    let mut transaction = Transaction::new_signed_with_payer(
-        &[init_instruction],
-        Some(&payer.pubkey()),
-        &[payer],
-        recent_blockhash,
-    );
-    transaction.sign(&[payer], recent_blockhash);
-    banks_client.process_transaction(transaction).await.unwrap();
-
-    MailboxAccounts {
-        program: *mailbox_program_id,
-        inbox: inbox_account,
-        outbox: outbox_account,
-    }
-}
 
 #[tokio::test]
 async fn test_initialize() {
