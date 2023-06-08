cbbd84ed69b6c4bed36d2d9e5f5477e7783eeb88
Trevor Porter <trkporter@ucdavis.edu>
2023-06-08 10:14:50 +0100

    Update sealevel multisig ism to be compatible with CheckpointWithMessageId (#2362)
    ### Description
    
    Fixes #2347, changes tests as required
    
    ### Drive-by changes
    
    no
    
    ### Related issues
    
    - Fixes #2347 
    
    ### Backward compatibility
    
    _Are these changes backward compatible?_
    
    Yes
    
    _Are there any infrastructure implications, e.g. changes that would
    prohibit deploying older commits using this infra tooling?_
    
    None
    
    
    ### Testing
    
    _What kind of testing have these changes undergone?_
    
    Unit Tests

--- rust/sealevel/hyperlane-core/src/types/checkpoint.rs
+++ rust/sealevel/hyperlane-core/src/types/checkpoint.rs
@@ -1,8 +1,11 @@
+use derive_more::Deref;
 use sha3::digest::Update;
 use sha3::{Digest, Keccak256};
 
 use crate::{Signable, H256};
 
+/// An Hyperlane checkpoint
+#[derive(Copy, Clone, Eq, PartialEq, Debug)]
 pub struct Checkpoint {
     /// The mailbox address
     pub mailbox_address: H256,
@@ -12,7 +15,15 @@ pub struct Checkpoint {
     pub root: H256,
     /// The index of the checkpoint
     pub index: u32,
-    /// The ID of the message at the index.
+}
+
+/// A Hyperlane (checkpoint, messageId) tuple
+#[derive(Copy, Clone, Eq, PartialEq, Debug, Deref)]
+pub struct CheckpointWithMessageId {
+    /// existing Hyperlane checkpoint struct
+    #[deref]
+    pub checkpoint: Checkpoint,
+    /// hash of message emitted from mailbox checkpoint.index
     pub message_id: H256,
 }
 
@@ -27,6 +38,24 @@ impl Signable for Checkpoint {
                 .chain(domain_hash(self.mailbox_address, self.mailbox_domain))
                 .chain(self.root)
                 .chain(self.index.to_be_bytes())
+                .finalize()
+                .as_slice(),
+        )
+    }
+}
+
+impl Signable for CheckpointWithMessageId {
+    /// A hash of the checkpoint contents.
+    /// The EIP-191 compliant version of this hash is signed by validators.
+    fn signing_hash(&self) -> H256 {
+        // sign:
+        // domain_hash(mailbox_address, mailbox_domain) || root || index (as u32) || message_id
+        H256::from_slice(
+            Keccak256::new()
+                .chain(domain_hash(self.mailbox_address, self.mailbox_domain))
+                .chain(self.root)
+                .chain(self.index.to_be_bytes())
+                .chain(self.message_id)
                 .finalize()
                 .as_slice(),
         )
