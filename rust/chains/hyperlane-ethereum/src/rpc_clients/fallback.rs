8239a6967b521c1fcdfffb0c84a10bc01d515251
Daniel Savu <23065004+daniel-savu@users.noreply.github.com>
2023-06-08 17:54:25 +0100

    feat(fallback provider): re-prioritize providers on error

--- rust/chains/hyperlane-ethereum/src/rpc_clients/fallback.rs
+++ rust/chains/hyperlane-ethereum/src/rpc_clients/fallback.rs
@@ -1,5 +1,7 @@
 use std::fmt::{Debug, Formatter};
+use std::sync::Arc;
 use std::time::Duration;
+use tokio::sync::RwLock;
 
 use async_trait::async_trait;
 use ethers::providers::{Http, JsonRpcClient, ProviderError};
@@ -13,22 +15,35 @@ use ethers_prometheus::json_rpc_client::PrometheusJsonRpcClient;
 
 use crate::rpc_clients::{categorize_client_response, CategorizedResponse};
 
-/// A provider that bundles multiple providers and attempts to call the first,
-/// then the second, and so on until a response is received.
-#[derive(Clone)]
-pub struct FallbackProvider<T>(
+struct PrioritizedProviders<T> {
     /// Sorted list of providers this provider calls in order of most primary to
     /// most fallback.
-    Vec<T>,
-);
+    providers: Vec<T>,
+    /// List of provider indexes, sorted by reliability
+    priorities: RwLock<Vec<usize>>,
+}
+/// A provider that bundles multiple providers and attempts to call the first,
+/// then the second, and so on until a response is received.
+pub struct FallbackProvider<T>(Arc<PrioritizedProviders<T>>);
+
+impl<T> Clone for FallbackProvider<T> {
+    fn clone(&self) -> Self {
+        Self(self.0.clone())
+    }
+}
 
 impl Debug for FallbackProvider<PrometheusJsonRpcClient<Http>> {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         write!(
             f,
             "FallbackProvider {{ chain_name: {}, hosts: [{}] }}",
-            self.0.get(0).map(|v| v.chain_name()).unwrap_or("None"),
             self.0
+                .providers
+                .get(0)
+                .map(|v| v.chain_name())
+                .unwrap_or("None"),
+            self.0
+                .providers
                 .iter()
                 .map(|v| v.node_host())
                 .collect::<Vec<_>>()
@@ -80,7 +95,13 @@ impl<T> FallbackProviderBuilder<T> {
 
     /// Create a fallback provider.
     pub fn build(self) -> FallbackProvider<T> {
-        FallbackProvider(self.providers)
+        let provider_count = self.providers.len();
+        let prioritized_providers = PrioritizedProviders {
+            providers: self.providers,
+            // The order of `self.providers` gives the initial priority
+            priorities: RwLock::new((0..provider_count).collect()),
+        };
+        FallbackProvider(Arc::new(prioritized_providers))
     }
 }
 
@@ -98,6 +119,10 @@ impl From<FallbackError> for ProviderError {
     }
 }
 
+fn warn_span(idx: usize, provider: &PrometheusJsonRpcClient<Http>) {
+    let _span = warn_span!("request_with_fallback", provider_index=%idx, ?provider).entered();
+}
+
 #[cfg_attr(target_arch = "wasm32", async_trait(?Send))]
 #[cfg_attr(not(target_arch = "wasm32"), async_trait)]
 impl JsonRpcClient for FallbackProvider<PrometheusJsonRpcClient<Http>> {
@@ -110,7 +135,6 @@ impl JsonRpcClient for FallbackProvider<PrometheusJsonRpcClient<Http>> {
         R: DeserializeOwned,
     {
         use CategorizedResponse::*;
-
         let params = serde_json::to_value(params).expect("valid");
 
         let mut errors = vec![];
@@ -119,19 +143,42 @@ impl JsonRpcClient for FallbackProvider<PrometheusJsonRpcClient<Http>> {
             if !errors.is_empty() {
                 sleep(Duration::from_millis(100)).await;
             }
-            for (idx, provider) in self.0.iter().enumerate() {
+            let prioritized_providers: Vec<&_>;
+            {
+                let priorities = self.0.priorities.read().await;
+                prioritized_providers = priorities.iter().map(|i| &self.0.providers[*i]).collect();
+                // Free the read lock
+            }
+            for (idx, &provider) in prioritized_providers.iter().enumerate() {
                 let fut = match params {
                     Value::Null => provider.request(method, ()),
                     _ => provider.request(method, &params),
                 };
 
                 let resp = fut.await;
-                let _span =
-                    warn_span!("request_with_fallback", provider_index=%idx, ?provider).entered();
                 match categorize_client_response(method, resp) {
-                    IsOk(v) => return Ok(serde_json::from_value(v)?),
-                    RetryableErr(e) | RateLimitErr(e) => errors.push(e.into()),
-                    NonRetryableErr(e) => return Err(e.into()),
+                    IsOk(v) => {
+                        return {
+                            warn_span(idx, provider);
+                            Ok(serde_json::from_value(v)?)
+                        }
+                    }
+                    RetryableErr(e) | RateLimitErr(e) => {
+                        {
+                            // De-prioritize the current provider by moving it to the end of the queue
+                            // No need to break out of the loop afterwards because we're looping over a snapshot.
+                            let mut priorities = self.0.priorities.write().await;
+                            let p = priorities.remove(idx);
+                            priorities.push(p);
+                            // Free the write lock
+                        }
+                        warn_span(idx, provider);
+                        errors.push(e.into())
+                    }
+                    NonRetryableErr(e) => {
+                        warn_span(idx, provider);
+                        return Err(e.into());
+                    }
                 }
             }
         }
