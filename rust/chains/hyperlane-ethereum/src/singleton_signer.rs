15d15202b077ec5e68925a7984b30176fc0d2115
Yorke Rhodes <yorke@hyperlane.xyz>
2023-06-13 17:16:19 -0400

    Add retry config method

--- rust/chains/hyperlane-ethereum/src/singleton_signer.rs
+++ rust/chains/hyperlane-ethereum/src/singleton_signer.rs
@@ -19,6 +19,7 @@ type SignTask = (H256, Callback);
 /// made at a time. Mostly useful for the AWS signers.
 pub struct SingletonSigner {
     inner: Signers,
+    retries: usize,
     rx: mpsc::UnboundedReceiver<SignTask>,
 }
 
@@ -62,14 +63,18 @@ impl SingletonSigner {
     pub fn new(inner: Signers) -> (Self, SingletonSignerHandle) {
         let (tx, rx) = mpsc::unbounded_channel::<SignTask>();
         let address = inner.eth_address();
-        (Self { inner, rx }, SingletonSignerHandle { address, tx })
+        (Self { inner, rx, retries: 5 }, SingletonSignerHandle { address, tx })
+    }
+
+    /// Change default (5) retries for signing
+    pub fn config_retries(&mut self, retries: usize) {
+        self.retries = retries;
     }
 
     /// Run this signer's event loop.
     pub async fn run(mut self) {
         while let Some((hash, tx)) = self.rx.recv().await {
-            // retry 5 times
-            let mut retries = 5;
+            let mut retries = self.retries;
             let res = loop {
                 match self.inner.sign_hash(&hash).await {
                     Ok(res) => break Ok(res),
