c1ccd219f4de0e4f512df4b7dbc9b663f0a1b26e
Alex Harley <alex@alexbh.dev>
2023-06-16 13:43:28 +0200

    feat: initial ccip read contract verifying message body via a multi sig

---
+++ solidity/contracts/interfaces/isms/ICcipReadIsm.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT OR Apache-2.0
+pragma solidity >=0.8.0;
+
+import {IInterchainSecurityModule} from "../IInterchainSecurityModule.sol";
+
+interface ICcipReadIsm is IInterchainSecurityModule {
+    /**
+     * @notice Returns the ISM responsible for verifying _message
+     * @dev Can change based on the content of _message
+     * @param _message Formatted Hyperlane message (see Message.sol).
+     * @return module The ISM to use to verify _message
+     */
+    function ccipRead(bytes calldata _message) external view returns (uint256);
+}
---
+++ solidity/test/isms/CcipReadIsm.t.sol
@@ -0,0 +1,131 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+
+import {IMultisigIsm} from "../../contracts/interfaces/isms/IMultisigIsm.sol";
+import {TestMailbox} from "../../contracts/test/TestMailbox.sol";
+import {CcipReadIsm} from "../../contracts/isms/CcipReadIsm.sol";
+import {CheckpointLib} from "../../contracts/libs/CheckpointLib.sol";
+import {StaticMOfNAddressSetFactory} from "../../contracts/libs/StaticMOfNAddressSetFactory.sol";
+import {TypeCasts} from "../../contracts/libs/TypeCasts.sol";
+import {Message} from "../../contracts/libs/Message.sol";
+import {MOfNTestUtils} from "./IsmTestUtils.sol";
+
+contract CcipReadIsmTest is Test {
+    using Message for bytes;
+
+    uint32 constant ORIGIN = 11;
+    CcipReadIsm ism;
+    TestMailbox mailbox;
+
+    using Message for bytes;
+
+    function setUp() public {
+        mailbox = new TestMailbox(ORIGIN);
+    }
+
+    function getMetadata(
+        uint8 m,
+        uint8 n,
+        bytes32 seed,
+        bytes memory body
+    ) internal returns (bytes memory) {
+        uint256[] memory keys = addValidators(m, n, seed);
+        uint256[] memory signers = MOfNTestUtils.choose(m, keys, seed);
+
+        bytes32 digest = keccak256(body);
+        bytes memory metadata = abi.encodePacked(
+            TypeCasts.addressToBytes32(address(mailbox)),
+            uint8(m)
+        );
+        for (uint256 i = 0; i < m; i++) {
+            (uint8 v, bytes32 r, bytes32 s) = vm.sign(signers[i], digest);
+            metadata = bytes.concat(metadata, abi.encodePacked(r, s, v));
+        }
+
+        address[] memory validatorAddresses = new address[](n);
+        for (uint256 i = 0; i < n; i++) {
+            validatorAddresses[i] = vm.addr(keys[i]);
+        }
+
+        return bytes.concat(metadata, abi.encodePacked(validatorAddresses));
+    }
+
+    function addValidators(
+        uint8 m,
+        uint8 n,
+        bytes32 seed
+    ) internal returns (uint256[] memory) {
+        uint32[] memory domains = new uint32[](1);
+        domains[0] = ORIGIN;
+        uint256[] memory keys = new uint256[](n);
+        address[] memory addresses = new address[](n);
+        for (uint256 i = 0; i < n; i++) {
+            uint256 key = uint256(keccak256(abi.encode(seed, i)));
+            keys[i] = key;
+            addresses[i] = vm.addr(key);
+        }
+        ism = new CcipReadIsm();
+
+        address[][] memory wrapped = new address[][](1);
+        wrapped[0] = addresses;
+        ism.enrollValidators(domains, wrapped);
+        ism.setThreshold(ORIGIN, m);
+        return keys;
+    }
+
+    function getMessage(
+        uint32 destination,
+        bytes32 recipient,
+        bytes calldata body
+    ) internal returns (bytes memory) {
+        uint8 version = mailbox.VERSION();
+        uint32 origin = mailbox.localDomain();
+        bytes32 sender = TypeCasts.addressToBytes32(address(this));
+        uint32 nonce = mailbox.count();
+        mailbox.dispatch(destination, recipient, body);
+        bytes memory message = Message.formatMessage(
+            version,
+            nonce,
+            origin,
+            sender,
+            destination,
+            recipient,
+            body
+        );
+        return message;
+    }
+
+    function testVerify(
+        uint32 destination,
+        bytes32 recipient,
+        bytes calldata body,
+        uint8 m,
+        uint8 n,
+        bytes32 seed
+    ) public {
+        vm.assume(0 < m && m <= n && n < 10 && 0 < body.length);
+        bytes memory message = getMessage(destination, recipient, body);
+        bytes memory metadata = getMetadata(m, n, seed, body);
+        assertTrue(ism.verify(metadata, message));
+    }
+
+    function testFailVerify(
+        uint32 destination,
+        bytes32 recipient,
+        bytes calldata body,
+        uint8 m,
+        uint8 n,
+        bytes32 seed
+    ) public {
+        vm.assume(0 < m && m <= n && n < 10 && 0 < body.length);
+        bytes memory message = getMessage(destination, recipient, body);
+        bytes memory metadata = getMetadata(m, n, seed, message);
+
+        // changing single byte in metadata should fail signature verification
+        uint256 index = uint256(seed) % metadata.length;
+        metadata[index] = ~metadata[index];
+        assertFalse(ism.verify(metadata, message));
+    }
+}
