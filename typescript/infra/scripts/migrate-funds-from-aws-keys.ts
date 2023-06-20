e08ac231a9b7f10a6adcfed447843a9ab0781bde
Mattie Conover <git@mconover.dev>
2023-06-20 12:28:32 -0700

    Migration script

---
+++ typescript/infra/scripts/migrate-funds-from-aws-keys.ts
@@ -0,0 +1,138 @@
+import { PopulatedTransaction } from 'ethers';
+import { formatEther } from 'ethers/lib/utils';
+
+import {
+  AgentConnectionType,
+  ChainName,
+  Chains,
+  HyperlaneIgp,
+  MultiProvider,
+  ProtocolType,
+  chainMetadata,
+} from '@hyperlane-xyz/sdk';
+
+import { Contexts } from '../config/contexts';
+import { AgentAwsKey } from '../src/agents/aws';
+import { getCloudAgentKey } from '../src/agents/key-utils';
+import { BaseAgentKey } from '../src/agents/keys';
+import { AgentContextConfig } from '../src/config';
+import {
+  DeployEnvironment,
+  deployEnvToSdkEnv,
+} from '../src/config/environment';
+import { Role } from '../src/roles';
+
+import { getAgentConfig, getEnvironmentConfig } from './utils';
+
+const ENVIRONMENTS: DeployEnvironment[] = ['mainnet2', 'testnet3'];
+
+async function main() {
+  for (const ctx of Object.values(Contexts)) {
+    for (const env of ENVIRONMENTS) {
+      const envConfig = getEnvironmentConfig(env);
+      const agentConfig = getAgentConfig(ctx, envConfig);
+
+      // always fund from the current context and relayer role to the new relayer key
+      const multiProvider = await envConfig.getMultiProvider(
+        ctx,
+        Role.Relayer,
+        AgentConnectionType.Http,
+      );
+      const igp = HyperlaneIgp.fromEnvironment(
+        deployEnvToSdkEnv[env],
+        multiProvider,
+      );
+
+      const toKey = getCloudAgentKey(agentConfig, Role.Relayer);
+
+      for (const chain of Object.values(Chains)) {
+        await transfer(chain, agentConfig, igp, multiProvider, toKey);
+      }
+    }
+  }
+}
+
+async function transfer(
+  chain: Chains,
+  agentConfig: AgentContextConfig,
+  igp: HyperlaneIgp,
+  multiProvider: MultiProvider,
+  toKey: BaseAgentKey,
+) {
+  if (chainMetadata[chain].protocol != ProtocolType.Ethereum) return;
+  const fromKey = new OldRelayerAwsKey(agentConfig, chain);
+
+  const igpContract = igp.getContracts(chain).interchainGasPaymaster;
+
+  const igpBalance = await multiProvider
+    .getProvider(chain)
+    .getBalance(igpContract.address);
+  const claimIgpFundsTx = await igpContract.populateTransaction.claim();
+  const gasToClaimIgpFunds = await multiProvider.estimateGas(
+    chain,
+    claimIgpFundsTx,
+  );
+  let gasPrice = await multiProvider.getProvider(chain).getGasPrice();
+  const costToClaimIgpFunds = gasPrice.mul(gasToClaimIgpFunds);
+  if (igpBalance.gt(costToClaimIgpFunds)) {
+    // only claim if the cost to do so is less than the balance we are claiming
+    // await multiProvider.sendTransaction(chain, claimIgpFundsTx);
+    console.log('Claimed IGP funds', {
+      igpAddress: igpContract.address,
+      igpBalance: formatEther(igpBalance),
+      cost: formatEther(costToClaimIgpFunds),
+      tx: claimIgpFundsTx,
+    });
+    // update gas price since we might have waited a while
+    gasPrice = await multiProvider.getProvider(chain).getGasPrice();
+  } else {
+    console.log('IGP balance too low to claim', {
+      igpAddress: igpContract.address,
+      igpBalance: formatEther(igpBalance),
+      cost: formatEther(costToClaimIgpFunds),
+    });
+  }
+
+  const currentBalance = await multiProvider
+    .getProvider(chain)
+    .getBalance(fromKey.address);
+  const transferTx: PopulatedTransaction = {
+    to: toKey.address,
+    value: currentBalance,
+  };
+  const gasToTransfer = await multiProvider.estimateGas(chain, transferTx);
+  const costToTransfer = gasToTransfer.mul(gasPrice);
+  if (costToTransfer.gt(currentBalance)) {
+    console.log('Not enough funds to transfer', {
+      address: fromKey.address,
+      balance: formatEther(currentBalance),
+    });
+    return;
+  }
+  transferTx.value = currentBalance.sub(costToTransfer);
+  transferTx.gasLimit = gasToTransfer;
+  // await multiProvider.sendTransaction(chain, transferTx);
+  console.log('Transferred funds', {
+    originalBalance: formatEther(currentBalance),
+    finalBalance: formatEther(
+      await multiProvider.getProvider(chain).getBalance(fromKey.address),
+    ),
+    destinationBalance: formatEther(
+      await multiProvider.getProvider(chain).getBalance(toKey.address),
+    ),
+    gas: costToTransfer,
+    transferTx,
+  });
+}
+
+class OldRelayerAwsKey extends AgentAwsKey {
+  constructor(config: AgentContextConfig, chainName: ChainName) {
+    super(config, Role.Relayer, chainName);
+  }
+
+  get identifier(): string {
+    return `${this.context}-${this.environment}-key-${this.chainName}-${this.role}`;
+  }
+}
+
+main().then(console.log).catch(console.error);
