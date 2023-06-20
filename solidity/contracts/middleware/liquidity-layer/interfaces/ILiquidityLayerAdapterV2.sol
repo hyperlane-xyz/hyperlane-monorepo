bf0b90751842955ee07105739947562e7e9cafc5
liayoo <lia.yoo@smartcontract.com>
2023-06-20 18:07:29 +0900

    Remove gas functions from ILiquidityLayerAdapterV2

--- solidity/contracts/middleware/liquidity-layer/interfaces/ILiquidityLayerAdapterV2.sol
+++ solidity/contracts/middleware/liquidity-layer/interfaces/ILiquidityLayerAdapterV2.sol
@@ -6,17 +6,5 @@ interface ILiquidityLayerAdapterV2 {
         uint32 _destinationDomain,
         bytes32 _recipientAddress,
         uint256 _amount
-    ) external returns (bytes32);
-
-    function payForGas(
-        bytes32 _messageId,
-        uint32 _destinationDomain,
-        uint256 _gasAmount,
-        address _refundAddress
-    ) external payable;
-
-    function quoteGasPayment(uint32 _destinationDomain)
-        external
-        view
-        returns (uint256);
+    ) external payable returns (bytes32);
 }
