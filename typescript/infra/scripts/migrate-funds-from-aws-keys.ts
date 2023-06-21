97589f364e8f6fe16aa006923375fe0ce6e72f65
Mattie Conover <git@mconover.dev>
2023-06-21 14:13:49 -0700

    Testing

--- typescript/infra/scripts/migrate-funds-from-aws-keys.ts
+++ typescript/infra/scripts/migrate-funds-from-aws-keys.ts
@@ -14,7 +14,7 @@ import { Contexts } from '../config/contexts';
 import { AgentAwsKey } from '../src/agents/aws';
 import { getCloudAgentKey } from '../src/agents/key-utils';
 import { CloudAgentKey } from '../src/agents/keys';
-import { AgentContextConfig, DeployEnvironment } from '../src/config';
+import { AgentContextConfig } from '../src/config';
 import { Role } from '../src/roles';
 
 import { getAgentConfig, getEnvironmentConfig } from './utils';
@@ -45,51 +45,58 @@ async function main() {
   const from = new OldRelayerAwsKey(agentConfig, Chains.fuji);
   const to = getCloudAgentKey(agentConfig, Role.Relayer);
   await Promise.all([from.fetch(), to.fetch()]);
-  await transfer(Chains.optimismgoerli, agentConfig, multiProvider, from, to);
+  await transfer(
+    Chains.fuji,
+    Chains.optimismgoerli,
+    agentConfig,
+    multiProvider,
+    from,
+    to,
+  );
 }
 
-// @ts-ignore
-async function transferForEnv(ctx: Contexts, env: DeployEnvironment) {
-  const envConfig = getEnvironmentConfig(env);
-  const agentConfig = getAgentConfig(ctx, envConfig);
-
-  // always fund from the current context and relayer role to the new relayer key
-  const multiProvider = await envConfig.getMultiProvider(
-    ctx,
-    Role.Relayer,
-    AgentConnectionType.Http,
-  );
-
-  const toKey = getCloudAgentKey(agentConfig, Role.Relayer);
-  await toKey.fetch();
-
-  const chainsForEnv = Object.keys(envConfig.chainMetadataConfigs).filter(
-    (chain) => chainMetadata[chain].protocol == ProtocolType.Ethereum,
-  );
-  for (const originChain of chainsForEnv) {
-    try {
-      const fromKey = new OldRelayerAwsKey(agentConfig, originChain);
-      await fromKey.fetch();
-      for (const chain of chainsForEnv) {
-        await transfer(chain, agentConfig, multiProvider, fromKey, toKey);
-      }
-      await fromKey.delete();
-      console.log('Deleted key', {
-        from: fromKey.identifier,
-        fromKey: fromKey.address,
-      });
-    } catch (err) {
-      console.error('Error transferring funds', {
-        ctx,
-        env,
-        originChain,
-        err,
-      });
-    }
-  }
-}
+// async function transferForEnv(ctx: Contexts, env: DeployEnvironment) {
+//   const envConfig = getEnvironmentConfig(env);
+//   const agentConfig = getAgentConfig(ctx, envConfig);
+//
+//   // always fund from the current context and relayer role to the new relayer key
+//   const multiProvider = await envConfig.getMultiProvider(
+//     ctx,
+//     Role.Relayer,
+//     AgentConnectionType.Http,
+//   );
+//
+//   const toKey = getCloudAgentKey(agentConfig, Role.Relayer);
+//   await toKey.fetch();
+//
+//   const chainsForEnv = Object.keys(envConfig.chainMetadataConfigs).filter(
+//     (chain) => chainMetadata[chain].protocol == ProtocolType.Ethereum,
+//   );
+//   for (const originChain of chainsForEnv) {
+//     try {
+//       const fromKey = new OldRelayerAwsKey(agentConfig, originChain);
+//       await fromKey.fetch();
+//       for (const chain of chainsForEnv) {
+//         await transfer(chain, agentConfig, multiProvider, fromKey, toKey);
+//       }
+//       await fromKey.delete();
+//       console.log('Deleted key', {
+//         from: fromKey.identifier,
+//         fromKey: fromKey.address,
+//       });
+//     } catch (err) {
+//       console.error('Error transferring funds', {
+//         ctx,
+//         env,
+//         originChain,
+//         err,
+//       });
+//     }
+//   }
+// }
 
 async function transfer(
+  signerChain: ChainName,
   chain: ChainName,
   agentConfig: AgentContextConfig,
   multiProvider: MultiProvider,
@@ -107,16 +114,26 @@ async function transfer(
   console.log('Processing key', logCtx);
 
   const transferTx: PopulatedTransaction = {
+    from: fromKey.address,
     to: toKey.address,
     value: BigNumber.from(0),
   };
-  const [gasPrice, gasToTransfer, initialBalance] = await Promise.all([
+
+  console.debug('Estimating gas');
+  const gasToTransfer = await multiProvider.estimateGas(chain, transferTx);
+
+  console.debug('Getting gas price');
+  const [gasPrice, initialBalance] = await Promise.all([
     multiProvider.getProvider(chain).getGasPrice(),
-    multiProvider.estimateGas(chain, transferTx),
     multiProvider.getProvider(chain).getBalance(fromKey.address),
   ]);
 
   let costToTransfer = gasToTransfer.mul(gasPrice);
+
+  if (L2Chains.includes(chain))
+    // L2 chains have a gateway fee
+    costToTransfer = costToTransfer.mul(2);
+
   if (costToTransfer.gt(initialBalance)) {
     console.log('Not enough funds to transfer', {
       ...logCtx,
@@ -125,28 +142,39 @@ async function transfer(
     return;
   }
 
-  if (L2Chains.includes(chain))
-    // L2 chains have a gateway fee
-    costToTransfer = costToTransfer.mul(3).div(2);
-
-  transferTx.value = initialBalance.sub(costToTransfer);
+  // transferTx.value = initialBalance.sub(costToTransfer);
+  transferTx.value = BigNumber.from(1);
   transferTx.gasLimit = gasToTransfer;
-  transferTx.maxFeePerGas = gasPrice;
+  // transferTx.maxFeePerGas = gasPrice;
 
-  let receipt;
-  try {
-    receipt = await multiProvider.sendTransaction(chain, transferTx);
-  } catch (err) {
-    delete transferTx.maxFeePerGas;
-    transferTx.gasPrice = gasPrice;
-    receipt = await multiProvider.sendTransaction(chain, transferTx);
-  }
+  console.debug('Sending transaction');
+  const preparedTx = await multiProvider.prepareTx(
+    signerChain,
+    transferTx,
+    fromKey.address,
+  );
+
+  // const receipt = await multiProvider.sendTransaction(chain, transferTx);
+  const receipt = await multiProvider
+    .getSigner(chain)
+    .sendTransaction(preparedTx);
+
+  // let receipt;
+  // try {
+  //   receipt = await multiProvider.sendTransaction(chain, transferTx);
+  // } catch (err) {
+  //   console.debug('Sending transaction failed, retrying with gasPrice', err);
+  //   delete transferTx.maxFeePerGas;
+  //   transferTx.gasPrice = gasPrice;
+  //   receipt = await multiProvider.sendTransaction(chain, transferTx);
+  // }
   console.log('Transferred funds', {
     ...logCtx,
     initialBalance: formatEther(initialBalance),
     transferred: formatEther(transferTx.value),
     cost: formatEther(costToTransfer),
-    transferTx,
+    // transferTx,
+    preparedTx,
     receipt,
   });
 }
