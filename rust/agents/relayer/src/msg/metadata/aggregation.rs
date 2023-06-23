27bf1d47d3a7be4c4a244daf8d93a8a82c9a6ef2
Daniel Savu <23065004+daniel-savu@users.noreply.github.com>
2023-06-23 11:02:38 +0100

    chore(relayer): clean up AggregationISM integration

--- rust/agents/relayer/src/msg/metadata/aggregation.rs
+++ rust/agents/relayer/src/msg/metadata/aggregation.rs
@@ -19,10 +19,18 @@ pub struct AggregationIsmMetadataBuilder {
     base: BaseMetadataBuilder,
 }
 
+#[derive(Clone, Debug, new, PartialEq, Eq)]
+struct SubIsmMetadata {
+    /// The index of the sub-ISM in the aggregation ISM.
+    index: usize,
+    /// The metadata for the sub-ISM.
+    metadata: Vec<u8>,
+}
+
 impl AggregationIsmMetadataBuilder {
-    // also need to pass a vec of the indexes corresponding to the chosen metas
-    fn format_metadata(metadatas: &mut [(usize, Vec<u8>)], ism_count: usize) -> Vec<u8> {
-        // See test solidity implementation of this fn at `AggregationIsm.t.sol:getMetadata(...)`
+    fn format_metadata(metadatas: &mut [SubIsmMetadata], ism_count: usize) -> Vec<u8> {
+        // See test solidity implementation of this fn at:
+        // https://github.com/hyperlane-xyz/hyperlane-monorepo/blob/445da4fb0d8140a08c4b314e3051b7a934b0f968/solidity/test/isms/AggregationIsm.t.sol#L35
         fn encode_byte_index(i: usize) -> [u8; 4] {
             (i as u32).to_be_bytes()
         }
@@ -33,16 +41,15 @@ impl AggregationIsmMetadataBuilder {
         // Initialize the range tuple part of the buffer, so the actual metadatas can
         // simply be appended to it
         let mut buffer = vec![0; range_tuples_size];
-        for (index, metadata) in metadatas.iter_mut() {
+        for SubIsmMetadata { index, metadata } in metadatas.iter_mut() {
             let range_start = buffer.len();
-            // Append the ism metadata as-is, since it's already encoded
             buffer.append(metadata);
             let range_end = buffer.len();
 
             // The new tuple starts at the end of the previous ones.
-            // See `AggregationIsmMetadata.sol:_metadataRange()` as well.
+            // Also see: https://github.com/hyperlane-xyz/hyperlane-monorepo/blob/445da4fb0d8140a08c4b314e3051b7a934b0f968/solidity/contracts/libs/isms/AggregationIsmMetadata.sol#L49
             let encoded_range_start = METADATA_RANGE_SIZE * 2 * (*index);
-            // Overwrite the 0-initialized range tuple
+            // Overwrite the 0-initialized buffer
             buffer.splice(
                 encoded_range_start..(encoded_range_start + METADATA_RANGE_SIZE * 2),
                 [encode_byte_index(range_start), encode_byte_index(range_end)].concat(),
@@ -52,33 +59,29 @@ impl AggregationIsmMetadataBuilder {
     }
 
     fn n_cheapest_metas(
-        mut metas_and_gas: Vec<(usize, Vec<u8>, U256)>,
+        mut metas_and_gas: Vec<(SubIsmMetadata, U256)>,
         n: usize,
-    ) -> Vec<(usize, Vec<u8>)> {
-        metas_and_gas.sort_by(|(_, _, gas_1), (_, _, gas_2)| gas_1.cmp(gas_2));
+    ) -> Vec<SubIsmMetadata> {
+        metas_and_gas.sort_by(|(_, gas_1), (_, gas_2)| gas_1.cmp(gas_2));
         let mut cheapest: Vec<_> = metas_and_gas[..n].into();
-        cheapest.sort_by(|(index_1, _, _), (index_2, _, _)| index_1.cmp(index_2));
-        cheapest
-            .iter()
-            .cloned()
-            .map(|(index, meta, _)| (index, meta))
-            .collect()
+        cheapest.sort_by(|(meta_1, _), (meta_2, _)| meta_1.index.cmp(&meta_2.index));
+        cheapest.iter().cloned().map(|(meta, _)| meta).collect()
     }
 
     async fn cheapest_valid_metas(
-        sub_isms: Vec<(usize, Vec<u8>, Box<dyn InterchainSecurityModule>)>,
+        sub_isms: Vec<(SubIsmMetadata, Box<dyn InterchainSecurityModule>)>,
         message: &HyperlaneMessage,
         threshold: usize,
-    ) -> eyre::Result<Option<Vec<(usize, Vec<u8>)>>> {
+    ) -> eyre::Result<Option<Vec<SubIsmMetadata>>> {
         let metas_and_gas: Vec<_> =
-            join_all(sub_isms.into_iter().map(|(index, meta, ism)| async move {
-                let gas = ism.dry_run_verify(message, &meta).await;
-                (index, meta, gas)
+            join_all(sub_isms.into_iter().map(|(sub_ism_meta, ism)| async move {
+                let gas = ism.dry_run_verify(message, &sub_ism_meta.metadata).await;
+                (sub_ism_meta, gas)
             }))
             .await
             .into_iter()
-            .filter_map(|(index, meta, gast_result)| {
-                gast_result.ok().flatten().map(|gc| (index, meta, gc))
+            .filter_map(|(sub_ism_meta, gast_result)| {
+                gast_result.ok().flatten().map(|gc| (sub_ism_meta, gc))
             })
             .collect();
 
@@ -122,7 +125,7 @@ impl MetadataBuilder for AggregationIsmMetadataBuilder {
         .into_iter()
         .enumerate()
         .filter_map(|(i, r)| match r {
-            Ok((Some(meta), ism)) => Some((i, meta, ism)),
+            Ok((Some(meta), ism)) => Some((SubIsmMetadata::new(i, meta), ism)),
             _ => None,
         })
         .collect();
@@ -144,17 +147,17 @@ mod test {
     #[test]
     fn test_format_n_of_n_metadata_works_correctly() {
         let mut metadatas = vec![
-            (
+            SubIsmMetadata::new(
                 0,
                 Vec::from_hex("290decd9548b62a8d60345a988386fc84ba6bc95484008f6362f93160ef3e563")
                     .unwrap(),
             ),
-            (
+            SubIsmMetadata::new(
                 1,
                 Vec::from_hex("510e4e770828ddbf7f7b00ab00a9f6adaf81c0dc9cc85f1f8249c256942d61d9")
                     .unwrap(),
             ),
-            (
+            SubIsmMetadata::new(
                 2,
                 Vec::from_hex("356e5a2cc1eba076e650ac7473fccc37952b46bc2e419a200cec0c451dce2336")
                     .unwrap(),
@@ -170,22 +173,22 @@ mod test {
     #[test]
     fn test_format_n_of_m_metadata_works_correctly() {
         let mut metadatas = vec![
-            (
+            SubIsmMetadata::new(
                 0,
                 Vec::from_hex("290decd9548b62a8d60345a988386fc84ba6bc95484008f6362f93160ef3e563")
                     .unwrap(),
             ),
-            (
+            SubIsmMetadata::new(
                 1,
                 Vec::from_hex("510e4e770828ddbf7f7b00ab00a9f6adaf81c0dc9cc85f1f8249c256942d61d9")
                     .unwrap(),
             ),
-            (
+            SubIsmMetadata::new(
                 2,
                 Vec::from_hex("356e5a2cc1eba076e650ac7473fccc37952b46bc2e419a200cec0c451dce2336")
                     .unwrap(),
             ),
-            (
+            SubIsmMetadata::new(
                 4,
                 Vec::from_hex("f2e59013a0a379837166b59f871b20a8a0d101d1c355ea85d35329360e69c000")
                     .unwrap(),
@@ -200,7 +203,7 @@ mod test {
 
     #[test]
     fn test_format_empty_metadata_works_correctly() {
-        let mut metadatas = vec![(0, Vec::from_hex("").unwrap())];
+        let mut metadatas = vec![SubIsmMetadata::new(0, Vec::from_hex("").unwrap())];
         let expected = Vec::from_hex("0000000800000008").unwrap();
         assert_eq!(
             AggregationIsmMetadataBuilder::format_metadata(&mut metadatas, 1),
@@ -211,13 +214,25 @@ mod test {
     #[test]
     fn test_n_cheapest_metas_works() {
         let metas_and_gas = vec![
-            (3, vec![], U256::from_dec_str("3").unwrap()),
-            (2, vec![], U256::from_dec_str("2").unwrap()),
-            (1, vec![], U256::from_dec_str("1").unwrap()),
+            (
+                SubIsmMetadata::new(3, vec![]),
+                U256::from_dec_str("3").unwrap(),
+            ),
+            (
+                SubIsmMetadata::new(2, vec![]),
+                U256::from_dec_str("2").unwrap(),
+            ),
+            (
+                SubIsmMetadata::new(1, vec![]),
+                U256::from_dec_str("1").unwrap(),
+            ),
         ];
         assert_eq!(
             AggregationIsmMetadataBuilder::n_cheapest_metas(metas_and_gas, 2),
-            vec![(1, vec![]), (2, vec![])]
+            vec![
+                SubIsmMetadata::new(1, vec![]),
+                SubIsmMetadata::new(2, vec![])
+            ]
         )
     }
 }
