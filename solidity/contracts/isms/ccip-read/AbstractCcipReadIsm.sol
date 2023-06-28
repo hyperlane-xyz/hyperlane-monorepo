1c1d442ebac83d5e23b7ae8897cbcd3304e0cc91
Alex Harley <alex@alexbh.dev>
2023-06-28 10:50:35 +0200

    docs & removing more logic from AbstractCcipReadIsm

--- solidity/contracts/isms/ccip-read/AbstractCcipReadIsm.sol
+++ solidity/contracts/isms/ccip-read/AbstractCcipReadIsm.sol
@@ -8,6 +8,7 @@ import {IMailbox} from "../../interfaces/IMailbox.sol";
 import {Message} from "../../libs/Message.sol";
 import {AbstractMultisigIsm} from "../multisig/AbstractMultisigIsm.sol";
 
+/// @dev https://eips.ethereum.org/EIPS/eip-3668
 /// @param sender the address of the contract making the call, usually address(this)
 /// @param urls the URLs to query for offchain data
 /// @param callData context needed for offchain service to service request
@@ -23,63 +24,37 @@ error OffchainLookup(
 
 /**
  * @title AbstractCcipReadIsm
- * @notice An ISM that allows arbitrary payloads to be submitted and verified on chain.
+ * @notice An ISM that allows arbitrary payloads to be submitted and verified on chain
+ * @dev https://eips.ethereum.org/EIPS/eip-3668
+ * @dev The AbstractCcipReadIsm provided by Hyperlane is left intentially minimalist as
+ * the range of applications that could be supported by a CcipReadIsm are so broad. However
+ * there are few things to note when building a custom CcipReadIsm.
+ *
+ * 1. `getOffchainVerifyInfo` should revert with a `OffchainLookup` error, which encodes
+ *    the data necessary to query for offchain information
+ * 2. For full CCIP Read specification compatibility, CcipReadIsm's should expose a function
+ *    that in turn calls `process` on the configured Mailbox with the provided metadata and
+ *    message. This functions selector should be provided as the `callbackFunction` payload
+ *    for the OffchainLookup error
  */
 abstract contract AbstractCcipReadIsm is ICcipReadIsm {
-    // ============ Libraries ============
-
-    using Message for bytes;
-
     // ============ Constants ============
 
     // solhint-disable-next-line const-name-snakecase
     uint8 public constant moduleType =
         uint8(IInterchainSecurityModule.Types.CCIP_READ);
 
-    // ============ Mutable Storage ============
-
-    /// @notice The URLs to query for offchain data from.
-    string[] public offchainUrls;
-
-    /// @notice Data to pass back to the callback function.
-    // slither-disable-next-line uninitialized-state
-    bytes public offchainExtraData;
-
-    // ============ Events ============
-
-    /**
-     * @notice Emitted when the offchain URLs are updated
-     * @param urls the new URLs
-     */
-    event OffchainUrlsUpdated(string[] urls);
-
-    /**
-     * @notice Emitted when the offchain extraData is updated
-     * @param extraData the new extraData
-     */
-    event OffchainExtraDataUpdated(bytes extraData);
-
     // ============ External Functions ============
 
     /**
      * @notice Reverts with the data needed to query information offchain before
      * mailbox submission
-     * @dev See https://eips.ethereum.org/EIPS/eip-3668 for more information
+     * @param _message the Hyperlane encoded message
      * @return bool Ignored
      */
-    function getOffchainVerifyInfo(bytes calldata _calldata)
+    function getOffchainVerifyInfo(bytes calldata _message)
         external
         view
-        returns (bool)
-    {
-        revert OffchainLookup(
-            address(this),
-            offchainUrls,
-            _calldata,
-            // Note: consumers of getOffchainVerifyInfo should call the destination
-            // chain mailbox.process(_metadata, _message)
-            IMailbox.process.selector,
-            offchainExtraData
-        );
-    }
+        virtual
+        returns (bool);
 }
