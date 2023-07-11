425a1b28151912cd567cffb4f33acc6ced62c606
Arman Aurobindo <66505181+armanthepythonguy@users.noreply.github.com>
2023-07-12 00:44:27 +0530

    Update typescript/token/contracts/HypERC20CollateralVotable.sol
    Co-authored-by: Yorke Rhodes <email@yorke.dev>

--- typescript/token/contracts/HypERC20CollateralVotable.sol
+++ typescript/token/contracts/HypERC20CollateralVotable.sol
@@ -8,11 +8,11 @@ import "@openzeppelin/contracts-upgradeable/governance/utils/IVotesUpgradeable.s
  * @title Hyperlane ERC20 Token Collateral that wraps an existing votable ERC20 with remote transfer functionality.
  * @author  Arman Aurobindo @armanthepythonguy
  */
-contract HypERC20CollateralVotable is HypERC20Collateral {
+contract HypERC20CollateralVotable is HypERC20Collateral, IVotesUpgradeable {
     constructor(address _erc20) HypERC20Collateral(_erc20) {}
 
     // Using this function you can delegate the voting power
-    function delegateVotes(address _L1VoteDelegator) external onlyOwner {
+    function delegate(address _L1VoteDelegator) external override onlyOwner {
         IVotesUpgradeable(address(wrappedToken)).delegate(_L1VoteDelegator);
     }
 }
