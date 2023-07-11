d69a3f639aa7110e4108a76033a5096f0a3cbad2
Kunal Arora <55632507+aroralanuk@users.noreply.github.com>
2023-07-11 18:58:38 -0400

    No metadata relayer support for hooks (#2495)
    ### Description
    
    - Adding `NoMetadataRelayer` moduleType which returns an empty `vec![]`
    on `MetadataBuilder::build()`
    - Adding `HyperlaneHookDeployer` which lets you deploy the hook, ISM and
    a testRecipient which specifies to use the `AbtractNativeISM`
    ### Drive-by changes
    
    - none
    
    ### Related issues
    
    - Accompanies #2316 
    - Fixes https://github.com/hyperlane-xyz/issues/issues/379 for Optimism
    - Related to adding native hooks to Mailbox V3
    https://github.com/hyperlane-xyz/issues/issues/486
    
    ### Backward compatibility
    
    Backward compatible? Yes
    
    Tooling changes needed? Need to update node to ^18
    
    ### Testing
    
    - Manual on testnet3

--- rust/agents/relayer/src/msg/metadata/mod.rs
+++ rust/agents/relayer/src/msg/metadata/mod.rs
@@ -1,9 +1,11 @@
 mod aggregation;
 mod base;
 mod multisig;
+mod null_metadata;
 mod routing;
 
 use aggregation::AggregationIsmMetadataBuilder;
 pub(crate) use base::BaseMetadataBuilder;
 pub(crate) use base::MetadataBuilder;
+use null_metadata::NullMetadataBuilder;
 use routing::RoutingIsmMetadataBuilder;
