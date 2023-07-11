7da53488b0a09ef0de4ab8941538d80c7f691b72
Kunal Arora <kunalarora1729@gmail.com>
2023-07-11 14:43:22 -0400

    deleted objects from utils

--- typescript/utils/index.ts
+++ typescript/utils/index.ts
@@ -3,4 +3,3 @@ export * as utils from './src/utils';
 export { Validator, BaseValidator } from './src/validator';
 export * as logging from './src/logging';
 export { error, warn, log, debug, trace } from './src/logging';
-export { filteredOwners } from './src/objects';
