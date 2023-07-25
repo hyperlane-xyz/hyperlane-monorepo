df69886ab508e2c7db8df622dfa1d40a77621362
Yorke Rhodes <yorke@hyperlane.xyz>
2023-07-25 16:51:06 -0400

    Simplify mailbox and make interfaces payable

---
+++ solidity/contracts/interfaces/IMessageRecipientV3.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT OR Apache-2.0
+pragma solidity >=0.6.11;
+
+interface IMessageRecipient {
+    function handle(
+        uint32 _origin,
+        bytes32 _sender,
+        bytes calldata _message
+    ) external payable;
+}
