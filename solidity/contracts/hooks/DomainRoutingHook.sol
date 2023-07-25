f74f0618cd72e1a88d65be76cfa0081c44b4882d
Yorke Rhodes <yorke@hyperlane.xyz>
2023-07-25 16:22:02 -0400

    Init immutable mailbox v3 and native hooks

---
+++ solidity/contracts/hooks/DomainRoutingHook.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0;
+
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+import {AbstractHook} from "./AbstractHook.sol";
+import {Message} from "../libs/Message.sol";
+
+import {IPostDispatchHook} from "../interfaces/hooks/IPostDispatchHook.sol";
+
+contract DomainRoutingHook is AbstractHook, Ownable {
+    using Message for bytes;
+
+    mapping(uint32 => IPostDispatchHook) public hooks;
+
+    constructor(address _mailbox, address _owner) AbstractHook(_mailbox) {
+        _transferOwnership(_owner);
+    }
+
+    function setHook(uint32 destination, address hook) external onlyOwner {
+        hooks[destination] = IPostDispatchHook(hook);
+    }
+
+    function _postDispatch(bytes calldata message) internal override {
+        hooks[message.destination()].postDispatch{value: msg.value}(message);
+    }
+}
---
+++ solidity/contracts/interfaces/IMailboxV3.sol
@@ -0,0 +1,68 @@
+// SPDX-License-Identifier: MIT OR Apache-2.0
+pragma solidity >=0.8.0;
+
+import {IInterchainSecurityModule} from "./IInterchainSecurityModule.sol";
+import {IPostDispatchHook} from "./hooks/IPostDispatchHook.sol";
+
+interface IMailboxV3 {
+    // ============ Events ============
+    /**
+     * @notice Emitted when a new message is dispatched via Hyperlane
+     * @param sender The address that dispatched the message
+     * @param destination The destination domain of the message
+     * @param recipient The message recipient address on `destination`
+     * @param message Raw bytes of message
+     */
+    event Dispatch(
+        address indexed sender,
+        uint32 indexed destination,
+        bytes32 indexed recipient,
+        bytes message
+    );
+
+    /**
+     * @notice Emitted when a new message is dispatched via Hyperlane
+     * @param messageId The unique message identifier
+     */
+    event DispatchId(bytes32 indexed messageId);
+
+    /**
+     * @notice Emitted when a Hyperlane message is processed
+     * @param messageId The unique message identifier
+     */
+    event ProcessId(bytes32 indexed messageId);
+
+    /**
+     * @notice Emitted when a Hyperlane message is delivered
+     * @param origin The origin domain of the message
+     * @param sender The message sender address on `origin`
+     * @param recipient The address that handled the message
+     */
+    event Process(
+        uint32 indexed origin,
+        bytes32 indexed sender,
+        address indexed recipient
+    );
+
+    function localDomain() external view returns (uint32);
+
+    function delivered(bytes32 messageId) external view returns (bool);
+
+    function defaultIsm() external view returns (IInterchainSecurityModule);
+
+    function defaultHook() external view returns (IPostDispatchHook);
+
+    function dispatch(
+        uint32 _destinationDomain,
+        bytes32 _recipientAddress,
+        bytes calldata _messageBody
+    ) external returns (bytes32);
+
+    function process(bytes calldata _metadata, bytes calldata _message)
+        external;
+
+    function recipientIsm(address _recipient)
+        external
+        view
+        returns (IInterchainSecurityModule);
+}
