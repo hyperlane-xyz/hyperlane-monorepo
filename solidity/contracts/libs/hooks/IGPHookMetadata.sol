71055030bab0ad2d72eee78f366183f7196bb862
Kunal Arora <kunalarora1729@gmail.com>
2023-08-02 19:09:46 -0400

    offsets corrected

--- solidity/contracts/libs/hooks/IGPHookMetadata.sol
+++ solidity/contracts/libs/hooks/IGPHookMetadata.sol
@@ -5,22 +5,41 @@ pragma solidity >=0.8.0;
  * Format of metadata:
  *
  * [0:32] Gas limit for message
- * [33:52] Refund address for message
+ * [32:52] Refund address for message
  */
 library IGPHookMetadata {
+    uint8 private constant GAS_LIMIT_OFFSET = 0;
+    uint8 private constant REFUND_ADDRESS_OFFSET = 32;
+
+    /**
+     * @notice Returns the specified gas limit for the message.
+     * @param _metadata ABI encoded IGP hook metadata.
+     * @return Gas limit for the message as uint256.
+     */
     function gasLimit(bytes calldata _metadata)
         internal
         pure
         returns (uint256)
     {
-        return uint256(bytes32(_metadata[0:32]));
+        return
+            uint256(bytes32(_metadata[GAS_LIMIT_OFFSET:GAS_LIMIT_OFFSET + 32]));
     }
 
+    /**
+     * @notice Returns the specified refund address for the message.
+     * @param _metadata ABI encoded IGP hook metadata.
+     * @return Refund address for the message as address.
+     */
     function refundAddress(bytes calldata _metadata)
         internal
         pure
         returns (address)
     {
-        return address(bytes20(_metadata[33:52]));
+        return
+            address(
+                bytes20(
+                    _metadata[REFUND_ADDRESS_OFFSET:REFUND_ADDRESS_OFFSET + 20]
+                )
+            );
     }
 }
