b72c7fbf84e47520ac66c8f09443489cf3e54b13
Mattie Conover <git@mconover.dev>
2023-08-04 09:10:02 -0700

    fmt

--- rust/utils/run-locally/src/ethereum.rs
+++ rust/utils/run-locally/src/ethereum.rs
@@ -23,7 +23,11 @@ pub fn start_anvil(config: Arc<Config>) -> AgentHandles {
 
     if !config.is_ci_env {
         // Kill any existing anvil processes just in case since it seems to have issues getting cleaned up
-        Program::new("pkill").raw_arg("-SIGKILL").cmd("anvil").run_ignore_code().join();
+        Program::new("pkill")
+            .raw_arg("-SIGKILL")
+            .cmd("anvil")
+            .run_ignore_code()
+            .join();
     }
     log!("Launching anvil...");
     let anvil_args = Program::new("anvil").flag("silent").filter_logs(|_| false); // for now do not keep any of the anvil logs
