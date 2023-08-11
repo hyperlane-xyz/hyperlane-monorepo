hyperlane-xyz/hyperlane-monorepo

commit 63562c7211bad659aaa6719652133b883c164a09

Refactor agent event indexing (#2246)
### Description

This PR refactors event indexing in the agents, allowing similar logic
to be shared across multiple event types (i.e. messages, deliveries, gas
payments) and database types (i.e. the Relayer rocks DB and Scraper SQL
DB).

Furthermore, it adds new syncing modes by way of `MessageSyncCursors`
that take advantage of the monotonically increasing dispatched message
nonce to sync more intelligently.

### Drive-by changes

- Fixes a bug in the existing cursor that caused the same block range to
be indexed three times
- Modifies kathy to get rid of the idea of "rounds", just sends messages
with a sleep in between
- Minor modifications to the e2e test for performance
- Expand macros in settings
- Add scraper to e2e test

### Opportunities for improvement

- We can further reduce RPC usage (or improve latency) by sharing the
view of the latest finalized block number between cursors
- We can speed up the effective time for (a relayer to start deliving
messages | the scraper to scraper recent events) by creating
forward/backward cursors for gas payments and deliveries where the
backwards cursor terminates at index_settings.from
- We can remove the need for index_settings.from by terminating
backwards cursors based on the block number that the first message was
dispatched at

### Related issues

- Fixes #[issue number here]

### Backward compatibility

_Are these changes backward compatible?_

Yes

_Are there any infrastructure implications, e.g. changes that would
prohibit deploying older commits using this infra tooling?_

None


### Testing

_What kind of testing have these changes undergone?_

E2E tests

---------

Co-authored-by: Mattie Conover <git@mconover.dev>

rust/agents/scraper/src/chain_scraper/mod.rs
@@ -2,29 +2,25 @@
 //! keeping things updated.
 
 use std::collections::HashMap;
-use std::future::Future;
 use std::sync::Arc;
 
-use eyre::{eyre, Result};
-use futures::TryFutureExt;
+use async_trait::async_trait;
+use eyre::Result;
 use itertools::Itertools;
 use tracing::trace;
 
-use hyperlane_base::{chains::IndexSettings, ContractSyncMetrics};
+use hyperlane_base::chains::IndexSettings;
 use hyperlane_core::{
-    BlockInfo, HyperlaneChain, HyperlaneContract, HyperlaneDomain, HyperlaneMessage,
-    HyperlaneProvider, InterchainGasPaymasterIndexer, InterchainGasPayment, LogMeta, Mailbox,
-    MailboxIndexer, H256,
+    BlockInfo, Delivery, HyperlaneDomain, HyperlaneLogStore, HyperlaneMessage,
+    HyperlaneMessageStore, HyperlaneProvider, HyperlaneWatermarkedLogStore, InterchainGasPayment,
+    LogMeta, H256,
 };
 
 use crate::db::StorablePayment;
 use crate::{
-    chain_scraper::sync::Syncer,
     date_time,
     db::{BasicBlock, BlockCursor, ScraperDb, StorableDelivery, StorableMessage, StorableTxn},
 };
-
-mod sync;
 
 /// Maximum number of records to query at a time. This came about because when a
 /// lot of messages are sent in a short period of time we were ending up with a
@@ -32,140 +28,41 @@ mod sync;
 /// actually save it to the database.
 const CHUNK_SIZE: usize = 50;
 
-/// Local chain components like the mailbox.
-#[derive(Debug, Clone)]
-pub struct Contracts {
-    pub mailbox: Arc<dyn Mailbox>,
-    pub mailbox_indexer: Arc<dyn MailboxIndexer>,
-    pub igp_indexer: Arc<dyn InterchainGasPaymasterIndexer>,
-    pub provider: Arc<dyn HyperlaneProvider>,
-}
-
 /// A chain scraper is comprised of all the information and contract/provider
 /// connections needed to scrape the contracts on a single blockchain.
 #[derive(Clone, Debug)]
-pub struct SqlChainScraper {
+pub struct HyperlaneSqlDb {
+    mailbox_address: H256,
+    domain: HyperlaneDomain,
     db: ScraperDb,
-    /// Contracts on this chain representing this chain (e.g. mailbox)
-    contracts: Contracts,
-    chunk_size: u32,
-    metrics: ContractSyncMetrics,
+    provider: Arc<dyn HyperlaneProvider>,
     cursor: Arc<BlockCursor>,
 }
 
 #[allow(unused)]
-impl SqlChainScraper {
+impl HyperlaneSqlDb {
     pub async fn new(
         db: ScraperDb,
-        contracts: Contracts,
+        mailbox_address: H256,
+        domain: HyperlaneDomain,
+        provider: Arc<dyn HyperlaneProvider>,
         index_settings: &IndexSettings,
-        metrics: ContractSyncMetrics,
     ) -> Result<Self> {
         let cursor = Arc::new(
-            db.block_cursor(contracts.mailbox.domain().id(), index_settings.from as u64)
+            db.block_cursor(domain.id(), index_settings.from as u64)
                 .await?,
         );
         Ok(Self {
             db,
-            contracts,
-            chunk_size: index_settings.chunk_size,
-            metrics,
+            domain,
+            provider,
+            mailbox_address,
             cursor,
         })
     }
 
     pub fn domain(&self) -> &HyperlaneDomain {
-        self.contracts.mailbox.domain()
-    }
-
-    /// Sync contract data and other blockchain with the current chain state.
-    /// This will create a long-running task that should be spawned.
-    pub fn sync(self) -> impl Future<Output = Result<()>> + Send + 'static {
-        let chain = self.contracts.mailbox.domain().name().to_owned();
-        Syncer::new(self)
-            .and_then(|syncer| syncer.run())
-            .and_then(|()| async move { panic!("Sync task for {chain} stopped!") })
-    }
-
-    /// Fetch the highest message nonce we have seen for the local domain.
-    async fn last_message_nonce(&self) -> Result<Option<u32>> {
-        self.db
-            .last_message_nonce(self.domain().id(), &self.contracts.mailbox.address())
-            .await
-    }
-
-    /// Store messages from the origin mailbox into the database.
-    ///
-    /// Returns the highest message nonce which was provided to this
-    /// function.
-    async fn store_messages(
-        &self,
-        messages: &[HyperlaneMessageWithMeta],
-        txns: &HashMap<H256, TxnWithId>,
-    ) -> Result<u32> {
-        debug_assert!(!messages.is_empty());
-
-        let max_nonce = messages
-            .iter()
-            .map(|m| m.message.nonce)
-            .max()
-            .ok_or_else(|| eyre!("Received empty list"))?;
-        self.db
-            .store_messages(
-                &self.contracts.mailbox.address(),
-                messages.iter().map(|m| {
-                    let txn = txns.get(&m.meta.transaction_hash).unwrap();
-                    StorableMessage {
-                        msg: m.message.clone(),
-                        meta: &m.meta,
-                        txn_id: txn.id,
-                    }
-                }),
-            )
-            .await?;
-
-        Ok(max_nonce)
-    }
-
-    /// Record that a message was delivered.
-    async fn store_deliveries(
-        &self,
-        deliveries: &[Delivery],
-        txns: &HashMap<H256, TxnWithId>,
-    ) -> Result<()> {
-        if deliveries.is_empty() {
-            return Ok(());
-        }
-
-        let storable = deliveries.iter().map(|delivery| {
-            let txn_id = txns.get(&delivery.meta.transaction_hash).unwrap().id;
-            delivery.as_storable(txn_id)
-        });
-
-        self.db
-            .store_deliveries(
-                self.domain().id(),
-                self.contracts.mailbox.address(),
-                storable,
-            )
-            .await
-    }
-
-    async fn store_payments(
-        &self,
-        payments: &[Payment],
-        txns: &HashMap<H256, TxnWithId>,
-    ) -> Result<()> {
-        if payments.is_empty() {
-            return Ok(());
-        }
-
-        let storable = payments.iter().map(|payment| {
-            let txn_id = txns.get(&payment.meta.transaction_hash).unwrap().id;
-            payment.as_storable(txn_id)
-        });
-
-        self.db.store_payments(self.domain().id(), storable).await
+        &self.domain
     }
 
     /// Takes a list of txn and block hashes and ensure they are all in the
@@ -174,9 +71,9 @@ impl SqlChainScraper {
     /// Returns the relevant transaction info.
     async fn ensure_blocks_and_txns(
         &self,
-        message_metadata: impl Iterator<Item = &LogMeta>,
+        log_meta: impl Iterator<Item = &LogMeta>,
     ) -> Result<impl Iterator<Item = TxnWithId>> {
-        let block_hash_by_txn_hash: HashMap<H256, H256> = message_metadata
+        let block_hash_by_txn_hash: HashMap<H256, H256> = log_meta
             .map(|meta| (meta.transaction_hash, meta.block_hash))
             .collect();
 
@@ -242,7 +139,7 @@ impl SqlChainScraper {
         let mut txns_to_insert: Vec<StorableTxn> = Vec::with_capacity(CHUNK_SIZE);
         for mut chunk in as_chunks::<(&H256, &mut (Option<i64>, i64))>(txns_to_fetch, CHUNK_SIZE) {
             for (hash, (_, block_id)) in chunk.iter() {
-                let info = self.contracts.provider.get_txn_by_hash(hash).await?;
+                let info = self.provider.get_txn_by_hash(hash).await?;
                 txns_to_insert.push(StorableTxn {
                     info,
                     block_id: *block_id,
@@ -309,7 +206,7 @@ impl SqlChainScraper {
         for chunk in as_chunks(blocks_to_fetch, CHUNK_SIZE) {
             debug_assert!(!chunk.is_empty());
             for (hash, block_info) in chunk {
-                let info = self.contracts.provider.get_block_by_hash(hash).await?;
+                let info = self.provider.get_block_by_hash(hash).await?;
                 let basic_info_ref = block_info.insert(BasicBlock {
                     id: -1,
                     hash: *hash,
@@ -349,35 +246,130 @@ impl SqlChainScraper {
     }
 }
 
-#[derive(Debug, Clone)]
-struct Delivery {
-    message_id: H256,
-    meta: LogMeta,
-}
-
-impl Delivery {
-    fn as_storable(&self, txn_id: i64) -> StorableDelivery {
-        StorableDelivery {
-            message_id: self.message_id,
-            meta: &self.meta,
-            txn_id,
+#[async_trait]
+impl HyperlaneLogStore<HyperlaneMessage> for HyperlaneSqlDb {
+    /// Store messages from the origin mailbox into the database.
+    async fn store_logs(&self, messages: &[(HyperlaneMessage, LogMeta)]) -> Result<u32> {
+        if messages.is_empty() {
+            return Ok(0);
         }
+        let txns: HashMap<H256, TxnWithId> = self
+            .ensure_blocks_and_txns(messages.iter().map(|r| &r.1))
+            .await?
+            .map(|t| (t.hash, t))
+            .collect();
+        let storable = messages.iter().map(|m| {
+            let txn = txns.get(&m.1.transaction_hash).unwrap();
+            StorableMessage {
+                msg: m.0.clone(),
+                meta: &m.1,
+                txn_id: txn.id,
+            }
+        });
+        let stored = self
+            .db
+            .store_dispatched_messages(self.domain().id(), &self.mailbox_address, storable)
+            .await?;
+        Ok(stored as u32)
     }
 }
 
-#[derive(Debug, Clone)]
-struct Payment {
-    payment: InterchainGasPayment,
-    meta: LogMeta,
+#[async_trait]
+impl HyperlaneLogStore<Delivery> for HyperlaneSqlDb {
+    async fn store_logs(&self, deliveries: &[(Delivery, LogMeta)]) -> Result<u32> {
+        if deliveries.is_empty() {
+            return Ok(0);
+        }
+        let txns: HashMap<Delivery, TxnWithId> = self
+            .ensure_blocks_and_txns(deliveries.iter().map(|r| &r.1))
+            .await?
+            .map(|t| (t.hash, t))
+            .collect();
+        let storable = deliveries.iter().map(|(message_id, meta)| {
+            let txn_id = txns.get(&meta.transaction_hash).unwrap().id;
+            StorableDelivery {
+                message_id: *message_id,
+                meta,
+                txn_id,
+            }
+        });
+
+        let stored = self
+            .db
+            .store_deliveries(self.domain().id(), self.mailbox_address, storable)
+            .await?;
+        Ok(stored as u32)
+    }
 }
 
-impl Payment {
-    fn as_storable(&self, txn_id: i64) -> StorablePayment {
-        StorablePayment {
-            payment: &self.payment,
-            meta: &self.meta,
-            txn_id,
+#[async_trait]
+impl HyperlaneLogStore<InterchainGasPayment> for HyperlaneSqlDb {
+    async fn store_logs(&self, payments: &[(InterchainGasPayment, LogMeta)]) -> Result<u32> {
+        if payments.is_empty() {
+            return Ok(0);
         }
+        let txns: HashMap<H256, TxnWithId> = self
+            .ensure_blocks_and_txns(payments.iter().map(|r| &r.1))
+            .await?
+            .map(|t| (t.hash, t))
+            .collect();
+        let storable = payments.iter().map(|(payment, meta)| {
+            let txn_id = txns.get(&meta.transaction_hash).unwrap().id;
+            StorablePayment {
+                payment,
+                meta,
+                txn_id,
+            }
+        });
+
+        let stored = self.db.store_payments(self.domain().id(), storable).await?;
+        Ok(stored as u32)
+    }
+}
+
+#[async_trait]
+impl HyperlaneMessageStore for HyperlaneSqlDb {
+    /// Gets a message by nonce.
+    async fn retrieve_message_by_nonce(&self, nonce: u32) -> Result<Option<HyperlaneMessage>> {
+        let message = self
+            .db
+            .retrieve_message_by_nonce(self.domain().id(), &self.mailbox_address, nonce)
+            .await?;
+        Ok(message)
+    }
+
+    /// Retrieves the block number at which the message with the provided nonce
+    /// was dispatched.
+    async fn retrieve_dispatched_block_number(&self, nonce: u32) -> Result<Option<u64>> {
+        let Some(tx_id) = self
+        .db
+        .retrieve_dispatched_tx_id(self.domain().id(), &self.mailbox_address, nonce)
+        .await?
+        else {
+            return Ok(None);
+        };
+
+        let Some(block_id) = self.db.retrieve_block_id(tx_id).await? else {
+            return Ok(None);
+        };
+
+        Ok(self.db.retrieve_block_number(block_id).await?)
+    }
+}
+
+#[async_trait]
+impl<T> HyperlaneWatermarkedLogStore<T> for HyperlaneSqlDb
+where
+    HyperlaneSqlDb: HyperlaneLogStore<T>,
+{
+    /// Gets the block number high watermark
+    async fn retrieve_high_watermark(&self) -> Result<Option<u32>> {
+        Ok(Some(self.cursor.height().await.try_into()?))
+    }
+    /// Stores the block number high watermark
+    async fn store_high_watermark(&self, block_number: u32) -> Result<()> {
+        self.cursor.update(block_number.into()).await;
+        Ok(())
     }
 }
 
@@ -391,12 +383,6 @@ struct TxnWithId {
 struct TxnWithBlockId {
     txn_hash: H256,
     block_id: i64,
-}
-
-#[derive(Debug, Clone)]
-struct HyperlaneMessageWithMeta {
-    message: HyperlaneMessage,
-    meta: LogMeta,
 }
 
 fn as_chunks<T>(iter: impl Iterator<Item = T>, chunk_size: usize) -> impl Iterator<Item = Vec<T>> {

rust/agents/scraper/src/db/block.rs
@@ -35,6 +35,25 @@ impl FromQueryResult for BasicBlock {
 }
 
 impl ScraperDb {
+    /// Retrieves the block number for a given block database ID
+    pub async fn retrieve_block_number(&self, block_id: i64) -> Result<Option<u64>> {
+        #[derive(Copy, Clone, Debug, EnumIter, DeriveColumn)]
+        enum QueryAs {
+            Height,
+        }
+        let block_height = block::Entity::find()
+            .filter(block::Column::Id.eq(block_id))
+            .select_only()
+            .column_as(block::Column::Height, QueryAs::Height)
+            .into_values::<i64, QueryAs>()
+            .one(&self.0)
+            .await?;
+        match block_height {
+            Some(height) => Ok(Some(height.try_into()?)),
+            None => Ok(None),
+        }
+    }
+
     /// Get basic block data that can be used to insert a transaction or
     /// message. Any blocks which are not found will be excluded from the
     /// response.

rust/agents/scraper/src/db/txn.rs
@@ -19,6 +19,21 @@ pub struct StorableTxn {
 }
 
 impl ScraperDb {
+    pub async fn retrieve_block_id(&self, tx_id: i64) -> Result<Option<i64>> {
+        #[derive(Copy, Clone, Debug, EnumIter, DeriveColumn)]
+        enum QueryAs {
+            BlockId,
+        }
+        let block_id = transaction::Entity::find()
+            .filter(transaction::Column::Id.eq(tx_id))
+            .select_only()
+            .column_as(transaction::Column::BlockId, QueryAs::BlockId)
+            .into_values::<i64, QueryAs>()
+            .one(&self.0)
+            .await?;
+        Ok(block_id)
+    }
+
     /// Lookup transactions and find their ids. Any transactions which are not
     /// found be excluded from the hashmap.
     pub async fn get_txn_ids(

rust/agents/relayer/src/msg/gas_payment/mod.rs
@@ -4,7 +4,7 @@ use async_trait::async_trait;
 use eyre::Result;
 use tracing::{debug, error, trace};
 
-use hyperlane_base::db::HyperlaneDB;
+use hyperlane_base::db::HyperlaneRocksDB;
 use hyperlane_core::{
     HyperlaneMessage, InterchainGasExpenditure, InterchainGasPayment, TxCostEstimate, TxOutcome,
     U256,
@@ -40,13 +40,13 @@ pub struct GasPaymentEnforcer {
     /// policy or another. If a message matches multiple policies'
     /// whitelists, then whichever is first in the list will be used.
     policies: Vec<(Box<dyn GasPaymentPolicy>, MatchingList)>,
-    db: HyperlaneDB,
+    db: HyperlaneRocksDB,
 }
 
 impl GasPaymentEnforcer {
     pub fn new(
         policy_configs: impl IntoIterator<Item = GasPaymentEnforcementConf>,
-        db: HyperlaneDB,
+        db: HyperlaneRocksDB,
     ) -> Self {
         let policies = policy_configs
             .into_iter()
@@ -136,7 +136,7 @@ impl GasPaymentEnforcer {
 mod test {
     use std::str::FromStr;
 
-    use hyperlane_base::db::{test_utils, HyperlaneDB};
+    use hyperlane_base::db::{test_utils, HyperlaneRocksDB};
     use hyperlane_core::{HyperlaneDomain, HyperlaneMessage, TxCostEstimate, H160, H256, U256};
 
     use crate::settings::{
@@ -148,7 +148,7 @@ mod test {
     #[tokio::test]
     async fn test_empty_whitelist() {
         test_utils::run_test_db(|db| async move {
-            let hyperlane_db = HyperlaneDB::new(
+            let hyperlane_db = HyperlaneRocksDB::new(
                 &HyperlaneDomain::new_test_domain("test_empty_whitelist"),
                 db,
             );
@@ -185,7 +185,7 @@ mod test {
         #[allow(unused_must_use)]
         test_utils::run_test_db(|db| async move {
             let hyperlane_db =
-                HyperlaneDB::new(&HyperlaneDomain::new_test_domain("test_no_match"), db);
+                HyperlaneRocksDB::new(&HyperlaneDomain::new_test_domain("test_no_match"), db);
             let matching_list = serde_json::from_str(r#"[{"originDomain": 234}]"#).unwrap();
             let enforcer = GasPaymentEnforcer::new(
                 // Require a payment
@@ -212,7 +212,7 @@ mod test {
     #[tokio::test]
     async fn test_non_empty_matching_list() {
         test_utils::run_test_db(|db| async move {
-            let hyperlane_db = HyperlaneDB::new(&HyperlaneDomain::new_test_domain("test_non_empty_matching_list"), db);
+            let hyperlane_db = HyperlaneRocksDB::new(&HyperlaneDomain::new_test_domain("test_non_empty_matching_list"), db);
 
             let sender_address = "0xaa000000000000000000000000000000000000aa";
             let recipient_address = "0xbb000000000000000000000000000000000000bb";

commit 041cf92d5722356b00c3709c146c4a30c183f0b4

Fix insertion errors for txn and block (#2629)
### Description

This resolves the insertion conflict that is being caused by #2246 that
introduced multiple concurrent tasks to scrape. This does not address
the underlying concurrency issue I discovered, however. Please review
the issue comments for more details.

### Drive-by changes

- Cleans up logs a bit (This was useful during testing and I think is
useful globally)
- Removed an unused file

### Related issues

Fixes hyperlane-xyz/issues#493

### Backward compatibility

Yes

### Testing

Manual

rust/agents/scraper/src/chain_scraper/mod.rs
@@ -17,9 +17,8 @@ use hyperlane_core::{
 };
 
 use crate::db::StorablePayment;
-use crate::{
-    date_time,
-    db::{BasicBlock, BlockCursor, ScraperDb, StorableDelivery, StorableMessage, StorableTxn},
+use crate::db::{
+    BasicBlock, BlockCursor, ScraperDb, StorableDelivery, StorableMessage, StorableTxn,
 };
 
 /// Maximum number of records to query at a time. This came about because when a
@@ -150,20 +149,23 @@ impl HyperlaneSqlDb {
         let mut txns_to_fetch = txns.iter_mut().filter(|(_, id)| id.0.is_none());
 
         let mut txns_to_insert: Vec<StorableTxn> = Vec::with_capacity(CHUNK_SIZE);
+        let mut hashes_to_insert: Vec<&H256> = Vec::with_capacity(CHUNK_SIZE);
+
         for mut chunk in as_chunks::<(&H256, &mut (Option<i64>, i64))>(txns_to_fetch, CHUNK_SIZE) {
             for (hash, (_, block_id)) in chunk.iter() {
                 let info = self.provider.get_txn_by_hash(hash).await?;
+                hashes_to_insert.push(*hash);
                 txns_to_insert.push(StorableTxn {
                     info,
                     block_id: *block_id,
                 });
             }
 
-            let mut cur_id = self.db.store_txns(txns_to_insert.drain(..)).await?;
-            for (_hash, (txn_id, _block_id)) in chunk.iter_mut() {
-                debug_assert!(cur_id > 0);
-                let _ = txn_id.insert(cur_id);
-                cur_id += 1;
+            self.db.store_txns(txns_to_insert.drain(..)).await?;
+            let ids = self.db.get_txn_ids(hashes_to_insert.drain(..)).await?;
+
+            for (hash, (txn_id, _block_id)) in chunk.iter_mut() {
+                let _ = txn_id.insert(ids[hash]);
             }
         }
 
@@ -216,6 +218,7 @@ impl HyperlaneSqlDb {
 
         let mut blocks_to_insert: Vec<(&mut BasicBlock, Option<BlockInfo>)> =
             Vec::with_capacity(CHUNK_SIZE);
+        let mut hashes_to_insert: Vec<&H256> = Vec::with_capacity(CHUNK_SIZE);
         for chunk in as_chunks(blocks_to_fetch, CHUNK_SIZE) {
             debug_assert!(!chunk.is_empty());
             for (hash, block_info) in chunk {
@@ -223,13 +226,12 @@ impl HyperlaneSqlDb {
                 let basic_info_ref = block_info.insert(BasicBlock {
                     id: -1,
                     hash: *hash,
-                    timestamp: date_time::from_unix_timestamp_s(info.timestamp),
                 });
                 blocks_to_insert.push((basic_info_ref, Some(info)));
+                hashes_to_insert.push(hash);
             }
 
-            let mut cur_id = self
-                .db
+            self.db
                 .store_blocks(
                     self.domain().id(),
                     blocks_to_insert
@@ -238,9 +240,16 @@ impl HyperlaneSqlDb {
                 )
                 .await?;
 
+            let hashes = self
+                .db
+                .get_block_basic(hashes_to_insert.drain(..))
+                .await?
+                .into_iter()
+                .map(|b| (b.hash, b.id))
+                .collect::<HashMap<_, _>>();
+
             for (block_ref, _) in blocks_to_insert.drain(..) {
-                block_ref.id = cur_id;
-                cur_id += 1;
+                block_ref.id = hashes[&block_ref.hash];
             }
         }
 

rust/agents/scraper/src/db/block.rs
@@ -6,6 +6,7 @@ use sea_orm::{
 use tracing::{debug, trace};
 
 use hyperlane_core::{BlockInfo, H256};
+use migration::OnConflict;
 
 use crate::conversions::{address_to_bytes, h256_to_bytes};
 use crate::date_time;
@@ -21,7 +22,6 @@ pub struct BasicBlock {
     /// the database id of this block
     pub id: i64,
     pub hash: H256,
-    pub timestamp: TimeDateTime,
 }
 
 impl FromQueryResult for BasicBlock {
@@ -29,7 +29,6 @@ impl FromQueryResult for BasicBlock {
         Ok(Self {
             id: res.try_get::<i64>(pre, "id")?,
             hash: H256::from_slice(&res.try_get::<Vec<u8>>(pre, "hash")?),
-            timestamp: res.try_get::<TimeDateTime>(pre, "timestamp")?,
         })
     }
 }
@@ -68,7 +67,6 @@ impl ScraperDb {
             // these must align with the custom impl of FromQueryResult
             .column_as(block::Column::Id, "id")
             .column_as(block::Column::Hash, "hash")
-            .column_as(block::Column::Timestamp, "timestamp")
             .into_model::<BasicBlock>()
             .all(&self.0)
             .await
@@ -83,7 +81,7 @@ impl ScraperDb {
         &self,
         domain: u32,
         blocks: impl Iterator<Item = BlockInfo>,
-    ) -> Result<i64> {
+    ) -> Result<()> {
         let models = blocks
             .map(|info| block::ActiveModel {
                 id: NotSet,
@@ -96,11 +94,20 @@ impl ScraperDb {
             .collect::<Vec<_>>();
 
         debug_assert!(!models.is_empty());
-        let id_offset = models.len() as i64 - 1;
         debug!(blocks = models.len(), "Writing blocks to database");
         trace!(?models, "Writing blocks to database");
-        let first_id = Insert::many(models).exec(&self.0).await?.last_insert_id - id_offset;
-        debug_assert!(first_id > 0);
-        Ok(first_id)
+        match Insert::many(models)
+            .on_conflict(
+                OnConflict::column(block::Column::Hash)
+                    .do_nothing()
+                    .to_owned(),
+            )
+            .exec(&self.0)
+            .await
+        {
+            Ok(_) => Ok(()),
+            Err(DbErr::RecordNotInserted) => Ok(()),
+            Err(e) => Err(e).context("When inserting blocks"),
+        }
     }
 }

rust/agents/scraper/src/db/txn.rs
@@ -1,6 +1,7 @@
 use std::collections::HashMap;
 
 use eyre::{eyre, Context, Result};
+use sea_orm::sea_query::OnConflict;
 use sea_orm::{prelude::*, ActiveValue::*, DeriveColumn, EnumIter, Insert, NotSet, QuerySelect};
 use tracing::{debug, instrument, trace};
 
@@ -66,7 +67,7 @@ impl ScraperDb {
 
     /// Store a new transaction into the database (or update an existing one).
     #[instrument(skip_all)]
-    pub async fn store_txns(&self, txns: impl Iterator<Item = StorableTxn>) -> Result<i64> {
+    pub async fn store_txns(&self, txns: impl Iterator<Item = StorableTxn>) -> Result<()> {
         let models = txns
             .map(|txn| {
                 let receipt = txn
@@ -96,11 +97,21 @@ impl ScraperDb {
             .collect::<Result<Vec<_>>>()?;
 
         debug_assert!(!models.is_empty());
-        let id_offset = models.len() as i64 - 1;
         debug!(txns = models.len(), "Writing txns to database");
         trace!(?models, "Writing txns to database");
-        let first_id = Insert::many(models).exec(&self.0).await?.last_insert_id - id_offset;
-        debug_assert!(first_id > 0);
-        Ok(first_id)
+
+        match Insert::many(models)
+            .on_conflict(
+                OnConflict::column(transaction::Column::Hash)
+                    .do_nothing()
+                    .to_owned(),
+            )
+            .exec(&self.0)
+            .await
+        {
+            Ok(_) => Ok(()),
+            Err(DbErr::RecordNotInserted) => Ok(()),
+            Err(e) => Err(e).context("When inserting transactions"),
+        }
     }
 }

rust/hyperlane-base/src/settings/trace/mod.rs
@@ -37,6 +37,8 @@ pub enum Level {
     Debug = 3,
     /// Trace
     Trace = 5,
+    /// Trace + Additional logs from dependencies
+    DependencyTrace = 6,
     /// Info
     #[serde(other)]
     #[default]
@@ -50,7 +52,7 @@ impl From<Level> for LevelFilter {
             Level::Error => LevelFilter::ERROR,
             Level::Warn => LevelFilter::WARN,
             Level::Debug => LevelFilter::DEBUG,
-            Level::Trace => LevelFilter::TRACE,
+            Level::Trace | Level::DependencyTrace => LevelFilter::TRACE,
             Level::Info => LevelFilter::INFO,
         }
     }
@@ -72,12 +74,19 @@ impl TracingConfig {
     /// settings.
     pub fn start_tracing(&self, metrics: &CoreMetrics) -> Result<()> {
         let mut target_layer = Targets::new().with_default(self.level);
-        if self.level < Level::Trace {
-            // only show these debug and trace logs at trace level
-            target_layer = target_layer.with_target("hyper", Level::Info);
-            target_layer = target_layer.with_target("rusoto_core", Level::Info);
-            target_layer = target_layer.with_target("reqwest", Level::Info);
 
+        if self.level < Level::DependencyTrace {
+            // Reduce log noise from trusted libraries that we can reasonably assume are working correctly
+            target_layer = target_layer
+                .with_target("hyper", Level::Info)
+                .with_target("rusoto_core", Level::Info)
+                .with_target("reqwest", Level::Info)
+                .with_target("tokio", Level::Debug)
+                .with_target("tokio_util", Level::Debug)
+                .with_target("ethers_providers", Level::Debug);
+        }
+
+        if self.level < Level::Trace {
             // only show sqlx query logs at trace level
             target_layer = target_layer.with_target("sqlx::query", Level::Warn);
         }