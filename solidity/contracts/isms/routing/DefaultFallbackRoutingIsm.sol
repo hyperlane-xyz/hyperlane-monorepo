b23d3119c055c54873e560f18d910d58228ac01e
Yorke Rhodes <yorke@hyperlane.xyz>
2023-08-10 20:08:45 -0400

    Fix default fallback

--- solidity/contracts/isms/routing/DefaultFallbackRoutingIsm.sol
+++ solidity/contracts/isms/routing/DefaultFallbackRoutingIsm.sol
@@ -2,8 +2,15 @@
 pragma solidity >=0.8.0;
 
 import {DomainRoutingIsm} from "./DomainRoutingIsm.sol";
+import {IMailbox} from "../../Mailbox.sol";
+import {IInterchainSecurityModule} from "../../interfaces/IInterchainSecurityModule.sol";
+import {EnumerableMapExtended} from "../../libs/EnumerableMapExtended.sol";
+import {TypeCasts} from "../../libs/TypeCasts.sol";
 
 contract DefaultFallbackRoutingIsm is DomainRoutingIsm {
+    using EnumerableMapExtended for EnumerableMapExtended.UintToBytes32Map;
+    using TypeCasts for bytes32;
+
     IMailbox public immutable mailbox;
 
     constructor(address _mailbox) {
@@ -13,12 +20,12 @@ contract DefaultFallbackRoutingIsm is DomainRoutingIsm {
     function module(uint32 origin)
         public
         view
-        virtual
+        override
         returns (IInterchainSecurityModule)
     {
         (bool contained, bytes32 _module) = modules.tryGet(origin);
         if (contained) {
-            return _module;
+            return IInterchainSecurityModule(_module.bytes32ToAddress());
         } else {
             return mailbox.defaultIsm();
         }
