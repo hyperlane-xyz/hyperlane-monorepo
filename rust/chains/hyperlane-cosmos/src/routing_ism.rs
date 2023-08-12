5222983d2ccbd58bee95f9d3446547ffd2a65872
hashableric <hashableric@gmail.com>
2023-08-12 16:47:51 +0900

    feat: apply changes into routing isms

--- rust/chains/hyperlane-cosmos/src/routing_ism.rs
+++ rust/chains/hyperlane-cosmos/src/routing_ism.rs
@@ -2,69 +2,50 @@ use async_trait::async_trait;
 
 use cosmrs::crypto::secp256k1::SigningKey;
 use hyperlane_core::{
-    ChainResult, HyperlaneChain, HyperlaneContract, HyperlaneDomain, HyperlaneMessage,
-    HyperlaneProvider, RawHyperlaneMessage, RoutingIsm, H256,
+    ChainResult, ContractLocator, HyperlaneChain, HyperlaneContract, HyperlaneDomain,
+    HyperlaneMessage, HyperlaneProvider, RawHyperlaneMessage, RoutingIsm, H256,
 };
 
 use crate::{
     grpc::{WasmGrpcProvider, WasmProvider},
     payloads::ism_routes::{IsmRouteRequest, IsmRouteRequestInner, IsmRouteRespnose},
+    signers::Signer,
     verify::{self, bech32_decode},
+    ConnectionConf,
 };
 
 /// A reference to a RoutingIsm contract on some Cosmos chain
 #[derive(Debug)]
-pub struct CosmosRoutingIsm {
-    domain: HyperlaneDomain,
-    address: String,
-    provider: Box<WasmGrpcProvider>,
+pub struct CosmosRoutingIsm<'a> {
+    conf: &'a ConnectionConf,
+    locator: &'a ContractLocator<'a>,
+    signer: &'a Signer,
+    provider: Box<WasmGrpcProvider<'a>>,
 }
 
-impl CosmosRoutingIsm {
+impl CosmosRoutingIsm<'_> {
     /// create a new instance of CosmosRoutingIsm
-    pub fn new(
-        domain: HyperlaneDomain,
-        address: String,
-        prefix: String,
-        private_key: Vec<u8>,
-        grpc_endpoint: String,
-        chain_id: String,
-    ) -> Self {
-        let signer_address = verify::pub_to_addr(
-            SigningKey::from_slice(&private_key)
-                .unwrap()
-                .public_key()
-                .to_bytes(),
-            &prefix,
-        )
-        .unwrap();
-
-        let provider = WasmGrpcProvider::new(
-            address.clone(),
-            private_key,
-            signer_address,
-            prefix,
-            grpc_endpoint,
-            chain_id,
-        );
+    pub fn new(conf: &ConnectionConf, locator: &ContractLocator, signer: &Signer) -> Self {
+        let provider = WasmGrpcProvider::new(conf, locator, signer);
 
         Self {
-            domain,
-            address,
+            conf,
+            locator,
+            signer,
             provider: Box::new(provider),
         }
     }
 }
 
-impl HyperlaneContract for CosmosRoutingIsm {
+impl HyperlaneContract for CosmosRoutingIsm<'_> {
     fn address(&self) -> H256 {
-        bech32_decode(self.address.clone())
+        self.locator.address
     }
 }
 
-impl HyperlaneChain for CosmosRoutingIsm {
+impl HyperlaneChain for CosmosRoutingIsm<'_> {
     fn domain(&self) -> &HyperlaneDomain {
-        &self.domain
+        &self.locator.domain
     }
 
     fn provider(&self) -> Box<dyn HyperlaneProvider> {
@@ -73,7 +54,7 @@ impl HyperlaneChain for CosmosRoutingIsm {
 }
 
 #[async_trait]
-impl RoutingIsm for CosmosRoutingIsm {
+impl RoutingIsm for CosmosRoutingIsm<'_> {
     async fn route(&self, message: &HyperlaneMessage) -> ChainResult<H256> {
         let payload = IsmRouteRequest {
             route: IsmRouteRequestInner {
