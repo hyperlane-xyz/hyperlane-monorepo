6446e17b4942ab3b228d4334327c3a0756a22c56
Spannos <cspannos@blockdaemon.com>
2023-08-17 00:51:58 +0100

    added external imports, 'ownable' and 'address' from OpenZeppelin

--- solidity/contracts/isms/optimistic-ism/OptimisticIsm.sol
+++ solidity/contracts/isms/optimistic-ism/OptimisticIsm.sol
@@ -1,2 +1,6 @@
 // SPDX-License-Identifier: MIT OR Apache-2.0
 pragma solidity >=0.8.0;
+
+// ============ External Imports ============
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {Address} from "@openzeppelin/contracts/utils/Address.sol";
