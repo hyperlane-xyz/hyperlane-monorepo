6040c331c43c8e266cb6890c8f2e66a7c36a98d0
Spannos <cspannos@blockdaemon.com>
2023-08-17 01:16:57 +0100

    initiated contarct, added events

--- solidity/contracts/isms/optimistic-ism/OptimisticIsm.sol
+++ solidity/contracts/isms/optimistic-ism/OptimisticIsm.sol
@@ -9,3 +9,12 @@ import {Address} from "@openzeppelin/contracts/utils/Address.sol";
 import {IOptimisticIsm} from "../../interfaces/isms/IOptimisticIsm.sol";
 import {IInterchainSecurityModule} from "../../interfaces/IInterchainSecurityModule.sol";
 import {Message} from "../../libs/Message.sol";
+
+// ============ CONTRACT ============
+abstract contract OptimisticIsm is IOptimisticIsm, OwnableUpgradeable {
+    // ============ Events ============
+    event RelayerCalledMessagePreVerify(address indexed _relayer);
+    event MessageDelivered(bytes indexed _message);
+    event SubmoduleChanged(IInterchainSecurityModule _module);
+    event FraudWindowOpened(IInterchainSecurityModule _module);
+    event SubmoduleMarkedFraudulent(IInterchainSecurityModule _module);
