ed6475c089f42710bb499702f2260bb521ff71d0
Spannos <cspannos@blockdaemon.com>
2023-08-17 01:33:57 +0100

    added public functions

--- solidity/contracts/isms/optimistic-ism/OptimisticIsm.sol
+++ solidity/contracts/isms/optimistic-ism/OptimisticIsm.sol
@@ -93,4 +93,90 @@ abstract contract OptimisticIsm is IOptimisticIsm, OwnableUpgradeable {
         fraudWindow = _newFraudWindow;
     }
 
+  // ============ External/Public Functions ============
+    /**
+     * @notice checks to see if:
+     * 1	The message has been pre-verified
+     * 2	The submodule used to pre-verify the message has not been flagged as compromised by m-of-n watchers
+     * 3	The fraud window has elapsed
+     */
+    function preVerifiedCheck(bytes calldata _metadata, bytes calldata _message)
+        public
+        returns (bool)
+    {
+        IInterchainSecurityModule currentModule = submodule(_message);
+        if (
+            relayers[msg.sender] &&
+            !subModuleFlags[currentModule] &&
+            _checkFraudWindow(_message)
+        ) {
+            return true;
+        }
+    }
+
+    /**
+     * @notice allows owner to modify ISM being used for message verification
+     * @param _domain origin domain of the ISM
+     * @param _module alternative ISM module to be used
+     */
+    function setIsm(uint32 _domain, IInterchainSecurityModule _module)
+        external
+        onlyOwner
+    {
+        _set(_domain, _module);
+        emit SubmoduleChanged(_module);
+    }
+
+    /**
+     * @notice returns the ISM responsible for verifying _message
+     * @dev changes based on the content of _message
+     * @param _message formatted Hyperlane message (see Message.sol).
+     * @return module ISM being used to verify _message
+     */
+    function submodule(bytes calldata _message)
+        public
+        view
+        override
+        returns (IInterchainSecurityModule)
+    {
+        IInterchainSecurityModule module = module[Message.origin(_message)];
+        require(
+            address(module) != address(0),
+            "No ISM found for origin domain"
+        );
+        return module;
+    }
+
+    /**
+     * @notice allows watchers added by owner to flag ISM submodule(s) as fraudulent
+     * @param _message formatted Hyperlane message (see Message.sol).
+     */
+    function markFraudulent(bytes calldata _message)
+        external
+        onlyWatcher(msg.sender)
+    {
+        IInterchainSecurityModule thisModule = submodule(_message);
+        subModuleFlags[thisModule] = true;
+        emit SubmoduleMarkedFraudulent(thisModule);
+    }
+
+    /**
+     * @notice allows owner to add watchers to watchers mapping
+     * @param _watchersArray array of watcher addresses
+     */
+    function addWatchers(address[] calldata _watchersArray) external onlyOwner {
+        uint8 i;
+        for (i = 0; i < _watchersArray.length; i++) {
+            watchers[_watchersArray[i]] = true;
+        }
+    }
+
+    /**
+     * @notice allows owner to mark watchers as redunant in watchers mapping
+     * @param _watcherToBeRemoved address of watcher to be made redunant
+     */
+    function removeWatcher(address _watcherToBeRemoved) external onlyOwner {
+        watchers[_watcherToBeRemoved] = false;
+    }
+
 
