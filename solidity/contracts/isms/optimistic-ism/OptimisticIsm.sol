bcaaf367b338732bda7e8a55c3718d399e61cd7d
Spannos <cspannos@blockdaemon.com>
2023-08-17 01:00:48 +0100

    set internal imports'

--- solidity/contracts/isms/optimistic-ism/OptimisticIsm.sol
+++ solidity/contracts/isms/optimistic-ism/OptimisticIsm.sol
@@ -4,3 +4,8 @@ pragma solidity >=0.8.0;
 // ============ External Imports ============
 import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import {Address} from "@openzeppelin/contracts/utils/Address.sol";
+
+// ============ Internal Imports ============
+import {IOptimisticIsm} from "./IOptimisticIsm.sol";
+import {IInterchainSecurityModule} from "../../interfaces/IInterchainSecurityModule.sol";
+import {Message} from "../../libs/Message.sol";
