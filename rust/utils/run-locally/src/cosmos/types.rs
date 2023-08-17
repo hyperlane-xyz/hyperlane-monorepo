d79c398a5c086c08c423c4e3ecf5aa40d906a403
byeongsu-hong <hong@byeongsu.dev>
2023-08-17 11:19:09 +0000

    feat: wasm helpers

--- rust/utils/run-locally/src/cosmos/types.rs
+++ rust/utils/run-locally/src/cosmos/types.rs
@@ -1,25 +1,25 @@
 #[derive(serde::Serialize, serde::Deserialize)]
-pub(crate) struct TxEventAttr {
+pub struct TxEventAttr {
     pub key: String,
     pub value: String,
 }
 
 #[derive(serde::Serialize, serde::Deserialize)]
-pub(crate) struct TxEvent {
+pub struct TxEvent {
     #[serde(rename = "type")]
     pub typ: String,
     pub attributes: Vec<TxEventAttr>,
 }
 
 #[derive(serde::Serialize, serde::Deserialize)]
-pub(crate) struct TxLog {
+pub struct TxLog {
     pub msg_index: u32,
     pub log: String,
     pub events: Vec<TxEvent>,
 }
 
 #[derive(serde::Serialize, serde::Deserialize)]
-pub(crate) struct TxResponse {
+pub struct TxResponse {
     pub height: String,
     pub txhash: String,
     pub codespace: String,
@@ -27,4 +27,10 @@ pub(crate) struct TxResponse {
     pub data: String,
     pub raw_log: String,
     pub logs: Vec<TxLog>,
+}
+
+#[derive(serde::Serialize, serde::Deserialize)]
+pub struct Coin {
+    pub denom: String,
+    pub amount: String,
 }
