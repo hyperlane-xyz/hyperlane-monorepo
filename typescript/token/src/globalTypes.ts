7dd38cdb19475e49575adfd04a0388ac3a752248
Kunal Arora <kunalarora1729@gmail.com>
2023-08-21 11:14:05 -0400

    global types

---
+++ typescript/token/src/globalTypes.ts
@@ -0,0 +1,3 @@
+export type DomainId = number;
+export type Caip2Id = `${string}:${string}`; // e.g. ethereum:1 or solana:mainnet-beta
+export type Caip19Id = `${string}:${string}/${string}:${string}`; // e.g. ethereum:1/erc20:0x6b175474e89094c44da98b954eedeac495271d0f
