2f987fcca37c098f59d6d003cdc700b05b6d49fa
Kunal Arora <kunalarora1729@gmail.com>
2023-08-22 18:57:58 -0400

    wafrp routes helm

---
+++ typescript/infra/scripts/warp-routes/helm.ts
@@ -0,0 +1,29 @@
+import { HelmCommand } from '../../src/utils/helm';
+import { execCmd } from '../../src/utils/utils';
+import { assertCorrectKubeContext, getEnvironmentConfig } from '../utils';
+
+export async function runWarpRouteHelmCommand(helmCommand: HelmCommand) {
+  //   const values = getWarpRoutesHelmValues();
+  const mainnetConfig = getEnvironmentConfig('mainnet2');
+  await assertCorrectKubeContext(mainnetConfig);
+  // TODO from config
+  return execCmd(
+    `helm ${helmCommand} ${getHelmReleaseName(
+      'zebec',
+    )} ./helm/warp-routes --namespace mainnet2 --set image.repository="gcr.io/abacus-labs-dev/hyperlane-monorepo" --set image.tag="4c598b9-20230503-205323"`,
+  );
+}
+
+function getHelmReleaseName(route: string): string {
+  return `helloworld-warp-route-${route}`;
+}
+
+// function getWarpRoutesHelmValues() {
+//   const values = {
+//     image: {
+//       repository: warpRouteConfig.docker.repo,
+//       tag: warpRouteConfig.docker.tag,
+//     },
+//   };
+//   return helmifyValues(values);
+// }
---
+++ typescript/infra/src/config/warp-routes.ts
@@ -0,0 +1,40 @@
+import { AgentConnectionType, ChainMap, ChainName } from '@hyperlane-xyz/sdk';
+
+import { DockerConfig } from './agent';
+
+export enum HelloWorldKathyRunMode {
+  // Sends messages between all pairwise chains
+  CycleOnce,
+  // Long-running service, sending messages according to a full cycle time
+  Service,
+}
+
+export interface HelloWorldKathyConfig {
+  docker: DockerConfig;
+  runEnv: string;
+  namespace: string;
+  chainsToSkip: ChainName[];
+  runConfig:
+    | {
+        mode: HelloWorldKathyRunMode.CycleOnce;
+      }
+    | {
+        mode: HelloWorldKathyRunMode.Service;
+        /** How long kathy should take to send a message to all chain pairs before looping (milliseconds) */
+        fullCycleTime: number;
+      };
+  /** How long kathy should wait before declaring an attempted to send a failure (milliseconds). */
+  messageSendTimeout: number;
+  /** How long kathy should wait before giving up on waiting for the message to be received (milliseconds). */
+  messageReceiptTimeout: number;
+
+  // Which type of provider to use
+  connectionType: Exclude<AgentConnectionType, AgentConnectionType.Ws>;
+  // How many cycles to skip between a cycles that send messages to/from Ethereum. Defaults to 0.
+  cyclesBetweenEthereumMessages?: number;
+}
+
+export interface HelloWorldConfig {
+  addresses: ChainMap<{ router: string }>;
+  kathy: HelloWorldKathyConfig;
+}
