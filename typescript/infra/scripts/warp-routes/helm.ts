19f98183c22c74afff472a16dc77d1cc9b387f0c
Kunal Arora <kunalarora1729@gmail.com>
2023-08-23 12:20:38 -0400

    updated monitor script file name

--- typescript/infra/scripts/warp-routes/helm.ts
+++ typescript/infra/scripts/warp-routes/helm.ts
@@ -10,7 +10,7 @@ export async function runWarpRouteHelmCommand(helmCommand: HelmCommand) {
   return execCmd(
     `helm ${helmCommand} ${getHelmReleaseName(
       'zebec',
-    )} ./helm/warp-routes --namespace mainnet2 --set image.repository="gcr.io/abacus-labs-dev/hyperlane-monorepo" --set image.tag="c328618-20230822-230047"`,
+    )} ./helm/warp-routes --namespace mainnet2 --set image.repository="gcr.io/abacus-labs-dev/hyperlane-monorepo" --set image.tag="0608468-20230823-155234"`,
   );
 }
 
