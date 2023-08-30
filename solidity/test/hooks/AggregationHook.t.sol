179af0574148b0b3d4e4e6b9f456d6714568d6b6
Kunal Arora <kunalarora1729@gmail.com>
2023-08-30 08:24:56 -0700

    test for msgvalue

--- solidity/test/hooks/AggregationHook.t.sol
+++ solidity/test/hooks/AggregationHook.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.8.13;
 
-import "forge-std/Test.sol";
+import {Test} from "forge-std/Test.sol";
 
 import {StaticAggregationHook} from "../../contracts/hooks/aggregation/StaticAggregationHook.sol";
 import {StaticAggregationHookFactory} from "../../contracts/hooks/aggregation/StaticAggregationHookFactory.sol";
@@ -15,14 +15,9 @@ contract AggregationHookTest is Test {
         factory = new StaticAggregationHookFactory();
     }
 
-    function deployHooks(uint8 n, bytes32 seed)
-        internal
-        returns (address[] memory)
-    {
-        bytes32 randomness = seed;
+    function deployHooks(uint8 n) internal returns (address[] memory) {
         address[] memory hooks = new address[](n);
         for (uint8 i = 0; i < n; i++) {
-            randomness = keccak256(abi.encode(randomness));
             TestPostDispatchHook subHook = new TestPostDispatchHook();
             hooks[i] = address(subHook);
         }
@@ -30,19 +25,15 @@ contract AggregationHookTest is Test {
         return hooks;
     }
 
-    function getMetadata() private view returns (bytes memory) {
-        address[] memory choices = hook.hooks("");
-        return abi.encode(choices);
-    }
-
-    function testPostDispatch(uint8 _hooks, bytes32 _seed) public {
-        address[] memory hooksDeployed = deployHooks(_hooks, bytes32(_seed));
+    function testPostDispatch(uint8 _hooks) public {
+        address[] memory hooksDeployed = deployHooks(_hooks);
         uint256 _msgValue = hooksDeployed.length * 25000;
 
         bytes memory message = abi.encodePacked("hello world");
         for (uint256 i = 0; i < hooksDeployed.length; i++) {
             vm.expectCall(
                 hooksDeployed[i],
+                25000,
                 abi.encodeCall(
                     TestPostDispatchHook(hooksDeployed[i]).postDispatch,
                     ("", "hello world")
@@ -52,20 +43,16 @@ contract AggregationHookTest is Test {
         hook.postDispatch{value: _msgValue}("", message);
     }
 
-    function testPostDispatch_reverts_outOfFund(
-        uint8 _hooks,
-        bytes32 _seed,
-        uint8 k
-    ) public {
-        address[] memory hooksDeployed = deployHooks(_hooks, bytes32(_seed));
+    function testPostDispatch_reverts_outOfFund(uint8 _hooks, uint8 k) public {
+        address[] memory hooksDeployed = deployHooks(_hooks);
         vm.assume(k < hooksDeployed.length);
-        console.log("k: %s", k);
         uint256 _msgValue = uint256(k) * 25000;
 
         bytes memory message = abi.encodePacked("hello world");
         for (uint256 i = 0; i < k; i++) {
             vm.expectCall(
                 hooksDeployed[i],
+                25000,
                 abi.encodeCall(
                     TestPostDispatchHook(hooksDeployed[i]).postDispatch,
                     ("", "hello world")
@@ -76,8 +63,8 @@ contract AggregationHookTest is Test {
         hook.postDispatch{value: _msgValue}("", message);
     }
 
-    function testQuoteDispatch(uint8 _hooks, bytes32 _seed) public {
-        address[] memory hooksDeployed = deployHooks(_hooks, bytes32(_seed));
+    function testQuoteDispatch(uint8 _hooks) public {
+        address[] memory hooksDeployed = deployHooks(_hooks);
         uint256 _msgValue = hooksDeployed.length * 25000;
 
         bytes memory message = abi.encodePacked("hello world");
@@ -86,8 +73,8 @@ contract AggregationHookTest is Test {
         assertEq(totalQuote, _msgValue);
     }
 
-    function testMetadata(uint8 _hooks, bytes32 _seed) public {
-        address[] memory expectedHooks = deployHooks(_hooks, bytes32(_seed));
+    function testMetadata(uint8 _hooks) public {
+        address[] memory expectedHooks = deployHooks(_hooks);
         address[] memory actualHook = hook.hooks("");
         assertEq(actualHook, expectedHooks);
     }
