111ef1210b3be8d41d5c614d41ecdcf589d6e541
Mattie Conover <git@mconover.dev>
2023-09-01 17:15:15 -0700

    Cleanup dead code

--- typescript/sdk/src/metadata/agentConfig.ts
+++ typescript/sdk/src/metadata/agentConfig.ts
@@ -19,14 +19,6 @@ import {
 } from './deploymentArtifacts';
 import { MatchingListSchema } from './matchingList';
 
-// TODO(2214): Remove this commented out code
-// export enum AgentConnectionType {
-//   Http = 'http',
-//   Ws = 'ws',
-//   HttpQuorum = 'httpQuorum',
-//   HttpFallback = 'httpFallback',
-// }
-
 export enum RpcConsensusType {
   Single = 'single',
   Fallback = 'fallback',
@@ -308,82 +300,7 @@ export const ValidatorAgentConfigSchema = AgentConfigSchema.extend({
 
 export type ValidatorConfig = z.infer<typeof ValidatorAgentConfigSchema>;
 
-// TODO(2214): Rename this
 export type AgentConfig = z.infer<typeof AgentConfigSchema>;
-
-// TODO(2214): Remove this commented out code
-// export interface AgentSigner {
-//   key: string;
-//   type: string;
-// }
-//
-// export type AgentConnection =
-//   | { type: AgentConnectionType.Http; url: string }
-//   | { type: AgentConnectionType.Ws; url: string }
-//   | { type: AgentConnectionType.HttpQuorum; urls: string }
-//   | { type: AgentConnectionType.HttpFallback; urls: string };
-//
-// export interface AgentChainSetupBase {
-//   name: ChainName;
-//   domain: number;
-//   signer?: AgentSigner;
-//   finalityBlocks: number;
-//   addresses: HyperlaneDeploymentArtifacts;
-//   protocol: ProtocolType;
-//   connection?: AgentConnection;
-//   index?: { from: number };
-// }
-//
-// export interface AgentChainSetup extends AgentChainSetupBase {
-//   signer: AgentSigner;
-//   connection: AgentConnection;
-// }
-//
-// export interface AgentConfig {
-//   chains: Partial<ChainMap<AgentChainSetupBase>>;
-//   tracing?: {
-//     level?: string;
-//     fmt?: 'json';
-//   };
-// }
-
-/**
- * Utilities for generating agent configs from metadata / artifacts.
- */
-
-// // Returns the current (but deprecated) agent config shape.
-// export function buildAgentConfigDeprecated(
-//   chains: ChainName[],
-//   multiProvider: MultiProvider,
-//   addresses: ChainMap<HyperlaneDeploymentArtifacts>,
-//   startBlocks: ChainMap<number>,
-// ): AgentConfig {
-//   const agentConfig: AgentConfig = {
-//     chains: {},
-//   };
-//
-//   for (const chain of [...chains].sort()) {
-//     const metadata = multiProvider.getChainMetadata(chain);
-//     const chainConfig: AgentChainSetupBase = {
-//       name: chain,
-//       domain: metadata.chainId,
-//       addresses: {
-//         mailbox: addresses[chain].mailbox,
-//         interchainGasPaymaster: addresses[chain].interchainGasPaymaster,
-//         validatorAnnounce: addresses[chain].validatorAnnounce,
-//       },
-//       protocol: metadata.protocol,
-//       finalityBlocks: metadata.blocks?.reorgPeriod ?? 1,
-//     };
-//
-//     chainConfig.index = {
-//       from: startBlocks[chain],
-//     };
-//
-//     agentConfig.chains[chain] = chainConfig;
-//   }
-//   return agentConfig;
-// }
 
 export function buildAgentConfig(
   chains: ChainName[],
