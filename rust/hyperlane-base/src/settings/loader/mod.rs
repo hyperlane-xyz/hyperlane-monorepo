d9b2414c95e4c02b621a649b1c8a46828d906385
Mattie Conover <git@mconover.dev>
2023-09-06 16:18:16 -0700

    Fix loader to be debuggable

--- rust/hyperlane-base/src/settings/loader/mod.rs
+++ rust/hyperlane-base/src/settings/loader/mod.rs
@@ -1,10 +1,10 @@
 //! Load a settings object from the config locations.
 
-use std::{collections::HashMap, env, error::Error, fmt::Debug, path::PathBuf};
+use std::{env, error::Error, fmt::Debug, path::PathBuf};
 
 use config::{Config, File};
 use convert_case::Case;
-use eyre::{bail, Context, Result};
+use eyre::{eyre, Context, Result};
 use hyperlane_core::config::*;
 use serde::de::DeserializeOwned;
 
@@ -23,24 +23,6 @@ where
     R: FromRawConf<T>,
 {
     let root_path = ConfigPath::default();
-    let raw = load_settings_object::<T, &str>(&[]).into_config_result(|| root_path.clone())?;
-    raw.parse_config(&root_path)
-}
-
-/// Load a settings object from the config locations.
-/// Further documentation can be found in the `settings` module.
-fn load_settings_object<T, S>(ignore_prefixes: &[S]) -> Result<T>
-where
-    T: DeserializeOwned,
-    S: AsRef<str>,
-{
-    let filtered_env: HashMap<String, String> = env::vars()
-        .filter(|(k, _v)| {
-            !ignore_prefixes
-                .iter()
-                .any(|prefix| k.starts_with(prefix.as_ref()))
-        })
-        .collect();
 
     let mut base_config_sources = vec![];
     let mut builder = Config::builder();
@@ -48,7 +30,8 @@ where
     // Always load the default config files (`rust/config/*.json`)
     for entry in PathBuf::from("./config")
         .read_dir()
-        .expect("Failed to open config directory")
+        .context("Failed to open config directory")
+        .into_config_result(|| root_path.clone())?
         .map(Result::unwrap)
     {
         if !entry.file_type().unwrap().is_file() {
@@ -59,7 +42,7 @@ where
         let ext = fname.to_str().unwrap().split('.').last().unwrap_or("");
         if ext == "json" {
             base_config_sources.push(format!("{:?}", entry.path()));
-            builder = builder.add_source(File::from(entry.path()));
+            builder = builder.add_source(CaseAdapter::new(File::from(entry.path()), Case::Flat));
         }
     }
 
@@ -76,29 +59,37 @@ where
                 let re_cased_config_file = CaseAdapter::new(config_file, Case::Flat);
                 builder = builder.add_source(re_cased_config_file);
             } else {
-                bail!("Provided config path via CONFIG_FILES is of an unsupported type ({p:?})")
+                return Err(eyre!(
+                    "Provided config path via CONFIG_FILES is of an unsupported type ({p:?})"
+                ))
+                .into_config_result(|| root_path.clone());
             }
         } else if !p.exists() {
-            bail!("Provided config path via CONFIG_FILES does not exist ({p:?})")
+            return Err(eyre!(
+                "Provided config path via CONFIG_FILES does not exist ({p:?})"
+            ))
+            .into_config_result(|| root_path.clone());
         } else {
-            bail!("Provided config path via CONFIG_FILES is not a file ({p:?})")
+            return Err(eyre!(
+                "Provided config path via CONFIG_FILES is not a file ({p:?})"
+            ))
+            .into_config_result(|| root_path.clone());
         }
     }
 
     let config_deserializer = builder
         // Use a base configuration env variable prefix
         .add_source(CaseAdapter::new(
-            Environment::default()
-                .prefix("HYP_")
-                .separator("_")
-                .source(&filtered_env),
+            Environment::default().prefix("HYP_").separator("_"),
             Case::Flat,
         ))
         .add_source(CaseAdapter::new(
             CommandLineArguments::default().separator("."),
             Case::Flat,
         ))
-        .build()?;
+        .build()
+        .context("Failed to load config sources")
+        .into_config_result(|| root_path.clone())?;
 
     let formatted_config = {
         let f = format!("{config_deserializer:#?}");
@@ -113,19 +104,26 @@ where
         }
     };
 
-    Config::try_deserialize::<T>(config_deserializer).or_else(|err| {
-        let mut err = if let Some(source_err) = err.source() {
-            let source = format!("Config error source: {source_err}");
-            Err(err).context(source)
-        } else {
-            Err(err.into())
-        };
+    let raw_config = Config::try_deserialize::<T>(config_deserializer)
+        .or_else(|err| {
+            let mut err = if let Some(source_err) = err.source() {
+                let source = format!("Config error source: {source_err}");
+                Err(err).context(source)
+            } else {
+                Err(err.into())
+            };
 
-        for cfg_path in base_config_sources.iter().chain(config_file_paths.iter()) {
-            err = err.with_context(|| format!("Config loaded: {cfg_path}"));
-        }
+            for cfg_path in base_config_sources.iter().chain(config_file_paths.iter()) {
+                err = err.with_context(|| format!("Config loaded: {cfg_path}"));
+            }
+            eprintln!("Loaded config for debugging: {formatted_config}");
+            err.context("Config deserialization error, please check the config reference (https://docs.hyperlane.xyz/docs/operators/agent-configuration/configuration-reference)")
+        })
+        .into_config_result(|| root_path.clone())?;
 
-        println!("Error during deserialization, showing the config for debugging: {formatted_config}");
-        err.context("Config deserialization error, please check the config reference (https://docs.hyperlane.xyz/docs/operators/agent-configuration/configuration-reference)")
-    })
+    let res = raw_config.parse_config(&root_path);
+    if res.is_err() {
+        eprintln!("Loaded config for debugging: {formatted_config}");
+    }
+    res
 }
