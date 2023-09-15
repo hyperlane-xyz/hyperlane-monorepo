1384d76da7ee0daaff33529e2220fae18521e047
-f <kunalarora1729@gmail.com>
2023-09-15 15:54:37 -0400

    separate test for merkle hook

--- solidity/test/merkle.test.ts
+++ solidity/test/merkle.test.ts
@@ -1,31 +1,21 @@
 import { expect } from 'chai';
-import { keccak256 } from 'ethers/lib/utils';
 import { ethers } from 'hardhat';
 
-import { addressToBytes32 } from '@hyperlane-xyz/utils/dist/src/utils';
-
 import merkleTestCases from '../../vectors/merkle.json';
-import {
-  TestMailbox,
-  TestMailbox__factory,
-  TestMerkleTreeHook,
-  TestMerkleTreeHook__factory,
-} from '../types';
+import { TestMerkle, TestMerkle__factory } from '../types';
 
 describe('Merkle', async () => {
   for (const testCase of merkleTestCases) {
     const { testName, leaves, expectedRoot, proofs } = testCase;
 
     describe(testName, async () => {
-      let merkle: TestMerkleTreeHook;
-      let mailboxFactory: TestMailbox;
+      let merkle: TestMerkle;
 
       before(async () => {
         const [signer] = await ethers.getSigners();
-        mailboxFactory = await new TestMailbox__factory(signer).deploy(1);
 
-        const merkleFactory = new TestMerkleTreeHook__factory(signer);
-        merkle = await merkleFactory.deploy(mailboxFactory.address);
+        const merkleFactory = new TestMerkle__factory(signer);
+        merkle = await merkleFactory.deploy();
 
         //insert the leaves
         for (const leaf of leaves) {
@@ -43,19 +33,13 @@ describe('Merkle', async () => {
         expect(await merkle.root()).to.equal(expectedRoot);
       });
 
-      it("emit 'InsertedIntoTree' events on postDispatch", async () => {
-        const [signer] = await ethers.getSigners();
-        const message = await mailboxFactory.buildOutboundMessage(
-          1,
-          addressToBytes32(signer.address),
-          '0x',
-        );
-        const messageId = await keccak256(message);
-        await mailboxFactory.updateLatestDispatchedId(messageId);
-        await expect(merkle.postDispatch('0x', message, { value: 0 })).to.emit(
-          merkle,
-          'InsertedIntoTree',
-        );
+      it("can verify the leaves' proofs", async () => {
+        for (const proof of proofs) {
+          const { leaf, path, index } = proof;
+
+          const proofRoot = await merkle.branchRoot(leaf, path, index);
+          expect(proofRoot).to.equal(expectedRoot);
+        }
       });
     });
   }
