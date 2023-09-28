d21bc881b9529e7c956c2f42d4426b3a1f1c86d8
Yorke Rhodes <yorke@hyperlane.xyz>
2023-09-28 12:08:01 -0400

    Migrate from lcov_branch_coverage

--- solidity/coverage.sh
+++ solidity/coverage.sh
@@ -9,12 +9,12 @@ else
 fi
 
 EXCLUDE="*test* *mock* *node_modules* $(grep -r 'library' contracts -l)" # forge does not instrument libraries
-lcov --rc lcov_branch_coverage=1 \
+lcov --rc branch_coverage=1 \
     --output-file forge-pruned-lcov.info \
     --remove lcov.info $EXCLUDE    
 
 if [ "$CI" != "true" ]; then
-    genhtml --rc lcov_branch_coverage=1 \
+    genhtml --rc branch_coverage=1 \
         --output-directory coverage forge-pruned-lcov.info \
         && open coverage/index.html
 fi
