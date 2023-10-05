a8b1335fca9891908236af1f397915cfbcbd4ca8
J M Rossy <jm.rossy@gmail.com>
2023-10-05 14:21:04 -0400

    Update helloworld docker tags

--- typescript/infra/config/environments/testnet3/helloworld.ts
+++ typescript/infra/config/environments/testnet3/helloworld.ts
@@ -13,7 +13,7 @@ export const hyperlaneHelloworld: HelloWorldConfig = {
   kathy: {
     docker: {
       repo: 'gcr.io/abacus-labs-dev/hyperlane-monorepo',
-      tag: '25da8e0-20231004-135818',
+      tag: '86aec06-20231005-180845',
     },
     chainsToSkip: [],
     runEnv: environment,
@@ -33,7 +33,7 @@ export const releaseCandidateHelloworld: HelloWorldConfig = {
   kathy: {
     docker: {
       repo: 'gcr.io/abacus-labs-dev/hyperlane-monorepo',
-      tag: '25da8e0-20231004-135818',
+      tag: '86aec06-20231005-180845',
     },
     chainsToSkip: [],
     runEnv: environment,
