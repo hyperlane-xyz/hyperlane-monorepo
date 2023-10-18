ed2e08f9074abc48da043285f2b927e36380793f
Yorke Rhodes <yorke@hyperlane.xyz>
2023-10-18 14:20:38 -0400

    Add script for flattening

---
+++ solidity/flatten.sh
@@ -0,0 +1,21 @@
+rm -rf flattened
+mkdir -p flattened
+
+# flatten contracts
+yarn hardhat flatten > flattened/flattened.sol
+
+# remove duplicate licenses
+grep -vE "// SPDX.*" flattened/flattened.sol > flattened/delicensed.sol
+
+# add license
+LICENSE="// SPDX-License-Identifier: MIT OR Apache-2.0"
+echo "$LICENSE" | cat - flattened/delicensed.sol > flattened/licensed.sol
+
+# compile
+solc flattened/licensed.sol
+
+if [ $? -ne 0 ]; then
+    echo "Remove @openzeppelin/../ICrossDomainMessenger and replace Optimism_Bridge with ICrossDomainMessenger"
+    echo "Then try compiling again with solc flattened/licensed.sol"
+    exit 1
+fi
