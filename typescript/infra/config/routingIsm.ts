690e9929c8fcd2a84445a7e23f0560a55e9c5aca
Yorke Rhodes <yorke@hyperlane.xyz>
2023-10-24 16:14:52 -0400

    Reuse mainnet2 proxyadmin and storage gas oracle

--- typescript/infra/config/routingIsm.ts
+++ typescript/infra/config/routingIsm.ts
@@ -6,7 +6,6 @@ import {
   ModuleType,
   RoutingIsmConfig,
 } from '@hyperlane-xyz/sdk';
-import { Address } from '@hyperlane-xyz/utils';
 
 import { DeployEnvironment } from '../src/config';
 
@@ -31,19 +30,6 @@ const owners = {
   test: testOwners,
 };
 
-export const mainnetHyperlaneDefaultIsmCache: ChainMap<Address> = {
-  ethereum: '0x3Ef03aEf1392E5e0C16fd4D22C3c3b4f81C8AF0C',
-  optimism: '0xA7a0f9CB7d3bc3520A82ECF009F8f3949a926237',
-  arbitrum: '0xD629aB5353D6B11f52eD80EFb26a28e5E347B52F',
-  avalanche: '0x143A34E3Eaf1E77a8c994EcADb5268d717183150',
-  polygon: '0xE1403b9d64185f715414A4a7BEcec124Bd9198A7',
-  bsc: '0x307c66E1E2E9f20b6ED3c4561398215CF9b633c4',
-  celo: '0xAC0246a09f1FEaF4CEe32e43792eE12d6B277332',
-  moonbeam: '0xB32d147334AF9C15A65716Ab254a2460307648D1',
-  gnosis: '0xF6c174AcC399eD8407663387857f30f92B0db958',
-  solana: '6pHP4EeX2Xek24Be7PPTWCqcpmNEPENW1m9RnZSFSmA1',
-};
-
 // Intended to be the "entrypoint" ISM.
 // Routing ISM => Aggregation (1/2)
 //                 |              |
@@ -55,10 +41,6 @@ export const routingIsm = (
   local: ChainName,
   context: Contexts,
 ): RoutingIsmConfig | string => {
-  if (environment === 'mainnet3' && context === Contexts.Hyperlane) {
-    return mainnetHyperlaneDefaultIsmCache[local];
-  }
-
   const aggregationIsms: ChainMap<AggregationIsmConfig> = chains[
     environment
   ].reduce(
