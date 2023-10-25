505e327f10255b4f31c35dc73f2c82dfb6d769d2
byeongsu-hong <hong@byeongsu.dev>
2023-10-25 12:02:01 +0000

    remove block hash

--- rust/chains/hyperlane-cosmos/build.rs
+++
@@ -1,8 +0,0 @@
-use std::fs;
-
-fn main() {
-    // TODO: build the cosmos contracts/bindings
-    fs::create_dir_all("src/contracts").expect("failed to create contracts dir");
-    fs::write("src/contracts/mod.rs", "// TODO: this should be generated")
-        .expect("failed to write contracts/mod.rs");
-}
