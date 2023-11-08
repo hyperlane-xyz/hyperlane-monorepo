552522748c958ec3d51281b50fad45b94d2a14fc
Yorke Rhodes <yorke@hyperlane.xyz>
2023-11-08 17:32:40 -0500

    Use new OZ proxy interface

--- typescript/sdk/src/deploy/HyperlaneDeployer.ts
+++ typescript/sdk/src/deploy/HyperlaneDeployer.ts
@@ -2,13 +2,13 @@ import { Debugger, debug } from 'debug';
 import { Contract, PopulatedTransaction, ethers } from 'ethers';
 
 import {
+  ITransparentUpgradeableProxy,
   MailboxClient,
   Ownable,
   ProxyAdmin,
   ProxyAdmin__factory,
   TimelockController,
   TimelockController__factory,
-  TransparentUpgradeableProxy,
   TransparentUpgradeableProxy__factory,
 } from '@hyperlane-xyz/core';
 import {
@@ -353,7 +353,7 @@ export abstract class HyperlaneDeployer<
 
   protected async changeAdmin(
     chain: ChainName,
-    proxy: TransparentUpgradeableProxy,
+    proxy: ITransparentUpgradeableProxy,
     admin: string,
   ): Promise<void> {
     const actualAdmin = await proxyAdmin(
@@ -385,7 +385,7 @@ export abstract class HyperlaneDeployer<
 
   protected async upgradeAndInitialize<C extends ethers.Contract>(
     chain: ChainName,
-    proxy: TransparentUpgradeableProxy,
+    proxy: ITransparentUpgradeableProxy,
     implementation: C,
     initializeArgs: Parameters<C['initialize']>,
   ): Promise<void> {
