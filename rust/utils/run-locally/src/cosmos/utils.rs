5d2ee001471d9b5b071c943137181b062ea3b32a
Daniel Savu <23065004+daniel-savu@users.noreply.github.com>
2023-11-15 13:36:43 +0000

    chore: use larger runner for github actions (#2909)
    Size of rust build exceeds the storage space in the `ubuntu-latest`
    runner. `larger-runner` has more storage, but let's see if it also has
    all the system dependencies required to build

--- rust/utils/run-locally/src/cosmos/utils.rs
+++ rust/utils/run-locally/src/cosmos/utils.rs
@@ -8,7 +8,9 @@ use crate::utils::TaskHandle;
 pub(crate) fn sed(from: &str, to: &str, file: &str) {
     Program::new("sed")
         .raw_arg("-i")
-        .cmd("")
+        // Temporary fix to get `sed` working on linux
+        // Note that this breaks the script on mac
+        // .cmd("")
         .cmd(format!("s/{from}/{to}/g"))
         .cmd(file)
         .run()
