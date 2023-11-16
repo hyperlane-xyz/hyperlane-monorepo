2dfce59d3d06f84714c67dbab5c8025e8858242c
Yorke Rhodes <yorke@hyperlane.xyz>
2023-11-16 08:15:55 -0500

    Fix comment

--- typescript/cli/src/consts.ts
+++ typescript/cli/src/consts.ts
@@ -1,4 +1,4 @@
-// TODO: scale gas with number of
+// TODO: derive this more intelligently
 export const MINIMUM_CORE_DEPLOY_GAS = 1e16;
 export const MINIMUM_WARP_DEPLOY_GAS = 1e15;
 export const MINIMUM_TEST_SEND_GAS = 200_000;
