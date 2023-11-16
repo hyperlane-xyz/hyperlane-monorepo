bd70a599e925110cc2b6d299fb0d296f7dddc70f
J M Rossy <jm.rossy@gmail.com>
2023-11-16 12:46:21 -0500

    Fixes for amount conversion & BigNumber lib de-duping (#2913)
    ### Description
    
    - Bring BigNumber de-duping improvements from @ottbunn into v3
    - Fixes for amount conversion
    - Create unit tests for some amount fns
    - Run utils unit tests in CI
    
    ### Related issues
    
    https://github.com/hyperlane-xyz/issues/issues/690
    https://github.com/hyperlane-xyz/hyperlane-monorepo/issues/2751
    https://github.com/hyperlane-xyz/hyperlane-monorepo/pull/2828
    
    ### Backward compatibility
    
    No, minor breaking changes to signatures of amount util fns
    
    ### Testing
    
    Unit tests

--- typescript/utils/src/amount.ts
+++ typescript/utils/src/amount.ts
@@ -1,77 +1,84 @@
 import { formatUnits, parseUnits } from '@ethersproject/units';
 import BigNumber from 'bignumber.js';
-import { ethers } from 'ethers';
 
-const DEFAULT_MIN_ROUNDED_VALUE = 0.00001;
 const DEFAULT_DISPLAY_DECIMALS = 4;
 const DEFAULT_TOKEN_DECIMALS = 18;
 
-type NumberT = BigNumber.Value;
-
+/**
+ * Convert the given Wei value to Ether value
+ * @param value The value to convert.
+ * @returns Converted value in string type.
+ */
 export function fromWei(
-  value: NumberT | null | undefined,
+  value: BigNumber.Value | null | undefined,
   decimals = DEFAULT_TOKEN_DECIMALS,
-): number {
-  if (!value) return 0;
-  const valueString = value.toString().trim();
-  const flooredValue = new BigNumber(valueString).toFixed(
-    0,
-    BigNumber.ROUND_FLOOR,
-  );
-  return parseFloat(formatUnits(flooredValue, decimals));
+): string {
+  if (!value) return (0).toString();
+  const valueString = value.toString(10).trim();
+  const flooredValue = BigNumber(valueString).toFixed(0, BigNumber.ROUND_FLOOR);
+  return parseFloat(formatUnits(flooredValue, decimals)).toString();
 }
 
-// Similar to fromWei above but rounds to set number of decimals
-// with a minimum floor, configured per token
+/**
+ * Convert the given Wei value to Ether value,
+ * round to set number of decimals with a minimum floor, configured per token
+ * @param value The value to convert.
+ * @param decimals
+ * @returns Converted value in string type.
+ */
 export function fromWeiRounded(
-  value: NumberT | null | undefined,
+  value: BigNumber.Value | null | undefined,
   decimals = DEFAULT_TOKEN_DECIMALS,
-  roundDownIfSmall = true,
+  displayDecimals?: number,
 ): string {
   if (!value) return '0';
-  const flooredValue = new BigNumber(value).toFixed(0, BigNumber.ROUND_FLOOR);
-  const amount = new BigNumber(formatUnits(flooredValue, decimals));
+  const flooredValue = BigNumber(value).toFixed(0, BigNumber.ROUND_FLOOR);
+  const amount = BigNumber(formatUnits(flooredValue, decimals));
   if (amount.isZero()) return '0';
-
-  // If amount is less than min value
-  if (amount.lt(DEFAULT_MIN_ROUNDED_VALUE)) {
-    if (roundDownIfSmall) return '0';
-    return amount.toString(10);
-  }
-
-  const displayDecimals = amount.gte(10000) ? 2 : DEFAULT_DISPLAY_DECIMALS;
-  return amount.toFixed(displayDecimals).toString();
+  displayDecimals ??= amount.gte(10000) ? 2 : DEFAULT_DISPLAY_DECIMALS;
+  return amount.toFixed(displayDecimals, BigNumber.ROUND_FLOOR);
 }
 
+/**
+ * Convert the given value to Wei value
+ * @param value The value to convert.
+ * @returns Converted value in string type.
+ */
 export function toWei(
-  value: NumberT | null | undefined,
+  value: BigNumber.Value | null | undefined,
   decimals = DEFAULT_TOKEN_DECIMALS,
-): BigNumber {
-  if (!value) return new BigNumber(0);
+): string {
+  if (!value) return BigNumber(0).toString();
   // First convert to a BigNumber, and then call `toString` with the
   // explicit radix 10 such that the result is formatted as a base-10 string
   // and not in scientific notation.
-  const valueBN = new BigNumber(value);
+  const valueBN = BigNumber(value);
   const valueString = valueBN.toString(10).trim();
   const components = valueString.split('.');
   if (components.length === 1) {
-    return new BigNumber(parseUnits(valueString, decimals).toString());
+    return parseUnits(valueString, decimals).toString();
   } else if (components.length === 2) {
     const trimmedFraction = components[1].substring(0, decimals);
-    return new BigNumber(
-      parseUnits(`${components[0]}.${trimmedFraction}`, decimals).toString(),
-    );
+    return parseUnits(
+      `${components[0]}.${trimmedFraction}`,
+      decimals,
+    ).toString();
   } else {
     throw new Error(`Cannot convert ${valueString} to wei`);
   }
 }
 
+/**
+ * Try to parse the given value into BigNumber.js BigNumber
+ * @param value The value to parse.
+ * @returns Parsed value in BigNumber.js BigNumber type.
+ */
 export function tryParseAmount(
-  value: NumberT | null | undefined,
+  value: BigNumber.Value | null | undefined,
 ): BigNumber | null {
   try {
     if (!value) return null;
-    const parsed = new BigNumber(value);
+    const parsed = BigNumber(value);
     if (!parsed || parsed.isNaN() || !parsed.isFinite()) return null;
     else return parsed;
   } catch (error) {
@@ -79,15 +86,20 @@ export function tryParseAmount(
   }
 }
 
-// Checks if an amount is equal of nearly equal to balance within a small margin of error
-// Necessary because amounts in the UI are often rounded
+/**
+ * Checks if an amount is equal of nearly equal to balance within a small margin of error
+ * Necessary because amounts in the UI are often rounded
+ * @param amount1 The amount to compare.
+ * @param amount2 The amount to compare.
+ * @returns true/false.
+ */
 export function eqAmountApproximate(
-  amountInWei1: BigNumber,
-  amountInWei2: NumberT,
-) {
-  const minValueWei = toWei(DEFAULT_MIN_ROUNDED_VALUE);
-  // Is difference btwn amount and balance less than min amount shown for token
-  return amountInWei1.minus(amountInWei2).abs().lt(minValueWei);
+  amount1: BigNumber.Value,
+  amount2: BigNumber.Value,
+  maxDifference: BigNumber.Value,
+): boolean {
+  // Is difference btwn amounts less than maxDifference
+  return BigNumber(amount1).minus(amount2).abs().lte(maxDifference);
 }
 
 /**
@@ -96,50 +108,26 @@ export function eqAmountApproximate(
  * @param fromDecimals The number of decimals `value` has.
  * @param toDecimals The number of decimals to convert `value` to.
  * @param value The value to convert.
- * @returns `value` represented with `toDecimals` decimals.
+ * @returns `value` represented with `toDecimals` decimals in string type.
  */
 export function convertDecimals(
   fromDecimals: number,
   toDecimals: number,
-  value: NumberT,
-) {
-  const amount = new BigNumber(value);
+  value: BigNumber.Value,
+): string {
+  const amount = BigNumber(value);
 
-  if (fromDecimals === toDecimals) return amount;
+  if (fromDecimals === toDecimals) return amount.toString(10);
   else if (fromDecimals > toDecimals) {
     const difference = fromDecimals - toDecimals;
     return amount
-      .div(new BigNumber(10).pow(difference))
-      .integerValue(BigNumber.ROUND_FLOOR);
+      .div(BigNumber(10).pow(difference))
+      .integerValue(BigNumber.ROUND_FLOOR)
+      .toString(10);
   }
   // fromDecimals < toDecimals
   else {
     const difference = toDecimals - fromDecimals;
-    return amount.times(new BigNumber(10).pow(difference));
-  }
-}
-
-/**
- * Converts a value with `fromDecimals` decimals to a value with `toDecimals` decimals.
- * Incurs a loss of precision when `fromDecimals` > `toDecimals`.
- * @param fromDecimals The number of decimals `value` has.
- * @param toDecimals The number of decimals to convert `value` to.
- * @param value The value to convert.
- * @returns `value` represented with `toDecimals` decimals.
- */
-export function convertDecimalsEthersBigNumber(
-  fromDecimals: number,
-  toDecimals: number,
-  value: ethers.BigNumber,
-) {
-  if (fromDecimals === toDecimals) return value;
-  else if (fromDecimals > toDecimals) {
-    const difference = fromDecimals - toDecimals;
-    return value.div(ethers.BigNumber.from('10').pow(difference));
-  }
-  // fromDecimals < toDecimals
-  else {
-    const difference = toDecimals - fromDecimals;
-    return value.mul(ethers.BigNumber.from('10').pow(difference));
+    return amount.times(BigNumber(10).pow(difference)).toString(10);
   }
 }
