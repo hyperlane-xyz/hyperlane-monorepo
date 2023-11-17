4a62e8712748e45f7e48f35c572b717f585197bd
-f <kunalarora1729@gmail.com>
2023-11-17 13:26:46 -0500

    example ism schema

--- typescript/cli/src/config/multisig.ts
+++ typescript/cli/src/config/multisig.ts
@@ -1,54 +1,64 @@
 import { input, select } from '@inquirer/prompts';
 import { z } from 'zod';
 
-import { ChainMap, IsmType, MultisigConfig } from '@hyperlane-xyz/sdk';
-import { objMap } from '@hyperlane-xyz/utils';
+import { IsmType } from '@hyperlane-xyz/sdk';
 
 import { errorRed, log, logBlue, logGreen } from '../../logger.js';
 import { runMultiChainSelectionStep } from '../utils/chains.js';
-import { FileFormat, mergeYamlOrJson, readYamlOrJson } from '../utils/files.js';
+import { FileFormat, mergeYamlOrJson } from '../utils/files.js';
 
 import { readChainConfigsIfExists } from './chain.js';
 
-const MultisigConfigMapSchema = z.object({}).catchall(
+const RoutingIsmConfigSchema: z.ZodSchema<any> = z.lazy(() =>
   z.object({
-    type: z.nativeEnum(IsmType),
-    threshold: z.number(),
-    validators: z.array(z.string()),
+    type: z.literal(IsmType.ROUTING),
+    owner: z.string(),
+    doamins: z.record(IsmConfigSchema),
   }),
 );
-export type MultisigConfigMap = z.infer<typeof MultisigConfigMapSchema>;
 
-export function readMultisigConfig(filePath: string) {
-  const config = readYamlOrJson(filePath);
-  if (!config) throw new Error(`No multisig config found at ${filePath}`);
-  const result = MultisigConfigMapSchema.safeParse(config);
-  if (!result.success) {
-    const firstIssue = result.error.issues[0];
-    throw new Error(
-      `Invalid multisig config: ${firstIssue.path} => ${firstIssue.message}`,
-    );
-  }
-  const parsedConfig = result.data;
-  const formattedConfig: ChainMap<MultisigConfig> = objMap(
-    parsedConfig,
-    (_, config) =>
-      ({
-        type: config.type as IsmType,
-        threshold: config.threshold,
-        validators: config.validators,
-      } as MultisigConfig),
-  );
+const MultisigIsmConfigSchema = z.object({
+  type: z.nativeEnum(IsmType),
+  threshold: z.number(),
+  validators: z.array(z.string()),
+});
 
-  logGreen(`All multisig configs in ${filePath} are valid`);
-  return formattedConfig;
+const IsmConfigSchema = z.union([
+  MultisigIsmConfigSchema,
+  RoutingIsmConfigSchema,
+]);
+const IsmConfigMapSchema = z.record(IsmConfigSchema);
+export type IsmConfigMap = z.infer<typeof IsmConfigMapSchema>;
+
+export function readMultisigConfig(_: string) {
+  // const config = readYamlOrJson(filePath);
+  // if (!config) throw new Error(`No multisig config found at ${filePath}`);
+  // const result = MultisigIsmConfigMapSchema.safeParse(config);
+  // if (!result.success) {
+  //   const firstIssue = result.error.issues[0];
+  //   throw new Error(
+  //     `Invalid multisig config: ${firstIssue.path} => ${firstIssue.message}`,
+  //   );
+  // }
+  // const parsedConfig = result.data;
+  // const formattedConfig: ChainMap<MultisigConfig> = objMap(
+  //   parsedConfig,
+  //   (_, config) =>
+  //     ({
+  //       type: config.type as IsmType,
+  //       threshold: config.threshold,
+  //       validators: config.validators,
+  //     } as MultisigConfig),
+  // );
+  // logGreen(`All multisig configs in ${filePath} are valid`);
+  // return formattedConfig;
 }
 
 export function isValidMultisigConfig(config: any) {
-  return MultisigConfigMapSchema.safeParse(config).success;
+  return IsmConfigSchema.safeParse(config).success;
 }
 
-export async function createMultisigConfig({
+export async function createIsmConfig({
   format,
   outPath,
   chainConfigPath,
@@ -57,12 +67,12 @@ export async function createMultisigConfig({
   outPath: string;
   chainConfigPath: string;
 }) {
-  logBlue('Creating a new multisig config');
+  logBlue('Creating a new ISM config');
   const customChains = readChainConfigsIfExists(chainConfigPath);
   const chains = await runMultiChainSelectionStep(customChains);
 
-  const result: MultisigConfigMap = {};
-  let lastConfig: MultisigConfigMap['string'] | undefined = undefined;
+  const result: IsmConfigMap = {};
+  let lastConfig: IsmConfigMap['string'] | undefined = undefined;
   const repeat = false;
   for (const chain of chains) {
     log(`Setting values for chain ${chain}`);
