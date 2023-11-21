1a584ad8a4fc0d588404de053cf503657ba3059a
Yorke Rhodes <yorke@hyperlane.xyz>
2023-11-21 18:11:43 -0500

    Improve standard hook metadata API

--- solidity/contracts/test/TestSendReceiver.sol
+++ solidity/contracts/test/TestSendReceiver.sol
@@ -9,7 +9,6 @@ import {IMailbox} from "../interfaces/IMailbox.sol";
 import {IPostDispatchHook} from "../interfaces/hooks/IPostDispatchHook.sol";
 import {IInterchainSecurityModule, ISpecifiesInterchainSecurityModule} from "../interfaces/IInterchainSecurityModule.sol";
 
-import {StandardHookMetadata} from "../hooks/libs/StandardHookMetadata.sol";
 import {MailboxClient} from "../client/MailboxClient.sol";
 
 contract TestSendReceiver is IMessageRecipient {
@@ -24,16 +23,11 @@ contract TestSendReceiver is IMessageRecipient {
         uint32 _destinationDomain,
         bytes calldata _messageBody
     ) external payable {
-        bytes memory hookMetadata = StandardHookMetadata.formatMetadata(
-            HANDLE_GAS_AMOUNT,
-            msg.sender
-        );
         // TODO: handle topping up?
         _mailbox.dispatch{value: msg.value}(
             _destinationDomain,
             address(this).addressToBytes32(),
-            _messageBody,
-            hookMetadata
+            _messageBody
         );
     }
 
@@ -43,16 +37,12 @@ contract TestSendReceiver is IMessageRecipient {
         bytes calldata _messageBody,
         IPostDispatchHook hook
     ) external payable {
-        bytes memory hookMetadata = StandardHookMetadata.formatMetadata(
-            HANDLE_GAS_AMOUNT,
-            msg.sender
-        );
         // TODO: handle topping up?
         _mailbox.dispatch{value: msg.value}(
             _destinationDomain,
             address(this).addressToBytes32(),
             _messageBody,
-            hookMetadata,
+            bytes(""),
             hook
         );
     }
