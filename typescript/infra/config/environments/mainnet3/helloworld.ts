4fe4000d69e810759023456ebdae269c5d1262c3
Trevor Porter <trkporter@ucdavis.edu>
2023-11-22 11:23:15 +0000

    Deploy kathy (#2918)
    ### Description
    
    Deployed Kathy last night with a newer image that doesn't include Solana
    in the list of chains to send/receive kathy messages on v3
    
    ### Drive-by changes
    
    <!--
    Are there any minor or drive-by changes also included?
    -->
    
    ### Related issues
    
    <!--
    - Fixes #[issue number here]
    -->
    
    ### Backward compatibility
    
    <!--
    Are these changes backward compatible? Are there any infrastructure
    implications, e.g. changes that would prohibit deploying older commits
    using this infra tooling?
    
    Yes/No
    -->
    
    ### Testing
    
    <!--
    What kind of testing have these changes undergone?
    
    None/Manual/Unit Tests
    -->

--- typescript/infra/config/environments/mainnet3/helloworld.ts
+++ typescript/infra/config/environments/mainnet3/helloworld.ts
@@ -13,7 +13,7 @@ export const hyperlane: HelloWorldConfig = {
   kathy: {
     docker: {
       repo: 'gcr.io/abacus-labs-dev/hyperlane-monorepo',
-      tag: 'bbe8928-20231025-215311',
+      tag: 'a05a102-20231110-182534',
     },
     chainsToSkip: [],
     runEnv: environment,
@@ -34,7 +34,7 @@ export const releaseCandidate: HelloWorldConfig = {
   kathy: {
     docker: {
       repo: 'gcr.io/abacus-labs-dev/hyperlane-monorepo',
-      tag: 'bef2251-20231025-174850',
+      tag: 'a05a102-20231110-182534',
     },
     chainsToSkip: [],
     runEnv: environment,
