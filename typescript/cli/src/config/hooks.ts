17c6503d541b23b84df60bb6f54857603dce0fce
-f <kunalarora1729@gmail.com>
2023-11-22 19:08:33 -0500

    igp hook

--- typescript/cli/src/config/hooks.ts
+++ typescript/cli/src/config/hooks.ts
@@ -42,17 +42,30 @@ const MerkleTreeSchema = z.object({
   type: z.literal(HookType.MERKLE_TREE),
 });
 
-const HookSchema = z.union([ProtocolFeeSchema, MerkleTreeSchema]);
-
-const ConfigSchema = z.object({
-  required: HookSchema,
-  default: HookSchema,
+const IGPSchema = z.object({
+  type: z.literal(HookType.INTERCHAIN_GAS_PAYMASTER),
+  owner: z.string(),
+  beneficiary: z.string(),
+  overhead: z.record(z.string()),
+  oracleKey: z.string(),
 });
-const HookConfigMapSchema = z.object({}).catchall(ConfigSchema);
-export type HookConfigMap = z.infer<typeof HookConfigMapSchema>;
+
+const HookConfigSchema = z.union([
+  ProtocolFeeSchema,
+  MerkleTreeSchema,
+  IGPSchema,
+]);
+export type ZODHookConfig = z.infer<typeof HookConfigSchema>;
+
+const HooksConfigSchema = z.object({
+  required: HookConfigSchema,
+  default: HookConfigSchema,
+});
+const HooksConfigMapSchema = z.object({}).catchall(HooksConfigSchema);
+export type HooksConfigMap = z.infer<typeof HooksConfigMapSchema>;
 
 export function isValidHookConfigMap(config: any) {
-  return HookConfigMapSchema.safeParse(config).success;
+  return HooksConfigMapSchema.safeParse(config).success;
 }
 
 export function presetHookConfigs(
@@ -120,7 +133,7 @@ export function readHooksConfig(filePath: string) {
     logRed(`No hook config found at ${filePath}`);
     return;
   }
-  const result = HookConfigMapSchema.safeParse(config);
+  const result = HooksConfigMapSchema.safeParse(config);
   if (!result.success) {
     const firstIssue = result.error.issues[0];
     throw new Error(
@@ -158,7 +171,7 @@ export function readHookConfig(parsedConfig: any): HookConfig {
   }
 }
 
-export async function createHookConfig({
+export async function createHookConfigMap({
   format,
   outPath,
   chainConfigPath,
@@ -171,71 +184,14 @@ export async function createHookConfig({
   const customChains = readChainConfigsIfExists(chainConfigPath);
   const chains = await runMultiChainSelectionStep(customChains);
 
-  const result: HookConfigMap = {};
+  const result: HooksConfigMap = {};
   for (const chain of chains) {
     for (const hookRequirements of ['required', 'default']) {
       log(`Setting ${hookRequirements} hook for chain ${chain}`);
-      const hookType = await select({
-        message: 'Select hook type',
-        choices: [
-          { value: 'merkle_tree', name: 'MerkleTreeHook' },
-          { value: 'protocol_fee', name: 'StaticProtocolFee' },
-        ],
-        pageSize: 5,
-      });
-      if (hookType === 'merkle_tree') {
-        result[chain] = {
-          ...result[chain],
-          [hookRequirements]: { type: HookType.MERKLE_TREE },
-        };
-      } else if (hookType === 'protocol_fee') {
-        const owner = await input({
-          message: 'Enter owner address',
-        });
-        const ownerAddress = normalizeAddressEvm(owner);
-        let beneficiary;
-        let sameAsOwner = false;
-        sameAsOwner = await confirm({
-          message: 'Use this same address for the beneficiary?',
-        });
-        if (sameAsOwner) {
-          beneficiary = ownerAddress;
-        } else {
-          beneficiary = await input({
-            message: 'Enter beneficiary address',
-          });
-        }
-        const beneficiaryAddress = normalizeAddressEvm(beneficiary);
-        // TODO: input in gwei, wei, etc
-        const maxProtocolFee = toWei(
-          await input({
-            message:
-              'Enter max protocol fee which you cannot exceed (in eth e.g. 1.0)',
-          }),
-        );
-        const protocolFee = toWei(
-          await input({
-            message: 'Enter protocol fee (in eth e.g. 0.01)',
-          }),
-        );
-        if (BigNumberJs(protocolFee).gt(maxProtocolFee)) {
-          errorRed('Protocol fee cannot be greater than max protocol fee');
-          throw new Error('Invalid protocol fee');
-        }
-
-        result[chain] = {
-          ...result[chain],
-          [hookRequirements]: {
-            type: HookType.PROTOCOL_FEE,
-            maxProtocolFee: maxProtocolFee.toString(),
-            protocolFee: protocolFee.toString(),
-            beneficiary: beneficiaryAddress,
-            owner: ownerAddress,
-          },
-        };
-      } else {
-        throw new Error(`Invalid hook type: ${hookType}}`);
-      }
+      result[chain] = {
+        ...result[chain],
+        [hookRequirements]: createHookConfig(chain, chainConfigPath),
+      };
     }
     if (isValidHookConfigMap(result)) {
       logGreen(`Hook config is valid, writing to file ${outPath}`);
@@ -247,4 +203,136 @@ export async function createHookConfig({
       throw new Error('Invalid hook config');
     }
   }
+}
+
+export async function createHookConfig(
+  _?: ChainName,
+  __?: string,
+): Promise<ZODHookConfig> {
+  let lastConfig: ZODHookConfig;
+  const hookType = await select({
+    message: 'Select hook type',
+    choices: [
+      {
+        value: HookType.MERKLE_TREE,
+        name: HookType.MERKLE_TREE,
+        description:
+          'Add messages to the incremental merkle tree on origin chain (needed for the merkleRootMultisigIsm on the remote chain)',
+      },
+      {
+        value: HookType.PROTOCOL_FEE,
+        name: HookType.PROTOCOL_FEE,
+        description: 'Charge fees for each message dispatch from this chain',
+      },
+      {
+        value: HookType.INTERCHAIN_GAS_PAYMASTER,
+        name: HookType.INTERCHAIN_GAS_PAYMASTER,
+        description:
+          'Allow for payments for expected gas to be paid by the relayer while delivering on remote chain',
+      },
+    ],
+    pageSize: 5,
+  });
+  if (hookType === HookType.MERKLE_TREE) {
+    lastConfig = { type: HookType.MERKLE_TREE };
+  } else if (hookType === HookType.PROTOCOL_FEE) {
+    lastConfig = await createProtocolFeeHookConfig();
+  } else {
+    throw new Error(`Invalid hook type: ${hookType}}`);
+  }
+  return lastConfig;
+}
+
+export async function createProtocolFeeHookConfig(): Promise<ZODHookConfig> {
+  const owner = await input({
+    message: 'Enter owner address',
+  });
+  const ownerAddress = normalizeAddressEvm(owner);
+  let beneficiary;
+  let sameAsOwner = false;
+  sameAsOwner = await confirm({
+    message: 'Use this same address for the beneficiary?',
+  });
+  if (sameAsOwner) {
+    beneficiary = ownerAddress;
+  } else {
+    beneficiary = await input({
+      message: 'Enter beneficiary address',
+    });
+  }
+  const beneficiaryAddress = normalizeAddressEvm(beneficiary);
+  // TODO: input in gwei, wei, etc
+  const maxProtocolFee = toWei(
+    await input({
+      message:
+        'Enter max protocol fee which you cannot exceed (in eth e.g. 1.0)',
+    }),
+  );
+  const protocolFee = toWei(
+    await input({
+      message: 'Enter protocol fee (in eth e.g. 0.01)',
+    }),
+  );
+  if (BigNumberJs(protocolFee).gt(maxProtocolFee)) {
+    errorRed('Protocol fee cannot be greater than max protocol fee');
+    throw new Error('Invalid protocol fee');
+  }
+
+  return {
+    type: HookType.PROTOCOL_FEE,
+    maxProtocolFee: maxProtocolFee.toString(),
+    protocolFee: protocolFee.toString(),
+    beneficiary: beneficiaryAddress,
+    owner: ownerAddress,
+  };
+}
+
+export async function createIGPConfig(
+  chain: ChainName,
+  chainConfigPath: string,
+): Promise<ZODHookConfig> {
+  const owner = await input({
+    message: 'Enter owner address',
+  });
+  const ownerAddress = normalizeAddressEvm(owner);
+  let beneficiary, oracleKey;
+  let sameAsOwner = false;
+  sameAsOwner = await confirm({
+    message: 'Use this same address for the beneficiary and gasOracleKey?',
+  });
+  if (sameAsOwner) {
+    beneficiary = ownerAddress;
+    oracleKey = ownerAddress;
+  } else {
+    beneficiary = await input({
+      message: 'Enter beneficiary address',
+    });
+    oracleKey = await input({
+      message: 'Enter gasOracleKey address',
+    });
+  }
+  const beneficiaryAddress = normalizeAddressEvm(beneficiary);
+  const oracleKeyAddress = normalizeAddressEvm(oracleKey);
+  const customChains = readChainConfigsIfExists(chainConfigPath);
+  delete customChains[chain];
+  const chains = await runMultiChainSelectionStep(
+    customChains,
+    `Select destination chains to pay overhead from ${chain}`,
+    [chain],
+  );
+  const overheads: ChainMap<string> = {};
+  for (const chain of chains) {
+    const overhead = await input({
+      message: `Enter overhead for ${chain} (in eth e.g. 0.01)`,
+    });
+    overheads[chain] = toWei(overhead);
+  }
+
+  return {
+    type: HookType.INTERCHAIN_GAS_PAYMASTER,
+    beneficiary: beneficiaryAddress,
+    owner: ownerAddress,
+    oracleKey: oracleKeyAddress,
+    overhead: overheads,
+  };
 }
