1829930681a8d487f07e26ff201bf8e1e48ac2fa
Trevor Porter <trkporter@ucdavis.edu>
2023-11-23 15:20:07 +0000

    Use Polygon gas oracle (#2965)
    After investigating #2959, I found the following
    
    this is a known problem with Polygon, explanation here
    https://github.com/ethers-io/ethers.js/issues/2828#issuecomment-1283014250
    
    Fun fact Asa looked into this once
    https://github.com/hyperlane-xyz/hyperlane-monorepo/issues/771
    
    Here's a discussion it in Foundry, which I found hoping that ethers-rs
    folks had ran into this before
    https://github.com/foundry-rs/foundry/issues/1703
    
    Foundry fixed this by using the Polygon gas station oracle which seems
    to be recommended path
    https://github.com/foundry-rs/foundry/pull/3368/files#diff-c89a4bbf7a90da118dcf00c5fe70eba78f8e5d95662bb5f039a353113e95042bR205
    
    There's actually a polygon ethers middleware for this
    https://docs.rs/ethers/latest/ethers/middleware/gas_oracle/polygon/struct.Polygon.html
    
    So I (originally) borrowed this code from Foundry
    https://github.com/foundry-rs/foundry/blob/master/crates/common/src/provider.rs#L254-L290
    
    Changed to use Middlewares
    
    This also means we can remove our existing janky Polygon logic
    
    <!--
    Are there any minor or drive-by changes also included?
    -->
    
    <!--
    - Fixes #[issue number here]
    -->
    
    <!--
    Are these changes backward compatible? Are there any infrastructure
    implications, e.g. changes that would prohibit deploying older commits
    using this infra tooling?
    
    Yes/No
    -->
    
    <!--
    What kind of testing have these changes undergone?
    
    None/Manual/Unit Tests
    -->

--- rust/chains/hyperlane-ethereum/src/tx.rs
+++ rust/chains/hyperlane-ethereum/src/tx.rs
@@ -6,6 +6,7 @@ use ethers::{
     types::Eip1559TransactionRequest,
 };
 use ethers_contract::builders::ContractCall;
+use ethers_core::types::BlockNumber;
 use hyperlane_core::{
     utils::fmt_bytes, ChainCommunicationError, ChainResult, KnownHyperlaneDomain, H256, U256,
 };
@@ -70,7 +71,6 @@ pub(crate) async fn fill_tx_gas_params<M, D>(
     tx: ContractCall<M, D>,
     tx_gas_limit: Option<U256>,
     provider: Arc<M>,
-    domain: u32,
 ) -> ChainResult<ContractCall<M, D>>
 where
     M: Middleware + 'static,
@@ -87,16 +87,6 @@ where
     let Ok((max_fee, max_priority_fee)) = provider.estimate_eip1559_fees(None).await else {
         // Is not EIP 1559 chain
         return Ok(tx.gas(gas_limit));
-    };
-    let max_priority_fee = if matches!(
-        KnownHyperlaneDomain::try_from(domain),
-        Ok(KnownHyperlaneDomain::Polygon)
-    ) {
-        // Polygon needs a max priority fee >= 30 gwei
-        let min_polygon_fee = U256::from(30_000_000_000u64);
-        max_priority_fee.max(min_polygon_fee.into())
-    } else {
-        max_priority_fee
     };
     // Is EIP 1559 chain
     let mut request = Eip1559TransactionRequest::new();
