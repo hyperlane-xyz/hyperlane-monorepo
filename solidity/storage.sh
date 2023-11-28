6bca55d50e786f3ffc5781a78856a80922f47aec
Yorke Rhodes <yorke@hyperlane.xyz>
2023-11-28 13:57:36 -0500

    Allow customizing output dir

--- solidity/storage.sh
+++ solidity/storage.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
+OUTPUT_PATH=${1:-storage}
+EXCLUDE="test|mock|interfaces|libs|upgrade|README|Abstract|Static"
 
 IFS=$'\n'
 CONTRACT_FILES=($(find ./contracts -type f))
 unset IFS
 
+echo "Generating layouts in $OUTPUT_PATH"
+mkdir -p $OUTPUT_PATH
+
 for file in "${CONTRACT_FILES[@]}";
 do
-    contract=$(basename "$file" .sol)
-
-    if [[ $file =~ .*(test|mock|interfaces|libs|upgrade|README|Abstract|Static).* ]]; then
+    if [[ $file =~ .*($EXCLUDE).* ]]; then
         continue
     fi
 
+    contract=$(basename "$file" .sol)
     echo "Generating storage layout of $contract"
-    forge inspect "$contract" storage --pretty > "storage/$contract.md"
+    forge inspect "$contract" storage --pretty > "$OUTPUT_PATH/$contract.md"
 done
