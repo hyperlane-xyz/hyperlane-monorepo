b4e5113e8d237e4e8a840f150bee52b4d72ea1df
Trevor Porter <trkporter@ucdavis.edu>
2023-11-28 13:50:54 +0000

    Deploy kathy with helloworld Sealevel fix (#2989)
    ### Description
    
    deploy with
    https://github.com/hyperlane-xyz/hyperlane-monorepo/pull/2981
    
    ### Drive-by changes
    
    n/a
    
    ### Related issues
    
    n/a
    
    ### Backward compatibility
    
    ye
    
    ### Testing
    
    deployed

--- typescript/infra/config/environments/mainnet2/helloworld.ts
+++ typescript/infra/config/environments/mainnet2/helloworld.ts
@@ -13,7 +13,7 @@ export const hyperlane: HelloWorldConfig = {
   kathy: {
     docker: {
       repo: 'gcr.io/abacus-labs-dev/hyperlane-monorepo',
-      tag: '01dcf5d-20231010-145824',
+      tag: '66066f9-20231128-113426',
     },
     chainsToSkip: [],
     runEnv: environment,
@@ -34,7 +34,7 @@ export const releaseCandidate: HelloWorldConfig = {
   kathy: {
     docker: {
       repo: 'gcr.io/abacus-labs-dev/hyperlane-monorepo',
-      tag: '01dcf5d-20231010-145824',
+      tag: '66066f9-20231128-113426',
     },
     chainsToSkip: [],
     runEnv: environment,
