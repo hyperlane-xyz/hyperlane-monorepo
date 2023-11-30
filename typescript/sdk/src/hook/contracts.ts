35017558165be125b2ed64f3f7c445fa81ed9c04
Yorke Rhodes <yorke@hyperlane.xyz>
2023-11-30 12:09:45 -0500

    Add storage layout diff checks (#2991)

--- typescript/sdk/src/hook/contracts.ts
+++ typescript/sdk/src/hook/contracts.ts
@@ -4,15 +4,15 @@ import {
   InterchainGasPaymaster__factory,
   MerkleTreeHook__factory,
   OPStackHook__factory,
+  ProtocolFee__factory,
   StaticAggregationHook__factory,
-  StaticProtocolFee__factory,
 } from '@hyperlane-xyz/core';
 
 import { HookType } from './types';
 
 export const hookFactories = {
   [HookType.MERKLE_TREE]: new MerkleTreeHook__factory(),
-  [HookType.PROTOCOL_FEE]: new StaticProtocolFee__factory(),
+  [HookType.PROTOCOL_FEE]: new ProtocolFee__factory(),
   [HookType.INTERCHAIN_GAS_PAYMASTER]: new InterchainGasPaymaster__factory(), // unused
   [HookType.AGGREGATION]: new StaticAggregationHook__factory(), // unused
   [HookType.OP_STACK]: new OPStackHook__factory(),
