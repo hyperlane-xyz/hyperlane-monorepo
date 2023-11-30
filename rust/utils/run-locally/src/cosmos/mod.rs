4bafa7678ccb42b111c2cd14fe896c02a7321136
Daniel Savu <23065004+daniel-savu@users.noreply.github.com>
2023-11-30 00:14:48 +0000

    chore: use a paranoid sleep in cosmos e2e because wasm init may be slow

--- rust/utils/run-locally/src/cosmos/mod.rs
+++ rust/utils/run-locally/src/cosmos/mod.rs
@@ -462,7 +462,9 @@ fn run_locally() {
     );
 
     // give things a chance to fully start.
-    sleep(Duration::from_secs(10));
+    println!("sleeping for 100s");
+    sleep(Duration::from_secs(100));
+    println!("done sleeping for 100s");
 
     let starting_relayer_balance: f64 = relayer_balance_sum(hpl_rly_metrics_port).unwrap();
 
