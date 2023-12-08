7433a63642b3190f605a93d581dff9c29044ffc9
Ivan Temchenko <35359595i@proton.me>
2023-12-08 15:41:21 -0800

    feat(GCS): CheckpointSyncer for GCS client impl

---
+++ rust/hyperlane-base/src/types/gcs_storage.rs
@@ -0,0 +1,179 @@
+use crate::CheckpointSyncer;
+use async_trait::async_trait;
+use derive_new::new;
+use eyre::{bail, Result};
+use hyperlane_core::{SignedAnnouncement, SignedCheckpointWithMessageId};
+use std::fmt;
+use ya_gcp::{
+    storage::StorageClient, AuthFlow, ClientBuilder, ClientBuilderConfig, ServiceAccountAuth,
+};
+
+const LATEST_INDEX_KEY: &str = "gcsLatestIndexKey";
+const ANNOUNCEMENT_KEY: &str = "gcsAnnouncementKey";
+
+/// Google Cloud Storage client builder
+#[derive(Debug, new)]
+pub struct GcsStorageClientBuilder {
+    /// A path to the oauth service account key json file.
+    ///
+    /// If this is not provided, `user_secrets` must be.
+    service_account_key: Option<std::path::PathBuf>,
+
+    /// Path to oauth user secrets, like those created by
+    /// `gcloud auth application-default login`
+    ///
+    /// If this is not provided, `service_account_key` must be.
+    user_secrets: Option<std::path::PathBuf>,
+}
+
+/// Google Cloud Storage client
+/// Enables use of any of service account key OR user secrets to authenticate
+pub struct GcsStorageClient {
+    // GCS storage client
+    // # Read methods:
+    //
+    // * `get_object(&self,
+    //        bucket_name: impl AsRef<str>,
+    //        object_name: impl AsRef<str>
+    //    ) -> Result<Bytes, ObjectError>`
+    //
+    // * `get_metadata(
+    //        &self,
+    //        bucket_name: impl AsRef<str>,
+    //        object_name: impl AsRef<str>
+    //   ) -> Result<Metadata, ObjectError>`
+    //
+    // # Write methods:
+    //
+    // * `insert_object(
+    //        &self,
+    //        bucket_name: impl AsRef<str>,
+    //        object_name: impl AsRef<str>,
+    //        data: impl Into<Bytes>
+    //    ) -> Result<Metadata, ObjectError>`
+    //
+    // * `insert_with_metadata(
+    //        &self,
+    //        bucket_name: impl AsRef<str>,
+    //        metadata: &Metadata,
+    //        data: impl Into<Bytes>
+    //    ) -> Result<Metadata, ObjectError>`
+    //
+    // # Details: https://docs.rs/ya-gcp/latest/ya_gcp/storage/struct.StorageClient.html
+    inner: StorageClient,
+    // bucket name of this client's storage
+    bucket: String,
+}
+
+impl GcsStorageClientBuilder {
+    /// Instantiates `ya_gcp:StorageClient` based on provided auth method
+    /// # Param
+    /// * `baucket_name` - String name of target bucket to work with, will be used by all store and get ops
+    pub async fn build(self, bucket_name: impl Into<String>) -> Result<GcsStorageClient> {
+        let auth = if let Some(path) = self.service_account_key {
+            AuthFlow::ServiceAccount(ServiceAccountAuth::Path(path))
+        } else if let Some(path) = self.user_secrets {
+            AuthFlow::UserAccount(path)
+        } else {
+            bail!("at least one of service-account-key or user-secrets must be specified");
+        };
+
+        let inner = ClientBuilder::new(ClientBuilderConfig::new().auth_flow(auth))
+            .await?
+            .build_storage_client();
+        let bucket = bucket_name.into();
+
+        Ok(GcsStorageClient { inner, bucket })
+    }
+}
+
+impl GcsStorageClient {
+    // convinience formatter
+    fn get_checkpoint_key(index: u32) -> String {
+        format!("checkpoint_{index}_with_id.json")
+    }
+}
+
+// required by `CheckpointSyncer`
+impl fmt::Debug for GcsStorageClient {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("S3Storage")
+            .field("bucket", &self.bucket)
+            .finish()
+    }
+}
+
+#[async_trait]
+impl CheckpointSyncer for GcsStorageClient {
+    /// Read the highest index of this Syncer
+    async fn latest_index(&self) -> Result<Option<u32>> {
+        match self.inner.get_object(&self.bucket, LATEST_INDEX_KEY).await {
+            Ok(data) => Ok(Some(serde_json::from_slice(data.as_ref())?)),
+            Err(e) => match e {
+                // never written before to this bucket
+                ya_gcp::storage::ObjectError::InvalidName(_) => Ok(None),
+                _ => bail!(e),
+            },
+        }
+    }
+
+    /// Writes the highest index of this Syncer
+    async fn write_latest_index(&self, index: u32) -> Result<()> {
+        let d = serde_json::to_vec(&index)?;
+        self.inner
+            .insert_object(&self.bucket, LATEST_INDEX_KEY, d)
+            .await?;
+        Ok(())
+    }
+
+    /// Update the latest index of this syncer if necessary
+    async fn update_latest_index(&self, index: u32) -> Result<()> {
+        let curr = self.latest_index().await?.unwrap_or(0);
+        if index > curr {
+            self.write_latest_index(index).await?;
+        }
+        Ok(())
+    }
+
+    /// Attempt to fetch the signed (checkpoint, messageId) tuple at this index
+    async fn fetch_checkpoint(&self, index: u32) -> Result<Option<SignedCheckpointWithMessageId>> {
+        let res = self
+            .inner
+            .get_object(&self.bucket, GcsStorageClient::get_checkpoint_key(index))
+            .await?;
+        let scwmi = serde_json::from_slice(res.as_ref())?;
+        Ok(Some(scwmi))
+    }
+
+    /// Write the signed (checkpoint, messageId) tuple to this syncer
+    async fn write_checkpoint(
+        &self,
+        signed_checkpoint: &SignedCheckpointWithMessageId,
+    ) -> Result<()> {
+        self.inner
+            .insert_object(
+                &self.bucket,
+                GcsStorageClient::get_checkpoint_key(signed_checkpoint.value.index),
+                serde_json::to_vec(signed_checkpoint)?,
+            )
+            .await?;
+        Ok(())
+    }
+
+    /// Write the signed announcement to this syncer
+    async fn write_announcement(&self, signed_announcement: &SignedAnnouncement) -> Result<()> {
+        self.inner
+            .insert_object(
+                &self.bucket,
+                ANNOUNCEMENT_KEY,
+                serde_json::to_string(signed_announcement)?,
+            )
+            .await?;
+        Ok(())
+    }
+
+    /// Return the announcement storage location for this syncer
+    fn announcement_location(&self) -> String {
+        format!("gs://{}/{}", &self.bucket, ANNOUNCEMENT_KEY)
+    }
+}
