5805e9265f722ae9565f1aef09eafb7c6026413b
NicholasDotSol <nicholas.e.evans@gmail.com>
2023-12-16 16:50:04 -0600

    hook fixes

--- solidity/contracts/hooks/wormhole/WormholeHook.sol
+++ solidity/contracts/hooks/wormhole/WormholeHook.sol
@@ -3,6 +3,8 @@ pragma solidity >=0.8.0;
 
 // ============ Internal Imports ============
 import {IPostDispatchHook} from "../../interfaces/hooks/IPostDispatchHook.sol";
+import {Message} from "../../libs/Message.sol";
+import {MailboxClient} from "../../client/MailboxClient.sol";
 
 // TODO: figure out whether it is possible to import this using Hardhat:
 // https://github.com/wormhole-foundation/wormhole/blob/main/ethereum/contracts/interfaces/IWormhole.sol
@@ -14,10 +16,12 @@ interface IWormhole {
     ) external payable returns (uint64 sequence);
 }
 
-contract WormholeHook is IPostDispatchHook {
+contract WormholeHook is IPostDispatchHook, MailboxClient {
+    using Message for bytes;
+
     IWormhole public wormhole;
 
-    constructor(address _wormhole) {
+    constructor(address _wormhole, address _mailbox) MailboxClient(_mailbox) {
         wormhole = IWormhole(_wormhole);
     }
 
@@ -26,14 +30,19 @@ contract WormholeHook is IPostDispatchHook {
     }
 
     function supportsMetadata(bytes calldata) external pure returns (bool) {
-        return false;
+        return true;
     }
 
     function postDispatch(
         bytes calldata,
         bytes calldata message
     ) external payable {
-        wormhole.publishMessage{value: msg.value}(0, message, 200);
+        // ensure hook only dispatches messages that are dispatched by the mailbox
+        bytes32 id = message.id();
+        require(_isLatestDispatched(id), "message not dispatched by mailbox");
+        // use 0 nonce, _isLatestDispatched is sufficient check.
+        // 201 consistency level iis safest as it ensures finality is reached before bridging.
+        wormhole.publishMessage{value: msg.value}(0, abi.encodePacked(id), 201);
     }
 
     function quoteDispatch(
