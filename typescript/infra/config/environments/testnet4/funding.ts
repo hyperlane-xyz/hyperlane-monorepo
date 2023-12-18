e0ac79aee040f215a6189efc62ed865042e11f94
Trevor Porter <trkporter@ucdavis.edu>
2023-12-18 16:25:24 +0000

    Bump desired AVAX balance (#3068)
    ### Description
    
    Avalanche is like 5x more expensive now, see relayer txs
    https://snowtrace.io/address/0x74Cae0ECC47B02Ed9B9D32E000Fd70B9417970C5.
    A single funding of 0.3 is only enough to cover like 2 messages
    
    ### Drive-by changes
    
    <!--
    Are there any minor or drive-by changes also included?
    -->
    
    ### Related issues
    
    <!--
    - Fixes #[issue number here]
    -->
    
    ### Backward compatibility
    
    <!--
    Are these changes backward compatible? Are there any infrastructure
    implications, e.g. changes that would prohibit deploying older commits
    using this infra tooling?
    
    Yes/No
    -->
    
    ### Testing
    
    <!--
    What kind of testing have these changes undergone?
    
    None/Manual/Unit Tests
    -->

--- typescript/infra/config/environments/testnet4/funding.ts
+++ typescript/infra/config/environments/testnet4/funding.ts
@@ -9,7 +9,7 @@ import { environment } from './chains';
 export const keyFunderConfig: KeyFunderConfig = {
   docker: {
     repo: 'gcr.io/abacus-labs-dev/hyperlane-monorepo',
-    tag: 'b86ebc1-20231207-123951',
+    tag: 'f6da03e-20231216-141949',
   },
   // We're currently using the same deployer key as testnet2.
   // To minimize nonce clobbering we offset the key funder cron
