89262180aeadc1866d8643c2fe011e55d090f03b
Le <leyu00@gmail.com>
2023-12-18 17:55:33 -0500

    Add natspec. Move into internal function

--- solidity/contracts/token/HypERC20CollateralVaultDeposit.sol
+++ solidity/contracts/token/HypERC20CollateralVaultDeposit.sol
@@ -6,14 +6,14 @@ import {HypERC20Collateral} from "./HypERC20Collateral.sol";
 import "forge-std/console.sol";
 
 /**
- * @title Hyperlane ERC20 Token Collateral deposits collateral to a vault
+ * @title Hyperlane ERC20 Token Collateral with deposits collateral to a vault
  * @author brolee
  */
 contract HypERC20CollateralVaultDeposit is HypERC20Collateral {
     // Address of the ERC4626 compatible vault
     ERC4626 public immutable vault;
 
-    // Internal balance of total asset deposits
+    // Internal balance of total asset deposited
     uint256 public assetDeposited;
 
     constructor(
@@ -24,6 +24,10 @@ contract HypERC20CollateralVaultDeposit is HypERC20Collateral {
         vault = ERC4626(_vault);
     }
 
+    /**
+     * @dev Transfers `_amount` of `wrappedToken` from `msg.sender` to this contract, and deposit into vault
+     * @inheritdoc HypERC20Collateral
+     */
     function _transferFromSender(
         uint256 _amount
     ) internal override returns (bytes memory metadata) {
@@ -31,34 +35,57 @@ contract HypERC20CollateralVaultDeposit is HypERC20Collateral {
         _depositIntoVault(_amount);
     }
 
+    /**
+     * @dev Deposits into the vault and increment assetDeposited
+     */
     function _depositIntoVault(uint256 _amount) internal {
         wrappedToken.approve(address(vault), _amount);
         vault.deposit(_amount, address(this));
         assetDeposited += _amount;
     }
 
+    /**
+     * @dev Transfers `_amount` of `wrappedToken` from this contract to `_recipient`.
+     * @inheritdoc HypERC20Collateral
+     */
     function _transferTo(
         address _recipient,
         uint256 _amount,
         bytes calldata _metadata
     ) internal virtual override {
-        // TODO maybe Get slippage from meta data
-        vault.withdraw(_amount, address(this), address(this));
-        assetDeposited -= _amount;
+        _withdrawFromVault(_amount);
         super._transferTo(_recipient, _amount, _metadata);
     }
 
+    /**
+     * @dev Withdraws from the vault and decrement assetDeposited
+     */
+    function _withdrawFromVault(uint256 _amount) internal {
+        vault.withdraw(_amount, address(this), address(this));
+        assetDeposited -= _amount;
+    }
+
+    /**
+     * @notice Allows the owner to redeem excess shares
+     */
     function sweep() external onlyOwner {
-        if (_hasExcessVaultShares()) {
+        if (_excessVaultShares() > 0) {
             uint256 excessShares = vault.maxRedeem(address(this)) -
                 vault.convertToShares(assetDeposited);
             vault.redeem(excessShares, owner(), address(this));
         }
     }
 
-    function _hasExcessVaultShares() internal view returns (bool) {
+    /**
+     * @notice Calculates excess vault shares using the converted assetDeposited and max redeemable shares
+     * @return excess vault shares or 0
+     */
+    function _excessVaultShares() internal view returns (uint256) {
         return
             vault.maxRedeem(address(this)) >
-            vault.convertToShares(assetDeposited);
+                vault.convertToShares(assetDeposited)
+                ? vault.maxRedeem(address(this)) -
+                    vault.convertToShares(assetDeposited)
+                : 0;
     }
 }
