671d43ff90861c9cca0a09a99d71c8ef9fb5bc29
Le <leyu00@gmail.com>
2023-12-24 21:38:12 -0500

    Implement dispatch() with tests

--- solidity/contracts/hooks/LayerZeroHook.sol
+++ solidity/contracts/hooks/LayerZeroHook.sol
@@ -19,24 +19,39 @@ import {Indexed} from "../libs/Indexed.sol";
 import {IPostDispatchHook} from "../interfaces/hooks/IPostDispatchHook.sol";
 import {AbstractPostDispatchHook} from "./libs/AbstractPostDispatchHook.sol";
 import {StandardHookMetadata} from "./libs/StandardHookMetadata.sol";
-import "forge-std/console.sol";
 
 struct LayerZeroMetadata {
     /// @dev the destination chain identifier
-    uint16 _dstChainId;
+    uint16 dstChainId;
     /// @dev the user app address on this EVM chain
-    address _userApplication;
+    address userApplication;
     /// @dev if the source transaction is cheaper than the amount of value passed, refund the additional amount to this address
-    address _refundAddress;
+    address refundAddress;
     /// @dev the custom message to send over LayerZero
-    bytes _payload;
+    bytes payload;
     /// @dev the address on destination chain (in bytes). address length/format may vary by chains
-    bytes _destination;
+    bytes destination;
     /// @dev  parameters for the adapter service, e.g. send some dust native token to dstChain
-    bytes _adapterParam;
+    bytes adapterParam;
 }
 
 interface ILayerZeroEndpoint {
+    // @notice send a LayerZero message to the specified address at a LayerZero endpoint.
+    // @param _dstChainId - the destination chain identifier
+    // @param _destination - the address on destination chain (in bytes). address length/format may vary by chains
+    // @param _payload - a custom bytes payload to send to the destination contract
+    // @param _refundAddress - if the source transaction is cheaper than the amount of value passed, refund the additional amount to this address
+    // @param _zroPaymentAddress - the address of the ZRO token holder who would pay for the transaction
+    // @param _adapterParams - parameters for custom functionality. e.g. receive airdropped native gas from the relayer on destination
+    function send(
+        uint16 _chainId,
+        bytes memory _destination,
+        bytes calldata _payload,
+        address payable _refundAddress,
+        address _zroPaymentAddress,
+        bytes memory _adapterParams
+    ) external payable;
+
     // @notice gets a quote in source native gas, for the amount that send() requires to pay for message delivery
     // @param _dstChainId - the destination chain identifier
     // @param _userApplication - the user app address on this EVM chain
@@ -54,6 +69,8 @@ interface ILayerZeroEndpoint {
 
 contract LayerZeroHook is AbstractPostDispatchHook, MailboxClient, Indexed {
     using StandardHookMetadata for bytes;
+    using Message for bytes;
+
     ILayerZeroEndpoint immutable lZEndpoint;
 
     // In bytes
@@ -76,13 +93,37 @@ contract LayerZeroHook is AbstractPostDispatchHook, MailboxClient, Indexed {
     function _postDispatch(
         bytes calldata metadata,
         bytes calldata message
-    ) internal virtual override {}
+    ) internal virtual override {
+        // ensure hook only dispatches messages that are dispatched by the mailbox
+        bytes32 id = message.id();
+        require(_isLatestDispatched(id), "message not dispatched by mailbox");
+
+        bytes calldata lZMetadata = metadata.getCustomMetadata();
+        (
+            uint16 dstChainId,
+            ,
+            address payable refundAddress,
+            bytes memory payload,
+            bytes memory destination,
+            bytes memory adapterParam
+        ) = parseLzMetadata(lZMetadata);
+
+        /// @dev _zroPaymentAddress is hardcoded to addr(0) because zro tokens should not be used
+        lZEndpoint.send{value: msg.value}(
+            dstChainId,
+            destination,
+            payload,
+            refundAddress,
+            address(0),
+            adapterParam
+        );
+    }
 
     /// @inheritdoc AbstractPostDispatchHook
     function _quoteDispatch(
         bytes calldata metadata,
         bytes calldata message
-    ) internal view virtual override returns (uint256) {
+    ) internal view virtual override returns (uint256 nativeFee) {
         bytes calldata lZMetadata = metadata.getCustomMetadata();
         (
             uint16 dstChainId,
@@ -93,30 +134,38 @@ contract LayerZeroHook is AbstractPostDispatchHook, MailboxClient, Indexed {
             bytes memory adapterParam
         ) = parseLzMetadata(lZMetadata);
 
-        (uint256 nativeFee, ) = lZEndpoint.estimateFees(
+        (nativeFee, ) = lZEndpoint.estimateFees(
             dstChainId,
             userApplication,
             payload,
             false,
             adapterParam
         );
-        return nativeFee;
     }
 
+    /**
+     * @notice Formats LayerZero metadata using default abi encoding
+     * @param layerZeroMetadata LayerZero specific metadata
+     * @return ABI encoded metadata
+     */
     function formatLzMetadata(
         LayerZeroMetadata calldata layerZeroMetadata
     ) public view returns (bytes memory) {
         return
             abi.encode(
-                layerZeroMetadata._dstChainId,
-                layerZeroMetadata._userApplication,
-                layerZeroMetadata._refundAddress,
-                layerZeroMetadata._payload,
-                layerZeroMetadata._destination,
-                layerZeroMetadata._adapterParam
+                layerZeroMetadata.dstChainId,
+                layerZeroMetadata.userApplication,
+                layerZeroMetadata.refundAddress,
+                layerZeroMetadata.payload,
+                layerZeroMetadata.destination,
+                layerZeroMetadata.adapterParam
             );
     }
 
+    /**
+     * @notice Decodes LayerZero metadata. Should be used after formatLzMetadata()
+     * @param lZMetadata ABI encoded metadata
+     */
     function parseLzMetadata(
         bytes calldata lZMetadata
     )
@@ -125,7 +174,7 @@ contract LayerZeroHook is AbstractPostDispatchHook, MailboxClient, Indexed {
         returns (
             uint16 dstChainId,
             address userApplication,
-            address refundAddress,
+            address payable refundAddress,
             bytes memory payload,
             bytes memory destination,
             bytes memory adapterParam
--- solidity/test/hooks/LayerZeroHook.t.sol
+++ solidity/test/hooks/LayerZeroHook.t.sol
@@ -2,21 +2,42 @@
 pragma solidity ^0.8.13;
 
 import {Test} from "forge-std/Test.sol";
-import {StandardHookMetadata} from "../../contracts/hooks/libs/StandardHookMetadata.sol";
 import {LZEndpointMock} from "@layerzerolabs/solidity-examples/contracts/lzApp/mocks/LZEndpointMock.sol";
+import {OmniCounter} from "@layerzerolabs/solidity-examples/contracts/examples/OmniCounter.sol";
+
+import {StandardHookMetadata} from "../../contracts/hooks/libs/StandardHookMetadata.sol";
 import {TestMailbox} from "../../contracts/test/TestMailbox.sol";
+import {TestPostDispatchHook} from "../../contracts/test/TestPostDispatchHook.sol";
 import {LayerZeroHook, LayerZeroMetadata} from "../../contracts/hooks/LayerZeroHook.sol";
 import {IPostDispatchHook} from "../../contracts/interfaces/hooks/IPostDispatchHook.sol";
 
 contract LayerZeroHookTest is Test {
+    OmniCounter crossChainCounterApp;
     LZEndpointMock lZEndpointMock;
     TestMailbox public mailbox;
+    TestPostDispatchHook requiredHook;
     LayerZeroHook hook;
 
     function setUp() public {
         lZEndpointMock = new LZEndpointMock(uint16(block.chainid));
+        crossChainCounterApp = new OmniCounter(address(lZEndpointMock));
+        requiredHook = new TestPostDispatchHook();
         mailbox = new TestMailbox(0);
         hook = new LayerZeroHook(address(mailbox), address(lZEndpointMock));
+
+        mailbox.setRequiredHook(address(requiredHook));
+
+        // Sets the endpoint destinations
+        lZEndpointMock.setDestLzEndpoint(
+            address(crossChainCounterApp),
+            address(lZEndpointMock)
+        );
+
+        // set hook as a trusted remote
+        crossChainCounterApp.setTrustedRemote(
+            uint16(block.chainid),
+            abi.encodePacked(address(hook), address(crossChainCounterApp))
+        );
     }
 
     function testParseLzMetadata_returnsCorrectData() public {
@@ -55,14 +76,20 @@ contract LayerZeroHookTest is Test {
         assertEq(_adapterParam, adapterParam);
     }
 
-    function testQuoteDispatch_returnsCorrectData() public {
+    function testQuoteDispatch_returnsFeeAmount()
+        public
+        returns (uint256 nativeFee, bytes memory metadata)
+    {
         // Build metadata to include LZ-specific data
         // format metadata
         uint16 dstChainId = uint16(block.chainid);
         address userApplication = makeAddr("user app");
         address refundAddress = address(this);
         bytes memory payload = "";
-        bytes memory destination = "";
+        bytes memory destination = abi.encodePacked(
+            address(crossChainCounterApp),
+            address(lZEndpointMock)
+        ); // remoteAndLocalAddresses
         bytes memory adapterParam = "";
         LayerZeroMetadata memory layerZeroMetadata = LayerZeroMetadata(
             dstChainId,
@@ -72,17 +99,31 @@ contract LayerZeroHookTest is Test {
             destination,
             adapterParam
         );
-        bytes memory formattedMetadata = hook.formatLzMetadata(
+        bytes memory formattedLzMetadata = hook.formatLzMetadata(
             layerZeroMetadata
         );
-        bytes memory metadata = StandardHookMetadata.formatMetadata(
+        metadata = StandardHookMetadata.formatMetadata(
             0,
             0,
             refundAddress,
-            formattedMetadata
+            formattedLzMetadata
         );
 
-        assertEq(hook.quoteDispatch(metadata, ""), 252);
+        nativeFee = hook.quoteDispatch(metadata, "");
+        // It costs something
+        assertGt(nativeFee, 0);
+    }
+
+    function testPostDispatch_executesCrossChain() public {
+        (
+            uint256 nativeFee,
+            bytes memory metadata
+        ) = testQuoteDispatch_returnsFeeAmount();
+
+        // dispatch also executes L0 call to increment counter
+        assertEq(crossChainCounterApp.counter(), 0);
+        mailbox.dispatch{value: nativeFee}(0, "", "", metadata, hook);
+        assertEq(crossChainCounterApp.counter(), 1);
     }
 
     function testHookType() public {
