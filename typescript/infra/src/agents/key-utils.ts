375ca0cbec09d3b4159930934a56eeae383d51cc
-f <kunalarora1729@gmail.com>
2024-01-19 10:07:41 -0500

    fetchb local addresses

--- typescript/infra/src/agents/key-utils.ts
+++ typescript/infra/src/agents/key-utils.ts
@@ -475,6 +475,19 @@ async function fetchGCPKeyAddresses(
   return addresses as KeyAsAddress[];
 }
 
+export function fetchLocalKeyAddresses(
+  role: Role,
+  environment: DeployEnvironment,
+  context: Contexts,
+): Address {
+  // Resolve the relative path
+  const filePath = path.resolve(__dirname, `../../config/${role}.json`);
+  const data = fs.readFileSync(filePath, 'utf8');
+  const addresses: LocalRoleAddresses = JSON.parse(data);
+
+  return addresses[environment][context];
+}
+
 function addressesIdentifier(
   environment: DeployEnvironment,
   context: Contexts,
