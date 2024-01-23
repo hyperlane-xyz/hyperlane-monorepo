ba630fbdb8845d4be9503175b42e664f70d10c04
Le <leyu00@gmail.com>
2024-01-23 12:42:13 -0500

    Update to import iface from LZ

--- solidity/contracts/hooks/layer-zero/LayerZeroV1Hook.sol
+++ solidity/contracts/hooks/layer-zero/LayerZeroV1Hook.sol
@@ -12,6 +12,7 @@ pragma solidity >=0.8.0;
   @@@@@@@@@       @@@@@@@@@
  @@@@@@@@@       @@@@@@@@@
 @@@@@@@@@       @@@@@@@@*/
+import {ILayerZeroEndpoint} from "@layerzerolabs/lz-evm-v1-0.7/contracts/interfaces/ILayerZeroEndpoint.sol";
 import {Message} from "../../libs/Message.sol";
 import {TypeCasts} from "../../libs/TypeCasts.sol";
 import {MailboxClient} from "../../client/MailboxClient.sol";
@@ -33,38 +34,6 @@ struct LayerZeroMetadata {
     bytes destination;
     /// @dev  parameters for the adapter service, e.g. send some dust native token to dstChain
     bytes adapterParam;
-}
-
-interface ILayerZeroEndpoint {
-    // @notice send a LayerZero message to the specified address at a LayerZero endpoint.
-    // @param _dstChainId - the destination chain identifier
-    // @param _destination - the address on destination chain (in bytes). address length/format may vary by chains
-    // @param _payload - a custom bytes payload to send to the destination contract
-    // @param _refundAddress - if the source transaction is cheaper than the amount of value passed, refund the additional amount to this address
-    // @param _zroPaymentAddress - the address of the ZRO token holder who would pay for the transaction
-    // @param _adapterParams - parameters for custom functionality. e.g. receive airdropped native gas from the relayer on destination
-    function send(
-        uint16 _chainId,
-        bytes memory _destination,
-        bytes calldata _payload,
-        address payable _refundAddress,
-        address _zroPaymentAddress,
-        bytes memory _adapterParams
-    ) external payable;
-
-    // @notice gets a quote in source native gas, for the amount that send() requires to pay for message delivery
-    // @param _dstChainId - the destination chain identifier
-    // @param _userApplication - the user app address on this EVM chain
-    // @param _payload - the custom message to send over LayerZero
-    // @param _payInZRO - if false, user app pays the protocol fee in native token
-    // @param _adapterParam - parameters for the adapter service, e.g. send some dust native token to dstChain
-    function estimateFees(
-        uint16 _dstChainId,
-        address _userApplication,
-        bytes calldata _payload,
-        bool _payInZRO,
-        bytes calldata _adapterParam
-    ) external view returns (uint nativeFee, uint zroFee);
 }
 
 contract LayerZeroV1Hook is AbstractPostDispatchHook, MailboxClient, Indexed {
