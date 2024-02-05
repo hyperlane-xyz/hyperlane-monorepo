58de46a0c3876d4201868160ead54f6d7d85c39a
-f <kunalarora1729@gmail.com>
2024-02-05 17:28:38 -0500

    rm basesepolia

--- typescript/sdk/src/consts/chainMetadata.ts
+++ typescript/sdk/src/consts/chainMetadata.ts
@@ -181,30 +181,6 @@ export const basegoerli: ChainMetadata = {
   ],
 };
 
-export const basesepolia: ChainMetadata = {
-  blockExplorers: [
-    {
-      apiUrl: 'https://api-goerli.basescan.org/api',
-      family: ExplorerFamily.Etherscan,
-      name: 'BaseScan',
-      url: 'https://goerli.basescan.org',
-    },
-  ],
-  blocks: {
-    confirmations: 1,
-    estimateBlockTime: 3,
-    reorgPeriod: 1,
-  },
-  chainId: 84532,
-  displayName: 'Base Sepolia',
-  domainId: 84532,
-  isTestnet: true,
-  name: Chains.basesepolia,
-  nativeToken: etherToken,
-  protocol: ProtocolType.Ethereum,
-  rpcUrls: [{ http: 'https://sepolia.base.org' }],
-};
-
 export const bsc: ChainMetadata = {
   blockExplorers: [
     {
@@ -1016,7 +992,6 @@ export const chainMetadata: ChainMap<ChainMetadata> = {
   avalanche,
   base,
   basegoerli,
-  basesepolia,
   bsc,
   bsctestnet,
   celo,
