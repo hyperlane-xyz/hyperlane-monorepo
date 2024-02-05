f2819ebde9f99527d6300df719439e14453a82fb
-f <kunalarora1729@gmail.com>
2024-02-05 13:15:03 -0500

    temp

--- typescript/infra/config/environments/index.ts
+++ typescript/infra/config/environments/index.ts
@@ -1,9 +1,18 @@
 import { environment as mainnet3 } from './mainnet3';
+import { agents as mainnet3Agents } from './mainnet3/agent';
 import { environment as test } from './test';
+import { agents as testAgents } from './test/agent';
 import { environment as testnet4 } from './testnet4';
+import { agents as testnet4Agents } from './testnet4/agent';
 
 export const environments = {
   test,
   testnet4,
   mainnet3,
+};
+
+export const agents = {
+  mainnet3: mainnet3Agents,
+  testnet4: testnet4Agents,
+  test: testAgents,
 };
--- typescript/infra/scripts/create-keys.ts
+++ typescript/infra/scripts/create-keys.ts
@@ -1,10 +1,13 @@
-import { createAgentKeysIfNotExists } from '../src/agents/key-utils';
-
 import { getAgentConfigsBasedOnArgs } from './agent-utils';
 
 async function main() {
   const { agentConfig, newThresholds } = await getAgentConfigsBasedOnArgs();
-  await createAgentKeysIfNotExists(agentConfig, newThresholds);
+  console.log(
+    'Creating keys for agent config:',
+    newThresholds,
+    JSON.stringify(agentConfig, null, 2),
+  );
+  // await createAgentKeysIfNotExists(agentConfig, newThresholds);
   return 'Keys created successfully!';
 }
 
