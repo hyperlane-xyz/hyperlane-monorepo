e192d31315797a184a18cabf16dbab8ae07a54a5
Yorke Rhodes <yorke@hyperlane.xyz>
2024-02-06 17:09:01 -0500

    Deploy USDC warp route to inEVM

--- typescript/infra/scripts/deploy.ts
+++ typescript/infra/scripts/deploy.ts
@@ -82,7 +82,7 @@ async function main() {
     const ethereum = {
       ...routerConfig.ethereum,
       type: TokenType.collateral,
-      token: '0xdac17f958d2ee523a2206206994597c13d831ec7',
+      token: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
     };
     config = {
       inevm,
