d5311da0f18b4ea0841800587d0d04a69cc76bee
-f <kunalarora1729@gmail.com>
2024-02-07 12:53:09 -0500

    add to merged agent artifacts

--- typescript/cli/src/deploy/core.ts
+++ typescript/cli/src/deploy/core.ts
@@ -484,6 +484,10 @@ async function writeAgentConfig(
     artifacts,
   ) as ChainMap<HyperlaneDeploymentArtifacts>;
 
+  for (const chain of chains) {
+    mergedAddressesMap[chain].interchainGasPaymaster =
+      ethers.constants.AddressZero;
+  }
   const agentConfig = buildAgentConfig(
     chains, // Use only the chains that were deployed to
     multiProvider,
