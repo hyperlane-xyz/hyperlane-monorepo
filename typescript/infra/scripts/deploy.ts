66d9b2907dd9308e630292876c1c6ad985cf692c
Yorke Rhodes <yorke@hyperlane.xyz>
2024-02-13 16:39:58 -0500

    Deploy stable warp routes to inEVM (#3233)
    ### Description
    
    Deploy USDC and USDT from ethereum to inEVM
    
    ### Related issues
    
    - Fixes https://github.com/hyperlane-xyz/issues/issues/1010
    
    ### Backward compatibility
    
    Yes
    
    ### Testing
    
    - [x] Manul warp UI testing
    
    ---------
    
    Co-authored-by: Daniel Savu <23065004+daniel-savu@users.noreply.github.com>
    Co-authored-by: J M Rossy <jm.rossy@gmail.com>

--- typescript/infra/scripts/deploy.ts
+++ typescript/infra/scripts/deploy.ts
@@ -4,6 +4,7 @@ import { prompt } from 'prompts';
 import { HelloWorldDeployer } from '@hyperlane-xyz/helloworld';
 import {
   ChainMap,
+  HypERC20Deployer,
   HyperlaneCore,
   HyperlaneCoreDeployer,
   HyperlaneDeployer,
@@ -13,10 +14,12 @@ import {
   InterchainAccountDeployer,
   InterchainQueryDeployer,
   LiquidityLayerDeployer,
+  TokenType,
 } from '@hyperlane-xyz/sdk';
 import { objMap } from '@hyperlane-xyz/utils';
 
 import { Contexts } from '../config/contexts';
+import { aggregationIsm } from '../config/routingIsm';
 import { deployEnvToSdkEnv } from '../src/config/environment';
 import { deployWithArtifacts } from '../src/deployment/deploy';
 import { TestQuerySenderDeployer } from '../src/deployment/testcontracts/testquerysender';
@@ -71,7 +74,28 @@ async function main() {
     );
     deployer = new HyperlaneCoreDeployer(multiProvider, ismFactory);
   } else if (module === Modules.WARP) {
-    throw new Error('Warp is not supported. Use CLI instead.');
+    const core = HyperlaneCore.fromEnvironment(env, multiProvider);
+    const ismFactory = HyperlaneIsmFactory.fromAddressesMap(
+      getAddresses(environment, Modules.PROXY_FACTORY),
+      multiProvider,
+    );
+    const routerConfig = core.getRouterConfig(envConfig.owners);
+    const inevm = {
+      ...routerConfig.inevm,
+      type: TokenType.synthetic,
+    };
+    const ethereum = {
+      ...routerConfig.ethereum,
+      type: TokenType.collateral,
+      token: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
+      hook: '0xb87AC8EA4533AE017604E44470F7c1E550AC6F10', // aggregation of IGP and Merkle, arbitrary config not supported for now
+      interchainSecurityModule: aggregationIsm('inevm', Contexts.Hyperlane),
+    };
+    config = {
+      inevm,
+      ethereum,
+    };
+    deployer = new HypERC20Deployer(multiProvider, ismFactory);
   } else if (module === Modules.INTERCHAIN_GAS_PAYMASTER) {
     config = envConfig.igp;
     deployer = new HyperlaneIgpDeployer(multiProvider);
