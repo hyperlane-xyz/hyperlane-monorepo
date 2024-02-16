cbb2c75e5c0f8801f998c2130ef880e796a3c6cb
armanthepythonguy <armanityours@gmail.com>
2024-02-16 21:05:35 +0530

    changed-message

--- typescript/cli/src/utils/version-check.ts
+++ typescript/cli/src/utils/version-check.ts
@@ -1,11 +1,11 @@
 import latestVersion from 'latest-version';
 
-import { logRed } from '../../logger.js';
+import { log } from '../../logger.js';
 import { VERSION } from '../version.js';
 
 export async function checkVersion() {
   const currentVersion = await latestVersion('@hyperlane-xyz/cli');
   if (VERSION < currentVersion) {
-    logRed(`Please update your CLI to latest version(${currentVersion})`);
+    log(`Your CLI version: ${VERSION}, latest version: ${currentVersion}`);
   }
 }
