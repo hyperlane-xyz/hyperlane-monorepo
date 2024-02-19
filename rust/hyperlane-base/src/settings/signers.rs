b2438238d4934b81936bb6f6b7d36e593bbfc267
Daniel Savu <23065004+daniel-savu@users.noreply.github.com>
2024-02-19 16:37:09 +0000

    fix: lower rusoto timeout to 15s (#3283)
    Applies the fix in
    https://github.com/hyperlane-xyz/hyperlane-monorepo/pull/2384 everywhere
    an `HttpClient` is constructed via rusoto.
    
    It lowers the S3 timeout to 15s based on tips in [this
    thread](https://github.com/hyperium/hyper/issues/2136#issuecomment-589488526),
    to avoid `Error during dispatch: connection closed before message
    completed` errors. Note that we'll probably still run into these issues,
    but less frequently
    ([source](https://github.com/rusoto/rusoto/issues/1766#issuecomment-639299961)).
    
    <!--
    Are there any minor or drive-by changes also included?
    -->
    
    <!--
    - Fixes #[issue number here]
    -->
    
    <!--
    Are these changes backward compatible? Are there any infrastructure
    implications, e.g. changes that would prohibit deploying older commits
    using this infra tooling?
    
    Yes/No
    -->
    
    <!--
    What kind of testing have these changes undergone?
    
    None/Manual/Unit Tests
    -->

--- rust/hyperlane-base/src/settings/signers.rs
+++ rust/hyperlane-base/src/settings/signers.rs
@@ -1,16 +1,15 @@
-use std::time::Duration;
-
 use async_trait::async_trait;
 use ed25519_dalek::SecretKey;
 use ethers::prelude::{AwsSigner, LocalWallet};
 use eyre::{bail, Context, Report};
 use hyperlane_core::H256;
 use hyperlane_sealevel::Keypair;
-use rusoto_core::{HttpClient, HttpConfig, Region};
+use rusoto_core::Region;
 use rusoto_kms::KmsClient;
 use tracing::instrument;
 
 use super::aws_credentials::AwsChainCredentialsProvider;
+use crate::types::utils;
 
 /// Signer types
 #[derive(Default, Debug, Clone)]
@@ -59,13 +58,10 @@ impl BuildableWithSignerConf for hyperlane_ethereum::Signers {
                 ),
             )),
             SignerConf::Aws { id, region } => {
-                let mut config = HttpConfig::new();
-                // see https://github.com/hyperium/hyper/issues/2136#issuecomment-589345238
-                config.pool_idle_timeout(Duration::from_secs(20));
                 let client = KmsClient::new_with_client(
                     rusoto_core::Client::new_with(
                         AwsChainCredentialsProvider::new(),
-                        HttpClient::new_with_config(config).unwrap(),
+                        utils::http_client_with_timeout().unwrap(),
                     ),
                     region.clone(),
                 );
--- rust/hyperlane-base/src/types/mod.rs
+++ rust/hyperlane-base/src/types/mod.rs
@@ -2,6 +2,8 @@ mod local_storage;
 mod multisig;
 mod s3_storage;
 
+/// Reusable logic for working with storage backends.
+pub mod utils;
 pub use local_storage::*;
 pub use multisig::*;
 pub use s3_storage::*;
