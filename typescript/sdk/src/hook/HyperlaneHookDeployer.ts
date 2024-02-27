eeac4679e4b58132f16027742c1ed145cf6a9bab
Nam Chu Hoai <nambrot@googlemail.com>
2024-02-26 21:09:27 -0500

    Account for non-evm for domain fetching (#3316)
    ### Description
    
    Copies https://github.com/hyperlane-xyz/hyperlane-monorepo/pull/3275 for
    the core deployer

--- typescript/sdk/src/hook/HyperlaneHookDeployer.ts
+++ typescript/sdk/src/hook/HyperlaneHookDeployer.ts
@@ -12,6 +12,7 @@ import {
 } from '@hyperlane-xyz/core';
 import { Address, addressToBytes32 } from '@hyperlane-xyz/utils';
 
+import { chainMetadata } from '../consts/chainMetadata';
 import { HyperlaneContracts } from '../contracts/types';
 import { CoreAddresses } from '../core/contracts';
 import { HyperlaneDeployer } from '../deploy/HyperlaneDeployer';
@@ -287,7 +288,8 @@ export class HyperlaneHookDeployer extends HyperlaneDeployer<
 
     const routingConfigs: DomainRoutingHook.HookConfigStruct[] = [];
     for (const [dest, hookConfig] of Object.entries(config.domains)) {
-      const destDomain = this.multiProvider.getDomainId(dest);
+      const destDomain =
+        chainMetadata[dest]?.domainId ?? this.multiProvider.getDomainId(dest);
       if (typeof hookConfig === 'string') {
         routingConfigs.push({
           destination: destDomain,
