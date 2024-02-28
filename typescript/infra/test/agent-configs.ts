586ee2802910baf23565b7f73a7ab7edd6e0724a
Trevor Porter <trkporter@ucdavis.edu>
2024-02-28 15:48:51 +0000

    Renames, add test to ensure agent configs match supported chains

--- typescript/infra/test/agent-configs.ts
+++ typescript/infra/test/agent-configs.ts
@@ -1,34 +1,42 @@
 import { expect } from 'chai';
 
-import { Contexts } from '../config/contexts';
+import mainnet3AgentConfig from '../../../rust/config/mainnet3_config.json';
+import testnet4AgentConfig from '../../../rust/config/testnet4_config.json';
 import { hyperlaneContextAgentChainConfig as mainnet3AgentChainConfig } from '../config/environments/mainnet3/agent';
 import { supportedChainNames as mainnet3SupportedChainNames } from '../config/environments/mainnet3/chains';
 import { hyperlaneContextAgentChainConfig as testnet4AgentChainConfig } from '../config/environments/testnet4/agent';
 import { supportedChainNames as testnet4SupportedChainNames } from '../config/environments/testnet4/chains';
-import { AgentAwsKey } from '../src/agents/aws';
-import { AgentGCPKey } from '../src/agents/gcp';
-import { ReadOnlyCloudAgentKey } from '../src/agents/keys';
 import { ensureAgentChainConfigIncludesAllChainNames } from '../src/config';
-import { Role } from '../src/roles';
 
 const environmentChainConfigs = {
   mainnet3: {
     agentChainConfig: mainnet3AgentChainConfig,
+    agentJsonConfig: mainnet3AgentConfig,
     supportedChainNames: mainnet3SupportedChainNames,
   },
   testnet4: {
     agentChainConfig: testnet4AgentChainConfig,
+    agentJsonConfig: testnet4AgentConfig,
     supportedChainNames: testnet4SupportedChainNames,
   },
 };
 
-describe('Environment agent chain configs', () => {
+describe('Agent configs', () => {
   Object.entries(environmentChainConfigs).forEach(([environment, config]) => {
     describe(`Environment: ${environment}`, () => {
-      it('Specifies all chains for each role in the agent chain config', () => {
+      it('AgentChainConfig specifies all chains for each role in the agent chain config', () => {
         // This will throw if there are any inconsistencies
         ensureAgentChainConfigIncludesAllChainNames(
           config.agentChainConfig,
+          config.supportedChainNames,
+        );
+      });
+
+      it('Agent JSON config matches environment chains', () => {
+        const agentJsonConfigChains = Object.keys(
+          config.agentJsonConfig.chains,
+        );
+        expect(agentJsonConfigChains).to.have.members(
           config.supportedChainNames,
         );
       });
