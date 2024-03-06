3eff18249a50c27a6382910666c6d0b501877b6d
Yorke Rhodes <yorke@hyperlane.xyz>
2024-03-06 11:11:41 -0700

    Do not double-hash in signing

--- typescript/infra/add-delegate.sh
+++ typescript/infra/add-delegate.sh
@@ -1,16 +1,21 @@
 CHAIN=$1
 DELEGATE=$2
 
+if [ -z "$CHAIN" ] || [ -z "$DELEGATE" ]; then
+  echo "Usage: $0 <chain> <delegate>"
+  exit 1
+fi
+
 set -x
 
 SAFE_URL=`yarn ts-node ./scripts/print-chain-metadatas.ts -e mainnet3 | jq -r ".$CHAIN.gnosisSafeTransactionServiceUrl"`
 SAFE_ADDRESS=`yarn ts-node ./scripts/print-safes.ts | jq -r ".$CHAIN"`
 
+# see https://safe-transaction-mainnet.safe.global delegates endpoint docs
 TOTP=$(($(date +%s) / 3600))
-HASH=$(cast keccak "$DELEGATE$TOTP")
 
 DELEGATOR=$(cast wallet address --ledger)
-SIGNATURE=$(cast wallet sign --ledger $HASH)
+SIGNATURE=$(cast wallet sign --ledger "$DELEGATE$TOTP")
 
 DATA=$(jq -n \
   --arg safe "$SAFE_ADDRESS" \
