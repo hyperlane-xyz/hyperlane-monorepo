9d3a90e1268fb0e0357b1062121596b3cfa94837
Paul Balaji <paul@hyperlane.xyz>
2024-03-08 14:11:07 +0000

    update keyfunder with deployed image version

--- typescript/infra/config/environments/mainnet3/funding.ts
+++ typescript/infra/config/environments/mainnet3/funding.ts
@@ -9,7 +9,7 @@ import { environment } from './chains';
 export const keyFunderConfig: KeyFunderConfig = {
   docker: {
     repo: 'gcr.io/abacus-labs-dev/hyperlane-monorepo',
-    tag: 'c037206-20240220-152500',
+    tag: '402a7d4-20240308-134716',
   },
   // We're currently using the same deployer key as mainnet.
   // To minimize nonce clobbering we offset the key funder cron
