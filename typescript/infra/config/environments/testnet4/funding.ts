3253469e363e053a2dd9e019476b1fa5fa5e2717
Paul Balaji <github@pbio.xyz>
2024-03-09 11:38:38 +0000

    Revert "feat: tweak keyfunder params (#3301)"
    This reverts commit 9abdf7c832f2ba59080cdee339228f27eeaae362.

--- typescript/infra/config/environments/testnet4/funding.ts
+++ typescript/infra/config/environments/testnet4/funding.ts
@@ -9,7 +9,7 @@ import { environment } from './chains';
 export const keyFunderConfig: KeyFunderConfig = {
   docker: {
     repo: 'gcr.io/abacus-labs-dev/hyperlane-monorepo',
-    tag: '402a7d4-20240308-134716',
+    tag: 'c037206-20240220-152500',
   },
   // We're currently using the same deployer key as testnet2.
   // To minimize nonce clobbering we offset the key funder cron
@@ -27,17 +27,16 @@ export const keyFunderConfig: KeyFunderConfig = {
   // desired balance config
   desiredBalancePerChain: {
     alfajores: '5',
+    arbitrumgoerli: '0.5',
     bsctestnet: '5',
     fuji: '5',
     goerli: '0.5',
     mumbai: '5',
-    plumetestnet: '0.2',
-    sepolia: '5',
-    // Funder boosts itself upto 5x balance on L2 before dispersing funds
-    arbitrumgoerli: '0.5',
     optimismgoerli: '0.5',
+    plumetestnet: '0.2',
     polygonzkevmtestnet: '1',
     scrollsepolia: '1',
+    sepolia: '5',
   },
   desiredKathyBalancePerChain: {
     plumetestnet: '0.05',
