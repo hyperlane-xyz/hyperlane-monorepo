5cbc0651c2faec6a49ff3e2e76507ba77fe1f09f
-f <kunalarora1729@gmail.com>
2024-03-12 17:44:52 -0400

    changes

--- typescript/sdk/src/token/app.ts
+++ typescript/sdk/src/token/app.ts
@@ -1,3 +1,6 @@
+import { appFromAddressesMapHelper } from '../contracts/contracts';
+import { HyperlaneAddressesMap } from '../contracts/types';
+import { MultiProvider } from '../providers/MultiProvider';
 import { RouterApp } from '../router/RouterApps';
 
 import { HypERC20Factories } from './contracts';
@@ -9,5 +12,17 @@ export class HypERC20App extends RouterApp<HypERC20Factories> {
 
   router(contracts: any): any {
     return contracts.hypERC20Router;
+  }
+
+  static fromAddressesMap(
+    addressesMap: HyperlaneAddressesMap<any>,
+    multiProvider: MultiProvider,
+  ): HypERC20App {
+    const helper = appFromAddressesMapHelper(
+      addressesMap,
+      // factories,
+      multiProvider,
+    );
+    return new HypERC20App(helper.contractsMap, helper.multiProvider);
   }
 }
