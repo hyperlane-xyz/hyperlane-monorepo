053501e50bdbd232c27c216c470a0b568187cb40
J M Rossy <jm.rossy@gmail.com>
2024-03-25 14:38:25 -0400

    Update typescript/infra/src/utils/metrics.ts
    Co-authored-by: Paul Balaji <paul@hyperlane.xyz>

--- typescript/infra/src/utils/metrics.ts
+++ typescript/infra/src/utils/metrics.ts
@@ -47,7 +47,7 @@ export async function submitMetrics(
  * Start a simple HTTP server to host metrics. This just takes the registry and dumps the text
  * string to people who request `GET /metrics`.
  *
- * PROMETHEUS_PORT env var is used to determine what host port, defaults to 9090.
+ * PROMETHEUS_PORT env var is used to determine what port to host on, defaults to 9090.
  */
 export function startMetricsServer(register: Registry): http.Server {
   return http
