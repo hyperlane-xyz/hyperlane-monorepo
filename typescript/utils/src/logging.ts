ec612947c0e86b7267ad14992377ca304d29fb59
J M Rossy <jm.rossy@gmail.com>
2024-03-26 16:10:25 -0400

    PR feedback

--- typescript/utils/src/logging.ts
+++ typescript/utils/src/logging.ts
@@ -2,32 +2,35 @@ import { LevelWithSilent, pino } from 'pino';
 
 import { envVarToBoolean, safelyAccessEnvVar } from './env';
 
-const DEFAULT_LOG_LEVEL = 'warn';
-
-let ENV_LOG_LEVEL = (
-  safelyAccessEnvVar('LOG_LEVEL') ?? DEFAULT_LOG_LEVEL
-).toLowerCase() as LevelWithSilent | 'none' | 'off';
+let logLevel: LevelWithSilent = 'info';
+const envLogLevel = safelyAccessEnvVar('LOG_LEVEL')?.toLowerCase();
+if (envLogLevel && pino.levels.values[envLogLevel]) {
+  logLevel = envLogLevel as LevelWithSilent;
+}
 // For backwards compat and also to match agent level options
-if (ENV_LOG_LEVEL === 'none' || ENV_LOG_LEVEL === 'off')
-  ENV_LOG_LEVEL = 'silent';
+else if (envLogLevel === 'none' || envLogLevel === 'off') {
+  logLevel = 'silent';
+}
 
-const ENV_LOG_PRETTY = envVarToBoolean(safelyAccessEnvVar('LOG_PRETTY'));
+const logPretty = envVarToBoolean(safelyAccessEnvVar('LOG_PRETTY'));
 
 export const rootLogger = pino({
-  level: ENV_LOG_LEVEL,
+  level: logLevel,
   name: 'hyperlane',
   formatters: {
-    // Remove pino's default bindings of hostname and pid
-    bindings: () => ({}),
+    // Remove pino's default bindings of hostname but keep pid
+    bindings: (defaultBindings) => ({ pid: defaultBindings.pid }),
   },
   hooks: {
     logMethod(inputArgs, method, level) {
       // Pino has no simple way of setting custom log shapes and they
       // recommend against using pino-pretty in production so when
       // pretty is enabled we circumvent pino and log directly to console
-      if (ENV_LOG_PRETTY && level >= pino.levels.values[ENV_LOG_LEVEL]) {
+      if (logPretty && level >= pino.levels.values[logLevel]) {
+        // Log the first argument, which is the message
         // eslint-disable-next-line no-console
         console.log(inputArgs[0]);
+        // Then return null to prevent pino from logging
         return null;
       }
       return method.apply(this, inputArgs);
