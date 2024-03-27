3183b8a3d1dcf394bf17dd2e5e99acff0d0019a1
Paul Balaji <paul@hyperlane.xyz>
2024-03-27 16:28:33 +0000

    add comment to deployContractWithName

--- typescript/sdk/src/deploy/HyperlaneDeployer.ts
+++ typescript/sdk/src/deploy/HyperlaneDeployer.ts
@@ -379,6 +379,19 @@ export abstract class HyperlaneDeployer<
     return contract;
   }
 
+  /**
+   * Deploys a contract with a specified name.
+   *
+   * This is a generic function capable of deploying any contract type, defined within the `Factories` type, to a specified chain.
+   *
+   * @param {ChainName} chain - The name of the chain on which the contract is to be deployed.
+   * @param {K} contractKey - The key identifying the factory to use for deployment.
+   * @param {string} contractName - The name of the contract to deploy. This must match the contract source code.
+   * @param {Parameters<Factories[K]['deploy']>} constructorArgs - Arguments for the contract's constructor.
+   * @param {Parameters<Awaited<ReturnType<Factories[K]['deploy']>>['initialize']>?} initializeArgs - Optional arguments for the contract's initialization function.
+   * @param {boolean} shouldRecover - Flag indicating whether to attempt recovery if deployment fails.
+   * @returns {Promise<HyperlaneContracts<Factories>[K]>} A promise that resolves to the deployed contract instance.
+   */
   async deployContractWithName<K extends keyof Factories>(
     chain: ChainName,
     contractKey: K,
