4f79341ffb476c98b0eaccfff1b87517f270ff76
J M Rossy <jm.rossy@gmail.com>
2024-03-28 16:27:04 -0400

    Add global log options to CLI Improve consistency with agent log options

--- typescript/cli/env.ts
+++ typescript/cli/env.ts
@@ -1,9 +1,6 @@
 // This file isn't in the src dir so it it's imported before others
 // See https://github.com/trivago/prettier-plugin-sort-imports/issues/112
 
-// Default rootLogger into pretty mode
-process.env.LOG_PRETTY ??= 'true';
-
 // Workaround for bug in bigint-buffer which solana-web3.js depends on
 // https://github.com/no2chem/bigint-buffer/issues/31#issuecomment-1752134062
 const defaultWarn = console.warn;
