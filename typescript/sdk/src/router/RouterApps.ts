a61e2816962b4b8de39bf2ceed94363b87de10ec
-f <kunalarora1729@gmail.com>
2024-04-03 11:07:39 -0400

    add more accurate types

--- typescript/sdk/src/router/RouterApps.ts
+++ typescript/sdk/src/router/RouterApps.ts
@@ -47,14 +47,14 @@ export abstract class RouterApp<
   // check onchain for remote enrollments
   override async remoteChains(chainName: string): Promise<ChainName[]> {
     const router = this.router(this.contractsMap[chainName]);
-    const domains = (await router.domains()).map((domain) => {
+    const domainIds = (await router.domains()).map((domain) => {
       const chainName = this.multiProvider.tryGetChainName(domain);
       if (chainName === null) {
         throw new Error(`Chain name not found for domain: ${domain}`);
       }
       return chainName;
     });
-    return domains;
+    return domainIds;
   }
 
   getSecurityModules(): Promise<ChainMap<Address>> {
--- typescript/sdk/src/token/app.ts
+++ typescript/sdk/src/token/app.ts
@@ -2,14 +2,21 @@ import { TokenRouter } from '@hyperlane-xyz/core';
 import { objKeys } from '@hyperlane-xyz/utils';
 
 import { appFromAddressesMapHelper } from '../contracts/contracts';
-import { HyperlaneAddressesMap, HyperlaneContracts } from '../contracts/types';
+import {
+  HyperlaneAddressesMap,
+  HyperlaneContracts,
+  HyperlaneContractsMap,
+} from '../contracts/types';
 import { MultiProvider } from '../providers/MultiProvider';
 import { GasRouterApp } from '../router/RouterApps';
 
 import { HypERC20Factories, hypERC20factories } from './contracts';
 
 export class HypERC20App extends GasRouterApp<HypERC20Factories, TokenRouter> {
-  constructor(contractsMap: any, multiProvider: any) {
+  constructor(
+    contractsMap: HyperlaneContractsMap<HypERC20Factories>,
+    multiProvider: MultiProvider,
+  ) {
     super(contractsMap, multiProvider);
   }
 
