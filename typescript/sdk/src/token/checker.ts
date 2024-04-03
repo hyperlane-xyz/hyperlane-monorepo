8a9df2301aa94bac90d7e87fc8770c395be9846c
-f <kunalarora1729@gmail.com>
2024-04-03 18:56:33 -0400

    fix type checker

--- typescript/sdk/src/token/checker.ts
+++ typescript/sdk/src/token/checker.ts
@@ -1,6 +1,6 @@
 import { BigNumber } from 'ethers';
 
-import { ERC20, ERC20__factory } from '@hyperlane-xyz/core';
+import { ERC20, ERC20__factory, HypERC20Collateral } from '@hyperlane-xyz/core';
 import { eqAddress } from '@hyperlane-xyz/utils';
 
 import { TokenMismatchViolation } from '../deploy/types';
@@ -78,13 +78,15 @@ export class HypERC20Checker extends HyperlaneRouterChecker<
         this.addViolation(violation);
       }
     } else if (isSyntheticConfig(expectedConfig)) {
-      await checkERC20(hypToken, expectedConfig);
+      await checkERC20(hypToken as unknown as ERC20, expectedConfig);
     } else if (isCollateralConfig(expectedConfig)) {
       const collateralToken = await ERC20__factory.connect(
         expectedConfig.token,
         this.multiProvider.getProvider(chain),
       );
-      const actualToken = await hypToken.collateralToken();
+      const actualToken = await (
+        hypToken as unknown as HypERC20Collateral
+      ).wrappedToken();
       if (!eqAddress(collateralToken.address, actualToken)) {
         const violation: TokenMismatchViolation = {
           type: 'CollateralTokenMismatch',
