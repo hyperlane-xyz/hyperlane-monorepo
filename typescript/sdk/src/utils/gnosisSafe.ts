a1b723c5307797597fd9b4febbd3992949f9a54a
Noah Bayindirli 🥂 <noah@primeprotocol.xyz>
2024-04-22 18:32:19 -0400

    add Gnosis safe util to SDK

---
+++ typescript/sdk/src/utils/gnosisSafe.ts
@@ -0,0 +1,58 @@
+import SafeApiKit from '@safe-global/api-kit';
+import Safe, { EthersAdapter } from '@safe-global/protocol-kit';
+import { ethers } from 'ethers';
+
+import { chainMetadata } from '../consts/chainMetadata.js';
+import { MultiProvider } from '../providers/MultiProvider.js';
+import { ChainName } from '../types.js';
+
+export function getSafeService(
+  chain: ChainName,
+  multiProvider: MultiProvider,
+): SafeApiKit.default {
+  const signer = multiProvider.getSigner(chain);
+  const ethAdapter = new EthersAdapter({ ethers, signerOrProvider: signer });
+  const txServiceUrl = chainMetadata[chain].gnosisSafeTransactionServiceUrl;
+  if (!txServiceUrl)
+    throw new Error(`must provide tx service url for ${chain}`);
+  return new SafeApiKit.default({ txServiceUrl, ethAdapter });
+}
+
+export function getSafe(
+  chain: ChainName,
+  multiProvider: MultiProvider,
+  safeAddress: string,
+): Promise<Safe.default> {
+  const signer = multiProvider.getSigner(chain);
+  const ethAdapter = new EthersAdapter({ ethers, signerOrProvider: signer });
+  return Safe.default.create({
+    ethAdapter,
+    safeAddress: safeAddress,
+  });
+}
+
+export async function getSafeDelegates(
+  service: SafeApiKit.default,
+  safeAddress: string,
+) {
+  const delegateResponse = await service.getSafeDelegates({ safeAddress });
+  return delegateResponse.results.map((r) => r.delegate);
+}
+
+export async function canProposeSafeTransactions(
+  proposer: string,
+  chain: ChainName,
+  multiProvider: MultiProvider,
+  safeAddress: string,
+): Promise<boolean> {
+  let safeService;
+  try {
+    safeService = getSafeService(chain, multiProvider);
+  } catch (e) {
+    return false;
+  }
+  const safe = await getSafe(chain, multiProvider, safeAddress);
+  const delegates = await getSafeDelegates(safeService, safeAddress);
+  const owners = await safe.getOwners();
+  return delegates.includes(proposer) || owners.includes(proposer);
+}
