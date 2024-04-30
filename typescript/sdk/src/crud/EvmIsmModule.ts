8ac752b41e9d45494096e3f73dbcb7931414d4d1
Paul Balaji <paul@hyperlane.xyz>
2024-04-30 16:30:28 +0100

    draft push

--- typescript/sdk/src/crud/EvmIsmModule.ts
+++ typescript/sdk/src/crud/EvmIsmModule.ts
@@ -1,11 +1,14 @@
 import { Address, ProtocolType, rootLogger } from '@hyperlane-xyz/utils';
 
-import { HyperlaneAddresses } from '../contracts/types.js';
+import { HyperlaneContracts } from '../contracts/types.js';
+import { HyperlaneDeployer } from '../deploy/HyperlaneDeployer.js';
 import { ProxyFactoryFactories } from '../deploy/contracts.js';
+import { EvmIsmCreator } from '../ism/EvmIsmCreator.js';
 import { EvmIsmReader } from '../ism/read.js';
 import { IsmConfig } from '../ism/types.js';
 import { MultiProvider } from '../providers/MultiProvider.js';
 import { EthersV5Transaction } from '../providers/ProviderType.js';
+import { ChainNameOrId } from '../types.js';
 
 import { CrudModule, CrudModuleArgs } from './AbstractCrudModule.js';
 
@@ -13,32 +16,29 @@ import { CrudModule, CrudModuleArgs } from './AbstractCrudModule.js';
 export class EvmIsmModule extends CrudModule<
   ProtocolType.Ethereum,
   IsmConfig,
-  HyperlaneAddresses<ProxyFactoryFactories>
+  HyperlaneContracts<ProxyFactoryFactories> & {
+    deployedIsm: Address;
+  }
 > {
   protected logger = rootLogger.child({ module: 'EvmIsmModule' });
   protected reader: EvmIsmReader;
 
   protected constructor(
     protected readonly multiProvider: MultiProvider,
-    args: Omit<
-      CrudModuleArgs<
-        ProtocolType.Ethereum,
-        IsmConfig,
-        HyperlaneAddresses<ProxyFactoryFactories>
-      >,
-      'provider'
+    args: CrudModuleArgs<
+      IsmConfig,
+      HyperlaneContracts<ProxyFactoryFactories> & {
+        deployedIsm: Address;
+      }
     >,
   ) {
-    super({
-      ...args,
-      provider: multiProvider.getProvider(args.chain),
-    });
+    super(args);
 
     this.reader = new EvmIsmReader(multiProvider, args.chain);
   }
 
-  public async read(address: Address): Promise<IsmConfig> {
-    return await this.reader.deriveIsmConfig(address);
+  public async read(): Promise<IsmConfig> {
+    return await this.reader.deriveIsmConfig(this.args.addresses.deployedIsm);
   }
 
   public async update(_config: IsmConfig): Promise<EthersV5Transaction[]> {
@@ -46,7 +46,32 @@ export class EvmIsmModule extends CrudModule<
   }
 
   // manually write static create function
-  public static create(_config: IsmConfig): Promise<EvmIsmModule> {
-    throw new Error('not implemented');
+  public static async create({
+    chain,
+    config,
+    deployer,
+    factories,
+    multiProvider,
+  }: {
+    chain: ChainNameOrId;
+    config: IsmConfig;
+    deployer: HyperlaneDeployer<any, any>;
+    factories: HyperlaneContracts<ProxyFactoryFactories>;
+    multiProvider: MultiProvider;
+  }): Promise<EvmIsmModule> {
+    const destination = multiProvider.getChainName(chain);
+    const ismCreator = new EvmIsmCreator(deployer, multiProvider, factories);
+    const deployedIsm = await ismCreator.deploy({
+      config,
+      destination,
+    });
+    return new EvmIsmModule(multiProvider, {
+      addresses: {
+        ...factories,
+        deployedIsm: deployedIsm.address,
+      },
+      chain,
+      config,
+    });
   }
 }
