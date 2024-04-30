ea283dc05bbc4ebf161af51a72a7caa6ffcd87f3
J M Rossy <jm.rossy@gmail.com>
2024-04-30 15:50:11 -0400

    Filter infra IGP owners to evm chains

--- typescript/infra/config/environments/test/igp.ts
+++ typescript/infra/config/environments/test/igp.ts
@@ -3,9 +3,18 @@ import {
   ChainName,
   GasOracleContractType,
   IgpConfig,
+  OwnableConfig,
   multisigIsmVerificationCost,
 } from '@hyperlane-xyz/sdk';
-import { Address, exclude, objMap } from '@hyperlane-xyz/utils';
+import {
+  Address,
+  ProtocolType,
+  exclude,
+  objFilter,
+  objMap,
+} from '@hyperlane-xyz/utils';
+
+import { getChain } from '../../registry.js';
 
 import { testChainNames } from './chains.js';
 import { multisigIsm } from './multisigIsm.js';
@@ -20,21 +29,30 @@ function getGasOracles(local: ChainName) {
   );
 }
 
-export const igp: ChainMap<IgpConfig> = objMap(owners, (chain, ownerConfig) => {
-  const overhead = Object.fromEntries(
-    exclude(chain, testChainNames).map((remote) => [
-      remote,
-      multisigIsmVerificationCost(
-        multisigIsm[remote].threshold,
-        multisigIsm[remote].validators.length,
-      ),
-    ]),
-  );
-  return {
-    oracleKey: ownerConfig.owner as Address, // owner can be AccountConfig
-    beneficiary: ownerConfig.owner as Address, // same as above
-    gasOracleType: getGasOracles(chain),
-    overhead,
-    ...ownerConfig,
-  };
-});
+const evmOwners = objFilter(
+  owners,
+  (chain, _): _ is OwnableConfig =>
+    getChain(chain).protocol === ProtocolType.Ethereum,
+);
+
+export const igp: ChainMap<IgpConfig> = objMap(
+  evmOwners,
+  (chain, ownerConfig) => {
+    const overhead = Object.fromEntries(
+      exclude(chain, testChainNames).map((remote) => [
+        remote,
+        multisigIsmVerificationCost(
+          multisigIsm[remote].threshold,
+          multisigIsm[remote].validators.length,
+        ),
+      ]),
+    );
+    return {
+      oracleKey: ownerConfig.owner as Address, // owner can be AccountConfig
+      beneficiary: ownerConfig.owner as Address, // same as above
+      gasOracleType: getGasOracles(chain),
+      overhead,
+      ...ownerConfig,
+    };
+  },
+);
--- typescript/infra/config/environments/testnet4/igp.ts
+++ typescript/infra/config/environments/testnet4/igp.ts
@@ -1,30 +1,48 @@
 import {
   ChainMap,
   IgpConfig,
+  OwnableConfig,
   defaultMultisigConfigs,
   multisigIsmVerificationCost,
 } from '@hyperlane-xyz/sdk';
-import { Address, exclude, objMap } from '@hyperlane-xyz/utils';
+import {
+  Address,
+  ProtocolType,
+  exclude,
+  objFilter,
+  objMap,
+} from '@hyperlane-xyz/utils';
+
+import { getChain } from '../../registry.js';
 
 import { storageGasOracleConfig } from './gas-oracle.js';
 import { owners } from './owners.js';
 import { supportedChainNames } from './supportedChainNames.js';
 
-export const igp: ChainMap<IgpConfig> = objMap(owners, (chain, ownerConfig) => {
-  return {
-    ...ownerConfig,
-    oracleKey: ownerConfig.owner as Address,
-    beneficiary: ownerConfig.owner as Address,
-    oracleConfig: storageGasOracleConfig[chain],
-    overhead: Object.fromEntries(
-      exclude(chain, supportedChainNames).map((remote) => [
-        remote,
-        multisigIsmVerificationCost(
-          // TODO: parameterize this
-          defaultMultisigConfigs[remote].threshold,
-          defaultMultisigConfigs[remote].validators.length,
-        ),
-      ]),
-    ),
-  };
-});
+const evmOwners = objFilter(
+  owners,
+  (chain, _): _ is OwnableConfig =>
+    getChain(chain).protocol === ProtocolType.Ethereum,
+);
+
+export const igp: ChainMap<IgpConfig> = objMap(
+  evmOwners,
+  (chain, ownerConfig) => {
+    return {
+      ...ownerConfig,
+      oracleKey: ownerConfig.owner as Address,
+      beneficiary: ownerConfig.owner as Address,
+      oracleConfig: storageGasOracleConfig[chain],
+      overhead: Object.fromEntries(
+        exclude(chain, supportedChainNames).map((remote) => [
+          remote,
+          multisigIsmVerificationCost(
+            // TODO: parameterize this
+            defaultMultisigConfigs[remote].threshold,
+            defaultMultisigConfigs[remote].validators.length,
+          ),
+        ]),
+      ),
+    };
+  },
+);
