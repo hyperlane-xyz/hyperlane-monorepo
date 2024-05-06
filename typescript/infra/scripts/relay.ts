3c1f4a1f470cf00a4e965248278ba14791446f22
Yorke Rhodes <yorke@hyperlane.xyz>
2024-05-06 18:57:21 -0400

    Add caching to infra relay script

--- typescript/infra/scripts/relay.ts
+++ typescript/infra/scripts/relay.ts
@@ -1,13 +1,40 @@
 import { HyperlaneRelayer } from '@hyperlane-xyz/sdk';
 
+import { readFile, writeFile } from 'fs/promises';
+
 import { getArgs } from './agent-utils.js';
 import { getHyperlaneCore } from './core-utils.js';
+
+const CACHE_PATH = process.env.RELAYER_CACHE ?? './relayer-cache.json';
 
 async function main() {
   const { environment } = await getArgs().argv;
   const { core } = await getHyperlaneCore(environment);
   const relayer = new HyperlaneRelayer(core);
-  await relayer.relay();
+
+  const chains = ['optimism', 'arbitrum', 'polygon', 'celo', 'base'];
+
+  try {
+    const contents = await readFile(CACHE_PATH, 'utf-8');
+    const cache = JSON.parse(contents);
+    console.log(`Relayer cache loaded from ${CACHE_PATH}`);
+
+    await relayer.hydrate(cache);
+  } catch (e) {
+    console.error(`Failed to load cache from ${CACHE_PATH}`);
+  }
+
+  relayer.start(chains);
+
+  process.once('SIGINT', async () => {
+    relayer.stop(chains);
+
+    const cache = JSON.stringify(relayer.cache);
+    await writeFile(CACHE_PATH, cache, 'utf-8');
+    console.log(`Relayer cache saved to ${CACHE_PATH}`);
+
+    process.exit(0);
+  });
 }
 
 main();
