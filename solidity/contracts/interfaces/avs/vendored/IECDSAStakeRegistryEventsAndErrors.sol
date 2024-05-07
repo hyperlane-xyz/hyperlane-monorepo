e11ed99cf7b8f8117ac437e92e4182bc2d8b6435
-f <kunalarora1729@gmail.com>
2024-05-07 21:23:46 +0530

    update spelling

--- solidity/contracts/interfaces/avs/vendored/IECDSAStakeRegistryEventsAndErrors.sol
+++ solidity/contracts/interfaces/avs/vendored/IECDSAStakeRegistryEventsAndErrors.sol
@@ -95,6 +95,6 @@ interface IECDSAStakeRegistryEventsAndErrors {
     /// @notice Thrown when registering an already registered operator
     error OperatorAlreadyRegistered();
 
-    /// @notice Thrown when de-registering or updating the stake for an unregisted operator
+    /// @notice Thrown when de-registering or updating the stake for an unregistered operator
     error OperatorNotRegistered();
 }
