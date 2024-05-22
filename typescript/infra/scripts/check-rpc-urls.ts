6263565ade935a52b29d3c06a7693383b5d44ab0
Trevor Porter <tkporter4@gmail.com>
2024-05-22 12:22:07 +0100

    Fix check-rpc-urls.ts

--- typescript/infra/scripts/check-rpc-urls.ts
+++ typescript/infra/scripts/check-rpc-urls.ts
@@ -2,13 +2,16 @@ import { ethers } from 'ethers';
 
 import { rootLogger } from '@hyperlane-xyz/utils';
 
-import { getSecretRpcEndpoint } from '../../src/agents/index.js';
-import { getArgs } from '../agent-utils.js';
-import { getEnvironmentConfig } from '../core-utils.js';
+import { getSecretRpcEndpoint } from '../src/agents/index.js';
 
+import { getArgs } from './agent-utils.js';
+import { getEnvironmentConfig } from './core-utils.js';
+
+// TODO remove this script as part of migration to CLI
+// It's redundant with metadata-check.ts in the SDK
 async function main() {
   const { environment } = await getArgs().argv;
-  const config = getEnvironmentConfig(environment);
+  const config = await getEnvironmentConfig(environment);
   const multiProvider = await config.getMultiProvider();
   const chains = multiProvider.getKnownChainNames();
   const providers: [string, ethers.providers.JsonRpcProvider][] = [];
