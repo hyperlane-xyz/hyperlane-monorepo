7ec776a84d1700bc61999332a4bc7b0bedb7b42c
0xevolve <Artevolve@yahoo.com>
2024-05-23 17:01:14 +0100

    feat: merkle tree hook

--- rust/chains/hyperlane-starknet/src/lib.rs
+++ rust/chains/hyperlane-starknet/src/lib.rs
@@ -8,12 +8,12 @@ pub use error::*;
 pub use indexer::*;
 pub use ism::*;
 pub use mailbox::*;
+pub use merkle_tree_hook::*;
 pub use provider::*;
 pub use signers::*;
 pub use trait_builder::*;
 pub use utils::*;
 pub use validator_announce::*;
-mod ism;
 
 /// Generated contract bindings.
 #[cfg(not(doctest))]
@@ -21,7 +21,9 @@ mod contracts;
 
 mod error;
 mod indexer;
+mod ism;
 mod mailbox;
+mod merkle_tree_hook;
 mod provider;
 mod signers;
 mod trait_builder;
