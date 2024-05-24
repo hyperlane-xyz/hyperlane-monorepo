a1417865516f014c66bccea724bf71c9b19e5324
Le Yu <leyu00@gmail.com>
2024-05-24 15:01:51 -0400

    Clean up deploy.hardhat-test.ts

--- typescript/sdk/src/token/deploy.hardhat-test.ts
+++ typescript/sdk/src/token/deploy.hardhat-test.ts
@@ -1,96 +1,56 @@
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers.js';
-import { expect } from 'chai';
 import hre from 'hardhat';
 
-import {
-  ERC20Test,
-  ERC20Test__factory,
-  Mailbox,
-  Mailbox__factory,
-} from '@hyperlane-xyz/core';
-import {
-  HyperlaneContractsMap,
-  RouterConfig,
-  TestChainName,
-} from '@hyperlane-xyz/sdk';
+import { RouterConfig } from '@hyperlane-xyz/sdk';
 import { objMap } from '@hyperlane-xyz/utils';
 
 import { TestCoreApp } from '../core/TestCoreApp.js';
 import { TestCoreDeployer } from '../core/TestCoreDeployer.js';
 import { HyperlaneProxyFactoryDeployer } from '../deploy/HyperlaneProxyFactoryDeployer.js';
-import { ProxyFactoryFactories } from '../deploy/contracts.js';
 import { HyperlaneIsmFactory } from '../ism/HyperlaneIsmFactory.js';
 import { MultiProvider } from '../providers/MultiProvider.js';
 import { ChainMap } from '../types.js';
 
-import { EvmERC20WarpModule } from './EvmERC20WarpModule.js';
-import { DerivedTokenRouterConfig } from './EvmERC20WarpRouteReader.js';
 import { HypERC20Config, TokenConfig, TokenType } from './config.js';
 import { HypERC20Deployer } from './deploy.js';
 
-describe('TokenDeployer', async () => {
-  const TOKEN_NAME = 'fake';
-  const TOKEN_SUPPLY = '100000000000000000000';
-  const TOKEN_DECIMALS = 18;
-  const GAS = 65_000;
-  const chain = TestChainName.test4;
-  let mailbox: Mailbox;
-  let ismFactory: HyperlaneIsmFactory;
-  let factories: HyperlaneContractsMap<ProxyFactoryFactories>;
-  let erc20Factory: ERC20Test__factory;
-  let token: ERC20Test;
+describe.only('TokenDeployer', async () => {
   let signer: SignerWithAddress;
   let deployer: HypERC20Deployer;
   let multiProvider: MultiProvider;
   let coreApp: TestCoreApp;
   let routerConfigMap: ChainMap<RouterConfig>;
-  let config: DerivedTokenRouterConfig;
-  let baseConfig: RouterConfig;
+  let config: ChainMap<TokenConfig & RouterConfig>;
 
   before(async () => {
     [signer] = await hre.ethers.getSigners();
     multiProvider = MultiProvider.createTestMultiProvider({ signer });
     const ismFactoryDeployer = new HyperlaneProxyFactoryDeployer(multiProvider);
-    factories = await ismFactoryDeployer.deploy(
+    const factories = await ismFactoryDeployer.deploy(
       multiProvider.mapKnownChains(() => ({})),
     );
-    ismFactory = new HyperlaneIsmFactory(factories, multiProvider);
+    const ismFactory = new HyperlaneIsmFactory(factories, multiProvider);
     coreApp = await new TestCoreDeployer(multiProvider, ismFactory).deployApp();
     routerConfigMap = coreApp.getRouterConfig(signer.address);
-
-    erc20Factory = new ERC20Test__factory(signer);
-    token = await erc20Factory.deploy(
-      TOKEN_NAME,
-      TOKEN_NAME,
-      TOKEN_SUPPLY,
-      TOKEN_DECIMALS,
-    );
-
-    baseConfig = routerConfigMap[chain];
-    deployer = new HypERC20Deployer(multiProvider);
-
-    mailbox = Mailbox__factory.connect(baseConfig.mailbox, signer);
-    config = {
-      type: TokenType.collateral,
-      token: token.address,
-      hook: await mailbox.defaultHook(),
-      ...baseConfig,
-    } as DerivedTokenRouterConfig;
-  });
-
-  it('deploys', async () => {
-    const config = objMap(
+    config = objMap(
       routerConfigMap,
       (chain, c): HypERC20Config => ({
         type: TokenType.synthetic,
         name: chain,
         symbol: `u${chain}`,
-        decimals: TOKEN_DECIMALS,
-        totalSupply: TOKEN_SUPPLY,
-        gas: GAS,
+        decimals: 18,
+        totalSupply: 100_000,
+        gas: 65_000,
         ...c,
       }),
     );
-    await deployer.deploy(config as ChainMap<TokenConfig & RouterConfig>);
+  });
+
+  beforeEach(async () => {
+    deployer = new HypERC20Deployer(multiProvider);
+  });
+
+  it('deploys', async () => {
+    await deployer.deploy(config);
   });
 });
