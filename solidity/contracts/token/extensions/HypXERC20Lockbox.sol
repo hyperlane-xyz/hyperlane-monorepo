0403e56ca03848629bf4bc1b50b779b93b1deef6
Yorke Rhodes <yorke@hyperlane.xyz>
2024-05-24 13:29:39 -0400

    Burn xERC20 rather than custodying

--- solidity/contracts/token/extensions/HypXERC20Lockbox.sol
+++ solidity/contracts/token/extensions/HypXERC20Lockbox.sol
@@ -2,31 +2,38 @@
 pragma solidity >=0.8.0;
 
 import {IXERC20Lockbox} from "../interfaces/IXERC20Lockbox.sol";
+import {IXERC20} from "../interfaces/IXERC20.sol";
 import {HypERC20Collateral} from "../HypERC20Collateral.sol";
 
 contract HypXERC20Lockbox is HypERC20Collateral {
     IXERC20Lockbox public immutable lockbox;
+    IXERC20 public immutable xERC20;
 
     constructor(
-        address _lockbox,
+        IXERC20Lockbox _lockbox,
         address _mailbox
-    ) HypERC20Collateral(address(IXERC20Lockbox(_lockbox).ERC20()), _mailbox) {
-        lockbox = IXERC20Lockbox(_lockbox);
+    ) HypERC20Collateral(address(_lockbox.ERC20()), _mailbox) {
+        lockbox = _lockbox;
+        xERC20 = _lockbox.XERC20();
     }
 
     function _transferFromSender(
         uint256 _amount
     ) internal override returns (bytes memory) {
+        // transfer erc20 from sender
         super._transferFromSender(_amount);
-        lockbox.deposit(_amount);
+        // convert erc20 to xERC20
+        lockbox.depositTo(address(this), _amount);
+        // burn xERC20
+        xERC20.burn(address(this), _amount);
         return bytes("");
     }
 
     function _transferTo(
         address _recipient,
-        uint256 _amountOrId,
+        uint256 _amount,
         bytes calldata /*metadata*/
     ) internal override {
-        lockbox.withdrawTo(_recipient, _amountOrId);
+        lockbox.withdrawTo(_recipient, _amount);
     }
 }
