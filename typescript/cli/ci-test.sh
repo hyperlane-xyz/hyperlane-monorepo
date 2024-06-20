ebef0e51a891ba47e8beba26804f68ce39e488aa
Paul Balaji <10051819+paulbalaji@users.noreply.github.com>
2024-06-20 16:03:05 +0100

    CR dan
    Signed-off-by: Paul Balaji <10051819+paulbalaji@users.noreply.github.com>

--- typescript/cli/ci-test.sh
+++ typescript/cli/ci-test.sh
@@ -254,7 +254,7 @@ run_hyperlane_send_message() {
 
 run_validator() {
     echo -e "\nPre-building validator with cargo"
-    cargo build --bin validator
+    cargo build --bin validator --features test-utils
 
     # set some default agent env vars, used by both validators and relayer
     export HYP_CHAINS_${CHAIN1_CAPS}_BLOCKS_REORGPERIOD=0
@@ -308,7 +308,7 @@ run_validator() {
 
 run_relayer() {
     echo -e "\nPre-building relayer with cargo"
-    cargo build --bin relayer
+    cargo build --bin relayer --features test-utils
 
     echo "Running relayer"
     export CONFIG_FILES=/tmp/agent-config.json
