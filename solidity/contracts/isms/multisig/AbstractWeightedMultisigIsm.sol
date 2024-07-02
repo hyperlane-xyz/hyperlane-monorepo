8738d82d21d36d23df79367d4b7a56266b7a2427
-f <kunalarora1729@gmail.com>
2024-07-02 23:15:03 +0530

    concrete contracts

--- solidity/contracts/isms/multisig/AbstractWeightedMultisigIsm.sol
+++ solidity/contracts/isms/multisig/AbstractWeightedMultisigIsm.sol
@@ -3,12 +3,15 @@ pragma solidity >=0.8.0;
 
 // ============ External Imports ============
 import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 
 // ============ Internal Imports ============
 import {IInterchainSecurityModule} from "../../interfaces/IInterchainSecurityModule.sol";
 import {IWeightedMultisigIsm} from "../../interfaces/isms/IWeightedMultisigIsm.sol";
 import {Message} from "../../libs/Message.sol";
+
 import {MerkleLib} from "../../libs/Merkle.sol";
+import {AbstractMultisig} from "./AbstractMultisigIsm.sol";
 
 /**
  * @title WeightedMultisigIsm
@@ -18,7 +21,11 @@ import {MerkleLib} from "../../libs/Merkle.sol";
  * for concrete implementations of `digest` and `signatureAt`.
  * @dev See ./StaticWeightedIsm.sol for concrete implementations.
  */
-abstract contract AbstractWeightedMultisigIsm is IWeightedMultisigIsm {
+abstract contract AbstractWeightedMultisigIsm is
+    AbstractMultisig,
+    IWeightedMultisigIsm,
+    OwnableUpgradeable
+{
     // ============ Constants ============
     uint96 public constant BASIS_POINTS = 10000;
 
@@ -33,16 +40,33 @@ abstract contract AbstractWeightedMultisigIsm is IWeightedMultisigIsm {
     );
 
     /**
+     * @param _owner The owner of the contract.
+     */
+    function initialize(address _owner) public initializer {
+        __Ownable_init();
+        _transferOwnership(_owner);
+    }
+
+    function initialize(
+        address _owner,
+        ValidatorInfo[] calldata _validators,
+        uint96 _thresholdWeight
+    ) public initializer {
+        __Ownable_init();
+        _updateValidatorSet(_validators, _thresholdWeight);
+        _transferOwnership(_owner);
+    }
+
+    /**
      * @notice Returns the set of validators responsible for verifying _message
      * and the number of signatures required
      * @dev Can change based on the content of _message
      * @dev Signatures provided to `verify` must be consistent with validator ordering
-     * @param _message Hyperlane formatted interchain message
      * @return validators The array of validator addresses
      * @return threshold The number of validator signatures needed
      */
     function validatorsAndThresholdWeight(
-        bytes calldata _message
+        bytes calldata /* _message*/
     ) public view virtual returns (ValidatorInfo[] memory, uint96) {
         return (validators, thresholdWeight);
     }
@@ -50,60 +74,9 @@ abstract contract AbstractWeightedMultisigIsm is IWeightedMultisigIsm {
     function updateValidatorSet(
         ValidatorInfo[] calldata _newValidators,
         uint96 _newThresholdWeight
-    ) external {
-        require(_newValidators.length > 0, "Validator set cannot be empty");
-        require(
-            _newThresholdWeight > 0 && _newThresholdWeight <= BASIS_POINTS,
-            "Invalid threshold weight"
-        );
-
-        uint96 totalWeight = 0;
-        for (uint256 i = 0; i < _newValidators.length; i++) {
-            require(
-                _newValidators[i].signingKey != address(0),
-                "Invalid validator address"
-            );
-            require(
-                _newValidators[i].weight > 0,
-                "Validator weight must be positive"
-            );
-            totalWeight += _newValidators[i].weight;
-        }
-        require(
-            totalWeight == BASIS_POINTS,
-            "Total weight must equal BASIS_POINTS"
-        );
-
-        delete validators;
-        for (uint256 i = 0; i < _newValidators.length; i++) {
-            validators.push(_newValidators[i]);
-        }
-        thresholdWeight = _newThresholdWeight;
-
-        emit ValidatorSetUpdated(_newValidators, _newThresholdWeight);
+    ) external onlyOwner {
+        _updateValidatorSet(_newValidators, _newThresholdWeight);
     }
-
-    /**
-     * @notice Returns the digest to be used for signature verification.
-     * @param _metadata ABI encoded module metadata
-     * @param _message Formatted Hyperlane message (see Message.sol).
-     * @return digest The digest to be signed by validators
-     */
-    function digest(
-        bytes calldata _metadata,
-        bytes calldata _message
-    ) internal view virtual returns (bytes32);
-
-    /**
-     * @notice Returns the signature at a given index from the metadata.
-     * @param _metadata ABI encoded module metadata
-     * @param _index The index of the signature to return
-     * @return signature Packed encoding of signature (65 bytes)
-     */
-    function signatureAt(
-        bytes calldata _metadata,
-        uint256 _index
-    ) internal pure virtual returns (bytes calldata);
 
     // ============ Public Functions ============
 
@@ -118,7 +91,7 @@ abstract contract AbstractWeightedMultisigIsm is IWeightedMultisigIsm {
     function verify(
         bytes calldata _metadata,
         bytes calldata _message
-    ) public view returns (bool) {
+    ) public view virtual returns (bool) {
         bytes32 _digest = digest(_metadata, _message);
         (
             ValidatorInfo[] memory _validators,
@@ -159,5 +132,41 @@ abstract contract AbstractWeightedMultisigIsm is IWeightedMultisigIsm {
             "Insufficient validator weight"
         );
         return true;
+    }
+
+    function _updateValidatorSet(
+        ValidatorInfo[] calldata _newValidators,
+        uint96 _newThresholdWeight
+    ) internal {
+        require(_newValidators.length > 0, "Validator set cannot be empty");
+        require(
+            _newThresholdWeight > 0 && _newThresholdWeight <= BASIS_POINTS,
+            "Invalid threshold weight"
+        );
+
+        uint96 totalWeight = 0;
+        for (uint256 i = 0; i < _newValidators.length; i++) {
+            require(
+                _newValidators[i].signingKey != address(0),
+                "Invalid validator address"
+            );
+            require(
+                _newValidators[i].weight > 0,
+                "Validator weight must be positive"
+            );
+            totalWeight += _newValidators[i].weight;
+        }
+        require(
+            totalWeight == BASIS_POINTS,
+            "Total weight must equal BASIS_POINTS"
+        );
+
+        delete validators;
+        for (uint256 i = 0; i < _newValidators.length; i++) {
+            validators.push(_newValidators[i]);
+        }
+        thresholdWeight = _newThresholdWeight;
+
+        emit ValidatorSetUpdated(_newValidators, _newThresholdWeight);
     }
 }
---
+++ solidity/contracts/isms/multisig/WeightedMultisigIsm.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT OR Apache-2.0
+pragma solidity >=0.8.0;
+
+import {IInterchainSecurityModule} from "../../interfaces/IInterchainSecurityModule.sol";
+import {AbstractMerkleRootMultisigIsm} from "./AbstractMerkleRootMultisigIsm.sol";
+import {AbstractMessageIdMultisigIsm} from "./AbstractMessageIdMultisigIsm.sol";
+import {AbstractWeightedMultisigIsm} from "./AbstractWeightedMultisigIsm.sol";
+import {AbstractMultisigIsm} from "./AbstractMultisigIsm.sol";
+
+// solhint-disable no-empty-blocks
+
+/**
+ * @title StaticMerkleRootMultisigIsm
+ * @notice Manages per-domain m-of-n validator set that is used
+ * to verify interchain messages using a merkle root signature quorum
+ * and merkle proof of inclusion.
+ */
+contract MerkleRootWeightedMultisigIsm is
+    AbstractWeightedMultisigIsm,
+    AbstractMerkleRootMultisigIsm
+{
+    uint8 public constant moduleType =
+        uint8(IInterchainSecurityModule.Types.WEIGHT_MERKLE_ROOT_MULTISIG);
+}
+
+/**
+ * @title StaticMessageIdMultisigIsm
+ * @notice Manages per-domain m-of-n validator set that is used
+ * to verify interchain messages using a message ID signature quorum.
+ */
+contract MessageIdWeightedMultisigIsm is
+    AbstractMessageIdMultisigIsm,
+    AbstractWeightedMultisigIsm
+{
+    uint8 public constant moduleType =
+        uint8(IInterchainSecurityModule.Types.WEIGHT_MESSAGE_ID_MULTISIG);
+}
