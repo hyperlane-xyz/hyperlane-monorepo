471cce46c23faa39d528c9e53be27a6e8e92b41b
Daniel Savu <23065004+daniel-savu@users.noreply.github.com>
2024-07-24 14:24:16 +0100

    fix: e2e

--- rust/utils/run-locally/src/main.rs
+++ rust/utils/run-locally/src/main.rs
@@ -85,7 +85,7 @@ const SEALEVEL_VALIDATOR_KEYS: &[&str] = &[
     "0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d",
 ];
 
-const AGENT_BIN_PATH: &str = "target/debug";
+const AGENT_BIN_PATH: &str = "target/memory-profiling";
 const INFRA_PATH: &str = "../typescript/infra";
 const MONOREPO_ROOT_PATH: &str = "../";
 
