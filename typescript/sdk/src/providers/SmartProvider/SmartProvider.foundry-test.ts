a62a6ae787b18dc95116108af35bc0559be7fa5e
Le Yu <6251863+ltyu@users.noreply.github.com>
2024-07-25 14:26:56 -0400

    Test CI issue without unit tests

--- typescript/sdk/src/providers/SmartProvider/SmartProvider.foundry-test.ts
+++ typescript/sdk/src/providers/SmartProvider/SmartProvider.foundry-test.ts
@@ -1,12 +1,9 @@
 import { expect } from 'chai';
-import { errors as EthersError, Wallet, constants } from 'ethers';
+import { Wallet, constants } from 'ethers';
 
 import { ERC20__factory } from '@hyperlane-xyz/core';
 
-import {
-  HyperlaneSmartProvider,
-  getSmartProviderErrorMessage,
-} from './SmartProvider.js';
+import { HyperlaneSmartProvider } from './SmartProvider.js';
 
 const PK = '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80';
 const NETWORK = 31337;
@@ -85,65 +82,68 @@ describe('SmartProvider', async () => {
     expect(Array.isArray(logs)).to.be.true;
   });
 
-  it('throws with invalid RPC', async () => {
-    const INVALID_URL = 'http://1337.1337.1337.1:8545';
-    const smartProvider = HyperlaneSmartProvider.fromRpcUrl(
-      NETWORK,
-      INVALID_URL,
-      {
-        maxRetries: 3,
-      },
-    );
-    const signer = new Wallet(PK, smartProvider);
+  // it('throws with invalid RPC', async () => {
+  //   const INVALID_URL = 'http://1337.1337.1337.1:8545';
+  //   const NETWORK = 11337;
+  //   const smartProvider = HyperlaneSmartProvider.fromRpcUrl(
+  //     NETWORK,
+  //     INVALID_URL,
+  //     {
+  //       maxRetries: 3,
+  //     },
+  //   );
+  //   const signer = new Wallet(PK, smartProvider);
 
-    try {
-      const factory = new ERC20__factory(signer);
-      await factory.deploy('fake', 'FAKE');
-    } catch (e: any) {
-      expect(e.message).to.equal(
-        getSmartProviderErrorMessage(EthersError.SERVER_ERROR),
-      );
-    }
-  });
+  //   try {
+  //     const factory = new ERC20__factory(signer);
+  //     await factory.deploy('fake', 'FAKE');
+  //   } catch (e: any) {
+  //     expect(e.message).to.equal(
+  //       getSmartProviderErrorMessage(EthersError.SERVER_ERROR),
+  //     );
+  //   }
+  // });
 
-  it('throws with multiple invalid RPCs', async () => {
-    const INVALID_URL_1 = 'http://1337.1337.1337.1:8545';
-    const INVALID_URL_2 = 'http://1338.1338.1338.1:8545';
-    const smartProvider = new HyperlaneSmartProvider(
-      NETWORK,
-      [{ http: INVALID_URL_1 }, { http: INVALID_URL_2 }],
-      [],
-      {
-        maxRetries: 3,
-      },
-    );
-    const signer = new Wallet(PK, smartProvider);
+  // it('throws with multiple invalid RPCs', async () => {
+  //   const INVALID_URL_1 = 'http://1337.1337.1337.1:8545';
+  //   const INVALID_URL_2 = 'http://1338.1338.1338.1:8545';
+  //   const NETWORK = 11337;
+  //   const smartProvider = new HyperlaneSmartProvider(
+  //     NETWORK,
+  //     [{ http: INVALID_URL_1 }, { http: INVALID_URL_2 }],
+  //     [],
+  //     {
+  //       maxRetries: 3,
+  //     },
+  //   );
+  //   const signer = new Wallet(PK, smartProvider);
 
-    try {
-      const factory = new ERC20__factory(signer);
-      await factory.deploy('fake', 'FAKE');
-    } catch (e: any) {
-      expect(e.message).to.equal(
-        getSmartProviderErrorMessage(EthersError.SERVER_ERROR),
-      );
-    }
-  });
+  //   try {
+  //     const factory = new ERC20__factory(signer);
+  //     await factory.deploy('fake', 'FAKE');
+  //   } catch (e: any) {
+  //     expect(e.message).to.equal(
+  //       getSmartProviderErrorMessage(EthersError.SERVER_ERROR),
+  //     );
+  //   }
+  // });
 
-  it('handles invalid and valid RPCs', async () => {
-    const INVALID_URL = 'http://1337.1337.1337.1:8545';
-    const smartProvider = new HyperlaneSmartProvider(
-      NETWORK,
-      [{ http: INVALID_URL }, { http: URL }],
-      [],
-      {
-        maxRetries: 3,
-      },
-    );
-    const signer = new Wallet(PK, smartProvider);
+  // it('handles invalid and valid RPCs', async () => {
+  //   const INVALID_URL = 'http://1337.1337.1337.1:8545';
+  //   const NETWORK = 11337;
+  //   const smartProvider = new HyperlaneSmartProvider(
+  //     NETWORK,
+  //     [{ http: INVALID_URL }, { http: URL }],
+  //     [],
+  //     {
+  //       maxRetries: 3,
+  //     },
+  //   );
+  //   const signer = new Wallet(PK, smartProvider);
 
-    const factory = new ERC20__factory(signer);
-    const erc20 = await factory.deploy('fake', 'FAKE');
+  //   const factory = new ERC20__factory(signer);
+  //   const erc20 = await factory.deploy('fake', 'FAKE');
 
-    expect(erc20.address).to.not.be.empty;
-  });
+  //   expect(erc20.address).to.not.be.empty;
+  // });
 });
