f41a2957517311707aa00e33b907f311d9c30834
Yorke Rhodes <yorke@hyperlane.xyz>
2024-08-01 16:21:27 -0400

    Implement transfer and call prototype

---
+++ solidity/contracts/middleware/TransferAndCallRouter.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.13;
+
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {InterchainAccountRouter, CallLib} from "./InterchainAccountRouter.sol";
+import {TokenRouter} from "../token/libs/TokenRouter.sol";
+import {TypeCasts} from "../libs/TypeCasts.sol";
+
+contract TransferAndCall {
+    using TypeCasts for address;
+
+    InterchainAccountRouter public immutable interchainAccountRouter;
+
+    constructor(InterchainAccountRouter _interchainAccountRouter) {
+        interchainAccountRouter = _interchainAccountRouter;
+    }
+
+    function transferAndCall(
+        IERC20 asset, // not derivable from TokenRouter
+        TokenRouter warpRoute,
+        uint32 destination,
+        uint256 amount,
+        CallLib.Call[] calldata calls
+    ) public {
+        asset.transferFrom(msg.sender, address(this), amount);
+        address recipient = interchainAccountRouter.getRemoteInterchainAccount(
+            destination,
+            msg.sender
+        );
+        warpRoute.transferRemote(
+            destination,
+            recipient.addressToBytes32(),
+            amount
+        );
+        interchainAccountRouter.callRemote(destination, calls);
+    }
+}
