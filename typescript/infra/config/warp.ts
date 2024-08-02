d3534915ae8288dc2c2369d0a565e1f4c1e43122
Trevor Porter <tkporter4@gmail.com>
2024-08-02 13:22:22 +0100

    undo some changes

--- typescript/infra/config/warp.ts
+++ typescript/infra/config/warp.ts
@@ -22,56 +22,52 @@ export async function getWarpConfig(
   envConfig: EnvironmentConfig,
 ): Promise<ChainMap<TokenRouterConfig>> {
   const { core } = await getHyperlaneCore(envConfig.environment, multiProvider);
+  const ismFactory = HyperlaneIsmFactory.fromAddressesMap(
+    getAddresses(envConfig.environment, Modules.PROXY_FACTORY),
+    multiProvider,
+  );
 
-  const deployer = '0xfaD1C94469700833717Fa8a3017278BC1cA8031C';
-  const safe = '0xA9B8afF7d18E15b1613C28f110fD0dEeE06Da509';
-  // owned by 0xfad
-  // const alfajoresIca = '0xb8c49ef544c43d3842d693a61fc99911f22b1453';
-  // owned by multisig
-  const alfajoresIca = '0x60063ce2466dc3c55f379328c0d1cef3d41f4ed8';
+  const owner = DEPLOYER;
+
+  // "Manually" deploying an ISM because the warp deployer doesn't support
+  // ISM objects at the moment, and the deploy involves strictly recoverable ISMs.
+  const ism = await ismFactory.deploy({
+    destination: 'ethereum',
+    config: buildAggregationIsmConfigs(
+      'ethereum',
+      ['ancient8'],
+      defaultMultisigConfigs,
+    ).ancient8,
+  });
 
   const routerConfig = core.getRouterConfig(envConfig.owners);
 
-  const sepolia: TokenRouterConfig = {
-    ...routerConfig.sepolia,
-    type: TokenType.native,
-    owner: safe, // owner,
+  const ethereum: TokenRouterConfig = {
+    ...routerConfig.ethereum,
+    type: TokenType.collateral,
+    token: tokens.ethereum.USDC,
+    interchainSecurityModule: ism.address,
+    // This hook was recovered from running the deploy script
+    // for the hook module. The hook configuration is the Ethereum
+    // default hook for the Ancient8 remote (no routing).
+    hook: '0x19b2cF952b70b217c90FC408714Fbc1acD29A6A8',
+    owner,
   };
 
-  const alfajores: TokenRouterConfig = {
-    ...routerConfig.alfajores,
+  // @ts-ignore - The types as they stand require a synthetic to specify
+  // TokenMetadata, but in practice these are actually inferred from a
+  // collateral config. To avoid needing to specify the TokenMetadata, just
+  // ts-ignore for synthetic tokens.
+  const ancient8: TokenRouterConfig = {
+    ...routerConfig.ancient8,
     type: TokenType.synthetic,
-    name: 'Test ETH',
-    symbol: 'tETH',
-    owner: deployer, //alfajoresIca, // deployer, // alfajoresIca,
+    // Uses the default ISM
+    interchainSecurityModule: ethers.constants.AddressZero,
+    owner,
   };
-
-  // const ethereum: TokenRouterConfig = {
-  //   ...routerConfig.ethereum,
-  //   type: TokenType.collateral,
-  //   token: tokens.ethereum.USDC,
-  //   interchainSecurityModule: ism.address,
-  //   // This hook was recovered from running the deploy script
-  //   // for the hook module. The hook configuration is the Ethereum
-  //   // default hook for the Ancient8 remote (no routing).
-  //   hook: '0x19b2cF952b70b217c90FC408714Fbc1acD29A6A8',
-  //   owner,
-  // };
-
-  // // @ts-ignore - The types as they stand require a synthetic to specify
-  // // TokenMetadata, but in practice these are actually inferred from a
-  // // collateral config. To avoid needing to specify the TokenMetadata, just
-  // // ts-ignore for synthetic tokens.
-  // const ancient8: TokenRouterConfig = {
-  //   ...routerConfig.ancient8,
-  //   type: TokenType.synthetic,
-  //   // Uses the default ISM
-  //   interchainSecurityModule: ethers.constants.AddressZero,
-  //   owner,
-  // };
 
   return {
-    sepolia,
-    alfajores,
+    ethereum,
+    ancient8,
   };
 }
