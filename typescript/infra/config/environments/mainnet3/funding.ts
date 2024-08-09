d787e8bfb3aad10e8aa71206ce71461e4a88367e
pbio <10051819+paulbalaji@users.noreply.github.com>
2024-08-09 14:52:52 +0100

    disable kf for merlin temporarily

--- typescript/infra/config/environments/mainnet3/funding.ts
+++ typescript/infra/config/environments/mainnet3/funding.ts
@@ -10,7 +10,7 @@ export const keyFunderConfig: KeyFunderConfig<
 > = {
   docker: {
     repo: 'gcr.io/abacus-labs-dev/hyperlane-monorepo',
-    tag: '2bc110e-20240808-145949',
+    tag: '71eb553-20240809-135015',
   },
   // We're currently using the same deployer/key funder key as mainnet2.
   // To minimize nonce clobbering we offset the key funder cron
@@ -49,7 +49,7 @@ export const keyFunderConfig: KeyFunderConfig<
     lukso: '20',
     mantapacific: '0.2',
     mantle: '20',
-    merlin: '0.002',
+    merlin: '0',
     metis: '10',
     mint: '0.1',
     mode: '0.2',
