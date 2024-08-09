ddd2b7e7fec4fd4a7e0841a066840342da015c47
Danil Nemirovsky <4614623+ameten@users.noreply.github.com>
2024-08-09 09:36:58 +0100

    Replace fn new with a macro

--- rust/hyperlane-base/src/agent/metadata.rs
+++ rust/hyperlane-base/src/agent/metadata.rs
@@ -1,16 +1,10 @@
+use derive_new::new;
 use serde::{Deserialize, Serialize};
 
 /// Metadata about agent
-#[derive(Debug, Deserialize, Serialize)]
+#[derive(Debug, Deserialize, Serialize, new)]
 pub struct AgentMetadata {
     /// Latest git commit hash at the time when agent was built
     /// If .git was not present at the time of build, field defaults to "VERGEN_IDEMPOTENT_OUTPUT"
     pub git_sha: String,
-}
-
-impl AgentMetadata {
-    /// Creates new instance
-    pub fn new(git_sha: String) -> Self {
-        Self { git_sha }
-    }
 }
