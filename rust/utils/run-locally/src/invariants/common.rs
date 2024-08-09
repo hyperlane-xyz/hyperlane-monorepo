d076e42ce4d1e05882912af5fd0211d83c16e5e7
Danil Nemirovsky <4614623+ameten@users.noreply.github.com>
2024-08-09 10:36:48 +0100

    Move termination invariant checks into separate module

---
+++ rust/utils/run-locally/src/invariants/common.rs
@@ -0,0 +1,3 @@
+// This number should be even, so the messages can be split into two equal halves
+// sent before and after the relayer spins up, to avoid rounding errors.
+pub const SOL_MESSAGES_EXPECTED: u32 = 20;
