8be75b65690719fd29601cb8a3500c88befb9e41
Paul Balaji <10051819+paulbalaji@users.noreply.github.com>
2024-08-13 14:48:27 +0100

    feat: core deploy to new chains (#4281)
    Deploy to new chains:
    1. cyber
    1. degenchain
    1. kroma
    1. lisk
    1. lukso
    1. merlin
    1. metis
    1. mint
    1. proofofplay
    1. real
    1. sanko
    1. tangle
    1. xai
    
    drive-by:
    - fix bug in `set-rpc-urls-from-registry.ts`
    - add script to generate sql to add multiple chains to scraper db

--- typescript/sdk/src/consts/multisigIsm.ts
+++ typescript/sdk/src/consts/multisigIsm.ts
@@ -111,8 +111,11 @@ export const defaultMultisigConfigs: ChainMap<MultisigConfig> = {
   },
 
   cheesechain: {
-    threshold: 1,
-    validators: ['0x478fb53c6860ae8fc35235ba0d38d49b13128226'],
+    threshold: 2,
+    validators: [
+      '0x478fb53c6860ae8fc35235ba0d38d49b13128226',
+      '0xcf0211fafbb91fd9d06d7e306b30032dc3a1934f', // merkly
+    ],
   },
 
   chiado: {
@@ -127,6 +130,16 @@ export const defaultMultisigConfigs: ChainMap<MultisigConfig> = {
   connextsepolia: {
     threshold: 1,
     validators: ['0xffbbec8c499585d80ef69eb613db624d27e089ab'],
+  },
+
+  cyber: {
+    threshold: 1,
+    validators: ['0x94d7119ceeb802173b6924e6cc8c4cd731089a27'],
+  },
+
+  degenchain: {
+    threshold: 1,
+    validators: ['0x433e311f19524cd64fb2123ad0aa1579a4e1fc83'],
   },
 
   eclipse: {
@@ -225,6 +238,11 @@ export const defaultMultisigConfigs: ChainMap<MultisigConfig> = {
     ],
   },
 
+  kroma: {
+    threshold: 1,
+    validators: ['0x71b83c21342787d758199e4b8634d3a15f02dc6e'],
+  },
+
   linea: {
     threshold: 2,
     validators: [
@@ -232,6 +250,16 @@ export const defaultMultisigConfigs: ChainMap<MultisigConfig> = {
       '0xcf0211fafbb91fd9d06d7e306b30032dc3a1934f', // merkly
       '0x4f977a59fdc2d9e39f6d780a84d5b4add1495a36', // mitosis
     ],
+  },
+
+  lisk: {
+    threshold: 1,
+    validators: ['0xc0b282aa5bac43fee83cf71dc3dd1797c1090ea5'],
+  },
+
+  lukso: {
+    threshold: 1,
+    validators: ['0xa5e953701dcddc5b958b5defb677a829d908df6d'],
   },
 
   mantapacific: {
@@ -254,6 +282,21 @@ export const defaultMultisigConfigs: ChainMap<MultisigConfig> = {
       '0xcf0211fafbb91fd9d06d7e306b30032dc3a1934f', // merkly
       '0x4f977a59fdc2d9e39f6d780a84d5b4add1495a36', // mitosis
     ],
+  },
+
+  merlin: {
+    threshold: 1,
+    validators: ['0xc1d6600cb9326ed2198cc8c4ba8d6668e8671247'],
+  },
+
+  metis: {
+    threshold: 1,
+    validators: ['0xc4a3d25107060e800a43842964546db508092260'],
+  },
+
+  mint: {
+    threshold: 1,
+    validators: ['0xfed01ccdd7a65e8a6ad867b7fb03b9eb47777ac9'],
   },
 
   mode: {
@@ -337,12 +380,27 @@ export const defaultMultisigConfigs: ChainMap<MultisigConfig> = {
     ],
   },
 
+  proofofplay: {
+    threshold: 1,
+    validators: ['0xcda40baa71970a06e5f55e306474de5ca4e21c3b'],
+  },
+
+  real: {
+    threshold: 1,
+    validators: ['0xaebadd4998c70b05ce8715cf0c3cb8862fe0beec'],
+  },
+
   redstone: {
     threshold: 2,
     validators: [
       '0x1400b9737007f7978d8b4bbafb4a69c83f0641a7',
       '0xcf0211fafbb91fd9d06d7e306b30032dc3a1934f', // merkly
     ],
+  },
+
+  sanko: {
+    threshold: 1,
+    validators: ['0x795c37d5babbc44094b084b0c89ed9db9b5fae39'],
   },
 
   scroll: {
@@ -414,6 +472,11 @@ export const defaultMultisigConfigs: ChainMap<MultisigConfig> = {
     ],
   },
 
+  tangle: {
+    threshold: 1,
+    validators: ['0x1ee52cbbfacd7dcb0ba4e91efaa6fbc61602b15b'],
+  },
+
   viction: {
     threshold: 2,
     validators: [
@@ -424,13 +487,26 @@ export const defaultMultisigConfigs: ChainMap<MultisigConfig> = {
   },
 
   worldchain: {
+    threshold: 2,
+    validators: [
+      '0x31048785845325b22817448b68d08f8a8fe36854',
+      '0x11e2a683e83617f186614071e422b857256a9aae', // imperator
+      '0xcf0211fafbb91fd9d06d7e306b30032dc3a1934f', // merkly
+    ],
+  },
+
+  xai: {
     threshold: 1,
-    validators: ['0x31048785845325b22817448b68d08f8a8fe36854'],
+    validators: ['0xe993f01fea86eb64cda45ae5af1d5be40ac0c7e9'],
   },
 
   xlayer: {
-    threshold: 1,
-    validators: ['0xa2ae7c594703e988f23d97220717c513db638ea3'],
+    threshold: 2,
+    validators: [
+      '0xa2ae7c594703e988f23d97220717c513db638ea3',
+      '0xfed056cC0967F5BC9C6350F6C42eE97d3983394d', // imperator
+      '0xcf0211fafbb91fd9d06d7e306b30032dc3a1934f', // merkly
+    ],
   },
 
   zetachain: {
