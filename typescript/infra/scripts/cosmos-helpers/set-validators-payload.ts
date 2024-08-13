e801d81530728c42e67e2c7858deefe81b61aab9
Trevor Porter <tkporter4@gmail.com>
2024-08-13 11:54:07 +0100

    updte script

--- typescript/infra/scripts/cosmos-helpers/set-validators-payload.ts
+++ typescript/infra/scripts/cosmos-helpers/set-validators-payload.ts
@@ -1,4 +1,6 @@
-import { defaultMultisigConfigs, getDomainId } from '@hyperlane-xyz/sdk';
+import { stringify as yamlStringify } from 'yaml';
+
+import { defaultMultisigConfigs } from '@hyperlane-xyz/sdk';
 import { strip0x } from '@hyperlane-xyz/utils';
 
 import { getArgs } from '../agent-utils.js';
@@ -9,33 +11,46 @@ async function main() {
   const config = getEnvironmentConfig(environment);
   const multiProvider = await config.getMultiProvider();
 
-  const payloads = config.supportedChainNames.map((chain) => {
-    const multisig = defaultMultisigConfigs[chain];
-    if (!multisig) {
-      throw Error(`No multisig config found for ${chain}`);
-    }
+  const validatorConfigs = Object.fromEntries(
+    config.supportedChainNames.map((chain) => {
+      const multisig = defaultMultisigConfigs[chain];
+      if (!multisig) {
+        throw Error(`No multisig config found for ${chain}`);
+      }
 
-    return {
-      set_validators: {
-        domain: multiProvider.getDomainId(chain),
-        threshold: multisig.threshold,
-        validators: multisig.validators.map(strip0x),
+      return [
+        multiProvider.getDomainId(chain),
+        {
+          addrs: multisig.validators.map(strip0x),
+          threshold: multisig.threshold,
+        },
+      ];
+    }),
+  );
+
+  const deployConfig = {
+    deploy: {
+      ism: {
+        type: 'multisig',
+        validators: validatorConfigs,
       },
-    };
-  });
+    },
+  };
 
-  const keyName = 'low_balance_test';
+  console.log(yamlStringify(deployConfig));
 
-  for (const payload of payloads) {
-    const cmd = `neutrond tx wasm execute neutron1pa0fupajl0ysmdylcwau5szdkdcxg7rxt967p04felf9n6hc7zvqh4ycv9 '{ "set_validators": ${JSON.stringify(
-      payload,
-    )} }' --from ${keyName} --chain-id neutron-1 --node https://rpc-magnetix.neutron-1.neutron.org:443 --fees 50000ntrn`;
+  // const keyName = 'low_balance_test';
 
-    console.log(cmd);
+  // for (const payload of payloads) {
+  //   const cmd = `neutrond tx wasm execute neutron1pa0fupajl0ysmdylcwau5szdkdcxg7rxt967p04felf9n6hc7zvqh4ycv9 '${JSON.stringify(
+  //     payload,
+  //   )}' --from ${keyName} --chain-id neutron-1 --node https://rpc-magnetix.neutron-1.neutron.org:443 --fees 50000untrn`;
 
-    return;
-  }
-  // console.log(JSON.stringify(payload, null, 2));
+  //   console.log(cmd);
+
+  //   return;
+  // }
+  // // console.log(JSON.stringify(payload, null, 2));
 }
 
 main().catch((err) => {
