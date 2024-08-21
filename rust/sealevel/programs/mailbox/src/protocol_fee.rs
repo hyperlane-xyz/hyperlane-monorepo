c79045d513e38c4461b8b18e328d535c47707f88
Daniel Savu <23065004+daniel-savu@users.noreply.github.com>
2024-08-21 19:31:41 +0100

    feat: client support for sealevel protocol fees

--- rust/sealevel/programs/mailbox/src/protocol_fee.rs
+++ rust/sealevel/programs/mailbox/src/protocol_fee.rs
@@ -5,6 +5,8 @@ use solana_program::pubkey::Pubkey;
 
 /// The Protocol Fee configuration.
 #[derive(BorshSerialize, BorshDeserialize, Debug, PartialEq, Eq, Clone, Default)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[cfg_attr(feature = "serde", serde(rename_all = "camelCase"))]
 pub struct ProtocolFee {
     /// The current protocol fee, expressed in the lowest denomination.
     pub fee: u64,
