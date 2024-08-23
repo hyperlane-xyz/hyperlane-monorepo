47c196d151ea81683efb8737d44219e2b92358e6
Daniel Savu <23065004+daniel-savu@users.noreply.github.com>
2024-08-23 20:06:41 +0100

    fix: use `confirmed` commitment level in sealevel-client again

--- rust/sealevel/client/src/main.rs
+++ rust/sealevel/client/src/main.rs
@@ -721,7 +721,7 @@ fn main() {
         (Pubkey::from_str(&keypair_path).unwrap(), None)
     };
 
-    let commitment = CommitmentConfig::processed();
+    let commitment = CommitmentConfig::confirmed();
 
     let mut instructions = vec![];
 
