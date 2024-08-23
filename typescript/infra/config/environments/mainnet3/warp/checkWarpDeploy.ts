ca55fcc2ce576de1fb6b9895f99bbcefc3454ea7
Mo Hussan <22501692+Mo-Hussain@users.noreply.github.com>
2024-08-23 17:13:41 +0100

    update schedule

--- typescript/infra/config/environments/mainnet3/warp/checkWarpDeploy.ts
+++ typescript/infra/config/environments/mainnet3/warp/checkWarpDeploy.ts
@@ -4,10 +4,10 @@ import { environment } from '../chains.js';
 export const checkWarpDeployConfig: CheckWarpDeployConfig = {
   docker: {
     repo: 'gcr.io/abacus-labs-dev/hyperlane-monorepo',
-    tag: '9fa5231-20240822-124838',
+    tag: '36f7e14-20240823-160646',
   },
   namespace: environment,
-  cronSchedule: '*/5 * * * *',
+  cronSchedule: '0 15 * * *', // set to 3pm utc every day
   prometheusPushGateway:
     'http://prometheus-prometheus-pushgateway.monitoring.svc.cluster.local:9091',
 };
