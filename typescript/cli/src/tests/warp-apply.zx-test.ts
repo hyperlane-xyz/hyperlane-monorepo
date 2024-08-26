665df19e3d50fb76b356dd688dd49d28591ccfae
Le Yu <6251863+ltyu@users.noreply.github.com>
2024-08-26 17:58:45 -0400

    Uncomment tests

--- typescript/cli/src/tests/warp-apply.zx-test.ts
+++ typescript/cli/src/tests/warp-apply.zx-test.ts
@@ -1,11 +1,11 @@
 import { expect } from 'chai';
 import { beforeEach } from 'mocha';
-import path from 'path';
 
 import { updateWarpOwner } from './commands/helpers.js';
 import {
-  REGISTRY_PATH, // hyperlaneCoreDeploy,
-  hyperlaneWarpApply, // hyperlaneWarpApply,
+  REGISTRY_PATH,
+  hyperlaneCoreDeploy,
+  hyperlaneWarpApply,
   hyperlaneWarpDeploy,
   readWarpConfig,
 } from './commands/warp.js';
@@ -14,7 +14,7 @@ import {
 
 const BURN_ADDRESS = '0x0000000000000000000000000000000000000001';
 const EXAMPLES_PATH = './examples';
-// const CORE_CONFIG_PATH = `${EXAMPLES_PATH}/core-config.yaml`;
+const CORE_CONFIG_PATH = `${EXAMPLES_PATH}/core-config.yaml`;
 const WARP_CONFIG_PATH = `${EXAMPLES_PATH}/warp-route-deployment.yaml`;
 const WARP_CORE_CONFIG_PATH = `${REGISTRY_PATH}/deployments/warp_routes/ETH/anvil1-config.yaml`;
 
@@ -22,8 +22,7 @@ describe('WarpApply', function () {
   this.timeout(0); // No limit timeout since these tests can take a while
 
   beforeEach(async function () {
-    console.log(path.dirname(EXAMPLES_PATH));
-    // await hyperlaneCoreDeploy(CORE_CONFIG_PATH);
+    await hyperlaneCoreDeploy(CORE_CONFIG_PATH);
     await hyperlaneWarpDeploy(WARP_CONFIG_PATH);
   });
   it('should burn owner address', async function () {
@@ -38,7 +37,6 @@ describe('WarpApply', function () {
       WARP_CORE_CONFIG_PATH,
       warpConfigPath,
     );
-    console.log('updatedWarpDeployConfig', updatedWarpDeployConfig);
     expect(updatedWarpDeployConfig.anvil1.owner).to.equal(BURN_ADDRESS);
   });
 });
