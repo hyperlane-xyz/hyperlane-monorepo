b6cea10b6349eab6bc762f392ba38621e6572700
Danil Nemirovsky <ameten@users.noreply.github.com>
2024-08-28 14:27:34 +0100

    feat: Add scraper to Cosmos E2E test (#4387)
    ### Description
    
    Add scraper to Cosmos E2E test
    
    * Add minimal functionality to scraper for cosmos so that scraper does
    not crash
    * Add scraper to Cosmos E2E test
    
    ### Backward compatibility
    
    Yes
    
    ### Testing
    
    E2E Tests for Cosmos
    
    ---------
    
    Co-authored-by: Danil Nemirovsky <4614623+ameten@users.noreply.github.com>

--- rust/chains/hyperlane-cosmos/src/providers/mod.rs
+++ rust/chains/hyperlane-cosmos/src/providers/mod.rs
@@ -1,11 +1,17 @@
 use async_trait::async_trait;
-use tendermint_rpc::{client::CompatMode, HttpClient};
+use cosmrs::crypto::PublicKey;
+use cosmrs::Tx;
+use tendermint::hash::Algorithm;
+use tendermint::Hash;
+use tendermint_rpc::{client::CompatMode, Client, HttpClient};
+use time::OffsetDateTime;
 
 use hyperlane_core::{
-    BlockInfo, ChainInfo, ChainResult, ContractLocator, HyperlaneChain, HyperlaneDomain,
-    HyperlaneProvider, TxnInfo, H256, U256,
+    BlockInfo, ChainCommunicationError, ChainInfo, ChainResult, ContractLocator, HyperlaneChain,
+    HyperlaneDomain, HyperlaneProvider, TxnInfo, TxnReceiptInfo, H256, U256,
 };
 
+use crate::address::CosmosAddress;
 use crate::grpc::WasmProvider;
 use crate::{ConnectionConf, CosmosAmount, HyperlaneCosmosError, Signer};
 
@@ -20,7 +26,7 @@ pub mod rpc;
 #[derive(Debug, Clone)]
 pub struct CosmosProvider {
     domain: HyperlaneDomain,
-    canonical_asset: String,
+    connection_conf: ConnectionConf,
     grpc_client: WasmGrpcProvider,
     rpc_client: HttpClient,
 }
@@ -53,9 +59,9 @@ impl CosmosProvider {
 
         Ok(Self {
             domain,
+            connection_conf: conf,
             rpc_client,
             grpc_client,
-            canonical_asset: conf.get_canonical_asset(),
         })
     }
 
@@ -82,12 +88,95 @@ impl HyperlaneChain for CosmosProvider {
 
 #[async_trait]
 impl HyperlaneProvider for CosmosProvider {
-    async fn get_block_by_hash(&self, _hash: &H256) -> ChainResult<BlockInfo> {
-        todo!() // FIXME
+    async fn get_block_by_hash(&self, hash: &H256) -> ChainResult<BlockInfo> {
+        let tendermint_hash = Hash::from_bytes(Algorithm::Sha256, hash.as_bytes())
+            .expect("block hash should be of correct size");
+
+        // TODO add proper error handling
+        let response = self
+            .rpc_client
+            .block_by_hash(tendermint_hash)
+            .await
+            .map_err(|_| ChainCommunicationError::from_other_str("generic error"))?;
+
+        let received_hash = H256::from_slice(response.block_id.hash.as_bytes());
+
+        if &received_hash != hash {
+            return Err(ChainCommunicationError::from_other_str(
+                "received incorrect block",
+            ));
+        }
+
+        let block = response
+            .block
+            .ok_or_else(|| ChainCommunicationError::from_other_str("empty block info"))?;
+
+        let time: OffsetDateTime = block.header.time.into();
+
+        let block_info = BlockInfo {
+            hash: hash.to_owned(),
+            timestamp: time.unix_timestamp() as u64,
+            number: block.header.height.value(),
+        };
+
+        Ok(block_info)
     }
 
-    async fn get_txn_by_hash(&self, _hash: &H256) -> ChainResult<TxnInfo> {
-        todo!() // FIXME
+    async fn get_txn_by_hash(&self, hash: &H256) -> ChainResult<TxnInfo> {
+        let tendermint_hash = Hash::from_bytes(Algorithm::Sha256, hash.as_bytes())
+            .expect("transaction hash should be of correct size");
+
+        // TODO add proper error handling
+        let response = self
+            .rpc_client
+            .tx(tendermint_hash, false)
+            .await
+            .map_err(|_| ChainCommunicationError::from_other_str("generic error"))?;
+
+        let received_hash = H256::from_slice(response.hash.as_bytes());
+
+        if &received_hash != hash {
+            return Err(ChainCommunicationError::from_other_str(
+                "received incorrect transaction",
+            ));
+        }
+
+        let tx = Tx::from_bytes(&response.tx).map_err(|_| {
+            ChainCommunicationError::from_other_str("could not deserialize transaction")
+        })?;
+
+        let signer = tx
+            .auth_info
+            .signer_infos
+            .get(0)
+            .expect("there should be at least one signer");
+        let signer_public_key = signer
+            .public_key
+            .clone()
+            .ok_or_else(|| ChainCommunicationError::from_other_str("no public key"))?;
+
+        let public_key: PublicKey = signer_public_key.try_into()?;
+        let key =
+            CosmosAddress::from_pubkey(public_key, &self.connection_conf.get_bech32_prefix())?;
+        let sender = key.digest();
+
+        let tx_info = TxnInfo {
+            hash: hash.to_owned(),
+            gas_limit: U256::from(response.tx_result.gas_wanted),
+            max_priority_fee_per_gas: None,
+            max_fee_per_gas: None,
+            gas_price: None,
+            nonce: 0,
+            sender,
+            recipient: None,
+            receipt: Some(TxnReceiptInfo {
+                gas_used: U256::from(response.tx_result.gas_used),
+                cumulative_gas_used: U256::from(response.tx_result.gas_used),
+                effective_gas_price: None,
+            }),
+        };
+
+        Ok(tx_info)
     }
 
     async fn is_contract(&self, address: &H256) -> ChainResult<bool> {
@@ -100,7 +189,7 @@ impl HyperlaneProvider for CosmosProvider {
     async fn get_balance(&self, address: String) -> ChainResult<U256> {
         Ok(self
             .grpc_client
-            .get_balance(address, self.canonical_asset.clone())
+            .get_balance(address, self.connection_conf.get_canonical_asset())
             .await?)
     }
 
