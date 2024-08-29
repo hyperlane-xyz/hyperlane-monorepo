c422bc132adaff0c6fa44a29c07b701bef9b5256
Le Yu <6251863+ltyu@users.noreply.github.com>
2024-08-29 12:34:12 -0400

    Change anvil ports to try to get tests working on CI

--- typescript/cli/scripts/all-test.sh
+++ typescript/cli/scripts/all-test.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env bash
 
 function cleanup() {
-  set +e 
-  pkill -f anvil
-  rm -rf /tmp/anvil*
-  set -e
-
   rm -f ./test-configs/anvil/chains/anvil1/addresses.yaml
   rm -f ./test-configs/anvil/chains/anvil2/addresses.yaml
 }
@@ -13,8 +8,8 @@ function cleanup() {
 cleanup
 
 echo "Starting anvil1 and anvil2 chain"
-anvil --chain-id 31337 -p 8545 --state /tmp/anvil1/state --gas-price 1 > /dev/null &
-anvil --chain-id 31338 -p 8555 --state /tmp/anvil2/state --gas-price 1 > /dev/null &
+anvil --chain-id 31347 -p 8600 --state /tmp/anvil1/state --gas-price 1 > /dev/null &
+anvil --chain-id 31348 -p 8700 --state /tmp/anvil2/state --gas-price 1 > /dev/null &
 
 echo "Running all tests"
 yarn mocha --config .mocharc.json
