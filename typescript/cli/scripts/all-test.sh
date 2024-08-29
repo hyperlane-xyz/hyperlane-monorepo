2d89c047e078d8a86cc165b5c83f2a4efa3a2e74
Le Yu <6251863+ltyu@users.noreply.github.com>
2024-08-29 15:35:12 -0400

    Re-add tests, use anvil3 to prevent collision with existing tests

--- typescript/cli/scripts/all-test.sh
+++ typescript/cli/scripts/all-test.sh
@@ -1,24 +1,24 @@
-# #!/usr/bin/env bash
+#!/usr/bin/env bash
 
-# function cleanup() {
-#   set +e 
-#   pkill -f anvil
-#   rm -rf /tmp/anvil_1
-#   rm -rf /tmp/anvil_2
-#   set -e
-#   rm -f ./test-configs/anvil/chains/anvil1/addresses.yaml
-#   rm -f ./test-configs/anvil/chains/anvil2/addresses.yaml
-# }
+function cleanup() {
+  set +e 
+  pkill -f anvil
+  rm -rf /tmp/anvil2
+  rm -rf /tmp/anvil3
+  set -e
+  rm -f ./test-configs/anvil/chains/anvil2/addresses.yaml
+  rm -f ./test-configs/anvil/chains/anvil3/addresses.yaml
+}
 
-# cleanup
+cleanup
 
-# echo "Starting anvil1 and anvil2 chain"
-# anvil --chain-id 31347 -p 8600 --state /tmp/anvil_1/state --gas-price 1 &
-# anvil --chain-id 31348 -p 8700 --state /tmp/anvil_2/state --gas-price 1 &
+echo "Starting anvil2 and anvil3 chain"
+anvil --chain-id 31347 -p 8600 --state /tmp/anvil2/state --gas-price 1 &
+anvil --chain-id 31348 -p 8700 --state /tmp/anvil3/state --gas-price 1 &
 
-# echo "Running all tests"
-# yarn mocha --config .mocharc.json
+echo "Running all tests"
+yarn mocha --config .mocharc.json
 
-# cleanup
+cleanup
 
-# echo "Done all tests"
+echo "Done all tests"
