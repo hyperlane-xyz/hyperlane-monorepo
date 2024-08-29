b7cc5a39789932797796287acde656720da8f02b
Le Yu <6251863+ltyu@users.noreply.github.com>
2024-08-29 16:44:01 -0400

    Remove cleanup....

--- typescript/cli/scripts/all-test.sh
+++ typescript/cli/scripts/all-test.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
 
 function cleanup() {
-  # set +e 
-  # pkill -f anvil
-  # rm -rf /tmp/anvil2
-  # rm -rf /tmp/anvil3
-  # set -e
+  set +e 
+  pkill -f anvil
+  rm -rf /tmp/anvil2
+  rm -rf /tmp/anvil3
   rm -f ./test-configs/anvil/chains/anvil2/addresses.yaml
   rm -f ./test-configs/anvil/chains/anvil3/addresses.yaml
+  set -e
+  
 }
 
-cleanup
+# cleanup
 
 echo "Starting anvil2 and anvil3 chain"
 anvil --chain-id 31338 -p 8555 --state /tmp/anvil2/state --gas-price 1 > /dev/null &
@@ -19,6 +20,6 @@ anvil --chain-id 31347 -p 8600 --state /tmp/anvil3/state --gas-price 1 > /dev/nu
 echo "Running all tests"
 yarn mocha --config .mocharc.json
 
-cleanup
+# cleanup
 
 echo "Done all tests"
