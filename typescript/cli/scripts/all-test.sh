03bb4ff96742d5656a67b2e0e4286ffc598c8244
Le Yu <6251863+ltyu@users.noreply.github.com>
2024-08-29 16:32:37 -0400

    Remove cleanup logic

--- typescript/cli/scripts/all-test.sh
+++ typescript/cli/scripts/all-test.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 function cleanup() {
-  set +e 
-  pkill -f anvil
-  rm -rf /tmp/anvil2
-  rm -rf /tmp/anvil3
-  set -e
+  # set +e 
+  # pkill -f anvil
+  # rm -rf /tmp/anvil2
+  # rm -rf /tmp/anvil3
+  # set -e
   rm -f ./test-configs/anvil/chains/anvil2/addresses.yaml
   rm -f ./test-configs/anvil/chains/anvil3/addresses.yaml
 }
