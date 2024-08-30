3c07ded5b7188150641866a192798fef75740d95
Noah Bayindirli 🥂 <15343884+nbayindirli@users.noreply.github.com>
2024-08-29 20:28:12 -0700

    feat(cli): add submit functionality support to warp apply (#4225)
    ### Description
    
    - adds submit functionality support to warp apply
    - enables dynamic submission of transactions to vanilla json rpc, gnosis
    safe, and impersonated accounts while dry-running
    - allows easy support of ICA tx submissions in the future
    
    ### Drive-by changes
    - just updated `ApplyParams` to `WarpApplyParams`
    - e2e tests written in ts
    
    ### Related issues
    
    - https://github.com/hyperlane-xyz/hyperlane-monorepo/issues/4242
    
    ### Backward compatibility
    
    - yes
    
    ### Testing
    - [x] Single warp route transfer ownership from EOA to Safe
    - [x] Single warp route transfer from Safe to EOA
    - [x] 2 warp route transfer ownership from EOA to safe
    - [x] 2 warp route transfer from Safe to EOA (sepolia and basesepolia)
    
    Multichain enrollments through their respective Safes:
    - [x] Deploy to sepolia with address to Signer
    - [x] Transfer Owner to safe
    - [x] Warp apply to extend a synthetic Route to base sepolia and set
    owner to safe
    - [x] Approve safe txs to enroll each other
    - [x] Send a test message
    
    E2e Testing
    
    ---------
    
    Co-authored-by: Le Yu <6251863+ltyu@users.noreply.github.com>

---
+++ typescript/cli/src/tests/commands/warp.ts
@@ -0,0 +1,68 @@
+import { $ } from 'zx';
+
+import { WarpRouteDeployConfig } from '@hyperlane-xyz/sdk';
+
+import { readYamlOrJson } from '../../utils/files.js';
+
+import { ANVIL_KEY, REGISTRY_PATH, getDeployedWarpAddress } from './helpers.js';
+
+$.verbose = true;
+
+/**
+ * Deploys the Warp route to the specified chain using the provided config.
+ */
+export async function hyperlaneWarpDeploy(warpCorePath: string) {
+  return $`yarn workspace @hyperlane-xyz/cli run hyperlane warp deploy \
+        --registry ${REGISTRY_PATH} \
+        --overrides " " \
+        --config ${warpCorePath} \
+        --key ${ANVIL_KEY} \
+        --verbosity debug \
+        --yes`;
+}
+
+/**
+ * Applies updates to the Warp route config.
+ */
+export async function hyperlaneWarpApply(
+  warpDeployPath: string,
+  warpCorePath: string,
+) {
+  return $`yarn workspace @hyperlane-xyz/cli run hyperlane warp apply \
+        --registry ${REGISTRY_PATH} \
+        --overrides " " \
+        --config ${warpDeployPath} \
+        --warp ${warpCorePath} \
+        --key ${ANVIL_KEY} \
+        --verbosity debug \
+        --yes`;
+}
+
+export async function hyperlaneWarpRead(
+  chain: string,
+  warpAddress: string,
+  warpDeployPath: string,
+) {
+  return $`yarn workspace @hyperlane-xyz/cli run hyperlane warp read \
+        --registry ${REGISTRY_PATH} \
+        --overrides " " \
+        --address ${warpAddress} \
+        --chain ${chain} \
+        --config ${warpDeployPath}`;
+}
+
+/**
+ * Reads the Warp route deployment config to specified output path.
+ * @param warpCorePath path to warp core
+ * @param warpDeployPath path to output the resulting read
+ * @returns The Warp route deployment config.
+ */
+export async function readWarpConfig(
+  chain: string,
+  warpCorePath: string,
+  warpDeployPath: string,
+): Promise<WarpRouteDeployConfig> {
+  const warpAddress = getDeployedWarpAddress(chain, warpCorePath);
+  await hyperlaneWarpRead(chain, warpAddress!, warpDeployPath);
+  return readYamlOrJson(warpDeployPath);
+}
