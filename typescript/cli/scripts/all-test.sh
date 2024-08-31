16fc737d2db9b9564e0b2bee841bf27db3094c04
Le Yu <6251863+ltyu@users.noreply.github.com>
2024-08-31 19:28:21 -0400

    Add cleanup. Add error into re-throw

--- typescript/cli/scripts/all-test.sh
+++ typescript/cli/scripts/all-test.sh
@@ -11,7 +11,7 @@ function cleanup() {
   
 }
 
-# cleanup
+cleanup
 
 echo "Starting anvil2 and anvil3 chain"
 anvil --chain-id 31338 -p 8555 --state /tmp/anvil2/state --gas-price 1 > /dev/null &
@@ -20,6 +20,6 @@ anvil --chain-id 31347 -p 8600 --state /tmp/anvil3/state --gas-price 1 > /dev/nu
 echo "Running all tests"
 yarn mocha --config .mocharc.json
 
-# cleanup
+cleanup
 
 echo "Done all tests"
