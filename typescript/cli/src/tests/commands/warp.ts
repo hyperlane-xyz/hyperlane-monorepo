99b78549f32d5c6ddda42729ab73f824419609d0
Le Yu <6251863+ltyu@users.noreply.github.com>
2024-08-31 18:48:15 -0400

    Add verbosity read

--- typescript/cli/src/tests/commands/warp.ts
+++ typescript/cli/src/tests/commands/warp.ts
@@ -48,6 +48,7 @@ export async function hyperlaneWarpRead(
         --overrides " " \
         --address ${warpAddress} \
         --chain ${chain} \
+        --verbosity debug \
         --config ${warpDeployPath}`;
 }
 
