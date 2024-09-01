1690a039cd754ef9632348c4ad01e4092b92f734
Le Yu <6251863+ltyu@users.noreply.github.com>
2024-08-31 20:25:27 -0400

    Remove "test"

--- typescript/cli/scripts/all-test.sh
+++ typescript/cli/scripts/all-test.sh
@@ -7,8 +7,7 @@ function cleanup() {
   rm -rf /tmp/anvil3
   rm -f ./test-configs/anvil/chains/anvil2/addresses.yaml
   rm -f ./test-configs/anvil/chains/anvil3/addresses.yaml
-  set -e
-  
+  set -e 
 }
 
 cleanup
