eaf951376b2dd32231d2630ccddbda07265fa6a7
Mo Hussan <22501692+Mo-Hussain@users.noreply.github.com>
2024-09-03 12:52:19 +0100

    feat(infra): use latest image tag for the check-warp-deploy cron job config

--- typescript/infra/config/environments/mainnet3/warp/checkWarpDeploy.ts
+++ typescript/infra/config/environments/mainnet3/warp/checkWarpDeploy.ts
@@ -4,7 +4,7 @@ import { environment } from '../chains.js';
 export const checkWarpDeployConfig: CheckWarpDeployConfig = {
   docker: {
     repo: 'gcr.io/abacus-labs-dev/hyperlane-monorepo',
-    tag: '7ce7aef-20240912-090321',
+    tag: 'latest',
   },
   namespace: environment,
   cronSchedule: '0 15 * * *', // set to 3pm utc every day
