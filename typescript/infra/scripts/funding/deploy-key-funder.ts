b1d8bb8777684cfc863020d9f2eb170f166c112c
Trevor Porter <tkporter4@gmail.com>
2024-09-04 15:10:30 +0100

    feat: more ergonomic RPC URL setting (#4413)
    ### Description
    
    - Updates `scripts/secret-rpc-urls/set-rpc-urls.ts` to:
      - Interactively prompt for what RPC URLs to use
      - If desired, update K8s consumers (secrets & reset pods)
    
    Usage:
    ```
    yarn tsx ./scripts/secret-rpc-urls/set-rpc-urls.ts -e mainnet3 --chain lisk
    ```
    
    or even
    ```
    yarn tsx ./scripts/secret-rpc-urls/set-rpc-urls.ts -e mainnet3 --chains lisk zircuit
    ```
    
    Removes `set-rpc-urls-from-registry` in favor of this new command, which
    lets you do this as well.
    
    Note that atm it doesn't consider warp monitors as dependent k8s
    resources. This is because we haven't really defined infrastructure as
    code for these -- the deployments that exist are kinda manually created
    by passing in a config file. This should change but didn't feel
    appropriate in this PR
    
    The meatiest changes are in utils/rpcUrls.ts and utils/k8s.ts, the rest
    were kinda drivebys to accommodate this
    
    ### Drive-by changes
    
    - Various tweaks to labels used throughout k8s to be a bit more uniform
    - Refactored a lot of helm related scripts to use `HelmManager` as a
    superclass
    
    ### Related issues
    
    <!--
    - Fixes #[issue number here]
    -->
    
    ### Backward compatibility
    
    <!--
    Are these changes backward compatible? Are there any infrastructure
    implications, e.g. changes that would prohibit deploying older commits
    using this infra tooling?
    
    Yes/No
    -->
    
    ### Testing
    
    <!--
    What kind of testing have these changes undergone?
    
    None/Manual/Unit Tests
    -->

--- typescript/infra/scripts/funding/deploy-key-funder.ts
+++ typescript/infra/scripts/funding/deploy-key-funder.ts
@@ -1,14 +1,12 @@
 import { Contexts } from '../../config/contexts.js';
-import {
-  getKeyFunderConfig,
-  runKeyFunderHelmCommand,
-} from '../../src/funding/key-funder.js';
+import { environment } from '../../config/environments/mainnet3/chains.js';
+import { KeyFunderHelmManager } from '../../src/funding/key-funder.js';
 import { HelmCommand } from '../../src/utils/helm.js';
 import { assertCorrectKubeContext } from '../agent-utils.js';
 import { getConfigsBasedOnArgs } from '../core-utils.js';
 
 async function main() {
-  const { agentConfig, envConfig } = await getConfigsBasedOnArgs();
+  const { agentConfig, envConfig, context } = await getConfigsBasedOnArgs();
   if (agentConfig.context != Contexts.Hyperlane)
     throw new Error(
       `Invalid context ${agentConfig.context}, must be ${Contexts.Hyperlane}`,
@@ -16,13 +14,8 @@ async function main() {
 
   await assertCorrectKubeContext(envConfig);
 
-  const keyFunderConfig = getKeyFunderConfig(envConfig);
-
-  await runKeyFunderHelmCommand(
-    HelmCommand.InstallOrUpgrade,
-    agentConfig,
-    keyFunderConfig,
-  );
+  const manager = KeyFunderHelmManager.forEnvironment(environment);
+  await manager.runHelmCommand(HelmCommand.InstallOrUpgrade);
 }
 
 main()
---
+++ typescript/infra/src/warp/helm.ts
@@ -0,0 +1,45 @@
+import path from 'path';
+
+import { DeployEnvironment } from '../../src/config/environment.js';
+import { HelmManager } from '../../src/utils/helm.js';
+import { getInfraPath } from '../../src/utils/utils.js';
+
+export class WarpRouteMonitorHelmManager extends HelmManager {
+  readonly helmChartPath: string = path.join(
+    getInfraPath(),
+    './helm/warp-routes',
+  );
+
+  constructor(
+    readonly configFilePath: string,
+    readonly runEnv: DeployEnvironment,
+  ) {
+    super();
+  }
+
+  async helmValues() {
+    const pathRelativeToMonorepoRoot = this.configFilePath.includes(
+      'typescript/infra',
+    )
+      ? this.configFilePath
+      : path.join('typescript/infra', this.configFilePath);
+    return {
+      image: {
+        repository: 'gcr.io/abacus-labs-dev/hyperlane-monorepo',
+        tag: '38ff1c4-20240823-093934',
+      },
+      configFilePath: pathRelativeToMonorepoRoot,
+      fullnameOverride: this.helmReleaseName,
+    };
+  }
+
+  get namespace() {
+    return this.runEnv;
+  }
+
+  get helmReleaseName(): string {
+    const match = this.configFilePath.match(/\/([^/]+)-deployments\.yaml$/);
+    const name = match ? match[1] : this.configFilePath;
+    return `hyperlane-warp-route-${name.toLowerCase()}`; // helm requires lower case release names
+  }
+}
