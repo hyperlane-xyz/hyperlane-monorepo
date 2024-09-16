d680e50d49118885434d9b3c129120d54389c0c3
-f <kunalarora1729@gmail.com>
2024-09-16 17:05:38 +0530

    opl2tol1

---
+++ solidity/test/isms/ExternalBridgeTest.sol
@@ -0,0 +1,107 @@
+// SPDX-License-Identifier: MIT or Apache-2.0
+pragma solidity ^0.8.13;
+
+import {Test} from "forge-std/Test.sol";
+import {TypeCasts} from "../../contracts/libs/TypeCasts.sol";
+import {StandardHookMetadata} from "../../contracts/hooks/libs/StandardHookMetadata.sol";
+import {TestMailbox} from "../../contracts/test/TestMailbox.sol";
+import {Message} from "../../contracts/libs/Message.sol";
+import {MessageUtils} from "./IsmTestUtils.sol";
+import {TestRecipient} from "../../contracts/test/TestRecipient.sol";
+import {AbstractMessageIdAuthorizedIsm} from "../../contracts/isms/hook/AbstractMessageIdAuthorizedIsm.sol";
+import {AbstractMessageIdAuthHook} from "../../contracts/hooks/libs/AbstractMessageIdAuthHook.sol";
+
+abstract contract ExternalBridgeTest is Test {
+    using TypeCasts for address;
+    using MessageUtils for bytes;
+
+    uint8 internal constant HYPERLANE_VERSION = 1;
+    uint32 internal ORIGIN_DOMAIN;
+    uint32 internal DESTINATION_DOMAIN;
+    uint256 internal GAS_QUOTE;
+    TestMailbox internal originMailbox;
+    TestMailbox internal destinationMailbox;
+    TestRecipient internal testRecipient;
+
+    AbstractMessageIdAuthHook internal hook;
+
+    bytes internal testMessage =
+        abi.encodePacked("Hello from the other chain!");
+    bytes internal testMetadata =
+        StandardHookMetadata.overrideRefundAddress(address(this));
+    bytes internal encodedMessage;
+    bytes32 internal messageId;
+
+    function setUp() public virtual {
+        testRecipient = new TestRecipient();
+        encodedMessage = _encodeTestMessage();
+        messageId = Message.id(encodedMessage);
+    }
+
+    function _encodeTestMessage() internal view returns (bytes memory) {
+        return
+            MessageUtils.formatMessage(
+                HYPERLANE_VERSION,
+                uint32(0),
+                ORIGIN_DOMAIN,
+                TypeCasts.addressToBytes32(address(this)),
+                DESTINATION_DOMAIN,
+                TypeCasts.addressToBytes32(address(testRecipient)),
+                testMessage
+            );
+    }
+
+    function _encodeHookData(
+        bytes32 _messageId
+    ) internal pure returns (bytes memory) {
+        return
+            abi.encodeCall(
+                AbstractMessageIdAuthorizedIsm.verifyMessageId,
+                (_messageId)
+            );
+    }
+
+    function testFork_postDispatch_revertWhen_chainIDNotSupported() public {
+        bytes memory message = MessageUtils.formatMessage(
+            0,
+            uint32(0),
+            DESTINATION_DOMAIN,
+            TypeCasts.addressToBytes32(address(this)),
+            2, // wrong domain
+            TypeCasts.addressToBytes32(address(testRecipient)),
+            testMessage
+        );
+
+        destinationMailbox.updateLatestDispatchedId(Message.id(message));
+        vm.expectRevert(
+            "AbstractMessageIdAuthHook: invalid destination domain"
+        );
+        hook.postDispatch(testMetadata, message);
+    }
+
+    function test_postDispatch_revertWhen_notLastDispatchedMessage() public {
+        vm.expectRevert(
+            "AbstractMessageIdAuthHook: message not latest dispatched"
+        );
+        hook.postDispatch(testMetadata, encodedMessage);
+    }
+
+    function test_postDispatch() public {
+        bytes memory encodedHookData = _encodeHookData(messageId);
+        destinationMailbox.updateLatestDispatchedId(messageId);
+        _expectOriginBridgeCall(encodedHookData);
+
+        hook.postDispatch{value: GAS_QUOTE}(testMetadata, encodedMessage);
+    }
+
+    function _expectOriginBridgeCall(
+        bytes memory _encodedHookData
+    ) internal virtual;
+
+    // function testPostDispatch_RevertWhen_ChainIDNotSupported() public virtual;
+    // function testPostDispatch_RevertWhen_NotLastDispatchedMessage() public virtual;
+    // function testVerify_WithValue(uint256 _msgValue) public virtual;
+    // function testVerify_RevertWhen_InvalidMessage() public virtual;
+
+    // Add more common test cases as needed
+}
--- solidity/test/isms/OPL2ToL1Ism.t.sol
+++ solidity/test/isms/OPL2ToL1Ism.t.sol
@@ -15,57 +15,45 @@ import {TestRecipient} from "../../contracts/test/TestRecipient.sol";
 import {MockOptimismMessenger, MockOptimismPortal} from "../../contracts/mock/MockOptimism.sol";
 import {OPL2ToL1Hook} from "../../contracts/hooks/OPL2ToL1Hook.sol";
 import {OPL2ToL1Ism} from "../../contracts/isms/hook/OPL2ToL1Ism.sol";
+import {ExternalBridgeTest} from "./ExternalBridgeTest.sol";
 
-contract OPL2ToL1IsmTest is Test {
-    uint8 internal constant HYPERLANE_VERSION = 1;
-    uint32 internal constant MAINNET_DOMAIN = 1;
-    uint32 internal constant OPTIMISM_DOMAIN = 10;
-    uint32 internal constant GAS_QUOTE = 120_000;
-
+contract OPL2ToL1IsmTest is ExternalBridgeTest {
     address internal constant L2_MESSENGER_ADDRESS =
         0x4200000000000000000000000000000000000007;
 
     uint256 internal constant MOCK_NONCE = 0;
 
-    TestMailbox public l2Mailbox;
-    TestRecipient internal testRecipient;
-    bytes internal testMessage =
-        abi.encodePacked("Hello from the other chain!");
-    bytes internal encodedMessage;
-    bytes internal testMetadata =
-        StandardHookMetadata.overrideRefundAddress(address(this));
-    bytes32 internal messageId;
-
     MockOptimismPortal internal portal;
     MockOptimismMessenger internal l1Messenger;
-    OPL2ToL1Hook public hook;
     OPL2ToL1Ism public ism;
 
     ///////////////////////////////////////////////////////////////////
     ///                            SETUP                            ///
     ///////////////////////////////////////////////////////////////////
 
-    function setUp() public {
+    function setUp() public override {
+        ORIGIN_DOMAIN = 10;
+        DESTINATION_DOMAIN = 1;
+        GAS_QUOTE = 120_000;
+        super.setUp();
+
         // Optimism messenger mock setup
         vm.etch(
             L2_MESSENGER_ADDRESS,
             address(new MockOptimismMessenger()).code
         );
 
-        testRecipient = new TestRecipient();
-
-        encodedMessage = _encodeTestMessage();
-        messageId = Message.id(encodedMessage);
+        deployAll();
     }
 
     function deployHook() public {
-        l2Mailbox = new TestMailbox(OPTIMISM_DOMAIN);
+        destinationMailbox = new TestMailbox(DESTINATION_DOMAIN);
         hook = new OPL2ToL1Hook(
-            address(l2Mailbox),
-            MAINNET_DOMAIN,
+            address(destinationMailbox),
+            DESTINATION_DOMAIN,
             TypeCasts.addressToBytes32(address(ism)),
             L2_MESSENGER_ADDRESS,
-            GAS_QUOTE
+            uint32(GAS_QUOTE)
         );
     }
 
@@ -85,59 +73,19 @@ contract OPL2ToL1IsmTest is Test {
         ism.setAuthorizedHook(TypeCasts.addressToBytes32(address(hook)));
     }
 
-    function test_postDispatch() public {
-        deployAll();
-
-        bytes memory encodedHookData = abi.encodeCall(
-            AbstractMessageIdAuthorizedIsm.verifyMessageId,
-            (messageId)
-        );
-
-        l2Mailbox.updateLatestDispatchedId(messageId);
-
+    function _expectOriginBridgeCall(
+        bytes memory _encodedHookData
+    ) internal override {
         vm.expectCall(
             L2_MESSENGER_ADDRESS,
             abi.encodeCall(
                 ICrossDomainMessenger.sendMessage,
-                (address(ism), encodedHookData, GAS_QUOTE)
+                (address(ism), _encodedHookData, uint32(GAS_QUOTE))
             )
         );
-
-        hook.postDispatch{value: GAS_QUOTE}(testMetadata, encodedMessage);
-    }
-
-    function testFork_postDispatch_revertWhen_chainIDNotSupported() public {
-        deployAll();
-
-        bytes memory message = MessageUtils.formatMessage(
-            0,
-            uint32(0),
-            OPTIMISM_DOMAIN,
-            TypeCasts.addressToBytes32(address(this)),
-            2, // wrong domain
-            TypeCasts.addressToBytes32(address(testRecipient)),
-            testMessage
-        );
-
-        l2Mailbox.updateLatestDispatchedId(Message.id(message));
-        vm.expectRevert(
-            "AbstractMessageIdAuthHook: invalid destination domain"
-        );
-        hook.postDispatch(testMetadata, message);
-    }
-
-    function test_postDispatch_revertWhen_notLastDispatchedMessage() public {
-        deployAll();
-
-        vm.expectRevert(
-            "AbstractMessageIdAuthHook: message not latest dispatched"
-        );
-        hook.postDispatch(testMetadata, encodedMessage);
     }
 
     function test_verify_directWithdrawalCall() public {
-        deployAll();
-
         bytes memory encodedWithdrawalTx = _encodeFinalizeWithdrawalTx(
             address(ism),
             0,
@@ -150,7 +98,6 @@ contract OPL2ToL1IsmTest is Test {
     function test_verify_directWithdrawalCall_revertsWhen_invalidSender()
         public
     {
-        deployAll();
         l1Messenger.setXDomainMessageSender(address(this));
 
         bytes memory encodedWithdrawalTx = _encodeFinalizeWithdrawalTx(
@@ -164,8 +111,6 @@ contract OPL2ToL1IsmTest is Test {
     }
 
     function test_verify_statefulVerify() public {
-        deployAll();
-
         vm.deal(address(portal), 1 ether);
         IOptimismPortal.WithdrawalTransaction
             memory withdrawal = IOptimismPortal.WithdrawalTransaction({
@@ -184,8 +129,6 @@ contract OPL2ToL1IsmTest is Test {
     }
 
     function test_verify_statefulAndDirectWithdrawal() public {
-        deployAll();
-
         IOptimismPortal.WithdrawalTransaction
             memory withdrawal = IOptimismPortal.WithdrawalTransaction({
                 nonce: MOCK_NONCE,
@@ -208,15 +151,11 @@ contract OPL2ToL1IsmTest is Test {
     }
 
     function test_verify_revertsWhen_noStatefulAndDirectWithdrawal() public {
-        deployAll();
-
         vm.expectRevert();
         ism.verify(new bytes(0), encodedMessage);
     }
 
     function test_verify_revertsWhen_invalidIsm() public {
-        deployAll();
-
         bytes memory encodedWithdrawalTx = _encodeFinalizeWithdrawalTx(
             address(this),
             0,
@@ -228,8 +167,6 @@ contract OPL2ToL1IsmTest is Test {
     }
 
     function test_verify_revertsWhen_incorrectMessageId() public {
-        deployAll();
-
         bytes32 incorrectMessageId = keccak256("incorrect message id");
 
         bytes memory encodedWithdrawalTx = _encodeFinalizeWithdrawalTx(
@@ -265,28 +202,12 @@ contract OPL2ToL1IsmTest is Test {
 
     /* ============ helper functions ============ */
 
-    function _encodeTestMessage() internal view returns (bytes memory) {
-        return
-            MessageUtils.formatMessage(
-                HYPERLANE_VERSION,
-                uint32(0),
-                OPTIMISM_DOMAIN,
-                TypeCasts.addressToBytes32(address(this)),
-                MAINNET_DOMAIN,
-                TypeCasts.addressToBytes32(address(testRecipient)),
-                testMessage
-            );
-    }
-
     function _encodeMessengerCalldata(
         address _ism,
         uint256 _value,
         bytes32 _messageId
     ) internal view returns (bytes memory) {
-        bytes memory encodedHookData = abi.encodeCall(
-            AbstractMessageIdAuthorizedIsm.verifyMessageId,
-            (_messageId)
-        );
+        bytes memory encodedHookData = _encodeHookData(_messageId);
 
         return
             abi.encodeCall(
