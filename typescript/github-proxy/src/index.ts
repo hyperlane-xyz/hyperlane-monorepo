00b320f2961ed83cb9133ef7bdb55d4404306519
Le Yu <6251863+ltyu@users.noreply.github.com>
2024-09-17 13:19:11 -0400

    Remove array

--- typescript/github-proxy/src/index.ts
+++ typescript/github-proxy/src/index.ts
@@ -1,8 +1,7 @@
 import { DISALLOWED_URL_MSG } from './errors.js';
 
-const GITHUB_API_ALLOWLIST = [
-  '/repos/hyperlane-xyz/hyperlane-registry/git/trees/main?recursive=true',
-];
+const GITHUB_API_ALLOWLIST =
+  '/repos/hyperlane-xyz/hyperlane-registry/git/trees/main?recursive=true';
 const GITPUB_API_HOST = 'https://api.github.com';
 export default {
   async fetch(request, env, _ctx): Promise<Response> {
