50a5ee06564b39d10dfcee408d1de21bd4d219ce
Yorke Rhodes <yorke@hyperlane.xyz>
2024-09-18 14:56:16 -0400

    Fix number in shellscript comment

--- solidity/bytecodeversion.sh
+++ solidity/bytecodeversion.sh
@@ -6,7 +6,7 @@ TEMPFILE=$(mktemp)
 # writes all but the last 2 lines to the temp file
 head -n $(($(wc -l < $FILEPATH) - 2)) $FILEPATH > $TEMPFILE
 
-# writes generated last 3 lines to the temp file
+# writes generated last 2 lines to the temp file
 cat <<EOF >> $TEMPFILE
     string public constant PACKAGE_VERSION = "$npm_package_version";
 }
