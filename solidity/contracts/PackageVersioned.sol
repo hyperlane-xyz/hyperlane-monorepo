bbc7b986a7fb238ee2a76380e192c13ead9f9b03
Yorke Rhodes <yorke@hyperlane.xyz>
2024-09-18 14:15:04 -0400

    Add PackageVersioned contract and build script

---
+++ solidity/contracts/PackageVersioned.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT OR Apache-2.0
+pragma solidity >=0.6.11;
+
+/**
+ * @title Versioned
+ * @notice Version getter for contracts
+ **/
+contract PackageVersioned {
+    // GENERATED CODE - DO NOT EDIT
+    string public constant PACKAGE_VERSION = "5.1.0";
+}
