1ecb3f2a6dbb97a22745bca87cd587d906018b64
pbio <10051819+paulbalaji@users.noreply.github.com>
2024-09-23 18:02:13 -0400

    feat: revamp testnet4 gasoracle/igp calculations

--- typescript/infra/config/environments/testnet4/igp.ts
+++ typescript/infra/config/environments/testnet4/igp.ts
@@ -1,16 +1,24 @@
+import { BigNumber, utils as ethersUtils } from 'ethers';
+
 import {
   ChainMap,
   ChainName,
   HookType,
   IgpConfig,
+  TOKEN_EXCHANGE_RATE_DECIMALS,
   defaultMultisigConfigs,
   multisigIsmVerificationCost,
 } from '@hyperlane-xyz/sdk';
 import { Address, exclude, objMap } from '@hyperlane-xyz/utils';
 
+import {
+  AllStorageGasOracleConfigs,
+  getAllStorageGasOracleConfigs,
+  getTokenExchangeRateFromValues,
+} from '../../../src/config/gas-oracle.js';
 import { isEthereumProtocolChain } from '../../../src/utils/utils.js';
 
-import { storageGasOracleConfig } from './gas-oracle.js';
+import gasPrices from './gasPrices.json';
 import { owners } from './owners.js';
 import { supportedChainNames } from './supportedChainNames.js';
 
@@ -22,6 +30,25 @@ const remoteOverhead = (remote: ChainName) =>
         defaultMultisigConfigs[remote].validators.length,
       )
     : FOREIGN_DEFAULT_OVERHEAD; // non-ethereum overhead
+
+const testnetTokenValue = ethersUtils.parseUnits(
+  '1',
+  TOKEN_EXCHANGE_RATE_DECIMALS,
+);
+
+export const storageGasOracleConfig: AllStorageGasOracleConfigs =
+  getAllStorageGasOracleConfigs(
+    supportedChainNames,
+    gasPrices,
+    (local: ChainName, remote: ChainName): BigNumber =>
+      getTokenExchangeRateFromValues(
+        local,
+        testnetTokenValue,
+        remote,
+        testnetTokenValue,
+      ),
+    (local) => remoteOverhead(local),
+  );
 
 export const igp: ChainMap<IgpConfig> = objMap(
   owners,
