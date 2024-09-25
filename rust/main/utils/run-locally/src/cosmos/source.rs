aa678b85f70550578538927d97ebe1ff05052559
Daniel Savu <23065004+daniel-savu@users.noreply.github.com>
2024-09-25 23:37:25 +0100

    Update source.rs

--- rust/main/utils/run-locally/src/cosmos/source.rs
+++ rust/main/utils/run-locally/src/cosmos/source.rs
@@ -145,8 +145,7 @@ impl CLISource {
 
         log!("Uncompressing Osmosis release");
         untar(&release_comp, dir_path);
-
-        concat_path(dir_path, "osmosisd")
+        concat_path(dir_path, "release/osmosisd-26.0.1-linux-amd64")
     }
 
     pub fn install(self, dir: Option<PathBuf>) -> PathBuf {
