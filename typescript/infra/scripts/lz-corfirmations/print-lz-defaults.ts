1177a8185a6b77aa8befe3f545b00638b8357289
Mo Hussan <22501692+Mo-Hussain@users.noreply.github.com>
2024-09-26 14:14:53 -0400

    print stargate confirmations

--- typescript/infra/scripts/lz-corfirmations/print-lz-defaults.ts
+++ typescript/infra/scripts/lz-corfirmations/print-lz-defaults.ts
@@ -3,10 +3,11 @@ import { ethers } from 'ethers';
 
 import { MultiProvider } from '@hyperlane-xyz/sdk';
 
-import { getChain } from '../config/registry.js';
+import { getChain } from '../../config/registry.js';
+import { getEnvironmentConfig } from '../core-utils.js';
 
-import { getEnvironmentConfig } from './core-utils.js';
 import lzChainDeployments from './lz-chain-deployments.json';
+import stargateChainOApps from './stargate-chains.json';
 
 function translateLzChain(lzChain: string) {
   let translated = lzChain.replace('-', '');
@@ -28,16 +29,16 @@ async function main() {
   const envConfig = getEnvironmentConfig('mainnet3');
   const multiProvider = await envConfig.getMultiProvider();
 
-  const chainIntersection = Object.keys(lzChainDeployments).filter(
+  const chainIntersection = Object.keys(stargateChainOApps).filter(
     (lzChain) => {
       const lzChainTranslated = translateLzChain(lzChain);
       return envConfig.supportedChainNames.includes(lzChainTranslated);
     },
   );
-  const ourUnsupportedChains = Object.keys(lzChainDeployments).filter(
-    (lzChain) => {
-      const lzChainTranslated = translateLzChain(lzChain);
-      return !envConfig.supportedChainNames.includes(lzChainTranslated);
+  const ourUnsupportedChains = Object.keys(stargateChainOApps).filter(
+    (stargateChain) => {
+      const stargateChainTranslated = translateLzChain(stargateChain);
+      return !envConfig.supportedChainNames.includes(stargateChainTranslated);
     },
   );
 
@@ -48,56 +49,72 @@ async function main() {
   );
 
   const results = await Promise.all(
-    chainIntersection.map(async (lzChain) => {
-      const { hyperlaneConfirmations, lzConfirmations } =
-        await getHyperlaneAndLzConfirmations(multiProvider, lzChain);
-      const hyperlaneChain = translateLzChain(lzChain);
+    chainIntersection.map(async (stargateChain) => {
+      const { hyperlaneConfirmations, stargateConfirmations, lzConfirmations } =
+        await getHyperlaneAndStargateConfirmations(
+          multiProvider,
+          stargateChain,
+        );
+      const hyperlaneChain = translateLzChain(stargateChain);
       return {
-        hyperlaneChain,
-        lzChain,
+        chain: hyperlaneChain,
         hyperlaneConfirmations,
+        stargateConfirmations,
         lzConfirmations,
       };
     }),
   );
   console.table(results, [
-    'hyperlaneChain',
-    'lzChain',
+    'chain',
     'hyperlaneConfirmations',
+    'stargateConfirmations',
     'lzConfirmations',
   ]);
 }
 
-async function getHyperlaneAndLzConfirmations(
+async function getHyperlaneAndStargateConfirmations(
   multiProvider: MultiProvider,
-  lzChain: string,
+  stargateChain: string,
 ) {
-  const hyperlaneChain = translateLzChain(lzChain);
+  const hyperlaneChain = translateLzChain(stargateChain);
   const provider = multiProvider.getProvider(hyperlaneChain);
 
   const hyperlaneConfirmations =
     getChain(hyperlaneChain).blocks?.reorgPeriod ?? -1;
-  const lzConfirmations = await getLzDefaultConfirmations(provider, lzChain);
-  return { hyperlaneConfirmations, lzConfirmations };
+
+  const OAppAddress =
+    stargateChainOApps[stargateChain as keyof typeof stargateChainOApps][
+      'TokenMessaging'
+    ];
+
+  const stargateConfirmations = await getDefaultConfirmations(
+    provider,
+    stargateChain,
+    OAppAddress,
+  );
+
+  const lzConfirmations = await getDefaultConfirmations(
+    provider,
+    hyperlaneChain,
+    ethers.constants.AddressZero,
+  );
+
+  return { hyperlaneConfirmations, stargateConfirmations, lzConfirmations };
 }
 
-async function getLzDefaultConfirmations(
+async function getDefaultConfirmations(
   provider: Provider,
-  originLzChain: string,
+  originChain: string,
+  OAppAddress: string,
 ) {
-  const lzChainLookup = originLzChain as keyof typeof lzChainDeployments;
+  const lzChainLookup = originChain as keyof typeof lzChainDeployments;
   const sendLib = lzChainDeployments[lzChainLookup].sendUln302;
 
-  const endpoint = lzEndpointContract(provider, originLzChain);
-  // Just always choose arbitrum as the dest chain, unless the origin is arbitrum in which case choose astar
-  const destChain = originLzChain == 'arbitrum' ? 30210 : 30110;
+  const endpoint = lzEndpointContract(provider, originChain);
+  // Just always choose arbitrum as the dest chain, unless the origin is arbitrum in which case choose avalanche
+  const destChain = originChain == 'arbitrum' ? 30210 : 30106;
   // 2 for ulnconfig
-  const config = await endpoint.getConfig(
-    ethers.constants.AddressZero,
-    sendLib,
-    destChain,
-    2,
-  );
+  const config = await endpoint.getConfig(OAppAddress, sendLib, destChain, 2);
   const ulnConfigStructType = [
     'tuple(uint64 confirmations, uint8 requiredDVNCount, uint8 optionalDVNCount, uint8 optionalDVNThreshold, address[] requiredDVNs, address[] optionalDVNs)',
   ];
