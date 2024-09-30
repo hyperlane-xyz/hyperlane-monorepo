dcaecead02a11136bbb1b515f0fe9e0b1e73bf24
Morteza Shojaei <31728528+mortezashojaei@users.noreply.github.com>
2024-09-30 17:15:24 +0330

    minor: cleanup

--- typescript/cli/localSendMessage.sh
+++ typescript/cli/localSendMessage.sh
@@ -1,7 +1,1 @@
-yarn hyperlane send message -k 0x3d3cbc973389cb26f657686445bcc75662b415b656078503592ac8c1abb8810e -y --origin docker --destination zksynclocal --relay
-
-# sepolia pk
-# 2ec19c4bd799467912d36aa4f52e2b94704782243d19508c9aaec5a1c3b1c60c
-
-# rich wallet
-# 0x3d3cbc973389cb26f657686445bcc75662b415b656078503592ac8c1abb8810e
+yarn hyperlane send message -y --origin dockersync --destination zksynclocal --relay
