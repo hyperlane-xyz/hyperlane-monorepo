3f48f5a8ed5ed872ddbb5a592032a54352638f66
Paul Balaji <10051819+paulbalaji@users.noreply.github.com>
2024-10-17 16:36:05 +0100

    chore: add scroll gasPrice override (#4701)
    ### Description
    
    chore: add back the scroll gasPrice override
    
    relayer's unaffected because we override that to 0.8 gwei
    
    ### Drive-by changes
    
    update agent config
    
    ### Related issues
    
    key funder :'(
    
    ### Backward compatibility
    
    yes
    
    ### Testing
    
    manual?
    
    ---------
    
    Signed-off-by: pbio <10051819+paulbalaji@users.noreply.github.com>

--- typescript/infra/config/environments/mainnet3/funding.ts
+++ typescript/infra/config/environments/mainnet3/funding.ts
@@ -10,7 +10,7 @@ export const keyFunderConfig: KeyFunderConfig<
 > = {
   docker: {
     repo: 'gcr.io/abacus-labs-dev/hyperlane-monorepo',
-    tag: '6295693-20241017-095058',
+    tag: '436988a-20241017-151047',
   },
   // We're currently using the same deployer/key funder key as mainnet2.
   // To minimize nonce clobbering we offset the key funder cron
