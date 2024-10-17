024a67189a4a74999159b5639b3e04306c645dd5
pbio <10051819+paulbalaji@users.noreply.github.com>
2024-10-17 11:54:53 +0100

    add deploy-only overrides

--- typescript/infra/config/environments/mainnet3/chains.ts
+++ typescript/infra/config/environments/mainnet3/chains.ts
@@ -59,10 +59,23 @@ export const chainMetadataOverrides: ChainMap<Partial<ChainMetadata>> = {
       // gasLimit: 6800000, // set when deploying contracts
     },
   },
-  // set when deploying contracts
+  // Deploy-only overrides, set when deploying contracts
   // chiliz: {
   //   transactionOverrides: {
   //     maxFeePerGas: 100000 * 10 ** 9, // 100,000 gwei
+  //   },
+  // },
+  // scroll: {
+  //   transactionOverrides: {
+  //     // Scroll doesn't use EIP 1559 and the gas price that's returned is sometimes
+  //     // too low for the transaction to be included in a reasonable amount of time -
+  //     // this often leads to transaction underpriced issues.
+  //     gasPrice: 1 * 10 ** 9, // 1 gwei
+  //   },
+  // },
+  // zircuit: {
+  //   blocks: {
+  //     confirmations: 3,
   //   },
   // },
 };
