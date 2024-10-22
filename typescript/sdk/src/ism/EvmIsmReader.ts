86001f4e388388142c292ee3125fb4b648ba81f9
Le Yu <6251863+ltyu@users.noreply.github.com>
2024-10-22 15:12:11 -0400

    Remove retryAsync from ism and hook readers

--- typescript/sdk/src/ism/EvmIsmReader.ts
+++ typescript/sdk/src/ism/EvmIsmReader.ts
@@ -17,7 +17,6 @@ import {
   assert,
   concurrentMap,
   getLogLevel,
-  retryAsync,
   rootLogger,
 } from '@hyperlane-xyz/utils';
 
@@ -72,57 +71,55 @@ export class EvmIsmReader extends HyperlaneReader implements IsmReader {
   }
 
   async deriveIsmConfig(address: Address): Promise<DerivedIsmConfig> {
-    return retryAsync(async () => {
-      let moduleType: ModuleType | undefined = undefined;
-      let derivedIsmConfig: DerivedIsmConfig;
-      try {
-        const ism = IInterchainSecurityModule__factory.connect(
-          address,
-          this.provider,
-        );
-        this.logger.debug('Deriving IsmConfig:', { address });
+    let moduleType: ModuleType | undefined = undefined;
+    let derivedIsmConfig: DerivedIsmConfig;
+    try {
+      const ism = IInterchainSecurityModule__factory.connect(
+        address,
+        this.provider,
+      );
+      this.logger.debug('Deriving IsmConfig:', { address });
 
-        // Temporarily turn off SmartProvider logging
-        // Provider errors are expected because deriving will call methods that may not exist in the Bytecode
-        this.setSmartProviderLogLevel('silent');
-        moduleType = await ism.moduleType();
+      // Temporarily turn off SmartProvider logging
+      // Provider errors are expected because deriving will call methods that may not exist in the Bytecode
+      this.setSmartProviderLogLevel('silent');
+      moduleType = await ism.moduleType();
 
-        switch (moduleType) {
-          case ModuleType.UNUSED:
-            throw new Error('UNUSED does not have a corresponding IsmType');
-          case ModuleType.ROUTING:
-            // IsmType is either ROUTING or FALLBACK_ROUTING, but that's determined inside deriveRoutingConfig
-            derivedIsmConfig = await this.deriveRoutingConfig(address);
-            break;
-          case ModuleType.AGGREGATION:
-            derivedIsmConfig = await this.deriveAggregationConfig(address);
-            break;
-          case ModuleType.LEGACY_MULTISIG:
-            throw new Error('LEGACY_MULTISIG is deprecated and not supported');
-          case ModuleType.MERKLE_ROOT_MULTISIG:
-          case ModuleType.MESSAGE_ID_MULTISIG:
-            derivedIsmConfig = await this.deriveMultisigConfig(address);
-            break;
-          case ModuleType.NULL:
-            derivedIsmConfig = await this.deriveNullConfig(address);
-            break;
-          case ModuleType.CCIP_READ:
-            throw new Error('CCIP_READ does not have a corresponding IsmType');
-          case ModuleType.ARB_L2_TO_L1:
-            return this.deriveArbL2ToL1Config(address);
-          default:
-            throw new Error(`Unknown ISM ModuleType: ${moduleType}`);
-        }
-      } catch (e: any) {
-        const errorMessage = `Failed to derive ISM module type ${moduleType} on ${this.chain} (${address}) :\n\t${e}`;
-        this.logger.debug(errorMessage);
-        throw new Error(errorMessage);
-      } finally {
-        this.setSmartProviderLogLevel(getLogLevel()); // returns to original level defined by rootLogger
+      switch (moduleType) {
+        case ModuleType.UNUSED:
+          throw new Error('UNUSED does not have a corresponding IsmType');
+        case ModuleType.ROUTING:
+          // IsmType is either ROUTING or FALLBACK_ROUTING, but that's determined inside deriveRoutingConfig
+          derivedIsmConfig = await this.deriveRoutingConfig(address);
+          break;
+        case ModuleType.AGGREGATION:
+          derivedIsmConfig = await this.deriveAggregationConfig(address);
+          break;
+        case ModuleType.LEGACY_MULTISIG:
+          throw new Error('LEGACY_MULTISIG is deprecated and not supported');
+        case ModuleType.MERKLE_ROOT_MULTISIG:
+        case ModuleType.MESSAGE_ID_MULTISIG:
+          derivedIsmConfig = await this.deriveMultisigConfig(address);
+          break;
+        case ModuleType.NULL:
+          derivedIsmConfig = await this.deriveNullConfig(address);
+          break;
+        case ModuleType.CCIP_READ:
+          throw new Error('CCIP_READ does not have a corresponding IsmType');
+        case ModuleType.ARB_L2_TO_L1:
+          return this.deriveArbL2ToL1Config(address);
+        default:
+          throw new Error(`Unknown ISM ModuleType: ${moduleType}`);
       }
+    } catch (e: any) {
+      const errorMessage = `Failed to derive ISM module type ${moduleType} on ${this.chain} (${address}) :\n\t${e}`;
+      this.logger.debug(errorMessage);
+      throw new Error(errorMessage);
+    } finally {
+      this.setSmartProviderLogLevel(getLogLevel()); // returns to original level defined by rootLogger
+    }
 
-      return derivedIsmConfig;
-    });
+    return derivedIsmConfig;
   }
 
   async deriveRoutingConfig(
