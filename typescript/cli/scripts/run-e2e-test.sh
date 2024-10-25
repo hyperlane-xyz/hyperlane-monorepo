86178c9413f75ead725bc5b6cd416a8d1e160ab1
xeno097 <xeno097.cp@gmail.com>
2024-10-25 16:39:24 -0400

    test(cli): updated the test e2e script to also run a anvil 1 chain only for core command testing

--- typescript/cli/scripts/run-e2e-test.sh
+++ typescript/cli/scripts/run-e2e-test.sh
@@ -3,8 +3,10 @@
 function cleanup() {
   set +e
   pkill -f anvil
+  rm -rf /tmp/anvil1
   rm -rf /tmp/anvil2
   rm -rf /tmp/anvil3
+  rm -f ./test-configs/anvil/chains/anvil1/addresses.yaml
   rm -f ./test-configs/anvil/chains/anvil2/addresses.yaml
   rm -f ./test-configs/anvil/chains/anvil3/addresses.yaml
   set -e
@@ -12,7 +14,9 @@ function cleanup() {
 
 cleanup
 
-echo "Starting anvil2 and anvil3 chain for E2E tests"
+echo "Starting anvil1, anvil2 and anvil3 chain for E2E tests"
+# Anvil1 should be used only for core commands e2e testing to avoid interfierence with other e2e tests
+anvil --chain-id 31337 -p 8545 --state /tmp/anvil1/state --gas-price 1 > /dev/null &
 anvil --chain-id 31338 -p 8555 --state /tmp/anvil2/state --gas-price 1 > /dev/null &
 anvil --chain-id 31347 -p 8600 --state /tmp/anvil3/state --gas-price 1 > /dev/null &
 
