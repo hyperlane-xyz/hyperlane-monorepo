cb4be359801c8207d5e3152adc5111d3f8ce8bbb
Trevor Porter <tkporter4@gmail.com>
2024-10-25 16:24:11 +0100

    Fix

--- typescript/sdk/src/token/adapters/SealevelTokenAdapter.ts
+++ typescript/sdk/src/token/adapters/SealevelTokenAdapter.ts
@@ -18,7 +18,7 @@ import { deserializeUnchecked, serialize } from 'borsh';
 import {
   Address,
   Domain,
-  addressToBytes32,
+  addressToBytes,
   eqAddress,
   median,
 } from '@hyperlane-xyz/utils';
@@ -295,7 +295,7 @@ export abstract class SealevelHypTokenAdapter
       instruction: SealevelHypTokenInstruction.TransferRemote,
       data: new SealevelTransferRemoteInstruction({
         destination_domain: destination,
-        recipient: addressToBytes32(recipient),
+        recipient: addressToBytes(recipient),
         amount_or_id: BigInt(weiAmountOrId),
       }),
     });
