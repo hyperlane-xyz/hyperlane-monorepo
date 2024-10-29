d9505ab585637c8e7d2e31ee234fa968156f4c70
Paul Balaji <10051819+paulbalaji@users.noreply.github.com>
2024-10-29 11:21:43 +0000

    feat: oct 25 deployment batch (#4766)
    ### Description
    
    - feat: oct 25 deployment batch
    - apechain, arbitrumnova, b3, fantom, gravity, harmony, kaia, morph,
    orderly, snaxchain
    	- deployed as normal
    - zeronetwork, zksync
    - deployed from special zksync branch containing txfusion's changes +
    latest `main` pulled in
    	- https://github.com/hyperlane-xyz/hyperlane-monorepo/pull/4761
    
    ### Drive-by changes
    
    - igp updates
    
    ### Related issues
    
    n/a
    
    ### Backward compatibility
    
    yes
    
    ### Testing
    
    manual using local CLI from this branch, no tx fusion changes required
    for hyperlane send or self-relay to work

--- typescript/sdk/src/core/HyperlaneCore.ts
+++ typescript/sdk/src/core/HyperlaneCore.ts
@@ -42,6 +42,10 @@ import { CoreFactories, coreFactories } from './contracts.js';
 import { DispatchEvent } from './events.js';
 import { DispatchedMessage } from './types.js';
 
+// If no metadata is provided, ensure we provide a default of 0x0001.
+// We set to 0x0001 instead of 0x0 to ensure it does not break on zksync.
+const DEFAULT_METADATA = '0x0001';
+
 export class HyperlaneCore extends HyperlaneApp<CoreFactories> {
   static fromAddressesMap(
     addressesMap: HyperlaneAddressesMap<any>,
@@ -94,7 +98,7 @@ export class HyperlaneCore extends HyperlaneApp<CoreFactories> {
       destinationId,
       recipient,
       body,
-      metadata || '0x',
+      metadata || DEFAULT_METADATA,
       hook || ethers.constants.AddressZero,
     );
   };
@@ -159,7 +163,7 @@ export class HyperlaneCore extends HyperlaneApp<CoreFactories> {
       destinationDomain,
       recipientBytes32,
       body,
-      metadata || '0x',
+      metadata || DEFAULT_METADATA,
       hook || ethers.constants.AddressZero,
     ] as const;
 
