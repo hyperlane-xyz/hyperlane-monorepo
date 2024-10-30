9f2ebdbdae5713a5b1843cf66a0c17897f8bc8a8
Trevor Porter <tkporter4@gmail.com>
2024-10-30 13:59:24 +0000

    move things into keys dir

--- typescript/infra/scripts/keys/get-owner-ica.ts
+++ typescript/infra/scripts/keys/get-owner-ica.ts
@@ -1,10 +1,9 @@
 import { AccountConfig, InterchainAccount } from '@hyperlane-xyz/sdk';
 import { Address, eqAddress } from '@hyperlane-xyz/utils';
 
-import { isEthereumProtocolChain } from '../src/utils/utils.js';
-
-import { getArgs as getEnvArgs, withChains } from './agent-utils.js';
-import { getEnvironmentConfig, getHyperlaneCore } from './core-utils.js';
+import { isEthereumProtocolChain } from '../../src/utils/utils.js';
+import { getArgs as getEnvArgs, withChains } from '../agent-utils.js';
+import { getEnvironmentConfig, getHyperlaneCore } from '../core-utils.js';
 
 function getArgs() {
   return withChains(getEnvArgs())
--- typescript/infra/scripts/keys/update-key.ts
+++ typescript/infra/scripts/keys/update-key.ts
@@ -3,29 +3,26 @@ import {
   getArgs,
   withContext,
   withKeyRoleAndChain,
-} from './agent-utils.js';
+} from '../agent-utils.js';
 
 async function rotateKey() {
-  const argv = await withContext(withKeyRoleAndChain(getArgs())).argv;
+  const argv = await withKeyRoleAndChain(withContext(getArgs())).argv;
   const agentConfig = getAgentConfig(argv.context, argv.environment);
 
   switch (argv.environment) {
-    // TODO: re-implement this when the environments actually get readded
+    // TODO: Reimplement this when the environments get readded
     case 'test': {
       console.log("I don't do anything");
       console.log(argv, agentConfig);
     }
-    // case DeployEnvironment.dev: {
-    //   await rotateGCPKey(environment, argv.r, argv.c);
-    //   break;
-    // }
     // case DeployEnvironment.testnet:
     // case DeployEnvironment.mainnet:
     //   const key = new AgentAwsKey(agentConfig, argv.r, argv.c);
     //   await key.fetch();
     //   console.log(`Current key: ${key.address}`);
-    //   await key.rotate();
-    //   console.log(`Key was rotated to ${key.address}. `);
+    //   await key.update();
+    //   console.log(`Create new key with address: ${key.address}`);
+    //   console.log('Run rotate-key script to rotate the key via the alias.');
     //   break;
     // default: {
     //   throw new Error('invalid environment');
