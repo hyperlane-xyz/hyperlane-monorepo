d000dc2808e0f1c1ac8adfaeaa8062d99cb4036e
xeno097 <xeno097.cp@gmail.com>
2024-11-01 16:45:19 -0400

    fix(cli): re-enable space key for multiselect prompt

--- typescript/cli/src/utils/chains.ts
+++ typescript/cli/src/utils/chains.ts
@@ -90,7 +90,7 @@ export async function runMultiChainSelectionStep({
           ? { ...choice, checked: true }
           : choice,
       ),
-      instructions: `Use TAB key to select at least ${requireNumber} chains, then press ENTER to proceed. Type to search for a specific chain.`,
+      instructions: `Use the TAB or the SPACE key to select at least ${requireNumber} chains, then press ENTER to proceed. Type to search for a specific chain.`,
       theme: {
         style: {
           // The leading space is needed because the help tip will be tightly close to the message header
