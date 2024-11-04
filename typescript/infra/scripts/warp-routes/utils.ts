04c54810ff08759bcd6615731100b6c1f4b73503
Daniel Savu <23065004+daniel-savu@users.noreply.github.com>
2024-11-04 12:14:13 +0000

    chore: instantiate router config in warp generators

---
+++ typescript/infra/scripts/warp-routes/utils.ts
@@ -0,0 +1,30 @@
+import { Contexts } from '../../config/contexts.js';
+import { Role } from '../../src/roles.js';
+import {
+  getArgs,
+  withBuildArtifactPath,
+  withChains,
+  withConcurrentDeploy,
+  withContext,
+} from '../agent-utils.js';
+import { getEnvironmentConfig, getHyperlaneCore } from '../core-utils.js';
+
+export async function getRouterConfig() {
+  const {
+    context = Contexts.Hyperlane,
+    environment,
+    chains,
+  } = await withContext(
+    withConcurrentDeploy(withChains(withBuildArtifactPath(getArgs()))),
+  ).argv;
+  const envConfig = getEnvironmentConfig(environment);
+
+  let multiProvider = await envConfig.getMultiProvider(
+    context,
+    Role.Deployer,
+    true,
+    chains,
+  );
+  const { core } = await getHyperlaneCore(environment, multiProvider);
+  return core.getRouterConfig(envConfig.owners);
+}
