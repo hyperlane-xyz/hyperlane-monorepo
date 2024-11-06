f48cf87667534c237f649dfeea2af65410bb4370
J M Rossy <jm.rossy@gmail.com>
2024-11-06 17:08:16 -0500

    chore: Upgrade Viem and Solana Web3 (#4821)
    ### Description
    
    - Breaking change: Upgrade Viem from 1.x to 2.2
    - Upgrade Solana Web3 to 1.9
    - Upgrade Solana SPL Token to 0.4.9
    
    ### Related issues
    
    Required for
    https://github.com/hyperlane-xyz/hyperlane-warp-ui-template/issues/173
    
    
    ### Backward compatibility
    
    No
    
    ### Testing
    
    Tested EVM and Solana wallet connections and transfers in the Warp UI.
    See https://github.com/hyperlane-xyz/hyperlane-warp-ui-template/pull/315

--- typescript/sdk/src/index.ts
+++ typescript/sdk/src/index.ts
@@ -95,8 +95,8 @@ export {
 export { HyperlaneProxyFactoryDeployer } from './deploy/HyperlaneProxyFactoryDeployer.js';
 export {
   CheckerViolation,
-  OwnableConfig,
   DeployedOwnableConfig,
+  OwnableConfig,
   OwnerViolation,
   ProxyAdminViolation,
   ViolationType,
@@ -197,11 +197,11 @@ export {
 export {
   BlockExplorer,
   BlockExplorerSchema,
-  EthJsonRpcBlockParameterTag,
   ChainMetadata,
   ChainMetadataSchema,
   ChainMetadataSchemaObject,
   ChainTechnicalStack,
+  EthJsonRpcBlockParameterTag,
   ExplorerFamily,
   ExplorerFamilyValue,
   NativeToken,
@@ -390,12 +390,12 @@ export {
   MailboxClientConfig as ConnectionClientConfig,
   ClientViolation as ConnectionClientViolation,
   ClientViolationType as ConnectionClientViolationType,
+  DestinationGas,
   GasRouterConfig,
   MailboxClientConfig,
   ProxiedFactories,
   ProxiedRouterConfig,
   RemoteRouters,
-  DestinationGas,
   RouterAddress,
   RouterConfig,
   RouterViolation,
@@ -448,9 +448,9 @@ export { HypERC20Checker } from './token/checker.js';
 export { TokenType } from './token/config.js';
 export {
   HypERC20Factories,
-  hypERC20contracts,
   HypERC721Factories,
   TokenFactories,
+  hypERC20contracts,
   hypERC20factories,
 } from './token/contracts.js';
 export { HypERC20Deployer, HypERC721Deployer } from './token/deploy.js';
@@ -499,7 +499,7 @@ export {
   getSealevelAccountDataSchema,
 } from './utils/sealevelSerialization.js';
 export { getChainIdFromTxs } from './utils/transactions.js';
-export { chainMetadataToWagmiChain } from './utils/wagmi.js';
+export { chainMetadataToViemChain } from './utils/viem.js';
 export {
   FeeConstantConfig,
   RouteBlacklist,
@@ -541,8 +541,8 @@ export {
 
 export { EvmCoreModule } from './core/EvmCoreModule.js';
 export {
-  proxyAdmin,
   isProxy,
+  proxyAdmin,
   proxyConstructorArgs,
   proxyImplementation,
 } from './deploy/proxy.js';
@@ -550,16 +550,16 @@ export {
   ProxyFactoryFactoriesAddresses,
   ProxyFactoryFactoriesSchema,
 } from './deploy/schemas.js';
-export { EvmIsmModule } from './ism/EvmIsmModule.js';
-export { AnnotatedEV5Transaction } from './providers/ProviderType.js';
-export { EvmERC20WarpModule } from './token/EvmERC20WarpModule.js';
 export {
+  ChainGasOracleParams,
   GasPriceConfig,
   NativeTokenPriceConfig,
-  ChainGasOracleParams,
   getCoingeckoTokenPrices,
   getCosmosChainGasPrice,
   getGasPrice,
   getLocalStorageGasOracleConfig,
   getTokenExchangeRateFromValues,
 } from './gas/utils.js';
+export { EvmIsmModule } from './ism/EvmIsmModule.js';
+export { AnnotatedEV5Transaction } from './providers/ProviderType.js';
+export { EvmERC20WarpModule } from './token/EvmERC20WarpModule.js';
