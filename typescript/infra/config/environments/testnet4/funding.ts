19dadf42affee2ab36b3ba27fbf6733644018e36
pbio <10051819+paulbalaji@users.noreply.github.com>
2024-11-07 19:44:21 +0000

    deploy agents/kf + fix val-announce script

--- typescript/infra/config/environments/testnet4/funding.ts
+++ typescript/infra/config/environments/testnet4/funding.ts
@@ -10,7 +10,7 @@ export const keyFunderConfig: KeyFunderConfig<
 > = {
   docker: {
     repo: 'gcr.io/abacus-labs-dev/hyperlane-monorepo',
-    tag: 'b8add3c-20241106-190239',
+    tag: 'bb4e82b-20241107-190423',
   },
   // We're currently using the same deployer key as testnet2.
   // To minimize nonce clobbering we offset the key funder cron
