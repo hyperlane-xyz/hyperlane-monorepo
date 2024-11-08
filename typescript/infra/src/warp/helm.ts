5deee08a06cf912e55a92b89e119ee1660f47023
Trevor Porter <tkporter4@gmail.com>
2024-11-08 19:16:56 +0000

    update image

--- typescript/infra/src/warp/helm.ts
+++ typescript/infra/src/warp/helm.ts
@@ -22,7 +22,7 @@ export class WarpRouteMonitorHelmManager extends HelmManager {
     return {
       image: {
         repository: 'gcr.io/abacus-labs-dev/hyperlane-monorepo',
-        tag: 'dfcfe08-20241107-194045',
+        tag: 'ca0df95-20241108-181740',
       },
       warpRouteId: this.warpRouteId,
       fullnameOverride: this.helmReleaseName,
