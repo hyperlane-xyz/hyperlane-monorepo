58257e596a2b7d59516159cc017fd47dd179350f
Trevor Porter <tkporter4@gmail.com>
2024-11-14 09:58:26 +0000

    wip

--- typescript/infra/config/environments/mainnet3/warp/configGetters/getEclipseEthereumWeETHsWarpConfig.ts
+++ typescript/infra/config/environments/mainnet3/warp/configGetters/getEclipseEthereumWeETHsWarpConfig.ts
@@ -8,7 +8,12 @@ import {
 } from '@hyperlane-xyz/sdk';
 
 import { tokens } from '../../../../../src/config/warp.js';
-import { DEPLOYER } from '../../owners.js';
+import { safes } from '../../owners.js';
+
+// Safe owned by Veda
+const ethereumOwner = '0xCEA8039076E35a825854c5C2f85659430b06ec96';
+// Vault owned by Veda
+const eclipseOwner = '4Cj1s2ipALjJk9foQV4oDaZYCZwSsVkAShQL1KFVJG9b';
 
 export const getEclipseEthereumWeEthsWarpConfig = async (
   routerConfig: ChainMap<RouterConfig>,
@@ -19,6 +24,7 @@ export const getEclipseEthereumWeEthsWarpConfig = async (
     foreignDeployment: '7Zx4wU1QAw98MfvnPFqRh1oyumek7G5VAX6TKB3U1tcn',
     gas: 300_000,
     interchainSecurityModule: ethers.constants.AddressZero,
+    owner: eclipseOwner,
   };
 
   let ethereum: TokenRouterConfig = {
@@ -26,8 +32,18 @@ export const getEclipseEthereumWeEthsWarpConfig = async (
     type: TokenType.collateral,
     token: tokens.ethereum.weETHs,
     interchainSecurityModule: ethers.constants.AddressZero,
-    owner: DEPLOYER,
+    owner: ethereumOwner,
+    ownerOverrides: {
+      proxyAdmin: ethereumOwner,
+      _safeAddress: safes['ethereum'],
+    },
+    proxyAdmin: {
+      address: '0x2ffc8e94eddda8356f6b66aa035b42b20cf24a08',
+      owner: ethereumOwner,
+    },
   };
+
+  console.log('ethereum?', ethereum);
 
   return {
     eclipsemainnet,
