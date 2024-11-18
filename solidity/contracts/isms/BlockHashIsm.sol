57d0ae6d84b3b9c9c0a87012f42d15ea22b8a929
JulissaDantes <julissadcj@gmail.com>
2024-11-18 17:31:28 -0400

    feat: update BlockHashISM contract

--- solidity/contracts/isms/BlockHashIsm.sol
+++ solidity/contracts/isms/BlockHashIsm.sol
@@ -3,6 +3,7 @@ pragma solidity >=0.8.0;
 
 import {IInterchainSecurityModule} from "../interfaces/IInterchainSecurityModule.sol";
 import {PackageVersioned} from "contracts/PackageVersioned.sol";
+import {Message} from "../libs/Message.sol";
 
 interface IBlockHashOracle {
     uint32 public immutable origin;
@@ -11,20 +12,24 @@ interface IBlockHashOracle {
 }
 
 contract BlockHashISM is IInterchainSecurityModule, PackageVersioned {
-    uint8 public constant override moduleType = uint8(Types.NULL); //TODO
+    using Message for bytes;
+    uint8 public constant override moduleType = uint8(Types.NULL);
     IBlockHashOracle public immutable oracle;
 
     /**
      * @inheritdoc IInterchainSecurityModule
-     * @notice Verifies whether a message was dispatched on the origin chain using a block hash oracle.
+     * @notice Verifies whether a message was dispatched on the origin chain using a block hash oracle against the
+     * block hash contained inside the message.
+     * @dev The `message` parameter must be ABI-encoded with the `blockHash` (as `uint256`) and `blockHeight` (as `uint256`)
+     * as the first two parameters, followed by any additional data.
+     * @param message Message to verify.
      */
     function verify(
-        bytes calldata metadata,
+        bytes calldata,
         bytes calldata message
     ) public pure override returns (bool) {
-        (bytes32 blockHash, uint256 blockHeight) = abi.decode(
-            metadata,
-            (bytes32, uint256)
+        (uint256 blockHash, uint256 blockHeight) = _extractBlockInfo(
+            message.body()
         );
 
         // if the block hash at the specified height does not match the oracle results means the transaction was not mined on that origin chain
@@ -32,35 +37,15 @@ contract BlockHashISM is IInterchainSecurityModule, PackageVersioned {
             oracle.blockhash(blockHeight) == blockHash,
             "Transaction not dispatched from origin chain"
         );
-        // TODO use the rlp decoder for solidity maybe why
-        return true; // TODO
+
+        return true;
     }
-    //TODO how could an untrusted relayer be verified here, or how can we avoid an unstruted relayer messing around
+
+    function _extractBlockInfo(
+        bytes calldata _messageBody
+    ) internal returns (uint256 hash, uint256 height) {
+        require(_messageBody.length >= 64, "Invalid message body");
+
+        (hash, height) = abi.decode(_messageBody[:64], (uint256, uint256));
+    }
 }
-
-/*
-Assume you have some magical origin chain block hash oracle on the destination chain.
-
-```solidity
-interface IBlockHashOracle {
-		uint32 public immutable origin;
-		function blockhash(uint256 height) external view returns (uint256 hash);
-}
-```
-
-Implement a `BlockHashISM` that verifies whether a message was dispatched on the origin chain using this oracle. You can assume that the origin chain uses the [ethereum block format](https://ethereum.org/en/developers/docs/blocks/#block-anatomy). 
-
-When you’re done implementing OptimisticISM, please **create a PR on your forked repo** merging `solidity-challenge` back into `main` and reach back out with a link to this PR. 
-
-We expect this challenge to take a few hours to complete, but feel free to spend as little or as much time as you like. As always, please do not hesitate to reach out if you have any questions or feedback.
-
-**Assumptions:**
-
-- BlockHashOracle is trusted
-- Relayers are untrusted
-
-**Design goals:**
-
-- Simplicity
-- Gas efficiency
-*/
