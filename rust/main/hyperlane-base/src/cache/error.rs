5a1387390c87aaa923e0eb22fb43608e777db1d9
Mantas M. <mantasm.dev@gmail.com>
2024-11-18 19:24:58 +0100

    fix(cache-error): spelling

--- rust/main/hyperlane-base/src/cache/error.rs
+++ rust/main/hyperlane-base/src/cache/error.rs
@@ -3,10 +3,10 @@
 pub enum CacheError {
     /// Error when key or value serialization fails.
     #[error("Failed to serialize input: {0}")]
-    FailedToSerializeInuput(#[source] serde_json::Error),
+    FailedToSerializeInput(#[source] serde_json::Error),
 
-    /// Error when enitity fetched from cache is deserialized incorrectly.
-    /// Most of the time this can be caused due the missmatch of the type
+    /// Error when entity fetched from cache is deserialized incorrectly.
+    /// Most of the time this can be caused due the mismatch of the type
     /// expected vs actual type of the entity.
     #[error("Failed to deserialize output: {0}")]
     FailedToDeserializeOutput(#[source] serde_json::Error),
