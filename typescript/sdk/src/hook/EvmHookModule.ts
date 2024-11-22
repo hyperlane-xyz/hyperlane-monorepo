66784dfbb45c3232a4dece2145a8caaf3860d8e1
Le Yu <6251863+ltyu@users.noreply.github.com>
2024-11-22 16:29:09 -0500

    Update coreAddresses Type

--- typescript/sdk/src/hook/EvmHookModule.ts
+++ typescript/sdk/src/hook/EvmHookModule.ts
@@ -243,7 +243,7 @@ export class EvmHookModule extends HyperlaneModule<
     chain: ChainNameOrId;
     config: HookConfig;
     proxyFactoryFactories: HyperlaneAddresses<ProxyFactoryFactories>;
-    coreAddresses: CoreAddresses;
+    coreAddresses: Omit<CoreAddresses, 'validatorAnnounce'>;
     multiProvider: MultiProvider;
     contractVerifier?: ContractVerifier;
   }): Promise<EvmHookModule> {
