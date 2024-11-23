f36d5ab08739a5674512f22f36b9c5b5c17f74ed
Le Yu <6251863+ltyu@users.noreply.github.com>
2024-11-23 12:23:21 -0500

    Add setDispatchFee

--- solidity/contracts/hooks/DispatchedHook.sol
+++ solidity/contracts/hooks/DispatchedHook.sol
@@ -4,19 +4,29 @@ pragma solidity ^0.8.13;
 import {AbstractPostDispatchHook} from "./libs/AbstractPostDispatchHook.sol";
 import {IPostDispatchHook} from "../interfaces/hooks/IPostDispatchHook.sol";
 import {Message} from "../libs/Message.sol";
+import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 
 /**
  * @title DispatchedHook
  * @notice Hook that updates a mapping to keep track of dispatched messages
  */
-contract DispatchedHook is AbstractPostDispatchHook {
+contract DispatchedHook is AbstractPostDispatchHook, OwnableUpgradeable {
     using Message for bytes;
 
+    uint256 public dispatchFee = 0;
     mapping(uint256 messageNonce => bytes32 messageId) public dispatched;
 
     /// @inheritdoc IPostDispatchHook
     function hookType() external pure returns (uint8) {
         return uint8(IPostDispatchHook.Types.DISPATCHED);
+    }
+
+    /**
+     * @notice Sets the dispatch fee to the provided value.
+     * @param _dispatchFee The new dispatch fee value.
+     */
+    function setDispatchFee(uint256 _dispatchFee) external onlyOwner {
+        dispatchFee = _dispatchFee;
     }
 
     /**
@@ -35,6 +45,6 @@ contract DispatchedHook is AbstractPostDispatchHook {
         bytes calldata,
         bytes calldata
     ) internal view virtual override returns (uint256) {
-        return 0;
+        return dispatchFee;
     }
 }
