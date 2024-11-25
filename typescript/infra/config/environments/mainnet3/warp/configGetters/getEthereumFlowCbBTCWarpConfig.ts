24c96785f1a600d6f9c9ab73f1ea91b778b28082
Trevor Porter <tkporter4@gmail.com>
2024-11-25 12:32:47 +0000

    nit

--- typescript/infra/config/environments/mainnet3/warp/configGetters/getEthereumFlowCbBTCWarpConfig.ts
+++ typescript/infra/config/environments/mainnet3/warp/configGetters/getEthereumFlowCbBTCWarpConfig.ts
@@ -12,11 +12,12 @@ import {
   RouterConfigWithoutOwner,
   tokens,
 } from '../../../../../src/config/warp.js';
-import { DEPLOYER } from '../../owners.js';
 
+// Flow team Safe
 const ethereumOwner = '0x58C3FB862a4F5f038C24F8506BE378e9415c5B6C';
 const ethereumOwnerConfig = getOwnerConfigForAddress(ethereumOwner);
 
+// Flow team Safe
 const flowOwner = '0xa507DFccA02727B46cBdC600C57E89b2b55E5330';
 const flowOwnerConfig = getOwnerConfigForAddress(flowOwner);
 
