c473b61e41a6e6a69569222951c93fee77beddd2
Daniel Savu <23065004+daniel-savu@users.noreply.github.com>
2024-12-04 11:48:48 +0000

    chore: use escalator image (#4943)
    ### Description
    
    <!--
    What's included in this PR?
    -->
    
    ### Drive-by changes
    
    <!--
    Are there any minor or drive-by changes also included?
    -->
    
    ### Related issues
    
    <!--
    - Fixes #[issue number here]
    -->
    
    ### Backward compatibility
    
    <!--
    Are these changes backward compatible? Are there any infrastructure
    implications, e.g. changes that would prohibit deploying older commits
    using this infra tooling?
    
    Yes/No
    -->
    
    ### Testing
    
    <!--
    What kind of testing have these changes undergone?
    
    None/Manual/Unit Tests
    -->

--- typescript/infra/config/environments/mainnet3/agent.ts
+++ typescript/infra/config/environments/mainnet3/agent.ts
@@ -460,7 +460,7 @@ const hyperlane: RootAgentConfig = {
     rpcConsensusType: RpcConsensusType.Fallback,
     docker: {
       repo,
-      tag: 'b35c105-20241128-172842',
+      tag: 'd0e53f5-20241203-171412',
     },
     gasPaymentEnforcement: gasPaymentEnforcement,
     metricAppContextsGetter,
@@ -494,7 +494,7 @@ const releaseCandidate: RootAgentConfig = {
     rpcConsensusType: RpcConsensusType.Fallback,
     docker: {
       repo,
-      tag: '25a927d-20241114-171323',
+      tag: 'd0e53f5-20241203-171412',
     },
     // We're temporarily (ab)using the RC relayer as a way to increase
     // message throughput.
