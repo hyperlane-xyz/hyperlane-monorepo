9fcc4d9dc4b8e65c0c10308afa9b1296b83ca2ac
-f <kunalarora1729@gmail.com>
2024-12-13 16:24:36 +0530

    rm comment

--- solidity/contracts/token/HypNative.sol
+++ solidity/contracts/token/HypNative.sol
@@ -76,7 +76,6 @@ contract HypNative is TokenRouter {
             _hookMetadata,
             _hook
         );
-        // require(msg.value >= _amount + quote, "HypNative: insufficient value");
 
         bytes memory hookMetadata = StandardHookMetadata.overrideMsgValue(
             _hookMetadata,
