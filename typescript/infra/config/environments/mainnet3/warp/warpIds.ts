8b87a225ff68a8f0515e5cdf8ee74a1bd39f54b4
Le Yu <6251863+ltyu@users.noreply.github.com>
2024-12-16 15:01:17 -0500

    Add RE7 config getter

--- typescript/infra/config/environments/mainnet3/warp/warpIds.ts
+++ typescript/infra/config/environments/mainnet3/warp/warpIds.ts
@@ -33,4 +33,5 @@ export enum WarpRouteIds {
   ArbitrumEthereumMantleModePolygonScrollZeroNetworkUSDT = 'USDT/arbitrum-ethereum-mantle-mode-polygon-scroll-zeronetwork',
   ArbitrumBaseEthereumLiskOptimismPolygonZeroNetworkUSDC = 'USDC/arbitrum-base-ethereum-lisk-optimism-polygon-zeronetwork',
   ArbitrumBaseBlastBscEthereumGnosisLiskMantleModeOptimismPolygonScrollZeroNetworkZoraMainnet = 'ETH/arbitrum-base-blast-bsc-ethereum-gnosis-lisk-mantle-mode-optimism-polygon-scroll-zeronetwork-zoramainnet',
+  EthereumZircuitRe7LRT = 'Re7LRT/ethereum-zircuit',
 }
