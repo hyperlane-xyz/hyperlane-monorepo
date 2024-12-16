0c274566d00b75f911e5d9dbebcb925232c312df
Le Yu <6251863+ltyu@users.noreply.github.com>
2024-12-16 16:37:22 -0500

    Add getter

---
+++ typescript/infra/config/environments/mainnet3/warp/configGetters/getEthereumBaseJackieChainWarpConfig.ts
@@ -0,0 +1,40 @@
+import { ethers } from 'ethers';
+
+import {
+  ChainMap,
+  HypTokenRouterConfig,
+  OwnableConfig,
+  TokenType,
+} from '@hyperlane-xyz/sdk';
+
+import {
+  RouterConfigWithoutOwner,
+  tokens,
+} from '../../../../../src/config/warp.js';
+
+const ISM_CONFIG = ethers.constants.AddressZero; // Default ISM
+
+export const getBasezeronetworkRe7LRTWarpConfig = async (
+  routerConfig: ChainMap<RouterConfigWithoutOwner>,
+  abacusWorksEnvOwnerConfig: ChainMap<OwnableConfig>,
+): Promise<ChainMap<HypTokenRouterConfig>> => {
+  const base: HypTokenRouterConfig = {
+    ...routerConfig.base,
+    ...abacusWorksEnvOwnerConfig.base,
+    type: TokenType.collateral,
+    token: tokens.base.jackieChain,
+    interchainSecurityModule: ISM_CONFIG,
+  };
+
+  const zeronetwork: HypTokenRouterConfig = {
+    ...routerConfig.zeronetwork,
+    ...abacusWorksEnvOwnerConfig.zeronetwork,
+    type: TokenType.synthetic,
+    interchainSecurityModule: ISM_CONFIG,
+  };
+
+  return {
+    base,
+    zeronetwork,
+  };
+};
