8ae9e6e78196d0b2d97eaac2449380e614856e77
-f <kunalarora1729@gmail.com>
2024-12-21 12:01:20 +0530

    update tag

--- typescript/infra/src/warp/helm.ts
+++ typescript/infra/src/warp/helm.ts
@@ -28,7 +28,7 @@ export class WarpRouteMonitorHelmManager extends HelmManager {
     return {
       image: {
         repository: 'gcr.io/abacus-labs-dev/hyperlane-monorepo',
-        tag: 'ecc5c1f-20241217-135735',
+        tag: '3cacafd-20241220-092417',
       },
       warpRouteId: this.warpRouteId,
       fullnameOverride: this.helmReleaseName,
