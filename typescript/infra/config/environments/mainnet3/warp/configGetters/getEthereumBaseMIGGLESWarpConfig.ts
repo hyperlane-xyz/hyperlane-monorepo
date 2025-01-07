2d76ebdb30726ec6eef37b063e2bb62d76796689
Le Yu <6251863+ltyu@users.noreply.github.com>
2025-01-07 17:56:04 -0500

    Add Miggles ProxyAdmin

--- typescript/infra/config/environments/mainnet3/warp/configGetters/getEthereumBaseMIGGLESWarpConfig.ts
+++ typescript/infra/config/environments/mainnet3/warp/configGetters/getEthereumBaseMIGGLESWarpConfig.ts
@@ -20,7 +20,11 @@ export const getBaseZeronetworkMigglesConfig = async (
 ): Promise<ChainMap<HypTokenRouterConfig>> => {
   const base: HypTokenRouterConfig = {
     ...routerConfig.base,
-    ...abacusWorksEnvOwnerConfig.base,
+    owner: abacusWorksEnvOwnerConfig.base.owner,
+    proxyAdmin: {
+      owner: abacusWorksEnvOwnerConfig.base.owner,
+      address: '0xAb15bf761276d4a9aaaB14B100c921B417D07BB2',
+    },
     type: TokenType.collateral,
     token: tokens.base.miggles,
     interchainSecurityModule: ISM_CONFIG,
@@ -28,7 +32,11 @@ export const getBaseZeronetworkMigglesConfig = async (
 
   const zeronetwork: HypTokenRouterConfig = {
     ...routerConfig.zeronetwork,
-    ...abacusWorksEnvOwnerConfig.zeronetwork,
+    owner: abacusWorksEnvOwnerConfig.zeronetwork.owner,
+    proxyAdmin: {
+      owner: abacusWorksEnvOwnerConfig.base.owner,
+      address: '0xc703DcF5ceaf33214dA115E1c86977A9b5590B59',
+    },
     type: TokenType.synthetic,
     interchainSecurityModule: ISM_CONFIG,
   };
