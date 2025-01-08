b1344086fbd0a778f56080999b8f07bcf7334dd7
Le Yu <6251863+ltyu@users.noreply.github.com>
2025-01-08 13:58:55 -0500

    Add ProxyAdmin blocks to base and zero

--- typescript/infra/config/environments/mainnet3/warp/configGetters/getEthereumBaseBRETTWarpConfig.ts
+++ typescript/infra/config/environments/mainnet3/warp/configGetters/getEthereumBaseBRETTWarpConfig.ts
@@ -21,6 +21,10 @@ export const getBaseZeronetworkBrettConfig = async (
   const base: HypTokenRouterConfig = {
     ...routerConfig.base,
     owner: abacusWorksEnvOwnerConfig.base.owner,
+    proxyAdmin: {
+      owner: abacusWorksEnvOwnerConfig.base.owner,
+      address: '0xdD702861AB97419858ccc85eDa4765e26D955d88',
+    },
     type: TokenType.collateral,
     token: tokens.base.brett,
     interchainSecurityModule: ISM_CONFIG,
@@ -29,6 +33,10 @@ export const getBaseZeronetworkBrettConfig = async (
   const zeronetwork: HypTokenRouterConfig = {
     ...routerConfig.zeronetwork,
     owner: abacusWorksEnvOwnerConfig.zeronetwork.owner,
+    proxyAdmin: {
+      owner: abacusWorksEnvOwnerConfig.zeronetwork.owner,
+      address: '0x4Ae15e875BDf5956D5c345A21AD83A20FD0692E6',
+    },
     type: TokenType.synthetic,
     interchainSecurityModule: ISM_CONFIG,
   };
