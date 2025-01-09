ea8fbb9a897142e9daea713fcc83337c8ba75ddf
Le Yu <6251863+ltyu@users.noreply.github.com>
2025-01-09 16:13:42 -0500

    Add helm for ink

--- typescript/infra/src/warp/helm.ts
+++ typescript/infra/src/warp/helm.ts
@@ -28,7 +28,7 @@ export class WarpRouteMonitorHelmManager extends HelmManager {
     return {
       image: {
         repository: 'gcr.io/abacus-labs-dev/hyperlane-monorepo',
-        tag: 'de11906-20241227-214834',
+        tag: '6da7f6f-20250109-210231',
       },
       warpRouteId: this.warpRouteId,
       fullnameOverride: this.helmReleaseName,
