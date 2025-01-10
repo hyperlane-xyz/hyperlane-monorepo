6c28235016c0a4fc48682d0c1e17a77c5dc875fe
Le Yu <6251863+ltyu@users.noreply.github.com>
2025-01-10 12:06:57 -0500

    Revert "Move functions around in attempt to reduce diffs"
    This reverts commit f46f2df02c37b302e3096b45d321ff2bef705f40.

--- typescript/infra/config/environments/mainnet3/warp/configGetters/getRenzoEZETHWarpConfig.ts
+++ typescript/infra/config/environments/mainnet3/warp/configGetters/getRenzoEZETHWarpConfig.ts
@@ -279,6 +279,11 @@ const existingProxyAdmins: ChainMap<{ address: string; owner: string }> = {
   },
 };
 
+export const getRenzoEZETHWarpConfig = getRenzoEZETHWarpConfigGenerator(
+  ezEthSafes,
+  xERC20,
+);
+
 export function getRenzoEZETHWarpConfigGenerator(
   ezEthSafes: Record<string, string>,
   xERC20: Record<(typeof chainsToDeploy)[number], string>,
@@ -381,8 +386,3 @@ export function getRenzoEZETHWarpConfigGenerator(
     return tokenConfig;
   };
 }
-
-export const getRenzoEZETHWarpConfig = getRenzoEZETHWarpConfigGenerator(
-  ezEthSafes,
-  xERC20,
-);
