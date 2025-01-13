3ce493a7515edb6551ff8388c9698524e76386fc
Yorke Rhodes <yorke@hyperlane.xyz>
2025-01-13 15:22:12 -0500

    Leverage caching middleware

--- typescript/infra/scripts/http-registry.ts
+++ typescript/infra/scripts/http-registry.ts
@@ -8,13 +8,10 @@ import { getArgs } from './agent-utils.js';
 async function main() {
   const { environment } = await getArgs().argv;
 
-  const registry =
-    environment === 'mainnet3'
-      ? await getMainnet3Registry()
-      : await getTestnet4Registry();
+  const getRegistry =
+    environment === 'mainnet3' ? getMainnet3Registry : getTestnet4Registry;
 
-  const server = new HttpServer(registry);
-  await server.start();
+  new HttpServer(getRegistry).start(3000, 5 * 60 * 1000);
 }
 
 main()
