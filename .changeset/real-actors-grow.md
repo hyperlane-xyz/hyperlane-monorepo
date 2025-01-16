69a684869c44e90d4d2ffaf88b6fad51d97e899f
Trevor Porter <tkporter4@gmail.com>
2025-01-16 11:10:32 +0000

    feat: TONY warp deploy, fix CLI warp deploys with non-EVM chains (#5186)
    ### Description
    
    - Deploys TONY
    - There was an issue with a non-EVM chain being in the warp deploy
    config. I made the fix but ultimately we should have a unit test to
    prevent this. Would prefer someone closer to the feature to do this, I
    just did a band aid fix
    
    ### Drive-by changes
    
    <!--
    Are there any minor or drive-by changes also included?
    -->
    
    ### Related issues
    
    <!--
    - Fixes #[issue number here]
    -->
    
    ### Backward compatibility
    
    <!--
    Are these changes backward compatible? Are there any infrastructure
    implications, e.g. changes that would prohibit deploying older commits
    using this infra tooling?
    
    Yes/No
    -->
    
    ### Testing
    
    <!--
    What kind of testing have these changes undergone?
    
    None/Manual/Unit Tests
    -->

---
+++ .changeset/real-actors-grow.md
@@ -0,0 +1,5 @@
+---
+'@hyperlane-xyz/sdk': minor
+---
+
+Don't try to build signers for non-EVM chains in MultiProtocolSignerManager
--- typescript/infra/config/warp.ts
+++ typescript/infra/config/warp.ts
@@ -24,6 +24,7 @@ import { getArtelaBaseUSDCWarpConfig } from './environments/mainnet3/warp/config
 import { getArtelaBaseWETHWarpConfig } from './environments/mainnet3/warp/configGetters/getArtelaBaseWETHWarpConfig.js';
 import { getBaseFormAIXBTWarpConfig } from './environments/mainnet3/warp/configGetters/getBaseFormAIXBTWarpConfig.js';
 import { getBaseFormGAMEWarpConfig } from './environments/mainnet3/warp/configGetters/getBaseFormGAMEWarpConfig.js';
+import { getBaseSolanamainnetTONYWarpConfig } from './environments/mainnet3/warp/configGetters/getBaseSolanamainnetTONYWarpConfig.js';
 import { getBaseZeroNetworkCBBTCWarpConfig } from './environments/mainnet3/warp/configGetters/getBaseZeroNetworkCBBTCWarpConfig.js';
 import { getBobaBsquaredSwellUBTCWarpConfig } from './environments/mainnet3/warp/configGetters/getBobaBsquaredSwellUBTCWarpConfig.js';
 import { getBscEthereumLumiaPrismPNDRWarpConfig } from './environments/mainnet3/warp/configGetters/getBscEthereumLumiaPNDRWarpConfig.js';
@@ -75,6 +76,7 @@ export const warpConfigGetterMap: Record<string, WarpConfigGetter> = {
   [WarpRouteIds.ArbitrumBaseBlastBscEthereumFraxtalLineaModeOptimismSeiSwellTaikoZircuitEZETH]:
     getRenzoEZETHWarpConfig,
   [WarpRouteIds.InevmInjectiveINJ]: getInevmInjectiveINJWarpConfig,
+  [WarpRouteIds.BaseSolanamainnetTONY]: getBaseSolanamainnetTONYWarpConfig,
   [WarpRouteIds.BscEthereumLumiaPrismPNDR]:
     getBscEthereumLumiaPrismPNDRWarpConfig,
   [WarpRouteIds.EthereumFlowCbBTC]: getEthereumFlowCbBTCWarpConfig,
