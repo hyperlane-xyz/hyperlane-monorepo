f3711ccd67b00f7f5409788757e9771458105862
kamiyaa <jeff.no.zhao@gmail.com>
2025-01-16 21:41:49 +0800

    feat: improve scraper startup error handling
     - set chain metrics if scraper fails for chain during startup

--- rust/main/agents/scraper/src/db/mod.rs
+++ rust/main/agents/scraper/src/db/mod.rs
@@ -3,7 +3,7 @@ pub use block_cursor::BlockCursor;
 use eyre::Result;
 pub use message::*;
 pub use payment::*;
-use sea_orm::{Database, DbConn};
+use sea_orm::{Database, DatabaseConnection, DbConn};
 use tracing::instrument;
 pub use txn::*;
 
@@ -19,7 +19,7 @@ mod txn;
 
 /// Database interface to the message explorer database for the scraper. This is
 /// focused on writing data to the database.
-#[derive(Clone, Debug)]
+#[derive(Debug)]
 pub struct ScraperDb(DbConn);
 
 impl ScraperDb {
@@ -27,5 +27,32 @@ impl ScraperDb {
     pub async fn connect(url: &str) -> Result<Self> {
         let db = Database::connect(url).await?;
         Ok(Self(db))
+    }
+
+    #[cfg(test)]
+    pub fn with_connection(db: DbConn) -> Self {
+        Self(db)
+    }
+
+    pub fn clone_connection(&self) -> DbConn {
+        match &self.0 {
+            DatabaseConnection::SqlxPostgresPoolConnection(conn) => {
+                DatabaseConnection::SqlxPostgresPoolConnection(conn.clone())
+            }
+            DatabaseConnection::Disconnected => DatabaseConnection::Disconnected,
+            DatabaseConnection::MockDatabaseConnection(conn) => {
+                DatabaseConnection::MockDatabaseConnection(conn.clone())
+            }
+        }
+    }
+}
+
+/// Not sure why Seaorm's DatabaseConnection does not #[derive(Clone)]
+/// when "mock" feature is enabled.
+/// So we have to implement our own clone instead of #[derive(Clone)]
+impl Clone for ScraperDb {
+    fn clone(&self) -> Self {
+        let conn = self.clone_connection();
+        Self(conn)
     }
 }
