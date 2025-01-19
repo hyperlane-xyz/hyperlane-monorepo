36f35ac789a581f92570eec80fe60a5579115750
nambrot <nambrot@googlemail.com>
2025-01-19 13:56:28 -0500

    Update docker image for monitor

--- typescript/infra/src/warp/helm.ts
+++ typescript/infra/src/warp/helm.ts
@@ -28,7 +28,7 @@ export class WarpRouteMonitorHelmManager extends HelmManager {
     return {
       image: {
         repository: 'gcr.io/abacus-labs-dev/hyperlane-monorepo',
-        tag: 'c080878-20250116-173913',
+        tag: '39f5a65-20250119-140452',
       },
       warpRouteId: this.warpRouteId,
       fullnameOverride: this.helmReleaseName,
