0054186b1ceefb752e128cf2f2f2506d6d5af2e2
-f <kunalarora1729@gmail.com>
2025-01-20 21:28:01 +0530

    trevor doesn't like &&

--- rust/main/utils/run-locally/src/invariants/termination_invariants.rs
+++ rust/main/utils/run-locally/src/invariants/termination_invariants.rs
@@ -158,8 +158,8 @@ pub fn termination_invariants_met(
     // check for each origin that the highest tree index seen by the syncer == # of messages sent + # of double insertions
     // LHS: sum(merkle_tree_max_sequence) + len(merkle_tree_max_sequence) (each is index so we add 1 to each)
     // RHS: total_messages_expected + non_matching_igp_messages + (config.kathy_messages as u32 / 4) * 2 (double insertions)
-    let non_zero_sequence_count =
-        merkle_tree_max_sequence.iter().filter(|&&x| x > 0).count() as u32;
+    let non_zero_sequence_count = 
+        merkle_tree_max_sequence.iter().filter(|&x| *x > 0).count() as u32;
     assert_eq!(
         merkle_tree_max_sequence.iter().sum::<u32>() + non_zero_sequence_count,
         total_messages_expected
