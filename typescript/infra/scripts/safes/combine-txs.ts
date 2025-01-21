82a9febc50112d069f832075562b2f0a11fb5fac
pbio <10051819+paulbalaji@users.noreply.github.com>
2025-01-21 13:51:45 +0000

    CR xeno097

--- typescript/infra/scripts/safes/combine-txs.ts
+++ typescript/infra/scripts/safes/combine-txs.ts
@@ -1,10 +1,13 @@
 // Import necessary modules
 import { SafeTransaction } from '@safe-global/safe-core-sdk-types';
+// eslint-disable-next-line
 import * as fs from 'fs';
 import * as path from 'path';
 import yargs from 'yargs';
 
-import { getRegistry } from '../../config/environments/mainnet3/chains.js';
+import { rootLogger } from '@hyperlane-xyz/utils';
+
+import { readJSONAtPath, writeJsonAtPath } from '../../src/utils/utils.js';
 import { getEnvironmentConfig } from '../core-utils.js';
 
 type TxFile = {
@@ -19,20 +22,17 @@ function readJSONFiles(directory: string): Record<string, TxFile[]> {
   const files = fs.readdirSync(directory);
   const transactionsByChainId: Record<string, TxFile[]> = {};
 
-  files.forEach((file) => {
+  for (const file of files) {
     if (path.extname(file) === '.json') {
       const filePath = path.join(directory, file);
-      const data = fs.readFileSync(filePath, 'utf8');
-      const jsonData = JSON.parse(data) as TxFile;
-
-      const chainId = jsonData.chainId;
+      const txs: TxFile = readJSONAtPath(filePath);
+      const chainId = txs.chainId;
       if (!transactionsByChainId[chainId]) {
         transactionsByChainId[chainId] = [];
       }
-
-      transactionsByChainId[chainId].push(jsonData);
+      transactionsByChainId[chainId].push(txs);
     }
-  });
+  }
 
   return transactionsByChainId;
 }
@@ -62,12 +62,8 @@ async function writeCombinedTransactions(
     // NOTE: hacky use of chainid instead of domainid or chain name here
     const chainName = multiProvider.getChainName(chainId);
     const outputFilePath = path.join(outputDir, `${chainId}-${chainName}.json`);
-    fs.writeFileSync(
-      outputFilePath,
-      JSON.stringify(outputData, null, 2),
-      'utf8',
-    );
-    console.log(`Combined transactions written to ${outputFilePath}`);
+    writeJsonAtPath(outputFilePath, outputData);
+    rootLogger.info(`Combined transactions written to ${outputFilePath}`);
   }
 }
 
@@ -81,16 +77,16 @@ async function main() {
   }).argv;
 
   if (!fs.existsSync(directory)) {
-    console.error(`Directory ${directory} does not exist`);
+    rootLogger.error(`Directory ${directory} does not exist`);
     process.exit(1);
   }
 
   const transactionsByChainId = readJSONFiles(directory);
-  writeCombinedTransactions(transactionsByChainId, directory);
+  await writeCombinedTransactions(transactionsByChainId, directory);
 }
 
 // Execute the main function and handle promise
 main().catch((error) => {
-  console.error('An error occurred:', error);
+  rootLogger.error('An error occurred:', error);
   process.exit(1);
 });
