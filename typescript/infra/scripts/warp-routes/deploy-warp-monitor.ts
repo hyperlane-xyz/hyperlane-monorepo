ab2c826e5302993717a6fdaf3211f7113956941f
pbio <10051819+paulbalaji@users.noreply.github.com>
2025-01-22 15:24:00 +0000

    validate registry commit before deploying warp monitor

--- typescript/infra/scripts/warp-routes/deploy-warp-monitor.ts
+++ typescript/infra/scripts/warp-routes/deploy-warp-monitor.ts
@@ -1,4 +1,6 @@
 import { input } from '@inquirer/prompts';
+import chalk from 'chalk';
+import { execSync } from 'child_process';
 
 import {
   LogFormat,
@@ -8,6 +10,7 @@ import {
 } from '@hyperlane-xyz/utils';
 
 import { Contexts } from '../../config/contexts.js';
+import { getRegistry } from '../../config/registry.js';
 import { HelmCommand } from '../../src/utils/helm.js';
 import { WarpRouteMonitorHelmManager } from '../../src/warp/helm.js';
 import {
@@ -18,6 +21,24 @@ import {
   withWarpRouteId,
 } from '../agent-utils.js';
 import { getEnvironmentConfig } from '../core-utils.js';
+
+async function validateRegistryCommit(commit: string) {
+  const registry = getRegistry();
+  const registryUri = registry.getUri();
+
+  try {
+    rootLogger.info(
+      chalk.grey.italic(`Attempting to fetch registry commit ${commit}...`),
+    );
+    execSync(`cd ${registryUri} && git fetch origin ${commit}`, {
+      stdio: 'inherit',
+    });
+    rootLogger.info(chalk.grey.italic('Fetch completed successfully.'));
+  } catch (_) {
+    rootLogger.error(chalk.red(`Unable to fetch registry commit ${commit}.`));
+    process.exit(1);
+  }
+}
 
 async function main() {
   configureRootLogger(LogFormat.Pretty, LogLevel.Info);
@@ -34,6 +55,7 @@ async function main() {
     message:
       'Enter the registry version to use (can be a commit, branch or tag):',
   });
+  await validateRegistryCommit(registryCommit);
 
   await assertCorrectKubeContext(getEnvironmentConfig(environment));
   const agentConfig = getAgentConfig(Contexts.Hyperlane, environment);
