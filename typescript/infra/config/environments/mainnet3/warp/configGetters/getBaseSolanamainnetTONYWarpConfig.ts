301998e18d5f913ced3b493857bfc67a5995ecf0
Trevor Porter <tkporter4@gmail.com>
2025-01-22 15:34:40 +0000

    enroll

--- typescript/infra/config/environments/mainnet3/warp/configGetters/getBaseSolanamainnetTONYWarpConfig.ts
+++ typescript/infra/config/environments/mainnet3/warp/configGetters/getBaseSolanamainnetTONYWarpConfig.ts
@@ -11,12 +11,13 @@ import {
   RouterConfigWithoutOwner,
   tokens,
 } from '../../../../../src/config/warp.js';
+import { DEPLOYER } from '../../owners.js';
 import { SEALEVEL_WARP_ROUTE_HANDLER_GAS_AMOUNT } from '../consts.js';
 
 // Cod3x SAFE
-const baseOwner = '0xfEfcb2fb19b9A70B30646Fdc1A0860Eb12F7ff8b';
+const baseOwner = DEPLOYER; // '0xfEfcb2fb19b9A70B30646Fdc1A0860Eb12F7ff8b';
 // Cod3x Squads vault
-const solanamainnetOwner = '7dRAVvdmV3dy4JieuRAirBQ9oSpYaHgmYwupoK5YZcFR';
+const solanamainnetOwner = DEPLOYER; // '7dRAVvdmV3dy4JieuRAirBQ9oSpYaHgmYwupoK5YZcFR';
 
 export async function getBaseSolanamainnetTONYWarpConfig(
   routerConfig: ChainMap<RouterConfigWithoutOwner>,
@@ -34,7 +35,7 @@ export async function getBaseSolanamainnetTONYWarpConfig(
     mailbox: routerConfig.solanamainnet.mailbox,
     owner: solanamainnetOwner,
     type: TokenType.synthetic,
-    foreignDeployment: '4AQVPTCAeLswnjksQdutxUDuxEJxUBwoWmVimGuPtGSt',
+    foreignDeployment: 'Fa4zQJCH7id5KL1eFJt2mHyFpUNfCCSkHgtMrLvrRJBN',
     gas: SEALEVEL_WARP_ROUTE_HANDLER_GAS_AMOUNT,
     interchainSecurityModule: ethers.constants.AddressZero,
   };
