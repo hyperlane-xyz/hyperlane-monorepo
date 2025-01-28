21a8c51f6ec1c93e106c0d2f35822dbf58e73c54
Le Yu <6251863+ltyu@users.noreply.github.com>
2025-01-28 13:24:06 -0500

    Update getBaseZeroNetworkCBBTCWarpConfig to remove override

--- typescript/infra/config/environments/mainnet3/warp/configGetters/getBaseZeroNetworkCBBTCWarpConfig.ts
+++ typescript/infra/config/environments/mainnet3/warp/configGetters/getBaseZeroNetworkCBBTCWarpConfig.ts
@@ -21,7 +21,7 @@ export const getBaseZeroNetworkCBBTCWarpConfig = async (
 
   const base: HypTokenRouterConfig = {
     ...routerConfig.base,
-    ...abacusWorksEnvOwnerConfig.base,
+    owner: abacusWorksEnvOwnerConfig.base.owner,
     proxyAdmin: {
       owner: abacusWorksEnvOwnerConfig.base.owner,
       address: '0x0FC41a92F526A8CD22060A4052e156502D6B9db0',
