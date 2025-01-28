ba53d443a55abb2714f61654409a96e041e3486a
kamiyaa <jeff.no.zhao@gmail.com>
2025-01-28 12:45:44 +0800

    feat: fix exposed private key in logs

--- rust/main/chains/hyperlane-sealevel/src/interchain_security_module.rs
+++ rust/main/chains/hyperlane-sealevel/src/interchain_security_module.rs
@@ -1,6 +1,6 @@
 use async_trait::async_trait;
 use num_traits::cast::FromPrimitive;
-use solana_sdk::{instruction::Instruction, pubkey::Pubkey, signature::Keypair};
+use solana_sdk::{instruction::Instruction, pubkey::Pubkey, signature::Keypair, signer::Signer};
 use tracing::warn;
 
 use hyperlane_core::{
@@ -13,7 +13,6 @@ use serializable_account_meta::SimulationReturnData;
 use crate::{ConnectionConf, SealevelProvider, SealevelRpcClient};
 
 /// A reference to an InterchainSecurityModule contract on some Sealevel chain
-#[derive(Debug)]
 pub struct SealevelInterchainSecurityModule {
     payer: Option<Keypair>,
     program_id: Pubkey,
@@ -93,5 +92,81 @@ impl InterchainSecurityModule for SealevelInterchainSecurityModule {
     ) -> ChainResult<Option<U256>> {
         // TODO: Implement this once we have aggregation ISM support in Sealevel
         Ok(Some(U256::zero()))
+    }
+}
+
+impl std::fmt::Debug for SealevelInterchainSecurityModule {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        #[allow(dead_code)]
+        #[derive(Debug)]
+        struct PublicKey {
+            base58_pubkey: String,
+        }
+
+        #[allow(dead_code)]
+        #[derive(Debug)]
+        struct SealevelInterchainSecurityModule<'a> {
+            payer: Option<PublicKey>,
+            program_id: &'a Pubkey,
+            provider: &'a SealevelProvider,
+        }
+
+        let payer = self.payer.as_ref().map(|s| PublicKey {
+            base58_pubkey: s.pubkey().to_string(),
+        });
+        let val = SealevelInterchainSecurityModule {
+            payer,
+            program_id: &self.program_id,
+            provider: &self.provider,
+        };
+        std::fmt::Debug::fmt(&val, f)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use std::str::FromStr;
+
+    use hyperlane_core::{
+        config::OperationBatchConfig, HyperlaneDomain, KnownHyperlaneDomain, NativeToken,
+    };
+    use solana_sdk::{signature::Keypair, signer::Signer};
+    use url::Url;
+
+    use crate::{
+        ConnectionConf, PriorityFeeOracleConfig, SealevelProvider, TransactionSubmitterConfig,
+    };
+
+    use super::SealevelInterchainSecurityModule;
+
+    #[test]
+    fn test_no_exposed_secret_key() {
+        let priv_key = "2ckDxzDFpZGeWd7VbHzd6dMgxYpqVDPA8XzeXFuuUJ1K8CjtyTBenD1TSPPovahXEFw3kBihoyAKktyro22MP4bN";
+        let pub_key = "6oKnHXD2LRzQ4iNsgvkGSNNx68vj5GCYYpR2icy5JZhE";
+
+        let keypair = Keypair::from_base58_string(priv_key);
+
+        let program_id = keypair.pubkey();
+
+        let value = SealevelInterchainSecurityModule {
+            payer: Some(keypair),
+            program_id,
+            provider: SealevelProvider::new(
+                HyperlaneDomain::Known(KnownHyperlaneDomain::SolanaMainnet),
+                &ConnectionConf {
+                    url: Url::from_str("https://solscan.io").expect("Failed to parse URL"),
+                    operation_batch: OperationBatchConfig::default(),
+                    native_token: NativeToken::default(),
+                    priority_fee_oracle: PriorityFeeOracleConfig::Constant(0),
+                    transaction_submitter: TransactionSubmitterConfig::Rpc { url: None },
+                },
+            ),
+        };
+
+        let expected = format!(
+            r#"SealevelInterchainSecurityModule {{ payer: Some(PublicKey {{ base58_pubkey: "{pub_key}" }}), program_id: {pub_key}, provider: SealevelProvider {{ domain: HyperlaneDomain(solanamainnet (1399811149)), rpc_client: RpcClient {{ ... }}, native_token: NativeToken {{ decimals: 0, denom: "" }} }} }}"#
+        );
+        let actual = format!("{:?}", value);
+        assert_eq!(expected, actual);
     }
 }
