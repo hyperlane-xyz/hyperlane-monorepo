db95ab0b8bbcf2a85cc5540e06290ca8a4955694
pbio <10051819+paulbalaji@users.noreply.github.com>
2025-01-30 17:42:03 +0000

    try deploy

--- typescript/sdk/src/deploy/HyperlaneProxyFactoryDeployer.ts
+++ typescript/sdk/src/deploy/HyperlaneProxyFactoryDeployer.ts
@@ -35,7 +35,9 @@ export class HyperlaneProxyFactoryDeployer extends HyperlaneDeployer<
     for (const factoryName of Object.keys(
       this.factories,
     ) as (keyof ProxyFactoryFactories)[]) {
+      rootLogger.info(`Deploying ${factoryName} on ${chain}`);
       const factory = await this.deployContract(chain, factoryName, []);
+      rootLogger.info(`Deployed ${factoryName} on ${chain}`);
       this.addVerificationArtifacts(chain, [
         {
           name: proxyFactoryImplementations[factoryName],
@@ -44,7 +46,9 @@ export class HyperlaneProxyFactoryDeployer extends HyperlaneDeployer<
           isProxy: true,
         },
       ]);
+      rootLogger.info(`Verified ${factoryName} on ${chain}`);
       contracts[factoryName] = factory;
+      rootLogger.info(`Added ${factoryName} to contracts on ${chain}`);
     }
     return contracts as HyperlaneContracts<ProxyFactoryFactories>;
   }
