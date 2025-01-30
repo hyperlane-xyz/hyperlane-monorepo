1841196ce21fcc93ed21d2a2a10d18fb53576075
An-tol <88716253+An-tol@users.noreply.github.com>
2025-01-30 22:08:04 +0300

    Update ChainSearchMenu.tsx
    Added:
    List virtualization: Added FixedSizeList from react-window.
    Memoization: The ChainListItem, ChainFilters and chainSearch components are wrapped in memo.
    Lazy loading: Added loading="lazy" attribute for images.
    Hook optimization

--- typescript/widgets/src/chains/ChainSearchMenu.tsx
+++ typescript/widgets/src/chains/ChainSearchMenu.tsx
@@ -1,5 +1,5 @@
-import React, { useCallback, useMemo, useState } from 'react';
-
+import React, { useCallback, useMemo, useState, memo } from 'react';
+import { FixedSizeList as List } from 'react-window';
 import {
   ChainMap,
   ChainMetadata,
@@ -54,15 +54,10 @@ export interface ChainSearchMenuProps {
     overrides?: ChainMap<Partial<ChainMetadata> | undefined>,
   ) => void;
   onClickChain: (chain: ChainMetadata) => void;
-  // Replace the default 2nd column (deployer) with custom data
   customListItemField?: CustomListItemField | null;
-  // Auto-navigate to a chain details menu
   showChainDetails?: ChainName;
-  // Auto-navigate to a chain add menu
   showAddChainMenu?: boolean;
-  // Include add button above list
   showAddChainButton?: boolean;
-  // Field by which data will be sorted by default
   defaultSortField?: DefaultSortField;
 }
 
@@ -80,7 +75,6 @@ export function ChainSearchMenu({
   const [drilldownChain, setDrilldownChain] = useState<ChainName | undefined>(
     showChainDetails,
   );
-
   const [addChain, setAddChain] = useState(showAddChainMenu || false);
 
   const { listData, mergedMetadata } = useMemo(() => {
@@ -150,149 +144,151 @@ export function ChainSearchMenu({
   );
 }
 
-function ChainListItem({
-  data: chain,
-  customField,
-}: {
-  data: ChainMetadata;
-  customField?: CustomListItemField | null;
-}) {
-  return (
-    <>
-      <div className="htw-flex htw-items-center">
-        <div className="htw-shrink-0">
-          <ChainLogo chainName={chain.name} logoUri={chain.logoURI} size={32} />
-        </div>
-        <div className="htw-ml-3 htw-text-left htw-overflow-hidden">
-          <div className="htw-text-sm htw-font-medium truncate">
-            {chain.displayName}
+const ChainListItem = memo(
+  ({
+    data: chain,
+    customField,
+  }: {
+    data: ChainMetadata;
+    customField?: CustomListItemField | null;
+  }) => {
+    return (
+      <>
+        <div className="htw-flex htw-items-center">
+          <div className="htw-shrink-0">
+            <ChainLogo
+              chainName={chain.name}
+              logoUri={chain.logoURI}
+              size={32}
+              loading="lazy"
+            />
           </div>
-          <div className="htw-text-[0.7rem] htw-text-gray-500">
-            {chain.isTestnet ? 'Testnet' : 'Mainnet'}
-          </div>
-        </div>
-      </div>
-      {customField !== null && (
-        <div className="htw-text-left htw-overflow-hidden">
-          <div className="htw-text-sm truncate">
-            {customField
-              ? customField.data[chain.name].display || 'Unknown'
-              : chain.deployer?.name || 'Unknown deployer'}
-          </div>
-          <div className="htw-text-[0.7rem] htw-text-gray-500">
-            {customField ? customField.header : 'Deployer'}
+          <div className="htw-ml-3 htw-text-left htw-overflow-hidden">
+            <div className="htw-text-sm htw-font-medium truncate">
+              {chain.displayName}
+            </div>
+            <div className="htw-text-[0.7rem] htw-text-gray-500">
+              {chain.isTestnet ? 'Testnet' : 'Mainnet'}
+            </div>
           </div>
         </div>
-      )}
-    </>
-  );
-}
+        {customField !== null && (
+          <div className="htw-text-left htw-overflow-hidden">
+            <div className="htw-text-sm truncate">
+              {customField
+                ? customField.data[chain.name].display || 'Unknown'
+                : chain.deployer?.name || 'Unknown deployer'}
+            </div>
+            <div className="htw-text-[0.7rem] htw-text-gray-500">
+              {customField ? customField.header : 'Deployer'}
+            </div>
+          </div>
+        )}
+      </>
+    );
+  },
+);
 
-function ChainFilters({
-  value,
-  onChange,
-}: {
-  value: ChainFilterState;
-  onChange: (s: ChainFilterState) => void;
-}) {
-  return (
-    <div className="htw-py-3 htw-px-2.5 htw-space-y-4">
-      <div className="htw-flex htw-flex-col htw-items-start htw-gap-2">
-        <label className="htw-text-sm htw-text-gray-600 htw-pl-px">Type</label>
-        <SegmentedControl
-          options={Object.values(FilterTestnetOption)}
-          onChange={(selected) => onChange({ ...value, type: selected })}
-          allowEmpty
-        />
+const ChainFilters = memo(
+  ({
+    value,
+    onChange,
+  }: {
+    value: ChainFilterState;
+    onChange: (s: ChainFilterState) => void;
+  }) => {
+    return (
+      <div className="htw-py-3 htw-px-2.5 htw-space-y-4">
+        <div className="htw-flex htw-flex-col htw-items-start htw-gap-2">
+          <label className="htw-text-sm htw-text-gray-600 htw-pl-px">Type</label>
+          <SegmentedControl
+            options={Object.values(FilterTestnetOption)}
+            onChange={(selected) => onChange({ ...value, type: selected })}
+            allowEmpty
+          />
+        </div>
+        <div className="htw-flex htw-flex-col htw-items-start htw-gap-2">
+          <label className="htw-text-sm htw-text-gray-600 htw-pl-px">
+            Protocol
+          </label>
+          <SegmentedControl
+            options={Object.values(ProtocolType)}
+            onChange={(selected) => onChange({ ...value, protocol: selected })}
+            allowEmpty
+          />
+        </div>
       </div>
-      <div className="htw-flex htw-flex-col htw-items-start htw-gap-2">
-        <label className="htw-text-sm htw-text-gray-600 htw-pl-px">
-          Protocol
-        </label>
-        <SegmentedControl
-          options={Object.values(ProtocolType)}
-          onChange={(selected) => onChange({ ...value, protocol: selected })}
-          allowEmpty
-        />
-      </div>
-    </div>
-  );
-}
+    );
+  },
+);
 
-function chainSearch({
-  data,
-  query,
-  sort,
-  filter,
-  customListItemField,
-}: {
-  data: ChainMetadata[];
-  query: string;
-  sort: SortState<ChainSortByOption>;
-  filter: ChainFilterState;
-  customListItemField?: CustomListItemField;
-}) {
-  const queryFormatted = query.trim().toLowerCase();
-  return (
-    data
-      // Query search
-      .filter(
-        (chain) =>
-          chain.name.includes(queryFormatted) ||
-          chain.displayName?.toLowerCase().includes(queryFormatted) ||
-          chain.chainId.toString().includes(queryFormatted) ||
-          chain.domainId.toString().includes(queryFormatted),
-      )
-      // Filter options
-      .filter((chain) => {
-        let included = true;
-        if (filter.type) {
-          included &&=
-            !!chain.isTestnet === (filter.type === FilterTestnetOption.Testnet);
-        }
-        if (filter.protocol) {
-          included &&= chain.protocol === filter.protocol;
-        }
-        return included;
-      })
-      // Sort options
-      .sort((c1, c2) => {
-        // Special case handling for if the chains are being sorted by the
-        // custom field provided to ChainSearchMenu
-        if (customListItemField && sort.sortBy === customListItemField.header) {
-          const result =
-            customListItemField.data[c1.name].sortValue -
-            customListItemField.data[c2.name].sortValue;
-          return sort.sortOrder === SortOrderOption.Asc ? result : -result;
-        }
+const chainSearch = memo(
+  ({
+    data,
+    query,
+    sort,
+    filter,
+    customListItemField,
+  }: {
+    data: ChainMetadata[];
+    query: string;
+    sort: SortState<ChainSortByOption>;
+    filter: ChainFilterState;
+    customListItemField?: CustomListItemField;
+  }) => {
+    const queryFormatted = query.trim().toLowerCase();
+    return (
+      data
+        // Query search
+        .filter(
+          (chain) =>
+            chain.name.includes(queryFormatted) ||
+            chain.displayName?.toLowerCase().includes(queryFormatted) ||
+            chain.chainId.toString().includes(queryFormatted) ||
+            chain.domainId.toString().includes(queryFormatted),
+        )
+        // Filter options
+        .filter((chain) => {
+          let included = true;
+          if (filter.type) {
+            included &&=
+              !!chain.isTestnet === (filter.type === FilterTestnetOption.Testnet);
+          }
+          if (filter.protocol) {
+            included &&= chain.protocol === filter.protocol;
+          }
+          return included;
+        })
+        // Sort options
+        .sort((c1, c2) => {
+          if (customListItemField && sort.sortBy === customListItemField.header) {
+            const result =
+              customListItemField.data[c1.name].sortValue -
+              customListItemField.data[c2.name].sortValue;
+            return sort.sortOrder === SortOrderOption.Asc ? result : -result;
+          }
 
-        // Otherwise sort by the default options
-        let sortValue1 = c1.name;
-        let sortValue2 = c2.name;
-        if (sort.sortBy === ChainSortByOption.ChainId) {
-          sortValue1 = c1.chainId.toString();
-          sortValue2 = c2.chainId.toString();
-        } else if (sort.sortBy === ChainSortByOption.Protocol) {
-          sortValue1 = c1.protocol;
-          sortValue2 = c2.protocol;
-        }
-        return sort.sortOrder === SortOrderOption.Asc
-          ? sortValue1.localeCompare(sortValue2)
-          : sortValue2.localeCompare(sortValue1);
-      })
-  );
-}
+          let sortValue1 = c1.name;
+          let sortValue2 = c2.name;
+          if (sort.sortBy === ChainSortByOption.ChainId) {
+            sortValue1 = c1.chainId.toString();
+            sortValue2 = c2.chainId.toString();
+          } else if (sort.sortBy === ChainSortByOption.Protocol) {
+            sortValue1 = c1.protocol;
+            sortValue2 = c2.protocol;
+          }
+          return sort.sortOrder === SortOrderOption.Asc
+            ? sortValue1.localeCompare(sortValue2)
+            : sortValue2.localeCompare(sortValue1);
+        })
+    );
+  },
+);
 
-/**
- * This hook creates closures around the provided customListItemField data
- * This is useful because SearchMenu will do handle the list item rendering and
- * management but the custom data is more or a chain-search-specific concern
- */
 function useCustomizedListItems(
   customListItemField,
   defaultSortField?: DefaultSortField,
 ) {
-  // Create closure of ChainListItem but with customField pre-bound
   const ListComponent = useCallback(
     ({ data }: { data: ChainMetadata<{ disabled?: boolean }> }) => (
       <ChainListItem data={data} customField={customListItemField} />
@@ -300,14 +296,12 @@ function useCustomizedListItems(
     [customListItemField],
   );
 
-  // Bind the custom field to the search function
   const searchFn = useCallback(
     (args: Parameters<typeof chainSearch>[0]) =>
       chainSearch({ ...args, customListItemField }),
     [customListItemField],
   );
 
-  // Merge the custom field into the sort options if a custom field exists
   const sortOptions = useMemo(
     () => [
       ...(customListItemField ? [customListItemField.header] : []),
@@ -316,7 +310,6 @@ function useCustomizedListItems(
     [customListItemField],
   ) as ChainSortByOption[];
 
-  // Sort by defaultSortField initially, if value is "custom", sort using custom field by default
   const defaultSortState = useMemo(
     () =>
       defaultSortField
