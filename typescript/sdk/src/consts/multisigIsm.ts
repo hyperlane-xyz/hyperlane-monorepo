7c70e5390782ce668363143dfc41e4c26c48acc6
pbio <10051819+paulbalaji@users.noreply.github.com>
2025-02-03 11:26:20 +0000

    feat: enroll jan 23 batch

--- typescript/sdk/src/consts/multisigIsm.ts
+++ typescript/sdk/src/consts/multisigIsm.ts
@@ -35,12 +35,14 @@ const DEFAULT_BWARE_LABS_VALIDATOR: ValidatorConfig = {
 // TODO: consider migrating these to the registry too
 export const defaultMultisigConfigs: ChainMap<MultisigConfig> = {
   abstract: {
-    threshold: 1,
+    threshold: 2,
     validators: [
       {
         address: '0x2ef8ece5b51562e65970c7d36007baa43a1de685',
         alias: AW_VALIDATOR_ALIAS,
       },
+      DEFAULT_MERKLY_VALIDATOR,
+      DEFAULT_MITOSIS_VALIDATOR,
     ],
   },
 
@@ -860,12 +862,14 @@ export const defaultMultisigConfigs: ChainMap<MultisigConfig> = {
   },
 
   glue: {
-    threshold: 1,
+    threshold: 2,
     validators: [
       {
         address: '0xbe2ded12f7b023916584836506677ea89a0b6924',
         alias: AW_VALIDATOR_ALIAS,
       },
+      DEFAULT_MERKLY_VALIDATOR,
+      DEFAULT_MITOSIS_VALIDATOR,
     ],
   },
 
@@ -988,8 +992,8 @@ export const defaultMultisigConfigs: ChainMap<MultisigConfig> = {
         alias: AW_VALIDATOR_ALIAS,
       },
       {
-        address: '0x6B1d09A97b813D53e9D4b7523DA36604C0B52242',
-        alias: 'Caldera',
+        address: '0xd6f6dee54443632490eddc82680d8917544bcb5a',
+        alias: 'Decentrio',
       },
       {
         address: '0x9e551b6694bbd295d7d6e6a2540c7d41ce70a3b9',
@@ -1174,12 +1178,14 @@ export const defaultMultisigConfigs: ChainMap<MultisigConfig> = {
   },
 
   matchain: {
-    threshold: 1,
+    threshold: 2,
     validators: [
       {
         address: '0x8a052f7934b0626105f34f980c875ec03aaf82e8',
         alias: AW_VALIDATOR_ALIAS,
       },
+      DEFAULT_MERKLY_VALIDATOR,
+      DEFAULT_MITOSIS_VALIDATOR,
     ],
   },
 
@@ -1771,11 +1777,17 @@ export const defaultMultisigConfigs: ChainMap<MultisigConfig> = {
   },
 
   sonicsvm: {
-    threshold: 1,
+    threshold: 3,
     validators: [
       {
         address: '0xf21f46905d8d09f76bc8c503f856e5466bc5ffea',
         alias: AW_VALIDATOR_ALIAS,
+      },
+      DEFAULT_MERKLY_VALIDATOR,
+      DEFAULT_MITOSIS_VALIDATOR,
+      {
+        address: '0x85c7a16790cfd9dad6d4abdd4e2d3f1d550c7606',
+        alias: 'Sonic SVM',
       },
     ],
   },
@@ -1903,7 +1915,7 @@ export const defaultMultisigConfigs: ChainMap<MultisigConfig> = {
   },
 
   swell: {
-    threshold: 3,
+    threshold: 4,
     validators: [
       {
         address: '0x4f51e4f4c7fb45d82f91568480a1a2cfb69216ed',
@@ -1913,12 +1925,16 @@ export const defaultMultisigConfigs: ChainMap<MultisigConfig> = {
         address: '0x9eadf9217be22d9878e0e464727a2176d5c69ff8',
         alias: 'Luganodes',
       },
-      DEFAULT_MERKLY_VALIDATOR,
-      DEFAULT_MITOSIS_VALIDATOR,
       {
-        address: '0x5aed2fd5cc5f9749c455646c86b0db6126cafcbb',
-        alias: 'Hashkey Cloud',
+        address: '0xa5a23fa2a67782bbf1a540cb5ca6a47a0f3f66fb',
+        alias: 'Imperator',
       },
+      {
+        address: '0x3f707633ccab09d2978e29107c0bbef8a993e7a0',
+        alias: 'Enigma',
+      },
+      DEFAULT_BWARE_LABS_VALIDATOR,
+      DEFAULT_TESSELLATED_VALIDATOR,
     ],
   },
 
@@ -2048,12 +2064,14 @@ export const defaultMultisigConfigs: ChainMap<MultisigConfig> = {
   },
 
   unitzero: {
-    threshold: 1,
+    threshold: 2,
     validators: [
       {
         address: '0x18818e3ad2012728465d394f2e3c0ea2357ae9c5',
         alias: AW_VALIDATOR_ALIAS,
       },
+      DEFAULT_MERKLY_VALIDATOR,
+      DEFAULT_MITOSIS_VALIDATOR,
     ],
   },
 
