5e966a51e1d23a5953a9116aac3d87df0f50d3dc
pbio <10051819+paulbalaji@users.noreply.github.com>
2025-02-04 14:42:25 +0000

    update keyfunder

--- typescript/infra/config/environments/testnet4/funding.ts
+++ typescript/infra/config/environments/testnet4/funding.ts
@@ -10,7 +10,7 @@ export const keyFunderConfig: KeyFunderConfig<
 > = {
   docker: {
     repo: 'gcr.io/abacus-labs-dev/hyperlane-monorepo',
-    tag: '7339a35-20250117-144300',
+    tag: '8781543-20250204-143029',
   },
   // We're currently using the same deployer key as testnet2.
   // To minimize nonce clobbering we offset the key funder cron
