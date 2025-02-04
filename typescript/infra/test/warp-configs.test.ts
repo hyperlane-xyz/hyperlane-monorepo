4f9dc2ccc1daeb94dc2b5861441c57bf79ff4b25
Le Yu <6251863+ltyu@users.noreply.github.com>
2025-02-04 15:43:01 -0500

    Remove comment

--- typescript/infra/test/warp-configs.test.ts
+++ typescript/infra/test/warp-configs.test.ts
@@ -20,7 +20,6 @@ const DEFAULT_TIMEOUT = 60000;
 describe('Warp Configs', async function () {
   this.timeout(DEFAULT_TIMEOUT);
   const ENV = 'mainnet3';
-  console.log('ENV');
   const warpIdsToCheck = Object.keys(warpConfigGetterMap); // TODO Fix this such that the ids to check will be from Github as it includes all the configs
   let multiProvider: MultiProvider;
   let configsFromGithub;
