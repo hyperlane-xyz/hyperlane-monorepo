a36260cd54b02d94c9bf52f1e9ff318e6ca10daa
Yorke Rhodes <yorke@hyperlane.xyz>
2025-02-07 12:28:37 -0500

    Use authorized hook pattern

--- solidity/contracts/isms/hook/CCIPIsm.sol
+++ solidity/contracts/isms/hook/CCIPIsm.sol
@@ -16,7 +16,6 @@ pragma solidity >=0.8.0;
 // ============ Internal Imports ============
 
 import {IInterchainSecurityModule} from "../../interfaces/IInterchainSecurityModule.sol";
-import {LibBit} from "../../libs/LibBit.sol";
 import {Message} from "../../libs/Message.sol";
 import {TypeCasts} from "../../libs/TypeCasts.sol";
 import {AbstractMessageIdAuthorizedIsm} from "./AbstractMessageIdAuthorizedIsm.sol";
@@ -26,124 +25,46 @@ import {Address} from "@openzeppelin/contracts/utils/Address.sol";
 import {IRouterClient} from "@chainlink/contracts-ccip/src/v0.8/ccip/interfaces/IRouterClient.sol";
 import {Client} from "@chainlink/contracts-ccip/src/v0.8/ccip/libraries/Client.sol";
 import {CCIPReceiver} from "@chainlink/contracts-ccip/src/v0.8/ccip/applications/CCIPReceiver.sol";
-import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 /**
  * @title CCIPIsm
  * @notice Uses CCIP hook to verify interchain messages.
  */
-contract CCIPIsm is AbstractMessageIdAuthorizedIsm, CCIPReceiver, Ownable {
-    using LibBit for uint256;
-    
-    // ============ Errors ============
-
-    error SourceChainNotAllowlisted(uint64 sourceChainSelector); // Used when the source chain has not been allowlisted by the contract owner.
-    error SenderNotAllowlisted(address sender); // Used when the sender has not been allowlisted by the contract owner.
-
-    // ============ Events ============
-
-    // Event emitted when a message is received from another chain.
-    event MessageReceived(
-        bytes32 indexed messageId, // The unique ID of the CCIP message.
-        uint64 indexed sourceChainSelector, // The chain selector of the source chain.
-        address sender, // The address of the sender from the source chain.
-        bytes32 payload // The payload that was received.
-    );
+contract CCIPIsm is AbstractMessageIdAuthorizedIsm, CCIPReceiver {
+    using TypeCasts for bytes32;
 
     // ============ Constants ============
 
     uint8 public constant moduleType =
         uint8(IInterchainSecurityModule.Types.NULL);
 
+    uint64 public immutable ccipOrigin;
+
     // ============ Storage ============
-
-    // Mapping to keep track of allowlisted source chains.
-    mapping(uint64 => bool) public allowlistedSourceChains;
-
-    // Mapping to keep track of allowlisted senders.
-    mapping(address => bool) public allowlistedSenders;
-
-    IRouterClient internal immutable ccip_router;
-
-    constructor(address _router) CCIPReceiver(_router) {
-        ccip_router = IRouterClient(_router);
-    }
-
-    // ============ Modifiers ============
-
-    /// @dev Modifier that checks if the chain with the given sourceChainSelector is allowlisted and if the sender is allowlisted.
-    /// @param _sourceChainSelector The selector of the destination chain.
-    /// @param _sender The address of the sender.
-    modifier onlyAllowlisted(uint64 _sourceChainSelector, address _sender) {
-        if (!allowlistedSourceChains[_sourceChainSelector])
-            revert SourceChainNotAllowlisted(_sourceChainSelector);
-        if (!allowlistedSenders[_sender]) revert SenderNotAllowlisted(_sender);
-        _;
+    constructor(
+        address _ccipRouter,
+        uint64 _ccipOrigin
+    ) CCIPReceiver(_ccipRouter) {
+        ccipOrigin = _ccipOrigin;
     }
 
     // ============ Internal functions ============
-
-    /// handle a received message
     function _ccipReceive(
         Client.Any2EVMMessage memory any2EvmMessage
-    )
-        internal
-        override
-        onlyAllowlisted(
-            any2EvmMessage.sourceChainSelector,
-            abi.decode(any2EvmMessage.sender, (address))
-        ) // Make sure source chain and sender are allowlisted
-    {
-        bytes32 lastReceivedPayload = abi.decode(any2EvmMessage.data, (bytes32));  
-        verifyMessageId(lastReceivedPayload); // verifyMessageId(bytes32)
-
-        emit MessageReceived(
-            any2EvmMessage.messageId,
-            any2EvmMessage.sourceChainSelector, // fetch the source chain identifier (aka selector)
-            abi.decode(any2EvmMessage.sender, (address)), // abi-decoding of the sender address,
-            lastReceivedPayload
-        );
-    }
-
-    /**
-     * @notice Receive a message from the AbstractMessageIdAuthHook
-     * @dev Only callable by the authorized hook.
-     * @param messageId Hyperlane Id of the message.
-     */
-    function verifyMessageId(bytes32 messageId) public payable override {
+    ) internal override {
         require(
-            _isAuthorized(),
-            "AbstractMessageIdAuthorizedIsm: sender is not the hook"
-        );
-        require(
-            msg.value < 2 ** VERIFIED_MASK_INDEX,
-            "AbstractMessageIdAuthorizedIsm: msg.value must be less than 2^255"
+            ccipOrigin == any2EvmMessage.sourceChainSelector,
+            "Unauthorized origin"
         );
 
-        verifiedMessages[messageId] = msg.value.setBit(VERIFIED_MASK_INDEX);
-        emit ReceivedMessage(messageId);
+        bytes32 sender = abi.decode(any2EvmMessage.sender, (bytes32));
+        require(sender == authorizedHook, "Unauthorized hook");
+
+        bytes32 messageId = abi.decode(any2EvmMessage.data, (bytes32));
+        preVerifyMessage(messageId, msg.value);
     }
 
-
-    /**
-     * @notice Check if sender is authorized to message `verifyMessageId`.
-     */
-    function _isAuthorized() internal view override returns (bool) {
-        return msg.sender == TypeCasts.bytes32ToAddress(authorizedHook);
-    }
-
-    // ============ Public / External functions ============
-
-    /// @dev Updates the allowlist status of a source chain for transactions.
-    function addSourceChainToAllowlist(
-        uint64 _sourceChainSelector,
-        bool allowed
-    ) external onlyOwner {
-        allowlistedSourceChains[_sourceChainSelector] = allowed;
-    }
-
-    /// @dev Updates the allowlist status of a sender for transactions.
-    function addSenderToAllowlist(address _sender, bool allowed) external onlyOwner {
-        allowlistedSenders[_sender] = allowed;
+    function _isAuthorized() internal pure override returns (bool) {
+        return true;
     }
 }
