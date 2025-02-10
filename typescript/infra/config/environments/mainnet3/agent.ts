ebb475277f42f94ed99cdc91a5ab297337e86c6b
Daniel Savu <23065004+daniel-savu@users.noreply.github.com>
2025-02-10 21:08:19 +0000

    chore: bump relayer image

--- typescript/infra/config/environments/mainnet3/agent.ts
+++ typescript/infra/config/environments/mainnet3/agent.ts
@@ -679,7 +679,7 @@ const hyperlane: RootAgentConfig = {
     rpcConsensusType: RpcConsensusType.Fallback,
     docker: {
       repo,
-      tag: '0f0161d-20250206-001154',
+      tag: 'dfd9e9f-20250210-191957',
     },
     blacklist,
     gasPaymentEnforcement: gasPaymentEnforcement,
@@ -714,7 +714,7 @@ const releaseCandidate: RootAgentConfig = {
     rpcConsensusType: RpcConsensusType.Fallback,
     docker: {
       repo,
-      tag: '0f0161d-20250206-001154',
+      tag: 'dfd9e9f-20250210-191957',
     },
     blacklist,
     // We're temporarily (ab)using the RC relayer as a way to increase
