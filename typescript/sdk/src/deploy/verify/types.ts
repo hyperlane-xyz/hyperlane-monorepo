27a305fd3064bb3dd5a6635eed756115a4cbed8b
Maxim Evtush <154841002+maximevtush@users.noreply.github.com>
2025-02-10 08:25:57 +0100

    Update types.ts

--- typescript/sdk/src/deploy/verify/types.ts
+++ typescript/sdk/src/deploy/verify/types.ts
@@ -89,7 +89,7 @@ export type FormOptions<Action extends ExplorerApiActions> =
         sourceCode: string;
         contractname: string;
         /* TYPO IS ENFORCED BY API */
-        constructorArguments?: string;
+        constructorArguements?: string;
       }
     : Action extends ExplorerApiActions.VERIFY_PROXY
     ? {
