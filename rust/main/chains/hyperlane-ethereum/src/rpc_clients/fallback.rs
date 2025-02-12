d9e2b127d5799c84c3de41fea4831545393c0757
Danil Nemirovsky <4614623+ameten@users.noreply.github.com>
2025-02-12 18:08:34 +0000

    Add multicast for tx submission to Ethereum fallback provider

--- rust/main/chains/hyperlane-ethereum/src/rpc_clients/fallback.rs
+++ rust/main/chains/hyperlane-ethereum/src/rpc_clients/fallback.rs
@@ -5,6 +5,7 @@ use std::time::Duration;
 use async_trait::async_trait;
 use derive_new::new;
 use ethers::providers::{HttpClientError, JsonRpcClient, ProviderError};
+use futures_util::{stream::FuturesUnordered, StreamExt};
 use serde::{de::DeserializeOwned, Serialize};
 use serde_json::Value;
 use thiserror::Error;
@@ -97,36 +98,87 @@ where
 
         let params = serde_json::to_value(params).expect("valid");
 
-        let mut errors = vec![];
-        // make sure we do at least 4 total retries.
-        while errors.len() <= 3 {
-            if !errors.is_empty() {
+        if method == "eth_sendRawTransaction" {
+            let mut i = 0; // retry if all providers returned a retryable error
+            let mut provider_errors;
+            loop {
+                let mut unordered = FuturesUnordered::new();
+                self.inner
+                    .providers
+                    .iter()
+                    .for_each(|p| unordered.push(Self::provider_request(p, method, &params)));
+
+                let mut errors = vec![];
+
+                loop {
+                    let Some(resp) = unordered.next().await else {
+                        break;
+                    };
+                    match categorize_client_response(method, resp) {
+                        IsOk(v) => return Ok(serde_json::from_value(v)?),
+                        e => {
+                            errors.push(e);
+                            continue;
+                        }
+                    }
+                }
+
+                provider_errors = errors.iter().map(|e| e.into()).collect();
+
+                let all_retryable = errors.iter().all(|e| match e {
+                    IsOk(_) | NonRetryableErr(_) => false,
+                    RetryableErr(_) | RateLimitErr(_) => true,
+                });
+
+                if !all_retryable {
+                    break;
+                }
+
+                i += 1;
+                if i == 3 {
+                    break;
+                }
+
                 sleep(Duration::from_millis(100)).await;
             }
-            let priorities_snapshot = self.take_priorities_snapshot().await;
-            for (idx, priority) in priorities_snapshot.iter().enumerate() {
-                let provider = &self.inner.providers[priority.index];
-                let fut = Self::provider_request(provider, method, &params);
-                let resp = fut.await;
-                self.handle_stalled_provider(priority, provider).await;
-                let _span =
-                    warn_span!("request", fallback_count=%idx, provider_index=%priority.index, ?provider).entered();
 
-                match categorize_client_response(method, resp) {
-                    IsOk(v) => return Ok(serde_json::from_value(v)?),
-                    RetryableErr(e) | RateLimitErr(e) => errors.push(e.into()),
-                    NonRetryableErr(e) => return Err(e.into()),
+            Err(FallbackError::AllProvidersFailed(provider_errors).into())
+        } else {
+            let mut errors = vec![];
+            // make sure we do at least 4 total retries.
+            while errors.len() <= 3 {
+                if !errors.is_empty() {
+                    sleep(Duration::from_millis(100)).await;
+                }
+                let priorities_snapshot = self.take_priorities_snapshot().await;
+                for (idx, priority) in priorities_snapshot.iter().enumerate() {
+                    let provider = &self.inner.providers[priority.index];
+                    let fut = Self::provider_request(&provider, method, &params);
+                    let resp = fut.await;
+                    self.handle_stalled_provider(priority, provider).await;
+                    let _span =
+                        warn_span!("request", fallback_count=%idx, provider_index=%priority.index, ?provider).entered();
+
+                    match categorize_client_response(method, resp) {
+                        IsOk(v) => return Ok(serde_json::from_value(v)?),
+                        RetryableErr(e) | RateLimitErr(e) => errors.push(e.into()),
+                        NonRetryableErr(e) => return Err(e.into()),
+                    }
                 }
             }
-        }
 
-        Err(FallbackError::AllProvidersFailed(errors).into())
+            Err(FallbackError::AllProvidersFailed(errors).into())
+        }
     }
 }
 
-impl<C, B> EthereumFallbackProvider<C, B>
+impl<C> EthereumFallbackProvider<C, JsonRpcBlockGetter<C>>
 where
-    C: JsonRpcClient<Error = HttpClientError>,
+    C: JsonRpcClient<Error = HttpClientError>
+        + Into<JsonRpcBlockGetter<C>>
+        + PrometheusJsonRpcClientConfigExt
+        + Clone,
+    JsonRpcBlockGetter<C>: BlockNumberGetter,
 {
     async fn provider_request<'a>(
         provider: &'a C,
