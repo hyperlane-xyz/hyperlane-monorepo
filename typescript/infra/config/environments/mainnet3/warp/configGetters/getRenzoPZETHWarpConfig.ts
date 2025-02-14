b3a3068d46aea74436b9a4646868037dcfa72d48
Le Yu <6251863+ltyu@users.noreply.github.com>
2025-02-14 16:25:58 -0500

    Add uni and bera to PZETH also add swell to existing proxyAdmins

--- typescript/infra/config/environments/mainnet3/warp/configGetters/getRenzoPZETHWarpConfig.ts
+++ typescript/infra/config/environments/mainnet3/warp/configGetters/getRenzoPZETHWarpConfig.ts
@@ -13,9 +13,17 @@ const pzEthAddresses = {
   ethereum: '0x9cb41CD74D01ae4b4f640EC40f7A60cA1bCF83E7',
   zircuit: '0x9cb41CD74D01ae4b4f640EC40f7A60cA1bCF83E7',
   swell: '0x9cb41CD74D01ae4b4f640EC40f7A60cA1bCF83E7',
+  unichain: '0x9cb41CD74D01ae4b4f640EC40f7A60cA1bCF83E7',
+  berachain: '0x9cb41CD74D01ae4b4f640EC40f7A60cA1bCF83E7',
 };
 
-export const pzEthChainsToDeploy = ['ethereum', 'swell', 'zircuit'];
+export const pzEthChainsToDeploy = [
+  'ethereum',
+  'swell',
+  'zircuit',
+  'unichain',
+  'berachain',
+];
 
 const pzEthValidators = pick(ezEthValidators, pzEthChainsToDeploy);
 const pzEthSafes = pick(ezEthSafes, pzEthChainsToDeploy);
@@ -26,6 +34,10 @@ const existingProxyAdmins: ChainMap<{ address: string; owner: string }> = {
     owner: '0xD1e6626310fD54Eceb5b9a51dA2eC329D6D4B68A',
   },
   zircuit: {
+    address: '0x8b789B4A56675240c9f0985B467752b870c75711',
+    owner: '0x8410927C286A38883BC23721e640F31D3E3E79F8',
+  },
+  swell: {
     address: '0x8b789B4A56675240c9f0985B467752b870c75711',
     owner: '0x8410927C286A38883BC23721e640F31D3E3E79F8',
   },
