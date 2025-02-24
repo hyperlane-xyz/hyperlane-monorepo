fc1b91a80011abb6bc1ef1e52913bc37089c86c0
Trevor Porter <tkporter4@gmail.com>
2025-02-24 13:02:42 +0000

    update image

--- typescript/infra/config/environments/mainnet3/funding.ts
+++ typescript/infra/config/environments/mainnet3/funding.ts
@@ -10,7 +10,7 @@ export const keyFunderConfig: KeyFunderConfig<
 > = {
   docker: {
     repo: 'gcr.io/abacus-labs-dev/hyperlane-monorepo',
-    tag: '114fd57-20250218-171940',
+    tag: 'aec7eee-20250224-122544',
   },
   // We're currently using the same deployer/key funder key as mainnet2.
   // To minimize nonce clobbering we offset the key funder cron
