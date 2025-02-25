cfc53e81b3ad9286e9b9b1111820d2949bfb6a0d
Le Yu <6251863+ltyu@users.noreply.github.com>
2025-02-25 17:25:38 -0500

    Reorganize consts

--- typescript/infra/config/environments/mainnet3/warp/configGetters/getRenzoREZBaseEthereum.ts
+++ typescript/infra/config/environments/mainnet3/warp/configGetters/getRenzoREZBaseEthereum.ts
@@ -8,17 +8,16 @@ import {
   renzoTokenPrices,
 } from './getRenzoEZETHWarpConfig.js';
 
+const rezEthChainsToDeploy = ['ethereum', 'base'];
 const rezProductionLockbox = '0xd8B543fEac4EEcEF5a46a926e10D6f4a72de6fE0';
 const rezEthAddresses = {
   ethereum: '0xf757c9804cF2EE8d8Ed64e0A8936293Fe43a7252',
   base: '0xf757c9804cF2EE8d8Ed64e0A8936293Fe43a7252',
 };
 
-export const rezEthChainsToDeploy = ['ethereum', 'base'];
-
 const rezEthValidators = pick(ezEthValidators, rezEthChainsToDeploy);
 const rezEthSafes = pick(ezEthSafes, rezEthChainsToDeploy);
-export const rezEthTokenPrices = pick(renzoTokenPrices, rezEthChainsToDeploy);
+const rezEthTokenPrices = pick(renzoTokenPrices, rezEthChainsToDeploy);
 
 export const getREZBaseEthereumWarpConfig = getRenzoWarpConfigGenerator({
   chainsToDeploy: rezEthChainsToDeploy,
