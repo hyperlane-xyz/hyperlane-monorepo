fe15fa42a91492c7c1e90ae48e2f34577194caee
Le Yu <6251863+ltyu@users.noreply.github.com>
2025-02-25 17:15:44 -0500

    Add REZ config Getter and WarpIds

---
+++ typescript/infra/config/environments/mainnet3/warp/configGetters/getRenzoREZBaseEthereum.ts
@@ -0,0 +1,30 @@
+import { ChainMap } from '@hyperlane-xyz/sdk';
+import { pick } from '@hyperlane-xyz/utils';
+
+import {
+  ezEthSafes,
+  ezEthValidators,
+  getRenzoWarpConfigGenerator,
+  renzoTokenPrices,
+} from './getRenzoEZETHWarpConfig.js';
+
+const rezProductionLockbox = '0xd8B543fEac4EEcEF5a46a926e10D6f4a72de6fE0';
+const rezEthAddresses = {
+  ethereum: '0xf757c9804cF2EE8d8Ed64e0A8936293Fe43a7252',
+  base: '0xf757c9804cF2EE8d8Ed64e0A8936293Fe43a7252',
+};
+
+export const rezEthChainsToDeploy = ['ethereum', 'base'];
+
+const rezEthValidators = pick(ezEthValidators, rezEthChainsToDeploy);
+const rezEthSafes = pick(ezEthSafes, rezEthChainsToDeploy);
+export const rezEthTokenPrices = pick(renzoTokenPrices, rezEthChainsToDeploy);
+
+export const getREZBaseEthereumWarpConfig = getRenzoWarpConfigGenerator({
+  chainsToDeploy: rezEthChainsToDeploy,
+  validators: rezEthValidators,
+  safes: rezEthSafes,
+  xERC20Addresses: rezEthAddresses,
+  xERC20Lockbox: rezProductionLockbox,
+  tokenPrices: rezEthTokenPrices,
+});
