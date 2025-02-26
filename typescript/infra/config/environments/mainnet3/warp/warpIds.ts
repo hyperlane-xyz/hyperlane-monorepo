52a09c989346d83b92b663dc76be7d074e54aec9
xeno097 <xeno097.cp@gmail.com>
2025-02-26 11:56:03 -0400

    chore(infra): update mint solana route id

--- typescript/infra/config/environments/mainnet3/warp/warpIds.ts
+++ typescript/infra/config/environments/mainnet3/warp/warpIds.ts
@@ -75,6 +75,6 @@ export enum WarpRouteIds {
   ArbitrumEthereumSolanaTreasureSMOL = 'SMOL/arbitrum-ethereum-solanamainnet-treasure',
   BaseSolanaCDX = 'CDX/base-solanamainnet',
   HyperevmSolanaSOL = 'SOL/hyperevm-solanamainnet',
-  MintSolanaMINT = 'MINT/mint-solana',
+  MintSolanaMINT = 'MINT/mint-solanamainnet',
   EthereumUnichainPumpBTC = 'pumpBTCuni/ethereum-unichain',
 }
