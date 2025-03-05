9239d81b247f8d4d22ace28cff987f46cbdb8eaa
Jeff <jeff.no.zhao@gmail.com>
2025-03-05 12:47:21 -0500

    feat: reduce error size by boxing some errors (#5577)
    ### Description
    The purpose of this is to reduce memory usage for our agents. When
    trying to migrate to rust2024, it warned about certain error types being
    unnecessarily large and recommended us to box some error types to reduce
    memory footprint.
    I set `clippy.toml` during development to:
    ```toml
    large-error-threshold = 64
    ```
    Then ran clippy. Then reduced all the errors that was showing a warning
    by boxing them.
    VS Code mouse hover was also able to show size of different errors. But
    it wasn't able to for some error types :/
    
    I've set it back to 128 in the PR because that's the default value and
    setting it to 64 will probably cause a lot of unnecessary warnings.
    Unless we feel 128 is too big, we can always set it to something more
    appropriate.
    
    ### Backward compatibility
    Yes
    
    ### Testing
    None

--- rust/main/chains/hyperlane-cosmos/src/providers/grpc.rs
+++ rust/main/chains/hyperlane-cosmos/src/providers/grpc.rs
@@ -136,6 +136,7 @@ impl CosmosChannel {
         > = grpc_client
             .unary(req, path, codec)
             .await
+            .map_err(Box::new)
             .map_err(Into::<HyperlaneCosmosError>::into)?;
         Ok(response.into_inner())
     }
@@ -212,6 +213,7 @@ impl CosmosChannel {
         let response = client
             .broadcast_tx(tx_req)
             .await
+            .map_err(Box::new)
             .map_err(Into::<HyperlaneCosmosError>::into)?
             .into_inner()
             .tx_response
@@ -383,6 +385,7 @@ impl WasmGrpcProvider {
             amount,
             self.conf.get_canonical_asset().as_str(),
         )
+        .map_err(Box::new)
         .map_err(Into::<HyperlaneCosmosError>::into)?;
         let auth_info =
             signer_info.auth_info(Fee::from_amount_and_gas(fee_coin.clone(), gas_limit));
@@ -391,10 +394,12 @@ impl WasmGrpcProvider {
             .conf
             .get_chain_id()
             .parse()
+            .map_err(Box::new)
             .map_err(Into::<HyperlaneCosmosError>::into)?;
 
         Ok((
             SignDoc::new(&tx_body, &auth_info, &chain_id, account_info.account_number)
+                .map_err(Box::new)
                 .map_err(Into::<HyperlaneCosmosError>::into)?,
             fee_coin,
         ))
@@ -420,10 +425,12 @@ impl WasmGrpcProvider {
         let signer = self.get_signer()?;
         let tx_signed = sign_doc
             .sign(&signer.signing_key()?)
+            .map_err(Box::new)
             .map_err(Into::<HyperlaneCosmosError>::into)?;
         Ok((
             tx_signed
                 .to_bytes()
+                .map_err(Box::new)
                 .map_err(Into::<HyperlaneCosmosError>::into)?,
             fee,
         ))
@@ -633,8 +640,8 @@ impl WasmProvider for WasmGrpcProvider {
         let fee_amount: U256 = fee.amount.into();
         if signer_balance < fee_amount {
             return Err(ChainCommunicationError::InsufficientFunds {
-                required: fee_amount,
-                available: signer_balance,
+                required: Box::new(fee_amount),
+                available: Box::new(signer_balance),
             });
         }
 
