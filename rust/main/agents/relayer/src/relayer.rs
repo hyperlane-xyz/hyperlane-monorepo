9e5baa326543dff1ef9d0f97d8bc15e7b83189b6
Keefer Taylor | Tessellated <keefer@tessellated.io>
2025-03-10 14:24:07 -0700

    Update rust/main/agents/relayer/src/relayer.rs
    Co-authored-by: Danil Nemirovsky <ameten@users.noreply.github.com>

--- rust/main/agents/relayer/src/relayer.rs
+++ rust/main/agents/relayer/src/relayer.rs
@@ -339,8 +339,7 @@ impl BaseAgent for Relayer {
                 .expect("spawning tokio task from Builder is infallible");
             tasks.push(console_server);
         }
-        let sender: BroadcastSender<relayer_server::MessageRetryRequest> =
-            BroadcastSender::new(ENDPOINT_MESSAGES_QUEUE_SIZE);
+        let sender = BroadcastSender::new(ENDPOINT_MESSAGES_QUEUE_SIZE);
         // send channels by destination chain
         let mut send_channels = HashMap::with_capacity(self.destination_chains.len());
         let mut prep_queues = HashMap::with_capacity(self.destination_chains.len());
