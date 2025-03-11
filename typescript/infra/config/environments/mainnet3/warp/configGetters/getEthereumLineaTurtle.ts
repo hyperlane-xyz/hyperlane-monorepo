f3e6d2be94c25f4de24504fd4286eef181007321
christopherbrumm <christopherbrumm2000@gmail.com>
2025-03-11 14:20:21 +0100

    chore: remove empty string `ISM_CONFIG`

--- typescript/infra/config/environments/mainnet3/warp/configGetters/getEthereumLineaTurtle.ts
+++ typescript/infra/config/environments/mainnet3/warp/configGetters/getEthereumLineaTurtle.ts
@@ -1,5 +1,3 @@
-import { ethers } from 'ethers';
-
 import { ChainMap, HypTokenRouterConfig, TokenType } from '@hyperlane-xyz/sdk';
 
 import {
@@ -12,8 +10,6 @@ const turtleOwners = {
   linea: '0x2e0355922EF3a5b77d29287C808aEafB4e7f25B2',
 };
 
-const ISM_CONFIG = ethers.constants.AddressZero; // Default ISM
-
 export const getEthereumLineaTurtle = async (
   routerConfig: ChainMap<RouterConfigWithoutOwner>,
 ): Promise<ChainMap<HypTokenRouterConfig>> => {
@@ -22,14 +18,12 @@ export const getEthereumLineaTurtle = async (
     owner: turtleOwners.ethereum,
     type: TokenType.collateral,
     token: tokens.ethereum.Turtle,
-    interchainSecurityModule: ISM_CONFIG,
   };
 
   const linea: HypTokenRouterConfig = {
     ...routerConfig.linea,
     owner: turtleOwners.linea,
     type: TokenType.synthetic,
-    interchainSecurityModule: ISM_CONFIG,
   };
 
   return {
