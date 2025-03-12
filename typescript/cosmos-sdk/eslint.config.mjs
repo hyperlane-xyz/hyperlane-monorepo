574b132ea2370259bb04a94897e92aeba028199e
Troy Kessler <troy.kessler99@gmail.com>
2025-03-12 14:10:50 +0100

    fix: temp disable linting for cosmos-sdk

--- typescript/cosmos-sdk/eslint.config.mjs
+++ typescript/cosmos-sdk/eslint.config.mjs
@@ -1,3 +1,7 @@
 import MonorepoDefaults from '../../eslint.config.mjs';
 
-export default [...MonorepoDefaults, { files: ['./src/**/*.ts'] }];
+export default [...MonorepoDefaults,
+  {
+    files: ['./src/**/*.ts']
+  }
+];
