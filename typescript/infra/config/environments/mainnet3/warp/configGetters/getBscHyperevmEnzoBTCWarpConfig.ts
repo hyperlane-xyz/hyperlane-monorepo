68bbdce5265970e13e0271c612c79dc56147eddc
Xaroz <jasonguo2013jg@gmail.com>
2025-03-12 14:25:02 -0400

    chore: remove default ISM and update registryrc

--- typescript/infra/config/environments/mainnet3/warp/configGetters/getBscHyperevmEnzoBTCWarpConfig.ts
+++ typescript/infra/config/environments/mainnet3/warp/configGetters/getBscHyperevmEnzoBTCWarpConfig.ts
@@ -1,5 +1,3 @@
-import { ethers } from 'ethers';
-
 import { ChainMap, HypTokenRouterConfig, TokenType } from '@hyperlane-xyz/sdk';
 import { Address } from '@hyperlane-xyz/utils';
 
@@ -13,8 +11,6 @@ const safeOwners: ChainMap<Address> = {
   hyperevm: '0x5A2ee9A4B4D6076cDb3a08c9ae5aca1bD8AD3b02',
 };
 
-const ISM_CONFIG = ethers.constants.AddressZero; // Default ISM
-
 export const getBscHyperevmEnzoBTCWarpConfig = async (
   routerConfig: ChainMap<RouterConfigWithoutOwner>,
 ): Promise<ChainMap<HypTokenRouterConfig>> => {
@@ -22,7 +18,6 @@ export const getBscHyperevmEnzoBTCWarpConfig = async (
     ...routerConfig.bsc,
     owner: safeOwners.bsc,
     type: TokenType.collateral,
-    interchainSecurityModule: ISM_CONFIG,
     token: tokens.bsc.enzoBTC,
   };
 
@@ -30,7 +25,6 @@ export const getBscHyperevmEnzoBTCWarpConfig = async (
     ...routerConfig.hyperevm,
     owner: safeOwners.hyperevm,
     type: TokenType.synthetic,
-    interchainSecurityModule: ISM_CONFIG,
   };
 
   return {
