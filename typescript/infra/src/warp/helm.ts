a09f20a50dcbb22359c4facfb5133b2003d8987d
Le Yu <6251863+ltyu@users.noreply.github.com>
2025-03-13 11:40:32 -0400

    Add latest docker image tag

--- typescript/infra/src/warp/helm.ts
+++ typescript/infra/src/warp/helm.ts
@@ -73,7 +73,7 @@ export class WarpRouteMonitorHelmManager extends HelmManager {
     return {
       image: {
         repository: 'gcr.io/abacus-labs-dev/hyperlane-monorepo',
-        tag: '1c4185a-20250312-165755',
+        tag: '89b6227-20250313-153031',
       },
       warpRouteId: this.warpRouteId,
       fullnameOverride: this.helmReleaseName,
