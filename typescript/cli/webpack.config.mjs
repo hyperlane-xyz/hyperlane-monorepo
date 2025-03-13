880454c510377bf601254495451024c81e5d4623
xeno097 <xeno097.cp@gmail.com>
2025-03-13 17:23:24 -0400

    fix(cli): maybe fix ci issues when bundling cli part 2

--- typescript/cli/webpack.config.mjs
+++ typescript/cli/webpack.config.mjs
@@ -1,4 +1,3 @@
-// webpack.config.js
 import * as path from 'path';
 import { fileURLToPath } from 'url';
 import webpack from 'webpack';
@@ -6,28 +5,63 @@ import webpack from 'webpack';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+// These packages should be included in the bundle otherwise they will be installed
+// when installing the cli with npm i -g which will fail if new changes are not in
+// in the NPM registry
+const bundleWhitelist = new Set([
+  '@hyperlane-xyz/registry',
+  '@hyperlane-xyz/sdk',
+  '@hyperlane-xyz/utils',
+]);
+
+// These packages won't play nice if bundled as commonjs modules
+const esmWhitelist = new Set(['chalk', 'latest-version']);
+
 export default {
   entry: './dist/cli.js',
   target: 'node',
   mode: 'production',
+  optimization: {
+    minimize: false,
+  },
   output: {
     filename: 'cli.js',
     path: path.resolve(__dirname, 'cli-bundle'),
+    library: { type: 'module' },
+  },
+  experiments: {
+    outputModule: true,
   },
   resolve: {
     extensions: ['.js', '.json'],
+    conditionNames: ['node', 'import'],
     modules: [
       path.resolve(__dirname, 'node_modules'),
       path.resolve(__dirname, '../node_modules'),
       'node_modules',
     ],
   },
-  experiments: {
-    outputModule: true,
-  },
+  externals: [
+    ({ request }, callback) => {
+      // Install as an external ES module if in esm whitelist.
+      if (esmWhitelist.has(request)) {
+        return callback(null, `module ${request}`);
+      }
+
+      // Bundle relative paths and whitelisted modules.
+      if (!request || request.startsWith('.') || bundleWhitelist.has(request)) {
+        return callback();
+      }
+
+      // All other packages will be installed when installing the package
+      callback(null, 'commonjs ' + request);
+    },
+  ],
   plugins: [
     new webpack.BannerPlugin({
-      banner: '#!/usr/bin/env node',
+      banner: `#!/usr/bin/env node
+import { createRequire } from 'module';
+globalThis.require = createRequire(import.meta.url);`,
       raw: true,
       entryOnly: true,
     }),
