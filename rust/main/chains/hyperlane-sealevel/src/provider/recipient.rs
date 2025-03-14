628afdc5ff3a07ccecd07b77131c461d7160f670
Danil Nemirovsky <4614623+ameten@users.noreply.github.com>
2025-03-14 15:37:23 +0000

    Use first non-native as recipient as fall back case

--- rust/main/chains/hyperlane-sealevel/src/provider/recipient.rs
+++ rust/main/chains/hyperlane-sealevel/src/provider/recipient.rs
@@ -1,13 +1,29 @@
 use std::collections::HashSet;
 
+use lazy_static::lazy_static;
 use solana_sdk::pubkey::Pubkey;
-use solana_transaction_status::{UiInstruction, UiParsedInstruction, UiTransaction};
+use solana_transaction_status::{
+    UiInstruction, UiParsedInstruction, UiPartiallyDecodedInstruction, UiTransaction,
+};
 
 use hyperlane_core::{ChainResult, H256, H512};
 
 use crate::error::HyperlaneSealevelError;
 use crate::provider::transaction::instructions;
 use crate::utils::decode_pubkey;
+
+lazy_static! {
+    static ref NATIVE_PROGRAMS: HashSet<String> = HashSet::from([
+        solana_sdk::bpf_loader_upgradeable::ID.to_string(),
+        solana_sdk::compute_budget::ID.to_string(),
+        solana_sdk::config::program::ID.to_string(),
+        solana_sdk::ed25519_program::ID.to_string(),
+        solana_sdk::secp256k1_program::ID.to_string(),
+        solana_sdk::stake::program::ID.to_string(),
+        solana_sdk::system_program::ID.to_string(),
+        solana_sdk::vote::program::ID.to_string(),
+    ]);
+}
 
 #[derive(Clone, Debug)]
 pub(crate) struct RecipientProvider {
@@ -29,7 +45,7 @@ impl RecipientProvider {
         println!("uitransaction: {:?}", transaction);
         let instructions = instructions(transaction)?;
 
-        let programs = instructions
+        let decoded = instructions
             .iter()
             .filter_map(|ii| {
                 if let UiInstruction::Parsed(iii) = ii {
@@ -48,6 +64,10 @@ impl RecipientProvider {
                     Some(iii)
                 }
             })
+            .collect::<Vec<&UiPartiallyDecodedInstruction>>();
+
+        let program_id = decoded
+            .iter()
             .filter(|program| {
                 self.programs.contains(&program.program_id)
                     || program
@@ -55,14 +75,20 @@ impl RecipientProvider {
                         .iter()
                         .any(|account| self.programs.contains(account))
             })
-            .map(|program| &program.program_id)
-            .collect::<Vec<&String>>();
+            .next()
+            .map(|program| program.program_id.clone());
 
-        let program_id = programs
-            .first()
-            .ok_or(HyperlaneSealevelError::NoNonNativePrograms(Box::new(*hash)))?;
+        let program_id = match program_id {
+            Some(p) => p,
+            None => decoded
+                .iter()
+                .filter(|ii| !NATIVE_PROGRAMS.contains(&ii.program_id))
+                .next()
+                .map(|i| i.program_id.clone())
+                .ok_or(HyperlaneSealevelError::NoNonNativePrograms(Box::new(*hash)))?,
+        };
 
-        let pubkey = decode_pubkey(program_id)?;
+        let pubkey = decode_pubkey(&program_id)?;
         let recipient = H256::from_slice(&pubkey.to_bytes());
         Ok(recipient)
     }
