eac32a60a75633c9d6421ed9a5f8fd015b72b409
Le Yu <6251863+ltyu@users.noreply.github.com>
2025-03-17 13:38:48 -0400

    Update tag to reflect changes to deploy-warp-monitor

--- typescript/infra/src/warp/helm.ts
+++ typescript/infra/src/warp/helm.ts
@@ -73,7 +73,7 @@ export class WarpRouteMonitorHelmManager extends HelmManager {
     return {
       image: {
         repository: 'gcr.io/abacus-labs-dev/hyperlane-monorepo',
-        tag: 'ceac574-20250312-143219',
+        tag: 'a09f20a-20250313-154105',
       },
       warpRouteId: this.warpRouteId,
       fullnameOverride: this.helmReleaseName,
