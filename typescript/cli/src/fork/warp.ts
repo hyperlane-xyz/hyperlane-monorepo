69b146399969755eaf9dde8b9931d7231f80b55d
xeno097 <xeno097.cp@gmail.com>
2025-03-19 16:50:39 -0400

    feat(cli): refactored the runWarpFork method to split it in smaller txs

--- typescript/cli/src/fork/warp.ts
+++ typescript/cli/src/fork/warp.ts
@@ -1,13 +1,20 @@
-import { JsonRpcProvider } from '@ethersproject/providers';
+import { JsonRpcProvider, Log } from '@ethersproject/providers';
+import { ethers } from 'ethers';
 import { z } from 'zod';
 import { $ } from 'zx';
 
 import {
+  ChainName,
   WarpCoreConfig,
   WarpRouteDeployConfigMailboxRequired,
   ZHash,
 } from '@hyperlane-xyz/sdk';
-import { ProtocolType, retryAsync } from '@hyperlane-xyz/utils';
+import {
+  Address,
+  ProtocolType,
+  deepEquals,
+  retryAsync,
+} from '@hyperlane-xyz/utils';
 
 import { CommandContext } from '../context/types.js';
 import { logGray, logRed } from '../logger.js';
@@ -27,6 +34,7 @@ export const EventAssertionSchema = z.discriminatedUnion('type', [
   z.object({
     type: z.literal(EventAssertionType.TOPIC_SIGNATURE),
     signature: z.string(),
+    args: z.array(z.string()).optional(),
   }),
 ]);
 
@@ -84,7 +92,6 @@ export async function runWarpFork({
     );
 
     const rpcUrl = chainMetadata.rpcUrls[0];
-
     if (!rpcUrl) {
       logRed(`Please specify either a symbol or warp config`);
       process.exit(0);
@@ -107,46 +114,125 @@ export async function runWarpFork({
       continue;
     }
 
-    if (currentChainForkConfig.impersonateAccounts.length !== 0) {
-      logGray(
-        `Impersonating accounts ${currentChainForkConfig.impersonateAccounts} on chain ${chainName}`,
-      );
-      await Promise.all(
-        currentChainForkConfig.impersonateAccounts.map((address) =>
-          provider.send('anvil_impersonateAccount', [address]),
-        ),
-      );
-    }
+    await handleImpersonations(
+      provider,
+      chainName,
+      currentChainForkConfig.impersonateAccounts,
+    );
 
-    if (currentChainForkConfig.transactions.length !== 0) {
-      logGray(`Executing transactions on chain ${chainName}`);
-      for (const transaction of currentChainForkConfig.transactions) {
-        const signer = provider.getSigner(transaction.from);
-
-        await provider.send('anvil_setBalance', [
-          transaction.from,
-          '10000000000000000000',
-        ]);
-
-        const pendingTx = await signer.sendTransaction({
-          to: transaction.to,
-          data: transaction.data,
-          value: transaction.value,
-        });
-
-        const txReceipt = await pendingTx.wait();
-
-        if (txReceipt.status == 0) {
-          throw new Error(
-            `Transaction ${transaction} reverted on chain ${chainName}`,
-          );
-        }
-
-        console.log(txReceipt);
-
-        console.log(txReceipt.logs);
-      }
-      logGray(`Successfully executed all transactions on chain ${chainName}`);
-    }
+    await handleTransactions(
+      provider,
+      chainName,
+      currentChainForkConfig.transactions,
+    );
   }
+}
+
+async function handleImpersonations(
+  provider: JsonRpcProvider,
+  chainName: ChainName,
+  accountsToImpersonate: Address[],
+): Promise<void> {
+  if (accountsToImpersonate.length === 0) {
+    return;
+  }
+
+  logGray(
+    `Impersonating accounts ${accountsToImpersonate} on chain ${chainName}`,
+  );
+  await Promise.all(
+    accountsToImpersonate.map((address) =>
+      provider.send('anvil_impersonateAccount', [address]),
+    ),
+  );
+}
+
+async function handleTransactions(
+  provider: JsonRpcProvider,
+  chainName: ChainName,
+  transactions: ForkedChainConfig['transactions'],
+): Promise<void> {
+  if (transactions.length === 0) {
+    return;
+  }
+
+  logGray(`Executing transactions on chain ${chainName}`);
+  for (const transaction of transactions) {
+    const signer = provider.getSigner(transaction.from);
+
+    await provider.send('anvil_setBalance', [
+      transaction.from,
+      '10000000000000000000',
+    ]);
+
+    const pendingTx = await signer.sendTransaction({
+      to: transaction.to,
+      data: transaction.data,
+      value: transaction.value,
+    });
+
+    const txReceipt = await pendingTx.wait();
+    if (txReceipt.status == 0) {
+      throw new Error(
+        `Transaction ${transaction} reverted on chain ${chainName}`,
+      );
+    }
+
+    transaction.eventAssertions.forEach((eventAssertion) =>
+      assertEvent(eventAssertion, txReceipt.logs),
+    );
+  }
+  logGray(`Successfully executed all transactions on chain ${chainName}`);
+}
+
+function assertEvent(eventAssertion: EventAssertion, logs: Log[]): boolean {
+  const [log] = logs.filter((rawLog) =>
+    eventAssertion.type === EventAssertionType.RAW_TOPIC
+      ? assertEventByTopic(eventAssertion, rawLog)
+      : assertEventBySignature(eventAssertion, rawLog),
+  );
+
+  return !!log;
+}
+
+function assertEventByTopic(
+  eventAssertion: Extract<
+    EventAssertion,
+    { type: EventAssertionType.RAW_TOPIC }
+  >,
+  rawLog: ethers.providers.Log,
+): boolean {
+  return rawLog.topics[0] === eventAssertion.topic;
+}
+
+function assertEventBySignature(
+  eventAssertion: Extract<
+    EventAssertion,
+    { type: EventAssertionType.TOPIC_SIGNATURE }
+  >,
+  rawLog: ethers.providers.Log,
+): boolean {
+  const eventInterface = new ethers.utils.Interface([eventAssertion.signature]);
+
+  let parsedLog: ethers.utils.LogDescription;
+  // parseLog throws if the event cannot be decoded
+  try {
+    parsedLog = eventInterface.parseLog(rawLog);
+
+    if (!parsedLog) {
+      return false;
+    }
+  } catch {
+    return false;
+  }
+
+  if (!eventAssertion.args) {
+    return true;
+  }
+
+  const logArgs = parsedLog.args
+    .slice(0, eventAssertion.args.length)
+    .map((arg) => String(arg));
+
+  return deepEquals(logArgs, eventAssertion.args);
 }
