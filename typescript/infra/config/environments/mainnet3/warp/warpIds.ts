f6096b73329aac6cd59780deb895c15277b64d7c
Trevor Porter <tkporter4@gmail.com>
2025-03-21 15:21:31 +0000

    feat: add LOGX warp route ID to infra (#5465)
    ### Description
    
    - As we are now relaying this, we need the warp monitor to track the ATA
    balance
    
    ### Drive-by changes
    
    <!--
    Are there any minor or drive-by changes also included?
    -->
    
    ### Related issues
    
    <!--
    - Fixes #[issue number here]
    -->
    
    ### Backward compatibility
    
    <!--
    Are these changes backward compatible? Are there any infrastructure
    implications, e.g. changes that would prohibit deploying older commits
    using this infra tooling?
    
    Yes/No
    -->
    
    ### Testing
    
    <!--
    What kind of testing have these changes undergone?
    
    None/Manual/Unit Tests
    -->

--- typescript/infra/config/environments/mainnet3/warp/warpIds.ts
+++ typescript/infra/config/environments/mainnet3/warp/warpIds.ts
@@ -76,6 +76,7 @@ export enum WarpRouteIds {
   SolanaSonicsvmLrtsSOL = 'lrtsSOL/solanamainnet-sonicsvm',
   SolanaSonicsvmSonicSOL = 'sonicSOL/solanamainnet-sonicsvm',
   ArbitrumEthereumSolanaTreasureSMOL = 'SMOL/arbitrum-ethereum-solanamainnet-treasure',
+  ArbitrumSolanaLOGX = 'LOGX/arbitrum-solanamainnet',
   // TODO: uncomment after merging the staging route to registry
   // this has been commented out as it leads to check-warp-deploy cron job failing
   // SuperTokenStaging = 'USDTSTAGING/base-celo-fraxtal-ink-lisk-mode-optimism-soneium-superseed-unichain-worldchain',
