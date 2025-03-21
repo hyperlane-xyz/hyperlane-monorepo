cd9ebaffbd292791c831d1e2d5152e9b70ca117b
Le Yu <6251863+ltyu@users.noreply.github.com>
2025-03-21 15:39:31 -0400

    Add solana to usdc superseed const

--- typescript/infra/src/config/warp.ts
+++ typescript/infra/src/config/warp.ts
@@ -74,6 +74,9 @@ export const tokens = {
   ink: {
     USDCe: '0xF1815bd50389c46847f0Bda824eC8da914045D14',
   },
+  solanamainnet: {
+    USDC: 'EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v',
+  },
 };
 
 export type RouterConfigWithoutOwner = Omit<RouterConfig, keyof OwnableConfig>;
