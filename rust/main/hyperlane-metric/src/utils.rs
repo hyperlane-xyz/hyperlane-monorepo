9b026995940585a77a21e3b7dc892c7c709e8266
Jeff Zhao <jeff.no.zhao@gmail.com>
2025-03-24 12:19:55 -0400

    feat: fix url parsing when there is no protocol provided

--- rust/main/hyperlane-metric/src/utils.rs
+++ rust/main/hyperlane-metric/src/utils.rs
@@ -5,13 +5,18 @@ use url::Url;
 /// converts url into a host:port string
 pub fn url_to_host_info(url: &Url) -> Option<String> {
     let mut s = String::new();
+
     if let Some(host) = url.host_str() {
         s.push_str(host);
-        if let Some(port) = url.port() {
+        if let Some(port) = url.port_or_known_default() {
             write!(&mut s, ":{port}").unwrap();
         }
         Some(s)
     } else {
-        None
+        let url_str = url.to_string();
+        match url_str.split_once('/') {
+            Some((left, _)) => Some(left.into()),
+            None => Some(url_str),
+        }
     }
 }
