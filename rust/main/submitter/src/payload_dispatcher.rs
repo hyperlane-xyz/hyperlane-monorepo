ac3932675f69c703b847626670c5805ce418ca9b
Daniel Savu <23065004+daniel-savu@users.noreply.github.com>
2025-03-25 23:32:13 +0000

    feat(submitter): define stage modules, use tx db in dispatcher

--- rust/main/submitter/src/payload_dispatcher.rs
+++ rust/main/submitter/src/payload_dispatcher.rs
@@ -1,8 +1,11 @@
 // TODO: re-enable clippy warnings
 #![allow(unused_imports)]
 
+mod building_stage;
 mod dispatcher;
 mod entrypoint;
+mod finality_stage;
+mod inclusion_stage;
 
 pub use dispatcher::*;
 pub use entrypoint::*;
--- rust/main/submitter/src/payload_dispatcher/dispatcher.rs
+++ rust/main/submitter/src/payload_dispatcher/dispatcher.rs
@@ -15,6 +15,7 @@ use tracing::instrument::Instrumented;
 use crate::{
     chain_tx_adapter::{AdaptsChain, ChainTxAdapterBuilder},
     payload::PayloadDb,
+    transaction::TransactionDb,
 };
 
 /// Settings for `PayloadDispatcher`
@@ -29,21 +30,31 @@ pub struct PayloadDispatcherSettings {
 }
 
 pub struct PayloadDispatcherState {
-    pub(crate) db: Arc<dyn PayloadDb>,
+    pub(crate) payload_db: Arc<dyn PayloadDb>,
+    pub(crate) tx_db: Arc<dyn TransactionDb>,
     pub(crate) adapter: Box<dyn AdaptsChain>,
 }
 
 impl PayloadDispatcherState {
-    pub fn new(db: Arc<dyn PayloadDb>, adapter: Box<dyn AdaptsChain>) -> Self {
-        Self { db, adapter }
+    pub fn new(
+        payload_db: Arc<dyn PayloadDb>,
+        tx_db: Arc<dyn TransactionDb>,
+        adapter: Box<dyn AdaptsChain>,
+    ) -> Self {
+        Self {
+            payload_db,
+            tx_db,
+            adapter,
+        }
     }
 
     pub fn try_from_settings(settings: PayloadDispatcherSettings) -> Result<Self> {
         let adapter = ChainTxAdapterBuilder::build(&settings.chain_conf, &settings.raw_chain_conf);
         let db = DB::from_path(&settings.db_path)?;
-        let rocksdb = HyperlaneRocksDB::new(&settings.domain, db);
-        let payload_db = Arc::new(rocksdb) as Arc<dyn PayloadDb>;
-        Ok(Self::new(payload_db, adapter))
+        let rocksdb = Arc::new(HyperlaneRocksDB::new(&settings.domain, db));
+        let payload_db = rocksdb.clone() as Arc<dyn PayloadDb>;
+        let tx_db = rocksdb as Arc<dyn TransactionDb>;
+        Ok(Self::new(payload_db, tx_db, adapter))
     }
 }
 
