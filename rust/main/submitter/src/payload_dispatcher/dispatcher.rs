2f9ec5b347ea6bf14faf45dfdb69ea1c1fd57d2f
Daniel Savu <23065004+daniel-savu@users.noreply.github.com>
2025-03-25 11:16:59 +0000

    feat(submitter): Entrypoint (#5741)
    ### Description
    
    Design doc section for reference:
    https://www.notion.so/hyperlanexyz/Relayer-Submitter-Redesign-17b6d35200d68020b516c6c9bc455213?pvs=4#1a56d35200d6808e8b2eccb5a9e085bd
    
    Implements the `Entrypoint` component of `PayloadDispatcher`, using the
    following trait
    ```rust
    pub trait Entrypoint {
        async fn send_payload(&self, payloads: FullPayload) -> Result<()>;
        async fn payload_status(&self, payload_id: PayloadId) -> Result<PayloadStatus>;
        async fn estimate_gas_limit(&self, payload: &FullPayload) -> Result<GasLimit>;
    }
    ```
    
    
    ### Drive-by changes
    
    - The `db` in `PayloadDispatcherState` is now held in an `Arc` to make
    testing easier
    - `PayloadDb` and `TransactionDb` trait methods are made async to avoid
    assuming we'll always use rocksdb
    
    ### Related issues
    
    - Fixes https://github.com/hyperlane-xyz/hyperlane-monorepo/issues/5730
    
    ### Backward compatibility
    
    Yes
    
    ### Testing
    
    `entrypoint.rs` has very high testing coverage, see the cargo-llvm-cov
    report below (the columns are `Function Coverage | Line Coverage |
    Region Coverage`)
    
    ![Screenshot 2025-03-24 at 22 14
    19](https://github.com/user-attachments/assets/40fc7e76-2653-4eb7-8f63-eb205f8083b2)

--- rust/main/submitter/src/payload_dispatcher/dispatcher.rs
+++ rust/main/submitter/src/payload_dispatcher/dispatcher.rs
@@ -1,14 +1,21 @@
 // TODO: re-enable clippy warnings
 #![allow(dead_code)]
 
-use std::path::PathBuf;
+use eyre::Result;
+use std::{path::PathBuf, sync::Arc};
 
-use hyperlane_base::settings::{ChainConf, RawChainConf};
+use hyperlane_base::{
+    db::{HyperlaneRocksDB, DB},
+    settings::{ChainConf, RawChainConf},
+};
 use hyperlane_core::HyperlaneDomain;
 use tokio::task::JoinHandle;
 use tracing::instrument::Instrumented;
 
-use crate::chain_tx_adapter::{AdaptsChain, ChainTxAdapterBuilder};
+use crate::{
+    chain_tx_adapter::{AdaptsChain, ChainTxAdapterBuilder},
+    payload::PayloadDb,
+};
 
 /// Settings for `PayloadDispatcher`
 #[derive(Debug)]
@@ -22,29 +29,33 @@ pub struct PayloadDispatcherSettings {
 }
 
 pub struct PayloadDispatcherState {
-    // db: DispatcherDb,
-    adapter: Box<dyn AdaptsChain>,
+    pub(crate) db: Arc<dyn PayloadDb>,
+    pub(crate) adapter: Box<dyn AdaptsChain>,
 }
 
 impl PayloadDispatcherState {
-    pub fn new(settings: PayloadDispatcherSettings) -> Self {
-        let adapter = ChainTxAdapterBuilder::build(&settings.chain_conf, &settings.raw_chain_conf);
-        Self { adapter }
+    pub fn new(db: Arc<dyn PayloadDb>, adapter: Box<dyn AdaptsChain>) -> Self {
+        Self { db, adapter }
     }
 
-    pub(crate) fn from_adapter(adapter: Box<dyn AdaptsChain>) -> Self {
-        Self { adapter }
+    pub fn try_from_settings(settings: PayloadDispatcherSettings) -> Result<Self> {
+        let adapter = ChainTxAdapterBuilder::build(&settings.chain_conf, &settings.raw_chain_conf);
+        let db = DB::from_path(&settings.db_path)?;
+        let rocksdb = HyperlaneRocksDB::new(&settings.domain, db);
+        let payload_db = Arc::new(rocksdb) as Arc<dyn PayloadDb>;
+        Ok(Self::new(payload_db, adapter))
     }
 }
+
 pub struct PayloadDispatcher {
     inner: PayloadDispatcherState,
 }
 
 impl PayloadDispatcher {
-    pub fn new(settings: PayloadDispatcherSettings) -> Self {
-        Self {
-            inner: PayloadDispatcherState::new(settings),
-        }
+    pub fn try_from_settings(settings: PayloadDispatcherSettings) -> Result<Self> {
+        Ok(Self {
+            inner: PayloadDispatcherState::try_from_settings(settings)?,
+        })
     }
 
     pub fn spawn(self) -> Instrumented<JoinHandle<()>> {
