b74274c72d7444c1a6b631627481188f5a2de315
gitmp01 <mauro.piazza.01@gmail.com>
2025-03-26 22:27:56 +0100

    chore(ValueTransferBridgeNative): move L2 withdrawal to `_transferFromSender`

--- solidity/contracts/token/HypNative.sol
+++ solidity/contracts/token/HypNative.sol
@@ -96,7 +96,7 @@ contract HypNative is FungibleTokenRouter, MovableCollateralRouter {
      */
     function _transferFromSender(
         uint256
-    ) internal pure override returns (bytes memory) {
+    ) internal virtual override returns (bytes memory) {
         return bytes(""); // no metadata
     }
 
