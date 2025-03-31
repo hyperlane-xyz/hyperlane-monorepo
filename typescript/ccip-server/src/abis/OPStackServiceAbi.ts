1afe078650ee1dbb8af23372cf85a0f541d26a8b
gitmp01 <mauro.piazza.01@gmail.com>
2025-03-31 12:45:34 +0200

    chore(OPL2ToL1CcipReadIsm): restore double off-chain calls
    ...to avoid expensise finalizeWithdrawal calldata

--- typescript/ccip-server/src/abis/OPStackServiceAbi.ts
+++ typescript/ccip-server/src/abis/OPStackServiceAbi.ts
@@ -1,5 +1,6 @@
 const OPStackServiceAbi = [
-  'function getOffchainData(bytes) public view returns (tuple(uint256,address,address,uint256,uint256,bytes),uint256,tuple(bytes32,bytes32,bytes32,bytes32),bytes[])',
+  'function getWithdrawalProof(bytes) public view returns (tuple(uint256,address,address,uint256,uint256,bytes),uint256,tuple(bytes32,bytes32,bytes32,bytes32),bytes[])',
+  'function getFinalizeWithdrawalTx(bytes) public view returns (tuple(uint256,address,address,uint256,uint256,bytes))',
 ];
 
 export { OPStackServiceAbi };
