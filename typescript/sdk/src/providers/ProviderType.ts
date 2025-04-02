07321f6f0f02e7b5d8142e8edf7b45fdc87e9183
ljankovic-txfusion <131957285+ljankovic-txfusion@users.noreply.github.com>
2025-04-02 15:14:33 +0200

    feat: ZKSync provider types (#5207)
    ### Description
    
    This PR introduces specific ZKSync Provider types for SDK.
    
    [PR to
    feat/core-zksync](https://github.com/txfusion/hyperlane-monorepo/pull/20)
    
    ### Drive-by changes
    
    - Added ZKSync provider types and interfaces
    - Created builder functions for ZKSync provider configuration
    
    ### Related issues
    
    https://linear.app/hyperlane-xyz/issue/ENG-1149
    
    ### Backward compatibility
    
    Yes
    
    ### Testing
    
    Build passing
    
    ---------
    
    Co-authored-by: Morteza Shojaei <31728528+mortezashojaei@users.noreply.github.com>
    Co-authored-by: mshojaei-txfusion <138107084+mshojaei-txfusion@users.noreply.github.com>
    Co-authored-by: Le Yu <6251863+ltyu@users.noreply.github.com>
    Co-authored-by: Paul Balaji <10051819+paulbalaji@users.noreply.github.com>

--- typescript/sdk/src/providers/ProviderType.ts
+++ typescript/sdk/src/providers/ProviderType.ts
@@ -28,6 +28,11 @@ import type {
   Transaction as VTransaction,
   TransactionReceipt as VTransactionReceipt,
 } from 'viem';
+import {
+  Contract as ZKSyncBaseContract,
+  Provider as ZKSyncBaseProvider,
+  types as zkSyncTypes,
+} from 'zksync-ethers';
 
 import { Annotated, ProtocolType } from '@hyperlane-xyz/utils';
 
@@ -39,6 +44,7 @@ export enum ProviderType {
   CosmJsWasm = 'cosmjs-wasm',
   GnosisTxBuilder = 'gnosis-txBuilder',
   Starknet = 'starknet',
+  ZkSync = 'zksync',
 }
 
 export const PROTOCOL_TO_DEFAULT_PROVIDER_TYPE: Record<
@@ -152,6 +158,11 @@ export interface StarknetJsProvider
   provider: StarknetProvider;
 }
 
+export interface ZKSyncProvider extends TypedProviderBase<ZKSyncBaseProvider> {
+  type: ProviderType.ZkSync;
+  provider: ZKSyncBaseProvider;
+}
+
 export type TypedProvider =
   | EthersV5Provider
   // | EthersV6Provider
@@ -159,7 +170,8 @@ export type TypedProvider =
   | SolanaWeb3Provider
   | CosmJsProvider
   | CosmJsWasmProvider
-  | StarknetJsProvider;
+  | StarknetJsProvider
+  | ZKSyncProvider;
 
 /**
  * Contracts with discriminated union of provider type
@@ -204,6 +216,11 @@ export interface StarknetJsContract
   contract: StarknetContract;
 }
 
+export interface ZKSyncContract extends TypedContractBase<ZKSyncBaseContract> {
+  type: ProviderType.ZkSync;
+  contract: ZKSyncBaseContract;
+}
+
 export type TypedContract =
   | EthersV5Contract
   // | EthersV6Contract
@@ -211,7 +228,8 @@ export type TypedContract =
   | SolanaWeb3Contract
   | CosmJsContract
   | CosmJsWasmContract
-  | StarknetJsContract;
+  | StarknetJsContract
+  | ZKSyncBaseContract;
 
 /**
  * Transactions with discriminated union of provider type
@@ -258,6 +276,12 @@ export interface StarknetJsTransaction
   transaction: StarknetInvocation;
 }
 
+export interface ZKSyncTransaction
+  extends TypedTransactionBase<zkSyncTypes.TransactionRequest> {
+  type: ProviderType.ZkSync;
+  transaction: zkSyncTypes.TransactionRequest;
+}
+
 export type TypedTransaction =
   | EthersV5Transaction
   // | EthersV6Transaction
@@ -265,7 +289,8 @@ export type TypedTransaction =
   | SolanaWeb3Transaction
   | CosmJsTransaction
   | CosmJsWasmTransaction
-  | StarknetJsTransaction;
+  | StarknetJsTransaction
+  | ZKSyncTransaction;
 
 /**
  * Transaction receipt/response with discriminated union of provider type
@@ -312,10 +337,17 @@ export interface StarknetJsTransactionReceipt
   receipt: StarknetTxReceipt | StarknetReceiptTx;
 }
 
+export interface ZKSyncTransactionReceipt
+  extends TypedTransactionReceiptBase<zkSyncTypes.TransactionReceipt> {
+  type: ProviderType.ZkSync;
+  receipt: zkSyncTypes.TransactionReceipt;
+}
+
 export type TypedTransactionReceipt =
   | EthersV5TransactionReceipt
   | ViemTransactionReceipt
   | SolanaWeb3TransactionReceipt
   | CosmJsTransactionReceipt
   | CosmJsWasmTransactionReceipt
-  | StarknetJsTransactionReceipt;
+  | StarknetJsTransactionReceipt
+  | ZKSyncTransactionReceipt;
