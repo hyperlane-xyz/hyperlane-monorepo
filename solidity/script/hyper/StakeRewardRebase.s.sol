b44e023302c1a5115429cba98f1075478b6513bb
Yorke Rhodes <yorke@hyperlane.xyz>
2025-04-03 16:32:57 -0700

    Fork test stHYPER warp route

--- solidity/script/hyper/StakeRewardRebase.s.sol
+++ solidity/script/hyper/StakeRewardRebase.s.sol
@@ -6,9 +6,14 @@ import "forge-std/Script.sol";
 import {TypeCasts} from "../../contracts/libs/TypeCasts.sol";
 import {HyperToken} from "../../contracts/token/extensions/HyperToken.sol";
 import {HypERC20} from "../../contracts/token/HypERC20.sol";
+import {HypERC4626Collateral} from "../../contracts/token/extensions/HypERC4626Collateral.sol";
+import {HypERC4626} from "../../contracts/token/extensions/HypERC4626.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 import {MockMailbox} from "../../contracts/mock/MockMailbox.sol";
 import {IVault} from "../../contracts/interfaces/network/vault/IVault.sol";
+import {ICompoundStakerRewards} from "../../contracts/interfaces/network/rewards/ICompoundStakerRewards.sol";
+import {IStakerRewards} from "../../contracts/interfaces/network/rewards/IStakerRewards.sol";
 
 import "forge-std/StdCheats.sol";
 
@@ -19,21 +24,34 @@ contract StakeRewardRebase is Script, StdCheats {
     string STAKE_RPC_URL = vm.envString("STAKE_RPC_URL");
     string REBASE_RPC_URL = vm.envString("REBASE_RPC_URL");
 
-    address WARP_ROUTE_ADDRESS = vm.envAddress("WARP_ROUTE_ADDRESS");
+    address STAKED_WARP_ROUTE_ADDRESS =
+        vm.envAddress("STAKED_WARP_ROUTE_ADDRESS");
 
     uint256 stakeFork;
     uint32 stakeDomainId;
     HyperToken public collateral;
+    HypERC4626Collateral public stakedCollateral;
+    IVault public vault;
+    IStakerRewards public rewards;
+    ICompoundStakerRewards compoundStakerRewards;
 
     uint256 rebaseFork;
     uint32 rebaseDomainId;
     HypERC20 public synthetic;
+    HypERC4626 public rebasingSynthetic;
 
     uint256 BALANCE = 1_000_000e18; // 1 million tokens, adjust decimals as needed
 
     function setUp() public {
         stakeFork = vm.createSelectFork(STAKE_RPC_URL);
-        collateral = HyperToken(WARP_ROUTE_ADDRESS);
+        stakedCollateral = HypERC4626Collateral(STAKED_WARP_ROUTE_ADDRESS);
+        compoundStakerRewards = ICompoundStakerRewards(
+            address(stakedCollateral.vault())
+        );
+        rewards = compoundStakerRewards.rewards();
+        vault = compoundStakerRewards.vault();
+
+        collateral = HyperToken(vault.collateral());
         deal(
             address(collateral),
             address(this), // sender
@@ -50,6 +68,9 @@ contract StakeRewardRebase is Script, StdCheats {
         vm.selectFork(stakeFork);
         synthetic = HypERC20(
             collateral.routers(rebaseDomainId).bytes32ToAddress()
+        );
+        rebasingSynthetic = HypERC4626(
+            stakedCollateral.routers(rebaseDomainId).bytes32ToAddress()
         );
 
         vm.selectFork(rebaseFork);
@@ -70,17 +91,53 @@ contract StakeRewardRebase is Script, StdCheats {
     }
 
     function run() external {
+        uint256 transferAmount = 1000;
+
         vm.selectFork(stakeFork);
         uint256 fee = collateral.quoteGasPayment(rebaseDomainId);
         vm.deal(address(this), fee);
         collateral.transferRemote{value: fee}(
             rebaseDomainId,
             msg.sender.addressToBytes32(),
-            1000
+            transferAmount
         );
 
         vm.selectFork(rebaseFork);
         MockMailbox(address(synthetic.mailbox())).handleNextInboundMessage();
-        assert(synthetic.balanceOf(msg.sender) == 1000);
+        assert(synthetic.balanceOf(msg.sender) == transferAmount);
+
+        vm.selectFork(stakeFork);
+        uint256 stakeAmount = 2000;
+        collateral.approve(address(vault), stakeAmount);
+        vault.deposit(address(this), stakeAmount);
+
+        IERC20(address(vault)).approve(address(stakedCollateral), stakeAmount);
+        fee = stakedCollateral.quoteGasPayment(rebaseDomainId);
+        vm.deal(address(this), fee);
+        stakedCollateral.transferRemote{value: fee}(
+            rebaseDomainId,
+            msg.sender.addressToBytes32(), // recipient
+            stakeAmount
+        );
+
+        vm.selectFork(rebaseFork);
+        MockMailbox(address(rebasingSynthetic.mailbox()))
+            .handleNextInboundMessage();
+        assert(rebasingSynthetic.balanceOf(msg.sender) == stakeAmount);
+
+        // address network = address(this);
+
+        // TODO:
+        // uint256 rewardAmount = 3000;
+        // rewards.distributeRewards(
+        //     network,
+        //     address(collateral),
+        //     rewardAmount,
+        //     abi.encode(uint48(block.timestamp), 0)
+        // );
+        // 1. distribute staking rewards
+        // 2. compound rewards
+        // 3. rebase the synthetic token
+        // 4. assert the balance has increased
     }
 }
