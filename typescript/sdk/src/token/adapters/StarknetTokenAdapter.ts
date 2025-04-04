535a725d13df4b879cff51ee7925b9b13c0cca54
ljankovic-txfusion <lazar@txfusion.io>
2025-04-04 14:44:47 +0200

    fix: update gas quote return type to use bigint literal

--- typescript/sdk/src/token/adapters/StarknetTokenAdapter.ts
+++ typescript/sdk/src/token/adapters/StarknetTokenAdapter.ts
@@ -90,7 +90,7 @@ export class StarknetHypSyntheticAdapter
   async quoteTransferRemoteGas(
     _destination: Domain,
   ): Promise<InterchainGasQuote> {
-    return { amount: BigInt(0) };
+    return { amount: 0n };
   }
 
   async populateTransferRemoteTx({
@@ -104,7 +104,7 @@ export class StarknetHypSyntheticAdapter
       destination,
       recipient,
       BigInt(weiAmountOrId.toString()),
-      BigInt(0),
+      0n,
       nonOption,
       nonOption,
     );
