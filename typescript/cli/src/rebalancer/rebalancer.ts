7499181e2344d9745d14bdc4832c0804d23fdd25
Fernando Zavalia <24811313+fzavalia@users.noreply.github.com>
2025-04-07 17:04:10 -0300

    feat: Add hyperlane rebalancer dir and class (#8)

---
+++ typescript/cli/src/rebalancer/rebalancer.ts
@@ -0,0 +1,11 @@
+export class HyperlaneRebalancer {
+  start(): void {
+    // Placeholder to simulate a long running process.
+    // The real implementation will be added later.
+    setInterval(() => {}, 1000);
+  }
+
+  stop(): void {
+    // Implement
+  }
+}
