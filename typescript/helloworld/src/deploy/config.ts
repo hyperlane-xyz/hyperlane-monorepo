8e47817aa683261d7639f4788f59bdba141e7423
Andrey Taranov <86911+antigremlin@users.noreply.github.com>
2025-04-08 16:50:55 +0100

    refactor: adjust types for optional mailboxes

--- typescript/helloworld/src/deploy/config.ts
+++ typescript/helloworld/src/deploy/config.ts
@@ -1,7 +1,9 @@
 import { chainMetadata } from '@hyperlane-xyz/registry';
 import { RouterConfig } from '@hyperlane-xyz/sdk';
 
-export type HelloWorldConfig = RouterConfig;
+import { MailboxAddress } from '../../../sdk/src/router/types.js';
+
+export type HelloWorldConfig = RouterConfig & MailboxAddress;
 
 // SET DESIRED NETWORKS HERE OR USE THE DEFAULT SET FROM THE REGISTRY
 export const prodConfigs = {
