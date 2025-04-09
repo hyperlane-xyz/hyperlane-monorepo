b5b20fd6d9603f5034f3b8255ae40d916b873735
Le Yu <6251863+ltyu@users.noreply.github.com>
2025-04-09 18:09:47 -0400

    Update pattern to regex and reduce restriction on main branch

--- typescript/github-proxy/src/index.ts
+++ typescript/github-proxy/src/index.ts
@@ -1,13 +1,15 @@
 import { DISALLOWED_URL_MSG } from './errors.js';
 
-const GITHUB_API_ALLOWLIST = [
-  '/repos/hyperlane-xyz/hyperlane-registry/git/trees/main',
+export const GITHUB_API_ALLOWLIST = [
+  /^\/repos\/hyperlane-xyz\/hyperlane-registry\/.*/,
 ];
 const GITPUB_API_HOST = 'https://api.github.com';
 export default {
   async fetch(request, env, _ctx): Promise<Response> {
     const apiUrlPath = new URL(request.url).pathname;
-    const isAllowed = GITHUB_API_ALLOWLIST.includes(apiUrlPath);
+    const isAllowed = GITHUB_API_ALLOWLIST.some((pattern) =>
+      pattern.test(apiUrlPath),
+    );
     if (!isAllowed) {
       return new Response(DISALLOWED_URL_MSG, { status: 401 });
     }
