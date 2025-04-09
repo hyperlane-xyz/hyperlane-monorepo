a67219d5073e1a02e08d8c2dd29764000264d1e9
Fernando Zavalia <24811313+fzavalia@users.noreply.github.com>
2025-04-09 11:59:05 -0300

    fix: Use sleep from utils instead of creating a new function

--- typescript/cli/src/tests/warp/warp-rebalancer.e2e-test.ts
+++ typescript/cli/src/tests/warp/warp-rebalancer.e2e-test.ts
@@ -1,12 +1,9 @@
 import { expect } from 'chai';
 
+import { sleep } from '@hyperlane-xyz/utils';
+
 import { DEFAULT_E2E_TEST_TIMEOUT } from '../commands/helpers.js';
 import { hyperlaneWarpRebalancer } from '../commands/warp.js';
-
-// Wait for a given amount of time
-function wait(ms: number) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
 
 describe('hyperlane warp rebalancer e2e tests', async function () {
   this.timeout(2 * DEFAULT_E2E_TEST_TIMEOUT);
@@ -17,13 +14,13 @@ describe('hyperlane warp rebalancer e2e tests', async function () {
       const process = hyperlaneWarpRebalancer().stdio('pipe');
 
       // Wait for the process to start
-      await wait(3000);
+      await sleep(5000);
 
       // Stop the process
       await process.kill('SIGINT');
 
       // Wait for the process to stop
-      await wait(1000);
+      await sleep(1000);
 
       // Get the output
       const text = await process.text();
