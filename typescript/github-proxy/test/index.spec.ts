16eb3fc6bbec0726a35f50a2c89b4cbfec1a2876
Le Yu <6251863+ltyu@users.noreply.github.com>
2025-04-09 19:03:35 -0400

    Add test for looser allowlist. Add mock to fetch

--- typescript/github-proxy/test/index.spec.ts
+++ typescript/github-proxy/test/index.spec.ts
@@ -1,10 +1,20 @@
 import { faker } from '@faker-js/faker';
-import { SELF } from 'cloudflare:test';
-import { describe, expect, it } from 'vitest';
+import { SELF, fetchMock } from 'cloudflare:test';
+import { afterEach, beforeEach, describe, expect, it } from 'vitest';
 
 import { DISALLOWED_URL_MSG } from '../src/errors.js';
 
-describe('Hello World worker', () => {
+describe('Github proxy worker', () => {
+  beforeEach(() => {
+    // Enable outbound request mocking...
+    fetchMock.activate();
+    // ...and throw errors if an outbound request isn't mocked
+    fetchMock.disableNetConnect();
+  });
+
+  afterEach(() => {
+    fetchMock.deactivate();
+  });
   it('returns empty response if pathname provided is not a valid api url', async () => {
     const results = await SELF.fetch('https://example.com/favicon.ico');
 
@@ -30,5 +40,21 @@ describe('Hello World worker', () => {
       expect(results.status).toBe(401);
       expect(await results.text()).toBe(DISALLOWED_URL_MSG);
     }
+  });
+
+  it('allows "/repos/hyperlane-xyz/hyperlane-registry/git/trees/*"', async () => {
+    const branchName = faker.git.branch();
+    fetchMock
+      .get('https://api.github.com')
+      .intercept({
+        path: `/repos/hyperlane-xyz/hyperlane-registry/git/trees/${branchName}?recursive=true`,
+      })
+      .reply(200, 'Ok');
+    const results = await SELF.fetch(
+      `https://example.com/repos/hyperlane-xyz/hyperlane-registry/git/trees/${branchName}`,
+    );
+
+    expect(results.status).toBe(200);
+    expect(await results.text()).toBe('Ok');
   });
 });
