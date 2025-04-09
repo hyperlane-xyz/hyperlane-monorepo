9aa28ec1f02c71c51af0fe08738925882f83fac6
Morteza Shojaei <morteza@txfusion.io>
2025-04-09 19:20:58 +0330

    feat(yaml): add ESLint rule to sort YAML arrays by specified keys

--- typescript/utils/src/eslint-rules/index.ts
+++ typescript/utils/src/eslint-rules/index.ts
@@ -1,8 +1,16 @@
 import noRestrictedImportsFromExports from './no-restricted-imports-from-exports.js';
+import sortYamlArrays from './sort-yaml-arrays.js';
 
 export const importRestrictionsPlugin = {
   name: '@hyperlane/import-restrictions',
   rules: {
     'no-restricted-imports-from-exports': noRestrictedImportsFromExports,
+  },
+};
+
+export const sortYamlArraysPlugin = {
+  name: '@hyperlane/sort-yaml-arrays',
+  rules: {
+    'sort-yaml-arrays': sortYamlArrays,
   },
 };
