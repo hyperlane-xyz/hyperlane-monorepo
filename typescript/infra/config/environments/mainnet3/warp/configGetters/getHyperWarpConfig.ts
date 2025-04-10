1dbb9a3f73e9248fc45bbdbb06aa8eb5a565a304
Mo Hussan <22501692+Mo-Hussain@users.noreply.github.com>
2025-04-10 21:43:34 +0100

    export OWNERS

--- typescript/infra/config/environments/mainnet3/warp/configGetters/getHyperWarpConfig.ts
+++ typescript/infra/config/environments/mainnet3/warp/configGetters/getHyperWarpConfig.ts
@@ -31,7 +31,7 @@ const STAKED_TOKEN_CONFIG = {
 
 const STAKED_TOKEN_CHAINS = [COLLATERAL_CHAIN, 'bsc'] as const;
 
-const OWNERS = {
+export const OWNERS = {
   ethereum:
     process.env.ACCESS_MANAGER || '0xf0b850930ede8807e7F472b610b017c187E0e493',
   // get-owner-ica output
@@ -50,7 +50,7 @@ export const getHyperWarpConfig = async (
 ): Promise<ChainMap<HypTokenRouterConfig>> => {
   return Object.fromEntries(
     TOKEN_CHAINS.map((chain) => {
-      let config = {
+      const config = {
         ...routerConfig[chain],
         ...TOKEN_CONFIG,
         owner: OWNERS[chain],
@@ -83,7 +83,7 @@ export const getStakedHyperWarpConfig = async (
 ): Promise<ChainMap<HypTokenRouterConfig>> => {
   return Object.fromEntries(
     STAKED_TOKEN_CHAINS.map((chain) => {
-      let config = {
+      const config = {
         ...routerConfig[chain],
         ...STAKED_TOKEN_CONFIG,
         owner: OWNERS[chain],
