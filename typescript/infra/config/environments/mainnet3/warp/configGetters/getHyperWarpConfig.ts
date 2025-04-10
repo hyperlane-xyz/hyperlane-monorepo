a4f6bff9694037a2b848a7b8e483b4fe8642a3ab
Yorke Rhodes <yorke@hyperlane.xyz>
2025-04-10 14:24:03 -0400

    Configure AM and owners

--- typescript/infra/config/environments/mainnet3/warp/configGetters/getHyperWarpConfig.ts
+++ typescript/infra/config/environments/mainnet3/warp/configGetters/getHyperWarpConfig.ts
@@ -32,13 +32,12 @@ const STAKED_TOKEN_CONFIG = {
 const STAKED_TOKEN_CHAINS = [COLLATERAL_CHAIN, 'bsc'] as const;
 
 const OWNERS = {
-  //TODO: configure deployed AccessManager
-  ethereum: '',
+  ethereum: '0xf0b850930ede8807e7F472b610b017c187E0e493',
   // get-owner-ica output
-  arbitrum: '',
-  optimism: '',
-  base: '',
-  bsc: '',
+  arbitrum: '0xC44C6eb6Fe37A6389B6b741A0A17dcB6b1aAbC89',
+  base: '0xf189b22a6Be9378C5cDa94a6E72C6d18611291cA',
+  optimism: '0x6Fd6Be005D6e122b51b33044968d6CDB5F7A292c',
+  bsc: '0xaE1DA73F3aB27F30b32239114d4d14c789D64176',
 };
 
 const COMPOUND_STAKING_REWARDS = '';
