095ebd3a11bdd79faea128220344f8289e510dca
Daniel Savu <23065004+daniel-savu@users.noreply.github.com>
2025-04-14 19:00:09 +0100

    no-match-test

--- solidity/coverage.sh
+++ solidity/coverage.sh
@@ -7,4 +7,5 @@ forge coverage \
     --report lcov \
     --report summary \
     --no-match-coverage "(test|mock|node_modules|script)" \
+    --no-match-test "fork" \
     --ir-minimum # https://github.com/foundry-rs/foundry/issues/3357
