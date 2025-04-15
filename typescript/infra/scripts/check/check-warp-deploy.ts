a53bada252595b5727b8ceddc5759c2e5a665b38
Le Yu <6251863+ltyu@users.noreply.github.com>
2025-04-15 14:27:45 -0400

    Use github registry first

--- typescript/infra/scripts/check/check-warp-deploy.ts
+++ typescript/infra/scripts/check/check-warp-deploy.ts
@@ -42,7 +42,7 @@ async function main() {
     WarpRouteIds.ArbitrumBaseBlastBscEthereumGnosisLiskMantleModeOptimismPolygonScrollZeroNetworkZoraMainnet,
   ];
 
-  const registries = [DEFAULT_REGISTRY_URI, DEFAULT_GITHUB_REGISTRY];
+  const registries = [DEFAULT_GITHUB_REGISTRY, DEFAULT_REGISTRY_URI];
   const warpCoreConfigMap = await getWarpConfigMapFromMergedRegistry(
     registries,
   );
