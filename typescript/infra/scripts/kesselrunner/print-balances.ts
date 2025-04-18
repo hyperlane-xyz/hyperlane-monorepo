e42d3c69ccd86d05c97f541f36b1993c19275985
pbio <10051819+paulbalaji@users.noreply.github.com>
2025-04-18 15:05:41 +0100

    print falcon balances

--- typescript/infra/scripts/kesselrunner/print-balances.ts
+++ typescript/infra/scripts/kesselrunner/print-balances.ts
@@ -1,12 +1,62 @@
 import { formatUnits } from 'ethers/lib/utils.js';
 
+import { IERC20__factory } from '@hyperlane-xyz/core';
 import { rootLogger } from '@hyperlane-xyz/utils';
 
-import { KESSEL_RUN_FUNDER_CONFIG } from '../../src/kesselrunner/config.js';
+import {
+  KESSEL_RUN_FUNDER_CONFIG,
+  KESSEL_RUN_SPICE_ROUTE,
+  MILLENNIUM_FALCON_ADDRESS,
+} from '../../src/kesselrunner/config.js';
 import { getKesselRunMultiProvider } from '../../src/kesselrunner/utils.js';
 
 async function printBalances() {
   const { multiProvider, targetNetworks } = await getKesselRunMultiProvider();
+
+  const spiceBalancesObject = await Promise.all(
+    Object.values(KESSEL_RUN_SPICE_ROUTE.tokens).map(
+      async ({ chainName, addressOrDenom }) => {
+        try {
+          const signer = multiProvider.getSigner(chainName);
+          const spiceToken = IERC20__factory.connect(addressOrDenom!, signer);
+          const falconAddress = MILLENNIUM_FALCON_ADDRESS[chainName];
+          const balance = await spiceToken.balanceOf(falconAddress);
+          const formattedBalance = formatUnits(balance, 18);
+          return {
+            chain: chainName,
+            type: 'falcon',
+            balance: Number(formattedBalance).toFixed(3),
+            symbol: 'SPICE',
+            address: falconAddress,
+          };
+        } catch (error) {
+          rootLogger.error(
+            `Error fetching spice balance for falcon on chain ${chainName}:`,
+            error,
+          );
+          return {
+            chain: chainName,
+            type: 'falcon',
+            balance: 'ERROR',
+            symbol: 'ERROR',
+            address: 'ERROR',
+          };
+        }
+      },
+    ),
+  );
+
+  const formattedSpiceBalances = spiceBalancesObject.reduce(
+    (acc, { chain, type, balance, symbol }) => {
+      if (!acc[chain]) acc[chain] = {};
+      acc[chain][type] = `${balance} ${symbol}`;
+      return acc;
+    },
+    {} as Record<string, any>,
+  );
+
+  // eslint-disable-next-line no-console
+  console.table(formattedSpiceBalances);
 
   const balancesObject = await Promise.all(
     targetNetworks.flatMap((chain) => {
