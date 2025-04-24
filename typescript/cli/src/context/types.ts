129c93cf62ed7264b7de5d9f3c6de33314e0da70
ljankovic-txfusion <lazar@txfusion.io>
2025-04-24 15:59:59 +0200

    chore: partial merge of cli from main

--- typescript/cli/src/context/types.ts
+++ typescript/cli/src/context/types.ts
@@ -20,6 +20,7 @@ export interface ContextSettings {
   disableProxy?: boolean;
   skipConfirmation?: boolean;
   strategyPath?: string;
+  authToken?: string;
 }
 
 export interface CommandContext {
