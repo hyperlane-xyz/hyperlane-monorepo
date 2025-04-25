89ba160b8cd36d44a46d629b3e52208afcba5cf8
Zhengyang Feng <nuoyiman00@gmail.com>
2025-04-25 13:56:12 -0400

    feat(types): implement Encode/Decode traits for LogMeta

--- rust/main/hyperlane-core/src/types/log_metadata.rs
+++ rust/main/hyperlane-core/src/types/log_metadata.rs
@@ -5,7 +5,7 @@ use serde::{Deserialize, Serialize};
 #[cfg(feature = "ethers")]
 use ethers_contract::LogMeta as EthersLogMeta;
 
-use crate::{H256, H512, U256};
+use crate::{Decode, Encode, HyperlaneProtocolError, H256, H512, U256};
 
 /// A close clone of the Ethereum `LogMeta`, this is designed to be a more
 /// generic metadata that we can use for other blockchains later. Some changes
@@ -35,6 +35,71 @@ pub struct LogMeta {
 impl From<EthersLogMeta> for LogMeta {
     fn from(v: EthersLogMeta) -> Self {
         Self::from(&v)
+    }
+}
+
+// Constants for byte lengths of fields
+const ADDRESS_LEN: usize = 32;
+const BLOCK_NUMBER_LEN: usize = 8;
+const BLOCK_HASH_LEN: usize = 32;
+const TRANSACTION_ID_LEN: usize = 64;
+const TRANSACTION_INDEX_LEN: usize = 8;
+const LOG_INDEX_LEN: usize = 32;
+const LOG_META_LEN: usize = ADDRESS_LEN
+    + BLOCK_NUMBER_LEN
+    + BLOCK_HASH_LEN
+    + TRANSACTION_ID_LEN
+    + TRANSACTION_INDEX_LEN
+    + LOG_INDEX_LEN; // 32 + 8 + 32 + 64 + 8 + 32 = 176
+
+impl Encode for LogMeta {
+    fn write_to<W>(&self, writer: &mut W) -> std::io::Result<usize>
+    where
+        W: std::io::Write,
+    {
+        writer.write_all(self.address.as_ref())?;
+        writer.write_all(&self.block_number.to_be_bytes())?;
+        writer.write_all(self.block_hash.as_ref())?;
+        writer.write_all(self.transaction_id.as_ref())?;
+        writer.write_all(&self.transaction_index.to_be_bytes())?;
+        let mut log_index_bytes = [0u8; LOG_INDEX_LEN];
+        self.log_index.to_big_endian(&mut log_index_bytes);
+        writer.write_all(&log_index_bytes)?;
+        Ok(LOG_META_LEN)
+    }
+}
+
+impl Decode for LogMeta {
+    fn read_from<R>(reader: &mut R) -> Result<Self, HyperlaneProtocolError>
+    where
+        R: std::io::Read,
+    {
+        let mut address = H256::zero();
+        reader.read_exact(address.as_mut())?;
+
+        let mut block_number_bytes = [0u8; BLOCK_NUMBER_LEN];
+        reader.read_exact(&mut block_number_bytes)?;
+
+        let mut block_hash = H256::zero();
+        reader.read_exact(block_hash.as_mut())?;
+
+        let mut transaction_id = H512::zero();
+        reader.read_exact(transaction_id.as_mut())?;
+
+        let mut transaction_index_bytes = [0u8; TRANSACTION_INDEX_LEN];
+        reader.read_exact(&mut transaction_index_bytes)?;
+
+        let mut log_index_bytes = [0u8; LOG_INDEX_LEN];
+        reader.read_exact(&mut log_index_bytes)?;
+
+        Ok(Self {
+            address,
+            block_number: u64::from_be_bytes(block_number_bytes),
+            block_hash,
+            transaction_id,
+            transaction_index: u64::from_be_bytes(transaction_index_bytes),
+            log_index: U256::from_big_endian(&log_index_bytes),
+        })
     }
 }
 
