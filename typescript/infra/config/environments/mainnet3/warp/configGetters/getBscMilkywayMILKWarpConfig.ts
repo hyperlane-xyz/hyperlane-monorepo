9aa239899840a2f162b5f608ff6f067bc584acd4
yjamin <kaminund@gmail.com>
2025-04-25 17:52:53 +0200

    feat: milkywaytestnet

--- typescript/infra/config/environments/mainnet3/warp/configGetters/getBscMilkywayMILKWarpConfig.ts
+++ typescript/infra/config/environments/mainnet3/warp/configGetters/getBscMilkywayMILKWarpConfig.ts
@@ -15,49 +15,46 @@ import {
 
 // TODO: Confirm ownership
 const safeOwners: ChainMap<Address> = {
-  bsc: '0x2313057ba402C55dAE1a1E8086B37fc6Ef7B3503',
-  milkyway: '0x5A2ee9A4B4D6076cDb3a08c9ae5aca1bD8AD3b02',
+  bsc: '',
+  milkyway: '',
+  ism: '',
 };
 
 const validators: ChainMap<MultisigConfig> = {
   milkyway: {
-    // TODO
     threshold: 1,
     validators: [
-      {
-        address: '0x9bccfad3bd12ef0ee8ae839dd9ed7835bccadc9d',
-        alias: 'Everclear',
-      },
-      { address: '0xc27032c6bbd48c20005f552af3aaa0dbf14260f3', alias: 'Renzo' },
+      { address: '0x242d8a855a8c932dec51f7999ae7d1e48b10c95e', alias: 'AW' },
+      { address: '0xf620f5e3d25a3ae848fec74bccae5de3edcd8796', alias: 'AW' },
+      { address: '0x1f030345963c54ff8229720dd3a711c15c554aeb}', alias: 'AW' },
     ],
   },
 };
 
 export const getBscMilkywayMILKWarpConfig = async (
-  routerConfig: ChainMap<RouterConfigWithoutOwner>,
+  _: ChainMap<RouterConfigWithoutOwner>,
 ): Promise<ChainMap<HypTokenRouterConfig>> => {
   return {
     milkyway: {
-      ...routerConfig.milkyway,
-      owner: '',
+      owner: safeOwners.milkyway,
       type: TokenType.native,
+      mailbox:
+        '0x68797065726c616e650000000000000000000000000000000000000000000000',
       foreignDeployment:
         '0x726f757465725f61707000000000000000000000000000010000000000000000',
       gas: 200000,
     },
     bsc: {
-      ...routerConfig.bsc,
-      type: TokenType.collateral,
+      type: TokenType.synthetic,
       owner: safeOwners.bsc,
-      token: tokens.mint.MINT,
+      symbol: 'MILK',
+      name: 'Milkyway',
+      decimals: 9,
+      mailbox: '0xF9F6F5646F478d5ab4e20B0F910C92F1CCC9Cc6D',
       interchainSecurityModule: {
         type: IsmType.ROUTING,
-        owner: '',
-        domains: buildAggregationIsmConfigs(
-          'bsc',
-          ['bsc', 'milkyway'],
-          validators,
-        ),
+        owner: safeOwners.ism,
+        domains: buildAggregationIsmConfigs('bsc', ['milkyway'], validators),
       },
     },
   };
