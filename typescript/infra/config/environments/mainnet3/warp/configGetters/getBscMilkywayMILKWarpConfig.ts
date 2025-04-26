6e501290623b831f0dab6fcce52d580482d0e570
pbio <10051819+paulbalaji@users.noreply.github.com>
2025-04-26 15:19:28 +0100

    update getter

--- typescript/infra/config/environments/mainnet3/warp/configGetters/getBscMilkywayMILKWarpConfig.ts
+++ typescript/infra/config/environments/mainnet3/warp/configGetters/getBscMilkywayMILKWarpConfig.ts
@@ -1,49 +1,63 @@
 import {
+  AW_VALIDATOR_ALIAS,
   ChainMap,
   HypTokenRouterConfig,
   IsmType,
+  MultisigConfig,
   TokenType,
   buildAggregationIsmConfigs,
+  defaultMultisigConfigs,
 } from '@hyperlane-xyz/sdk';
-import { defaultMultisigConfigs } from '@hyperlane-xyz/sdk';
 import { Address } from '@hyperlane-xyz/utils';
 
 import { RouterConfigWithoutOwner } from '../../../../../src/config/warp.js';
+import { DEPLOYER } from '../../owners.js';
 
 // TODO: Confirm ownership
 const safeOwners: ChainMap<Address> = {
-  bsc: '0xa7ECcdb9Be08178f896c26b7BbD8C3D4E844d9Ba',
+  bsc: DEPLOYER,
   milkyway: 'milk1326ley07fm6rpeqgxmxevnqevrsjfew2akzupg',
 };
 
-// 0x000000000000000000000000a7b49c0ed293742f6b0640b8b3db068c1e6fbd52;
+const multisigConfig: ChainMap<MultisigConfig> = {
+  ...defaultMultisigConfigs,
+  milkyway: {
+    threshold: 1,
+    validators: [
+      {
+        address: '0x9985e0c6df8e25b655b46a317af422f5e7756875',
+        alias: AW_VALIDATOR_ALIAS,
+      },
+    ],
+  },
+};
 
 export const getBscMilkywayMILKWarpConfig = async (
   routerConfig: ChainMap<RouterConfigWithoutOwner>,
 ): Promise<ChainMap<HypTokenRouterConfig>> => {
   return {
     milkyway: {
+      ...routerConfig.milkyway,
       owner: safeOwners.milkyway,
       type: TokenType.native,
       foreignDeployment:
         '0x726f757465725f61707000000000000000000000000000010000000000000000',
-      ...routerConfig.milkway,
+      gas: 200000,
     },
     bsc: {
       ...routerConfig.bsc,
-      type: TokenType.synthetic,
       owner: safeOwners.bsc,
+      type: TokenType.synthetic,
       symbol: 'MILK',
       name: 'MilkyWay',
       decimals: 6,
-      mailbox: '0x2971b9Aec44bE4eb673DF1B88cDB57b96eefe8a4',
       interchainSecurityModule: {
         type: IsmType.FALLBACK_ROUTING,
         owner: safeOwners.bsc,
         domains: buildAggregationIsmConfigs(
           'bsc',
           ['milkyway'],
-          defaultMultisigConfigs,
+          multisigConfig,
         ),
       },
     },
