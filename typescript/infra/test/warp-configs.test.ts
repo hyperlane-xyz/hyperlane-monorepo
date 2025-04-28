8a2f261de2684c896238584500a48a0dd75094a3
Le <leyu@Les-MacBook-Pro.local>
2025-04-28 11:46:13 -0400

    Remove unused var

--- typescript/infra/test/warp-configs.test.ts
+++ typescript/infra/test/warp-configs.test.ts
@@ -4,7 +4,7 @@ import chaiAsPromised from 'chai-as-promised';
 import { DEFAULT_GITHUB_REGISTRY } from '@hyperlane-xyz/registry';
 import { getRegistry } from '@hyperlane-xyz/registry/fs';
 import { HypTokenRouterConfig, MultiProvider } from '@hyperlane-xyz/sdk';
-import { diffObjMerge, rootLogger } from '@hyperlane-xyz/utils';
+import { rootLogger } from '@hyperlane-xyz/utils';
 
 import { getWarpConfig, warpConfigGetterMap } from '../config/warp.js';
 import {
