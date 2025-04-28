a156b8fde41eb442afec095cec9edd2496284607
Fernando Zavalia <24811313+fzavalia@users.noreply.github.com>
2025-04-28 09:20:44 -0300

    feat: Bring Rebalancer Agent development from BootNode fork (#5951)
    Move the rebalancer code from the bootnode fork
    https://github.com/BootNodeDev/hyperlane-monorepo/tree/feat/rebalancer-agent
    to this repo and start working on the feature directly.
    
    Current state includes:
    
    - monitor querying collateral balances every x amount of time
    - configurable strategy that calculates how rebalancing has to be
    performed
    
    Progress will still be tracked at
    https://github.com/orgs/BootNodeDev/projects/8
    
    -
    
    -
    
    Yes
    
    E2E, Unit & Manual
    
    ---------
    
    Co-authored-by: fernandomg <fernando.greco@gmail.com>

--- typescript/cli/src/tests/commands/warp.ts
+++ typescript/cli/src/tests/commands/warp.ts
@@ -142,7 +142,7 @@ export function hyperlaneWarpSendRelay(
   destination: string,
   warpCorePath: string,
   relay = true,
-  value = 1,
+  value: number | string = 1,
 ): ProcessPromise {
   return $`yarn workspace @hyperlane-xyz/cli run hyperlane warp send \
         ${relay ? '--relay' : ''} \
@@ -154,6 +154,18 @@ export function hyperlaneWarpSendRelay(
         --verbosity debug \
         --yes \
         --amount ${value}`;
+}
+
+export function hyperlaneWarpRebalancer(
+  warpRouteId: string,
+  checkFrequency: number,
+  strategyConfigFile: string,
+): ProcessPromise {
+  return $`yarn workspace @hyperlane-xyz/cli run hyperlane warp rebalancer \
+        --registry ${REGISTRY_PATH} \
+        --warpRouteId ${warpRouteId} \
+        --checkFrequency ${checkFrequency} \
+        --strategyConfigFile ${strategyConfigFile}`;
 }
 
 /**
