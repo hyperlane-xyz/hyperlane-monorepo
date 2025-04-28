411eef8cb7d6c2768002952314f4e6cb41ba3513
Yorke Rhodes IV <yorke@hyperlane.xyz>
2025-04-28 16:18:38 -0400

    Fix CLI e2e again

--- typescript/cli/src/tests/commands/warp.ts
+++ typescript/cli/src/tests/commands/warp.ts
@@ -221,15 +221,9 @@ function getWarpTokenConfigForType({
       };
       break;
     case TokenType.native:
-      tokenConfig = {
-        type: TokenType.native,
-        mailbox,
-        owner,
-      };
-      break;
     case TokenType.nativeScaled:
       tokenConfig = {
-        type: TokenType.nativeScaled,
+        type: tokenType,
         mailbox,
         owner,
         scale: 1,
