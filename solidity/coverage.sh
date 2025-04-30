3327a6ec7b381b3a41477c6d5005c72977ed1170
larryob <tosin@hyperlane.xyz>
2025-04-30 00:03:02 -0400

    feat: Add MovableCollateralRouter (#5878)
    ### Description
    Add abstract contract that allows a router's collateral to be moved.
    
    ### Drive-by changes
    
    N/A
    
    ### Related issues
    
    N/A
    
    ### Backward compatibility
    
    Yes
    ### Testing
    
    Unit

--- solidity/coverage.sh
+++ solidity/coverage.sh
@@ -8,4 +8,5 @@ forge coverage \
     --report lcov \
     --report summary \
     --no-match-coverage "(test|mock|node_modules|script|Fast)" \
+    --no-match-test "Fork" \
     --ir-minimum # https://github.com/foundry-rs/foundry/issues/3357
