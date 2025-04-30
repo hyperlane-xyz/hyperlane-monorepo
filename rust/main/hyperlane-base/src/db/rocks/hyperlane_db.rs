608a50756e3a3ef6fbf27320e011f8b988dcbc1f
Daniel Savu <23065004+daniel-savu@users.noreply.github.com>
2025-04-30 11:26:33 +0100

    feat(submitter): track all historic hashes in `Transaction` (#6068)
    ### Description
    
    - instead of tracking a single tx hash, the `Transaction` struct now
    tracks all historic hashes this logical transaction has had. This is
    because after resubmitting the tx with a new gas price, it may still be
    a previous hash that gets included in the end
    - the status of a transaction is now found by querying all of its
    historic hashes. If at least one hash is finalized, the tx is finalized.
    If at least one hash is included, the tx is included. If every hash
    query returns an error, the status default to `PendingInclusion`, to
    avoid dropping txs when RPCs go down (this happens even in e2e)
    - adds two new fields to `Transaction`: `creation_timestamp` and
    `last_submission_attempt`. Both of these are timestamps, and are used to
    enforce a minimum time interval before resubmitting a Transaction. They
    are useful because in the local solana validator it takes 10-30s for the
    hash of a submitted tx to be queried without returning an error. Without
    them, the transaction would get continuously resubmitted.
    - adds a new `tx_ready_for_resubmission` method to the `AdaptsChains`
    trait. It defaults to `true` everywhere, but on solana it is overridden
    to use `creation_timestamp`, `last_submission_attempt` and
    `TX_RESUBMISSION_MIN_DELAY_SECS` (15s).
    
    ### Drive-by changes
    
    - reduces verbosity of `Transaction` logs by silencing the `instruction`
    property of the `vm_specific_data` for Sealevel. I kept the tx cost data
    because that will likely be useful.
    - renames the db key for mapping a message to multiple payloads to
    `PAYLOAD_IDS_BY_MESSAGE_ID` from `PAYLOAD_ID_BY_MESSAGE_ID`
    
    ### Related issues
    
    - Fixes https://github.com/hyperlane-xyz/hyperlane-monorepo/issues/5893
    
    ### Testing
    
    E2E and some unit tests

--- rust/main/hyperlane-base/src/db/rocks/hyperlane_db.rs
+++ rust/main/hyperlane-base/src/db/rocks/hyperlane_db.rs
@@ -37,7 +37,7 @@ const MERKLE_LEAF_INDEX_BY_MESSAGE_ID: &str = "merkle_leaf_index_by_message_id_"
 const MERKLE_TREE_INSERTION_BLOCK_NUMBER_BY_LEAF_INDEX: &str =
     "merkle_tree_insertion_block_number_by_leaf_index_";
 const LATEST_INDEXED_GAS_PAYMENT_BLOCK: &str = "latest_indexed_gas_payment_block";
-const PAYLOAD_ID_BY_MESSAGE_ID: &str = "payload_id_by_message_id_";
+const PAYLOAD_IDS_BY_MESSAGE_ID: &str = "payload_ids_by_message_id_";
 
 /// Rocks DB result type
 pub type DbResult<T> = std::result::Result<T, DbError>;
@@ -664,14 +664,14 @@ impl HyperlaneDb for HyperlaneRocksDB {
         message_id: &H256,
         payload_ids: Vec<UniqueIdentifier>,
     ) -> DbResult<()> {
-        self.store_value_by_key(PAYLOAD_ID_BY_MESSAGE_ID, message_id, &payload_ids)
+        self.store_value_by_key(PAYLOAD_IDS_BY_MESSAGE_ID, message_id, &payload_ids)
     }
 
     fn retrieve_payload_ids_by_message_id(
         &self,
         message_id: &H256,
     ) -> DbResult<Option<Vec<UniqueIdentifier>>> {
-        self.retrieve_value_by_key(PAYLOAD_ID_BY_MESSAGE_ID, message_id)
+        self.retrieve_value_by_key(PAYLOAD_IDS_BY_MESSAGE_ID, message_id)
     }
 }
 
