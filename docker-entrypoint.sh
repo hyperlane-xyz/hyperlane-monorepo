5f73efb9da95a3d5feb9c74ae80d105466b2e18a
pbio <10051819+paulbalaji@users.noreply.github.com>
2025-04-30 08:18:07 +0100

    chore: fetch latest branch in docker-entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,11 +6,17 @@ REGISTRY_URI="/hyperlane-registry"
 
 # Only update registry if REGISTRY_COMMIT is set
 if [ -n "$REGISTRY_COMMIT" ]; then
-  echo "Updating Hyperlane registry to commit: ${REGISTRY_COMMIT}"
+  echo "Updating Hyperlane registry to: $REGISTRY_COMMIT"
   OLDPWD=$(pwd)
   cd "$REGISTRY_URI"
-  git fetch origin "$REGISTRY_COMMIT"
+  git fetch origin
   git checkout "$REGISTRY_COMMIT"
+
+  # Only reset if it's a branch
+  if git show-ref --verify --quiet "refs/remotes/origin/$REGISTRY_COMMIT"; then
+    git reset --hard "origin/$REGISTRY_COMMIT"
+  fi
+
   cd "$OLDPWD"
 fi
 
