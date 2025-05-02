c1f084ecef70d3cd600db5e9f5d4f9798fc505a1
-f <kunalarora1729@gmail.com>
2025-05-02 14:12:59 +0100

    new image

--- typescript/infra/config/environments/testnet4/agent.ts
+++ typescript/infra/config/environments/testnet4/agent.ts
@@ -413,7 +413,7 @@ const releaseCandidate: RootAgentConfig = {
     rpcConsensusType: RpcConsensusType.Fallback,
     docker: {
       repo,
-      tag: '391c097-20250428-141206',
+      tag: '21888a4-20250502-115939',
     },
     blacklist: relayBlacklist,
     gasPaymentEnforcement,
