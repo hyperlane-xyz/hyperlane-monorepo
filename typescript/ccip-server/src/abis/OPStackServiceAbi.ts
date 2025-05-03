11a22ef131d5351619599d69ab793796cc121f3a
gitmp01 <mauro.piazza.01@gmail.com>
2025-05-03 09:20:02 +0200

    chore(OPStackServiceAbi.ts): add comment

--- typescript/ccip-server/src/abis/OPStackServiceAbi.ts
+++ typescript/ccip-server/src/abis/OPStackServiceAbi.ts
@@ -1,3 +1,5 @@
+// ABI called by the relayer once OPL2ToL1CcipReadIsm.getOffChainVerifyInfo()
+// gets executed
 const OPStackServiceAbi = [
   'function getWithdrawalProof(bytes) public view returns (tuple(uint256,address,address,uint256,uint256,bytes),uint256,tuple(bytes32,bytes32,bytes32,bytes32),bytes[])',
   'function getFinalizeWithdrawalTx(bytes) public view returns (tuple(uint256,address,address,uint256,uint256,bytes))',
