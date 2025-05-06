03e8bb51bb830f62840cde7f2078e94379aca961
Fernando Zavalia <24811313+fzavalia@users.noreply.github.com>
2025-05-06 15:56:35 -0300

    fix: Add E2E test with mocked bridge (#6142)
    Closes https://github.com/BootNodeDev/hyperlane-monorepo/issues/44
    
    Includes https://github.com/hyperlane-xyz/hyperlane-monorepo/pull/6087
    
    Adds an e2e test that listens to the event emitted by the mocked value
    transfer bridge and performs an actual transfer to achieve balance

--- solidity/contracts/mock/MockValueTransferBridge.sol
+++ solidity/contracts/mock/MockValueTransferBridge.sol
@@ -1,10 +1,16 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.8.13;
 
-import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {ValueTransferBridge, Quote} from "../token/libs/ValueTransferBridge.sol";
 
 contract MockValueTransferBridge is ValueTransferBridge {
+    event SentTransferRemote(
+        uint32 indexed origin,
+        uint32 indexed destination,
+        bytes32 indexed recipient,
+        uint256 amount
+    );
+
     function quoteTransferRemote(
         uint32, //_destinationDomain,
         bytes32, //_recipient,
@@ -14,10 +20,17 @@ contract MockValueTransferBridge is ValueTransferBridge {
     }
 
     function transferRemote(
-        uint32, //_destinationDomain,
-        bytes32, //_recipient,
-        uint256 //_amountOut
+        uint32 _destinationDomain,
+        bytes32 _recipient,
+        uint256 _amountOut
     ) external payable virtual override returns (bytes32 transferId) {
+        emit SentTransferRemote(
+            uint32(block.chainid),
+            _destinationDomain,
+            _recipient,
+            _amountOut
+        );
+
         return keccak256("transferId");
     }
 }
