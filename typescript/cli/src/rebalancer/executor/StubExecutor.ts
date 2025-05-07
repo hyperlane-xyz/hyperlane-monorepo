af2b0ea50cfc3fea1c912362b1cc64605b234e05
Fernando Zavalia <24811313+fzavalia@users.noreply.github.com>
2025-05-07 16:39:37 -0300

    fix: Rename StubExecutor

--- typescript/cli/src/rebalancer/executor/StubExecutor.ts
+++ typescript/cli/src/rebalancer/executor/StubExecutor.ts
@@ -2,8 +2,8 @@ import { log } from '../../logger.js';
 import { IExecutor } from '../interfaces/IExecutor.js';
 import { RebalancingRoute } from '../interfaces/IStrategy.js';
 
-export class MonitorOnlyExecutor implements IExecutor {
+export class StubExecutor implements IExecutor {
   async rebalance(_routes: RebalancingRoute[]) {
-    log('No rebalance executed in monitorOnly mode');
+    log('StubExecutor rebalance called');
   }
 }
