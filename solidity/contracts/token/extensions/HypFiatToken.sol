5a6c863ec8c8a1c6efac7de684f82411dd214d96
nambrot <nambrot@googlemail.com>
2025-05-07 11:19:10 -0400

    Don use the IFiatToken interface

--- solidity/contracts/token/extensions/HypFiatToken.sol
+++ solidity/contracts/token/extensions/HypFiatToken.sol
@@ -4,6 +4,11 @@ pragma solidity >=0.8.0;
 import {IFiatToken} from "../interfaces/IFiatToken.sol";
 import {HypERC20Collateral} from "../HypERC20Collateral.sol";
 
+interface MintableERC20 {
+    function mint(address to, uint256 amount) external;
+    function burn(uint256 amount) external;
+}
+
 // see https://github.com/circlefin/stablecoin-evm/blob/master/doc/tokendesign.md#issuing-and-destroying-tokens
 contract HypFiatToken is HypERC20Collateral {
     constructor(
@@ -18,7 +23,7 @@ contract HypFiatToken is HypERC20Collateral {
         // transfer amount to address(this)
         metadata = super._transferFromSender(_amount);
         // burn amount of address(this) balance
-        IFiatToken(address(wrappedToken)).burn(_amount);
+        MintableERC20(address(wrappedToken)).burn(_amount);
     }
 
     function _transferTo(
@@ -26,6 +31,6 @@ contract HypFiatToken is HypERC20Collateral {
         uint256 _amount,
         bytes calldata /*metadata*/
     ) internal override {
-        IFiatToken(address(wrappedToken)).mint(_recipient, _amount);
+        MintableERC20(address(wrappedToken)).mint(_recipient, _amount);
     }
 }
