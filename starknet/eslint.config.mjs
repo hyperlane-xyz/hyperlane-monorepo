c518ac21255e404afaf4b4e600bf4cd70b1cf087
ljankovic-txfusion <lazar@txfusion.io>
2025-05-09 11:33:39 +0200

    feat/starknet-core-abi-exports

---
+++ starknet/eslint.config.mjs
@@ -0,0 +1,15 @@
+import MonorepoDefaults from '../eslint.config.mjs';
+
+export default [
+  ...MonorepoDefaults,
+  {
+    files: ['**/*.ts'],
+    rules: {
+      'no-console': 'off',
+      'no-restricted-imports': 'off',
+    },
+  },
+  {
+    ignores: ['scripts/**/*'],
+  },
+];
