2a87cbedc4086c1fbadaf2b264b6fbb6803bb991
gitmp01 <mauro.piazza.01@protonmail.com>
2025-05-15 01:05:20 +0200

    feat: add OpStack L2 to L1 ITokenBridge (#5645)
    ### Description
    
    1. Adds `ValueTransferBridgeNative` router abstracting the transfer of
    ETH from L2 to L1
    2. Adds `OPValueTransferBridgeNative` offering an implementation for 1.
    on Optimism
     3. Adds hooks and required ISM for 2.
    4. Adds an `OPStackService` to the CCIP-read server handling the
    CCIP-read requests
    
    <!--
    What's included in this PR?
    -->
    
    ### Drive-by changes
    
    <!--
    Are there any minor or drive-by changes also included?
    -->
    
    ### Related issues
    
    <!--
    - Fixes #[issue number here]
    -->
    
    ### Backward compatibility
    
    <!--
    Are these changes backward compatible? Are there any infrastructure
    implications, e.g. changes that would prohibit deploying older commits
    using this infra tooling?
    
    Yes/No
    -->
    
    ### Testing
    
    <!--
    What kind of testing have these changes undergone?
    
    None/Manual/Unit Tests
    -->

---
+++ solidity/test/isms/OPL2ToL1CcipReadIsm.t.sol
@@ -0,0 +1,191 @@
+import "forge-std/Test.sol";
+
+import {Message} from "../../contracts/libs/Message.sol";
+import {TypeCasts} from "../../contracts/libs/TypeCasts.sol";
+import {MockMailbox} from "../../contracts/mock/MockMailbox.sol";
+import {TokenMessage} from "../../contracts/token/libs/TokenMessage.sol";
+import {MockOptimismPortal} from "../../contracts/mock/MockOptimism.sol";
+import {OPL2ToL1CcipReadIsm} from "../../contracts/isms/hook/OPL2ToL1CcipReadIsm.sol";
+import {IOptimismPortal} from "../../contracts/interfaces/optimism/IOptimismPortal.sol";
+import {MockHyperlaneEnvironment} from "../../contracts/mock/MockHyperlaneEnvironment.sol";
+import {LightTestRecipient} from "../../contracts/test/LightTestRecipient.sol";
+
+import {console} from "forge-std/console.sol";
+
+contract OPL2ToL1CcipReadIsmTest is Test {
+    using TypeCasts for address;
+    using TokenMessage for bytes;
+
+    address payable internal constant L2_BRIDGE_ADDRESS =
+        payable(0x4200000000000000000000000000000000000010);
+
+    uint256 internal constant scale = 1;
+
+    uint32 internal constant origin = 1;
+    uint32 internal constant destination = 2;
+
+    address tokenBridgeOrigin;
+    address tokenBridgeDestination;
+
+    OPL2ToL1CcipReadIsm internal ism;
+
+    string[] urls;
+    IOptimismPortal portal;
+    MockHyperlaneEnvironment internal environment;
+    MockMailbox mailboxOrigin;
+    MockMailbox mailboxDestination;
+
+    ///////////////////////////////////////////////////////////////////
+    ///                            SETUP                            ///
+    ///////////////////////////////////////////////////////////////////
+
+    function setUp() public {
+        urls = new string[](1);
+        urls[0] = "https://ccip-read-gateway.io";
+        environment = new MockHyperlaneEnvironment(origin, destination);
+        mailboxOrigin = environment.mailboxes(origin);
+        mailboxDestination = environment.mailboxes(destination);
+        portal = new MockOptimismPortal();
+
+        // We just need two contracts
+        tokenBridgeOrigin = address(new LightTestRecipient());
+        tokenBridgeDestination = address(new LightTestRecipient());
+        uint32 portalVersion = 1;
+
+        ism = new OPL2ToL1CcipReadIsm(
+            urls,
+            address(portal),
+            portalVersion,
+            address(mailboxDestination)
+        );
+
+        mailboxDestination.setDefaultIsm(address(ism));
+        mailboxDestination.addRemoteMailbox(origin, mailboxOrigin);
+    }
+
+    function test_verify_revertsWhen_mailboxTryToCallVerify() public {
+        mailboxOrigin.dispatch(
+            destination,
+            address(ism).addressToBytes32(),
+            bytes("")
+        );
+
+        uint256 nonce = mailboxDestination.inboundProcessedNonce();
+        bytes memory message = mailboxDestination.inboundMessages(nonce);
+
+        vm.expectRevert();
+        environment.processNextPendingMessage();
+    }
+
+    function _getDummyVerifyMetadata(
+        IOptimismPortal.WithdrawalTransaction memory _tx
+    ) internal returns (bytes memory) {
+        uint256 gameIndex = 0;
+        IOptimismPortal.OutputRootProof memory outputRootProof;
+        bytes[] memory proof;
+        return abi.encode(_tx, gameIndex, outputRootProof, proof);
+    }
+
+    function _getDummyWithdrawalTx(
+        uint256 nonce,
+        uint256 amount,
+        bytes memory data
+    ) internal returns (IOptimismPortal.WithdrawalTransaction memory) {
+        uint256 gasLimit = 200_000;
+
+        return
+            IOptimismPortal.WithdrawalTransaction(
+                nonce,
+                tokenBridgeOrigin,
+                tokenBridgeDestination,
+                amount,
+                gasLimit,
+                data
+            );
+    }
+
+    function test_verify_proveWithdrawalSuccessfully() public {
+        uint256 amount = 0.001 ether;
+
+        IOptimismPortal.WithdrawalTransaction
+            memory withdrawalTx = _getDummyWithdrawalTx(0, amount, bytes(""));
+
+        mailboxOrigin.dispatch(
+            destination,
+            address(ism).addressToBytes32(),
+            bytes("")
+        );
+
+        uint256 nonce = mailboxDestination.inboundProcessedNonce();
+        bytes memory message = mailboxDestination.inboundMessages(nonce);
+        bytes memory metadata = _getDummyVerifyMetadata(withdrawalTx);
+
+        ism.verify(metadata, message);
+    }
+
+    function test_verify_finalizeWithdrawalSuccessfully() public {
+        uint256 amount = 0.001 ether;
+        IOptimismPortal.WithdrawalTransaction
+            memory withdrawalTx = _getDummyWithdrawalTx(0, amount, bytes(""));
+
+        mailboxOrigin.dispatch(
+            destination,
+            address(tokenBridgeDestination).addressToBytes32(),
+            bytes("")
+        );
+
+        vm.deal(address(portal), amount);
+
+        uint256 nonce = mailboxDestination.inboundProcessedNonce();
+        bytes memory message = mailboxDestination.inboundMessages(nonce);
+        bytes memory metadata = _getDummyVerifyMetadata(withdrawalTx);
+
+        ism.verify(metadata, message);
+    }
+
+    function test_interchainSecurityModule_givenIsmIsCorrect() public {
+        assertEq(address(ism.interchainSecurityModule()), address(ism));
+    }
+
+    function test_handle_emitReceivedMessageEvent() public {
+        uint256 amount = 0.001 ether;
+        IOptimismPortal.WithdrawalTransaction
+            memory withdrawalTx = _getDummyWithdrawalTx(0, amount, bytes(""));
+
+        mailboxOrigin.dispatch(
+            destination,
+            address(ism).addressToBytes32(),
+            bytes("")
+        );
+
+        vm.deal(address(portal), amount);
+
+        uint256 nonce = mailboxDestination.inboundProcessedNonce();
+        bytes memory message = mailboxDestination.inboundMessages(nonce);
+        bytes memory metadata = _getDummyVerifyMetadata(withdrawalTx);
+
+        vm.expectEmit(address(ism));
+        emit OPL2ToL1CcipReadIsm.ReceivedMessage(
+            origin,
+            address(this).addressToBytes32(),
+            0,
+            bytes("")
+        );
+        ism.process(metadata, message);
+    }
+
+    function testFuzz_constructor_revertWhen_creatingWithWrongPortalVersion(
+        uint32 portalVersion
+    ) public {
+        vm.assume(portalVersion != 1);
+        vm.assume(portalVersion != 2);
+
+        vm.expectRevert(bytes("Unsupported OP portal version"));
+        ism = new OPL2ToL1CcipReadIsm(
+            urls,
+            address(portal),
+            portalVersion,
+            address(mailboxDestination)
+        );
+    }
+}
