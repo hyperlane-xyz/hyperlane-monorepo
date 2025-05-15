bd1efb06becfad7526bd753c968863e40ebd8f00
gitmp01 <mauro.piazza.01@gmail.com>
2025-05-15 12:40:20 +0200

    chore(CctpMessage): rm unused functions

--- solidity/contracts/libs/CctpMessage.sol
+++ solidity/contracts/libs/CctpMessage.sol
@@ -24,92 +24,14 @@ library CctpMessage {
     using TypedMemView for bytes29;
 
     // Indices of each field in message
-    uint8 private constant VERSION_INDEX = 0;
     uint8 private constant SOURCE_DOMAIN_INDEX = 4;
-    uint8 private constant DESTINATION_DOMAIN_INDEX = 8;
     uint8 private constant NONCE_INDEX = 12;
-    uint8 private constant SENDER_INDEX = 20;
-    uint8 private constant RECIPIENT_INDEX = 52;
-    uint8 private constant DESTINATION_CALLER_INDEX = 84;
-    uint8 private constant MESSAGE_BODY_INDEX = 116;
-
-    function _formatMessage(
-        uint32 _msgVersion,
-        uint32 _msgSourceDomain,
-        uint32 _msgDestinationDomain,
-        uint64 _msgNonce,
-        bytes32 _msgSender,
-        bytes32 _msgRecipient,
-        bytes32 _msgDestinationCaller,
-        bytes memory _msgRawBody
-    ) internal pure returns (bytes memory) {
-        return
-            abi.encodePacked(
-                _msgVersion,
-                _msgSourceDomain,
-                _msgDestinationDomain,
-                _msgNonce,
-                _msgSender,
-                _msgRecipient,
-                _msgDestinationCaller,
-                _msgRawBody
-            );
-    }
-
-    function _version(bytes29 _message) internal pure returns (uint32) {
-        return uint32(_message.indexUint(VERSION_INDEX, 4));
-    }
 
     function _sourceDomain(bytes29 _message) internal pure returns (uint32) {
         return uint32(_message.indexUint(SOURCE_DOMAIN_INDEX, 4));
     }
 
-    function _destinationDomain(
-        bytes29 _message
-    ) internal pure returns (uint32) {
-        return uint32(_message.indexUint(DESTINATION_DOMAIN_INDEX, 4));
-    }
-
     function _nonce(bytes29 _message) internal pure returns (uint64) {
         return uint64(_message.indexUint(NONCE_INDEX, 8));
-    }
-
-    function _sender(bytes29 _message) internal pure returns (bytes32) {
-        return _message.index(SENDER_INDEX, 32);
-    }
-
-    function _recipient(bytes29 _message) internal pure returns (bytes32) {
-        return _message.index(RECIPIENT_INDEX, 32);
-    }
-
-    function _destinationCaller(
-        bytes29 _message
-    ) internal pure returns (bytes32) {
-        return _message.index(DESTINATION_CALLER_INDEX, 32);
-    }
-
-    function _messageBody(bytes29 _message) internal pure returns (bytes29) {
-        return
-            _message.slice(
-                MESSAGE_BODY_INDEX,
-                _message.len() - MESSAGE_BODY_INDEX,
-                0
-            );
-    }
-
-    function addressToBytes32(address addr) external pure returns (bytes32) {
-        return bytes32(uint256(uint160(addr)));
-    }
-
-    function bytes32ToAddress(bytes32 _buf) public pure returns (address) {
-        return address(uint160(uint256(_buf)));
-    }
-
-    function _validateMessageFormat(bytes29 _message) internal pure {
-        require(_message.isValid(), "Malformed message");
-        require(
-            _message.len() >= MESSAGE_BODY_INDEX,
-            "Invalid message: too short"
-        );
     }
 }
