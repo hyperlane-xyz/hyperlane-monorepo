2375fdc4c1d1c8cb7e2fd4da596947c4a8db37d9
Le <6251863+ltyu@users.noreply.github.com>
2025-05-20 16:00:28 -0400

    Export rezEthChainsToDeploy

--- typescript/infra/config/environments/mainnet3/warp/configGetters/getRenzoREZBaseEthereum.ts
+++ typescript/infra/config/environments/mainnet3/warp/configGetters/getRenzoREZBaseEthereum.ts
@@ -7,7 +7,7 @@ import {
   renzoTokenPrices,
 } from './getRenzoEZETHWarpConfig.js';
 
-const rezEthChainsToDeploy = ['ethereum', 'base'];
+export const rezEthChainsToDeploy = ['ethereum', 'base'];
 const rezProductionLockbox = '0xd8B543fEac4EEcEF5a46a926e10D6f4a72de6fE0';
 const rezEthAddresses = {
   ethereum: '0xf757c9804cF2EE8d8Ed64e0A8936293Fe43a7252',
