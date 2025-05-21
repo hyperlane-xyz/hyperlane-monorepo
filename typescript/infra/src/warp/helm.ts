5840ad1e4e933816dba9ee542e7fcbc4ac4f6bf7
Mohammed Hussan <mo@hyperlane.xyz>
2025-05-21 14:59:32 +0100

    chore: update monitor image to include new token standard handling (#6242)
    ### Description
    
    - update monitor image to include new token standard handling

--- typescript/infra/src/warp/helm.ts
+++ typescript/infra/src/warp/helm.ts
@@ -73,7 +73,7 @@ export class WarpRouteMonitorHelmManager extends HelmManager {
     return {
       image: {
         repository: 'gcr.io/abacus-labs-dev/hyperlane-monorepo',
-        tag: 'a015285-20250516-204210',
+        tag: '7aae0ec-20250521-103450',
       },
       warpRouteId: this.warpRouteId,
       fullnameOverride: this.helmReleaseName,
