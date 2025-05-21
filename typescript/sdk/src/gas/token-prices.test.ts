83fc5b57d0cefb8c619053b6bcdcb2322985d790
Andrey Taranov <86911+antigremlin@users.noreply.github.com>
2025-05-21 12:00:09 +0100

    Resolve circular imports between sdk-registry

--- typescript/sdk/src/gas/token-prices.test.ts
+++ typescript/sdk/src/gas/token-prices.test.ts
@@ -1,13 +1,14 @@
 import { expect } from 'chai';
 import sinon from 'sinon';
 
-import { ethereum, solanamainnet } from '@hyperlane-xyz/registry';
-
 import { TestChainName, testChainMetadata } from '../consts/testChains.js';
 
 import { CoinGeckoTokenPriceGetter } from './token-prices.js';
 
 const MOCK_FETCH_CALLS = true;
+
+const ethereum: { name?: string } = {};
+const solanamainnet: { name?: string } = {};
 
 describe('TokenPriceGetter', () => {
   let tokenPriceGetter: CoinGeckoTokenPriceGetter;
@@ -45,8 +46,8 @@ describe('TokenPriceGetter', () => {
       // stubbed results
       expect(
         await tokenPriceGetter.getTokenPriceByIds([
-          ethereum.name,
-          solanamainnet.name,
+          ethereum.name || 'ethereum',
+          solanamainnet.name || 'solanamainnet',
         ]),
       ).to.eql([priceA, priceB]);
     });
@@ -59,9 +60,9 @@ describe('TokenPriceGetter', () => {
         await tokenPriceGetter.getTokenPrice(TestChainName.test1),
       ).to.equal(1);
       // stubbed result for non-testnet
-      expect(await tokenPriceGetter.getTokenPrice(ethereum.name)).to.equal(
-        priceA,
-      );
+      expect(
+        await tokenPriceGetter.getTokenPrice(ethereum.name || 'ethereum'),
+      ).to.equal(priceA);
     });
   });
 
@@ -75,8 +76,8 @@ describe('TokenPriceGetter', () => {
       // stubbed result for non-testnet
       expect(
         await tokenPriceGetter.getTokenExchangeRate(
-          ethereum.name,
-          solanamainnet.name,
+          ethereum.name || 'ethereum',
+          solanamainnet.name || 'solanamainnet',
         ),
       ).to.equal(priceA / priceB);
     });
