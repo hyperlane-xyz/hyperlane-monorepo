304b32e2acc7cea338ee27c96c136c9acb1eb89b
pbio <10051819+paulbalaji@users.noreply.github.com>
2025-05-21 16:39:45 +0100

    CR andrey

--- typescript/infra/src/agents/gcp.ts
+++ typescript/infra/src/agents/gcp.ts
@@ -124,7 +124,7 @@ export class AgentGCPKey extends CloudAgentKey {
         );
         // TODO support other prefixes?
         // https://cosmosdrops.io/en/tools/bech32-converter is useful for converting to other prefixes.
-        return pubkeyToAddress(encodedPubkey, 'neutron');
+        return pubkeyToAddress(encodedPubkey, 'milk');
       }
       default:
         this.logger.debug(`Unsupported protocol: ${protocol}`);
