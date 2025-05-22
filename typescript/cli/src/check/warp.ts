bafbb8058e6a9f6120af2266850b2e974847de7f
christopherbrumm <christopherbrumm2000@gmail.com>
2025-05-22 16:17:45 +0200

    chore: cleanup `runWarpRouteCheck`

--- typescript/cli/src/check/warp.ts
+++ typescript/cli/src/check/warp.ts
@@ -26,31 +26,8 @@ export async function runWarpRouteCheck({
   onChainWarpConfig: DerivedWarpRouteDeployConfig &
     Record<string, Partial<HypTokenRouterVirtualConfig>>;
 }): Promise<void> {
-  Object.values(warpRouteConfig).forEach((config) => {
-    if (config.decimals === undefined && config.decimals !== 0) {
-      logRed('Decimals, if defined, must not be zero');
-      process.exit(1);
-    }
-  });
-
-  if (!areDecimalsUniform(warpRouteConfig)) {
-    const maxDecimals = Math.max(
-      ...Object.values(warpRouteConfig).map((config) => config.decimals!),
-    );
-
-    for (const [chain, config] of Object.entries(warpRouteConfig)) {
-      if (config.decimals) {
-        const scale = 10 ** (maxDecimals - config.decimals);
-        if (!config.scale && scale !== 1) {
-          logRed(`Scale is required for ${chain}`);
-          process.exit(1);
-        } else if (config.scale && scale !== config.scale) {
-          logRed(`Scale is not correct for ${chain}`);
-          process.exit(1);
-        }
-      }
-    }
-  }
+  // Check whether the decimals are consistent. If not, ensure that the scale is correct.
+  verifyDecimalsAndScale(warpRouteConfig);
 
   // Go through each chain and only add to the output the chains that have mismatches
   const [violations, isInvalid] = Object.keys(warpRouteConfig).reduce(
@@ -95,6 +72,37 @@ export async function runWarpRouteCheck({
   }
 
   logGreen(`No violations found`);
+}
+
+function verifyDecimalsAndScale(
+  warpRouteConfig: WarpRouteDeployConfigMailboxRequired &
+    Record<string, Partial<HypTokenRouterVirtualConfig>>,
+) {
+  Object.values(warpRouteConfig).forEach((config) => {
+    if (config.decimals === undefined && config.decimals !== 0) {
+      logRed('Decimals, if defined, must not be zero');
+      process.exit(1);
+    }
+  });
+
+  if (!areDecimalsUniform(warpRouteConfig)) {
+    const maxDecimals = Math.max(
+      ...Object.values(warpRouteConfig).map((config) => config.decimals!),
+    );
+
+    for (const [chain, config] of Object.entries(warpRouteConfig)) {
+      if (config.decimals) {
+        const scale = 10 ** (maxDecimals - config.decimals);
+        if (!config.scale && scale !== 1) {
+          logRed(`Scale is required for ${chain}`);
+          process.exit(1);
+        } else if (config.scale && scale !== config.scale) {
+          logRed(`Scale is not correct for ${chain}`);
+          process.exit(1);
+        }
+      }
+    }
+  }
 }
 
 function areDecimalsUniform(configMap: Record<string, any>): boolean {
