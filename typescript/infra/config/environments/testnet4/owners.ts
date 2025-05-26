98d4d7264ac6c3ba0eb50e96d91c546574d44da1
-f <kunalarora1729@gmail.com>
2025-05-26 16:56:53 +0100

    owner not supported here

--- typescript/infra/config/environments/testnet4/owners.ts
+++ typescript/infra/config/environments/testnet4/owners.ts
@@ -3,6 +3,7 @@ import { ChainMap, OwnableConfig } from '@hyperlane-xyz/sdk';
 import { ethereumChainNames } from './chains.js';
 
 const ETHEREUM_DEPLOYER_ADDRESS = '0xfaD1C94469700833717Fa8a3017278BC1cA8031C';
+
 // const SEALEVEL_DEPLOYER_ADDRESS = '6DjHX6Ezjpq3zZMZ8KsqyoFYo1zPSDoiZmLLkxD4xKXS';
 
 export const owners: ChainMap<OwnableConfig> = {
@@ -32,10 +33,10 @@ export const owners: ChainMap<OwnableConfig> = {
     owner: 'n/a - CSDK not supported here',
   },
   starknetsepolia: {
-    owner: '0x073Be77d7BD7B2A4eDFB15E33f4b0CF30AEaf795B39618667A4b0146f48Eeba4',
+    owner: 'n/a - Starknet not supported here',
   },
   paradexsepolia: {
-    owner: '0x05fe9ded5ffef4db68deaa012ce6d05e06e3e9906aed5801fc7a49696e28df66',
+    owner: 'n/a - Starknet not supported here',
   },
 };
 
