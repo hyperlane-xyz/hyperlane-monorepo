2e064b6822f5b6a75e78946a4009631b274124ca
xeno097 <xeno097.cp@gmail.com>
2025-05-27 18:45:10 -0400

    feat(sdk): implemented the getLegacySPLTokenMetadata fuction to get metadata for non spl2022 tokens

--- typescript/sdk/src/token/sealevel/token.ts
+++ typescript/sdk/src/token/sealevel/token.ts
@@ -9,6 +9,8 @@ import {
   SealevelHyperlaneTokenDataSchema,
 } from '../adapters/serialization.js';
 
+import { Metadata, SPL_TOKEN_METADATA_SCHEMA } from './serde.js';
+
 export async function getSealevelHypTokenAccountData(
   svmProvider: Readonly<Connection>,
   tokenMetaPda: PublicKey,
@@ -29,4 +31,35 @@ export async function getSealevelHypTokenAccountData(
   );
 
   return data;
+}
+
+const METADATA_PROGRAM_ID = new PublicKey(
+  'metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s',
+);
+
+const removeNullPaddingFromString = (s: string) => s.replaceAll('\x00', '');
+
+export async function getLegacySPLTokenMetadata(
+  connection: Connection,
+  mint: PublicKey,
+): Promise<{ name: string; symbol: string; uri: string } | null> {
+  const [metadataPda] = PublicKey.findProgramAddressSync(
+    [Buffer.from('metadata'), METADATA_PROGRAM_ID.toBuffer(), mint.toBuffer()],
+    METADATA_PROGRAM_ID,
+  );
+
+  const accountInfo = await connection.getAccountInfo(metadataPda);
+  if (!accountInfo) return null;
+
+  const metadata = deserializeUnchecked(
+    SPL_TOKEN_METADATA_SCHEMA,
+    Metadata,
+    accountInfo.data,
+  );
+
+  return {
+    name: removeNullPaddingFromString(metadata.data.name),
+    symbol: removeNullPaddingFromString(metadata.data.symbol),
+    uri: removeNullPaddingFromString(metadata.data.uri),
+  };
 }
