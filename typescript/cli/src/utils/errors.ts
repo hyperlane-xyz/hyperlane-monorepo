8004cee76a322131c5f39b3dacf52de35b24fd40
fernandomg <fernando.greco@gmail.com>
2025-05-29 18:51:32 +0200

    refactor(WrappedError): extend the error logging by adding `cause`

--- typescript/cli/src/utils/errors.ts
+++ typescript/cli/src/utils/errors.ts
@@ -1,11 +1,5 @@
 export class WrappedError extends Error {
-  constructor(message: string, originalError?: Error) {
-    super(message);
-    this.name = 'WrappedError';
-
-    // Preserve the stack trace of the original error if available
-    if (originalError?.stack) {
-      this.stack = `${this.stack}\nCaused by: ${originalError.stack}`;
-    }
+  constructor(message: string, cause?: Error) {
+    super(message, cause ? { cause } : undefined);
   }
 }
