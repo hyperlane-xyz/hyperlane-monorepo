20522c11be99d822eb71f6ad9afaadbb24a25a4e
Danil Nemirovsky <4614623+ameten@users.noreply.github.com>
2025-05-29 17:46:15 +0100

    Add to create keys script

--- typescript/infra/scripts/keys/create-keys.ts
+++ typescript/infra/scripts/keys/create-keys.ts
@@ -1,10 +1,36 @@
-import { createAgentKeysIfNotExists } from '../../src/agents/key-utils.js';
+import { confirm } from '@inquirer/prompts';
+import chalk from 'chalk';
+
+import {
+  agentKeysToBeCreated,
+  createAgentKeysIfNotExists,
+} from '../../src/agents/key-utils.js';
 import { getAgentConfigsBasedOnArgs } from '../agent-utils.js';
 
 async function main() {
   const { agentConfig } = await getAgentConfigsBasedOnArgs();
-  await createAgentKeysIfNotExists(agentConfig);
-  return 'Keys created successfully!';
+
+  const agentKeysToCreate = await agentKeysToBeCreated(agentConfig);
+
+  if (agentKeysToCreate.length > 0) {
+    const shouldContinue = await confirm({
+      message: chalk.yellow.bold(
+        `Warning: New agent key will be created: ${agentKeysToCreate}. Are you sure you want to continue?`,
+      ),
+      default: false,
+    });
+    if (!shouldContinue) {
+      console.log(chalk.red.bold('Exiting...'));
+      process.exit(1);
+    }
+
+    console.log(chalk.green.bold('Creating new agent key if needed.'));
+    await createAgentKeysIfNotExists(agentConfig);
+    return 'Keys created successfully!';
+  } else {
+    console.log(chalk.green.bold('No new agent key will be created.'));
+    return 'No new keys are created!';
+  }
 }
 
 main().then(console.log).catch(console.error);
