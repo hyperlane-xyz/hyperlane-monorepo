37a1098c04990ef9e440d64495702c9cc02af089
pbio <10051819+paulbalaji@users.noreply.github.com>
2025-06-02 21:02:26 +0100

    script for updating ousdt submitter config + rename file

---
+++ typescript/infra/scripts/warp-routes/ousdt/update-submitter-strategy.ts
@@ -0,0 +1,73 @@
+import { dirname, resolve } from 'path';
+import { fileURLToPath } from 'url';
+import { stringify as yamlStringify } from 'yaml';
+
+import {
+  ChainSubmissionStrategy,
+  TxSubmitterType,
+  WarpRouteDeployConfigSchema,
+} from '@hyperlane-xyz/sdk';
+import { rootLogger } from '@hyperlane-xyz/utils';
+
+import { getWarpConfig } from '../../../config/warp.js';
+import { Owner, determineGovernanceType } from '../../../src/governance.js';
+import { writeYamlAtPath } from '../../../src/utils/utils.js';
+import { getEnvironmentConfig } from '../../core-utils.js';
+
+const warpRouteId = 'oUSDT/production';
+const environment = 'mainnet3';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const strategyFilePath = resolve(
+  __dirname,
+  `../../../config/environments/${environment}/warp/strategies/ousdt.yaml`,
+);
+
+async function main() {
+  const envConfig = getEnvironmentConfig(environment);
+  const multiProvider = await envConfig.getMultiProvider();
+
+  const rawWarpConfig = await getWarpConfig(
+    multiProvider,
+    envConfig,
+    warpRouteId,
+  );
+
+  const parsed = WarpRouteDeployConfigSchema.safeParse(rawWarpConfig);
+  if (!parsed.success) {
+    rootLogger.error('Error parsing warp config:');
+    console.dir(rawWarpConfig, { depth: null });
+    console.dir(parsed.error.format(), { depth: null });
+    return;
+  }
+
+  const chainSubmissionStrategy: ChainSubmissionStrategy = {};
+  for (const [chain, config] of Object.entries(parsed.data)) {
+    const { ownerType } = await determineGovernanceType(chain, config.owner);
+    if (ownerType === Owner.SAFE) {
+      chainSubmissionStrategy[chain] = {
+        submitter: {
+          chain,
+          type: TxSubmitterType.GNOSIS_SAFE,
+          safeAddress: config.owner,
+        },
+      };
+    } else {
+      chainSubmissionStrategy[chain] = {
+        submitter: {
+          chain,
+          type: TxSubmitterType.JSON_RPC,
+        },
+      };
+    }
+  }
+
+  rootLogger.info('Generated strategy:');
+  rootLogger.info(yamlStringify(chainSubmissionStrategy, null, 2));
+  rootLogger.info(`Wrote strategy to ${strategyFilePath}`);
+  writeYamlAtPath(strategyFilePath, chainSubmissionStrategy);
+}
+
+main().catch((err) => rootLogger.error('Error:', err));
