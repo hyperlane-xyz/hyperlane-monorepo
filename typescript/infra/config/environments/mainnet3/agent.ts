229f50d8aac6429fcab699b5b171b46c8bc770ab
Danil Nemirovsky <ameten@users.noreply.github.com>
2025-06-04 18:48:14 +0100

    chore: Upgrade Relayer RC Mainnet (#6456)
    ### Description
    
    Upgrade Relayer RC Mainnet
    
    ### Backward compatibility
    
    Yes
    
    ### Testing
    
    Manual
    
    Co-authored-by: Danil Nemirovsky <4614623+ameten@users.noreply.github.com>

--- typescript/infra/config/environments/mainnet3/agent.ts
+++ typescript/infra/config/environments/mainnet3/agent.ts
@@ -850,7 +850,7 @@ const releaseCandidate: RootAgentConfig = {
     rpcConsensusType: RpcConsensusType.Fallback,
     docker: {
       repo,
-      tag: '3a58caa-20250604-135609',
+      tag: '6e9f822-20250604-173142',
     },
     blacklist,
     // We're temporarily (ab)using the RC relayer as a way to increase
