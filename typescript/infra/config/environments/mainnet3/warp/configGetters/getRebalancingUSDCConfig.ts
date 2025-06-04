224796afafdc0a540d2210c74b2a0688b8605d74
Yorke Rhodes IV <yorke@hyperlane.xyz>
2025-06-04 14:34:13 -0400

    Add rebalancing config

--- typescript/infra/config/environments/mainnet3/warp/configGetters/getRebalancingUSDCConfig.ts
+++ typescript/infra/config/environments/mainnet3/warp/configGetters/getRebalancingUSDCConfig.ts
@@ -4,12 +4,15 @@ import {
   OwnableConfig,
   TokenType,
 } from '@hyperlane-xyz/sdk';
+import { assert } from '@hyperlane-xyz/utils';
 
 import { RouterConfigWithoutOwner } from '../../../../../src/config/warp.js';
+import { getRegistry as getMainnet3Registry } from '../../chains.js';
 import { awIcas } from '../../governance/ica/aw.js';
 import { awSafes } from '../../governance/safe/aw.js';
 import { DEPLOYER } from '../../owners.js';
 import { usdcTokenAddresses } from '../cctp.js';
+import { WarpRouteIds } from '../warpIds.js';
 
 import { CCTP_CHAINS } from './getCCTPConfig.js';
 
@@ -22,6 +25,19 @@ export const getRebalancingUSDCConfig = async (
   _abacusWorksEnvOwnerConfig: ChainMap<OwnableConfig>,
   _warpRouteId: string,
 ): Promise<ChainMap<HypTokenRouterConfig>> => {
+  const registry = await getMainnet3Registry();
+  const mainnetCCTP = await registry.getWarpRoute(WarpRouteIds.MainnetCCTP);
+  assert(mainnetCCTP, 'MainnetCCTP warp route not found');
+
+  const metadata = await registry.getMetadata();
+
+  const cctpBridges = Object.fromEntries(
+    mainnetCCTP.tokens.map(({ chainName, addressOrDenom }) => [
+      chainName,
+      addressOrDenom!,
+    ]),
+  );
+
   return Object.fromEntries(
     chains.map((chain) => {
       const owner = awIcas[chain] ?? awSafes[chain];
@@ -38,6 +54,14 @@ export const getRebalancingUSDCConfig = async (
         ];
       }
 
+      const cctpBridge = cctpBridges[chain];
+      const remoteDomains = chains
+        .filter((c) => c !== chain)
+        .map((c) => metadata[c].domainId);
+      const allowedRebalancingBridges = Object.fromEntries(
+        remoteDomains.map((domainId) => [domainId, [{ bridge: cctpBridge }]]),
+      );
+
       const config: HypTokenRouterConfig = {
         owner: awIcas[chain] ?? awSafes[chain] ?? DEPLOYER,
         mailbox: routerConfig[chain].mailbox,
@@ -45,6 +69,8 @@ export const getRebalancingUSDCConfig = async (
         token: usdcTokenAddresses[chain],
         // from prerelease branch
         contractVersion: ' 8.0.0-next.0',
+        allowedRebalancers: [DEPLOYER],
+        allowedRebalancingBridges,
       };
 
       return [chain, config];
