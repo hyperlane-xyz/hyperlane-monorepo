ba8bedda2f07525650243a32e6f8f6682044a2d2
Trevor Porter <tkporter4@gmail.com>
2025-06-04 13:32:29 +0100

    feat: deploy agents, faster external-secrets (#6441)
    ### Description
    
    - Updates all testnet4 relayer / validator agents (except for testnet4
    neutron relayer)
    - Adds concurrency of 10 to external-secrets, so it can update 10
    secrets concurrently instead of the default of 1
    - Removes unnecessary chains from validator external-secrets templates,
    cutting down on the work that external-secrets needs to do
    - Makes it a bit safer to work with the external-secrets helm chart
    
    ### Drive-by changes
    
    <!--
    Are there any minor or drive-by changes also included?
    -->
    
    ### Related issues
    
    <!--
    - Fixes #[issue number here]
    -->
    
    ### Backward compatibility
    
    <!--
    Are these changes backward compatible? Are there any infrastructure
    implications, e.g. changes that would prohibit deploying older commits
    using this infra tooling?
    
    Yes/No
    -->
    
    ### Testing
    
    <!--
    What kind of testing have these changes undergone?
    
    None/Manual/Unit Tests
    -->
    
    ---------
    
    Co-authored-by: Danil Nemirovsky <ameten@users.noreply.github.com>
    Co-authored-by: graphite-app[bot] <96075541+graphite-app[bot]@users.noreply.github.com>

--- typescript/infra/config/environments/testnet4/agent.ts
+++ typescript/infra/config/environments/testnet4/agent.ts
@@ -408,7 +408,7 @@ const hyperlane: RootAgentConfig = {
     rpcConsensusType: RpcConsensusType.Fallback,
     docker: {
       repo,
-      tag: 'b23399a-20250603-075344',
+      tag: 'cedc8e1-20250603-094703',
     },
     blacklist: [...releaseCandidateHelloworldMatchingList, ...relayBlacklist],
     gasPaymentEnforcement,
@@ -423,7 +423,7 @@ const hyperlane: RootAgentConfig = {
     rpcConsensusType: RpcConsensusType.Fallback,
     docker: {
       repo,
-      tag: '761d140-20250526-120715',
+      tag: 'cedc8e1-20250603-094703',
     },
     chains: validatorChainConfig(Contexts.Hyperlane),
     resources: validatorResources,
@@ -447,7 +447,7 @@ const releaseCandidate: RootAgentConfig = {
     rpcConsensusType: RpcConsensusType.Fallback,
     docker: {
       repo,
-      tag: 'b23399a-20250603-075344',
+      tag: 'cedc8e1-20250603-094703',
     },
     blacklist: relayBlacklist,
     gasPaymentEnforcement,
@@ -462,7 +462,7 @@ const releaseCandidate: RootAgentConfig = {
     rpcConsensusType: RpcConsensusType.Fallback,
     docker: {
       repo,
-      tag: '385b307-20250418-150728',
+      tag: 'cedc8e1-20250603-094703',
     },
     chains: validatorChainConfig(Contexts.ReleaseCandidate),
     resources: validatorResources,
