d20729383c80f9ae63c54fc48ffd187e2098521f
pbio <10051819+paulbalaji@users.noreply.github.com>
2025-06-11 17:34:23 +0100

    changes for bulk deploying new fully connected ICA routers

--- typescript/infra/config/environments/mainnet3/owners.ts
+++ typescript/infra/config/environments/mainnet3/owners.ts
@@ -39,6 +39,8 @@ export const ethereumChainOwners: ChainMap<OwnableConfig> = Object.fromEntries(
           validatorAnnounce: DEPLOYER, // unused
           testRecipient: DEPLOYER,
           fallbackRoutingHook: DEPLOYER,
+          interchainAccountRouter: DEPLOYER,
+          offchainLookupIsm: DEPLOYER,
         },
       },
     ];
