422a78c46dacd89c31ca1bb41a841805bc43dace
Jamin <57451149+yjamin@users.noreply.github.com>
2025-06-12 11:09:32 +0200

    fix: make starknet signer key optional (#6497)

--- rust/main/chains/hyperlane-starknet/src/signers.rs
+++ rust/main/chains/hyperlane-starknet/src/signers.rs
@@ -67,3 +67,15 @@ impl Signer {
         ))
     }
 }
+
+impl Default for Signer {
+    fn default() -> Self {
+        // default signer is just all zeros
+        Self {
+            signing_key: SigningKey::from_secret_scalar(FieldElement::ZERO),
+            address: Default::default(),
+            is_legacy: Default::default(),
+            address_h256: Default::default(),
+        }
+    }
+}
