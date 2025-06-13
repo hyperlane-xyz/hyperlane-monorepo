c3b5f32093bcc6bf0eaa02a55c8cdebf966688ab
Danil Nemirovsky <4614623+ameten@users.noreply.github.com>
2025-06-13 10:25:43 +0100

    Add tests for validate

--- rust/main/lander/src/adapter/chains/ethereum/nonce/state.rs
+++ rust/main/lander/src/adapter/chains/ethereum/nonce/state.rs
@@ -15,6 +15,7 @@ use super::status::NonceStatus;
 
 mod boundary;
 mod db;
+mod validate;
 
 #[derive(Clone, Debug, Eq, PartialEq)]
 pub(crate) enum NonceAction {
@@ -38,87 +39,6 @@ impl NonceManagerState {
             nonce_db,
             tx_db,
             address,
-        }
-    }
-
-    pub(crate) async fn validate_assigned_nonce(
-        &self,
-        tx: &Transaction,
-    ) -> NonceResult<(NonceAction, Option<U256>)> {
-        use NonceAction::{Assign, Noop};
-        use NonceStatus::{Committed, Freed, Taken};
-
-        let tx_uuid = tx.uuid.clone();
-        let tx_status = tx.status.clone();
-        let Some(nonce): Option<U256> = tx.precursor().tx.nonce().map(Into::into) else {
-            return Ok((Assign, None));
-        };
-        let nonce_status = NonceStatus::calculate_nonce_status(tx_uuid.clone(), &tx_status);
-
-        // Fetching the tracked transaction uuid
-        let tracked_tx_uuid = self.get_tracked_tx_uuid(&nonce).await?;
-
-        if tracked_tx_uuid == TransactionUuid::default() {
-            // If the nonce, which currently assigned to the transaction, is not tracked,
-            // we should assign the new nonce.
-            warn!(?nonce, "Nonce is not tracked, assigning new nonce");
-            return Ok((Assign, Some(nonce)));
-        };
-
-        if tracked_tx_uuid != tx_uuid {
-            // If the tracked nonce is assigned to a different transaction,
-            // we should assign the new nonce. It should never happen
-            // If the tracked transaction was dropped and
-            // the calculated tracked nonce status is Freed, we may re-use the nonce
-            // when we assign it to the new transaction.
-            warn!(
-                ?nonce,
-                ?nonce_status,
-                "Nonce is assigned to a different transaction, assigning new nonce"
-            );
-            return Ok((Assign, Some(nonce)));
-        }
-
-        let finalized_nonce = self.get_finalized_nonce().await?;
-
-        match (&nonce_status, finalized_nonce) {
-            (Freed(_), _) => {
-                // If the nonce, which is currently assigned to the transaction, is Freed,
-                // then the transaction was dropped. But we are validating the nonce just before
-                // we submit the transaction again. It means that we should assign the new nonce.
-                warn!(?nonce, ?nonce_status, "Nonce is freed, assigning new nonce");
-                Ok((Assign, Some(nonce)))
-            }
-            (Taken(_), Some(finalized_nonce)) if nonce <= finalized_nonce => {
-                // If the nonce is taken, but it is below or equal to the finalized nonce,
-                // it means that the current nonce is outdated.
-                // We should assign the new nonce.
-                info!(
-                    ?nonce,
-                    ?nonce_status,
-                    "Nonce is taken by the transaction but below or equal to the finalized nonce, assigning new nonce"
-                );
-                Ok((Assign, Some(nonce)))
-            }
-            (Taken(_), _) => {
-                // If the nonce is taken or committed, we don't need to do anything.
-                info!(
-                    ?nonce,
-                    ?nonce_status,
-                    "Nonce is already assigned to the transaction, no action needed"
-                );
-                Ok((Noop, Some(nonce)))
-            }
-            (Committed(_), _) => {
-                // If the nonce is taken or committed, we don't need to do anything.
-                warn!(
-                    ?nonce,
-                    ?nonce_status,
-                    "Nonce is already assigned to the transaction, no action needed, \
-                    but transaction should not be committed at this point"
-                );
-                Ok((Noop, Some(nonce)))
-            }
         }
     }
 
---
+++ rust/main/lander/src/adapter/chains/ethereum/nonce/state/validate/tests.rs
@@ -0,0 +1,237 @@
+use ethers_core::types::Address;
+
+use hyperlane_core::U256;
+
+use crate::tests::test_utils::tmp_dbs;
+use crate::transaction::{
+    DropReason, Transaction, TransactionStatus, TransactionUuid, VmSpecificTxData,
+};
+use crate::TransactionDropReason;
+
+use super::super::super::super::EthereumTxPrecursor;
+use super::NonceAction;
+use super::NonceManagerState;
+
+fn make_tx(
+    uuid: TransactionUuid,
+    status: TransactionStatus,
+    nonce: Option<U256>,
+    address: Option<Address>,
+) -> Transaction {
+    use ethers_core::abi::Function;
+    let mut precursor = EthereumTxPrecursor {
+        tx: Default::default(),
+        function: Function {
+            name: "".to_string(),
+            inputs: vec![],
+            outputs: vec![],
+            #[allow(deprecated)]
+            constant: None,
+            state_mutability: Default::default(),
+        },
+    };
+    if let Some(n) = nonce {
+        precursor.tx.set_nonce(n);
+    }
+    if let Some(addr) = address {
+        precursor.tx.set_from(addr);
+    }
+    Transaction {
+        uuid,
+        tx_hashes: vec![],
+        vm_specific_data: VmSpecificTxData::Evm(precursor),
+        payload_details: vec![],
+        status,
+        submission_attempts: 0,
+        creation_timestamp: Default::default(),
+        last_submission_attempt: None,
+    }
+}
+
+#[tokio::test]
+async fn test_validate_assigned_nonce_none_nonce() {
+    let (_, tx_db, nonce_db) = tmp_dbs();
+    let address = Address::random();
+    let state = NonceManagerState::new(nonce_db, tx_db, address);
+
+    let uuid = TransactionUuid::random();
+    let tx = make_tx(
+        uuid,
+        TransactionStatus::PendingInclusion,
+        None,
+        Some(address),
+    );
+
+    let (action, nonce) = state.validate_assigned_nonce(&tx).await.unwrap();
+    assert!(matches!(action, NonceAction::Assign));
+    assert_eq!(nonce, None);
+}
+
+#[tokio::test]
+async fn test_validate_assigned_nonce_not_tracked() {
+    let (_, tx_db, nonce_db) = tmp_dbs();
+    let address = Address::random();
+    let state = NonceManagerState::new(nonce_db, tx_db, address);
+
+    let uuid = TransactionUuid::random();
+    let nonce_val = U256::from(1);
+    let tx = make_tx(
+        uuid,
+        TransactionStatus::PendingInclusion,
+        Some(nonce_val),
+        Some(address),
+    );
+
+    let (action, nonce) = state.validate_assigned_nonce(&tx).await.unwrap();
+    assert!(matches!(action, NonceAction::Assign));
+    assert_eq!(nonce, Some(nonce_val));
+}
+
+#[tokio::test]
+async fn test_validate_assigned_nonce_tracked_different_tx_uuid() {
+    let (_, tx_db, nonce_db) = tmp_dbs();
+    let address = Address::random();
+    let state = NonceManagerState::new(nonce_db.clone(), tx_db, address);
+
+    let uuid1 = TransactionUuid::random();
+    let uuid2 = TransactionUuid::random();
+    let nonce_val = U256::from(2);
+
+    // Set tracked_tx_uuid to uuid2
+    state.set_tracked_tx_uuid(&nonce_val, &uuid2).await.unwrap();
+
+    let tx = make_tx(
+        uuid1,
+        TransactionStatus::PendingInclusion,
+        Some(nonce_val),
+        Some(address),
+    );
+    let (action, nonce) = state.validate_assigned_nonce(&tx).await.unwrap();
+    assert!(matches!(action, NonceAction::Assign));
+    assert_eq!(nonce, Some(nonce_val));
+}
+
+#[tokio::test]
+async fn test_validate_assigned_nonce_freed_status() {
+    let (_, tx_db, nonce_db) = tmp_dbs();
+    let address = Address::random();
+    let state = NonceManagerState::new(nonce_db.clone(), tx_db, address);
+
+    let uuid = TransactionUuid::random();
+    let nonce_val = U256::from(3);
+
+    // Set tracked_tx_uuid to uuid
+    state.set_tracked_tx_uuid(&nonce_val, &uuid).await.unwrap();
+
+    // The transaction is Dropped, so nonce status is Freed
+    let tx = make_tx(
+        uuid,
+        TransactionStatus::Dropped(DropReason::DroppedByChain),
+        Some(nonce_val),
+        Some(address),
+    );
+    let (action, nonce) = state.validate_assigned_nonce(&tx).await.unwrap();
+    assert!(matches!(action, NonceAction::Assign));
+    assert_eq!(nonce, Some(nonce_val));
+}
+
+#[tokio::test]
+async fn test_validate_assigned_nonce_taken_status_below_finalized() {
+    let (_, tx_db, nonce_db) = tmp_dbs();
+    let address = Address::random();
+    let state = NonceManagerState::new(nonce_db.clone(), tx_db, address);
+
+    let uuid = TransactionUuid::random();
+    let nonce_val = U256::from(4);
+
+    // Set tracked_tx_uuid to uuid
+    state.set_tracked_tx_uuid(&nonce_val, &uuid).await.unwrap();
+
+    // Set finalized nonce above nonce_val
+    state.set_finalized_nonce(&(nonce_val + 1)).await.unwrap();
+
+    let tx = make_tx(
+        uuid,
+        TransactionStatus::PendingInclusion,
+        Some(nonce_val),
+        Some(address),
+    );
+    let (action, nonce) = state.validate_assigned_nonce(&tx).await.unwrap();
+    assert!(matches!(action, NonceAction::Assign));
+    assert_eq!(nonce, Some(nonce_val));
+}
+
+#[tokio::test]
+async fn test_validate_assigned_nonce_taken_status_equal_finalized() {
+    let (_, tx_db, nonce_db) = tmp_dbs();
+    let address = Address::random();
+    let state = NonceManagerState::new(nonce_db.clone(), tx_db, address);
+
+    let uuid = TransactionUuid::random();
+    let nonce_val = U256::from(5);
+
+    // Set tracked_tx_uuid to uuid
+    state.set_tracked_tx_uuid(&nonce_val, &uuid).await.unwrap();
+
+    // Set finalized nonce equal to nonce_val
+    state.set_finalized_nonce(&nonce_val).await.unwrap();
+
+    let tx = make_tx(
+        uuid,
+        TransactionStatus::PendingInclusion,
+        Some(nonce_val),
+        Some(address),
+    );
+    let (action, nonce) = state.validate_assigned_nonce(&tx).await.unwrap();
+    assert!(matches!(action, NonceAction::Assign));
+    assert_eq!(nonce, Some(nonce_val));
+}
+
+#[tokio::test]
+async fn test_validate_assigned_nonce_taken_status_above_finalized() {
+    let (_, tx_db, nonce_db) = tmp_dbs();
+    let address = Address::random();
+    let state = NonceManagerState::new(nonce_db.clone(), tx_db, address);
+
+    let uuid = TransactionUuid::random();
+    let nonce_val = U256::from(6);
+
+    // Set tracked_tx_uuid to uuid
+    state.set_tracked_tx_uuid(&nonce_val, &uuid).await.unwrap();
+
+    // Set finalized nonce below nonce_val
+    state.set_finalized_nonce(&(nonce_val - 1)).await.unwrap();
+
+    let tx = make_tx(
+        uuid,
+        TransactionStatus::PendingInclusion,
+        Some(nonce_val),
+        Some(address),
+    );
+    let (action, nonce) = state.validate_assigned_nonce(&tx).await.unwrap();
+    assert!(matches!(action, NonceAction::Noop));
+    assert_eq!(nonce, Some(nonce_val));
+}
+
+#[tokio::test]
+async fn test_validate_assigned_nonce_committed_status() {
+    let (_, tx_db, nonce_db) = tmp_dbs();
+    let address = Address::random();
+    let state = NonceManagerState::new(nonce_db.clone(), tx_db, address);
+
+    let uuid = TransactionUuid::random();
+    let nonce_val = U256::from(7);
+
+    // Set tracked_tx_uuid to uuid
+    state.set_tracked_tx_uuid(&nonce_val, &uuid).await.unwrap();
+
+    let tx = make_tx(
+        uuid,
+        TransactionStatus::Finalized,
+        Some(nonce_val),
+        Some(address),
+    );
+    let (action, nonce) = state.validate_assigned_nonce(&tx).await.unwrap();
+    assert!(matches!(action, NonceAction::Noop));
+    assert_eq!(nonce, Some(nonce_val));
+}
