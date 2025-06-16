309a19034e7b61062ee6e36470ed3b664e6cada8
danwt <30197399+danwt@users.noreply.github.com>
2025-06-16 13:01:49 +0100

    progress checkpoint: code might be broken

--- rust/main/chains/dymension-kaspa/src/indexers/indexer.rs
+++ rust/main/chains/dymension-kaspa/src/indexers/indexer.rs
@@ -1,25 +1,45 @@
 use hyperlane_core::{ChainResult, H256};
 
+use auto_impl::auto_impl;
+
 use hyperlane_core::{
     ChainCommunicationError, HyperlaneMessage, InterchainGasPayment, MerkleTreeInsertion,
     SequenceAwareIndexer,
 };
-
+#[auto_impl(&, Box, Arc)]
+#[derive(Debug, Clone)]
 pub struct KaspaIndexer {}
-pub struct DeliveryIndexer{}
-pub struct DispatchIndexer{}
-pub struct IGPIndexer{}
-pub struct MerkleTreeIndexer{}
-
-// deliveries
-impl SequenceAwareIndexer<H256> for DeliveryIndexer {
-    async fn latest_sequence_count_and_tip(&self) -> ChainResult<(Option<u32>, u32)> {
-        return ChainResult::Err(ChainCommunicationError::from_other_str("not implemented"));
-    }
-}
+pub struct DispatchIndexer {}
+pub struct DeliveryIndexer {}
+pub struct IGPIndexer {}
+pub struct MerkleTreeIndexer {}
 
 // dispatches
 impl SequenceAwareIndexer<HyperlaneMessage> for DispatchIndexer {
+    async fn latest_sequence_count_and_tip(&self) -> ChainResult<(Option<u32>, u32)> {
+        return ChainResult::Err(ChainCommunicationError::from_other_str("not implemented"));
+    }
+    async fn fetch_logs_in_range(
+        &self,
+        range: RangeInclusive<u32>,
+    ) -> ChainResult<Vec<(Indexed<HyperlaneMessage>, LogMeta)>> {
+        Ok(vec![])
+    }
+
+    async fn get_finalized_block_number(&self) -> ChainResult<u32> {
+        Ok(0)
+    }
+
+    async fn fetch_logs_by_tx_hash(
+        &self,
+        _tx_hash: H512,
+    ) -> ChainResult<Vec<(Indexed<T>, LogMeta)>> {
+        Ok(vec![])
+    }
+}
+
+// deliveries
+impl SequenceAwareIndexer<H256> for DeliveryIndexer {
     async fn latest_sequence_count_and_tip(&self) -> ChainResult<(Option<u32>, u32)> {
         return ChainResult::Err(ChainCommunicationError::from_other_str("not implemented"));
     }
