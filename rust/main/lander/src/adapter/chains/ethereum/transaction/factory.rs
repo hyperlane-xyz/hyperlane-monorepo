3ba5d4e282a290d89381561490b5563a0e58301a
Danil Nemirovsky <4614623+ameten@users.noreply.github.com>
2025-06-18 17:12:20 +0100

    Batch in the building stage

--- rust/main/lander/src/adapter/chains/ethereum/transaction/factory.rs
+++ rust/main/lander/src/adapter/chains/ethereum/transaction/factory.rs
@@ -1,20 +1,18 @@
 use uuid::Uuid;
 
-use crate::{
-    adapter::chains::ethereum::precursor::EthereumTxPrecursor,
-    payload::FullPayload,
-    transaction::{Transaction, TransactionStatus, TransactionUuid, VmSpecificTxData},
-};
+use crate::adapter::chains::ethereum::precursor::EthereumTxPrecursor;
+use crate::payload::{FullPayload, PayloadDetails};
+use crate::transaction::{Transaction, TransactionStatus, TransactionUuid, VmSpecificTxData};
 
 pub struct TransactionFactory {}
 
 impl TransactionFactory {
-    pub fn build(payload: &FullPayload, precursor: EthereumTxPrecursor) -> Transaction {
+    pub fn build(precursor: EthereumTxPrecursor, details: Vec<PayloadDetails>) -> Transaction {
         Transaction {
             uuid: TransactionUuid::new(Uuid::new_v4()),
             tx_hashes: vec![],
             vm_specific_data: VmSpecificTxData::Evm(precursor),
-            payload_details: vec![payload.details.clone()],
+            payload_details: details,
             status: TransactionStatus::PendingInclusion,
             submission_attempts: 0,
             creation_timestamp: chrono::Utc::now(),
