7316bb21e03fec77dc9b08abdee6ad277c3b50fb
danwt <30197399+danwt@users.noreply.github.com>
2025-06-18 12:19:34 +0100

    progress checkpoint: code might be broken

--- dymension/libs/kaspa/lib/core/src/api/deposits.rs
+++ dymension/libs/kaspa/lib/core/src/api/deposits.rs
@@ -2,7 +2,12 @@ use tracing::{debug, error, info, info_span, warn, Instrument};
 
 use url::Url;
 
+use eyre::{Error, Result};
+
 use api_rs::apis::configuration::Configuration;
+use api_rs::apis::kaspa_addresses_api::{
+    get_full_transactions_for_address_page_addresses_kaspa_address_full_transactions_page_get as transactions_page,
+};
 
 pub struct Deposit {}
 
@@ -14,7 +19,7 @@ pub fn get_deposits() -> Vec<Deposit> {
 #[derive(Debug)]
 pub struct HttpClient {
     url: Url,
-    client: reqwest::Client,
+    client: reqwest::Client, // TODO: ignored for now
 }
 
 impl HttpClient {
@@ -29,7 +34,7 @@ impl HttpClient {
         Configuration {
             base_path: self.url.to_string(),
             user_agent: Some("OpenAPI-Generator/a6a9569/rust".to_owned()),
-            client: self.client,
+            client: reqwest::Client::new(),
             basic_auth: None,
             oauth_access_token: None,
             bearer_access_token: None,
@@ -37,11 +42,31 @@ impl HttpClient {
         }
     }
 
-    pub async fn get_deposits(&self) -> Vec<Deposit> {
-        let url = self.url.join("/api/v1/deposits").unwrap();
-        let res = self.client.get(url).send().await.unwrap();
-        let body = res.text().await.unwrap();
-        println!("body: {:?}", body);
-        vec![]
+    pub async fn get_deposits(&self, address: &str) -> Result<Vec<Deposit>> {
+        let limit = 20;
+        let lower_bound = Some(0i64);
+        let upper_bound = Some(0i64);
+        let field = None;
+        let resolve_previous_outpoints = None;
+        let acceptance = None;
+
+        let res = transactions_page(
+            &self.get_config(),
+            address,
+            Some(limit),
+            lower_bound,
+            upper_bound,
+            field,
+            resolve_previous_outpoints,
+            acceptance,
+        )
+        .await?;
+
+        Ok(res.into_iter().map(|tx| Deposit {
+            tx_id: tx.tx_id,
+            block_height: tx.block_height,
+            block_time: tx.block_time,
+            amount: tx.amount,
+        }).collect())
     }
 }
