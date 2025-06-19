3e14be12285d1885adaed7b81a9355f0ecf776ea
danwt <30197399+danwt@users.noreply.github.com>
2025-06-19 11:11:36 +0100

    progress checkpoint: code might be broken

--- rust/main/chains/dymension-kaspa/src/providers/validators.rs
+++ rust/main/chains/dymension-kaspa/src/providers/validators.rs
@@ -44,7 +44,7 @@ impl ValidatorsClient {
         Ok(ValidatorsClient { conf })
     }
 
-    /// dococo
+    ///
     pub async fn validate_deposits(&self, fxg: &DepositFXG) -> ChainResult<Vec<bool>> {
         // TODO: in parallel
         let mut results = Vec::new();
@@ -59,4 +59,37 @@ impl ValidatorsClient {
         }
         Ok(results)
     }
+}
+
+
+pub async fn validate_new_deposits(host: String, deposits: &DepositFXG) -> Result<bool, Error> {
+    let bz = Bytes::from(deposits);
+    let c = reqwest::Client::new();
+    let res = c
+        .post(format!("{}{}", host, ROUTE_VALIDATE_NEW_DEPOSITS))
+        .body(bz)
+        .send()
+        .await?;
+    let status = res.status();
+    if status == StatusCode::OK {
+        Ok(true)
+    } else {
+        Ok(false)
+    }
+}
+
+struct PSKTRes;
+struct PSKTFXG;
+
+pub async fn sign_pskts(host: String, pskts: &PSKTFXG) -> Result<PSKTRes, Error> {
+    unimplemented!()
+}
+
+struct WithdrawalFXG;
+
+pub async fn validate_confirmed_withdrawals(
+    host: String,
+    withdrawals: &WithdrawalFXG,
+) -> Result<bool, Error> {
+    unimplemented!()
 }
