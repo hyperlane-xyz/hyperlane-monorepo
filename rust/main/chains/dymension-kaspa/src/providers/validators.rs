594e1a72638f6824b31bef928e6b5a99823ffa26
danwt <30197399+danwt@users.noreply.github.com>
2025-06-19 09:28:52 +0100

    progress checkpoint: code might be broken

---
+++ rust/main/chains/dymension-kaspa/src/providers/validators.rs
@@ -0,0 +1,55 @@
+use std::future::Future;
+use std::time::Instant;
+
+use tonic::async_trait;
+
+use hyperlane_core::{
+    rpc_clients::BlockNumberGetter, ChainCommunicationError, ChainResult, FixedPointNumber, H512,
+    U256,
+};
+use hyperlane_metric::prometheus_metric::{
+    ClientConnectionType, PrometheusClientMetrics, PrometheusConfig,
+};
+use url::Url;
+
+use dym_kas_core::api::deposits::*;
+
+use crate::{ConnectionConf, HyperlaneKaspaError, Signer};
+
+use dym_kas_core::api::deposits::*;
+
+pub use dym_kas_core::api::deposits::*;
+
+
+
+#[derive(Debug, Clone)]
+pub struct ValidatorsClient {
+    pub conf: ConnectionConf,
+}
+
+#[async_trait]
+impl BlockNumberGetter for ValidatorsClient { // TODO: needed?
+    async fn get_block_number(&self) -> Result<u64, ChainCommunicationError> {
+        return ChainResult::Err(ChainCommunicationError::from_other_str("not implemented"));
+    }
+}
+
+
+/// It needs to
+/// 1. Call validator.G() to see if validator is OK with a new deposit on Kaspa
+/// 2. Call validator.G() to get a signed batch of PSKT for withdrawal TX flow
+/// 2. Call validator.G() to see if validator is OK with a confirmation of withdrawal on Kaspa
+impl ValidatorsClient {
+    /// Returns a new Rpc Provider
+    pub fn new(
+        conf: ConnectionConf,
+        // TODO: prom metrics?
+    ) -> ChainResult<Self> {
+
+    }
+
+
+    pub async fn get_deposits(&self, fxg: &DepositFXG) -> ChainResult<Vec<bool>> {
+        
+    }
+}
