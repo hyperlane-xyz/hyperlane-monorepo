46e6fff016426b5c9877232451022c2e1ecf3b70
Yorke Rhodes IV <yorke@hyperlane.xyz>
2025-06-19 15:45:53 -0400

    Implement fee hook

--- solidity/contracts/token/libs/FungibleTokenRouter.sol
+++ solidity/contracts/token/libs/FungibleTokenRouter.sol
@@ -3,6 +3,8 @@ pragma solidity >=0.8.0;
 
 import {TokenRouter} from "./TokenRouter.sol";
 import {Quote} from "../../interfaces/ITokenBridge.sol";
+import {ITokenFee} from "../interfaces/ITokenFee.sol";
+import {LinearFee} from "../fees/LinearFee.sol";
 
 /**
  * @title Hyperlane Fungible Token Router that extends TokenRouter with scaling logic for fungible tokens with different decimals.
@@ -11,8 +13,16 @@ import {Quote} from "../../interfaces/ITokenBridge.sol";
 abstract contract FungibleTokenRouter is TokenRouter {
     uint256 public immutable scale;
 
+    ITokenFee public feeRecipient;
+
     constructor(uint256 _scale, address _mailbox) TokenRouter(_mailbox) {
         scale = _scale;
+        // default to 0% fee for now
+        feeRecipient = new LinearFee(0, 1);
+    }
+
+    function setFeeRecipient(address _feeRecipient) public onlyOwner {
+        feeRecipient = ITokenFee(_feeRecipient);
     }
 
     function quoteTransferRemote(
@@ -36,7 +46,7 @@ abstract contract FungibleTokenRouter is TokenRouter {
     function _quoteTransferFee(
         uint256 _amount
     ) internal view returns (uint256) {
-        return (_amount * feeNumerator) / feeDenominator;
+        return feeRecipient.quoteTransfer(_amount);
     }
 
     function _deductAmountAndFeeFromSender(
@@ -45,7 +55,7 @@ abstract contract FungibleTokenRouter is TokenRouter {
         uint256 fee = _quoteTransferFee(_amount);
         metadata = _transferFromSender(_amount + fee);
         if (fee > 0) {
-            _transferTo(feeRecipient, fee, msg.data[0:0]);
+            _transferTo(address(feeRecipient), fee, msg.data[0:0]);
         }
     }
 
