8af262299f05a4d7bd8665e60baf59287e6d83b2
danwt <30197399+danwt@users.noreply.github.com>
2025-06-19 12:59:28 +0100

    USE AI CODE TO IMPL ROUTER FUNC

--- rust/main/chains/dymension-kaspa/src/router.rs
+++ rust/main/chains/dymension-kaspa/src/router.rs
@@ -4,42 +4,31 @@ use axum::{
     body::Bytes,
     extract::State,
     http::StatusCode,
-    response::{IntoResponse, Response},
+    response::{IntoResponse, Json, Response},
     routing::post,
     Router,
 };
 use dym_kas_core::deposit::DepositFXG;
-use hyperlane_core::CheckpointWithMessageId;
-use hyperlane_core::{ChainResult, Checkpoint, SignedType, H256};
+use hyperlane_core::{ChainResult, Checkpoint, CheckpointWithMessageId, SignedType, H256};
 use std::sync::Arc;
 
 pub struct AppError(eyre::Report);
 
 impl IntoResponse for AppError {
     fn into_response(self) -> Response {
-        eprintln!("Error: {:?}", self.0); // Log the full error to the console
-
+        eprintln!("Error: {:?}", self.0);
         (
             StatusCode::INTERNAL_SERVER_ERROR,
-            "Something went wrong".to_string(),
+            "An internal error occurred".to_string(),
         )
             .into_response()
-    }
-}
-
-impl<E> From<E> for AppError
-where
-    E: Into<eyre::Report>,
-{
-    fn from(err: E) -> Self {
-        Self(err.into())
     }
 }
 
 pub type AppResult<T> = Result<T, AppError>;
 
 #[async_trait]
-pub trait Signer {
+pub trait Signer: Send + Sync + 'static {
     async fn sign_checkpoint(
         &self,
         checkpoint: CheckpointWithMessageId,
@@ -47,69 +36,41 @@ pub trait Signer {
 }
 
 #[derive(Clone)]
-struct ISMHandler<S: Signer> {
+struct AppState<S: Signer> {
     signer: Arc<S>,
 }
 
-impl<S: Signer> ISMHandler<S> {
-    /*
-    What needs to happen
-    1. Relayer has the vec<Deposit>
-    2. Call F() to get FXG
-    3. Call network to validator with FXG, and what's needed to produce a sig
-    4. Call G(FXG) to check if to sign
-    5. Possibly sign
-    6. Return to relayer over network the digest
-     */
-    async fn validate_new_deposits(
-        State(state): State<ISMHandler<S>>,
-        body: Bytes,
-        // ) -> impl IntoResponse {
-    ) -> AppResult<String> {
-        let deposits: DepositFXG = body.try_into()?; // TODO: use
+async fn validate_new_deposits<S: Signer>(
+    State(state): State<Arc<AppState<S>>>,
+    body: Bytes,
+) -> AppResult<Json<String>> {
+    let deposits: DepositFXG = body.try_into().map_err(|e: eyre::Report| AppError(e))?;
 
-        let message_id = H256::random(); // TODO: parse from request
-        let to_sign: CheckpointWithMessageId = CheckpointWithMessageId {
-            checkpoint: Checkpoint {
-                mailbox_domain: 1,                        // TODO: populate
-                merkle_tree_hook_address: H256::random(), // TODO: zero
-                root: H256::random(),                     // TODO: zero
-                index: 0,
-            },
-            message_id: message_id,
-        }; // TODO: parse from request
+    let message_id = H256::random();
+    let to_sign: CheckpointWithMessageId = CheckpointWithMessageId {
+        checkpoint: Checkpoint {
+            mailbox_domain: 1,
+            merkle_tree_hook_address: H256::random(),
+            root: H256::random(),
+            index: 0,
+        },
+        message_id,
+    };
 
-        let sig = state.signer.sign_checkpoint(to_sign).await?;
+    let sig = state.signer.sign_checkpoint(to_sign).await?;
+    let j =
+        serde_json::to_string_pretty(&sig).map_err(|e: serde_json::Error| AppError(e.into()))?;
 
-        // let j = serde_json::to_string_pretty(&sig).map_err(|e| AppError(e.into()))?;
-        let j = serde_json::to_string_pretty(&sig)?;
-        Ok(j)
-
-        // How are they bundled?
-        // https://github.com/dymensionxyz/hyperlane-monorepo/blob/779828c92e48796ae9816fb9ccab23c9e56f82fb/rust/main/hyperlane-base/src/types/multisig.rs#L207
-
-        // TODO: produce a digest sig
-    }
+    Ok(Json(j))
 }
 
-// TODO: take some trait which can sign whats needed to sign
-pub fn router<S: Signer>(handler: ISMHandler<S>) -> Router {
-    Router::new().route(
-        ROUTE_VALIDATE_NEW_DEPOSITS,
-        post(ISMHandler::<S>::validate_new_deposits),
-    )
-    // .route(ROUTE_SIGN_PSKTS, post(sign_pskts))
-    // .route(
-    // ROUTE_VALIDATE_CONFIRMED_WITHDRAWALS,
-    // post(validate_confirmed_withdrawals),
+pub fn router<S: Signer>(signer: Arc<S>) -> Router {
+    let state = Arc::new(AppState { signer });
+
+    Router::new()
+        .route(
+            ROUTE_VALIDATE_NEW_DEPOSITS,
+            post(validate_new_deposits::<S>),
+        )
+        .with_state(state)
 }
-
-// async fn validate_new_deposits(body: Bytes) -> (StatusCode, Bytes) {}
-
-// async fn sign_pskts(body: Bytes) -> (StatusCode, Bytes) {
-//     unimplemented!()
-// }
-
-// async fn validate_confirmed_withdrawals(body: Bytes) -> (StatusCode, Bytes) {
-//     unimplemented!()
-// }
