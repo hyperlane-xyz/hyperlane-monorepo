c26351da2eaea8e8a3bac8a3e51d4a7412e3c512
Michael Tsitrin <michael@dymension.xyz>
2025-06-22 11:29:44 +0300

    merge

--- dymension/libs/kaspa/lib/relayer/src/lib.rs
+++ dymension/libs/kaspa/lib/relayer/src/lib.rs
@@ -1,5 +1,6 @@
 pub mod hub_to_kaspa_builder;
 pub mod withdraw;
+pub mod confirmation;
 
 // Re-export the main function for easier access
 pub use hub_to_kaspa_builder::build_kaspa_withdrawal_pskts;
