c7a636efce3d3582224587a6902d7c84f613df1a
Troy Kessler <troy.kessler99@gmail.com>
2025-06-23 09:04:46 +0200

    fix: gas consts

--- typescript/cli/src/consts.ts
+++ typescript/cli/src/consts.ts
@@ -5,16 +5,16 @@ export const MINIMUM_CORE_DEPLOY_GAS = {
   [ProtocolType.CosmosNative]: (1e8).toString(),
 };
 export const MINIMUM_WARP_DEPLOY_GAS = {
-  [ProtocolType.Ethereum]: (1e7).toString(),
-  [ProtocolType.CosmosNative]: (1e7).toString(),
+  [ProtocolType.Ethereum]: (3e7).toString(),
+  [ProtocolType.CosmosNative]: (3e7).toString(),
 };
 export const MINIMUM_TEST_SEND_GAS = {
-  [ProtocolType.Ethereum]: (1e5).toString(),
-  [ProtocolType.CosmosNative]: (1e5).toString(),
+  [ProtocolType.Ethereum]: (3e5).toString(),
+  [ProtocolType.CosmosNative]: (3e5).toString(),
 };
 export const MINIMUM_AVS_GAS = {
-  [ProtocolType.Ethereum]: (1e6).toString(),
-  [ProtocolType.CosmosNative]: (1e6).toString(),
+  [ProtocolType.Ethereum]: (3e6).toString(),
+  [ProtocolType.CosmosNative]: (3e6).toString(),
 };
 
 export const PROXY_DEPLOYED_URL = 'https://proxy.hyperlane.xyz';
