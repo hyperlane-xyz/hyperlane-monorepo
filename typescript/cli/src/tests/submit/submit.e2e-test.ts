59b96149689822812a961119bdb6456a6f662617
Le <6251863+ltyu@users.noreply.github.com>
2025-06-24 18:01:51 -0400

    Initial commit for e2e tests

---
+++ typescript/cli/src/tests/submit/submit.e2e-test.ts
@@ -0,0 +1,8 @@
+import { hyperlaneSubmit } from '../commands/submit.js';
+
+describe('hyperlane submit', function () {
+  it('should execute transactions', async function () {
+    hyperlaneSubmit();
+  });
+  xit('should output receipts', function () {});
+});
