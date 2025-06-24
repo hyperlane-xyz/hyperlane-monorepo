dc07a8b2e23aad77ee81530b37703e8f19f6b90a
kamiyaa <jeff.no.zhao@gmail.com>
2025-06-24 13:16:39 -0400

    feat: remove unnecessary span

--- rust/main/hyperlane-base/src/contract_sync/mod.rs
+++ rust/main/hyperlane-base/src/contract_sync/mod.rs
@@ -230,7 +230,6 @@ where
     }
 
     #[allow(clippy::too_many_arguments)]
-    #[instrument(fields(domain=domain.name()), skip(indexer, store, stored_logs_metric, indexed_height_metric, liveness_metric))]
     async fn cursor_indexer_task(
         domain: HyperlaneDomain,
         indexer: I,
