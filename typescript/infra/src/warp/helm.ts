b994a7237d25012f23ae8f683d56d612d8d6e9c9
xeno097 <xeno097.cp@gmail.com>
2025-06-26 11:52:40 -0400

    chore: update monorepo helm image tag

--- typescript/infra/src/warp/helm.ts
+++ typescript/infra/src/warp/helm.ts
@@ -76,7 +76,7 @@ export class WarpRouteMonitorHelmManager extends HelmManager {
     return {
       image: {
         repository: 'gcr.io/abacus-labs-dev/hyperlane-monorepo',
-        tag: 'a294a4d-20250617-101754',
+        tag: '83744ba-20250626-154411',
       },
       warpRouteId: this.warpRouteId,
       fullnameOverride: this.helmReleaseName,
