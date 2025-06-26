f621aff33a7827ca030fbf21d8d15f1772b995a5
Yorke Rhodes IV <yorke@hyperlane.xyz>
2025-06-26 16:54:30 -0400

    Rename warp IDs

--- typescript/infra/config/warp.ts
+++ typescript/infra/config/warp.ts
@@ -63,7 +63,6 @@ import { getEthereumZircuitRe7LRTWarpConfig } from './environments/mainnet3/warp
 import { getInevmInjectiveINJWarpConfig } from './environments/mainnet3/warp/configGetters/getInevmInjectiveINJWarpConfig.js';
 import { getMantapacificNeutronTiaWarpConfig } from './environments/mainnet3/warp/configGetters/getMantapacificNeutronTiaWarpConfig.js';
 import { getMintSolanaMintWarpConfig } from './environments/mainnet3/warp/configGetters/getMintSolanaMintWarpConfig.js';
-import { getRebalancingUSDCConfig } from './environments/mainnet3/warp/configGetters/getRebalancingUSDCConfig.js';
 import {
   getEZETHSTAGEGnosisSafeBuilderStrategyConfig,
   getRenzoEZETHSTAGEWarpConfig,
@@ -155,7 +154,6 @@ export const warpConfigGetterMap: Record<string, WarpConfigGetter> = {
     getArbitrumAvalancheBaseBscEthereumLumiaprismOptimismPolygonLUMIAWarpConfig,
   [WarpRouteIds.TestnetCCTP]: getTestnetCCTPWarpConfig,
   [WarpRouteIds.MainnetCCTP]: getMainnetCCTPWarpConfig,
-  [WarpRouteIds.MainnetRebalancingUSDC]: getRebalancingUSDCConfig,
 };
 
 type StrategyConfigGetter = () => ChainSubmissionStrategy;
