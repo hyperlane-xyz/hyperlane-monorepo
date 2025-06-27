f9a383952a85cf451f6c9647816883d99080871f
pbio <10051819+paulbalaji@users.noreply.github.com>
2025-06-27 18:02:07 +0100

    try

--- solidity/bytecodeversion.sh
+++ solidity/bytecodeversion.sh
@@ -14,3 +14,19 @@ EOF
 
 # overwrite the original file with the temp file
 cat $TEMPFILE > $FILEPATH
+
+# Update core-utils/index.ts as well
+FILEPATH2="core-utils/index.ts"
+TEMPFILE2=$(mktemp)
+
+# writes all but the last 2 lines to the temp file
+head -n $(($(wc -l < $FILEPATH2) - 2)) $FILEPATH2 > $TEMPFILE2
+
+# writes generated last 2 lines to the temp file
+cat <<EOF >> $TEMPFILE2
+// GENERATED CODE - DO NOT EDIT
+export const CONTRACTS_PACKAGE_VERSION = '$npm_package_version';
+EOF
+
+# overwrite the original file with the temp file
+cat $TEMPFILE2 > $FILEPATH2
--- solidity/core-utils/index.ts
+++ solidity/core-utils/index.ts
@@ -1,2 +1,4 @@
 export * from './typechain/index.js';
 export * from './zksync/index.js';
+// GENERATED CODE - DO NOT EDIT
+export const CONTRACTS_PACKAGE_VERSION = '8.0.2';
