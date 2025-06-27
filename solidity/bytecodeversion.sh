995056e3b4967c99f37f4fd8def719c8a16406cd
pbio <10051819+paulbalaji@users.noreply.github.com>
2025-06-27 18:14:58 +0100

    simplify bytecodeversion.sh

--- solidity/bytecodeversion.sh
+++ solidity/bytecodeversion.sh
@@ -1,32 +1,16 @@
 #!/bin/sh
 
-FILEPATH="contracts/PackageVersioned.sol"
-TEMPFILE=$(mktemp)
-
-# writes all but the last 2 lines to the temp file
-head -n $(($(wc -l < $FILEPATH) - 2)) $FILEPATH > $TEMPFILE
-
-# writes generated last 2 lines to the temp file
-cat <<EOF >> $TEMPFILE
-    string public constant PACKAGE_VERSION = "$npm_package_version";
+# Cross-platform in-place sed
+sedi() {
+  if [ "$(uname)" = "Darwin" ]; then
+    sed -i '' "$@"
+  else
+    sed -i "$@"
+  fi
 }
-EOF
 
-# overwrite the original file with the temp file
-cat $TEMPFILE > $FILEPATH
+# Update Solidity contract
+sedi "s|string public constant PACKAGE_VERSION = \".*\";|string public constant PACKAGE_VERSION = \"$npm_package_version\";|" contracts/PackageVersioned.sol
 
-# Update core-utils/index.ts as well
-FILEPATH2="core-utils/index.ts"
-TEMPFILE2=$(mktemp)
-
-# writes all but the last 2 lines to the temp file
-head -n $(($(wc -l < $FILEPATH2) - 2)) $FILEPATH2 > $TEMPFILE2
-
-# writes generated last 2 lines to the temp file
-cat <<EOF >> $TEMPFILE2
-// GENERATED CODE - DO NOT EDIT
-export const CONTRACTS_PACKAGE_VERSION = '$npm_package_version';
-EOF
-
-# overwrite the original file with the temp file
-cat $TEMPFILE2 > $FILEPATH2
+# Update TypeScript file
+sedi "s|export const CONTRACTS_PACKAGE_VERSION = '.*';|export const CONTRACTS_PACKAGE_VERSION = '$npm_package_version';|" core-utils/index.ts
