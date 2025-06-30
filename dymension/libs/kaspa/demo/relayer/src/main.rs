935d247127083908400f7c385a921a884db5b4d6
danwt <30197399+danwt@users.noreply.github.com>
2025-06-30 09:53:14 +0100

    Revert "feat: deposit & relay demo + fixes (#53)"
    This reverts commit 3adbec0666b94da11269887be0e78e6ec7e27166.

--- dymension/libs/kaspa/demo/relayer/src/main.rs
+++ dymension/libs/kaspa/demo/relayer/src/main.rs
@@ -2,23 +2,17 @@
 
 mod x;
 
-use std::error::Error;
-use core::api::client::get_local_testnet_client;
 use core::deposit::*;
 use core::escrow::*;
 use core::util::*;
 use core::wallet::*;
-use core::ESCROW_ADDRESS;
-use bytes::Bytes;
 use relayer::withdraw::*;
 use validator::withdraw::*;
-use relayer::handle_new_deposit;
-use validator::validate_deposit;
 use x::args::Args;
 use x::consts::*;
+
 use std::sync::Arc;
-use hyperlane_core::{Encode,Decode,H256,HyperlaneMessage,U256};
-use hyperlane_warp_route::TokenMessage;
+
 use kaspa_addresses::Address;
 use kaspa_consensus_core::{
     constants::TX_VERSION,
@@ -32,102 +26,98 @@ use kaspa_consensus_core::{
 use kaspa_core::info;
 use kaspa_grpc_client::GrpcClient;
 use kaspa_wallet_core::api::{AccountsSendRequest, WalletApi};
-use kaspa_wallet_core::error::Error as KaspaError;
+use kaspa_wallet_core::error::Error;
 use kaspa_wallet_core::tx::Fees;
 
 use kaspa_wallet_core::prelude::*;
 use kaspa_wallet_pskt::prelude::*; // Import the prelude for easy access to traits/structs
+
+use kaspa_txscript::{
+    extract_script_pub_key_address, multisig_redeem_script, pay_to_address_script,
+    pay_to_script_hash_script,
+};
 
 use secp256k1::{rand::thread_rng, Keypair};
 
 use kaspa_rpc_core::api::rpc::RpcApi;
 use workflow_core::abortable::Abortable;
 
-pub async fn deposit(
-    w: &Arc<Wallet>,
-    secret: &Secret,
-    address: Address,
-    amt: u64,
-) -> Result<TransactionId, KaspaError> {
-    let a = w.account()?;
+/*
+Demo:
+The purpose is to test out using a multisig for securing an escrow address.
+There are three roles, signer 1 and 2, and a relayer.
+The relayer is responsible for building and orchestrating the multisig TXs, including paying any fees.
+The signers are just responsible for signing.
 
-    let dst = PaymentDestination::from(PaymentOutput::new(address, amt));
-    let fees = Fees::from(0i64);
-    let payment_secret = None;
-    let abortable = Abortable::new();
+The test involves a 'user', which corresponds to the local wallet account.
 
-    let mut hl_message = HyperlaneMessage::default();
-    let token_message = TokenMessage::new(H256::random(), U256::from(amt), vec![]);
+Steps are:
 
-    let encoded_bytes = token_message.to_vec();
-    hl_message.body = encoded_bytes;
+1. Create an escrow address.
+2. User deposits some funds to the escrow address.
+3. The relayer builds a multisig TX to send the funds back to the user from the escrow address.
+4. The signers sign the TX.
+5. The relayer sends the TX to the network.
 
-    let payload = hl_message.to_vec();
-    // use account.send, because wallet.accounts_send(AccountsSendRequest{..}) is buggy
-    let (summary, _) = a
-        .send(
-            dst,
-            fees,
-            Some(payload),
-            secret.clone(),
-            payment_secret,
-            &abortable,
-            None,
-        )
-        .await?;
+Always, we want to get confirmation that everything has worked, been accepted by the network etc.
 
-    summary.final_transaction_id().ok_or_else(|| {
-        KaspaError::Custom("Deposit transaction failed to generate a transaction ID".to_string())
-    })
-}
+We will test against testnet 10. The wallet has 200'000 KAS available.
 
 
-async fn demo() -> Result<(), Box<dyn Error>> {
+ */
+async fn demo() -> Result<(), Error> {
     kaspa_core::log::init_logger(None, "");
 
-    // parse demo args
     let args = Args::parse();
 
-    // load wallet (using kaspa wallet)
     let s = Secret::from(args.wallet_secret.unwrap_or("".to_string()));
     let w = get_wallet(&s, NETWORK_ID, URL.to_string()).await?;
 
+    let rpc = w.rpc_api();
 
-    println!("address {}",&w.account()?.receive_address()?);
-    println!("balance {}",&w.account()?.get_list_string()?);
+    check_balance("wallet", rpc.as_ref(), &w.account()?.receive_address()?).await?;
 
-    // deposit to escrow address
+    let e = Escrow::new(2);
+    info!("Created escrow address: {}", e.public(ADDRESS_PREFIX).addr);
+
     let amt = DEPOSIT_AMOUNT;
-    let escrow_address = Address::try_from(ESCROW_ADDRESS)?;
-    let tx_id = deposit(&w, &s, escrow_address, amt).await?;
+    let tx_id = deposit(&w, &s, &e, amt, ADDRESS_PREFIX).await?;
     info!("Sent deposit transaction: {}", tx_id);
 
-    // wait (it may take some time that the deposit is available to indexer-archive rpc service)
-    workflow_core::task::sleep(std::time::Duration::from_secs(10)).await;
+    workflow_core::task::sleep(std::time::Duration::from_secs(5)).await;
 
-    // handle deposit (relayer operation)
-    let deposit_fxg = handle_new_deposit(tx_id.to_string()).await?;
+    check_balance("wallet", rpc.as_ref(), &w.account()?.receive_address()?).await?;
+    check_balance("escrow", rpc.as_ref(), &e.public(ADDRESS_PREFIX).addr).await?;
 
-    // deposit encode to bytes 
-    let deposit_bytes_recv: Bytes = (&deposit_fxg).into(); 
+    let user_addr = w.account()?.receive_address()?;
 
-    // deposit from bytes
-    let deposit_recv  = DepositFXG::try_from(deposit_bytes_recv)?;
+    let pskt_unsigned = build_withdrawal_tx(
+        rpc.as_ref(),
+        &e.public(ADDRESS_PREFIX),
+        user_addr,
+        &w.account()?,
+        amt,
+        RELAYER_NETWORK_FEE,
+    )
+    .await?;
 
-    println!("Deposit pulled by relay tx_id:{} block_id:{} amount:{}", deposit_recv.tx_id, deposit_recv.block_id,deposit_recv.amount);
+    let pskt_signed_vals = sign_escrow_spend(&e, pskt_unsigned.clone())?;
 
-    // load kaspa validator rpc client 
-    let client = get_local_testnet_client().await?;
-    // validate deposit using kaspa rpc (validator operation)
-    let validation_result = validate_deposit(&client,deposit_recv).await?;
+    let tx_id = send_tx(
+        rpc.as_ref(),
+        pskt_signed_vals,
+        pskt_unsigned,
+        &e.public(ADDRESS_PREFIX),
+        &w,
+        &s,
+    )
+    .await?;
 
-    if validation_result {
-        println!("Deposit validated");
-    } else {
-        println!("Failed to validate deposit");
-    }
+    workflow_core::task::sleep(std::time::Duration::from_secs(5)).await;
 
-    client.stop().await?;
+    check_balance("wallet", rpc.as_ref(), &w.account()?.receive_address()?).await?;
+    check_balance("escrow", rpc.as_ref(), &e.public(ADDRESS_PREFIX).addr).await?;
+
     w.stop().await?;
     Ok(())
 }
--- dymension/libs/kaspa/lib/validator/src/deposit.rs
+++ dymension/libs/kaspa/lib/validator/src/deposit.rs
@@ -1,18 +1,7 @@
-use core::deposit::DepositFXG;
-use std::error::Error;
+use core::{confirmation::ConfirmationFXG, deposit::DepositFXG, withdraw::WithdrawFXG};
 
 use eyre::Result;
-use kaspa_wrpc_client::KaspaRpcClient;
 
-use crate::validate_deposit;
-
-pub async fn validate_deposits(client: &KaspaRpcClient, deposits: Vec<DepositFXG>) -> Result<Vec<bool>, Box<dyn Error>> {
-
-    let mut results: Vec<bool> = vec![];
-    // iterate over all deposits and validate one by one
-    for deposit in deposits {
-        let result = validate_deposit(client,deposit).await?;
-        results.push(result);
-    }
-    Ok(results)
+pub async fn validate_deposits(fxg: &DepositFXG) -> Result<bool> {
+    Ok(true)
 }
