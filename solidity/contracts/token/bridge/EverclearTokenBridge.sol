11cb16f91cfae3afdfcc5b33a579b4b73c36196c
larryob <tosin@hyperlane.xyz>
2025-07-01 14:51:40 -0400

    Add some events and remove absolute imports

--- solidity/contracts/token/bridge/EverclearTokenBridge.sol
+++ solidity/contracts/token/bridge/EverclearTokenBridge.sol
@@ -1,14 +1,14 @@
 // SPDX-License-Identifier: MIT OR Apache-2.0
 pragma solidity ^0.8.22;
 
-import {ITokenBridge, Quote} from "contracts/interfaces/ITokenBridge.sol";
-import {HypERC20Collateral} from "contracts/token/HypERC20Collateral.sol";
-import {IEverclearAdapter} from "contracts/interfaces/IEverclearAdapter.sol";
+import {ITokenBridge, Quote} from "../../interfaces/ITokenBridge.sol";
+import {HypERC20Collateral} from "../HypERC20Collateral.sol";
+import {IEverclearAdapter} from "../../interfaces/IEverclearAdapter.sol";
 
 contract EverclearTokenBridge is HypERC20Collateral {
     /// @notice The output asset for a given destination.
     /// @dev Everclear needs to know the output asset address to create intents.
-    mapping(uint32 destination => bytes32 outputAsset) public outputAssets;
+    mapping(uint32 destination => bytes32 outputAssets) public outputAssets;
 
     /// @notice Fee parameters for the bridge.
     /// @dev The signatures are produced by everclear and stored here for re-use. We use the same fee for all transfers to all destinations.
@@ -16,6 +16,9 @@ contract EverclearTokenBridge is HypERC20Collateral {
 
     /// @notice The everclear `FeeAdapter` contract
     IEverclearAdapter public immutable everclearAdapter;
+
+    event FeeParamsUpdated(uint256 fee, uint256 deadline);
+    event OutputAssetSet(uint32 destination, bytes32 outputAsset);
 
     function setFeeParams(
         uint256 _fee,
@@ -27,6 +30,7 @@ contract EverclearTokenBridge is HypERC20Collateral {
             deadline: _deadline,
             sig: _sig
         });
+        emit FeeParamsUpdated(_fee, _deadline);
     }
 
     function setOutputAsset(
@@ -34,6 +38,7 @@ contract EverclearTokenBridge is HypERC20Collateral {
         bytes32 _outputAsset
     ) external onlyOwner {
         outputAssets[_destination] = _outputAsset;
+        emit OutputAssetSet(_destination, _outputAsset);
     }
 
     constructor(
@@ -76,11 +81,13 @@ contract EverclearTokenBridge is HypERC20Collateral {
         bytes32 _recipient,
         uint256 _amount
     ) external payable override returns (bytes32) {
+        IEverclearAdapter.FeeParams memory _feeParams = feeParams;
+
         // Charge sender for the fee
-        _transferFromSender(_amount + feeParams.fee);
+        _transferFromSender(_amount + _feeParams.fee);
 
         // Create everclear intent
-        _createIntent(_destination, _recipient, _amount);
+        _createIntent(_destination, _recipient, _amount, _feeParams);
 
         // A hyperlane message will be sent by everclear internally.
         return bytes32(0);
@@ -90,7 +97,8 @@ contract EverclearTokenBridge is HypERC20Collateral {
     function _createIntent(
         uint32 _destination,
         bytes32 _recipient,
-        uint256 _amount
+        uint256 _amount,
+        IEverclearAdapter.FeeParams memory _feeParams
     ) internal {
         bytes32 outputAsset = outputAssets[_destination];
         require(outputAsset != bytes32(0), "ETB: Output asset not set");
@@ -98,7 +106,7 @@ contract EverclearTokenBridge is HypERC20Collateral {
         // Check that the fee params are still valid
         // This will also revert if the feeParams are not set
         require(
-            feeParams.deadline > block.timestamp,
+            _feeParams.deadline > block.timestamp,
             "ETB: Fee params deadline expired"
         );
 
@@ -115,7 +123,7 @@ contract EverclearTokenBridge is HypERC20Collateral {
             _maxFee: 0,
             _ttl: 0,
             _data: "",
-            _feeParams: feeParams
+            _feeParams: _feeParams
         });
     }
 }
