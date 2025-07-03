73809c5148891332910e73246cf6d9ee9c6a5e73
Mohammed Hussan <mo@hyperlane.xyz>
2025-07-03 16:50:34 +0100

    feat: add config USDC/lumia rebalancer (#6678)

--- typescript/infra/src/rebalancer/helm.ts
+++ typescript/infra/src/rebalancer/helm.ts
@@ -62,7 +62,7 @@ export class RebalancerHelmManager extends HelmManager {
     return {
       image: {
         repository: 'gcr.io/abacus-labs-dev/hyperlane-monorepo',
-        tag: 'a546be2-20250602-142949',
+        tag: '131646d-20250703-143300',
       },
       withMetrics: this.withMetrics,
       fullnameOverride: this.helmReleaseName,
