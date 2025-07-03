e68c67d5a5aa38f16f740d262aee606cdd520e0b
Yorke Rhodes IV <yorke@hyperlane.xyz>
2025-07-03 16:29:21 -0400

    Revert compiler version floor

--- solidity/contracts/token/TokenBridgeCctp.sol
+++ solidity/contracts/token/TokenBridgeCctp.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT OR Apache-2.0
-pragma solidity >=0.8.26;
+pragma solidity >=0.8.0;
 
 import {TokenRouter} from "./libs/TokenRouter.sol";
 import {HypERC20Collateral} from "./HypERC20Collateral.sol";
