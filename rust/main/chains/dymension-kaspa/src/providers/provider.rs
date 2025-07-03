2fa0091c9b9f575e987290952d62633f9489db5c
danwt <30197399+danwt@users.noreply.github.com>
2025-07-03 16:40:29 +0100

    progress checkpoint: code might be broken

--- rust/main/chains/dymension-kaspa/src/providers/provider.rs
+++ rust/main/chains/dymension-kaspa/src/providers/provider.rs
@@ -16,7 +16,7 @@ use dym_kas_core::escrow::EscrowPublic;
 use dym_kas_core::withdraw::WithdrawFXG;
 use dym_kas_relayer::withdraw::hub_to_kaspa::on_new_withdrawals;
 use dym_kas_relayer::withdraw::hub_to_kaspa::finalize_pskt;
-use dym_kas_relayer::withdraw::demo::sign_pay_fee;
+use dym_kas_relayer::withdraw::hub_to_kaspa::sign_pay_fee;
 pub use dym_kas_validator::KaspaSecpKeypair;
 use hyperlane_core::{
     BlockInfo, ChainInfo, ChainResult, HyperlaneChain, HyperlaneDomain, HyperlaneMessage,
