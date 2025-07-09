97b610a21f2f22549de09f9e950e5e880500d1cd
Mo Hussan <mohammed.hussan@outlook.com>
2025-07-09 11:21:52 +0100

    use 3001 as default port

--- typescript/http-registry-server/src/constants/ServerConstants.ts
+++ typescript/http-registry-server/src/constants/ServerConstants.ts
@@ -1,5 +1,5 @@
 export const ServerConstants = Object.freeze({
-  DEFAULT_PORT: 3000,
+  DEFAULT_PORT: 3001,
   DEFAULT_REFRESH_INTERVAL: 1000 * 60 * 5, // 5 minutes
   DEFAULT_HOST: '127.0.0.1',
 } as const);
