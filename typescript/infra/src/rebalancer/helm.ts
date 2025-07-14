695e3404052b53e8e0dd028da550b1d3d5283bab
Mo Hussan <mohammed.hussan@outlook.com>
2025-07-14 12:25:53 +0100

    update image

--- typescript/infra/src/rebalancer/helm.ts
+++ typescript/infra/src/rebalancer/helm.ts
@@ -62,7 +62,7 @@ export class RebalancerHelmManager extends HelmManager {
     return {
       image: {
         repository: 'gcr.io/abacus-labs-dev/hyperlane-monorepo',
-        tag: 'ef09844-20250707-104152',
+        tag: '0299400-20250711-180454',
       },
       withMetrics: this.withMetrics,
       fullnameOverride: this.helmReleaseName,
