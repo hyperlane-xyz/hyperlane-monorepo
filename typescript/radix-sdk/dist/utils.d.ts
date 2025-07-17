ce3169246c54807223aecf7a84464710bc086b99
Troy Kessler <troy.kessler99@gmail.com>
2025-07-17 14:31:08 +0200

    chore: remove folder

--- typescript/radix-sdk/dist/utils.d.ts
+++
@@ -1,3 +0,0 @@
-import { Value } from '@radixdlt/radix-engine-toolkit';
-export declare const bytes: (hex: string) => Value;
-//# sourceMappingURL=utils.d.ts.map
--- typescript/radix-sdk/dist/utils.js
+++
@@ -1,5 +0,0 @@
-import { Convert, ValueKind, array, u8, } from '@radixdlt/radix-engine-toolkit';
-export const bytes = (hex) => {
-    return array(ValueKind.U8, ...Array.from(Convert.HexString.toUint8Array(hex).values()).map((item) => u8(item)));
-};
-//# sourceMappingURL=utils.js.map
