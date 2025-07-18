1877c8a8325341d0afa0e4ea7f285bbb007f2468
xeno097 <xeno097.cp@gmail.com>
2025-07-18 15:24:43 -0400

    feat(sdk): updated the timelock deployer

--- typescript/sdk/src/timelock/evm/EvmTimelockDeployer.hardhat-test.ts
+++ typescript/sdk/src/timelock/evm/EvmTimelockDeployer.hardhat-test.ts
@@ -1,9 +1,14 @@
+import { JsonRpcProvider } from '@ethersproject/providers';
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers.js';
 import chai, { expect } from 'chai';
 import chaiAsPromised from 'chai-as-promised';
 import hre from 'hardhat';
 
-import { TimelockController__factory } from '@hyperlane-xyz/core';
+import {
+  TimelockController,
+  TimelockController__factory,
+} from '@hyperlane-xyz/core';
+import { assert, randomInt } from '@hyperlane-xyz/utils';
 
 import { TestChainName } from '../../consts/testChains.js';
 import { MultiProvider } from '../../providers/MultiProvider.js';
@@ -19,10 +24,11 @@ describe('EvmTimelockDeployer', async () => {
   let multiProvider: MultiProvider;
   let deployer: EvmTimelockDeployer;
   let signer: SignerWithAddress;
+  let otherSigner: SignerWithAddress;
   let config: TimelockConfig;
 
-  before(async () => {
-    [signer] = await hre.ethers.getSigners();
+  beforeEach(async () => {
+    [signer, otherSigner] = await hre.ethers.getSigners();
     multiProvider = MultiProvider.createTestMultiProvider({ signer });
     deployer = new EvmTimelockDeployer(multiProvider);
     config = {
@@ -51,6 +57,7 @@ describe('EvmTimelockDeployer', async () => {
     expect(await timelock.getMinDelay()).to.equal(config.minimumDelay);
     expect(await timelock.hasRole(PROPOSER_ROLE, config.proposers[0])).to.be
       .true;
+    assert(config.executors, 'Expected executors to be defined');
     expect(await timelock.hasRole(EXECUTOR_ROLE, config.executors[0])).to.be
       .true;
     expect(await timelock.hasRole(timelockAdminRoleHash, config.admin!)).to.be
@@ -87,6 +94,7 @@ describe('EvmTimelockDeployer', async () => {
       expect(await timelock.hasRole(CANCELLER_ROLE, proposer)).to.be.true;
     }
 
+    assert(multiConfig.executors, 'Expected executors to be defined');
     for (const executor of multiConfig.executors) {
       expect(await timelock.hasRole(EXECUTOR_ROLE, executor)).to.be.true;
     }
@@ -163,5 +171,184 @@ describe('EvmTimelockDeployer', async () => {
     );
 
     expect(await timelock.getMinDelay()).to.equal(0);
+  });
+
+  it('should set anyone as a executor if no one is set in the input config', async () => {
+    const zeroDelayConfig: TimelockConfig = {
+      minimumDelay: 0,
+      proposers: [signer.address],
+      admin: signer.address,
+    };
+
+    const { TimelockController } = await deployer.deployContracts(
+      TestChainName.test4,
+      zeroDelayConfig,
+    );
+    const timelockAddress = TimelockController.address;
+
+    const timelock = TimelockController__factory.connect(
+      timelockAddress,
+      multiProvider.getProvider(TestChainName.test4),
+    );
+
+    const _0_BYTES_32 =
+      '0x0000000000000000000000000000000000000000000000000000000000000000';
+    const updatedDelay = 100;
+    const testTxData =
+      TimelockController__factory.createInterface().encodeFunctionData(
+        'updateDelay',
+        [updatedDelay],
+      );
+
+    const signerTimelock = TimelockController__factory.connect(
+      timelockAddress,
+      signer,
+    );
+    const tx = await signerTimelock.schedule(
+      timelockAddress,
+      0,
+      testTxData,
+      _0_BYTES_32,
+      _0_BYTES_32,
+      0,
+    );
+    await tx.wait();
+
+    const randomSignerTimelockInstance = TimelockController__factory.connect(
+      timelockAddress,
+      otherSigner,
+    );
+    const executeTx = await randomSignerTimelockInstance.execute(
+      timelockAddress,
+      0,
+      testTxData,
+      _0_BYTES_32,
+      _0_BYTES_32,
+    );
+    await executeTx.wait();
+
+    expect(await timelock.getMinDelay()).to.equal(updatedDelay);
+  });
+
+  async function assertCancellerConfig(
+    expectedConfig: TimelockConfig,
+    timelockInstance: TimelockController,
+  ) {
+    // proposer should be a proposer but not a canceller if not in canceller config
+    assert(expectedConfig.proposers, 'Expected proposers to be defined');
+    for (const proposer of expectedConfig.proposers) {
+      expect(await timelockInstance.hasRole(PROPOSER_ROLE, proposer)).to.be
+        .true;
+
+      if (!expectedConfig.cancellers?.includes(proposer)) {
+        expect(await timelockInstance.hasRole(CANCELLER_ROLE, proposer)).to.be
+          .false;
+      }
+    }
+
+    // cancellers should be a canceller but not a proposer if not in the proposer config
+    assert(expectedConfig.cancellers, 'Expected cancellers to be defined');
+    for (const canceller of expectedConfig.cancellers) {
+      expect(await timelockInstance.hasRole(CANCELLER_ROLE, canceller)).to.be
+        .true;
+      if (!expectedConfig.proposers?.includes(canceller)) {
+        expect(await timelockInstance.hasRole(PROPOSER_ROLE, canceller)).to.be
+          .false;
+      }
+    }
+
+    // signer should not be the timelock admin after deployment
+    const timelockAdminRoleHash = await timelockInstance.TIMELOCK_ADMIN_ROLE();
+    expect(
+      await timelockInstance.hasRole(timelockAdminRoleHash, signer.address),
+    ).to.be.false;
+
+    // Timelock should still be the admin of itself
+    expect(
+      await timelockInstance.hasRole(
+        timelockAdminRoleHash,
+        timelockInstance.address,
+      ),
+    ).to.be.true;
+
+    // if an admin was set it should be the admin after the changes
+    if (expectedConfig.admin) {
+      expect(
+        await timelockInstance.hasRole(
+          timelockAdminRoleHash,
+          expectedConfig.admin,
+        ),
+      ).to.be.true;
+    }
+  }
+
+  it('should deploy with the correct canceller config', async () => {
+    const proposer = randomAddress();
+    const cancellerConfig: TimelockConfig = {
+      minimumDelay: 30,
+      proposers: [proposer],
+      executors: [signer.address],
+      cancellers: [randomAddress(), randomAddress()],
+    };
+
+    const { TimelockController } = await deployer.deployContracts(
+      TestChainName.test4,
+      cancellerConfig,
+    );
+    const timelockAddress = TimelockController.address;
+
+    const timelock = TimelockController__factory.connect(
+      timelockAddress,
+      multiProvider.getProvider(TestChainName.test4),
+    );
+
+    await assertCancellerConfig(cancellerConfig, timelock);
+  });
+
+  it('should not remove a proposer that it is also a canceller', async () => {
+    const proposer = randomAddress();
+    const cancellerConfig: TimelockConfig = {
+      minimumDelay: 30,
+      proposers: [proposer],
+      executors: [signer.address],
+      cancellers: [proposer, randomAddress()],
+    };
+
+    const { TimelockController } = await deployer.deployContracts(
+      TestChainName.test4,
+      cancellerConfig,
+    );
+    const timelockAddress = TimelockController.address;
+
+    const timelock = TimelockController__factory.connect(
+      timelockAddress,
+      multiProvider.getProvider(TestChainName.test4),
+    );
+
+    await assertCancellerConfig(cancellerConfig, timelock);
+  });
+
+  it('should set the expected admin from the config after applying the canceller changes', async () => {
+    const proposer = randomAddress();
+    const cancellerConfig: TimelockConfig = {
+      minimumDelay: 30,
+      proposers: [proposer],
+      executors: [signer.address],
+      cancellers: [proposer, randomAddress()],
+      admin: randomAddress(),
+    };
+
+    const { TimelockController } = await deployer.deployContracts(
+      TestChainName.test4,
+      cancellerConfig,
+    );
+    const timelockAddress = TimelockController.address;
+
+    const timelock = TimelockController__factory.connect(
+      timelockAddress,
+      multiProvider.getProvider(TestChainName.test4),
+    );
+
+    await assertCancellerConfig(cancellerConfig, timelock);
   });
 });
--- typescript/sdk/src/timelock/evm/EvmTimelockDeployer.ts
+++ typescript/sdk/src/timelock/evm/EvmTimelockDeployer.ts
@@ -9,6 +9,7 @@ import { HyperlaneContracts } from '../../index.js';
 import { MultiProvider } from '../../providers/MultiProvider.js';
 import { TimelockConfig } from '../types.js';
 
+import { CANCELLER_ROLE } from './constants.js';
 import { EvmTimelockFactories, evmTimelockFactories } from './contracts.js';
 
 export class EvmTimelockDeployer extends HyperlaneDeployer<
@@ -32,15 +33,72 @@ export class EvmTimelockDeployer extends HyperlaneDeployer<
     chain: string,
     config: TimelockConfig,
   ): Promise<HyperlaneContracts<EvmTimelockFactories>> {
+    const currentDeployer = this.multiProvider.getSigner(chain);
+    const deployerAddress = await currentDeployer.getAddress();
+
+    const admin = config.cancellers ? deployerAddress : config.admin;
+    const expectedFinalAdmin = config.admin ?? ethers.constants.AddressZero;
     const deployedContract = await this.deployContract(
       chain,
       'TimelockController',
       [
         config.minimumDelay,
         config.proposers,
-        config.executors,
-        config.admin ?? ethers.constants.AddressZero,
+        config.executors ?? [ethers.constants.AddressZero],
+        admin ?? ethers.constants.AddressZero,
       ],
+    );
+
+    if (!config.cancellers) {
+      return {
+        TimelockController: deployedContract,
+      };
+    }
+
+    // Remove all the proposers that should not be cancellers
+    const cancellers = new Set(config.cancellers ?? []);
+    const cancellersToRemove = config.proposers.filter(
+      (address) => !cancellers.has(address),
+    );
+
+    this.logger.info(`Revoking CANCELLER_ROLE to ${cancellersToRemove}`);
+    for (const proposer of cancellersToRemove) {
+      await this.multiProvider.handleTx(
+        chain,
+        deployedContract.revokeRole(CANCELLER_ROLE, proposer),
+      );
+    }
+
+    // Give canceller role only to the addresses in the cancellers config
+    this.logger.info(`Setting CANCELLER_ROLE to ${config.cancellers}`);
+    for (const canceller of config.cancellers) {
+      await this.multiProvider.handleTx(
+        chain,
+        deployedContract.grantRole(CANCELLER_ROLE, canceller),
+      );
+    }
+
+    const adminRole = await deployedContract.TIMELOCK_ADMIN_ROLE();
+    const isAdminSetCorrectly = await deployedContract.hasRole(
+      adminRole,
+      expectedFinalAdmin,
+    );
+    if (!isAdminSetCorrectly) {
+      this.logger.info(
+        `Granting admin role to the expected admin ${expectedFinalAdmin}`,
+      );
+      await this.multiProvider.handleTx(
+        chain,
+        deployedContract.grantRole(adminRole, expectedFinalAdmin),
+      );
+    }
+
+    this.logger.info(
+      `Revoking temporary admin role from deployer ${deployerAddress}`,
+    );
+    await this.multiProvider.handleTx(
+      chain,
+      deployedContract.revokeRole(adminRole, deployerAddress),
     );
 
     return {
