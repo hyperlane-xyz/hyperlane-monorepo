3a7c9483ffb4294eeeab01e55774ef19fa71482c
mbreithecker <m@breithecker.de>
2025-07-30 11:26:53 +0200

    chore: update agents

--- typescript/infra/config/environments/mainnet3/agent.ts
+++ typescript/infra/config/environments/mainnet3/agent.ts
@@ -871,7 +871,7 @@ const hyperlane: RootAgentConfig = {
   validators: {
     docker: {
       repo,
-      tag: '5291797-20250701-134531',
+      tag: 'e3559a3-20250730-083713',
     },
     rpcConsensusType: RpcConsensusType.Quorum,
     chains: validatorChainConfig(Contexts.Hyperlane),
@@ -882,7 +882,7 @@ const hyperlane: RootAgentConfig = {
     rpcConsensusType: RpcConsensusType.Fallback,
     docker: {
       repo,
-      tag: '5291797-20250701-134531',
+      tag: 'e3559a3-20250730-083713',
     },
     resources: scraperResources,
   },
@@ -897,7 +897,7 @@ const releaseCandidate: RootAgentConfig = {
     rpcConsensusType: RpcConsensusType.Fallback,
     docker: {
       repo,
-      tag: '2fe58b3-20250718-114304',
+      tag: 'e3559a3-20250730-083713',
     },
     blacklist,
     // We're temporarily (ab)using the RC relayer as a way to increase
