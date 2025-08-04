415d45a198f3ef0a0f3b1fc08b842afb25226a02
kamiyaa <jeff.no.zhao@gmail.com>
2025-08-04 12:17:20 -0400

    feat: update validator image

--- typescript/infra/config/environments/mainnet3/agent.ts
+++ typescript/infra/config/environments/mainnet3/agent.ts
@@ -868,7 +868,7 @@ const hyperlane: RootAgentConfig = {
   validators: {
     docker: {
       repo,
-      tag: '5291797-20250701-134531',
+      tag: 'bbf3258-20250804-133531',
     },
     rpcConsensusType: RpcConsensusType.Quorum,
     chains: validatorChainConfig(Contexts.Hyperlane),
