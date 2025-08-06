6b768a6ed7d75258b4d256bc99c95ff8a4ee603b
Danil Nemirovsky <4614623+ameten@users.noreply.github.com>
2025-08-06 17:22:15 +0100

    Another attempt to add span with domain

--- rust/main/agents/scraper/src/agent.rs
+++ rust/main/agents/scraper/src/agent.rs
@@ -5,7 +5,7 @@ use derive_more::AsRef;
 use futures::future::try_join_all;
 use hyperlane_core::{Delivery, HyperlaneDomain, HyperlaneMessage, InterchainGasPayment, H512};
 use tokio::{sync::mpsc::Receiver as MpscReceiver, task::JoinHandle};
-use tracing::{info, info_span, trace, Instrument};
+use tracing::{info, info_span, instrument, trace, Instrument};
 
 use hyperlane_base::{
     broadcast::BroadcastMpscSender, metrics::AgentMetrics, settings::IndexSettings, AgentMetadata,
@@ -149,6 +149,7 @@ impl BaseAgent for Scraper {
 impl Scraper {
     /// Sync contract data and other blockchain with the current chain state.
     /// This will spawn long-running contract sync tasks
+    #[instrument(fields(domain=%domain.name()), skip(self, scraper))]
     async fn scrape(&self, scraper: &ChainScraper) -> eyre::Result<JoinHandle<()>> {
         let store = scraper.store.clone();
         let index_settings = scraper.index_settings.clone();
