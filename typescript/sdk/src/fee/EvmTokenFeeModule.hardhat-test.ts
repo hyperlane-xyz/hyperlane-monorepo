b7930c9f9dc61486b725ab3aaf2c75eab14c4f7c
Le <6251863+ltyu@users.noreply.github.com>
2025-08-06 17:51:45 -0400

    Add Reader and Module with create()

---
+++ typescript/sdk/src/fee/EvmTokenFeeModule.hardhat-test.ts
@@ -0,0 +1,44 @@
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers.js';
+import { expect } from 'chai';
+import hre from 'hardhat';
+
+import { ERC20Test, ERC20Test__factory } from '@hyperlane-xyz/core';
+
+import { TestChainName } from '../consts/testChains.js';
+import { MultiProvider } from '../providers/MultiProvider.js';
+
+import { EvmTokenFeeModule } from './EvmTokenFeeModule.js';
+import { TokenFeeConfig, TokenFeeType } from './types.js';
+
+const MAX_FEE = '100000000000000000000';
+const HALF_AMOUNT = '50000000000000000000';
+describe('EvmTokenFeeModule', () => {
+  let multiProvider: MultiProvider;
+  let signer: SignerWithAddress;
+  let token: ERC20Test;
+  let config: Extract<TokenFeeConfig, { type: TokenFeeType.LinearFee }>;
+  before(async () => {
+    [signer] = await hre.ethers.getSigners();
+    multiProvider = MultiProvider.createTestMultiProvider({ signer });
+    const factory = new ERC20Test__factory(signer);
+    token = await factory.deploy('fake', 'FAKE', '100000000000000000000', 18);
+    await token.deployed();
+    config = {
+      type: TokenFeeType.LinearFee,
+      owner: signer.address,
+      token: token.address,
+      maxFee: MAX_FEE,
+      halfAmount: HALF_AMOUNT,
+      bps: 1000,
+    };
+  });
+  it('should create a new token fee', async () => {
+    const module = await EvmTokenFeeModule.create({
+      multiProvider,
+      chain: TestChainName.test2,
+      config,
+    });
+    const onchainConfig = await module.read();
+    expect(onchainConfig).to.deep.equal(config);
+  });
+});
