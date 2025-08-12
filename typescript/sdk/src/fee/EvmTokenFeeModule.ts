71da9de4393b20a993bc05592621372357520c03
Le <6251863+ltyu@users.noreply.github.com>
2025-08-12 19:11:22 -0400

    Add getDeployedFeeAddress to module

--- typescript/sdk/src/fee/EvmTokenFeeModule.ts
+++ typescript/sdk/src/fee/EvmTokenFeeModule.ts
@@ -76,6 +76,11 @@ export class EvmTokenFeeModule extends HyperlaneModule<
     return module;
   }
 
+  // Public accessor for the deployed fee contract address
+  getDeployedFeeAddress(): Address {
+    return this.args.addresses.deployedFee;
+  }
+
   async read(): Promise<DerivedTokenFeeConfig> {
     return this.reader.deriveTokenFeeConfig(this.args.addresses.deployedFee);
   }
