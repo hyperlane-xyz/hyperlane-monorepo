0142661c35e627ef6ac9eaf1ade0b5c85de6d37e
xeno097 <xeno097.cp@gmail.com>
2025-08-18 13:29:03 -0400

    feat(infra): updated ubtc config getter

--- typescript/infra/config/environments/mainnet3/warp/configGetters/getBsquaredUBTCWarpConfig.ts
+++ typescript/infra/config/environments/mainnet3/warp/configGetters/getBsquaredUBTCWarpConfig.ts
@@ -14,6 +14,7 @@ import {
 
 const safeOwners: ChainMap<Address> = {
   bsquared: '0x7A363efD42305BeDBA307d25351F8ea157b69A1A',
+  ethereum: '0x47ECa4cbfB1a4849Eea25892d1B15aC3F02f24Ed',
   swell: '0xC11e22A31787394950B31e2DEb1d2b5546689B65',
   boba: '0x207FfFa7325fC5d0362aB01605D84B268b61888f',
   soneium: '0x8433e6e9183B5AAdaf4b52c624B963D95956e3C9',
@@ -45,6 +46,15 @@ export const getBsquaredUBTCWarpConfig = async (
     decimals: 18,
   };
 
+  const ethereum: HypTokenRouterConfig = {
+    mailbox: routerConfig.ethereum.mailbox,
+    owner: safeOwners.ethereum,
+    type: TokenType.synthetic,
+    name: 'uBTC',
+    symbol: 'uBTC',
+    decimals: 18,
+  };
+
   const soneium: HypTokenRouterConfig = {
     mailbox: routerConfig.soneium.mailbox,
     owner: safeOwners.soneium,
@@ -60,6 +70,7 @@ export const getBsquaredUBTCWarpConfig = async (
   return {
     boba,
     bsquared,
+    ethereum,
     nibiru,
     soneium,
     swell,
@@ -73,7 +84,7 @@ export function getUbtcOwnerConfigGenerator(safes: ChainMap<Address>) {
         chain,
         {
           submitter:
-            chain === 'nibiru'
+            chain === 'ethereum'
               ? {
                   type: TxSubmitterType.JSON_RPC,
                   chain,
