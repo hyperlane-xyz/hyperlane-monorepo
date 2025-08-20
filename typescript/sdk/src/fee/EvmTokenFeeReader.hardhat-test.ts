af7a82bd7c38e25d195c377e3435b42be5f4f33f
Le <6251863+ltyu@users.noreply.github.com>
2025-08-20 16:04:36 -0400

    Fix tests where it unexpectedly hangs by moving out shared consts from before

--- typescript/sdk/src/fee/EvmTokenFeeReader.hardhat-test.ts
+++ typescript/sdk/src/fee/EvmTokenFeeReader.hardhat-test.ts
@@ -1,61 +1,59 @@
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers.js';
 import { expect } from 'chai';
+import { constants } from 'ethers';
 import hre from 'hardhat';
 
-import { ERC20Test, ERC20Test__factory, LinearFee } from '@hyperlane-xyz/core';
+import { ERC20Test, ERC20Test__factory } from '@hyperlane-xyz/core';
 
 import { TestChainName } from '../consts/testChains.js';
-import { HyperlaneContractsMap } from '../contracts/types.js';
 import { MultiProvider } from '../providers/MultiProvider.js';
 import { randomInt } from '../test/testUtils.js';
 import { normalizeConfig } from '../utils/ism.js';
 
 import { EvmTokenFeeDeployer } from './EvmTokenFeeDeployer.js';
 import { EvmTokenFeeReader } from './EvmTokenFeeReader.js';
-import { EvmTokenFeeFactories } from './contracts.js';
 import { TokenFeeConfig, TokenFeeConfigSchema, TokenFeeType } from './types.js';
 
 const MAX_FEE = 1157920892373161954235709850086879078532699846656405640394n;
 const HALF_AMOUNT = 5789604461865809771178549250434395392663499233282028201970n;
-const BPS = EvmTokenFeeReader.convertToBps(MAX_FEE, HALF_AMOUNT); // 0.1 or 1000bps
+const BPS = EvmTokenFeeReader.convertToBps(MAX_FEE, HALF_AMOUNT);
 
 describe('EvmTokenFeeReader', () => {
   let multiProvider: MultiProvider;
   let signer: SignerWithAddress;
-  let reader: EvmTokenFeeReader;
-  let tokenFee: LinearFee;
-  let deployer: EvmTokenFeeDeployer;
-  let deployedContracts: HyperlaneContractsMap<EvmTokenFeeFactories>;
   let token: ERC20Test;
 
-  let config: TokenFeeConfig;
   const TOKEN_TOTAL_SUPPLY = '100000000000000000000';
-  beforeEach(async () => {
+
+  before(async () => {
     [signer] = await hre.ethers.getSigners();
     multiProvider = MultiProvider.createTestMultiProvider({ signer });
-    deployer = new EvmTokenFeeDeployer(multiProvider, TestChainName.test2);
+
     const factory = new ERC20Test__factory(signer);
     token = await factory.deploy('fake', 'FAKE', TOKEN_TOTAL_SUPPLY, 18);
     await token.deployed();
-
-    config = TokenFeeConfigSchema.parse({
-      type: TokenFeeType.LinearFee,
-      maxFee: MAX_FEE,
-      halfAmount: HALF_AMOUNT,
-      bps: BPS,
-      token: token.address,
-      owner: signer.address,
-    });
-
-    deployedContracts = await deployer.deploy({
-      [TestChainName.test2]: config,
-    });
   });
 
-  describe('LinearFee', async () => {
+  describe('LinearFee', () => {
     it('should read the token fee config', async () => {
-      reader = new EvmTokenFeeReader(multiProvider, TestChainName.test2);
-      tokenFee = deployedContracts[TestChainName.test2][TokenFeeType.LinearFee];
+      const config = TokenFeeConfigSchema.parse({
+        type: TokenFeeType.LinearFee,
+        maxFee: MAX_FEE,
+        halfAmount: HALF_AMOUNT,
+        bps: BPS,
+        token: token.address,
+        owner: signer.address,
+      });
+      const deployer = new EvmTokenFeeDeployer(
+        multiProvider,
+        TestChainName.test2,
+      );
+      const deployedContracts = await deployer.deploy({
+        [TestChainName.test2]: config,
+      });
+      const reader = new EvmTokenFeeReader(multiProvider, TestChainName.test2);
+      const tokenFee =
+        deployedContracts[TestChainName.test2][TokenFeeType.LinearFee];
       const onchainConfig = await reader.deriveTokenFeeConfig(tokenFee.address);
       expect(normalizeConfig(onchainConfig)).to.deep.equal(
         normalizeConfig({
@@ -69,7 +67,7 @@ describe('EvmTokenFeeReader', () => {
 
     it('should convert maxFee and halfAmount to bps', async () => {
       const maxFee = BigInt(randomInt(2, 100_000_000));
-      const halfAmount = maxFee / 2n;
+      const halfAmount = maxFee * 5n;
 
       const config = {
         type: TokenFeeType.LinearFee,
@@ -80,10 +78,14 @@ describe('EvmTokenFeeReader', () => {
         bps: EvmTokenFeeReader.convertToBps(maxFee, halfAmount),
       };
       const parsedConfig = TokenFeeConfigSchema.parse(config);
-      deployedContracts = await deployer.deploy({
+      const deployer = new EvmTokenFeeDeployer(
+        multiProvider,
+        TestChainName.test2,
+      );
+      await deployer.deploy({
         [TestChainName.test3]: parsedConfig,
       });
-      tokenFee = deployedContracts[TestChainName.test3][TokenFeeType.LinearFee];
+
       const convertedBps = EvmTokenFeeReader.convertToBps(maxFee, halfAmount);
       expect(convertedBps).to.equal(BPS);
     });
@@ -110,14 +112,14 @@ describe('EvmTokenFeeReader', () => {
     });
   });
 
-  describe('RoutingFee', async () => {
+  describe('RoutingFee', () => {
     it('should be able to derive a routing fee config and its sub fees', async () => {
       const routingFeeConfig: TokenFeeConfig = {
         type: TokenFeeType.RoutingFee,
         owner: signer.address,
         token: token.address,
-        maxFee: MAX_FEE,
-        halfAmount: HALF_AMOUNT,
+        maxFee: constants.MaxUint256.toBigInt(),
+        halfAmount: constants.MaxUint256.toBigInt(),
         feeContracts: {
           [TestChainName.test2]: {
             owner: signer.address,
