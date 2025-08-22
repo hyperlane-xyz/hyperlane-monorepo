ca46502b57fbbf6dcbd2985cb7071151bf585363
xeno097 <xeno097.cp@gmail.com>
2025-08-22 08:06:25 -0700

    fix(sdk, cli): fix audit branch failing tests (#6922)

--- solidity/contracts/mock/MockValueTransferBridge.sol
+++ solidity/contracts/mock/MockValueTransferBridge.sol
@@ -4,6 +4,12 @@ pragma solidity ^0.8.13;
 import {ITokenBridge, Quote} from "../interfaces/ITokenBridge.sol";
 
 contract MockValueTransferBridge is ITokenBridge {
+    address public collateral;
+
+    constructor(address _collateral) {
+        collateral = _collateral;
+    }
+
     event SentTransferRemote(
         uint32 indexed origin,
         uint32 indexed destination,
@@ -17,7 +23,7 @@ contract MockValueTransferBridge is ITokenBridge {
         uint256 //_amountOut
     ) public view virtual override returns (Quote[] memory) {
         Quote[] memory quotes = new Quote[](1);
-        quotes[0] = Quote(address(0), 1);
+        quotes[0] = Quote(collateral, 1);
 
         return quotes;
     }
