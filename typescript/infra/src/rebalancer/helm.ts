57c3338b815e3e3f4963d8f46631785e38d0e691
xeno097 <xeno097.cp@gmail.com>
2025-09-15 12:51:05 -0400

    chore: update rebalancer docker image tag

--- typescript/infra/src/rebalancer/helm.ts
+++ typescript/infra/src/rebalancer/helm.ts
@@ -75,7 +75,7 @@ export class RebalancerHelmManager extends HelmManager {
     return {
       image: {
         repository: 'gcr.io/abacus-labs-dev/hyperlane-monorepo',
-        tag: '7baab46-20250815-123514',
+        tag: '24ad2f5-20250915-164612',
       },
       withMetrics: this.withMetrics,
       fullnameOverride: this.helmReleaseName,
--- typescript/infra/src/warp/helm.ts
+++ typescript/infra/src/warp/helm.ts
@@ -96,7 +96,7 @@ export class WarpRouteMonitorHelmManager extends HelmManager {
     return {
       image: {
         repository: 'gcr.io/abacus-labs-dev/hyperlane-monorepo',
-        tag: 'c27e3e2-20250903-205745',
+        tag: '24ad2f5-20250915-164612',
       },
       warpRouteId: this.warpRouteId,
       fullnameOverride: this.helmReleaseName,
