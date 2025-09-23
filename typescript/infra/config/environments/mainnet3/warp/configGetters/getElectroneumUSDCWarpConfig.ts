1d907c397828fe36376e99aeae4c577aa9706003
Le <6251863+ltyu@users.noreply.github.com>
2025-09-23 10:24:23 -0400

    Sebatiano Review

--- typescript/infra/config/environments/mainnet3/warp/configGetters/getElectroneumUSDCWarpConfig.ts
+++ typescript/infra/config/environments/mainnet3/warp/configGetters/getElectroneumUSDCWarpConfig.ts
@@ -4,9 +4,10 @@ import { Address } from '@hyperlane-xyz/utils';
 import { RouterConfigWithoutOwner } from '../../../../../src/config/warp.js';
 
 import {
-  getRebalanceableCollateralTokenConfigForChain,
+  getRebalancingUSDCConfigForChain,
   getSyntheticTokenConfigForChain,
-} from './getPulsechainUSDCWarpConfig.js';
+  getUSDCRebalancingBridgesConfigFor,
+} from './utils.js';
 
 type DeploymentChains<T> = {
   avalanche: T;
@@ -19,28 +20,35 @@ type DeploymentChains<T> = {
 const ownersByChain: DeploymentChains<Address> = {
   avalanche: '0xe0eb6194A56cdb6a51BB5855cddEbd61c03a199d',
   base: '0xe0eb6194A56cdb6a51BB5855cddEbd61c03a199d',
-  electroneum: '0xa7eccdb9be08178f896c26b7bbd8c3d4e844d9ba',
+  electroneum: '0xa7eccdb9be08178f896c26b7bbd8c3d4e844d9ba', // TODO: update this after ICA is deployed
   ethereum: '0xe0eb6194A56cdb6a51BB5855cddEbd61c03a199d',
 };
+
+const rebalancingConfigByChain = getUSDCRebalancingBridgesConfigFor(
+  Object.keys(ownersByChain),
+);
 
 export const getElectroneumUSDCWarpConfig = async (
   routerConfig: ChainMap<RouterConfigWithoutOwner>,
 ): Promise<ChainMap<HypTokenRouterConfig>> => {
   const deployConfig: DeploymentChains<HypTokenRouterConfig> = {
-    avalanche: getRebalanceableCollateralTokenConfigForChain(
+    avalanche: getRebalancingUSDCConfigForChain(
       'avalanche',
       routerConfig,
       ownersByChain,
+      rebalancingConfigByChain,
     ),
-    base: getRebalanceableCollateralTokenConfigForChain(
+    base: getRebalancingUSDCConfigForChain(
       'base',
       routerConfig,
       ownersByChain,
+      rebalancingConfigByChain,
     ),
-    ethereum: getRebalanceableCollateralTokenConfigForChain(
+    ethereum: getRebalancingUSDCConfigForChain(
       'ethereum',
       routerConfig,
       ownersByChain,
+      rebalancingConfigByChain,
     ),
 
     electroneum: getSyntheticTokenConfigForChain(
--- typescript/infra/config/environments/mainnet3/warp/configGetters/getPulsechainUSDCWarpConfig.ts
+++ typescript/infra/config/environments/mainnet3/warp/configGetters/getPulsechainUSDCWarpConfig.ts
@@ -1,15 +1,13 @@
-import {
-  ChainMap,
-  ChainName,
-  HypTokenRouterConfig,
-  TokenType,
-} from '@hyperlane-xyz/sdk';
-import { Address, assert } from '@hyperlane-xyz/utils';
+import { ChainMap, HypTokenRouterConfig } from '@hyperlane-xyz/sdk';
+import { Address } from '@hyperlane-xyz/utils';
 
 import { RouterConfigWithoutOwner } from '../../../../../src/config/warp.js';
-import { usdcTokenAddresses } from '../cctp.js';
 
-import { getUSDCRebalancingBridgesConfigFor } from './utils.js';
+import {
+  getRebalancingUSDCConfigForChain,
+  getSyntheticTokenConfigForChain,
+  getUSDCRebalancingBridgesConfigFor,
+} from './utils.js';
 
 type DeploymentChains<T> = {
   arbitrum: T;
@@ -28,81 +26,37 @@ const ownersByChain: DeploymentChains<Address> = {
   pulsechain: '0x703cf58975B14142eD0Ba272555789610c85520c',
 };
 
-export const getRebalanceableCollateralTokenConfigForChain = (
-  currentChain: keyof typeof usdcTokenAddresses,
-  routerConfigByChain: ChainMap<RouterConfigWithoutOwner>,
-  ownersByChain: ChainMap<Address>,
-): HypTokenRouterConfig => {
-  const owner = ownersByChain[currentChain];
-  assert(owner, `Owner not found for chain ${currentChain}`);
-
-  const usdcTokenAddress = usdcTokenAddresses[currentChain];
-  assert(
-    usdcTokenAddress,
-    `USDC token address not found for chain ${currentChain}`,
-  );
-
-  const currentRebalancingConfig = getUSDCRebalancingBridgesConfigFor(
-    Object.keys(ownersByChain),
-  )[currentChain];
-  assert(
-    currentRebalancingConfig,
-    `Rebalancing config not found for chain ${currentChain}`,
-  );
-
-  const { allowedRebalancers, allowedRebalancingBridges } =
-    currentRebalancingConfig;
-
-  return {
-    type: TokenType.collateral,
-    token: usdcTokenAddress,
-    mailbox: routerConfigByChain[currentChain].mailbox,
-    owner,
-    allowedRebalancers,
-    allowedRebalancingBridges,
-  };
-};
-
-export const getSyntheticTokenConfigForChain = <
-  TOwnerAddress extends ChainMap<Address>,
->(
-  currentChain: Extract<keyof TOwnerAddress, ChainName>,
-  routerConfigByChain: ChainMap<RouterConfigWithoutOwner>,
-  ownersByChain: TOwnerAddress,
-): HypTokenRouterConfig => {
-  const owner = ownersByChain[currentChain];
-  assert(owner, `Owner not found for chain ${currentChain}`);
-
-  return {
-    type: TokenType.synthetic,
-    mailbox: routerConfigByChain[currentChain].mailbox,
-    owner,
-  };
-};
+const rebalancingConfigByChain = getUSDCRebalancingBridgesConfigFor(
+  Object.keys(ownersByChain),
+);
 
 export const getPulsechainUSDCWarpConfig = async (
   routerConfig: ChainMap<RouterConfigWithoutOwner>,
 ): Promise<ChainMap<HypTokenRouterConfig>> => {
   const deployConfig: DeploymentChains<HypTokenRouterConfig> = {
-    arbitrum: getRebalanceableCollateralTokenConfigForChain(
+    arbitrum: getRebalancingUSDCConfigForChain(
       'arbitrum',
       routerConfig,
       ownersByChain,
+      rebalancingConfigByChain,
     ),
-    base: getRebalanceableCollateralTokenConfigForChain(
+    base: getRebalancingUSDCConfigForChain(
       'base',
       routerConfig,
       ownersByChain,
+      rebalancingConfigByChain,
     ),
-    ethereum: getRebalanceableCollateralTokenConfigForChain(
+    ethereum: getRebalancingUSDCConfigForChain(
       'ethereum',
       routerConfig,
       ownersByChain,
+      rebalancingConfigByChain,
     ),
-    polygon: getRebalanceableCollateralTokenConfigForChain(
+    polygon: getRebalancingUSDCConfigForChain(
       'polygon',
       routerConfig,
       ownersByChain,
+      rebalancingConfigByChain,
     ),
     pulsechain: getSyntheticTokenConfigForChain(
       'pulsechain',
