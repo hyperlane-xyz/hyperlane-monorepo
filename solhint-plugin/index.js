7c3850766af9bb8d5cb8f75fbdbe6747ffb6685e
Yorke Rhodes IV <yorke@hyperlane.xyz>
2025-09-23 18:14:01 -0400

    Try custom lint rule

---
+++ solhint-plugin/index.js
@@ -0,0 +1,23 @@
+class NoVirtualOverrideAllowed {
+  constructor(reporter, config) {
+    this.ruleId = 'no-virtual-override';
+
+    this.reporter = reporter;
+    this.config = config;
+  }
+
+  FunctionDefinition(ctx) {
+    const isVirtual = ctx.isVirtual;
+    const hasOverride = ctx.override !== null;
+
+    if (isVirtual && hasOverride) {
+      this.reporter.error(
+        ctx,
+        this.ruleId,
+        'Functions cannot be "virtual" and "override" at the same time',
+      );
+    }
+  }
+}
+
+module.exports = [NoVirtualOverrideAllowed];
