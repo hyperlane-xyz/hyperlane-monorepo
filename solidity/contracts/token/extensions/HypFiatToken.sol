2de604d1d9d803d826ba0a6e9794e9815523af7e
xeno097 <xeno097.cp@gmail.com>
2025-10-01 12:58:29 -0400

    chore: revert hyp fiat changes

--- solidity/contracts/token/extensions/HypFiatToken.sol
+++ solidity/contracts/token/extensions/HypFiatToken.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT OR Apache-2.0
 pragma solidity >=0.8.0;
 
-import {IERC20Mintable} from "../interfaces/IERC20Mintable.sol";
+import {IFiatToken} from "../interfaces/IFiatToken.sol";
 import {HypERC20Collateral} from "../HypERC20Collateral.sol";
 
 // see https://github.com/circlefin/stablecoin-evm/blob/master/doc/tokendesign.md#issuing-and-destroying-tokens
@@ -18,7 +18,7 @@ contract HypFiatToken is HypERC20Collateral {
         // transfer amount to address(this)
         metadata = super._transferFromSender(_amount);
         // burn amount of address(this) balance
-        IERC20Mintable(address(wrappedToken)).burn(_amount);
+        IFiatToken(address(wrappedToken)).burn(_amount);
     }
 
     function _transferTo(
@@ -26,6 +26,9 @@ contract HypFiatToken is HypERC20Collateral {
         uint256 _amount,
         bytes calldata /*metadata*/
     ) internal override {
-        IERC20Mintable(address(wrappedToken)).mint(_recipient, _amount);
+        require(
+            IFiatToken(address(wrappedToken)).mint(_recipient, _amount),
+            "FiatToken mint failed"
+        );
     }
 }
