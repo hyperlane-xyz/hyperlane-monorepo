4603635d085040796af739ad151ee3461e701ade
Mo Hussan <22501692+Mo-Hussain@users.noreply.github.com>
2025-10-10 10:22:16 -0400

    enable paradex as an altvm signer

--- typescript/cli/src/context/altvm.ts
+++ typescript/cli/src/context/altvm.ts
@@ -210,6 +210,10 @@ export class AltVMSignerFactory
         continue;
       }
 
+      if (metadata.protocol === ProtocolType.Starknet) {
+        continue;
+      }
+
       const protocol = ALT_VM_SUPPORTED_PROTOCOLS[metadata.protocol];
 
       if (!protocol) {
