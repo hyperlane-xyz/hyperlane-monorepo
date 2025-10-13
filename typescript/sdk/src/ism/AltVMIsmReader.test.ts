78f42bba98fdec6b6ea9636661569e69ab12bcac
Troy Kessler <troy.kessler99@gmail.com>
2025-10-13 16:23:13 +0200

    chore: routing ism test

--- typescript/sdk/src/ism/AltVMIsmReader.test.ts
+++ typescript/sdk/src/ism/AltVMIsmReader.test.ts
@@ -4,6 +4,7 @@ import Sinon from 'sinon';
 
 import { AltVM } from '@hyperlane-xyz/utils';
 
+import { TestChainName } from '../consts/testChains.js';
 import { MultiProtocolProvider } from '../providers/MultiProtocolProvider.js';
 import { MockProvider } from '../test/AltVMMockProvider.js';
 
@@ -17,7 +18,10 @@ const expect = chai.expect;
 
 TEST CASES - AltVMIsmReader tests
 
-* derive ISM config
+* derive message id multisig ISM
+* derive merkle root multisig ISM
+* derive noop ISM
+* derive routing ISM
 
 */
 
@@ -148,6 +152,100 @@ describe('AltVMIsmReader', () => {
     expect(
       getIsmStub.calledOnceWith({
         ismAddress,
+      }),
+    ).to.be.true;
+  });
+
+  it('derive routing ISM', async () => {
+    // ARRANGE
+    const routingIsmAddress =
+      '0x726f757465725f69736d00000000000000000000000000050000000000000000';
+    const messageIdAddress =
+      '0x726f757465725f69736d00000000000000000000000000060000000000000000';
+    const merkleRootAddress =
+      '0x726f757465725f69736d00000000000000000000000000070000000000000000';
+
+    const isms: any = {
+      [routingIsmAddress]: {
+        type: AltVM.IsmType.ROUTING,
+        owner: 'hyp1jq304cthpx0lwhpqzrdjrcza559ukyy3sc4dw5',
+        routes: [
+          {
+            domainId: multiProvider.getDomainId(TestChainName.test2),
+            ismAddress: messageIdAddress,
+          },
+          {
+            domainId: multiProvider.getDomainId(TestChainName.test3),
+            ismAddress: merkleRootAddress,
+          },
+        ],
+      },
+      [messageIdAddress]: {
+        type: AltVM.IsmType.MESSAGE_ID_MULTISIG,
+        validators: ['0xe699ceCa237DD38e8f2Fa308D7d1a2BeCFC5493E'],
+        threshold: 1,
+      },
+      [merkleRootAddress]: {
+        type: AltVM.IsmType.MERKLE_ROOT_MULTISIG,
+        validators: ['0xe699ceCa237DD38e8f2Fa308D7d1a2BeCFC5493E'],
+        threshold: 1,
+      },
+    };
+
+    const ismTypeStub = Sinon.stub(provider, 'getIsmType').callsFake(
+      (req) => (isms[req.ismAddress] as any).type,
+    );
+
+    const getRoutingIsm = Sinon.stub(provider, 'getRoutingIsm').resolves(
+      isms[routingIsmAddress],
+    );
+
+    const getMessageIdIsm = Sinon.stub(
+      provider,
+      'getMessageIdMultisigIsm',
+    ).resolves(isms[messageIdAddress]);
+    const getMerkleRootIsm = Sinon.stub(
+      provider,
+      'getMerkleRootMultisigIsm',
+    ).resolves(isms[merkleRootAddress]);
+
+    // ACT
+    const ismConfig = await ismReader.deriveIsmConfig(routingIsmAddress);
+
+    // ASSERT
+    expect(ismConfig).to.deep.equal({
+      type: IsmType.ROUTING,
+      address: routingIsmAddress,
+      owner: isms[routingIsmAddress].owner,
+      domains: {
+        [TestChainName.test2]: isms[messageIdAddress],
+        [TestChainName.test3]: isms[merkleRootAddress],
+      },
+    });
+
+    for (const ismAddress of Object.keys(isms)) {
+      expect(
+        ismTypeStub.calledWith({
+          ismAddress,
+        }),
+      ).to.be.true;
+    }
+
+    expect(
+      getRoutingIsm.calledOnceWith({
+        ismAddress: routingIsmAddress,
+      }),
+    ).to.be.true;
+
+    expect(
+      getMessageIdIsm.calledOnceWith({
+        ismAddress: messageIdAddress,
+      }),
+    ).to.be.true;
+
+    expect(
+      getMerkleRootIsm.calledOnceWith({
+        ismAddress: merkleRootAddress,
       }),
     ).to.be.true;
   });
