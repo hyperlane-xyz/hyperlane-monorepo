1121a0c2f3cf3a07d94b67622951420bedcc1653
xeno097 <xeno097.cp@gmail.com>
2025-10-15 13:41:33 -0400

    chore: update rebalancer docker tag

--- typescript/infra/src/rebalancer/helm.ts
+++ typescript/infra/src/rebalancer/helm.ts
@@ -75,7 +75,7 @@ export class RebalancerHelmManager extends HelmManager {
     return {
       image: {
         repository: 'gcr.io/abacus-labs-dev/hyperlane-monorepo',
-        tag: '6c97a30-20251013-113726',
+        tag: '4ea3698-20251015-173216',
       },
       withMetrics: this.withMetrics,
       fullnameOverride: this.helmReleaseName,
