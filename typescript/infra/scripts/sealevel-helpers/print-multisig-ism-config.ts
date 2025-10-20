aee2f3f79b33b090b198680f3af0087bfab49e32
pbio <10051819+paulbalaji@users.noreply.github.com>
2025-10-20 21:31:18 +0100

    --write directly with sealevel ism/igp helpers

--- typescript/infra/scripts/sealevel-helpers/print-multisig-ism-config.ts
+++ typescript/infra/scripts/sealevel-helpers/print-multisig-ism-config.ts
@@ -1,23 +1,43 @@
-import { IsmType } from '@hyperlane-xyz/sdk';
+import path from 'path';
 
+import { ChainName, IsmType } from '@hyperlane-xyz/sdk';
+
+import { Contexts } from '../../config/contexts.js';
 import { multisigIsms } from '../../config/multisigIsm.js';
 import { getChains } from '../../config/registry.js';
-import { getArgs, withContext } from '../agent-utils.js';
+import { DeployEnvironment } from '../../src/config/environment.js';
+import {
+  getMonorepoRoot,
+  writeAndFormatJsonAtPath,
+} from '../../src/utils/utils.js';
+import { getArgs, withContext, withWrite } from '../agent-utils.js';
 
 // This script exists to print the default multisig ISM validator sets for a given environment
 // so they can easily be copied into the Sealevel tooling. :'(
 
+const multisigIsmConfigPath = (
+  environment: DeployEnvironment,
+  context: Contexts,
+  local: ChainName,
+) =>
+  path.resolve(
+    getMonorepoRoot(),
+    `rust/sealevel/environments/${environment}/multisig-ism-message-id/${local}/${context}/multisig-config.json`,
+  );
+
 async function main() {
-  const args = await withContext(getArgs())
+  const { environment, local, context, write } = await withWrite(
+    withContext(getArgs()),
+  )
     .describe('local', 'local chain')
     .choices('local', getChains())
     .demandOption('local').argv;
 
   const config = multisigIsms(
-    args.environment,
-    args.local,
+    environment,
+    local,
     IsmType.MESSAGE_ID_MULTISIG,
-    args.context,
+    context,
   );
 
   // Cap any thresholds to 4 due to the Sealevel transaction size limit.
@@ -44,7 +64,13 @@ async function main() {
     }
   }
 
-  console.log(JSON.stringify(config, null, 2));
+  if (write) {
+    const filepath = multisigIsmConfigPath(environment, context, local);
+    console.log(`Writing config to ${filepath}`);
+    writeAndFormatJsonAtPath(filepath, config);
+  } else {
+    console.log(JSON.stringify(config, null, 2));
+  }
 }
 
 main().catch((err) => {
