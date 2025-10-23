d9db1677f713d98bb2dc87df2dd892cce7124815
Andrey Taranov <86911+antigremlin@users.noreply.github.com>
2025-10-23 18:14:53 +0100

    move altVM mocks

--- typescript/sdk/src/token/AltVMWarpModule.test.ts
+++ typescript/sdk/src/token/AltVMWarpModule.test.ts
@@ -2,7 +2,7 @@ import chai from 'chai';
 import chaiAsPromised from 'chai-as-promised';
 import Sinon from 'sinon';
 
-import { AltVM } from '@hyperlane-xyz/provider-sdk';
+import { AltVM, MockSigner } from '@hyperlane-xyz/provider-sdk';
 
 import { TestChainName } from '../consts/testChains.js';
 import { IsmType } from '../ism/types.js';
