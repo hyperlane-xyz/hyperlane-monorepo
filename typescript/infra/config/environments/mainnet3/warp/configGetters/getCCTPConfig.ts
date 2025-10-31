a50243d80777b6a4bf33c077b30845403ca3fbd4
Yorke Rhodes IV <yorke@hyperlane.xyz>
2025-10-31 17:16:09 -0400

    Add CCTP v2 consts

--- typescript/infra/config/environments/mainnet3/warp/configGetters/getCCTPConfig.ts
+++ typescript/infra/config/environments/mainnet3/warp/configGetters/getCCTPConfig.ts
@@ -1,5 +1,6 @@
 import {
   ChainMap,
+  ChainName,
   ChainSubmissionStrategy,
   HypTokenRouterConfig,
   OwnableConfig,
@@ -13,25 +14,17 @@ import { RouterConfigWithoutOwner } from '../../../../../src/config/warp.js';
 import { awIcasLegacy } from '../../governance/ica/_awLegacy.js';
 import { awSafes } from '../../governance/safe/aw.js';
 import {
-  messageTransmitterAddresses,
-  tokenMessengerAddresses,
+  messageTransmitterV1Addresses,
+  messageTransmitterV2Addresses,
+  tokenMessengerV1Addresses,
+  tokenMessengerV2Addresses,
   usdcTokenAddresses,
 } from '../cctp.js';
 
 const SERVICE_URL = 'https://offchain-lookup.services.hyperlane.xyz';
 
-export const CCTP_CHAINS = [
-  'ethereum',
-  'avalanche',
-  'optimism',
-  'arbitrum',
-  'base',
-  'polygon',
-  'unichain',
-] as const;
-
 // TODO: remove this once the route has been updated to be owned by non-legacy ownership
-const owners: Record<(typeof CCTP_CHAINS)[number], string> = {
+const owners: Record<ChainName, string> = {
   arbitrum: '0xaB547e6cde21a5cC3247b8F80e6CeC3a030FAD4A',
   avalanche: awIcasLegacy['avalanche'],
   base: '0xA6D9Aa3878423C266480B5a7cEe74917220a1ad2',
@@ -45,9 +38,20 @@ export const getCCTPWarpConfig = async (
   routerConfig: ChainMap<RouterConfigWithoutOwner>,
   _abacusWorksEnvOwnerConfig: ChainMap<OwnableConfig>,
   _warpRouteId: string,
+  version: 'V1' | 'V2',
 ): Promise<ChainMap<HypTokenRouterConfig>> => {
+  const messengerAddresses =
+    version === 'V1' ? tokenMessengerV1Addresses : tokenMessengerV2Addresses;
+  const transmitterAddresses =
+    version === 'V1'
+      ? messageTransmitterV1Addresses
+      : messageTransmitterV2Addresses;
+  const chains = Object.keys(messengerAddresses) as Array<
+    keyof typeof messengerAddresses
+  >;
+
   return Object.fromEntries(
-    CCTP_CHAINS.map((chain) => {
+    chains.map((chain) => {
       // TODO: restore after route has been updated
       // const owner = awIcasLegacy[chain] ?? awSafes[chain];
 
@@ -59,9 +63,9 @@ export const getCCTPWarpConfig = async (
         mailbox: routerConfig[chain].mailbox,
         type: TokenType.collateralCctp,
         token: usdcTokenAddresses[chain],
-        messageTransmitter: messageTransmitterAddresses[chain],
-        tokenMessenger: tokenMessengerAddresses[chain],
-        cctpVersion: 'V1',
+        messageTransmitter: transmitterAddresses[chain],
+        tokenMessenger: messengerAddresses[chain],
+        cctpVersion: version,
         urls: [`${SERVICE_URL}/cctp/getCctpAttestation`],
       };
       return [chain, config];
@@ -79,8 +83,14 @@ const safeSubmitter: SubmitterMetadata = {
 
 const icaChains = Object.keys(awIcasLegacy);
 
-export const getCCTPStrategyConfig = (): ChainSubmissionStrategy => {
-  const submitterMetadata = CCTP_CHAINS.map((chain): SubmitterMetadata => {
+export const getCCTPStrategyConfig = (
+  version: 'V1' | 'V2',
+): ChainSubmissionStrategy => {
+  const chains =
+    version === 'V1'
+      ? Object.keys(tokenMessengerV1Addresses)
+      : Object.keys(tokenMessengerV2Addresses);
+  const submitterMetadata = chains.map((chain): SubmitterMetadata => {
     if (!icaChains.includes(chain)) {
       return {
         type: TxSubmitterType.GNOSIS_SAFE,
@@ -99,7 +109,7 @@ export const getCCTPStrategyConfig = (): ChainSubmissionStrategy => {
   });
 
   return Object.fromEntries(
-    CCTP_CHAINS.map((chain, index) => [
+    chains.map((chain, index) => [
       chain,
       { submitter: submitterMetadata[index] },
     ]),
