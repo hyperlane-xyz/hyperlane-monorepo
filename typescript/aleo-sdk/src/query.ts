25b0c4938c5631e8e3fd7922d2c1df89c6322d2f
christopherbrumm <christopherbrumm2000@gmail.com>
2025-10-31 11:21:16 +0100

    chore: ISM & Hook queries

--- typescript/aleo-sdk/src/query.ts
+++ typescript/aleo-sdk/src/query.ts
@@ -1,15 +1,21 @@
 import { AleoProvider } from './clients/provider.js';
 
-export { AleoProvider } from './clients/provider.js';
 export { AleoSigner } from './clients/signer.js';
 export { AleoTransaction, AleoReceipt } from './utils/types.js';
 
 const main = async () => {
   const localnetRpc = 'http://localhost:3030';
   const provider = await AleoProvider.connect([localnetRpc], '');
+  // const client = await new AleoNetworkClient(localnetRpc)
 
-  const mailbox = await provider.getMailbox({ mailboxAddress: 'mailbox.aleo' });
-  console.log(mailbox);
+  console.log(await provider.getMailbox({ mailboxAddress: 'mailbox.aleo' }));
+
+  console.log(
+    await provider.getMessageIdMultisigIsm({
+      ismAddress:
+        'aleo1xg67v5l2ysnku6fkyhwym7upxmeeypdrtseyjtan3whrhqepyc8qtmm00u',
+    }),
+  );
 };
 
 main();
