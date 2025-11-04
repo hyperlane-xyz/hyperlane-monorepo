27def6c58dec977f43a0bb34c2ac2e60e8ae41ca
xeno097 <xeno097.cp@gmail.com>
2025-11-04 19:57:40 -0400

    feat(protocol-sdk): defiend the submitter types

---
+++ typescript/provider-sdk/src/submitter.ts
@@ -0,0 +1,25 @@
+import { AnnotatedTx, TxReceipt } from './module.js';
+
+// By default each VM implementation should at least define a JSON rpc submitter
+export const TransactionSubmitterType = {
+  JSON_RPC: 'jsonRpc',
+} as const;
+
+export type TransactionSubmitterType =
+  (typeof TransactionSubmitterType)[keyof typeof TransactionSubmitterType];
+
+export interface JsonRpcSubmitterConfig {
+  type: typeof TransactionSubmitterType.JSON_RPC;
+  privateKey: string;
+  accountAddress?: string;
+}
+
+export type TransactionSubmitterConfig<T extends { type: string }> =
+  | JsonRpcSubmitterConfig
+  | T;
+
+export interface ITransactionSubmitter<TSubmitterType extends string = string> {
+  type: TSubmitterType;
+
+  submit(...transactions: AnnotatedTx[]): Promise<TxReceipt[]>;
+}
