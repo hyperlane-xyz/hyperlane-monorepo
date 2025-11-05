9b2de878adfd32c8c9b803ec9fcc631c8e90e446
xeno097 <xeno097.cp@gmail.com>
2025-11-05 15:54:25 -0400

    feat: applied the new import restriction rules in the cosmos-sdk, radix-sdk, provider-sdk and deploy-sdk packages

--- typescript/provider-sdk/eslint.config.mjs
+++ typescript/provider-sdk/eslint.config.mjs
@@ -1,3 +1,12 @@
-import { jsRules, typescriptRules } from '@hyperlane-xyz/eslint-config';
+import {
+  jsRules,
+  restrictedSdkAndUtilsImportRules,
+  typescriptRules,
+} from '@hyperlane-xyz/eslint-config';
 
-export default [{ ignores: ['dist/**'] }, ...jsRules, ...typescriptRules];
+export default [
+  { ignores: ['dist/**'] },
+  ...jsRules,
+  ...typescriptRules,
+  ...restrictedSdkAndUtilsImportRules,
+];
