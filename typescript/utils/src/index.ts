3f75ad86d1dd5606f0b5ffb5143fd7b8dea3cf78
Paul Balaji <10051819+paulbalaji@users.noreply.github.com>
2025-11-05 20:37:32 +0000

    chore(funding): update thresholds (#7330)

--- typescript/utils/src/index.ts
+++ typescript/utils/src/index.ts
@@ -97,7 +97,7 @@ export {
   isS3CheckpointWithId,
 } from './checkpoints.js';
 export { domainHash } from './domains.js';
-export { safelyAccessEnvVar, inCIMode } from './env.js';
+export { safelyAccessEnvVar, inCIMode, inKubernetes } from './env.js';
 export { canonizeId, evmId } from './ids.js';
 export {
   LogFormat,
