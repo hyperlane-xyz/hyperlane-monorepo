0da7e4e215cf83afa8343c085488bb2f72fd27f0
kamiyaa <jeff.no.zhao@gmail.com>
2025-11-07 16:34:26 -0500

    feat: remove unnecessary code

--- rust/main/lander/src/adapter/chains/ethereum/nonce/manager/tests.rs
+++ rust/main/lander/src/adapter/chains/ethereum/nonce/manager/tests.rs
@@ -121,31 +121,6 @@ async fn test_assign_nonce_error_when_from_address_mismatch() {
         .contains("Transaction from address does not match nonce manager address"));
 }
 
-fn build_mock_typed_tx_and_function() -> (TypedTransaction, Function) {
-    let typed_tx = TypedTransaction::Eip1559(Eip1559TransactionRequest {
-        from: Some(Address::random()),
-        to: Some(NameOrAddress::Address(Address::random())),
-        gas: Some(EthersU256::from(1200)),
-        value: None,
-        data: None,
-        nonce: Some(EthersU256::from(1000)),
-        access_list: AccessList::default(),
-        max_fee_per_gas: Some(EthersU256::from(1000)),
-        max_priority_fee_per_gas: Some(EthersU256::from(1000)),
-        chain_id: Some(U64::from(1)),
-    });
-
-    #[allow(deprecated)]
-    let function = Function {
-        name: "test_function".into(),
-        inputs: Vec::new(),
-        outputs: Vec::new(),
-        constant: None,
-        state_mutability: StateMutability::Pure,
-    };
-    (typed_tx, function)
-}
-
 #[tokio::test]
 async fn test_calculate_nonce_tx_and_db_equal() {
     let (_, tx_db, nonce_db) = tmp_dbs();
@@ -156,18 +131,9 @@ async fn test_calculate_nonce_tx_and_db_equal() {
         .expect_get_next_nonce_on_finalized_block()
         .returning(|_, _| Ok(U256::from(100)));
 
-    let data = build_mock_typed_tx_and_function();
-    let json_data = serde_json::to_vec(&data).expect("Failed to serialize data");
-
-    let mut payload1 = FullPayload::random();
-    payload1.data = json_data.clone();
-    let mut payload2 = FullPayload::random();
-    payload2.data = json_data.clone();
-
-    let payloads = vec![payload1, payload2];
     let mut tx = dummy_evm_tx(
         ExpectedTxType::Eip1559,
-        payloads,
+        vec![],
         crate::TransactionStatus::Included,
         H160::random(),
     );
@@ -227,18 +193,9 @@ async fn test_calculate_nonce_tx_and_db_mismatch() {
         .expect_get_next_nonce_on_finalized_block()
         .returning(|_, _| Ok(U256::from(90)));
 
-    let data = build_mock_typed_tx_and_function();
-    let json_data = serde_json::to_vec(&data).expect("Failed to serialize data");
-
-    let mut payload1 = FullPayload::random();
-    payload1.data = json_data.clone();
-    let mut payload2 = FullPayload::random();
-    payload2.data = json_data.clone();
-
-    let payloads = vec![payload1, payload2];
     let mut tx = dummy_evm_tx(
         ExpectedTxType::Eip1559,
-        payloads,
+        vec![],
         crate::TransactionStatus::Included,
         H160::random(),
     );
@@ -285,5 +242,5 @@ async fn test_calculate_nonce_tx_and_db_mismatch() {
         .expect("Failed to calculate nonce");
 
     assert_eq!(nonce_resp, U256::from(90));
-    assert_eq!(nonce_manager.state.metrics.get_mismatched_nonce().get(), 0);
+    assert_eq!(nonce_manager.state.metrics.get_mismatched_nonce().get(), 1);
 }
