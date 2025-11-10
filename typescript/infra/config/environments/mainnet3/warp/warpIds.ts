07f957fc1d494f1d935161104bf92a4ae3c47a07
Paul Balaji <10051819+paulbalaji@users.noreply.github.com>
2025-11-10 16:17:54 +0000

    chore: update registryrc (#7353)

--- typescript/infra/config/environments/mainnet3/warp/warpIds.ts
+++ typescript/infra/config/environments/mainnet3/warp/warpIds.ts
@@ -46,7 +46,7 @@ export enum WarpRouteIds {
   EthereumZircuitRe7LRT = 'Re7LRT/ethereum-zircuit',
   InevmInjectiveINJ = 'INJ/inevm-injective',
   ArbitrumAvalancheBaseBscEthereumLumiaprismOptimismPolygonLUMIA = 'LUMIA/arbitrum-avalanche-base-bsc-ethereum-lumiaprism-optimism-polygon',
-  MantapacificNeutronTIA = 'TIA/mantapacific-neutron',
+  MantapacificNeutronTIA = 'TIA/mantapacific',
   BaseZeroNetworkCBBTC = 'CBBTC/base-zeronetwork',
   BaseEthereumREZ = 'REZ/base-ethereum-unichain',
   BaseEthereumREZSTAGING = 'REZSTAGING/base-ethereum-unichain',
