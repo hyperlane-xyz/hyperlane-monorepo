b1d78880173f0fb7794a6d1649237d28c09a39ea
Sergi Rene <sergi@dymension.xyz>
2025-11-12 10:48:13 +0100

    fix: enable cors kaspa RELAYER monitoring endpoints (#303)
    * enable cors
    
    * clean

--- rust/main/agents/relayer/src/server/kaspa/mod.rs
+++ rust/main/agents/relayer/src/server/kaspa/mod.rs
@@ -3,6 +3,7 @@ use std::sync::Arc;
 use axum::{routing::get, Router};
 use derive_new::new;
 use hyperlane_core::KaspaDb;
+use tower_http::cors::{Any, CorsLayer};
 
 pub mod list_deposits;
 pub mod list_withdrawals;
@@ -14,9 +15,15 @@ pub struct ServerState {
 
 impl ServerState {
     pub fn router(self) -> Router {
+        let cors = CorsLayer::new()
+            .allow_origin(Any)
+            .allow_methods(Any)
+            .allow_headers(Any);
+
         Router::new()
             .route("/kaspa/deposit", get(list_deposits::handler))
             .route("/kaspa/withdrawal", get(list_withdrawals::handler))
+            .layer(cors)
             .with_state(self)
     }
 }
