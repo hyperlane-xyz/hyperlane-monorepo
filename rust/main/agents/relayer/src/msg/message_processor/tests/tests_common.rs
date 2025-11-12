541feda64f45dfbe38c9cb6d79849e19a9fe3d45
Danil Nemirovsky <4614623+ameten@users.noreply.github.com>
2025-11-12 12:03:33 +0000

    Move HyperlaneDb mock to common place so that we can re-use it over code base

--- rust/main/agents/relayer/src/msg/message_processor/tests/tests_common.rs
+++ rust/main/agents/relayer/src/msg/message_processor/tests/tests_common.rs
@@ -9,14 +9,11 @@ use prometheus::{IntGauge, IntGaugeVec};
 use serde::Serialize;
 use tokio::sync::broadcast;
 
-use hyperlane_base::db::{
-    DbResult, HyperlaneDb, InterchainGasExpenditureData, InterchainGasPaymentData,
-};
+// Re-export for use in other test modules
+pub use hyperlane_base::tests::mock_hyperlane_db::MockHyperlaneDb;
 use hyperlane_core::{
-    identifiers::UniqueIdentifier, ChainResult, GasPaymentKey, HyperlaneDomain, HyperlaneMessage,
-    InterchainGasPayment, InterchainGasPaymentMeta, MerkleTreeInsertion, PendingOperation,
-    PendingOperationResult, PendingOperationStatus, ReprepareReason, TryBatchAs, TxOutcome, H256,
-    U256,
+    ChainResult, HyperlaneDomain, HyperlaneMessage, PendingOperation, PendingOperationResult,
+    PendingOperationStatus, ReprepareReason, TryBatchAs, TxOutcome, H256, U256,
 };
 use lander::{Entrypoint, FullPayload, LanderError, PayloadStatus, PayloadUuid};
 
@@ -135,124 +132,6 @@ impl PendingOperation for MockQueueOperation {
 }
 
 impl TryBatchAs<HyperlaneMessage> for MockQueueOperation {}
-
-// Mock HyperlaneDb
-mock! {
-    pub HyperlaneDb {}
-
-    impl HyperlaneDb for HyperlaneDb {
-        fn retrieve_payload_uuids_by_message_id(
-            &self,
-            message_id: &H256,
-        ) -> DbResult<Option<Vec<PayloadUuid>>>;
-        fn retrieve_highest_seen_message_nonce(&self) -> DbResult<Option<u32>>;
-        fn retrieve_message_by_nonce(&self, nonce: u32) -> DbResult<Option<HyperlaneMessage>>;
-        fn retrieve_processed_by_nonce(&self, nonce: &u32) -> DbResult<Option<bool>>;
-        fn domain(&self) -> &HyperlaneDomain;
-        fn store_message_id_by_nonce(&self, nonce: &u32, id: &H256) -> DbResult<()>;
-        fn retrieve_message_id_by_nonce(&self, nonce: &u32) -> DbResult<Option<H256>>;
-        fn store_message_by_id(&self, id: &H256, message: &HyperlaneMessage) -> DbResult<()>;
-        fn retrieve_message_by_id(&self, id: &H256) -> DbResult<Option<HyperlaneMessage>>;
-        fn store_dispatched_block_number_by_nonce(
-            &self,
-            nonce: &u32,
-            block_number: &u64,
-        ) -> DbResult<()>;
-        fn retrieve_dispatched_block_number_by_nonce(&self, nonce: &u32) -> DbResult<Option<u64>>;
-        fn store_processed_by_nonce(&self, nonce: &u32, processed: &bool) -> DbResult<()>;
-        fn store_processed_by_gas_payment_meta(
-            &self,
-            meta: &InterchainGasPaymentMeta,
-            processed: &bool,
-        ) -> DbResult<()>;
-        fn retrieve_processed_by_gas_payment_meta(
-            &self,
-            meta: &InterchainGasPaymentMeta,
-        ) -> DbResult<Option<bool>>;
-        fn store_interchain_gas_expenditure_data_by_message_id(
-            &self,
-            message_id: &H256,
-            data: &InterchainGasExpenditureData,
-        ) -> DbResult<()>;
-        fn retrieve_interchain_gas_expenditure_data_by_message_id(
-            &self,
-            message_id: &H256,
-        ) -> DbResult<Option<InterchainGasExpenditureData>>;
-        fn store_status_by_message_id(
-            &self,
-            message_id: &H256,
-            status: &PendingOperationStatus,
-        ) -> DbResult<()>;
-        fn retrieve_status_by_message_id(
-            &self,
-            message_id: &H256,
-        ) -> DbResult<Option<PendingOperationStatus>>;
-        fn store_interchain_gas_payment_data_by_gas_payment_key(
-            &self,
-            key: &GasPaymentKey,
-            data: &InterchainGasPaymentData,
-        ) -> DbResult<()>;
-        fn retrieve_interchain_gas_payment_data_by_gas_payment_key(
-            &self,
-            key: &GasPaymentKey,
-        ) -> DbResult<Option<InterchainGasPaymentData>>;
-        fn store_gas_payment_by_sequence(
-            &self,
-            sequence: &u32,
-            payment: &InterchainGasPayment,
-        ) -> DbResult<()>;
-        fn retrieve_gas_payment_by_sequence(
-            &self,
-            sequence: &u32,
-        ) -> DbResult<Option<InterchainGasPayment>>;
-        fn store_gas_payment_block_by_sequence(
-            &self,
-            sequence: &u32,
-            block_number: &u64,
-        ) -> DbResult<()>;
-        fn retrieve_gas_payment_block_by_sequence(&self, sequence: &u32) -> DbResult<Option<u64>>;
-        fn store_pending_message_retry_count_by_message_id(
-            &self,
-            message_id: &H256,
-            count: &u32,
-        ) -> DbResult<()>;
-        fn retrieve_pending_message_retry_count_by_message_id(
-            &self,
-            message_id: &H256,
-        ) -> DbResult<Option<u32>>;
-        fn store_merkle_tree_insertion_by_leaf_index(
-            &self,
-            leaf_index: &u32,
-            insertion: &MerkleTreeInsertion,
-        ) -> DbResult<()>;
-        fn retrieve_merkle_tree_insertion_by_leaf_index(
-            &self,
-            leaf_index: &u32,
-        ) -> DbResult<Option<MerkleTreeInsertion>>;
-        fn store_merkle_leaf_index_by_message_id(
-            &self,
-            message_id: &H256,
-            leaf_index: &u32,
-        ) -> DbResult<()>;
-        fn retrieve_merkle_leaf_index_by_message_id(&self, message_id: &H256) -> DbResult<Option<u32>>;
-        fn store_merkle_tree_insertion_block_number_by_leaf_index(
-            &self,
-            leaf_index: &u32,
-            block_number: &u64,
-        ) -> DbResult<()>;
-        fn retrieve_merkle_tree_insertion_block_number_by_leaf_index(
-            &self,
-            leaf_index: &u32,
-        ) -> DbResult<Option<u64>>;
-        fn store_highest_seen_message_nonce_number(&self, nonce: &u32) -> DbResult<()>;
-        fn retrieve_highest_seen_message_nonce_number(&self) -> DbResult<Option<u32>>;
-        fn store_payload_uuids_by_message_id(
-            &self,
-            message_id: &H256,
-            payloads_uuid: Vec<UniqueIdentifier>,
-        ) -> DbResult<()>;
-    }
-}
 
 // Mock DispatcherEntrypoint
 mock! {
---
+++ rust/main/hyperlane-base/src/tests.rs
@@ -0,0 +1,6 @@
+/// Mock Checkpoint Syncer
+pub mod mock_checkpoint_syncer;
+/// Mock HyperlaneDb for testing
+pub mod mock_hyperlane_db;
+/// Dummy Validators
+pub mod test_validators;
