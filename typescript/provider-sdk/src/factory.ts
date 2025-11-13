a2f2011833792c1784d67247a3cec44aa54cdb2f
Andrey Taranov <86911+antigremlin@users.noreply.github.com>
2025-11-13 14:18:40 +0000

    remove some module provider in favor of deploy-sdk

--- typescript/provider-sdk/src/factory.ts
+++ typescript/provider-sdk/src/factory.ts
@@ -1,15 +1,11 @@
 import { IProvider, ISigner } from './altvm.js';
 import { ChainMetadataForAltVM } from './chain.js';
-import { CoreModuleType } from './core.js';
-import { HookModuleType } from './hook.js';
-import { IsmModuleType } from './ism.js';
-import { AnnotatedTx, ModuleProvider, TxReceipt } from './module.js';
+import { AnnotatedTx, TxReceipt } from './module.js';
 import {
   ITransactionSubmitter,
   JsonRpcSubmitterConfig,
   TransactionSubmitterConfig,
 } from './submitter.js';
-import { TokenRouterModuleType } from './warp.js';
 
 export type SignerConfig = Pick<
   JsonRpcSubmitterConfig,
@@ -38,11 +34,4 @@ export interface ProtocolProvider {
       config: TConfig,
     ) => Promise<ITransactionSubmitter>,
   ): void;
-
-  // It's unclear if these belong here or in a separate interface
-  // Currently they can only be implemented in deploy-sdk for AltVM classes
-  coreProvider(): ModuleProvider<CoreModuleType>;
-  hookProvider(): ModuleProvider<HookModuleType>;
-  ismProvider(): ModuleProvider<IsmModuleType>;
-  tokenRouterProvider(): ModuleProvider<TokenRouterModuleType>;
 }
