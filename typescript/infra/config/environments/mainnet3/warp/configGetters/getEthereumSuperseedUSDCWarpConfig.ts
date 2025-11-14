81660a84919e9a79aa7fbd64d239cc3e24910bc8
Yorke Rhodes IV <yorke@hyperlane.xyz>
2025-11-14 15:39:53 -0500

    Update superseed rebalancing to cctp v2

--- typescript/infra/config/environments/mainnet3/warp/configGetters/getEthereumSuperseedUSDCWarpConfig.ts
+++ typescript/infra/config/environments/mainnet3/warp/configGetters/getEthereumSuperseedUSDCWarpConfig.ts
@@ -30,7 +30,7 @@ export const getEthereumSuperseedUSDCWarpConfig = async (
 ): Promise<ChainMap<HypTokenRouterConfig>> => {
   const rebalancingConfig = getUSDCRebalancingBridgesConfigFor(
     Object.keys(owners),
-    [WarpRouteIds.MainnetCCTPV1],
+    [WarpRouteIds.MainnetCCTPV2Standard],
   );
 
   const ethereum: HypTokenRouterConfig = {
