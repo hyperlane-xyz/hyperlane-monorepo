11b7bbdae880de605e61e14d61ae4396e0a996e4
Le <6251863+ltyu@users.noreply.github.com>
2025-11-17 12:32:54 -0500

    Rename deploy-sdk/src/protocols.ts to deploy-sdk/src/protocol.ts

--- typescript/deploy-sdk/src/protocol.ts
+++ typescript/deploy-sdk/src/protocol.ts
@@ -1,8 +1,4 @@
-import { ProtocolType } from '@hyperlane-xyz/provider-sdk';
-import {
-  ProtocolRegistrar,
-  registerProtocol,
-} from '@hyperlane-xyz/provider-sdk/registry';
+import { ProtocolType, registerProtocol } from '@hyperlane-xyz/provider-sdk';
 
 export async function loadProviders(neededProtocols: ProtocolType[]) {
   await Promise.all(
@@ -12,12 +8,7 @@ export async function loadProviders(neededProtocols: ProtocolType[]) {
           const { CosmosNativeProviderFactory } = await import(
             '@hyperlane-xyz/cosmos-sdk'
           );
-          registerProtocol((registrar: ProtocolRegistrar) => {
-            registrar.registerProtocol(
-              protocol,
-              () => new CosmosNativeProviderFactory(),
-            );
-          });
+          registerProtocol(protocol, () => new CosmosNativeProviderFactory());
           break;
         }
       }
