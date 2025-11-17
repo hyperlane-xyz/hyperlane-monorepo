e9c5570091e614589d3d04bbcd7ffdb09e54ec28
Danil Nemirovsky <4614623+ameten@users.noreply.github.com>
2025-11-17 18:01:21 +0000

    Clear up

--- rust/main/agents/relayer/src/msg/message_processor.rs
+++ rust/main/agents/relayer/src/msg/message_processor.rs
@@ -581,8 +581,6 @@ async fn submit_lander_task(
 
         if operations_to_process.is_empty() {
             // There are no operations to submit, so give some time before checking again
-            // the operations which were already send, but waiting to be included,
-            // to prevent burning CPU
             sleep(Duration::from_millis(100)).await;
             continue;
         }
