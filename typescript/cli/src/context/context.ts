df582e27a09b5f40869dee1a06fa078429ee6efd
Le <6251863+ltyu@users.noreply.github.com>
2025-11-19 17:56:05 -0500

    Move requiresKey back up

--- typescript/cli/src/context/context.ts
+++ typescript/cli/src/context/context.ts
@@ -55,6 +55,8 @@ export async function signerMiddleware(argv: Record<string, any>) {
   const { key, requiresKey, strategyPath, multiProtocolProvider } =
     argv.context;
 
+  if (!requiresKey) return argv;
+
   const strategyConfig = strategyPath
     ? await readChainSubmissionStrategyConfig(strategyPath)
     : {};
@@ -100,8 +102,6 @@ export async function signerMiddleware(argv: Record<string, any>) {
         altVmProvider.set(ProtocolType.Radix, RadixProvider);
     }),
   );
-
-  if (!requiresKey) return argv;
 
   /**
    * Extracts signer config
