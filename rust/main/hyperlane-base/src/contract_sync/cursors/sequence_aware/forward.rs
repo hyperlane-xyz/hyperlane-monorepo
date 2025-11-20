60062e5baaec4be316f076a1812d524497aedee1
kamiyaa <jeff.no.zhao@gmail.com>
2025-11-20 14:58:14 -0500

    feat: add more debug messages

--- rust/main/hyperlane-base/src/contract_sync/cursors/sequence_aware/forward.rs
+++ rust/main/hyperlane-base/src/contract_sync/cursors/sequence_aware/forward.rs
@@ -114,12 +114,16 @@ impl<T: Debug + Clone + Sync + Send + Indexable + 'static> ForwardSequenceAwareS
         // Skip any already indexed logs.
         self.skip_indexed().await?;
 
-        let (Some(onchain_sequence_count), tip) = self
+        let (onchain_sequence_count, tip) = match self
             .latest_sequence_querier
             .latest_sequence_count_and_tip()
             .await?
-        else {
-            return Ok(None);
+        {
+            (Some(onchain_sequence_count), tip) => (onchain_sequence_count, tip),
+            val => {
+                tracing::warn!(?val, "latest_sequence_count_and_tip");
+                return Ok(None);
+            }
         };
 
         // for updating metrics even if there's no indexable events available
@@ -127,7 +131,9 @@ impl<T: Debug + Clone + Sync + Send + Indexable + 'static> ForwardSequenceAwareS
         self.update_metrics(max_sequence);
 
         let current_sequence = self.current_indexing_snapshot.sequence;
-        let range = match current_sequence.cmp(&onchain_sequence_count) {
+        let res = current_sequence.cmp(&onchain_sequence_count);
+        tracing::debug!("current_sequence.cmp: {:?}", res);
+        let range = match res {
             Ordering::Equal => {
                 // We are synced up to the latest sequence so we don't need to index anything.
 
@@ -154,7 +160,11 @@ impl<T: Debug + Clone + Sync + Send + Indexable + 'static> ForwardSequenceAwareS
                 });
 
                 match &self.index_mode {
-                    IndexMode::Block => self.get_next_block_range(tip),
+                    IndexMode::Block => {
+                        let res = self.get_next_block_range(tip);
+                        tracing::debug!(?res, "get_next_block_range");
+                        res
+                    }
                     IndexMode::Sequence => {
                         Some(self.get_next_sequence_range(current_sequence, target_sequence))
                     }
