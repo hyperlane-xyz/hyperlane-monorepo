928c70d17a367882e4de3ee7d29ec9da11b67c59
xeno097 <xeno097.cp@gmail.com>
2025-11-20 16:05:02 -0400

    feat(radix-sdk): implemented the RadixTestIsmReader and RadixTestIsmModule

---
+++ typescript/radix-sdk/src/ism/test-ism.ts
@@ -0,0 +1,58 @@
+import { IProvider, IsmType } from '@hyperlane-xyz/provider-sdk/altvm';
+import {
+  IsmModuleAddresses,
+  TestIsmConfig,
+} from '@hyperlane-xyz/provider-sdk/ism';
+import {
+  AnnotatedTx,
+  HypModule,
+  HypModuleArgs,
+  HypReader,
+} from '@hyperlane-xyz/provider-sdk/module';
+import { WithAddress, assert } from '@hyperlane-xyz/utils';
+
+type TestIsmModule = {
+  config: TestIsmConfig;
+  addresses: IsmModuleAddresses;
+  derived: WithAddress<TestIsmConfig>;
+};
+
+export class RadixTestIsmReader implements HypReader<TestIsmModule> {
+  constructor(private readonly provider: IProvider) {}
+
+  async read(address: string): Promise<WithAddress<TestIsmConfig>> {
+    const ismType = await this.provider.getIsmType({
+      ismAddress: address,
+    });
+
+    assert(
+      ismType === IsmType.TEST_ISM,
+      `Expected Ism at address ${address} to be of type ${IsmType.TEST_ISM}`,
+    );
+
+    return {
+      address,
+      type: IsmType.TEST_ISM,
+    };
+  }
+}
+
+export class RadixTestIsmModule implements HypModule<TestIsmModule> {
+  constructor(
+    private readonly args: HypModuleArgs<TestIsmModule>,
+    private readonly reader: HypReader<TestIsmModule>,
+  ) {}
+
+  read(): Promise<WithAddress<TestIsmConfig>> {
+    return this.reader.read(this.args.addresses.deployedIsm);
+  }
+
+  serialize(): IsmModuleAddresses {
+    return this.args.addresses;
+  }
+
+  async update(_config: TestIsmConfig): Promise<AnnotatedTx[]> {
+    // The TestIsm does not have any updatable properties
+    return [];
+  }
+}
