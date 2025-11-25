14c8975d7e1de9774e898653b93041d4b69ca472
Danil Nemirovsky <ameten@users.noreply.github.com>
2025-11-25 17:51:20 +0000

    chore: Upgrade relayers to latest (#7462)
    Co-authored-by: Danil Nemirovsky <4614623+ameten@users.noreply.github.com>

--- typescript/infra/config/environments/mainnet3/agent.ts
+++ typescript/infra/config/environments/mainnet3/agent.ts
@@ -867,7 +867,7 @@ const hyperlane: RootAgentConfig = {
     rpcConsensusType: RpcConsensusType.Fallback,
     docker: {
       repo,
-      tag: 'd64bb3a-20251124-132420',
+      tag: '9770b73-20251125-164332',
     },
     blacklist,
     gasPaymentEnforcement: gasPaymentEnforcement,
@@ -913,7 +913,7 @@ const releaseCandidate: RootAgentConfig = {
     rpcConsensusType: RpcConsensusType.Fallback,
     docker: {
       repo,
-      tag: 'd64bb3a-20251124-132420',
+      tag: '9770b73-20251125-164332',
     },
     blacklist,
     // We're temporarily (ab)using the RC relayer as a way to increase
