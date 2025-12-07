8665be861b53a35cf06a60d735294932dedc3424
kamiyaa <jeff.no.zhao@gmail.com>
2025-12-07 14:28:04 -0500

    feat: dedup transaction hashes when we add them to tx_hashes

--- rust/main/lander/src/adapter/chains/sealevel/transaction/update.rs
+++ rust/main/lander/src/adapter/chains/sealevel/transaction/update.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 use hyperlane_core::H512;
 
 use crate::{
@@ -12,6 +14,10 @@ pub trait Update {
 impl Update for Transaction {
     fn update_after_submission(&mut self, hash: H512, precursor: SealevelTxPrecursor) -> &mut Self {
         self.tx_hashes.push(hash);
+
+        // deduplicate any duplicate transaction hashes
+        let unique_hashes: HashSet<_> = self.tx_hashes.iter().cloned().collect();
+        self.tx_hashes = unique_hashes.into_iter().collect();
 
         // Data is updated since transaction is re-estimated before submission
         self.vm_specific_data = VmSpecificTxData::Svm(Box::new(precursor));
