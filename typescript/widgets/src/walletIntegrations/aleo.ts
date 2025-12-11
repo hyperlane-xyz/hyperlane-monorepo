579e681c51f4f972387f778837c76df15a00d472
Troy Kessler <troy.kessler99@gmail.com>
2025-12-11 15:33:59 +0100

    chore: switch back to puzzle wallet

--- typescript/widgets/src/walletIntegrations/aleo.ts
+++ typescript/widgets/src/walletIntegrations/aleo.ts
@@ -1,6 +1,11 @@
-import { Network } from '@provablehq/aleo-types';
-import { GalileoWalletAdapter } from '@provablehq/aleo-wallet-adaptor-prove-alpha';
-import { WalletDecryptPermission } from '@provablehq/aleo-wallet-standard';
+import {
+  EventType,
+  Network,
+  requestCreateEvent,
+  useAccount,
+  useConnect,
+  useDisconnect,
+} from '@puzzlehq/sdk';
 import { useCallback, useMemo } from 'react';
 
 import { AleoTransaction } from '@hyperlane-xyz/aleo-sdk';
@@ -23,27 +28,26 @@ import {
   WatchAssetFns,
 } from './types.js';
 
-const adapter = new GalileoWalletAdapter();
-
 export function useAleoAccount(
   _multiProvider: MultiProtocolProvider,
 ): AccountInfo {
+  const { account } = useAccount();
   return {
     protocol: ProtocolType.Aleo,
     addresses: [
       {
-        address: adapter.account?.address ?? '',
+        address: account?.address ?? '',
         chainName: 'Aleo',
       },
     ],
     publicKey: undefined, // we don't need the public key for aleo
-    isReady: !!adapter.account,
+    isReady: !!account,
   };
 }
 
 export function useAleoWalletDetails() {
-  const name = adapter.name;
-  const logoUrl = adapter.icon;
+  const name = 'Puzzle Wallet';
+  const logoUrl = 'https://docs.puzzle.online/_astro/icon.CnSbKTxP.png';
 
   return useMemo<WalletDetails>(
     () => ({
@@ -55,21 +59,34 @@ export function useAleoWalletDetails() {
 }
 
 export function useAleoConnectFn(): () => void {
-  return () => {
-    adapter.connect(Network.TESTNET3, WalletDecryptPermission.AutoDecrypt, [
-      'credits.aleo',
-      'test_ism_manager.aleo',
-      'test_mailbox.aleo',
-      'test_hook_manager.aleo',
-      'test_dispatch_proxy.aleo',
-      'token_registry.aleo',
-      'test_hyp_native.aleo',
-    ]);
-  };
+  const { connect } = useConnect({
+    dAppInfo: {
+      name: '<YOUR DAPP NAME>',
+      description: '<YOUR DAPP DESCRIPTION>',
+      iconUrl: '<YOUR DAPP ICON URL>',
+    },
+    permissions: {
+      programIds: {
+        [Network.AleoMainnet]: [
+          'dapp_1.aleo',
+          'dapp_2.aleo',
+          'dapp_2_imports.aleo',
+        ],
+        [Network.AleoTestnet]: [
+          'dapp_3.aleo',
+          'dapp_3_imports_1.aleo',
+          'dapp_3_imports_2.aleo',
+        ],
+      },
+    },
+  });
+
+  return connect;
 }
 
 export function useAleoDisconnectFn(): () => Promise<void> {
-  return adapter.disconnect;
+  const { disconnect } = useDisconnect();
+  return disconnect;
 }
 
 export function useAleoActiveChain(
@@ -132,22 +149,22 @@ export function useAleoTransactionFns(
         transaction,
       });
 
-      const transactionResult = await adapter.executeTransaction({
-        program: transaction.programName,
-        function: transaction.functionName,
+      const createEventResponse = await requestCreateEvent({
+        type: EventType.Execute,
+        programId: transaction.programName,
+        functionId: transaction.functionName,
         fee: Number(fee),
         inputs: transaction.inputs,
-        privateFee: transaction.privateFee,
       });
 
-      if (!transactionResult) {
+      if (!createEventResponse) {
         throw new Error(`Failed to execute Aleo transaction`);
       }
 
       const confirm = async (): Promise<TypedTransactionReceipt> => {
         assert(
-          transactionResult.transactionId,
-          `Aleo tx failed: ${transactionResult}`,
+          createEventResponse.eventId,
+          `Aleo tx failed: ${createEventResponse}`,
         );
 
         // TODO: populate receipt
@@ -159,11 +176,11 @@ export function useAleoTransactionFns(
             index: 0n,
             transaction: {} as any,
             finalize: [],
-            transactionHash: transactionResult.transactionId || '',
+            transactionHash: createEventResponse.eventId || '',
           },
         };
       };
-      return { hash: transactionResult.transactionId || '', confirm };
+      return { hash: createEventResponse.eventId || '', confirm };
     },
     [switchNetwork],
   );
