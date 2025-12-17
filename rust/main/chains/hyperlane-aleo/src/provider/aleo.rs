cd94774a61ab855285a91169d733a90e5920c800
Jamin <57451149+yjamin@users.noreply.github.com>
2025-12-17 10:54:05 +0100

    feat: aleo detect signer transfers (#7557)

--- rust/main/chains/hyperlane-aleo/src/provider/aleo.rs
+++ rust/main/chains/hyperlane-aleo/src/provider/aleo.rs
@@ -246,6 +246,32 @@ impl<C: AleoClient> AleoProvider<C> {
         Ok(finalize_cost.saturating_add(execution_cost))
     }
 
+    /// Internal helper: checks for malicious authorizations
+    /// Because in Aleo the application is the entry point, we have to trust the application to not contain malicious code
+    /// Malicious code can drain the relayer's account out of credits and tokens
+    fn malicious_authorization_check<N: Network>(
+        &self,
+        entry_program_id: &str,
+        authorization: &Authorization<N>,
+    ) -> ChainResult<()> {
+        for (_, transition) in authorization.transitions() {
+            let program_id = transition.program_id().to_string();
+
+            // Native credits transfers from the signer are malicious
+            // Token transfers from the signer are malicious
+            if (program_id == "credits.aleo" || program_id == "token_registry.aleo")
+                && transition.function_name().to_string() == "transfer_public_as_signer"
+            {
+                return Err(HyperlaneAleoError::MaliciousProgramDetected {
+                    program_id: entry_program_id.to_string(),
+                    transition: transition.to_string(),
+                }
+                .into());
+            }
+        }
+        Ok(())
+    }
+
     /// Internal helper: builds VM, loads program + imports, parses identifiers, and creates authorization.
     async fn prepare_authorization<N: Network, I, V>(
         &self,
@@ -295,6 +321,10 @@ impl<C: AleoClient> AleoProvider<C> {
                 &mut rng,
             )
             .map_err(HyperlaneAleoError::from)?;
+
+        // Malicious authorization check
+        self.malicious_authorization_check(program_id, &authorization)?;
+
         debug!(
             "Prepared Authorization in {:.2}s",
             Instant::now().duration_since(start).as_secs_f32()
