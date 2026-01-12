c1ccf3836cf1d1215f1d3fe2d953ac56e312642c
pbio <10051819+paulbalaji@users.noreply.github.com>
2026-01-12 10:43:56 +0000

    configure

--- typescript/infra/config/environments/mainnet3/governance/safe/warpFees.ts
+++ typescript/infra/config/environments/mainnet3/governance/safe/warpFees.ts
@@ -1,6 +1,8 @@
 import { ChainMap } from '@hyperlane-xyz/sdk';
 import { Address } from '@hyperlane-xyz/utils';
 
+// Only need the Ethereum Safe here.
+// All other chains will use ICAs for warp fees.
 export const warpFeesSafes: ChainMap<Address> = {
-  // TODO: populate after the warp-fees Safe is created.
+  ethereum: '0x8Ff4c563f26db00e65bD93d9f662A51c304C09b0',
 };
