8006faf2c432f2b82aba183d94645f46663fd81a
xeno097 <xeno097.cp@gmail.com>
2026-01-13 10:39:17 -0500

    feat(aleo-sdk): implement new artifact api for aleo ism readers (#7660)
    Co-authored-by: Andrey Taranov <86911+antigremlin@users.noreply.github.com>

--- typescript/aleo-sdk/src/tests/index.e2e-test.ts
+++ typescript/aleo-sdk/src/tests/index.e2e-test.ts
@@ -3,3 +3,4 @@ import './1_interchain_security.e2e-test.js';
 import './2_core.e2e-test.js';
 import './3_post_dispatch.e2e-test';
 import './4_warp.e2e-test';
+import './5_ism_artifact_readers.e2e-test.js';
