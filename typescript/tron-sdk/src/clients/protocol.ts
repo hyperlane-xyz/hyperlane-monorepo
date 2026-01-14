c652eff6b62c66bbde26645bb26616b17912a9f9
Troy Kessler <troy.kessler99@gmail.com>
2026-01-14 10:49:35 +0100

    feat: add tron protocol

---
+++ typescript/tron-sdk/src/clients/protocol.ts
@@ -0,0 +1,57 @@
+import {
+  type AltVM,
+  type ChainMetadataForAltVM,
+  type ITransactionSubmitter,
+  type MinimumRequiredGasByAction,
+  type ProtocolProvider,
+  type SignerConfig,
+  type TransactionSubmitterConfig,
+} from '@hyperlane-xyz/provider-sdk';
+import { type IProvider } from '@hyperlane-xyz/provider-sdk/altvm';
+import {
+  type AnnotatedTx,
+  type TxReceipt,
+} from '@hyperlane-xyz/provider-sdk/module';
+import { assert } from '@hyperlane-xyz/utils';
+
+import { TronProvider } from './provider.js';
+import { TronSigner } from './signer.js';
+
+export class TronProtocolProvider implements ProtocolProvider {
+  createProvider(chainMetadata: ChainMetadataForAltVM): Promise<IProvider> {
+    assert(chainMetadata.rpcUrls, 'rpc urls undefined');
+    const rpcUrls = chainMetadata.rpcUrls.map((rpc) => rpc.http);
+    return TronProvider.connect(rpcUrls, chainMetadata.domainId);
+  }
+
+  async createSigner(
+    chainMetadata: ChainMetadataForAltVM,
+    config: SignerConfig,
+  ): Promise<AltVM.ISigner<AnnotatedTx, TxReceipt>> {
+    assert(chainMetadata.rpcUrls, 'rpc urls undefined');
+    const rpcUrls = chainMetadata.rpcUrls.map((rpc) => rpc.http);
+
+    const { privateKey } = config;
+
+    return TronSigner.connectWithSigner(rpcUrls, privateKey, {
+      metadata: chainMetadata,
+    });
+  }
+
+  createSubmitter<TConfig extends TransactionSubmitterConfig>(
+    _chainMetadata: ChainMetadataForAltVM,
+    _config: TConfig,
+  ): Promise<ITransactionSubmitter> {
+    // @TODO Implement in a follow up PR
+    throw Error('Not implemented');
+  }
+
+  getMinGas(): MinimumRequiredGasByAction {
+    return {
+      CORE_DEPLOY_GAS: BigInt(1e6),
+      WARP_DEPLOY_GAS: BigInt(3e6),
+      TEST_SEND_GAS: BigInt(3e5),
+      AVS_GAS: BigInt(3e6),
+    };
+  }
+}
