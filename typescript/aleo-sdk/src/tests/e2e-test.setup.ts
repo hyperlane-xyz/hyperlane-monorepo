cb52bcf9f9de3a697a75402074f5ebb198d84323
xeno097 <xeno097.cp@gmail.com>
2026-01-20 10:24:09 -0400

    fix: make linter happy

--- typescript/aleo-sdk/src/tests/e2e-test.setup.ts
+++ typescript/aleo-sdk/src/tests/e2e-test.setup.ts
@@ -1,5 +1,7 @@
 import type { StartedTestContainer } from 'testcontainers';
 
+import { rootLogger } from '@hyperlane-xyz/utils';
+
 import { DEFAULT_E2E_TEST_TIMEOUT, runAleoNode } from '../testing/index.js';
 
 let aleoNodeContainer: StartedTestContainer | undefined;
@@ -7,17 +9,17 @@ let aleoNodeContainer: StartedTestContainer | undefined;
 before(async function () {
   this.timeout(DEFAULT_E2E_TEST_TIMEOUT);
 
-  console.log('Starting Aleo devnode...');
+  rootLogger.info('Starting Aleo devnode...');
   aleoNodeContainer = await runAleoNode();
-  console.log('Aleo devnode started successfully');
+  rootLogger.info('Aleo devnode started successfully');
 });
 
 after(async function () {
   this.timeout(DEFAULT_E2E_TEST_TIMEOUT);
 
   if (aleoNodeContainer) {
-    console.log('Stopping Aleo devnode...');
+    rootLogger.info('Stopping Aleo devnode...');
     await aleoNodeContainer.stop();
-    console.log('Aleo devnode stopped');
+    rootLogger.info('Aleo devnode stopped');
   }
 });
