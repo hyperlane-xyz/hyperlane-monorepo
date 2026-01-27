255f8c53bac0a8660e6f0eb07fd584238213ea79
Troy Kessler <troy.kessler99@gmail.com>
2026-01-27 11:24:48 +0100

    fix: altvm igp hook

--- typescript/deploy-sdk/src/AltVMHookModule.ts
+++ typescript/deploy-sdk/src/AltVMHookModule.ts
@@ -146,6 +146,7 @@ export class AltVMHookModule implements HypModule<HookModuleType> {
 
     for (const [remote, c] of Object.entries(targetConfig.oracleConfig)) {
       if (
+        currentConfig.oracleConfig[remote] &&
         currentConfig.oracleConfig[remote].gasPrice === c.gasPrice &&
         currentConfig.oracleConfig[remote].tokenExchangeRate ===
           c.tokenExchangeRate
