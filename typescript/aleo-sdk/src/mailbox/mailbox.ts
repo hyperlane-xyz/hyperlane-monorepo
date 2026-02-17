69cdc9f166ecd4aed5f0e6d21c4de0036438bf45
xeno097 <xeno097.cp@gmail.com>
2026-02-17 16:29:36 -0400

    chore(aleo-sdk): cc code review

--- typescript/aleo-sdk/src/mailbox/mailbox.ts
+++ typescript/aleo-sdk/src/mailbox/mailbox.ts
@@ -16,6 +16,7 @@ import { type AleoSigner } from '../clients/signer.js';
 import {
   ALEO_NULL_ADDRESS,
   generateSuffix,
+  SUFFIX_LENGTH_LONG,
   toAleoAddress,
 } from '../utils/helper.js';
 import {
@@ -106,7 +107,7 @@ export class AleoMailboxWriter
     // Deploy mailbox programs (mailbox, dispatch_proxy, ism_manager, hook_manager)
     const programs = await this.signer.deployProgram(
       'dispatch_proxy',
-      generateSuffix(6), // SUFFIX_LENGTH_LONG
+      generateSuffix(SUFFIX_LENGTH_LONG),
     );
 
     const mailboxProgramId = programs['mailbox'];
