9e7699e4021cb8b0bc63520766e09a53d6566b6a
pbio <10051819+paulbalaji@users.noreply.github.com>
2026-02-26 12:10:36 +0000

    fix(solidity): ignore generated tron artifacts in eslint

--- solidity/eslint.config.mjs
+++ solidity/eslint.config.mjs
@@ -7,6 +7,8 @@ export default [
       '**/lib/**/*',
       '**/test/**/*',
       '**/dist/**/*',
+      '**/artifacts/**/*',
+      '**/artifacts-tron/**/*',
       '**/lib/**/*',
       '**/dependencies/**/*',
       'core-utils/generated/**/*',
