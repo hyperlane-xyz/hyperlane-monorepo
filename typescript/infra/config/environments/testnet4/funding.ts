2b8e5a1c48efe11a3ba6c3b9dc1fad2c8ca1e227
Jamin <kaminund@gmail.com>
2026-02-26 19:14:53 +0100

    chore: update balance

--- typescript/infra/config/environments/testnet4/funding.ts
+++ typescript/infra/config/environments/testnet4/funding.ts
@@ -54,8 +54,7 @@ export const keyFunderConfig: KeyFunderConfig<
     solanatestnet: '0',
     // no funding for SVM chains
     sonicsvmtestnet: '0',
-    // TODO:
-    tronshasta: '0',
+    tronshasta: '50',
   },
   desiredRebalancerBalancePerChain: {},
   igpClaimThresholdPerChain: {
