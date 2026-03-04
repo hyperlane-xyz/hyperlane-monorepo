bda324c81e610d6e99ce35a3be7260bfc13a0bb5
Mo Hussan <22501692+Mo-Hussain@users.noreply.github.com>
2026-03-04 12:57:44 +0000

    feat(sdk): add SignerSendTransactionOptions to sendAndConfirmTransaction

--- typescript/sdk/src/signers/types.ts
+++ typescript/sdk/src/signers/types.ts
@@ -2,9 +2,15 @@ import { Address, ProtocolType } from '@hyperlane-xyz/utils';
 
 import { ProtocolTypedTransaction } from '../providers/ProviderType.js';
 
+export interface SignerSendTransactionOptions {
+  /** Number of confirmations to wait for. EVM: passed to MultiProvider. Other protocols: ignored. */
+  waitConfirmations?: number;
+}
+
 export interface IMultiProtocolSigner<TProtocol extends ProtocolType> {
   address(): Promise<Address>;
   sendAndConfirmTransaction(
     tx: ProtocolTypedTransaction<TProtocol>,
+    options?: SignerSendTransactionOptions,
   ): Promise<string>;
 }
