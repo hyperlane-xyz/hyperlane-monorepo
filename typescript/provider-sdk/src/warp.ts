9d4b9fa44c8d585eb8d83f25530d13c1619f728e
xeno097 <xeno097.cp@gmail.com>
2026-03-05 19:02:28 -0400

    fix(cli,sdk): update sdk warp token types to support solana synthetic token deployments

--- typescript/provider-sdk/src/warp.ts
+++ typescript/provider-sdk/src/warp.ts
@@ -52,6 +52,7 @@ export interface BaseWarpConfig {
   hook?: HookConfig | string;
   remoteRouters?: RemoteRouters;
   destinationGas?: DestinationGas;
+  scale?: number;
 }
 
 export interface CollateralWarpConfig extends BaseWarpConfig {
@@ -64,6 +65,7 @@ export interface SyntheticWarpConfig extends BaseWarpConfig {
   name?: string;
   symbol?: string;
   decimals?: number;
+  metadataUri?: string;
 }
 
 export interface NativeWarpConfig extends BaseWarpConfig {
@@ -82,6 +84,7 @@ export interface BaseDerivedWarpConfig {
   hook: DerivedHookConfig | string;
   remoteRouters: RemoteRouters;
   destinationGas: DestinationGas;
+  scale?: number;
 }
 
 export interface DerivedCollateralWarpConfig extends BaseDerivedWarpConfig {
@@ -364,6 +367,7 @@ export function warpConfigToArtifact(
           name: config.name,
           symbol: config.symbol,
           decimals: config.decimals,
+          metadataUri: config.metadataUri,
         },
       };
 
