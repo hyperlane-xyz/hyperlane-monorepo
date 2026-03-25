ca3889dad6f60fd72145510157d98cd5db84b465
Yorke Rhodes IV <yorke@hyperlane.xyz>
2026-03-25 11:58:02 -0400

    feat(infra): add FeeQuotingHelmManager and deploy script, fix Docker build
    Added deployment infrastructure for fee-quoting service following the
    rebalancer/warp-monitor pattern. Fixed CI Docker build failure caused by
    test files included in tsc compilation without @types/chai.

---
+++ typescript/infra/src/fee-quoting/helm.ts
@@ -0,0 +1,47 @@
+import path from 'path';
+
+import { DEFAULT_GITHUB_REGISTRY } from '@hyperlane-xyz/registry';
+
+import { DockerImageRepos, mainnetDockerTags } from '../../config/docker.js';
+import { DeployEnvironment } from '../config/environment.js';
+import { HelmManager } from '../utils/helm.js';
+import { getInfraPath } from '../utils/utils.js';
+
+export class FeeQuotingHelmManager extends HelmManager {
+  readonly helmChartPath: string = path.join(
+    getInfraPath(),
+    './helm/fee-quoting',
+  );
+
+  constructor(
+    readonly environment: DeployEnvironment,
+    readonly registryCommit: string,
+  ) {
+    super();
+  }
+
+  get namespace() {
+    return this.environment;
+  }
+
+  get helmReleaseName() {
+    return 'hyperlane-fee-quoting';
+  }
+
+  async helmValues() {
+    const registryUri = this.registryCommit
+      ? `${DEFAULT_GITHUB_REGISTRY}/tree/${this.registryCommit}`
+      : DEFAULT_GITHUB_REGISTRY;
+
+    return {
+      image: {
+        repository: DockerImageRepos.FEE_QUOTING,
+        tag: mainnetDockerTags.feeQuoting,
+      },
+      hyperlane: {
+        runEnv: this.environment,
+        registryUri,
+      },
+    };
+  }
+}
