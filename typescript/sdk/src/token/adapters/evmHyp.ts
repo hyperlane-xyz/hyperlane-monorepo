2ef628e4bd65951fa51275704ea09cebcba58808
pbio <10051819+paulbalaji@users.noreply.github.com>
2026-03-26 14:05:09 +0000

    refactor: clean provider and hyp adapter internals

--- typescript/sdk/src/token/adapters/evmHyp.ts
+++ typescript/sdk/src/token/adapters/evmHyp.ts
@@ -1,20 +1,6 @@
-import {
-  EvmHypCollateralFiatAdapter,
-  EvmHypRebaseCollateralAdapter,
-  EvmHypSyntheticAdapter,
-  EvmHypSyntheticRebaseAdapter,
-  EvmHypXERC20Adapter,
-  EvmHypXERC20LockboxAdapter,
-  EvmMovableCollateralAdapter,
-  EvmHypNativeAdapter,
-} from './EvmTokenAdapter.js';
-import { EvmHypCrossCollateralAdapter } from './EvmCrossCollateralAdapter.js';
 import type { IHypTokenAdapter } from './ITokenAdapter.js';
-import {
-  hasChainMetadata,
-  hasOnlyHyperlaneConnections,
-  type HypTokenAdapterInput,
-} from './hypTokenAdapterUtils.js';
+import type { HypTokenAdapterInput } from './hypTokenAdapterUtils.js';
+import { createEvmLikeHypAdapter } from './evmLikeHyp.js';
 import type { ConfiguredMultiProtocolProvider as MultiProtocolProvider } from '../../providers/ConfiguredMultiProtocolProvider.js';
 import { TokenStandard } from '../TokenStandard.js';
 
@@ -22,62 +8,22 @@ export function createEvmHypAdapter(
   multiProvider: MultiProtocolProvider<{ mailbox?: string }>,
   token: HypTokenAdapterInput,
 ): IHypTokenAdapter<unknown> | undefined {
-  const { standard, chainName, addressOrDenom } = token;
-
-  if (!standard || !hasChainMetadata(multiProvider, chainName)) {
-    return undefined;
-  }
-
-  if (
-    standard === TokenStandard.EvmNative &&
-    hasOnlyHyperlaneConnections(token)
-  ) {
-    return new EvmHypNativeAdapter(chainName, multiProvider, {
-      token: addressOrDenom,
-    });
-  }
-
-  switch (standard) {
-    case TokenStandard.EvmHypNative:
-      return new EvmHypNativeAdapter(chainName, multiProvider, {
-        token: addressOrDenom,
-      });
-    case TokenStandard.EvmHypCollateral:
-    case TokenStandard.EvmHypOwnerCollateral:
-      return new EvmMovableCollateralAdapter(chainName, multiProvider, {
-        token: addressOrDenom,
-      });
-    case TokenStandard.EvmHypCrossCollateralRouter:
-      return new EvmHypCrossCollateralAdapter(chainName, multiProvider, {
-        token: addressOrDenom,
-      });
-    case TokenStandard.EvmHypRebaseCollateral:
-      return new EvmHypRebaseCollateralAdapter(chainName, multiProvider, {
-        token: addressOrDenom,
-      });
-    case TokenStandard.EvmHypCollateralFiat:
-      return new EvmHypCollateralFiatAdapter(chainName, multiProvider, {
-        token: addressOrDenom,
-      });
-    case TokenStandard.EvmHypSynthetic:
-      return new EvmHypSyntheticAdapter(chainName, multiProvider, {
-        token: addressOrDenom,
-      });
-    case TokenStandard.EvmHypSyntheticRebase:
-      return new EvmHypSyntheticRebaseAdapter(chainName, multiProvider, {
-        token: addressOrDenom,
-      });
-    case TokenStandard.EvmHypXERC20:
-    case TokenStandard.EvmHypVSXERC20:
-      return new EvmHypXERC20Adapter(chainName, multiProvider, {
-        token: addressOrDenom,
-      });
-    case TokenStandard.EvmHypXERC20Lockbox:
-    case TokenStandard.EvmHypVSXERC20Lockbox:
-      return new EvmHypXERC20LockboxAdapter(chainName, multiProvider, {
-        token: addressOrDenom,
-      });
-    default:
-      return undefined;
-  }
+  return createEvmLikeHypAdapter(multiProvider, token, {
+    native: TokenStandard.EvmNative,
+    hypNative: TokenStandard.EvmHypNative,
+    hypCollateral: [
+      TokenStandard.EvmHypCollateral,
+      TokenStandard.EvmHypOwnerCollateral,
+    ],
+    hypCrossCollateralRouter: TokenStandard.EvmHypCrossCollateralRouter,
+    hypRebaseCollateral: TokenStandard.EvmHypRebaseCollateral,
+    hypCollateralFiat: TokenStandard.EvmHypCollateralFiat,
+    hypSynthetic: TokenStandard.EvmHypSynthetic,
+    hypSyntheticRebase: TokenStandard.EvmHypSyntheticRebase,
+    hypXerc20: [TokenStandard.EvmHypXERC20, TokenStandard.EvmHypVSXERC20],
+    hypXerc20Lockbox: [
+      TokenStandard.EvmHypXERC20Lockbox,
+      TokenStandard.EvmHypVSXERC20Lockbox,
+    ],
+  });
 }
