d75814dc7c0723c6cb9330c8dde191f18757fe92
Troy Kessler <43882936+troykessler@users.noreply.github.com>
2026-03-26 18:23:27 +0100

    feat: fast path mainnet relayer (#8463)

--- typescript/infra/config/docker.ts
+++ typescript/infra/config/docker.ts
@@ -20,6 +20,7 @@ export const DockerImageRepos = Object.fromEntries(
 interface AgentDockerTags {
   relayer: string;
   relayerRC: string;
+  relayerFastPath: string;
   validator: string;
   validatorRC: string;
   scraper: string;
@@ -39,6 +40,7 @@ export const mainnetDockerTags: MainnetDockerTags = {
   // rust agents
   relayer: '6bbc7b5-20260326-002348',
   relayerRC: '6bbc7b5-20260326-002348',
+  relayerFastPath: '3754d4f-20260326-130321',
   validator: '910e8e8-20260318-204227',
   validatorRC: '910e8e8-20260318-204227',
   scraper: '910e8e8-20260318-204227',
@@ -54,6 +56,7 @@ export const testnetDockerTags: BaseDockerTags = {
   // rust agents
   relayer: '6bbc7b5-20260326-002348',
   relayerRC: '6bbc7b5-20260326-002348',
+  relayerFastPath: '3754d4f-20260326-130321',
   validator: '910e8e8-20260318-204227',
   validatorRC: '910e8e8-20260318-204227',
   scraper: '910e8e8-20260318-204227',
