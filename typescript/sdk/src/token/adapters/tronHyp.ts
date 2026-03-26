af2c6132cfb69153220b15ae20180e097e331e84
pbio <10051819+paulbalaji@users.noreply.github.com>
2026-03-26 14:05:09 +0000

    refactor: clean provider and hyp adapter internals

--- typescript/sdk/src/token/adapters/tronHyp.ts
+++ typescript/sdk/src/token/adapters/tronHyp.ts
@@ -1,20 +1,6 @@
-import {
-  EvmHypCollateralFiatAdapter,
-  EvmHypRebaseCollateralAdapter,
-  EvmHypSyntheticAdapter,
-  EvmHypSyntheticRebaseAdapter,
-  EvmHypXERC20Adapter,
-  EvmHypXERC20LockboxAdapter,
-  EvmMovableCollateralAdapter,
-  EvmHypNativeAdapter,
-} from './EvmTokenAdapter.js';
-import { EvmHypCrossCollateralAdapter } from './EvmCrossCollateralAdapter.js';
 import type { IHypTokenAdapter } from './ITokenAdapter.js';
-import {
-  hasChainMetadata,
-  hasOnlyHyperlaneConnections,
-  type HypTokenAdapterInput,
-} from './hypTokenAdapterUtils.js';
+import type { HypTokenAdapterInput } from './hypTokenAdapterUtils.js';
+import { createEvmLikeHypAdapter } from './evmLikeHyp.js';
 import type { ConfiguredMultiProtocolProvider as MultiProtocolProvider } from '../../providers/ConfiguredMultiProtocolProvider.js';
 import { TokenStandard } from '../TokenStandard.js';
 
@@ -22,62 +8,22 @@ export function createTronHypAdapter(
   multiProvider: MultiProtocolProvider<{ mailbox?: string }>,
   token: HypTokenAdapterInput,
 ): IHypTokenAdapter<unknown> | undefined {
-  const { standard, chainName, addressOrDenom } = token;
-
-  if (!standard || !hasChainMetadata(multiProvider, chainName)) {
-    return undefined;
-  }
-
-  if (
-    standard === TokenStandard.TronNative &&
-    hasOnlyHyperlaneConnections(token)
-  ) {
-    return new EvmHypNativeAdapter(chainName, multiProvider, {
-      token: addressOrDenom,
-    });
-  }
-
-  switch (standard) {
-    case TokenStandard.TronHypNative:
-      return new EvmHypNativeAdapter(chainName, multiProvider, {
-        token: addressOrDenom,
-      });
-    case TokenStandard.TronHypCollateral:
-    case TokenStandard.TronHypOwnerCollateral:
-      return new EvmMovableCollateralAdapter(chainName, multiProvider, {
-        token: addressOrDenom,
-      });
-    case TokenStandard.TronHypCrossCollateralRouter:
-      return new EvmHypCrossCollateralAdapter(chainName, multiProvider, {
-        token: addressOrDenom,
-      });
-    case TokenStandard.TronHypRebaseCollateral:
-      return new EvmHypRebaseCollateralAdapter(chainName, multiProvider, {
-        token: addressOrDenom,
-      });
-    case TokenStandard.TronHypCollateralFiat:
-      return new EvmHypCollateralFiatAdapter(chainName, multiProvider, {
-        token: addressOrDenom,
-      });
-    case TokenStandard.TronHypSynthetic:
-      return new EvmHypSyntheticAdapter(chainName, multiProvider, {
-        token: addressOrDenom,
-      });
-    case TokenStandard.TronHypSyntheticRebase:
-      return new EvmHypSyntheticRebaseAdapter(chainName, multiProvider, {
-        token: addressOrDenom,
-      });
-    case TokenStandard.TronHypXERC20:
-    case TokenStandard.TronHypVSXERC20:
-      return new EvmHypXERC20Adapter(chainName, multiProvider, {
-        token: addressOrDenom,
-      });
-    case TokenStandard.TronHypXERC20Lockbox:
-    case TokenStandard.TronHypVSXERC20Lockbox:
-      return new EvmHypXERC20LockboxAdapter(chainName, multiProvider, {
-        token: addressOrDenom,
-      });
-    default:
-      return undefined;
-  }
+  return createEvmLikeHypAdapter(multiProvider, token, {
+    native: TokenStandard.TronNative,
+    hypNative: TokenStandard.TronHypNative,
+    hypCollateral: [
+      TokenStandard.TronHypCollateral,
+      TokenStandard.TronHypOwnerCollateral,
+    ],
+    hypCrossCollateralRouter: TokenStandard.TronHypCrossCollateralRouter,
+    hypRebaseCollateral: TokenStandard.TronHypRebaseCollateral,
+    hypCollateralFiat: TokenStandard.TronHypCollateralFiat,
+    hypSynthetic: TokenStandard.TronHypSynthetic,
+    hypSyntheticRebase: TokenStandard.TronHypSyntheticRebase,
+    hypXerc20: [TokenStandard.TronHypXERC20, TokenStandard.TronHypVSXERC20],
+    hypXerc20Lockbox: [
+      TokenStandard.TronHypXERC20Lockbox,
+      TokenStandard.TronHypVSXERC20Lockbox,
+    ],
+  });
 }
