2549d941dde55208d8f0b0cacd4cd56950306719
Xaroz <jasonguo2013jg@gmail.com>
2026-03-27 12:16:02 -0500

    chore: fix eslint config, pnpm lock

--- typescript/warp-widget/examples/react-app/eslint.config.mjs
+++ typescript/warp-widget/examples/react-app/eslint.config.mjs
@@ -5,10 +5,10 @@ import tseslint from 'typescript-eslint';
 
 export default [
   { ignores: ['dist', 'node_modules'] },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
   {
     files: ['src/**/*.{ts,tsx}'],
-    ...js.configs.recommended,
-    ...tseslint.configs.recommended,
     plugins: {
       react: reactPlugin,
       'react-hooks': reactHooks,
