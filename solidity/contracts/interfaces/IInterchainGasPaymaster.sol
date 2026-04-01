7ced6bb814c74081501059d7e68d7e4690f9f9ea
Yorke Rhodes IV <yorke@hyperlane.xyz>
2026-04-01 18:03:45 -0400

    Restore audit branch IGP interface

--- solidity/contracts/interfaces/IInterchainGasPaymaster.sol
+++ solidity/contracts/interfaces/IInterchainGasPaymaster.sol
@@ -64,7 +64,7 @@ interface IInterchainGasPaymaster {
         bytes32 _messageId,
         uint32 _destinationDomain,
         uint256 _gasAmount
-    ) external payable;
+    ) external;
 
     function quoteGasPayment(
         uint32 _destinationDomain,
