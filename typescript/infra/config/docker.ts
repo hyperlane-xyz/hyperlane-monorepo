2d2f93c42ba3a58aaf505f5e385caf1da56df01a
pbio <10051819+paulbalaji@users.noreply.github.com>
2026-04-06 15:27:44 +0100

    update agents

--- typescript/infra/config/docker.ts
+++ typescript/infra/config/docker.ts
@@ -38,12 +38,12 @@ interface MainnetDockerTags extends BaseDockerTags {
 
 export const mainnetDockerTags: MainnetDockerTags = {
   // rust agents
-  relayer: '282d921-20260331-121156',
-  relayerRC: '282d921-20260331-121156',
-  relayerFastPath: '282d921-20260331-121156',
-  validator: '282d921-20260331-121156',
-  validatorRC: '282d921-20260331-121156',
-  scraper: '282d921-20260331-121156',
+  relayer: '7eb690c-20260406-142107',
+  relayerRC: '7eb690c-20260406-142107',
+  relayerFastPath: '7eb690c-20260406-142107',
+  validator: '7eb690c-20260406-142107',
+  validatorRC: '7eb690c-20260406-142107',
+  scraper: '7eb690c-20260406-142107',
   // monorepo services
   checkWarpDeploy: 'main',
   // standalone services
@@ -54,12 +54,12 @@ export const mainnetDockerTags: MainnetDockerTags = {
 
 export const testnetDockerTags: BaseDockerTags = {
   // rust agents
-  relayer: '1663fd7-20260327-165421',
-  relayerRC: '1663fd7-20260327-165421',
-  relayerFastPath: '1663fd7-20260327-165421',
-  validator: '910e8e8-20260318-204227',
-  validatorRC: '910e8e8-20260318-204227',
-  scraper: '910e8e8-20260318-204227',
+  relayer: '7eb690c-20260406-142107',
+  relayerRC: '7eb690c-20260406-142107',
+  relayerFastPath: '7eb690c-20260406-142107',
+  validator: '7eb690c-20260406-142107',
+  validatorRC: '7eb690c-20260406-142107',
+  scraper: '7eb690c-20260406-142107',
   // standalone services
   keyFunder: '3b17358-20260315-183126',
 };
