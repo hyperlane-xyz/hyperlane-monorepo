7a28c9b1b3eb4de2b5931f2e39de10a8d1e0b484
Mo Hussan <22501692+Mo-Hussain@users.noreply.github.com>
2026-04-13 11:09:06 +0100

    feat(infra): add IGP hook to USDTSTAGE solanamainnet config

--- typescript/infra/config/environments/mainnet3/warp/configGetters/getUSDTSTAGEWarpConfig.ts
+++ typescript/infra/config/environments/mainnet3/warp/configGetters/getUSDTSTAGEWarpConfig.ts
@@ -71,6 +71,7 @@ export async function getUSDTSTAGEWarpConfig(
       gas: SEALEVEL_WARP_ROUTE_HANDLER_GAS_AMOUNT,
       owner: SOLANA_OWNER,
       decimals: 6,
+      hook: 'AkeHBbE5JkwVppujCQQ6WuxsVsJtruBAjUo6fDCFp6fF',
     },
     mode: {
       ...routerConfig.mode,
