ad829097bc5e879192a01c5c54fab16ac8a47eb4
pbio <10051819+paulbalaji@users.noreply.github.com>
2026-04-16 15:01:26 +0100

    fix(relayer): handle ccip null metadata

--- typescript/relayer/src/metadata/builder.ts
+++ typescript/relayer/src/metadata/builder.ts
@@ -65,6 +65,7 @@ export class BaseMetadataBuilder implements MetadataBuilder {
       case IsmType.TEST_ISM:
       case IsmType.OP_STACK:
       case IsmType.PAUSABLE:
+      case IsmType.CCIP:
         return this.nullMetadataBuilder.build({ ...context, ism });
 
       case IsmType.MERKLE_ROOT_MULTISIG:
