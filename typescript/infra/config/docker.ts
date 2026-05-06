e50ebff8fed9c488dae7c95ef0191d4ded7549fc
Troy Kessler <troy.kessler99@gmail.com>
2026-05-06 17:43:03 +0200

    feat: add new testnet images

--- typescript/infra/config/docker.ts
+++ typescript/infra/config/docker.ts
@@ -57,9 +57,9 @@ export const mainnetDockerTags: MainnetDockerTags = {
 
 export const testnetDockerTags: BaseDockerTags = {
   // rust agents
-  relayer: '668b2ee-20260430-135346',
-  relayerRC: '668b2ee-20260430-135346',
-  relayerFastPath: '668b2ee-20260430-135346',
+  relayer: '81d5295-20260506-151439',
+  relayerRC: '81d5295-20260506-151439',
+  relayerFastPath: '81d5295-20260506-151439',
   validator: '7eb690c-20260406-142107',
   validatorRC: '7eb690c-20260406-142107',
   scraper: 'caa8162-20260409-132508',
