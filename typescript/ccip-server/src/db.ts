2c734c8a70237bf0b4aedefb7a6b2d14e20eaad1
pbio <10051819+paulbalaji@users.noreply.github.com>
2026-05-20 12:53:59 +0100

    Upgrade Prisma to 7.8

--- typescript/ccip-server/src/db.ts
+++ typescript/ccip-server/src/db.ts
@@ -1,8 +1,6 @@
 import { PrismaPg } from '@prisma/adapter-pg';
-import pg from 'pg';
 
 import { PrismaClient } from './generated/prisma/client.js';
 
-const pool = new pg.Pool({ connectionString: process.env.DATABASE_URL });
-const adapter = new PrismaPg(pool);
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
 export const prisma = new PrismaClient({ adapter });
