4950003ee59e93959b712a14481828edae7de874
Troy Kessler <troy.kessler99@gmail.com>
2026-05-28 14:03:23 +0200

    fix: omit recipient in rate limit ism read

--- typescript/sdk/src/ism/EvmIsmReader.ts
+++ typescript/sdk/src/ism/EvmIsmReader.ts
@@ -660,13 +660,12 @@ export class EvmIsmReader extends HyperlaneReader implements IsmReader {
       this.provider,
     );
     try {
-      const recipient = await rateLimitedIsm.recipient();
+      await rateLimitedIsm.recipient();
       const maxCapacity = (await rateLimitedIsm.maxCapacity()).toString();
       const owner = await rateLimitedIsm.owner();
       return {
         address,
         type: IsmType.RATE_LIMITED,
-        recipient,
         maxCapacity,
         owner,
       };
