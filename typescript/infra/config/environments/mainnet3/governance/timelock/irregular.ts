d6e1c4684427f63b972ffb8350a4568cf0bc9e04
pbio <10051819+paulbalaji@users.noreply.github.com>
2026-08-15 08:54:14 +0100

    fix: checksum irregular timelock address

--- typescript/infra/config/environments/mainnet3/governance/timelock/irregular.ts
+++ typescript/infra/config/environments/mainnet3/governance/timelock/irregular.ts
@@ -3,5 +3,5 @@ import { Address } from '@hyperlane-xyz/utils';
 
 export const irregularTimelocks: ChainMap<Address> = {
   // Apr 18, 2025 - Timelock for the Ethereum "Irregular" Safe
-  ethereum: '0xfa842F02439AF6D91D7d44525956f9E5E00e339F',
+  ethereum: '0xfA842f02439Af6d91d7D44525956F9E5e00e339f',
 };
