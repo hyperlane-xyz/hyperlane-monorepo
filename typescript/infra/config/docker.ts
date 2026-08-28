3c70166287e71dceab9a6b4cfd73d2e640873021
Mo Hussan <22501692+Mo-Hussain@users.noreply.github.com>
2026-08-28 15:17:19 +0100

    chore(rebalancer): pin oUSDT deployment image

--- typescript/infra/config/docker.ts
+++ typescript/infra/config/docker.ts
@@ -56,7 +56,7 @@ export const mainnetDockerTags: MainnetDockerTags = {
   // standalone services
   keyFunder: 'b0c3c5d-20260804-175736',
   warpMonitor: '744b3bb-20260521-215958',
-  rebalancer: 'da26d9a-20260703-122943',
+  rebalancer: 'e9d67ae-20260828-115602',
   scraperProxy: '220f01c-20260827-145329',
   feeQuoting: '12d899d-20260325-184337',
 };
