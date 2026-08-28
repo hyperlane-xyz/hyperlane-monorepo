f9849eb7b911aa6df805e8cd137d8e40f09f31ec
Mo Hussan <22501692+Mo-Hussain@users.noreply.github.com>
2026-08-28 11:44:21 +0100

    feat(rebalancer): execute swaps.xyz Solana transactions

--- typescript/rebalancer/src/factories/RebalancerContextFactory.ts
+++ typescript/rebalancer/src/factories/RebalancerContextFactory.ts
@@ -688,6 +688,8 @@ export class RebalancerContextFactory {
                 apiUrl: swapsxyzConfig.apiUrl,
                 defaultSlippage: swapsxyzConfig.defaultSlippage,
                 maxQuoteLossBps: swapsxyzConfig.maxQuoteLossBps,
+                maxSolanaNativeSpendLamports:
+                  swapsxyzConfig.maxSolanaNativeSpendLamports,
                 chainMetadata: this.multiProvider.metadata,
               },
               this.logger,
