ed7d5186687c1aa7e21d779507b9ef8c029cc66b
pbio <10051819+paulbalaji@users.noreply.github.com>
2026-09-02 20:47:05 +0100

    chore(infra): update mainnet relayer image

--- typescript/infra/config/docker.ts
+++ typescript/infra/config/docker.ts
@@ -43,9 +43,9 @@ interface MainnetDockerTags extends BaseDockerTags {
 
 export const mainnetDockerTags: MainnetDockerTags = {
   // rust agents
-  relayer: '6e2a9dc-20260902-184618',
-  relayerRC: '6e2a9dc-20260902-184618',
-  relayerFastPath: '6e2a9dc-20260902-184618',
+  relayer: 'c9a3852-20260902-193249',
+  relayerRC: 'c9a3852-20260902-193249',
+  relayerFastPath: 'c9a3852-20260902-193249',
   validator: 'c35e861-20260831-121837',
   validatorRC: 'c35e861-20260831-121837',
   validatorFastPath: 'c35e861-20260831-121837',
