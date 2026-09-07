71ef56b022deb808c4cdafe55a78e0feb783d5bb
xeno097 <xeno097.cp@gmail.com>
2026-09-07 14:30:30 -0400

    revert: remove unreliable Ethereum test RPC

--- typescript/sdk/src/consts/testChains.ts
+++ typescript/sdk/src/consts/testChains.ts
@@ -237,7 +237,6 @@ export const ethereumTestChain: ChainMetadata = {
   },
   protocol: ProtocolType.Ethereum,
   rpcUrls: [
-    { http: 'https://rpc.mevblocker.io' },
     { http: 'https://ethereum-rpc.publicnode.com' },
     { http: 'https://eth.llamarpc.com' },
     { http: 'https://1rpc.io/eth' },
