48f509cd46278c9a13ba02cbf49d5d3a7f7bb347
pbio <10051819+paulbalaji@users.noreply.github.com>
2026-09-11 10:40:46 +0100

    chore(infra): update scraper proxy image tags

--- typescript/infra/config/docker.ts
+++ typescript/infra/config/docker.ts
@@ -57,7 +57,7 @@ export const mainnetDockerTags: MainnetDockerTags = {
   keyFunder: 'fc544bf-20260908-174702',
   warpMonitor: 'fc544bf-20260908-174702',
   rebalancer: 'fc544bf-20260908-174702',
-  scraperProxy: 'fc544bf-20260908-174702',
+  scraperProxy: '7762855-20260911-015136',
   feeQuoting: '12d899d-20260325-184337',
 };
 
@@ -72,5 +72,5 @@ export const testnetDockerTags: BaseDockerTags = {
   scraper: 'b83bac5-20260909-225045',
   // standalone services
   keyFunder: 'fc544bf-20260908-174702',
-  scraperProxy: 'fc544bf-20260908-174702',
+  scraperProxy: '7762855-20260911-015136',
 };
