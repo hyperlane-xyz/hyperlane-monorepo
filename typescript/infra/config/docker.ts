71258895dbe68b2c4a21ee3d5b3da753197bd5cc
pbio <10051819+paulbalaji@users.noreply.github.com>
2026-09-24 12:15:42 +0100

    chore(infra): update scraper proxy image tags for near-head

--- typescript/infra/config/docker.ts
+++ typescript/infra/config/docker.ts
@@ -57,7 +57,7 @@ export const mainnetDockerTags: MainnetDockerTags = {
   keyFunder: 'fc544bf-20260908-174702',
   warpMonitor: 'fc544bf-20260908-174702',
   rebalancer: 'fc544bf-20260908-174702',
-  scraperProxy: '9e61e65-20260911-233459',
+  scraperProxy: 'f05bb6d-20260923-151825',
   feeQuoting: '12d899d-20260325-184337',
 };
 
@@ -72,5 +72,5 @@ export const testnetDockerTags: BaseDockerTags = {
   scraper: '2d4abed-20260924-092729',
   // standalone services
   keyFunder: 'fc544bf-20260908-174702',
-  scraperProxy: '9e61e65-20260911-233459',
+  scraperProxy: 'f05bb6d-20260923-151825',
 };
